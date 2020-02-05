8ed0b72e36c4bb505dd42cd6fd81fa24be4bc55c
pad <yoann.padioleau@gmail.com>
2020-02-05 16:09:35 +0100

    Typed metavar (part 2, sgrep part) for literals.
    This fixes issue #49.
    
    Test plan:
    test file included
    make test

---
+++ lib/typechecking_generic.ml
@@ -0,0 +1,45 @@
+(* Yoann Padioleau
+ *
+ * Copyright (C) 2020 r2c
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License (GPL)
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * file license.txt for more details.
+ *)
+open Ast_generic
+
+(*****************************************************************************)
+(* Prelude *)
+(*****************************************************************************)
+(* Poor's man typechecker on literals (for now).
+ *
+ * todo: 
+ *  - local type inference on AST generic? good coverage?
+ *  - we could allow metavars on the type itself, as in
+ *    foo($X: $T) ... $T x; ...
+ *    which would require to transform the code in the generic_vs_generic
+ *    style as typechecking could also bind metavariables in the process
+ *)
+
+(*****************************************************************************)
+(* Entry point *)
+(*****************************************************************************)
+
+(* very Python specific for now where the type is currently an OT_Expr
+ * todo: fill Ast_generic.expr_to_type at least.
+ *)
+let compatible_type t e =
+  match t, e with
+  | OtherType (OT_Expr, [E (Name ((("int", _tok), _nameinfo), _idinfo))]),
+    L (Int _) -> true
+  | OtherType (OT_Expr, [E (Name ((("float", _tok), _nameinfo), _idinfo))]),
+    L (Float _) -> true
+  | OtherType (OT_Expr, [E (Name ((("str", _tok), _nameinfo), _idinfo))]),
+    L (String _) -> true
+
+  | _ -> false
---
+++ lib/typechecking_generic.mli
@@ -0,0 +1,2 @@
+
+val compatible_type: Ast_generic.type_ -> Ast_generic.expr -> bool
