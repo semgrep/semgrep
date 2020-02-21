22ac174c3ca74a860a7bfc875ebab015e5bd5dfc
Yoann Padioleau <pad@returntocorp.com>
2020-02-21 16:14:20 +0100

    * tests/go/equivalence_naming_import.go: test file! (#190)
    show that it works for go for issue #162

--- tests/go/equivalence_naming_import.go
+++ tests/go/equivalence_naming_import.go
@@ -1,11 +1,10 @@
-package foo
+package Foo
+
 import sub "subprocess"
 
 func foo() {
   //ERROR:
   result = subprocess.open("ls")
-  //ERROR:
-  result = sub_open("ls")
   //ERROR:
   result = sub.open("ls")
 
