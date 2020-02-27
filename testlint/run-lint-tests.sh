0f3d9d1de36cf089fd4968fbede9af79d86b06a7
Isaac Evans <409041+ievans@users.noreply.github.com>
2020-02-27 08:26:11 -0800

    pattern-where-python (#198)
    * where-python pattern
    
    * complete port to SRange
    * flags.dangerously_allow_arbitrary_code_execution_from_rules
    * switich to a class for rule expression
    * avoid validating patterns that don't need to be sent to sgrep
    * clearer error messages for invalid YAML files (#215)
    * better error messages and tests, close issue #210
    
    Co-authored-by: Clint Gibler <clintgibler@users.noreply.github.com>

--- testlint/run-lint-tests.sh
+++ testlint/run-lint-tests.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$(realpath "$0")")";
 PYTHONPATH=.. python3 test_lint.py
 
 cd ..
-./sgrep.py --json --strict --config testlint/python/eqeq.yaml tests/lint -o tmp.out
+./sgrep.py --json --strict --config testlint/python/eqeq.yaml tests/lint -o tmp.out >/dev/null
 diff tmp.out ./testlint/python/eqeq.expected.json
 rm -f tmp.out
 
@@ -15,6 +15,9 @@ rm -f tmp.out
 
 # parsing bad2.yaml should fail 
 ./sgrep.py --strict --config testlint/python/bad2.yaml tests/lint && echo "bad2.yaml should have failed" && exit 1
+
+# parsing bad3.yaml should fail 
+./sgrep.py --strict --config testlint/python/bad3.yaml tests/lint && echo "bad3.yaml should have failed" && exit 1
 
 
 echo "-----------------------"
