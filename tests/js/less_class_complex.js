8e2e3184ca95a8705b96b7b61e11f3f7328c294f
Ulzii Otgonbaatar <ulzii@returntocorp.com>
2020-03-04 15:18:51 -0800

    use ellipsis in class body

--- tests/js/less_class_complex.js
+++ tests/js/less_class_complex.js
@@ -1,6 +1,17 @@
 //ERROR:
 class A {
-  foo = "bar";
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  foo() {
+    return 'foo';
+  }
+}
+
+// OK:
+class B {
 
   constructor(name) {
     this.name = name;
