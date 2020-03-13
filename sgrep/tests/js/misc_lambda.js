7f13f2129d16b379fcc73bbe83f62815367198f0
Yoann Padioleau <pad@returntocorp.com>
2020-03-13 16:13:52 +0100

    Test files showing sgrep can now parse lambda patterns (#288)
    This show issue #226 is now fixed
    
    Test plan:
    test files included

---
+++ sgrep/tests/js/misc_lambda.js
@@ -0,0 +1,23 @@
+var foo = "bar";
+
+// not a Lambda
+function foo () {
+    console.log(foo);
+}
+
+// not allowed by Ast_js_build
+//function () {
+//    console.log(foo);
+//}
+
+// still not parsed as a Lambda, even though it looks like, because
+// this common idiom is handled specially by Ast_js_build
+var a = function () {
+    console.log(foo);
+};
+
+// this is finally parsed as a Lambda
+var b = foo(
+   //ERROR: match
+   function () { console.log("here"); }
+);
