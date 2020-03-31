75290cf5e1e93051bf8c5ea3c8cc76ce63416c01
brendon <brendon.go@gmail.com>
2020-03-30 17:13:24 -0700

    Use --enable-shared when building python3.7

--- sgrep_lint/build.sh
+++ sgrep_lint/build.sh
@@ -4,16 +4,11 @@ set -e
 THIS_DIR="$(dirname "$(realpath "$0")")";
 cd "${THIS_DIR}"
 
-git clone -b develop https://github.com/Nuitka/Nuitka
-cd Nuitka
-git checkout 09ea153bd33293c622cda6fe44c06f970fe82886
-python3.7 setup.py install
-cd ..
-
+pip3 install Nuitka==0.6.7
 pip3 install -r requirements.txt
 
 echo "building and placing output binary in ${1}"
-python3.7 -m nuitka --follow-imports --standalone --recurse-stdlib --show-modules --recurse-to=colorama --output-dir="${1}" --warn-unusual-code --lto ./sgrep.py
+python3.7 -m nuitka --follow-imports --standalone --show-modules --recurse-to=colorama --output-dir="${1}" --warn-unusual-code --lto ./sgrep.py
 
 # rename to sgrep-lint
 mv "${1}/sgrep.dist/sgrep" "${1}/sgrep.dist/sgrep-lint-exe"
