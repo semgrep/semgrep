51e96d905200bb5caeef701deb0fbd959492250e
Russell Cohen <russell.r.cohen@gmail.com>
2020-04-15 16:35:46 -0400

    Adds ubuntu installers
    These are not fancy but they work.

---
+++ install-scripts/ubuntu-generic.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+version=$VERSION
+tarball=sgrep-$version-ubuntu-16.04.tgz
+tarball_url=https://github.com/returntocorp/sgrep/releases/download/v$version/$tarball
+sha_url=https://github.com/returntocorp/sgrep/releases/download/v$version/$tarball.sha256
+
+
+
+if ! out="$(semgrep --version 2>/dev/null)" || [[ "$out" != "$version" ]]
+then
+    tmpdir="$(mktemp -d)"
+    trap 'rm -r "$tmpdir"' EXIT
+    cd "$tmpdir"
+    wget -nv "$tarball_url"
+    sha256=$(curl -L "$sha_url" | awk '{ print $1 }')
+    sha256sum -c <<< "$sha256  $tarball"
+    # Be gentle on `/usr/local/lib`
+    sudo tar --skip-old-files -xzf  "$tarball" -C /usr/local/lib/
+    sudo ln -sf /usr/local/lib/sgrep-lint-files/sgrep-lint /usr/local/bin/semgrep
+    sudo ln -sf /usr/local/lib/sgrep-lint-files/sgrep /usr/local/bin/sgrep
+fi
+
+echo "Semgrep installed! Version: $(semgrep --version)"
+
+echo "def silly_eq(a, b):" >> /tmp/test.py
+echo " return a + b == a + b" >> /tmp/test.py
+
+echo -n "Testing your semgrep installation..."
+semgrep /tmp/test.py -l python -e '$X == $X' | grep 'a + b' > /dev/null || (echo "Something seems wrong with your semgrep install" && exit 1)
+echo "OK! Enjoy semgrep :-)"
---
+++ install-scripts/validate-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "def silly_eq(a, b):" >> /tmp/test.py
+echo " return a + b == a + b" >> /tmp/test.py
+
+echo -n "Testing your semgrep installation..."
+semgrep /tmp/test.py -l python -e '$X == $X' | grep "return a + b == a + b"
+echo "OK! Enjoy semgrep :-)"
