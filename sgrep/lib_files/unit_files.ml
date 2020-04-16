061989cba2ea51955612ae0a014341212c7821ba
Drew Dennison <drew@returntocorp.com>
2020-04-15 21:30:11 -0700

    remove old comment

--- sgrep/lib_files/unit_files.ml
+++ sgrep/lib_files/unit_files.ml
@@ -17,7 +17,7 @@ let unittest =
         let filters = Files_filter.mk_filters
           ~excludes:["*.{c,h}"; "*.go"]
           ~includes:["foo.*"]
-          ~exclude_dirs:["a/c"] in  (* I would like this to be 'a/c' so a/b/c still exists but if I change to 'a/c' then 'a/c/d/foo.js doesn't get filtered out *)
+          ~exclude_dirs:["a/c"] in
         assert_equal ~msg:"it should filter files"
           ["a/b/foo.js"; "a/b/c/foo.js";]
           (Files_filter.filter filters files)
