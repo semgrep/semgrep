1f0773302afbdfbd353ee5f4fb2e74b259694bd5
Russell Cohen <russell.r.cohen@gmail.com>
2020-04-16 11:26:47 -0400

    Factor out OSX release script

---
+++ release-scripts/osx-release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+brew install opam pkg-config coreutils
+opam init --no-setup --bare;
+opam switch create 4.07.1;
+opam switch 4.07.1;
+eval $(opam env); opam install -y reason dune ocamlfind camlp4 num ocamlgraph json-wheel conf-perl yaml
+git submodule update --init --recursive
+eval $(opam env) && opam install -y ./pfff
+eval $(opam env) && cd sgrep && opam install -y && make all && make test && make install
+mkdir -p artifacts
+cp ./sgrep/_build/default/bin/main_sgrep.exe artifacts/semgrep
+zip -r artifacts.zip artifacts
