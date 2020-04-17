d90cbcce5a7f28483ab4644aefa59eb5a0c60de0
Russell Cohen <russell.r.cohen@gmail.com>
2020-04-17 17:14:09 -0400

    Fix the ubuntu 16.04 build job (#496)
    * cd into github before initializing submodules
    
    * run on PR for testing
    
    * remove git installation
    
    * Break out git & python installs separately
    
    * Update the job
    
    * Add Python installation step
    
    * more installs never hurt anyone
    
    * oy
    
    * enough deps now?
    
    * enough deps now?
    
    * give it a temp dir
    
    * Fix path
    
    * give up on GH initialized submoudles
    
    * log some stuff
    
    * too much help
    
    * Add missing opam install -y
    
    * remove duplicate &&
    
    * Fix file name
    
    * more file name fixups
    
    * Dangerously close to working!
    
    * Add shebang to the Python install script
    
    * Don't run on PRs

--- release-scripts/ubuntu-release.sh
+++ release-scripts/ubuntu-release.sh
@@ -1,43 +1,20 @@
 #!/bin/bash
 set -e
 
-sudo apt-get update && sudo apt-get install -y --no-install-recommends make m4 perl wget swi-prolog mercurial pkg-config build-essential
-sudo apt-get install -y zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libbz2-dev
-sudo wget https://www.python.org/ftp/python/3.7.7/Python-3.7.7.tar.xz
-sudo tar xvf Python-3.7.7.tar.xz
-cd Python-3.7.7
-sudo ./configure --enable-shared
-sudo make altinstall
-sudo ldconfig /usr/local/lib
-python3.7 --version
-which python3.7
-/usr/local/bin/python3.7 -c "import bz2; print(bz2.__doc__)"
-sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-sudo -H python3.7 get-pip.py
-pip3 --version
-ldd --version
-echo "Pls help me make this at least 3.7"
+echo "here's some help"
+ls
+echo "---------"
 sudo apt-get install -y --no-install-recommends libcurl4-openssl-dev libexpat1-dev gettext libz-dev libssl-dev build-essential autoconf
-cd /usr/src/
-sudo wget https://github.com/git/git/archive/v2.18.0.tar.gz -O git.tar.gz
-sudo tar -xf git.tar.gz
-cd git-*
-sudo make prefix=/usr/local all;
-sudo make prefix=/usr/local install;
-git --version
-sudo chmod -R 777 . /github
 opam switch --root /home/opam/.opam 4.07;
-cd ..
 
 eval "$(opam env --root /home/opam/.opam --set-root)"; opam install -y reason dune ocamlfind camlp4 num ocamlgraph json-wheel conf-perl yaml
-git submodule update --init --recursive
 eval "$(opam env --root /home/opam/.opam --set-root)" && opam install -y ./pfff
-eval "$(opam env --root /home/opam/.opam --set-root)" && cd sgrep && make all && cd ..
+eval "$(opam env --root /home/opam/.opam --set-root)" && cd sgrep && opam install -y . && make all && cd ..
 eval "$(opam env --root /home/opam/.opam --set-root)" && cd sgrep_lint && export PATH=/github/home/.local/bin:$PATH && sudo make all && cd ..
 mkdir -p semgrep-lint-files
-cp ./sgrep/_build/default/bin/main_sgrep.exe sgrep-lint-files/semgrep
-cp -r ./sgrep_lint/build/sgrep.dist/* sgrep-lint-files
-chmod +x sgrep-lint-files/semgrep
-chmod +x sgrep-lint-files/semgrep-lint
-chmod +x sgrep-lint-files/__main__
+cp ./sgrep/_build/default/bin/main_sgrep.exe semgrep-lint-files/sgrep
+cp -r ./sgrep_lint/build/sgrep.dist/* semgrep-lint-files
+ls semgrep-lint-files
+chmod +x semgrep-lint-files/sgrep
+chmod +x semgrep-lint-files/sgrep-lint
 tar -cvzf artifacts.tar.gz semgrep-lint-files/
