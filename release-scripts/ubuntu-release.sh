dc4514b19fc1b10c36747f16524cb5ac180489cc
Russell Cohen <russell.r.cohen@gmail.com>
2020-04-17 13:40:59 -0400

    Fix file name

--- release-scripts/ubuntu-release.sh
+++ release-scripts/ubuntu-release.sh
@@ -12,9 +12,9 @@ eval "$(opam env --root /home/opam/.opam --set-root)" && opam install -y ./pfff
 eval "$(opam env --root /home/opam/.opam --set-root)" && cd sgrep && opam install -y . && make all && cd ..
 eval "$(opam env --root /home/opam/.opam --set-root)" && cd sgrep_lint && export PATH=/github/home/.local/bin:$PATH && sudo make all && cd ..
 mkdir -p semgrep-lint-files
-cp ./sgrep/_build/default/bin/main_sgrep.exe sgrep-lint-files/semgrep
-cp -r ./sgrep_lint/build/sgrep.dist/* sgrep-lint-files
-chmod +x sgrep-lint-files/semgrep
-chmod +x sgrep-lint-files/semgrep-lint
-chmod +x sgrep-lint-files/__main__
+cp ./sgrep/_build/default/bin/main_sgrep.exe semgrep-lint-files/semgrep
+cp -r ./sgrep_lint/build/sgrep.dist/* semgrep-lint-files
+chmod +x semgrep-lint-files/semgrep
+chmod +x semgrep-lint-files/semgrep-lint
+chmod +x semgrep-lint-files/__main__
 tar -cvzf artifacts.tar.gz semgrep-lint-files/
