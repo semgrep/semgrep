59414e7869edfaf30a7ad83679d3f50b02948843
Russell Cohen <russell.r.cohen@gmail.com>
2020-04-17 14:35:46 -0400

    Dangerously close to working!

--- release-scripts/install-python.sh
+++ release-scripts/install-python.sh
@@ -2,11 +2,10 @@ sudo apt-get update && sudo apt-get install -y --no-install-recommends make m4 p
 sudo apt-get install -y zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libbz2-dev
 sudo wget https://www.python.org/ftp/python/3.7.7/Python-3.7.7.tar.xz
 sudo tar xvf Python-3.7.7.tar.xz
-cd Python-3.7.7
+(cd Python-3.7.7 || exit
 sudo ./configure --enable-shared
 sudo make altinstall
-sudo ldconfig /usr/local/lib
-cd ..
+sudo ldconfig /usr/local/lib)
 python3.7 --version
 which python3.7
 /usr/local/bin/python3.7 -c "import bz2; print(bz2.__doc__)"
