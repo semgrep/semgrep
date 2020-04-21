7fb3831aca5d7c8a6ad1d27486eab51c080fce71
Russell Cohen <russell.r.cohen@gmail.com>
2020-04-21 18:26:28 -0400

    Release job fixes

--- release-scripts/ubuntu-release.sh
+++ release-scripts/ubuntu-release.sh
@@ -10,11 +10,13 @@ opam switch --root /home/opam/.opam 4.07;
 eval "$(opam env --root /home/opam/.opam --set-root)"; opam install -y reason dune ocamlfind camlp4 num ocamlgraph json-wheel conf-perl yaml
 eval "$(opam env --root /home/opam/.opam --set-root)" && opam install -y ./pfff
 eval "$(opam env --root /home/opam/.opam --set-root)" && cd semgrep-core && opam install -y . && make all && cd ..
-eval "$(opam env --root /home/opam/.opam --set-root)" && cd semgrep && export PATH=/github/home/.local/bin:$PATH && sudo make all && cd ..
+eval "$(opam env --root /home/opam/.opam --set-root)" && cd sgrep_lint && export PATH=/github/home/.local/bin:$PATH && sudo make all && cd ..
 mkdir -p semgrep-lint-files
 cp ./semgrep-core/_build/default/bin/main_sgrep.exe semgrep-lint-files/semgrep-core
-cp -r ./semgrep/build/sgrep.dist/* semgrep-lint-files
+cp -r ./sgrep_lint/build/sgrep.dist/* semgrep-lint-files
 ls semgrep-lint-files
+# TODO: remove once the Python build job makes something named `semgrep`
+mv semgrep-lint-files/sgrep-lint semgrep-lint-files/semgrep
 chmod +x semgrep-lint-files/semgrep-core
 chmod +x semgrep-lint-files/semgrep
 tar -cvzf artifacts.tar.gz semgrep-lint-files/
