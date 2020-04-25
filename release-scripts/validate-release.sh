9c3013985051a431eb75e3dc9d8fa02cbd7ea2ab
Russell Cohen <russell.r.cohen@gmail.com>
2020-04-24 23:49:18 -0400

    Fix invocation of Python script

--- release-scripts/validate-release.sh
+++ release-scripts/validate-release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eox
 curl https://api.github.com/repos/returntocorp/semgrep/releases | jq ".[] | select(.tag_name == \"$(cat version)\")" > release.json
-exec ./validate-release.py
+exec ./release-scripts/validate-release.py
