9f1ad5f70de6e5e6396279f980f4ab028aa42ea5
pad <yoann.padioleau@gmail.com>
2020-04-26 15:33:53 +0200

    update path to main_sgrep.exe

--- release-scripts/osx-release.sh
+++ release-scripts/osx-release.sh
@@ -9,5 +9,5 @@ git submodule update --init --recursive
 eval "$(opam env)" && opam install -y ./pfff
 cd semgrep-core && eval "$(opam env)" && opam install -y . && make all && make test && make install && cd ..
 mkdir -p artifacts
-cp ./semgrep-core/_build/default/bin/main_sgrep.exe artifacts/semgrep-core
+cp ./semgrep-core/_build/default/bin/main_semgrep_core.exe artifacts/semgrep-core
 zip -r artifacts.zip artifacts
