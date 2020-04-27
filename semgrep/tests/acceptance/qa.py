4bf6d31627df8b6d859f7eff01306b40f5a16c33
brendon <brendon.go@gmail.com>
2020-04-27 01:31:34 -0700

    fixup! Add regression testing infrastructure

--- semgrep/tests/acceptance/qa.py
+++ semgrep/tests/acceptance/qa.py
@@ -18,31 +18,15 @@ import yaml
 
 
 @contextmanager
-def git_checkout(url: str, commit_hash: str) -> Iterator[str]:
+def git_checkout(url: str, commit_hash: Optional[str] = None) -> Iterator[str]:
     """
-        Clones URL into destination and checks out commit_hash
+        Clones URL into destination
 
         Returns name of directory url was cloned into
     """
     with tempfile.TemporaryDirectory() as destination:
-        print("starting clone")
         subprocess.run(
-            ["git", "clone", url, destination],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            check=True,
-        )
-        print("done clone")
-        subprocess.run(
-            ["git", "checkout", commit_hash],
-            cwd=destination,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            check=True,
-        )
-        subprocess.run(
-            ["git", "clean", "-xdf"],
-            cwd=destination,
+            ["git", "clone", "--depth=1", url, destination],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             check=True,
@@ -52,24 +36,21 @@ def git_checkout(url: str, commit_hash: str) -> Iterator[str]:
 
 def run_repo(target: str, rewrite: bool = False) -> None:
     """
-    Runs semgrep on github repo at commithash in target/config.yaml
-    Runs rules in target/rule.yaml
+    Runs semgrep on github repo with a simple pattrern
 
-    Assert output is the same as target/expected
+    Check that semgrep did not into any errors parsing targets
     """
-    with open(f"tests/acceptance/{target}/config.yaml") as file:
-        info = yaml.safe_load(file)
-        print(info)
-
-    target_repo = info["repo_url"]
-    target_hash = info["commit_hash"]
-    expected_output_file = f"tests/acceptance/{target}/expected"
-    actual_output_file = f"tests/acceptance/{target}/actual"
-
-    with git_checkout(target_repo, target_hash) as target_dir:
+    with git_checkout(target) as target_dir:
         config_dir = Path(f"tests/acceptance/{target}/rule.yaml")
 
-        command = ["semgrep", f"--config={config_dir.resolve()}", "--json", "."]
+        command = [
+            "semgrep",
+            f"--pattern=$X==$X",
+            "--lang=python",
+            "--lang=javascript",
+            "--json",
+            ".",
+        ]
 
         runned = subprocess.run(
             command,
@@ -79,35 +60,126 @@ def run_repo(target: str, rewrite: bool = False) -> None:
             encoding="utf-8",
         )
 
-        actual_json = json.loads(runned.stdout)
+        # if runned.returncode != 0:
+        #     print(runned.stderr)
 
-        if rewrite:
-            with open(expected_output_file, "w") as file:
-                json.dump(actual_json, file, sort_keys=True, indent=4)
-
-        with open(actual_output_file, "w") as file:
-            json.dump(actual_json, file, sort_keys=True, indent=4)
-
-        diff_run = subprocess.run(
-            ["diff", actual_output_file, expected_output_file],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            encoding="utf-8",
-        )
-        os.remove(actual_output_file)
-        if diff_run.returncode != 0:
-            print(diff_run.stdout)
-        assert diff_run.returncode == 0
+        output = json.loads(runned.stdout)
+        assert "results" in output
+        print(output["errors"])
+        assert len(output["errors"]) == 0
 
 
-TARGETS = ["zulip"]
+def test_airtablejs() -> None:
+    run_repo("https://github.com/Airtable/airtable.js")
 
 
-def test_repos() -> None:
-    for target in TARGETS:
-        run_repo(target)
+def test_opendrop() -> None:
+    run_repo("https://github.com/seemoo-lab/opendrop")
 
 
-if __name__ == "__main__":
-    for t in TARGETS:
-        run_repo(t, rewrite=True)
+def test_lightstep_tracer_python() -> None:
+    run_repo("https://github.com/lightstep/lightstep-tracer-python")
+
+
+def test_sysdig_inspect() -> None:
+    run_repo("https://github.com/draios/sysdig-inspect")
+
+
+def test_sentry_python() -> None:
+    run_repo("https://github.com/getsentry/sentry-python")
+
+
+def test_signal_webrtc_ios() -> None:
+    run_repo("https://github.com/signalapp/signal-webrtc-ios")
+
+
+def test_scapy() -> None:
+    run_repo("https://github.com/secdev/scapy")
+
+
+def test_airflow() -> None:
+    run_repo("https://github.com/apache/airflow")
+
+
+def test_elasticsearch_dbapi() -> None:
+    run_repo("https://github.com/preset-io/elasticsearch-dbapi")
+
+
+def test_libcloud() -> None:
+    run_repo("https://github.com/apache/libcloud")
+
+
+def test_pykeybasebot() -> None:
+    run_repo("https://github.com/keybase/pykeybasebot")
+
+
+def test_cassandra() -> None:
+    run_repo("https://gitbox.apache.org/repos/asf/cassandra")
+
+
+def test_coinbase_commerce_python() -> None:
+    run_repo("https://github.com/coinbase/coinbase-commerce-python")
+
+
+def test_python_triplesec() -> None:
+    run_repo("https://github.com/keybase/python-triplesec")
+
+
+def test_psycopg2() -> None:
+    run_repo("https://github.com/psycopg/psycopg2")
+
+
+def test_flask_jwt_extended() -> None:
+    run_repo("https://github.com/preset-io/flask-jwt-extended")
+
+
+def test_pyperf() -> None:
+    run_repo("https://github.com/vstinner/pyperf")
+
+
+def test_mysql_connector_python() -> None:
+    run_repo("https://github.com/mysql/mysql-connector-python")
+
+
+def test_lemur() -> None:
+    run_repo("https://github.com/Netflix/lemur")
+
+
+# # fail
+# def test_highcharts() -> None:
+#     run_repo("https://github.com/highcharts/highcharts")
+
+
+# # fail
+# def test_yesterday_i_learned() -> None:
+#     run_repo("https://github.com/1337/yesterday-i-learned")
+
+
+# # fail Can uncomment after #580
+# def test_lodash() -> None:
+#     run_repo("https://github.com/lodash/lodash")
+
+
+# # fail
+# def test_Signal_Desktop() -> None:
+#     run_repo("https://github.com/signalapp/Signal-Desktop")
+
+
+# # fail
+# def test_call_power() -> None:
+#     run_repo("https://github.com/opensourceactivismtech/call-power")
+
+
+# # fail
+# def test_zulip() -> None:
+#     run_repo("https://github.com/zulip/zulip")
+
+
+# # fail
+# def test_home_assistant() -> None:
+#     run_repo("https://github.com/home-assistant/home-assistant")
+
+
+# # fail Can uncomment after #581 #582
+# def test_incubator_superset() -> None:
+#     run_repo("https://github.com/apache/incubator-superset")
