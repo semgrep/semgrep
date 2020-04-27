03cb8bc13af510c5a86e2b4f9b38e173da540513
Russell Cohen <russell.r.cohen@gmail.com>
2020-04-27 15:26:46 -0400

    Bundle linux install script & rewrite release validator in Python (#587)
    Bundle linux install scripts & validator improvements

--- install-scripts/ubuntu-generic.sh
+++ install-scripts/ubuntu-generic.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 set -e
 version="${VERSION:?Set a version to install}"
-tarball=sgrep-$version-ubuntu-16.04.tgz
-tarball_url=https://github.com/returntocorp/sgrep/releases/download/v$version/$tarball
-sha_url=https://github.com/returntocorp/sgrep/releases/download/v$version/$tarball.sha256
+tarball=semgrep-v$version-ubuntu-16.04.tgz
+tarball_url=https://github.com/returntocorp/semgrep/releases/download/v$version/$tarball
+sha_url=https://github.com/returntocorp/semgrep/releases/download/v$version/$tarball.sha256
 
 
+echo "Installing tarball from $tarball_url (checksum: $sha_url)"
 
 if ! out="$(semgrep --version 2>/dev/null)" || [[ "$out" != "$version" ]]
 then
     tmpdir="$(mktemp -d)"
     trap 'rm -r "$tmpdir"' EXIT
     cd "$tmpdir"
-    wget -nv "$tarball_url"
+    curl -L "$tarball_url" > "$tarball"
     sha256=$(curl -L "$sha_url" | awk '{ print $1 }')
     sha256sum -c <<< "$sha256  $tarball"
     # Be gentle on `/usr/local/lib`
+    rm -rf /usr/local/lib/semgrep-files
     sudo tar --skip-old-files -xzf  "$tarball" -C /usr/local/lib/
-    sudo ln -sf /usr/local/lib/sgrep-lint-files/sgrep-lint /usr/local/bin/semgrep
-    sudo ln -sf /usr/local/lib/sgrep-lint-files/sgrep /usr/local/bin/sgrep
+    sudo ln -sf /usr/local/lib/semgrep-files/semgrep /usr/local/bin/semgrep
+    sudo ln -sf /usr/local/lib/semgrep-files/semgrep-core /usr/local/bin/semgrep-core
 fi
 
 echo "Semgrep installed! Version: $(semgrep --version)"
