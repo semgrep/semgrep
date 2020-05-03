00b9ed957b3cec5b4e0daa749663005ee40fc877
Drew Dennison <drew@returntocorp.com>
2020-05-03 13:29:56 -0700

    add autofix test, fixup equiv test, and rename sgrep to semgrep

--- semgrep/tests/run-lint-tests.sh
+++ semgrep/tests/run-lint-tests.sh
@@ -19,7 +19,7 @@ assert_output_equal () {
 
 test_semgrep_local () {
     cd "${THIS_DIR}/../";
-    $SGREP --json --strict --config tests/python/eqeq.yaml tests/lint -o tmp.out >/dev/null
+    $SEMGREP --json --strict --config tests/python/eqeq.yaml tests/lint -o tmp.out >/dev/null
     assert_output_equal tmp.out tests/python/eqeq.expected.json
     rm -f tmp.out
 }
@@ -27,7 +27,7 @@ test_semgrep_local () {
 test_semgrep_relative() {
     # test relative paths
     cd "${THIS_DIR}/../";
-    $SGREP --json --strict --config ../semgrep/tests/python/eqeq.yaml tests/lint -o tmp.out >/dev/null
+    $SEMGREP --json --strict --config ../semgrep/tests/python/eqeq.yaml tests/lint -o tmp.out >/dev/null
     assert_output_equal tmp.out tests/python/eqeq.expected.relative.json
     rm -f tmp.out
 }
@@ -35,7 +35,7 @@ test_semgrep_relative() {
 test_semgrep_absolute() {
     cd "${THIS_DIR}/../";
     cp tests/python/eqeq.yaml /tmp
-    $SGREP --json --strict --config /tmp/eqeq.yaml tests/lint -o tmp.out >/dev/null
+    $SEMGREP --json --strict --config /tmp/eqeq.yaml tests/lint -o tmp.out >/dev/null
     assert_output_equal tmp.out tests/python/eqeq.expected.directory.json
     rm -f tmp.out
     rm -f /tmp/eqeq.yaml
@@ -44,7 +44,7 @@ test_semgrep_absolute() {
 # Explicitly included hidden configs should be included
 test_semgrep_explicit_hidden() {
     cd "${THIS_DIR}/../";
-    $SGREP --json --strict --config tests/python/hidden/.hidden tests/lint -o tmp.out >/dev/null
+    $SEMGREP --json --strict --config tests/python/hidden/.hidden tests/lint -o tmp.out >/dev/null
     if [ -z "$OVERRIDE_EXPECTED" ]; then
         diff tmp.out tests/python/eqeq.expected.explicit-hidden.json
     else
@@ -56,7 +56,7 @@ test_semgrep_explicit_hidden() {
 # Implicitly included hidden configs should be excluded
 test_semgrep_implicit_hidden() {
     cd "${THIS_DIR}/../";
-    $SGREP --json --strict --config tests/python/hidden tests/lint 2>tmp.out |:
+    $SEMGREP --json --strict --config tests/python/hidden tests/lint 2>tmp.out |:
     if [ -z "$OVERRIDE_EXPECTED" ]; then
         diff tmp.out tests/python/eqeq.expected.implicit-hidden.out
     else
@@ -68,13 +68,13 @@ test_semgrep_implicit_hidden() {
 test_semgrep_url_config() {
     cd "${THIS_DIR}/../";
     # test url paths
-    $SGREP --json --strict --config=https://raw.githubusercontent.com/returntocorp/semgrep-rules/develop/template.yaml tests/lint -o tmp.out >/dev/null
+    $SEMGREP --json --strict --config=https://raw.githubusercontent.com/returntocorp/semgrep-rules/develop/template.yaml tests/lint -o tmp.out >/dev/null
     assert_output_equal tmp.out tests/python/eqeq.expected.remote.json
     rm -f tmp.out
 }
 
 test_registry() {
-    $SGREP --json --strict --config=r2c tests/lint -o tmp.out >/dev/null
+    $SEMGREP --json --strict --config=r2c tests/lint -o tmp.out >/dev/null
     assert_output_equal tmp.out tests/python/eqeq.expected.registry.json
     rm -f tmp.out
 }
@@ -83,8 +83,8 @@ test_semgrep_default_file() {
     cd "${THIS_DIR}/../";
     # test .semgrep.yml
     rm -rf .semgrep.yml
-    $SGREP --generate-config
-    $SGREP --json --strict tests/lint -o tmp.out >/dev/null
+    $SEMGREP --generate-config
+    $SEMGREP --json --strict tests/lint -o tmp.out >/dev/null
     assert_output_equal tmp.out tests/python/eqeq.expected.template.json
     rm -f tmp.out
     rm -rf .semgrep.yml
@@ -94,9 +94,9 @@ test_semgrep_default_folder() {
     cd "${THIS_DIR}/../";
     # test .semgrep/ directory
     rm -rf .semgrep/ && mkdir .semgrep/
-    $SGREP --generate-config
+    $SEMGREP --generate-config
     mv .semgrep.yml .semgrep/
-    $SGREP --json --strict tests/lint -o tmp.out >/dev/null
+    $SEMGREP --json --strict tests/lint -o tmp.out >/dev/null
     assert_output_equal tmp.out tests/python/eqeq.expected.template.json
     rm -f tmp.out
     rm -rf .semgrep/
@@ -104,8 +104,15 @@ test_semgrep_default_folder() {
 
 test_semgrep_equivalence() {
     cd "${THIS_DIR}/../";
-    $SGREP --strict --config=tests/equivalence-tests/open-redirect.equiv.yml tests/equivalence-tests -o tmp.out >/dev/null
-    assert_output_equal tmp.out tests/equivalence-tests/expected.txt
+    $SEMGREP --json --strict --config=tests/equivalence-tests/open-redirect.equiv.yml tests/equivalence-tests -o tmp.out >/dev/null
+    assert_output_equal tmp.out tests/equivalence-tests/open-redirect.equiv.expected.json
+    rm -f tmp.out
+}
+
+test_semgrep_autofix() {
+    cd "${THIS_DIR}/../";
+    $SEMGREP --json --strict --config=tests/python/atuofix.yml tests/autofix.py -o tmp.out >/dev/null
+    assert_output_equal tmp.out tests/python/autofix.expected.json
     rm -f tmp.out
 }
 
@@ -118,7 +125,7 @@ cd "${THIS_DIR}"
 PYTHONPATH=.. pytest .
 
 local_tests() {
-    SGREP="python3 -m semgrep"
+    SEMGREP="python3 -m semgrep"
     test_semgrep_local
     test_semgrep_relative
     test_semgrep_absolute
@@ -132,7 +139,7 @@ local_tests() {
 }
 
 docker_tests() {
-    SGREP="docker run --rm -v \"\${PWD}:/home/repo\" returntocorp/semgrep:develop"
+    SEMGREP="docker run --rm -v \"\${PWD}:/home/repo\" returntocorp/semgrep:develop"
     test_semgrep_local
     #test_semgrep_relative
     #test_semgrep_absolute
@@ -147,33 +154,33 @@ local_tests
 #docker_tests
 
 # parsing bad.yaml should fail
-$SGREP --strict --config tests/python/bad.yaml tests/lint && echo "bad.yaml should have failed" && exit 1
+$SEMGREP --strict --config tests/python/bad.yaml tests/lint && echo "bad.yaml should have failed" && exit 1
 
 # parsing badpattern.yaml should fail
-$SGREP --strict --config tests/python/badpattern.yaml tests/lint && echo "badpattern.yaml should have failed" && exit 1
+$SEMGREP --strict --config tests/python/badpattern.yaml tests/lint && echo "badpattern.yaml should have failed" && exit 1
 
 # parsing bad2.yaml should fail
-$SGREP --strict --config tests/python/bad2.yaml tests/lint && echo "bad2.yaml should have failed" && exit 1
+$SEMGREP --strict --config tests/python/bad2.yaml tests/lint && echo "bad2.yaml should have failed" && exit 1
 
 # parsing bad3.yaml should fail
-$SGREP --strict --config tests/python/bad3.yaml tests/lint && echo "bad3.yaml should have failed" && exit 1
+$SEMGREP --strict --config tests/python/bad3.yaml tests/lint && echo "bad3.yaml should have failed" && exit 1
 
 # parsing bad4.yaml should fail
-$SGREP --strict --config tests/python/bad4.yaml tests/lint && echo "bad4.yaml should have failed" && exit 1
+$SEMGREP --strict --config tests/python/bad4.yaml tests/lint && echo "bad4.yaml should have failed" && exit 1
 
 # parsing good.yaml should succeed
-$SGREP --strict --config=tests/python/good.yaml tests/lint
+$SEMGREP --strict --config=tests/python/good.yaml tests/lint
 
 # parsing good_info_severity.yaml should succeed
-$SGREP --strict --config=tests/python/good_info_severity.yaml tests/lint
+$SEMGREP --strict --config=tests/python/good_info_severity.yaml tests/lint
 
 # parsing good_metadata.yaml should succeed
-$SGREP --strict --config=tests/python/good_metadata.yaml tests/lint
+$SEMGREP --strict --config=tests/python/good_metadata.yaml tests/lint
 
 #echo TODO: disabled semgrep-rules regression testing for now
 rm -rf /tmp/semgrep-rules && git clone https://github.com/returntocorp/semgrep-rules /tmp/semgrep-rules
-$SGREP --dangerously-allow-arbitrary-code-execution-from-rules --strict --test --test-ignore-todo /tmp/semgrep-rules
-$SGREP --validate --config=/tmp/semgrep-rules
+$SEMGREP --dangerously-allow-arbitrary-code-execution-from-rules --strict --test --test-ignore-todo /tmp/semgrep-rules
+$SEMGREP --validate --config=/tmp/semgrep-rules
 
 echo "-----------------------"
 echo "all lint tests passed"
