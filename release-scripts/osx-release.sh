a179a2568caeac701d9d020245583a034ab6db78
Russell Cohen <russell.r.cohen@gmail.com>
2020-05-04 16:26:12 -0400

    It isn't necessary to manually install deps anymore (#679)

--- release-scripts/osx-release.sh
+++ release-scripts/osx-release.sh
@@ -4,7 +4,6 @@ brew install opam pkg-config coreutils
 opam init --no-setup --bare;
 opam switch create 4.07.1;
 opam switch 4.07.1;
-eval "$(opam env)"; opam install -y reason dune ocamlfind camlp4 num ocamlgraph json-wheel conf-perl yaml
 git submodule update --init --recursive
 eval "$(opam env)" && opam install -y ./pfff
 cd semgrep-core && eval "$(opam env)" && opam install -y . && make all && make test && make install && cd ..
