0f78ac1a3b5096f3530c62ef6fdf34bdf0762e8f
Russell Cohen <russell.r.cohen@gmail.com>
2020-05-04 16:26:12 -0400

    It isn't necessary to manually install deps anymore (#679)

--- release-scripts/ubuntu-release.sh
+++ release-scripts/ubuntu-release.sh
@@ -7,7 +7,6 @@ echo "---------"
 sudo apt-get install -y --no-install-recommends libcurl4-openssl-dev libexpat1-dev gettext libz-dev libssl-dev build-essential autoconf
 opam switch --root /home/opam/.opam 4.07;
 
-eval "$(opam env --root /home/opam/.opam --set-root)"; opam install -y reason dune ocamlfind camlp4 num ocamlgraph json-wheel conf-perl yaml
 eval "$(opam env --root /home/opam/.opam --set-root)" && opam install -y ./pfff
 eval "$(opam env --root /home/opam/.opam --set-root)" && cd semgrep-core && opam install -y . && make all && cd ..
 eval "$(opam env --root /home/opam/.opam --set-root)" && cd semgrep && export PATH=/github/home/.local/bin:$PATH && sudo make all && cd ..
