a11fd6ccca405e894f783a3a6e4318b884a65afe
Brendon Go <brendon.go@gmail.com>
2020-05-04 21:35:02 -0700

    Add matching line to json output (#681)
    Move around functionaliy in rule_match object and output.py.

--- semgrep/semgrep/output.py
+++ semgrep/semgrep/output.py
@@ -1,28 +1,11 @@
-import itertools
 import json
-from pathlib import Path
 from typing import Any
-from typing import Dict
-from typing import Iterable
 from typing import Iterator
-from typing import Optional
+from typing import List
 
 import colorama
 
-
-def fetch_lines_in_file(
-    path: Path, start_line_number: int, end_line_number: int
-) -> Optional[Iterable[str]]:
-    """
-    `line_number` is one-indexed! Returns the line if it can be found, returns None if the path doesn't exist
-    TODO: cachine
-    """
-    if not path.exists():
-        return None
-    with path.open(
-        buffering=1, errors="replace"
-    ) as fin:  # buffering=1 turns on line-level reads
-        return list(itertools.islice(fin, start_line_number - 1, end_line_number))
+from semgrep.rule_match import RuleMatch
 
 
 def color_line(
@@ -48,41 +31,38 @@ def color_line(
     return line
 
 
-def finding_to_line(finding: Dict[str, Any], color_output: bool) -> Iterator[str]:
-    path = finding.get("path")
-    start_line = finding.get("start", {}).get("line")
-    end_line = finding.get("end", {}).get("line")
-    start_col = finding.get("start", {}).get("col")
-    end_col = finding.get("end", {}).get("col")
+def finding_to_line(rule_match: RuleMatch, color_output: bool) -> Iterator[str]:
+    path = rule_match.path
+    start_line = rule_match.start.get("line")
+    end_line = rule_match.end.get("line")
+    start_col = rule_match.start.get("col")
+    end_col = rule_match.end.get("col")
     if path and start_line:
-        file_lines = fetch_lines_in_file(Path(path), start_line, end_line)
+        file_lines = rule_match.lines
         if file_lines:
             for i, line in enumerate(file_lines):
                 if color_output:
-                    yield f"{colorama.Fore.GREEN}{start_line + i}{colorama.Style.RESET_ALL}:{color_line(line.rstrip(), start_line + i, start_line, start_col, end_line, end_col)}"
+                    yield f"{colorama.Fore.GREEN}{start_line + i}{colorama.Style.RESET_ALL}:{color_line(line.rstrip(), start_line + i, start_line, start_col, end_line, end_col)}"  # type: ignore
                 else:
                     yield f"{start_line + i}:{line.rstrip()}"
 
 
 def build_normal_output(
-    output_data: Dict[str, Any], color_output: bool
+    rule_matches: List[RuleMatch], semgrep_errors: List[Any], color_output: bool
 ) -> Iterator[str]:
-    results = output_data.get("results", [])
+    RESET_COLOR = colorama.Style.RESET_ALL if color_output else ""
+    GREEN_COLOR = colorama.Fore.GREEN if color_output else ""
+    YELLOW_COLOR = colorama.Fore.YELLOW if color_output else ""
+    RED_COLOR = colorama.Fore.RED if color_output else ""
+    BLUE_COLOR = colorama.Fore.BLUE if color_output else ""
+
     last_file = None
     last_message = None
-    for finding in sorted(
-        results,
-        key=lambda k: (k.get("path", "<no path>"), k.get("check_id", "<no rule id>")),
-    ):
-        RESET_COLOR = colorama.Style.RESET_ALL if color_output else ""
-        GREEN_COLOR = colorama.Fore.GREEN if color_output else ""
-        YELLOW_COLOR = colorama.Fore.YELLOW if color_output else ""
-        RED_COLOR = colorama.Fore.RED if color_output else ""
-        BLUE_COLOR = colorama.Fore.BLUE if color_output else ""
+    for rule_match in sorted(rule_matches, key=lambda r: (r.path, r.id),):
 
-        current_file = finding.get("path", "<no path>")
-        check_id = finding.get("check_id")
-        extra = finding.get("extra", {})
+        current_file = rule_match.path
+        check_id = rule_match.id
+        extra = rule_match.extra
         message = extra.get("message")
         severity = extra.get("severity")
         fix = extra.get("fix")
@@ -105,22 +85,39 @@ def build_normal_output(
                     severity_prepend = f"{YELLOW_COLOR}{severity} "
                 else:
                     severity_prepend = f"{severity} "
-            yield f"{severity_prepend}{YELLOW_COLOR}rule:{check_id}: {finding.get('extra', {}).get('message')}{RESET_COLOR}"
+            yield f"{severity_prepend}{YELLOW_COLOR}rule:{check_id}: {message}{RESET_COLOR}"
 
         last_file = current_file
         last_message = message
-        yield from finding_to_line(finding, color_output)
+        yield from finding_to_line(rule_match, color_output)
         if fix:
             yield f"{BLUE_COLOR}autofix:{RESET_COLOR} {fix}"
 
 
-def build_output_json(output_json: Dict[str, Any]) -> str:
+def build_output_json(rule_matches: List[RuleMatch], semgrep_errors: List[Any]) -> str:
     # wrap errors under "data" entry to be compatible with
     # https://docs.r2c.dev/en/latest/api/output.html#errors
-    errors = output_json["errors"]
-    if errors:
-        output_json["errors"] = {
-            "data": output_json["errors"],
-            "message": "SgrepRuntimeErrors",
-        }
+    output_json = {}
+    output_json["results"] = [rm.to_json() for rm in rule_matches]
+
+    errors = []
+    for error in semgrep_errors:
+        errors.append(
+            {"data": semgrep_errors, "message": "SemgrepCoreRuntimeErrors",}
+        )
+    output_json["errors"] = errors
     return json.dumps(output_json)
+
+
+def build_output(
+    rule_matches: List[RuleMatch],
+    semgrep_errors: List[Any],
+    json_format: bool,
+    color_output: bool,
+) -> str:
+    if json_format:
+        return build_output_json(rule_matches, semgrep_errors)
+    else:
+        return "\n".join(
+            build_normal_output(rule_matches, semgrep_errors, color_output)
+        )
