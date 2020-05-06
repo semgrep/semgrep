8c750933e36619bfd5e10bd1f2282f08aceff1a5
Bence Nagy <bence@underyx.me>
2020-05-06 18:12:53 +0200

    tests: Fix test_public_repos not scanning Python code
    Passing --language python --language javascript apparently made semgrep
    scan only javascript.
    
    This commit changes it so we run semgrep twice for each repo,
    once with both languages. Also added more assertions regarding output.

--- semgrep/tests/qa/test_public_repos.py
+++ semgrep/tests/qa/test_public_repos.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import pytest
 
 pytestmark = pytest.mark.qa
+SENTINEL_VALUE = 87518275812375164
 
 
 def xfail_repo(url, *, reason=None):
@@ -69,31 +70,42 @@ def test_semgrep_on_repo(run_semgrep_in_tmp, repo_url):
             "repo",
         ]
     )
-    semgrep_run = subprocess.run(
-        [
-            "python3",
-            "-m",
-            "semgrep",
-            "--pattern",
-            "$X == $X",
-            "--lang=python",
-            "--lang=javascript",
-            "--json",
-            "repo",
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        encoding="utf-8",
-    )
+    languages = {
+        "python": "py",
+        "javascript": "js",
+    }
+    for language, file_ext in languages.items():
+        sentinel_path = Path("repo") / f"sentinel.{file_ext}"
+        with sentinel_path.open("w") as sentinel_file:
+            sentinel_file.write(f"x = {SENTINEL_VALUE}")
 
-    assert semgrep_run.returncode == 0
-    try:
-        output = json.loads(semgrep_run.stdout)
-    except json.JSONDecodeError:
-        pytest.fail(
-            f"Failed to parse JSON from semgrep output:\n"
-            + semgrep_run.stdout
-            + semgrep_run.stderr
+        semgrep_run = subprocess.run(
+            [
+                "python3",
+                "-m",
+                "semgrep",
+                "--pattern",
+                f"$X = {SENTINEL_VALUE}",
+                "--lang",
+                language,
+                "--json",
+                "repo",
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            encoding="utf-8",
         )
 
-    assert "results" in output
+        assert semgrep_run.returncode == 0
+        try:
+            output = json.loads(semgrep_run.stdout)
+        except json.JSONDecodeError:
+            pytest.fail(
+                f"Failed to parse JSON from semgrep output:\n"
+                + semgrep_run.stdout
+                + semgrep_run.stderr
+            )
+
+        assert output["errors"] == []
+        assert len(output["results"]) == 1
+        assert output["results"][0]["path"] == str(sentinel_path)
