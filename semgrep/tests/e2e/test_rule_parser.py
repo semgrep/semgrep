8a6837f5c15bfccc110a449a66dc799a98e60d37
Bence Nagy <bence@underyx.me>
2020-05-06 16:46:06 +0200

    tests: Refactor rule parsing tests to use pytest

---
+++ semgrep/tests/e2e/test_rule_parser.py
@@ -0,0 +1,40 @@
+from subprocess import CalledProcessError
+
+import pytest
+
+
+@pytest.mark.parametrize("filename", ["good", "good_info_severity", "good_metadata"])
+def test_rule_parser__success(run_semgrep, snapshot, filename):
+    run_semgrep(f"rules/syntax/{filename}.yaml")
+
+
+@pytest.mark.parametrize(
+    "filename", ["bad1", "bad2", "bad3", "bad4", "badpattern",],
+)
+def test_rule_parser__failure(run_semgrep, snapshot, filename):
+    with pytest.raises(CalledProcessError) as excinfo:
+        run_semgrep(f"rules/syntax/{filename}.yaml")
+    assert excinfo.value.returncode != 0
+
+
+@pytest.mark.parametrize(
+    "filename",
+    [
+        pytest.param(
+            "bad1",
+            marks=pytest.mark.xfail(
+                reason="https://github.com/returntocorp/semgrep/issues/678"
+            ),
+        ),
+        pytest.param(
+            "bad2", marks=pytest.mark.xfail(reason="prints set with arbitrary order")
+        ),
+        "bad3",
+        "bad4",
+        "badpattern",
+    ],
+)
+def test_rule_parser__failure__error_messages(run_semgrep, snapshot, filename):
+    with pytest.raises(CalledProcessError) as excinfo:
+        run_semgrep(f"rules/syntax/{filename}.yaml", stderr=True)
+    snapshot.assert_match(excinfo.value.output, "error.txt")
--- semgrep/tests/run-lint-tests.sh
+++ semgrep/tests/run-lint-tests.sh
@@ -14,30 +14,6 @@ PYTHONPATH=.. pytest .
 
 SEMGREP="python3 -m semgrep"
 
-# parsing bad.yaml should fail
-$SEMGREP --strict --config tests/python/bad.yaml tests/lint && echo "bad.yaml should have failed" && exit 1
-
-# parsing badpattern.yaml should fail
-$SEMGREP --strict --config tests/python/badpattern.yaml tests/lint && echo "badpattern.yaml should have failed" && exit 1
-
-# parsing bad2.yaml should fail
-$SEMGREP --strict --config tests/python/bad2.yaml tests/lint && echo "bad2.yaml should have failed" && exit 1
-
-# parsing bad3.yaml should fail
-$SEMGREP --strict --config tests/python/bad3.yaml tests/lint && echo "bad3.yaml should have failed" && exit 1
-
-# parsing bad4.yaml should fail
-$SEMGREP --strict --config tests/python/bad4.yaml tests/lint && echo "bad4.yaml should have failed" && exit 1
-
-# parsing good.yaml should succeed
-$SEMGREP --strict --config=tests/python/good.yaml tests/lint
-
-# parsing good_info_severity.yaml should succeed
-$SEMGREP --strict --config=tests/python/good_info_severity.yaml tests/lint
-
-# parsing good_metadata.yaml should succeed
-$SEMGREP --strict --config=tests/python/good_metadata.yaml tests/lint
-
 #echo TODO: disabled semgrep-rules regression testing for now
 rm -rf /tmp/semgrep-rules && git clone https://github.com/returntocorp/semgrep-rules /tmp/semgrep-rules
 $SEMGREP --dangerously-allow-arbitrary-code-execution-from-rules --strict --test --test-ignore-todo /tmp/semgrep-rules
