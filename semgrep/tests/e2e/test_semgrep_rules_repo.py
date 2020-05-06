e313adb17bd8b6b0fb464efa2e2f83f764670a36
Bence Nagy <bence@underyx.me>
2020-05-06 16:48:49 +0200

    tests: Refactor semgrep-rules regression testing to use pytest

---
+++ semgrep/tests/e2e/test_semgrep_rules_repo.py
@@ -0,0 +1,22 @@
+import subprocess
+from pathlib import Path
+
+
+def test_semgrep_rules_repo(run_semgrep):
+    subprocess.check_output(
+        ["git", "clone", "--depth=1", "https://github.com/returntocorp/semgrep-rules"]
+    )
+    subprocess.check_output(["python", "-m", "semgrep", "--generate-config"])
+    subprocess.check_output(
+        [
+            "python",
+            "-m",
+            "semgrep",
+            "--dangerously-allow-arbitrary-code-execution-from-rules",
+            "--strict",
+            "--test",
+            "--test-ignore-todo",
+            "semgrep-rules",
+        ]
+    )
+    subprocess.check_output(["python", "-m", "semgrep", "--validate", "semgrep-rules"])
