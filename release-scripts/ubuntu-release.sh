faced59611839346910545aaff4a1ed4944518e3
Russell Cohen <russell.r.cohen@gmail.com>
2020-05-13 22:01:45 -0400

    Build static binaries for linux  (#730)
    * Switch to build static binaries on linux
    
    * Remove ubuntu 18.04 from build matrix
    
    * Fix dune
    
    * Guess there's no 20.04 on Github yet
    
    * OS specific flags!
    
    * Install musl tools
    
    * Working on manylinux build job
    
    * Working on manylinux build job
    
    * Add manylinux flag and fix comments
    
    * Produce many-linux compatible wheels
    
    * manylinux is too hard to use
    
    * Search for semgrep-core in adjacent directory to Python
    
    * Fix workdir
    
    * Fix test job
    
    * Add missing container
    
    * Fix constants.py loading
    
    * Use which instead of system
    
    * Capture output to unbreak tests

--- release-scripts/ubuntu-release.sh
+++ release-scripts/ubuntu-release.sh
@@ -4,8 +4,8 @@ set -e
 echo "here's some help"
 ls
 echo "---------"
-sudo apt-get install -y --no-install-recommends libcurl4-openssl-dev libexpat1-dev gettext libz-dev libssl-dev build-essential autoconf
-opam switch --root /home/opam/.opam 4.07;
+sudo apt-get install -y --no-install-recommends libcurl4-openssl-dev libexpat1-dev gettext libz-dev libssl-dev build-essential autoconf musl-tools
+opam switch create --root /home/opam/.opam 4.10.0+musl+static+flambda;
 
 eval "$(opam env --root /home/opam/.opam --set-root)" && opam install -y ./pfff
 eval "$(opam env --root /home/opam/.opam --set-root)" && cd semgrep-core && opam install -y . && make all && cd ..
@@ -17,7 +17,6 @@ mkdir -p semgrep-files
 cp ./semgrep-core/_build/default/bin/main_semgrep_core.exe semgrep-files/semgrep-core
 cp -r ./semgrep/build/semgrep.dist/* semgrep-files
 ls semgrep-files
-# TODO: remove once the Python build job makes something named `semgrep`
 chmod +x semgrep-files/semgrep-core
 chmod +x semgrep-files/semgrep
 tar -cvzf artifacts.tar.gz semgrep-files/
