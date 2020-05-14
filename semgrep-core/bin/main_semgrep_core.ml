7be84db0d212c002e33ce79285cf885b8dc951fd
brendon <brendon.go@gmail.com>
2020-05-14 14:20:23 -0700

    Use git tag for semgrep-core version string

--- semgrep-core/bin/main_semgrep_core.ml
+++ semgrep-core/bin/main_semgrep_core.ml
@@ -795,8 +795,8 @@ let options () =
   (*s: [[Main_semgrep_core.options]] concatenated actions *)
   Common.options_of_actions action (all_actions()) @
   (*e: [[Main_semgrep_core.options]] concatenated actions *)
-  [ "-version",   Arg.Unit (fun () -> 
-    pr2 (spf "semgrep-core version: %s" Config_pfff.version);
+  [ "-version",   Arg.Unit (fun () ->
+    pr2 (spf "semgrep-core version: %%VERSION%%, pfff: %s" Config_pfff.version);
     exit 0;
     ), "  guess what"; 
   ]
