9779847ef87d4f3403e4a99185ecd6000bf40728
Emma Jin <emjin@users.noreply.github.com>
2020-05-20 12:00:35 -0400

    Wrote tests for the parse list of statements java issue (#791)
    * Wrote tests for the parse list of statements java issue
    
    Addresses https://github.com/returntocorp/semgrep/issues/723
    Related to https://github.com/returntocorp/pfff/pull/115
    
    Ran make test in semgrep-core.
    
    * Used latest version of pfff.

---
+++ semgrep-core/tests/java/statement_list.java
@@ -0,0 +1,15 @@
+class Bar {
+    void main() {
+        boolean myBoolean;
+
+        //myBoolean == myBoolean;
+
+        // ruleid:assignment-comparison
+        if (myBoolean = true) {
+            continue;
+	}
+	//ERROR:
+        String sb = "a";
+        String a;
+    }
+}
