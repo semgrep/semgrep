8320110366f3ccfd44fc7d7ad506f2211c3f0685
Brendon Go <brendon.go@gmail.com>
2020-05-29 13:23:11 -0700

    tests: more verbose output for semgrep-rules regression tests (#869)

--- semgrep/tests/qa/test_semgrep_rules_repo.py
+++ semgrep/tests/qa/test_semgrep_rules_repo.py
@@ -11,7 +11,7 @@ def test_semgrep_rules_repo(run_semgrep_in_tmp):
         ["git", "clone", "--depth=1", "https://github.com/returntocorp/semgrep-rules"]
     )
     subprocess.check_output(["python3", "-m", "semgrep", "--generate-config"])
-    subprocess.check_output(
+    output = subprocess.run(
         [
             "python3",
             "-m",
@@ -21,6 +21,13 @@ def test_semgrep_rules_repo(run_semgrep_in_tmp):
             "--test",
             "--test-ignore-todo",
             "semgrep-rules",
-        ]
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
     )
+
+    if output.returncode != 0:
+        pytest.fail("Regression in Semgrep-Rules Repo" + output.stdout + output.stderr)
+
     subprocess.check_output(["python3", "-m", "semgrep", "--validate", "semgrep-rules"])
