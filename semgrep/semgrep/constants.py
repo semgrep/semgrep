201128f2abd8d1a34ded75c2672c38163a09378d
brendon <brendon.go@gmail.com>
2020-06-09 20:01:08 -0700

    Release 0.10.0

--- semgrep/semgrep/constants.py
+++ semgrep/semgrep/constants.py
@@ -17,7 +17,7 @@ DEFAULT_CONFIG_FOLDER = f".{DEFAULT_SEMGREP_CONFIG_NAME}"
 
 YML_EXTENSIONS = {".yml", ".yaml"}
 
-__VERSION__ = "0.10.0b1"
+__VERSION__ = "0.10.0"
 
 
 def compute_semgrep_path() -> str:
