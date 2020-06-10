065ee430020907473c16ca525c0148961eaaca42
Yoann Padioleau <pad@returntocorp.com>
2020-06-10 15:48:41 +0200

    Use cache for parsing files faster the second time (#892)
    * Use cache for parsing files faster the second time
    
    This should fix #876 (at least the ocaml part).
    This saves parsed (and Naming_AST.resolved) generic AST
    in /tmp/semgrep_core_cache<uid/ if the -use_parsing_cache command-line
    argument was passed to semgrep-core.
    
    To clean the cache, simply do
    rm -f /tmp/semgrep_core_cache<uid/ from the Python side if you need to.
    
    Test plan:
    takes 2.1s the first time and 0.7sec the second time when testing
    on tests/PERF/ js files.
    
    /home/pad/github/semgrep/semgrep-core/_build/default/bin/Main.exe -equivalences /home/pad/github/semgrep/semgrep-core/data/basic_equivalences.yml -verbose -profile -e 1==2 -lang js tests/PERF/
    ---------------------
    profiling result
    ---------------------
    Main total                               :      1.996 sec          1 count
    Parse_js.parse                           :      1.043 sec          4 count
    Semgrep.check                            :      0.537 sec          4 count
    Parse_js.tokens                          :      0.401 sec         13 count
    Semgrep.match_e_e                        :      0.307 sec     132621 count
    ...
    
    This time with -use_parsing_cache:
    /home/pad/github/semgrep/semgrep-core/_build/default/bin/Main.exe -equivalences /home/pad/github/semgrep/semgrep-core/data/basic_equivalences.yml -use_parsing_cache -verbose -profile -e 1==2 -lang js tests/PERF/
    
    ---------------------
    profiling result
    ---------------------
    Main total                               :      0.546 sec          1 count
    Semgrep.check                            :      0.486 sec          4 count
    Semgrep.match_e_e                        :      0.285 sec     132621 count
    rule:-e/-f                               :      0.105 sec     132621 count
    Apply_equivalences.apply                 :      0.042 sec          4 count
    Main.cache_computation                   :      0.040 sec          4 count
    ...
    
    ls -al /tmp/semgrep_core_cache_1000/
    total 9984
    drwx------  2 pad  pad     4096 Jun  2 16:08 .
    drwxrwxrwt 17 root root   49152 Jun  2 16:26 ..
    -rw-r--r--  1 pad  pad  1019805 Jun  2 16:07 027e133a90b2c79d01c3224dfb0fed8f.ast_cache
    -rw-r--r--  1 pad  pad  3953523 Jun  2 16:05 20d25efa622e4bfbc4015d03ba0d1d68.ast_cache
    -rw-r--r--  1 pad  pad  3953523 Jun  2 16:08 6c05be109ca532cf545c082bda4a07fd.ast_cache
    -rw-r--r--  1 pad  pad     7590 Jun  2 16:05 8cae6609c9048056880aa68cc383912c.ast_cache
    -rw-r--r--  1 pad  pad     7590 Jun  2 16:07 92a4a1e679734b50b6dbc1cbfae37e86.ast_cache
    -rw-r--r--  1 pad  pad    98103 Jun  2 16:07 a51fb3d6d3645f7004df0cee7b18e176.ast_cache
    -rw-r--r--  1 pad  pad    98103 Jun  2 16:05 bd7d534ea53a44d782902098c97fedb9.ast_cache
    -rw-r--r--  1 pad  pad  1019805 Jun  2 16:05 e763c7f5ef9424a2a7e295dd564cfabb.ast_cache
    
    There are each time 2 files with the same size because I bumped ast_version
    and so it regenerated new cache entries.
    
    * * semgrep-core/bin/Main.ml: address nathan and martin suggestions

--- semgrep-core/bin/Main.ml
+++ semgrep-core/bin/Main.ml
@@ -136,6 +136,7 @@ let supported_langs: string = String.concat ", " keys
 let ncores = ref 1
 (*e: constant [[Main_semgrep_core.ncores]] *)
 
+let use_parsing_cache = ref false
 let target_file = ref ""
 
 (*s: constant [[Main_semgrep_core.action]] *)
@@ -266,6 +267,62 @@ let gen_layer ~root ~query file =
   ()
 (*e: function [[Main_semgrep_core.gen_layer]] *)
 
+let filemtime file =
+  (Unix.stat file).Unix.st_mtime
+
+(* The function below is mostly a copy-paste of Common.cache_computation.
+ * This function is slightly more flexible because we can put the cache file
+ * anywhere thanks to the argument 'cache_file_of_file'.
+ * We also try to be a bit more type-safe by using the version tag above.
+ * TODO: merge in pfff/commons/Common.ml at some point
+ *)
+let cache_computation file cache_file_of_file f =
+  if not !use_parsing_cache
+  then f ()
+  else begin
+    if not (Sys.file_exists file)
+    then begin
+      pr2 ("WARNING: cache_computation: can't find file "  ^ file);
+      pr2 ("defaulting to calling the function");
+      f ()
+    end else begin
+    profile_code "Main.cache_computation" (fun () ->
+
+      let file_cache = cache_file_of_file file in
+      if Sys.file_exists file_cache && filemtime file_cache >= filemtime file
+      then begin
+        if !verbose then pr2 ("using cache: " ^ file_cache);
+        let (version, file2, res) = Common2.get_value file_cache in
+        if version <> Version.version
+        then failwith (spf "Version mismatch! Clean the cache file %s"
+                      file_cache);
+        if file <> file2
+        then failwith (spf
+          "Not the same file! Md5sum collision! Clean the cache file %s"
+                      file_cache);
+
+        res
+      end
+      else begin
+        let res = f () in
+        Common2.write_value (Version.version, file, res) file_cache;
+        res
+      end
+      )
+    end
+  end
+
+
+let cache_file_of_file filename =
+  let dir = spf "%s/semgrep_core_cache_%d"
+      (Filename.get_temp_dir_name ())
+      (Unix.getuid()) in
+  if not (Sys.file_exists dir)
+  then Unix.mkdir dir 0o700;
+  (* hopefully there will be no collision *)
+  let md5 = Digest.string filename in
+  Filename.concat dir (spf "%s.ast_cache" (Digest.to_hex md5))
+
 (*s: function [[Main_semgrep_core.parse_generic]] *)
 (* coupling: you need also to modify tests/Test.ml *)
 let parse_generic lang file =
@@ -274,6 +331,17 @@ let parse_generic lang file =
   then Parse_cpp.init_defs !Flag_parsing_cpp.macros_h;
   (*e: [[Main_semgrep_core.parse_generic()]] use standard macros if parsing C *)
 
+  cache_computation file (fun file ->
+    (* we may use different parsers for the same file (e.g., in Python3 or
+     * Python2 mode), so put the lang as part of the cache "dependency".
+     * We also add ast_version here so bumping the version will not
+     * try to use the old cache file (which should generate an exception).
+     *)
+     let full_filename = spf "%s__%s__%s"
+      file (Lang.string_of_lang lang) Version.version
+     in
+     cache_file_of_file full_filename)
+ (fun () ->
   let ast = Parse_generic.parse_with_lang lang file in
   (*s: [[Main_semgrep_core.parse_generic()]] resolve names in the AST *)
   (* to be deterministic, reset the gensym; anyway right now sgrep is
@@ -284,6 +352,7 @@ let parse_generic lang file =
   Naming_AST.resolve lang ast;
   (*e: [[Main_semgrep_core.parse_generic()]] resolve names in the AST *)
   ast
+  )
 (*e: function [[Main_semgrep_core.parse_generic]] *)
 
 (*s: function [[Main_semgrep_core.parse_equivalences]] *)
@@ -791,9 +860,11 @@ let options () =
         error_recovery := true;
         Flag_parsing.error_recovery := true;
     ),
-    (*e: [[Main_semgrep_core.options]] other cases *)
-
     " do not stop at first parsing error with -e/-f";
+    (*e: [[Main_semgrep_core.options]] other cases *)
+    "-use_parsing_cache", Arg.Set use_parsing_cache,
+    " save and use parsed ASTs in a cache";
+
     "-verbose", Arg.Unit (fun () ->
       verbose := true;
       Flag_semgrep.verbose := true;
