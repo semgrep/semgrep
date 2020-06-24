e91038bbfed8f369a745650043f67e7ad42da2a0
Emma Jin <emjin@users.noreply.github.com>
2020-06-24 16:50:54 -0400

    Added an e2e test for the synthesize_patterns action
    Modified synthesize_patterns to print a json object. Ran with pipenv run pytest tests/e2e/test_synthesize_patterns.py

--- semgrep-core/bin/Main.ml
+++ semgrep-core/bin/Main.ml
@@ -834,9 +834,16 @@ let synthesize_patterns s file =
   (match e_opt with
   | Some e ->
       let options = Pattern_from_Code.from_expr e in
-      options |> List.iter (fun (s, pat) ->
-        pr (spf "%s: %s" s (Pretty_print_generic.pattern_to_string lang pat))
-      );
+      let json_opts =
+        J.Object
+            (List.map (fun (k, v) -> (k, J.String (Pretty_print_generic.pattern_to_string lang v)))
+                      options) in
+      let s = Json_io.string_of_json json_opts in
+      pr s
+      (* options |> List.iter (fun (s, pat) ->
+        pr (spf "%s: %s" s (Pretty_print_generic.pattern_to_string lang pat));
+      ); *)
+
   | None -> failwith (spf "could not find an expr at range %s in %s" s file)
   )
 
