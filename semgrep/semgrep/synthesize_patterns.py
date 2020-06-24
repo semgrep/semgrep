dee49162fe248cfaf34d751caeedb437cc66d343
Emma Jin <emjin@users.noreply.github.com>
2020-06-23 20:02:51 -0400

    Linked pattern synthesis through python
    Added synthesize-patterns argument, had it call semgrep-core with appropriate arguments
    
    Tested with
    python -m semgrep --synthesize-patterns 4:9-4:29 /Users/emma/workspace/semgrep/semgrep-core/tests/SYNTHESIZING/mytest.py
    
    (where mytest.py contains the code referenced in the first commit)
    
    Also ran
    semgrep-core -synthesize_patterns 17:3-17:36 tests/SYNTHESIZING/mytest.py

---
+++ semgrep/semgrep/synthesize_patterns.py
@@ -0,0 +1,26 @@
+import subprocess
+import tempfile
+from typing import List
+from typing import Optional
+
+import semgrep.config_resolver
+from semgrep.constants import PLEASE_FILE_ISSUE_TEXT
+from semgrep.constants import SEMGREP_PATH
+from semgrep.error import SemgrepError
+
+
+def synthesize_patterns(
+    language: str, code_to_synthesize: str, targets_str: List[str]
+) -> None:
+    targets = semgrep.config_resolver.resolve_targets(targets_str)
+
+    args = ["-synthesize_patterns", code_to_synthesize]
+
+    cmd = [SEMGREP_PATH] + args + targets_str
+    try:
+        output = subprocess.check_output(cmd, shell=False)
+    except subprocess.CalledProcessError as ex:
+        raise SemgrepError(
+            f"error invoking semgrep with:\n\t{' '.join(cmd)}\n\t{ex}\n{PLEASE_FILE_ISSUE_TEXT}"
+        )
+    print(output.decode())
