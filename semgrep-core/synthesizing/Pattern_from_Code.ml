62544c5e526b965470bfccbaf09a5c521157ab92
Emma Jin <emjin@users.noreply.github.com>
2020-06-25 12:10:11 -0400

    Changed count to not be a ref cell

--- semgrep-core/synthesizing/Pattern_from_Code.ml
+++ semgrep-core/synthesizing/Pattern_from_Code.ml
@@ -34,40 +34,54 @@ type named_variants =
 let fk = Parse_info.fake_info "fake"
 let _bk f (lp,x,rp) = (lp, f x, rp)
 
+(* let fst (x, _) = x *)
+let snd (_, x) = x
+
 (*****************************************************************************)
 (* Algorithm *)
 (*****************************************************************************)
-
-let default_id count =
-    count := !count + 1;
-    Id((Format.sprintf "$%d" (!count), Parse_info.fake_info " "),
-    {id_resolved = ref None; id_type = ref None; id_const_literal = ref None})
 (* TODO: look if Call, and propose $ on each argument, ...,
  * propose also typed metavars, and resolved id in the call, etc.
  *)
 
+let default_id count =
+    (count + 1,
+     Id((Format.sprintf "$X%d" (count), Parse_info.fake_info " "),
+     {id_resolved = ref None; id_type = ref None; id_const_literal = ref None})
+    )
+
 let shallow_dots (e, (lp, rp)) =
    ("dots", E (Call (e, (lp, [Arg (Ellipsis fk)], rp))))
 
+let rec map_args count = function
+   | [] -> (count, [])
+   | _x::xs ->
+      let (c, new_id) = default_id count in
+      let (_, args) = map_args c xs in
+      (c, (Arg new_id)::args)
+
 let shallow_metavar (e, (lp, es, rp)) count =
-   let replaced_args = List.map (fun _ -> Arg (default_id count) ) es in
+   let (_, replaced_args) = map_args count es in
    ("metavars", E (Call (e, (lp, replaced_args, rp))))
 
 
 let generalize_call count = function
-  | Call (IdSpecial _, _) -> []
+  | Call (IdSpecial e1, e2) -> (shallow_metavar (IdSpecial e1, e2) count) :: []
   | Call (e, (lp, es, rp)) ->
       (shallow_dots (e, (lp, rp))) :: (shallow_metavar (e, (lp, es, rp)) count) :: []
   | _ -> []
 
-let generalize e =
-  let count = ref 0 in
+let generalize_exp e count =
   match e with
   | Call _ -> generalize_call count e
   | Id ((_s, t1), info) ->
       ["metavar", E (Id(("$X", t1), info))]
-  | L _ -> ["metavar", E (default_id count)]
+  | L _ -> ["metavar", E (snd (default_id count))]
   | _ -> []
+
+let generalize e =
+  let count = 1 in
+  generalize_exp e count
 
 (*****************************************************************************)
 (* Entry point *)
