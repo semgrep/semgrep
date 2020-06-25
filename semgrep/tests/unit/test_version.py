9989efb7520c0127a03c18787ff59cb28f566f5c
Matt Schwager <matt@returntocorp.com>
2020-06-25 15:39:16 -0400

    Don't depend on timing anymore which could be flaky

--- semgrep/tests/unit/test_version.py
+++ semgrep/tests/unit/test_version.py
@@ -1,4 +1,3 @@
-import time
 from unittest import mock
 
 import semgrep
@@ -9,14 +8,7 @@ def test_version_check_caching(tmp_path):
 
     with mock.patch("semgrep.version._fetch_latest_version") as fetched_mock:
         fetched_mock.return_value = "1.2.3"
-
-        time1 = time.time()
         semgrep.version.is_running_latest(tmp_cache_path)
-        time1 = time.time() - time1
-
-        time2 = time.time()
         semgrep.version.is_running_latest(tmp_cache_path)
-        time2 = time.time() - time2
 
     assert fetched_mock.call_count == 1
-    assert time2 < time1
