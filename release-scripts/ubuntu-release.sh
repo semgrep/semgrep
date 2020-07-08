f243218c6033059fb071ec5d48b5cb0973a65259
Martin Jambon <Martin Jambon>
2020-07-07 21:14:51 -0700

    Make ubuntu release reuse the semgrep-core binary.

--- release-scripts/ubuntu-release.sh
+++ release-scripts/ubuntu-release.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-set -e
+#
+# Make a semgrep release for Linux x86_64.
+#
+set -eu
 
-echo "here's some help"
-ls
-echo "---------"
-sudo apt-get install -y --no-install-recommends libcurl4-openssl-dev libexpat1-dev gettext libz-dev libssl-dev build-essential autoconf musl-tools
-opam switch create --root /home/opam/.opam 4.10.0+musl+static+flambda;
+if [[ ! -f semgrep-files/semgrep-core ]]; then
+  echo "Missing semgrep-files/semgrep-core binary." 2>&1
+  exit 1
+fi
 
-eval "$(opam env --root /home/opam/.opam --set-root)"
-opam install -y ./pfff
-(
-  cd semgrep-core
-  opam install --deps-only -y .
-  make all
-)
+# We may not need all of these packages anymore since semgrep-core now
+# comes pre-built.
+sudo apt-get install -y --no-install-recommends \
+     libcurl4-openssl-dev libexpat1-dev gettext libz-dev libssl-dev \
+     build-essential autoconf musl-tools
 
 if [[ -z "$SKIP_NUITKA" ]]; then
   export PATH=/github/home/.local/bin:$PATH
@@ -22,10 +22,10 @@ if [[ -z "$SKIP_NUITKA" ]]; then
     sudo make all
   )
 fi
+
 mkdir -p semgrep-files
-cp ./semgrep-core/_build/default/bin/Main.exe semgrep-files/semgrep-core
 cp -r ./semgrep/build/semgrep.dist/* semgrep-files
 ls semgrep-files
-chmod +x semgrep-files/semgrep-core
 chmod +x semgrep-files/semgrep
+chmod +x semgrep-files/semgrep-core
 tar -cvzf artifacts.tar.gz semgrep-files/
