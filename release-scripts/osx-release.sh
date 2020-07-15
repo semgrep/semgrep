9a4d4c0333d459ba24dd32fbca99241bd9b8b293
brendon <brendon.go@gmail.com>
2020-07-14 23:02:24 -0700

    Test semgrep-core linking

--- release-scripts/osx-release.sh
+++ release-scripts/osx-release.sh
@@ -20,12 +20,12 @@ opam install -y ./pfff
 )
 
 mkdir -p artifacts
-if [[ -z "$SKIP_NUITKA" ]]; then
-  (
-    cd semgrep
-    sudo make all
-  )
-  cp -r ./semgrep/build/semgrep.dist/* artifacts/
-fi
+# if [[ -z "$SKIP_NUITKA" ]]; then
+#   (
+#     cd semgrep
+#     sudo make all
+#   )
+#   cp -r ./semgrep/build/semgrep.dist/* artifacts/
+# fi
 cp ./semgrep-core/_build/default/bin/Main.exe artifacts/semgrep-core
 zip -r artifacts.zip artifacts
