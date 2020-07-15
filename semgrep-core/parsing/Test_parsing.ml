06753466feed78a072b362809737ffef7fa9ed5e
Yoann Padioleau <pad@returntocorp.com>
2020-07-15 20:49:20 +0200

    Skeleton to use the Go tree-sitter parser (#1284)
    * Skeleton to use the Go tree-sitter parser
    
    This is part1 of adding support for the Go tree-sitter-based parser.
    This diff just integrate the go tree-sitter library in the build system
    and create a boilerplate Parse_go_tree_sitter.ml to be filled later.
    
    Test plan:
    make
    
    * * semgrep-core/parsing/Parse_go_tree_sitter.ml:
    remove the map_ prefix to every func (optional but I prefer the boilerplate
    code that way)
    
    * * semgrep-core/parsing/Parse_go_tree_sitter.ml: add prelude header and
    main emtry point

--- semgrep-core/parsing/Test_parsing.ml
+++ semgrep-core/parsing/Test_parsing.ml
@@ -52,6 +52,7 @@ let test_parse_lang verbose lang get_final_files xs =
               match lang with
               | Lang.Ruby -> Tree_sitter_ruby.Parse.file file |> ignore
               | Lang.Java -> Tree_sitter_java.Parse.file file |> ignore
+              | Lang.Go   -> Tree_sitter_go.Parse.file file |> ignore
               | _ -> failwith "lang not supported by ocaml-tree-sitter"
               )
              file ()
