a87ee7aba744ee5ecc09a0baa85388c907fdaa35
Matt Schwager <matt@returntocorp.com>
2020-07-21 15:40:50 -0400

    Remove parentheses from Golang test

--- semgrep-core/tests/go/metavar_equality_stmt.go
+++ semgrep-core/tests/go/metavar_equality_stmt.go
@@ -3,19 +3,19 @@ package main
 import "fmt"
 
 func main() {
-    //ERROR:
-    if (x > 2) {
-        fmt.Println("hello world")
-        fmt.Println("goodbye world")
-    } else {
-        fmt.Println("hello world")
-        fmt.Println("goodbye world")
-    }
+	//ERROR:
+	if x > 2 {
+		fmt.Println("hello world")
+		fmt.Println("goodbye world")
+	} else {
+		fmt.Println("hello world")
+		fmt.Println("goodbye world")
+	}
 
-    if (x > 2) {
-        fmt.Println("hello world")
-        fmt.Println("goodbye world")
-    } else {
-        fmt.Println("hello world")
-    }
+	if x > 2 {
+		fmt.Println("hello world")
+		fmt.Println("goodbye world")
+	} else {
+		fmt.Println("hello world")
+	}
 }
