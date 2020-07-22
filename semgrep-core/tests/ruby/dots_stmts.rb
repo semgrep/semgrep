58179fbdabb35394518ff215ee1b29a099e3ebda
grayson <grayson@returntocorp.com>
2020-07-22 12:19:33 -0700

    Fix ERROR: comments in ruby tests

--- semgrep-core/tests/ruby/dots_stmts.rb
+++ semgrep-core/tests/ruby/dots_stmts.rb
@@ -1,7 +1,7 @@
-def foo()
+def foo(user)
     # ERROR:
-    user_data = get();
-    puts("... more stuff here ...")
-    eval(user_data);
+    user_data = get_data user
+    puts "... more stuff here ..."
+    eval user_data
 
 end
