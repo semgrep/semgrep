7cc64986cb2367b5ba422c9688b0e4bc58031303
Brendon Go <brendon.go@gmail.com>
2020-07-28 17:33:03 -0700

    Add timeout for maximum time spent running rule on file (#1385)

--- semgrep/semgrep/core_exception.py
+++ semgrep/semgrep/core_exception.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import Any
 from typing import Dict
 
+from semgrep.error import MatchTimeoutError
 from semgrep.error import SemgrepError
 from semgrep.error import SourceParseError
 from semgrep.rule_lang import Position
@@ -18,6 +19,7 @@ class CoreException:
         end: Position,
         extra: Dict[str, Any],
         language: str,
+        rule_id: str,
     ):
         """
             Object that encapsulates exception information returned by
@@ -28,6 +30,7 @@ class CoreException:
         self._start = start
         self._end = end
         self._language = language
+        self._rule_id = rule_id
 
         if "message" not in extra or "line" not in extra:
             # in pfff/h_program-lang/R2c.ml these fields always exist
@@ -36,7 +39,7 @@ class CoreException:
 
     @classmethod
     def from_json(  # type: ignore
-        cls, json_obj: Dict[str, Any], language: str
+        cls, json_obj: Dict[str, Any], language: str, rule_id: str
     ) -> "CoreException":
         if {"check_id", "path", "start", "end", "extra"}.difference(
             json_obj.keys()
@@ -56,27 +59,38 @@ class CoreException:
         start_pos = Position(start["line"], start["col"])
         end_pos = Position(end["line"], end["col"])
 
+        # Semgrep-Core caches TimeoutErrors as FatalErrors
+        # Hack to treat these as TimeoutErrors
+        check_id = json_obj["check_id"]
+        if check_id == "FatalError" and "Timeout" in json_obj["extra"]["message"]:
+            check_id = "Timeout"
+
         return cls(
-            json_obj["check_id"],
+            check_id,
             Path(json_obj["path"]),
             start_pos,
             end_pos,
             json_obj["extra"],
             language,
+            rule_id,
         )
 
     def into_semgrep_error(self) -> SemgrepError:
         with open(self._path, errors="replace") as f:
             file_hash = SourceTracker.add_source(f.read())
-        error_span = Span(
-            start=self._start,
-            end=self._end,
-            source_hash=file_hash,
-            file=self._path.name,
-        )
-        return SourceParseError(
-            short_msg="parse error",
-            long_msg=f"Could not parse {self._path.name} as {self._language}",
-            spans=[error_span],
-            help="If the code appears to be valid, this may be a semgrep bug.",
-        )
+
+        if self._check_id == "Timeout":
+            return MatchTimeoutError(self._path, self._rule_id)
+        else:
+            error_span = Span(
+                start=self._start,
+                end=self._end,
+                source_hash=file_hash,
+                file=self._path.name,
+            )
+            return SourceParseError(
+                short_msg="parse error",
+                long_msg=f"Could not parse {self._path.name} as {self._language}",
+                spans=[error_span],
+                help="If the code appears to be valid, this may be a semgrep bug.",
+            )
