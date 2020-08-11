0314f0cd6424c63be787dcedc82a2bfae52cde35
Sabrina Brogren <sabrina@returntocorp.com>
2020-08-11 10:12:22 -0400

    timeout threshold flag (#1458)

--- semgrep/tests/e2e/test_check.py
+++ semgrep/tests/e2e/test_check.py
@@ -186,8 +186,6 @@ def test_timeout(run_semgrep_in_tmp, snapshot):
 
 
 def test_max_memory(run_semgrep_in_tmp, snapshot):
-    # Check that semgrep-core timeouts are properly handled
-
     snapshot.assert_match(
         run_semgrep_in_tmp(
             "rules/long.yaml",
@@ -208,4 +206,40 @@ def test_max_memory(run_semgrep_in_tmp, snapshot):
             stderr=True,
         ),
         "error.txt",
+    )
+
+
+def test_timeout_threshold(run_semgrep_in_tmp, snapshot):
+    snapshot.assert_match(
+        run_semgrep_in_tmp(
+            "rules/multiple-long.yaml",
+            options=["--timeout", "1", "--timeout-threshold", "1"],
+            target_name="equivalence",
+            strict=False,
+        ),
+        "results.json",
+    )
+
+    snapshot.assert_match(
+        run_semgrep_in_tmp(
+            "rules/multiple-long.yaml",
+            output_format="normal",
+            options=["--timeout", "1", "--timeout-threshold", "1"],
+            target_name="equivalence",
+            strict=False,
+            stderr=True,
+        ),
+        "error.txt",
+    )
+
+    snapshot.assert_match(
+        run_semgrep_in_tmp(
+            "rules/multiple-long.yaml",
+            output_format="normal",
+            options=["--timeout", "1", "--timeout-threshold", "2"],
+            target_name="equivalence",
+            strict=False,
+            stderr=True,
+        ),
+        "error_2.txt",
     )
