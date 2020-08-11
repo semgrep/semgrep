344ddddadd6ca8a88ccbdf9c6c4104cddf8a73c1
brendon <brendon.go@gmail.com>
2020-08-11 15:22:46 -0700

    fixup! Check perf tests

--- semgrep/tests/performance/test_ci_perf.py
+++ semgrep/tests/performance/test_ci_perf.py
@@ -15,27 +15,6 @@ def test_perf(clone_github_repo):
         sha="d1c5df41393ba512cbd362874a7a0bdc7dbf43fc",
     )
 
-    # Running on Juice Shop without three.js takes ~150 sec
-    target_path = clone_github_repo(
-        repo_url="https://github.com/bkimminich/juice-shop",
-        sha="98633f5ef242bf943608324a562058b22eca6dfe",
-    )
-    start = time.time()
-    subprocess.check_output(
-        [
-            "python3",
-            "-m" "semgrep",
-            "--config",
-            str(rules_path),
-            "--exclude",
-            "three.js",
-            str(target_path),
-        ]
-    )
-    duration = time.time() - start
-    print(duration)
-    assert duration < 160
-
     # Dvna takes about 30 sec
     target_path = clone_github_repo(
         repo_url="https://github.com/appsecco/dvna", sha="c637437"
@@ -47,3 +26,25 @@ def test_perf(clone_github_repo):
     duration = time.time() - start
     print(duration)
     assert duration < 40
+
+    # Running on Juice Shop without three.js takes ~150 sec on 2019MBP 15"
+    # takes ~270 on GHA
+    target_path = clone_github_repo(
+        repo_url="https://github.com/bkimminich/juice-shop",
+        sha="98633f5ef242bf943608324a562058b22eca6dfe",
+    )
+    start = time.time()
+    subprocess.check_output(
+        [
+            "python3",
+            "-m" "semgrep",
+            "--config",
+            str(rules_path / "njsscan/rules/semantic_grep"),
+            "--exclude",
+            "three.js",
+            str(target_path),
+        ]
+    )
+    duration = time.time() - start
+    print(duration)
+    assert duration < 275
