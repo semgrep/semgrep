631da60e5deace6cfa2ece3f4f3f92626d624fb9
mschwager <mschwager@users.noreply.github.com>
2020-08-31 15:01:32 -0600

    Remove install-script release functionality (#1615)
    * Remove install-script release functionality
    
    * Bump CHANGELOG
    
    * Remove additional unnecessary behavior

--- scripts/ubuntu-release.sh
+++ scripts/ubuntu-release.sh
@@ -15,17 +15,5 @@ sudo apt-get install -y --no-install-recommends \
      libcurl4-openssl-dev libexpat1-dev gettext libz-dev libssl-dev \
      build-essential autoconf musl-tools
 
-if [[ -z "${SKIP_NUITKA+x}" ]]; then
-  export PATH=/github/home/.local/bin:$PATH
-  (
-    cd semgrep
-    sudo make all
-  )
-fi
-
-mkdir -p semgrep-files
-cp -r ./semgrep/build/semgrep.dist/* semgrep-files
-ls semgrep-files
-chmod +x semgrep-files/semgrep
 chmod +x semgrep-files/semgrep-core
 tar -cvzf artifacts.tar.gz semgrep-files/
