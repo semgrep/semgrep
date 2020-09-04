2d2c141c303682cf3ea3d6b19afb5b25d68dbaed
mschwager <mschwager@users.noreply.github.com>
2020-09-04 15:32:36 -0600

    Remove Semgrep binary checks from ubuntu release (#1627)

--- scripts/validate-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "def silly_eq(a, b):" >> /tmp/test.py
-echo " return a + b == a + b" >> /tmp/test.py
-
-echo -n "Testing your semgrep installation..."
-# shellcheck disable=SC2016
-semgrep /tmp/test.py -l python -e '$X == $X' | grep "return a + b == a + b"
-echo "OK! Enjoy semgrep :-)"
