603cc9dd91a25795d074ea6cf691db434c38a11a
pad <yoann.padioleau@gmail.com>
2020-09-09 17:25:43 +0200

    * semgrep-core/bin/Main.ml: remove errors

--- semgrep-core/bin/Main.ml
+++ semgrep-core/bin/Main.ml
@@ -1068,8 +1068,6 @@ let format_output_exception e : string =
 (*s: function [[Main_semgrep_core.main]] *)
 let main () =
   set_gc ();
-  let _ = 1 = 1 in
-  let _ = 2 = 2 in
 
   let usage_msg =
     spf "Usage: %s [options] <pattern> <files_or_dirs> \nOptions:"
