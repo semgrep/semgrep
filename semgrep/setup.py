f758aa8bd291cdbdef072a66daa5c89c6d9e62b0
grayson <grayson@returntocorp.com>
2020-09-23 10:06:02 -0700

    Remove duplicate packages in Pipfile and setup.py

--- semgrep/setup.py
+++ semgrep/setup.py
@@ -125,7 +125,6 @@ setup(
         "attrs>=19.3.0",
         "colorama>=0.4.3",
         "junit_xml==1.9",
-        "packaging>=20.4",
         "requests>=2.22.0",
         # exact version because of unstable API
         "ruamel.yaml==0.16.10",
