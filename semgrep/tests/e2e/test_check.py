454daaf0cfaa0e0c6cf294db27d6f109d00d324e
Isaac Evans <ine@mit.edu>
2020-09-26 15:37:23 -0700

    fix expected tests

--- semgrep/tests/e2e/test_check.py
+++ semgrep/tests/e2e/test_check.py
@@ -1,9 +1,9 @@
 import json
-from xmldiff import main
 from pathlib import Path
 from subprocess import CalledProcessError
 
 import pytest
+from xmldiff import main
 
 from semgrep import __VERSION__
 
@@ -52,7 +52,7 @@ def test_multiline(run_semgrep_in_tmp, snapshot):
 def test_junit_xml_output(run_semgrep_in_tmp, snapshot):
     actual_output = run_semgrep_in_tmp("rules/eqeq.yaml", output_format="junit-xml")
 
-    f = open(str(snapshot.snapshot_dir) + '/results.xml', "r")
+    f = open(str(snapshot.snapshot_dir) + "/results.xml", "r")
     expected_output = f.read()
     f.close()
 
@@ -236,7 +236,7 @@ def test_max_memory(run_semgrep_in_tmp, snapshot):
     snapshot.assert_match(
         run_semgrep_in_tmp(
             "rules/long.yaml",
-            options=["--max-memory", "1"],
+            options=["--verbose", "--max-memory", "1"],
             target_name="equivalence",
             strict=False,
         ),
@@ -247,7 +247,7 @@ def test_max_memory(run_semgrep_in_tmp, snapshot):
         run_semgrep_in_tmp(
             "rules/long.yaml",
             output_format="normal",
-            options=["--max-memory", "1"],
+            options=["--verbose", "--max-memory", "1"],
             target_name="equivalence",
             strict=False,
             stderr=True,
@@ -260,7 +260,7 @@ def test_timeout_threshold(run_semgrep_in_tmp, snapshot):
     snapshot.assert_match(
         run_semgrep_in_tmp(
             "rules/multiple-long.yaml",
-            options=["--timeout", "1", "--timeout-threshold", "1"],
+            options=["--verbose", "--timeout", "1", "--timeout-threshold", "1"],
             target_name="equivalence",
             strict=False,
         ),
@@ -271,7 +271,7 @@ def test_timeout_threshold(run_semgrep_in_tmp, snapshot):
         run_semgrep_in_tmp(
             "rules/multiple-long.yaml",
             output_format="normal",
-            options=["--timeout", "1", "--timeout-threshold", "1"],
+            options=["--verbose", "--timeout", "1", "--timeout-threshold", "1"],
             target_name="equivalence",
             strict=False,
             stderr=True,
@@ -283,7 +283,7 @@ def test_timeout_threshold(run_semgrep_in_tmp, snapshot):
         run_semgrep_in_tmp(
             "rules/multiple-long.yaml",
             output_format="normal",
-            options=["--timeout", "1", "--timeout-threshold", "2"],
+            options=["--verbose", "--timeout", "1", "--timeout-threshold", "2"],
             target_name="equivalence",
             strict=False,
             stderr=True,
