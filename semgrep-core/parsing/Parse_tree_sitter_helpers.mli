2687b409a587e12d13b1b23604d8319bcdcab261
pad <yoann.padioleau@gmail.com>
2020-10-01 16:43:31 +0200

    Convert tree-sitter parse error exn to Parse_info.Parsing_error
    There is already some code to automatically intercept Parse_info exns
    to display their location, so better to use a consistent scheme
    across pfff and tree-sitter parsers.
    
    test plan:
    ```
    (semgrep) pad@yrax:~/semgrep/tests/OTHER/parsing_errors$ semgrep -l ts -e 'FOO' .
    warn: parse error
      --> err.ts:2
    2 |          return 1+
      |          ^^^^^^^^^
    = help: If the code appears to be valid, this may be a semgrep bug.
    Could not parse err.ts as ts
    
    Warnings exist. Run with `--strict` to turn warnings into errors.
    ```
    
    better than the previous error that was always reported on the first line.
    This also improves error location for the errors reported in
    https://github.com/returntocorp/semgrep/issues/1713

--- semgrep-core/parsing/Parse_tree_sitter_helpers.mli
+++ semgrep-core/parsing/Parse_tree_sitter_helpers.mli
@@ -12,3 +12,5 @@ val token: env -> Tree_sitter_run.Token.t -> Parse_info.t
 val str: env -> Tree_sitter_run.Token.t -> string * Parse_info.t
 
 val combine_tokens: env -> Tree_sitter_run.Token.t list -> Parse_info.t
+
+val convert_tree_sitter_exn_to_pfff_exn: (unit -> 'a) -> 'a
