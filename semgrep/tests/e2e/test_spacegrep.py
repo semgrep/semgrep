2779e1411477147bccab6accc1c45ce810f4e748
grayson <grayson@returntocorp.com>
2020-10-15 19:08:08 -0500

    Add tests for spacegrep

---
+++ semgrep/tests/e2e/test_spacegrep.py
@@ -0,0 +1,19 @@
+import tempfile
+from pathlib import Path
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "rule,target",
+    [
+        ("rules/spacegrep/terraform.yaml", "spacegrep/terraform.tf"),
+        ("rules/spacegrep/html.yaml", "spacegrep/html.mustache"),
+        ("rules/spacegrep/markdown.yaml", "spacegrep/markdown.md"),
+    ],
+)
+def test_spacegrep(run_semgrep_in_tmp, snapshot, rule, target):
+    # Yes, this is fugly. I apologize. T_T
+    snapshot.assert_match(
+        run_semgrep_in_tmp(rule, target_name=target), "results.json",
+    )
