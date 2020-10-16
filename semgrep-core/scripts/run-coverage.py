485a3915f2f7db030e3bc625c7f6be93f6a8b085
Yoann Padioleau <pad@returntocorp.com>
2020-10-16 15:54:40 +0200

    Basic coverage analysis per languages (#1847)
    * Basic coverage analysis per languages
    
    This uses the bisect_ppx OCaml package to turn-on instrumentation
    for coverage analysis.
    Right now, we enable this coverage just for the semgrep-core/matching/
    directory (the matching engine behing semgrep).
    We run all the semgrep-core tests in tests/Test.ml, which itself
    use the many tests under sengrep-core/tests/.
    
    Test plan:
    $ ./scripts/run-coverage.py
    Coverage statistics for files under semgrep-core/matching/
    Aggregated coverage for all languages: 59.21%
    Coverage stat for Python: 41.10%
    Coverage stat for Javascript: 38.90%
    Coverage stat for Typescript: 38.74%
    Coverage stat for JSON: 9.11%
    Coverage stat for Java: 44.97%
    Coverage stat for C: 22.51%
    Coverage stat for Go: 34.92%
    Coverage stat for OCaml: 13.04%
    Coverage stat for Ruby: 18.06%
    Coverage stat for PHP: 13.72%
    Coverage stat for eval in file matching/Eval_generic.ml: 45.83%
    
    * * semgrep-core/scripts/run-coverage.py: fix wrong types
    
    * * semgrep-core/scripts/run-coverage.py: cleanup
    
    * Adding the computed coverage metrics to the dashboard
    
    Test plan:
    $ ./scripts/run-coverage.sh
    
    go to https://dashboard.semgrep.dev/metric/semgrep.core.go.coverage.matching.pct

---
+++ semgrep-core/scripts/run-coverage.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+import os
+import re
+
+path = "_build/default/tests"
+
+print("Coverage statistics for files under semgrep-core/matching/")
+
+
+def report_summary_stat() -> str:
+    stat = os.popen("bisect-ppx-report summary").read()
+    patt = re.compile("Coverage:\s+\d+/\d+\s+\((\d+\.\d*)%\)")
+    # mobj = patt.match("Coverage: 4/4 (4.4%)")
+    mobj = patt.match(stat)
+    if mobj is not None:
+        return mobj.group(1)
+    raise Exception("")
+
+
+def report_summary_for_file_stat(file: str) -> str:
+    stat = os.popen("bisect-ppx-report summary --per-file").readlines()
+    patt = re.compile(f"\s*(\d+.\d*)\s+%\s+\d+/\d+\s+{file}")
+    for line in stat:
+        mobj = patt.match(line)
+        if mobj is not None:
+            return mobj.group(1)
+    raise Exception("")
+
+
+metrics_URL = "https://dashboard.semgrep.dev/api/metric"
+
+
+def add_metric(category: str, value: str) -> None:
+    path = f"semgrep.core.{category}.coverage.matching.pct"
+    cmd = f"curl -X POST {metrics_URL}/{path} -d '{value}' 2> /dev/null"
+    out = os.popen(cmd).read()  # nosem
+    patt = re.compile(".*successfully recorded")
+    if patt.match(out) is None:
+        raise Exception(f"Could not push the metric: {out}")
+
+
+# to compile with coverage on
+os.system("dune runtest --instrument-with bisect_ppx --force 2> /dev/null")
+global_stat = report_summary_stat()
+print(f"Aggregated coverage for all languages: {global_stat}%")
+add_metric("all", global_stat)
+
+# Substrings to pass to text.exe to run a test subset; see ../tests/Test.ml
+languages = [
+    "Python",
+    "Javascript",
+    "Typescript",
+    "JSON",
+    "Java",
+    "C",
+    "Go",
+    "OCaml",
+    "Ruby",
+    "PHP",
+]
+# cleanup and run the analysis for each separate languages
+for lang in languages:
+    os.system(f"rm -f {path}/*.coverage")  # nosem
+    os.system(f"cd {path};./test.exe {lang} > /dev/null")  # nosem
+    lang_stat = report_summary_stat()
+    print(f"Coverage stat for {lang}: {lang_stat}%")
+    add_metric(lang.lower(), lang_stat)
+
+subsystems = [
+    ("eval", "matching/Eval_generic.ml"),
+]
+
+
+for (test, file) in subsystems:
+    os.system(f"rm -f {path}/*.coverage")  # nosem
+    os.system(f"cd {path};./test.exe {test} > /dev/null")  # nosem
+    stat = report_summary_for_file_stat(file)
+    print(f"Coverage stat for {test} in file {file}: {stat}%")
