e688b679a61e1d8a53a2fbfcb4f0bd2e05f12e04
pad <yoann.padioleau@gmail.com>
2020-10-16 13:25:36 +0200

    Adding the computed coverage metrics to the dashboard
    Test plan:
    $ ./scripts/run-coverage.sh
    
    go to https://dashboard.semgrep.dev/metric/semgrep.core.go.coverage.matching.pct

--- semgrep-core/scripts/run-coverage.py
+++ semgrep-core/scripts/run-coverage.py
@@ -27,10 +27,23 @@ def report_summary_for_file_stat(file: str) -> str:
     raise Exception("")
 
 
+metrics_URL = "https://dashboard.semgrep.dev/api/metric"
+
+
+def add_metric(category: str, value: str) -> None:
+    path = f"semgrep.core.{category}.coverage.matching.pct"
+    cmd = f"curl -X POST {metrics_URL}/{path} -d '{value}' 2> /dev/null"
+    out = os.popen(cmd).read()  # nosem
+    patt = re.compile(".*successfully recorded")
+    if patt.match(out) is None:
+        raise Exception(f"Could not push the metric: {out}")
+
+
 # to compile with coverage on
 os.system("dune runtest --instrument-with bisect_ppx --force 2> /dev/null")
 global_stat = report_summary_stat()
 print(f"Aggregated coverage for all languages: {global_stat}%")
+add_metric("all", global_stat)
 
 # Substrings to pass to text.exe to run a test subset; see ../tests/Test.ml
 languages = [
@@ -51,6 +64,7 @@ for lang in languages:
     os.system(f"cd {path};./test.exe {lang} > /dev/null")  # nosem
     lang_stat = report_summary_stat()
     print(f"Coverage stat for {lang}: {lang_stat}%")
+    add_metric(lang.lower(), lang_stat)
 
 subsystems = [
     ("eval", "matching/Eval_generic.ml"),
