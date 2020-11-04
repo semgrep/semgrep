68362a023bd35da107bbde9baf526c9d88a81735
grayson <grayson@returntocorp.com>
2020-11-03 22:37:57 -0600

    Fix https://github.com/returntocorp/semgrep/issues/1912. Patch 'config_id' with an incrementally increasing integer. Added unit test to make sure two configs are resolved.

--- semgrep/tests/unit/test_yaml_parsing.py
+++ semgrep/tests/unit/test_yaml_parsing.py
@@ -1,3 +1,7 @@
+from tempfile import NamedTemporaryFile
+from textwrap import dedent
+
+from semgrep.config_resolver import Config
 from semgrep.config_resolver import parse_config_string
 from semgrep.config_resolver import validate_single_rule
 from semgrep.constants import RULES_KEY
@@ -39,3 +43,43 @@ rules:
     for rule_dict in rules.value:
         validate_single_rule("testfile", rule_dict)
     assert True
+
+
+def test_multiple_configs():
+    config1 = dedent(
+        """
+    rules:
+    - id: rule1
+      pattern: $X == $X
+      languages: [python]
+      severity: INFO
+      message: bad
+    """
+    )
+    config2 = dedent(
+        """
+    rules:
+    - id: rule2
+      pattern: $X == $Y
+      languages: [python]
+      severity: INFO
+      message: good
+    - id: rule3
+      pattern: $X < $Y
+      languages: [c]
+      severity: INFO
+      message: doog
+    """
+    )
+
+    with NamedTemporaryFile() as tf1, NamedTemporaryFile() as tf2:
+        tf1.write(config1.encode("utf-8"))
+        tf2.write(config2.encode("utf-8"))
+        tf1.flush()
+        tf2.flush()
+        config_list = [tf1.name, tf2.name]
+        config, errors = Config.from_config_list(config_list)
+        assert not errors
+        rules = config.get_rules(True)
+        assert len(rules) == 3
+        assert {"rule1", "rule2", "rule3"} == set([rule.id for rule in rules])
