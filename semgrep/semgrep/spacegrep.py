9240ac33921458ee88e6055da8f9ed09f1b53574
minusworld <grayson@returntocorp.com>
2020-11-05 10:16:24 -0800

    Add error handling to spacegrep (#1966)
    * Add error handling to spacegrep
    
    * Print spacegrep's stderr when CalledProcessError occurs

--- semgrep/semgrep/spacegrep.py
+++ semgrep/semgrep/spacegrep.py
@@ -15,6 +15,7 @@ from semgrep.constants import PLEASE_FILE_ISSUE_TEXT
 from semgrep.constants import SPACEGREP_PATH
 from semgrep.core_exception import CoreException
 from semgrep.error import InvalidPatternError
+from semgrep.error import SemgrepError
 from semgrep.pattern import Pattern
 from semgrep.pattern_match import PatternMatch
 from semgrep.util import sub_run
@@ -38,15 +39,32 @@ def run_spacegrep(patterns: List[Pattern], targets: List[Path]) -> dict:
                 str(target),
                 pattern_str,
             ]
-            p = sub_run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            raw_output = p.stdout
-            raw_error = p.stderr
+            try:
+                p = sub_run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                p.check_returncode()
+                raw_output = p.stdout
 
-            output_json = _parse_spacegrep_output(raw_output)
-            output_json["matches"] = _patch_id(pattern, output_json.get("matches", []))
+                output_json = _parse_spacegrep_output(raw_output)
+                output_json["matches"] = _patch_id(
+                    pattern, output_json.get("matches", [])
+                )
 
-            matches.extend(output_json["matches"])
-            errors.extend(output_json["errors"])
+                matches.extend(output_json["matches"])
+                errors.extend(output_json["errors"])
+            except subprocess.CalledProcessError as e:
+                raw_error = p.stderr
+                spacegrep_error_text = raw_error.decode("utf-8")
+                raise SemgrepError(
+                    f"Error running spacegrep on file {target}: Process error: {e}\n\nspacegrep error: {spacegrep_error_text}"
+                )
+            except json.JSONDecodeError as e:
+                raise SemgrepError(
+                    f"Could not parse spacegrep output as JSON: JSON error: {e}"
+                )
+            except KeyError as e:
+                raise SemgrepError(
+                    f"Invalid JSON output was received from spacegrep: {e}"
+                )
 
     return {
         "matches": matches,
@@ -55,13 +73,9 @@ def run_spacegrep(patterns: List[Pattern], targets: List[Path]) -> dict:
 
 
 def _parse_spacegrep_output(raw_output: bytes) -> dict:
-    try:
-        output = raw_output.decode("utf-8")
-        data = json.loads(output)
-        return cast(dict, data)
-    except Exception:
-        logger.error("spacegrep output could not be parsed as JSON.")
-        return {}
+    output = raw_output.decode("utf-8")
+    data = json.loads(output)
+    return cast(dict, data)
 
 
 def _patch_id(pattern: Pattern, matches: List[dict]) -> List[dict]:
