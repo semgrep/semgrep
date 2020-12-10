6a5bf8fd793c4744f346df5c56483664f5a65d1c
Colleen Dai <colleen@returntocorp.com>
2020-12-10 11:23:00 -0600

    del file

--- semgrep-core/tests/js/metavar_anno.js
+++ semgrep-core/tests/js/metavar_anno.js
@@ -1,29 +1,21 @@
 // ERROR:
 @Anno1
 class Foo {
-    foo() {
-        var foo = 1;
-    }
+    var foo = 1;
 }
 
 // ERROR:
-@Anno1()
-@Anno2()
+@Anno1
+@Anno2
 class FooBar {
-    foo() {
-        var foo = 2;
-    }
+    var foo = 2;
 }
 
 @AnnoVar("Bar")
 class Bar {
-    bar() {
-        var bar = 1;
-    }
+    var bar = 1;
 }
 
 class NoAnno {
-    noanno() {
-        var no_anno = 1;
-    }
+    var no_anno = 1;
 }
