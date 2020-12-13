736115f8939d4df5ea9807116f6760a8bf2e4c7b
Martin Jambon <martin@returntocorp.com>
2020-12-13 02:03:15 -0800

    Some tests for the toy matcher. Needs fixing.

--- semgrep-core/toy-matcher/Matcher.ml
+++ semgrep-core/toy-matcher/Matcher.ml
@@ -40,7 +40,7 @@ let sample_pattern : pattern = [
 
    This is a sanity check, not part of the matching algorithm.
 *)
-let rec check_pattern env (pat : pattern) =
+let rec check_pattern_ env (pat : pattern) =
   match pat with
   | [] -> ()
   | (atom, opt_name) :: pat ->
@@ -61,7 +61,10 @@ let rec check_pattern env (pat : pattern) =
            if not (List.mem name env) then
              failwith ("backreference to invalid name: " ^ name)
       );
-      check_pattern new_env pat
+      check_pattern_ new_env pat
+
+let check_pattern pat =
+  check_pattern_ [] pat
 
 module Env = Map.Make (String)
 
@@ -105,18 +108,30 @@ let extend env opt_ellipsis opt_name captured_sequence =
   | Some name ->
       Env.add name captured_sequence env
 
+type stat = {
+  mutable match_calls: int;
+}
+
 (*
-   Match a pattern at the beginning of the input sequence.
+   Check if a pattern matches the input sequence completely.
 *)
-let rec match_ ~ellipsis env pat input =
+let rec match_ stat ~ellipsis env pat input =
+  stat.match_calls <- stat.match_calls + 1;
   let orig_pat = pat in
   let in_ellipsis = ellipsis <> None in
   match pat with
   | [] ->
-      if in_ellipsis || input = [] then
-        Some env
-      else
-        None
+      (match input with
+       | [] ->
+           let env = close_ellipsis ellipsis env in
+           Some env
+       | symbol :: input ->
+           if in_ellipsis then
+             let ellipsis = extend_ellipsis ellipsis symbol in
+             match_ stat ~ellipsis env orig_pat input
+           else
+             None
+      )
   | (pat_atom, opt_name) :: pat ->
       match input with
       | [] ->
@@ -124,12 +139,12 @@ let rec match_ ~ellipsis env pat input =
           (match pat_atom with
            | Ellipsis ->
                let env = extend env ellipsis opt_name [] in
-               match_ ~ellipsis:(init_ellipsis opt_name []) env pat input
+               match_ stat ~ellipsis:(init_ellipsis opt_name []) env pat input
            | Backref name ->
                (match Env.find_opt name env with
                 | Some [] ->
                     let env = extend env ellipsis opt_name [] in
-                    match_ ~ellipsis:None env pat input
+                    match_ stat ~ellipsis:None env pat input
                 | _ ->
                     None
                )
@@ -164,20 +179,24 @@ let rec match_ ~ellipsis env pat input =
               if in_ellipsis then
                 (* skip input symbol and retry *)
                 let ellipsis = extend_ellipsis ellipsis symbol in
-                match_ ~ellipsis env orig_pat input
+                match_ stat ~ellipsis env orig_pat input
               else
                 None
           | Some (ellipsis, env) ->
               (* match the rest of the pattern with the rest of the input *)
-              match_ ~ellipsis env pat input
+              match_ stat ~ellipsis env pat input
 
 (*
    Public interface for the recursive 'match_' function.
 *)
 let match_input pat input =
-  match match_ ~ellipsis:None Env.empty pat input with
-  | None -> None
-  | Some env -> Some (Env.bindings env)
+  let stat = { match_calls = 0 } in
+  let opt_captures =
+    match match_ stat ~ellipsis:None Env.empty pat input with
+    | None -> None
+    | Some env -> Some (Env.bindings env)
+  in
+  opt_captures, stat
 
 (*
    Turn a string into a list of chars.
@@ -200,24 +219,136 @@ let print_result opt_bindings =
       print_endline "not a match"
   | Some l ->
       print_endline "match: {";
-      List.iter (fun (name, symbols) ->
-        printf "%s: %s\n"
-          name (unparse symbols)
+      List.iter (fun (name, subseq) ->
+        printf "  %s: %S\n"
+          name subseq
       ) l;
       print_endline "}"
 
+let print_time name f =
+  let t1 = Unix.gettimeofday () in
+  let res = f () in
+  let t2 = Unix.gettimeofday () in
+  printf "%s: %.6f s\n%!" name (t2 -. t1);
+  res
+
 let check_match pat input_str expected_opt_bindings =
   let sort = Option.map (List.sort compare) in
+  let normalize opt_bindings =
+    Option.map (fun bindings ->
+      List.map (fun (var, symbols) -> (var, unparse symbols)) bindings
+    ) opt_bindings
+    |> sort
+  in
+  check_pattern pat;
   let input = parse input_str in
   let expected = sort expected_opt_bindings in
-  let actual = match_input pat input |> sort in
+  let actual =
+    let res, stat =
+      print_time "match" (fun () ->
+        match_input pat input
+      )
+    in
+    printf "input length: %i\n" (String.length input_str);
+    printf "number of calls to the match function: %i\n" stat.match_calls;
+    normalize res
+  in
+  print_result actual;
   Alcotest.(check bool) "equal" true (expected = actual)
 
-let test_simple () =
-  let pat = [Symbol 'A', None] in
+let test_simple_symbol () =
+  let pat = [Symbol 'A', Some "a"] in
   let input = "A" in
-  check_match pat input (Some [])
+  check_match pat input (Some ["a", "A"])
+
+let test_simple_ellipsis () =
+  let pat = [Ellipsis, Some "x"] in
+  let input = "ABC" in
+  check_match pat input (Some ["x", "ABC"])
+
+let test_any_symbol () =
+  let pat = [Any_symbol, Some "a"] in
+  let input = "A" in
+  check_match pat input (Some ["a", "A"])
+
+let test_floating_symbol () =
+  let pat = [
+    Ellipsis, Some "head";
+    Symbol 'A', Some "a";
+    Ellipsis, Some "tail";
+  ] in
+  let input = "012345A6789" in
+  check_match pat input (Some [
+    "head", "012345";
+    "a", "A";
+    "tail", "6789";
+  ])
+
+let test_backref () =
+  let pat = [
+    Ellipsis, None;
+    Any_symbol, Some "orig";
+    Backref "orig", Some "copy";
+    Ellipsis, None;
+  ] in
+  let input = "ABBC" in
+  check_match pat input (Some [
+    "orig", "B";
+    "copy", "B";
+  ])
+
+let test_gap () =
+  let pat = [
+    Symbol 'A', Some "a";
+    Ellipsis, Some "gap";
+    Symbol 'B', Some "b";
+  ] in
+  let input = "A12B" in
+  check_match pat input (Some [
+    "a", "A";
+    "gap", "12";
+    "b", "B";
+  ])
+
+let test_backref_backtrack () =
+  let pat = [
+    Ellipsis, None;
+    Any_symbol, Some "x";
+    Ellipsis, None;
+    Backref "x", None;
+    Symbol 'C', None;
+    Ellipsis, None;
+  ] in
+  let input = "ABBCA" in
+  check_match pat input (Some [
+    "x", "B";
+  ])
+
+(*
+   This is equivalent in semgrep to searching for a pattern like
+   '$A; ... foo;', in a file where the statement 'foo;' doesn't exist.
+
+   With the naive match algorithm, the complexity is O(n^2) where n is the
+   input length.
+*)
+let test_quadratic () =
+  let pat = [
+    Ellipsis, None;
+    Any_symbol, None; (* matches everywhere *)
+    Ellipsis, None;
+    Symbol 'x', None; (* doesn't exist in the input *)
+    Ellipsis, None;
+  ] in
+  let input = String.make 10_000 'A' in
+  check_match pat input None
 
 let test = "Matcher", [
-  "simple", `Quick, test_simple;
+  "simple symbol", `Quick, test_simple_symbol;
+  "simple ellipsis", `Quick, test_simple_ellipsis;
+  "any symbol", `Quick, test_any_symbol;
+  "floating symbol", `Quick, test_floating_symbol;
+  "backref", `Quick, test_backref;
+  "gap", `Quick, test_gap;
+  "backref backtrack", `Quick, test_backref_backtrack;
+  "quadratic", `Quick, test_quadratic;
 ]
