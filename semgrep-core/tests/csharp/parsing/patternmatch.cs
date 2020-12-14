2d2148829dff88b7397ab59b4dd0143e799a1643
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2020-12-14 15:02:58 +0100

    Parse pattern matching in switch in C#
    E.g.
    ```csharp
    var text = (i % 3, i % 5) switch {
        (0, 0) => "FizzBuzz",
        ...
    ```
    
    Related to #1392

---
+++ semgrep-core/tests/csharp/parsing/patternmatch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class HelloWorldPattern
+{
+    public static void Main()
+    {
+        for (int i = 1; i < 20; i++)
+        {
+            var text = (i % 3, i % 5) switch
+            {
+                (0, 0) => "FizzBuzz",
+                (0, _) => "Fizz",
+                (_, 0) => "Buzz",
+                (_, _) => i.ToString()
+            };
+            Console.WriteLine(text);
+        }
+    }
+}
