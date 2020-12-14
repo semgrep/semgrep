c9c4ac28499c4df0b634af41d56ea99af7fb1a77
colleend <colleen@returntocorp.com>
2020-12-14 11:42:55 -0600

    Fix metavar anno (#2180)
    * fixing metavar_anno js code that couldn't be fully parsed
    
    * fixing metavar_anno js code that couldn't be fully parsed
    
    * remove i

--- semgrep-core/tests/js/metavar_anno.js
+++ semgrep-core/tests/js/metavar_anno.js
@@ -1,21 +1,29 @@
 // ERROR:
 @Anno1
 class Foo {
-    var foo = 1;
+    foo() {
+        var foo = 1;
+    }
 }
 
 // ERROR:
-@Anno1
-@Anno2
+@Anno1()
+@Anno2()
 class FooBar {
-    var foo = 2;
+    foo() {
+        var foo = 2;
+    }
 }
 
 @AnnoVar("Bar")
 class Bar {
-    var bar = 1;
+    bar() {
+        var bar = 1;
+    }
 }
 
 class NoAnno {
-    var no_anno = 1;
+    noanno() {
+        var no_anno = 1;
+    }
 }
