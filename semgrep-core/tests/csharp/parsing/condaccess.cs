fcf3e455a85bf3ee1adfaad0dcb3ff913675c2e7
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2021-01-05 14:31:29 +0100

    Test file for conditional access

---
+++ semgrep-core/tests/csharp/parsing/condaccess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class HelloWorldCondAccess
+{
+    private static string MaybeNull(int arg)
+    {
+        if (arg > 0)
+        {
+            return "Hello";
+        }
+
+        return null;
+    }
+    public static void Main()
+    {
+        Console.WriteLine(MaybeNull(0)?.Length);
+        Console.WriteLine(MaybeNull(1)?.Length);
+        var greeting = MaybeNull(0);
+        Console.WriteLine(greeting?.Length);
+    }
+}
