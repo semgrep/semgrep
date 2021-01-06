becd86bb0420dc82dc1f6277dbc8fc5432083ecf
Colleen Dai <colleen@returntocorp.com>
2021-01-06 16:27:41 -0600

    more tests for string interpolation

--- semgrep-core/tests/python/dots_string.py
+++ semgrep-core/tests/python/dots_string.py
@@ -2,3 +2,6 @@ def foo():
     # ERROR:
     foo("whatever sequence of chars")
     # foo('whatever sequence of chars')
+    foo(f'string {var} interpolation')
+    # ERROR:
+    foo(f'constant string')
--- semgrep-core/tests/ruby/dots_string.rb
+++ semgrep-core/tests/ruby/dots_string.rb
@@ -2,5 +2,6 @@ def foo()
     # ERROR:
     foo("whatever sequence of chars")
     # foo('whatever sequence of chars')
+    foo("string ${var} interpolation")
 end
 
