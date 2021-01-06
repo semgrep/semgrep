471b96459e18708894465c3a6c2e29bde4b8f794
Matt Schwager <matt@returntocorp.com>
2021-01-06 09:43:31 -0700

    Fix CI

--- scripts/build-wheels.sh
+++ scripts/build-wheels.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 set -e
 pip install setuptools wheel
-cd semgrep && SEMGREP_SKIP_BIN=true python setup.py sdist bdist_wheel
+cd semgrep && python setup.py sdist bdist_wheel
 # Zipping for a stable name to upload as an artifact
 zip -r dist.zip dist
