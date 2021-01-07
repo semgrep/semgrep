e74d7301b83993d49903f9fda4f60cbb545501e7
colleend <colleen@returntocorp.com>
2021-01-07 10:58:20 -0600

    more tests for string interpolation (#2342)
    * more tests for string interpolation
    
    * adding comment to make it more clear

--- semgrep-core/tests/ruby/dots_string.rb
+++ semgrep-core/tests/ruby/dots_string.rb
@@ -2,5 +2,8 @@ def foo()
     # ERROR:
     foo("whatever sequence of chars")
     # foo('whatever sequence of chars')
+
+    # this string is not a constant, and therefore will not be matched.
+    foo("string #{var} interpolation")
 end
 
