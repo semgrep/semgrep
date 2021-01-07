36619725779d0a557ff178608f1bf5e13826d364
colleend <colleen@returntocorp.com>
2021-01-07 10:58:20 -0600

    more tests for string interpolation (#2342)
    * more tests for string interpolation
    
    * adding comment to make it more clear

--- semgrep-core/tests/python/dots_string.py
+++ semgrep-core/tests/python/dots_string.py
@@ -2,3 +2,9 @@ def foo():
     # ERROR:
     foo("whatever sequence of chars")
     # foo('whatever sequence of chars')
+
+    # ERROR:
+    foo(f'constant string')
+
+    # this string is not a constant, and therefore will not be matched.
+    foo(f'string {var} interpolation')
