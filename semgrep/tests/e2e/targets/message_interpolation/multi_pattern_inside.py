eef990d6ff743df3bcec0973f07118673c19f255
grayson <grayson@returntocorp.com>
2021-01-09 00:35:47 -0600

    Update e2e tests. Validated results by inspecting messages.

---
+++ semgrep/tests/e2e/targets/message_interpolation/multi_pattern_inside.py
@@ -0,0 +1,16 @@
+class A():
+    def B():
+        print("blah")
+
+    def C():
+        print("bla")
+
+    def D():
+        print('ble')
+
+def E():
+    print("asdf")
+
+class F():
+    def G():
+        print("qwerty")
--- semgrep/tests/e2e/test_message_interpolation.py
+++ semgrep/tests/e2e/test_message_interpolation.py
@@ -8,18 +8,16 @@ import pytest
             "rules/message_interpolation/pattern-inside.yaml",
             "message_interpolation/pattern_inside_basic.py",
         ),
-        # This is an example where we do not correctly report $CLASS.
         (
             "rules/message_interpolation/pattern-inside.yaml",
             "message_interpolation/pattern_inside_complex.py",
         ),
+        # pattern-not-inside is not currently interpolated. These tests make sure
+        # something doesn't break accidentally.
         (
             "rules/message_interpolation/pattern-not-inside.yaml",
             "message_interpolation/pattern_not_inside_basic.py",
         ),
-        # This case below is an example of our simple message replacement not inferring what the user is after successfully.
-        # I, as the author, would actually want to display a message of all classes. However, this is an extreme corner case that
-        # we don't need to cover right now.
         (
             "rules/message_interpolation/pattern-not-inside.yaml",
             "message_interpolation/pattern_not_inside_complex.py",
@@ -27,6 +25,14 @@ import pytest
         (
             "rules/message_interpolation/pattern-either.yaml",
             "message_interpolation/pattern_either_basic.py",
+        ),
+        (
+            "rules/message_interpolation/multi-pattern-inside.yaml",
+            "message_interpolation/multi_pattern_inside.py",
+        ),
+        (
+            "rules/message_interpolation/multi-pattern-inside.yaml",
+            "message_interpolation/multi_pattern_inside_nested.py",
         ),
     ],
 )
