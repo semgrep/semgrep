31a5a8f392dfd58097dafb65b7d6bb84ade7dda7
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2021-01-09 15:51:29 +0100

    Parse conditional access in C# (#2358)
    * Test file for conditional access
    
    * Parse conditional access in C#
    
    E.g. `a?.b` and `a?[0]`.
    
    AST_generic has no `?.` or `?[...]` equivalents, so we map this to a
    Conditional: `null == a ? null : a.b`. The downside of this is that it seems
    that `a` is evaluated twice.o
    
    Related to #1392
    
    * Reindent Parse_csharp_tree_sitter.ml

---
+++ semgrep-core/tests/csharp/parsing/condaccess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class HelloWorldCondAccess
+{
+    private static string MaybeNull(int arg)
+    {
+        if (arg > 0)
+        {
+            return "Hello";
+        }
+
+        return null;
+    }
+    public static void Main()
+    {
+        Console.WriteLine(MaybeNull(0)?.Length);
+        Console.WriteLine(MaybeNull(1)?.Length);
+        var greeting = MaybeNull(1);
+        Console.WriteLine(greeting?.Length);
+        Console.WriteLine(greeting?[1]);
+    }
+}
