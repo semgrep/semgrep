8133971dcdcaa96038f1b8dbe1cfe2e09f371b3a
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2021-01-16 16:30:57 +0100

    Parse second `from` clause in query

--- semgrep-core/tests/csharp/parsing/linq.cs
+++ semgrep-core/tests/csharp/parsing/linq.cs
@@ -6,6 +6,11 @@ class HelloWorldLinq
 {
     public static void Main()
     {
+        FromLetSelect();
+        DoubleFrom();
+    }
+
+    private static void FromLetSelect() {
         var textInfo = new CultureInfo("en-US", false).TextInfo;
 
         var words = new string[] {"hello", "world"};
@@ -17,6 +22,20 @@ class HelloWorldLinq
         foreach (string w in greeting)
         {
             Console.WriteLine(w);
+        }
+    }
+
+    public static void DoubleFrom()
+    {
+        var words1 = new string[] {"hello", "world"};
+
+        var wordList = from w1 in words1
+            from w2 in w1
+            select w1 + w2;
+
+        foreach (var word in wordList)
+        {
+            Console.WriteLine(word);
         }
     }
 }
