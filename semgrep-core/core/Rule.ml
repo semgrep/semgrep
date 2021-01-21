846ce238e673313de20c3354889c1d516f6bf35f
pad <yoann.padioleau@gmail.com>
2021-01-21 12:25:28 +0100

    syncwebise Rule.ml, Parse_rule.ml, Semgrep.ml

--- semgrep-core/core/Rule.ml
+++ semgrep-core/core/Rule.ml
@@ -1,16 +1,20 @@
+(*s: semgrep/core/Rule.ml *)
+(*s: pad/r2c copyright *)
 (* Yoann Padioleau
  *
  * Copyright (C) 2019-2021 r2c
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License (GPL)
- * version 2 as published by the Free Software Foundation.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation, with the
+ * special exception on linking described in file license.txt.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * file license.txt for more details.
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
+ * license.txt for more details.
 *)
+(*e: pad/r2c copyright *)
 module MV = Metavariable
 
 (*****************************************************************************)
@@ -173,3 +177,5 @@ and rules = rule list
 (* alias *)
 type t = rule
 [@@deriving show]
+
+(*e: semgrep/core/Rule.ml *)
--- semgrep-core/parsing/Parse_rule.ml
+++ semgrep-core/parsing/Parse_rule.ml
@@ -1,16 +1,20 @@
+(*s: semgrep/parsing/Parse_rule.ml *)
+(*s: pad/r2c copyright *)
 (* Yoann Padioleau
  *
  * Copyright (C) 2019-2021 r2c
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License (GPL)
- * version 2 as published by the Free Software Foundation.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation, with the
+ * special exception on linking described in file license.txt.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * file license.txt for more details.
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
+ * license.txt for more details.
 *)
+(*e: pad/r2c copyright *)
 open Common
 
 module J = JSON
@@ -323,3 +327,5 @@ let parse file =
       )
   | Result.Error (`Msg s) ->
       raise (E.UnparsableYamlException s)
+
+(*e: semgrep/parsing/Parse_rule.ml *)
