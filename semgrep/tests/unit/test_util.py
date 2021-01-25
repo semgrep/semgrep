713410441d2ec0ce84d0f12f321319d885892da2
mschwager <mschwager@users.noreply.github.com>
2021-01-25 09:11:57 -0700

    Only warn on unrunnable pattern-where-python, unless --strict (#2450)
    * Only warn on unrunnable pattern-where-python, unless --strict
    
    Fixes #2427.
    
    * Exit early, if possible
    
    * Use better util function name

---
+++ semgrep/tests/unit/test_util.py
@@ -0,0 +1,27 @@
+import pytest
+
+from semgrep import util
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ({}, False),
+        ({"key": "value"}, True),
+        ({"key1": "value"}, False),
+        ({"key1": {"key": "value"}}, True),
+        ({"key1": {"key2": "value"}}, False),
+        ({"key1": [{"key": "value"}]}, True),
+        ({"key1": [{"key2": "value"}]}, False),
+        ({"key1": [{"key2": "value"}, {"key": "value"}]}, True),
+        ({"key1": [{"key2": "value"}, {"key3": "value"}]}, False),
+        ({"key1": [{"key2": "value"}, {"key3": {"key": "value"}}]}, True),
+        ({"key1": [{"key2": "value"}, {"key3": {"key4": "value"}}]}, False),
+        ({"zzz": {"aaa": "value", "key": "value"}}, True),
+        ({"zzz": {"aaa": "value", "key1": "value"}}, False),
+        ({"bbb": {"aaa": "value"}, "zzz": {"key": "value"}}, True),
+        ({"bbb": {"aaa": "value"}, "zzz": {"key1": "value"}}, False),
+    ],
+)
+def test_recursive_has_item(test_input, expected):
+    assert util.recursive_has_item(lambda d: "key" in d, test_input) == expected
