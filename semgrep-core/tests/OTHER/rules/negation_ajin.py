3d1e625d6a4b8e604a6ca69b91a62decfde29860
pad <yoann.padioleau@gmail.com>
2021-01-28 13:28:41 +0100

    * semgrep-core/tests/OTHER/rules/negation_ajin.jsonnet: New test file
    related to https://github.com/returntocorp/semgrep/issues/2457
    
    test plan:
    ```
    $ /home/pad/semgrep/_build/default/CLI/Main.exe -lang python -config negation_ajin.jsonnet negation_ajin.py
    negation_ajin.py:3
     x = os.environ
    
    $ /home/pad/semgrep/_build/default/CLI/Main.exe -test_rules .

---
+++ semgrep-core/tests/OTHER/rules/negation_ajin.py
@@ -0,0 +1,8 @@
+import os
+#ERROR: 
+x = os.environ
+
+verify = (os.environ.get('REQUESTS_CA_BUNDLE') or os.environ.get('CURL_CA_BUNDLE'))
+os.environ[env_name] = 1
+k = None
+get_proxy = os.environ.get(k) or os.environ.get(k.upper())
