782ca1416de22459c73248f28355ea31705c0ae1
Emma Jin <emjin@users.noreply.github.com>
2021-02-03 08:35:24 -0500

    Use bloom filters to reduce tree traversal (#2464)
    * Use bloom filters to reduce tree traversal
    
    Closes #2352
    
    When deciding whether or not to match a subtree, we can definitely choose not to if essential parts of the pattern are not present. It is cheap and space efficient to first hash these essential parts using a bloom filter, which will guarantee a "no" answer if it is not present. Then, when matching the pattern against a node, we only traverse further if all essential parts of the pattern are present in that node.
    
    For the current implementation:
    - the essential parts are non-metavariable idents and string literals
    - a bloom filter is stored at every statement node
    
    The initial bloom annotation is worst case O(sn), where s is the number of strings and n is the number of statement nodes. This is because each string for a given statement is added to that statement's bloom filter and each of its parents'.
    
    Later computations will be O(s_p), where s_p is the number of strings in the pattern.
    
    Test plan: make test (correctness should still hold), benchmarking to see if it is faster. Will also need to tweak the parameters passed into Bloomf.create
    
    This implementation makes use of the bloomf library in opam
    
    * Create a list of strings in the pattern rather than a bloom filter for them
    
    * Renamed in semgrep_generic and fixed infinite loop
    
    Changed bf to pattern_strs where it originally represented the bloom filter
    
    Had infinite loop because the visitor visits the top level statement. Hacky fix--just always skip the first statement reached
    
    * Ignore string literal semgrep constructs (..., regexp)
    
    * Traverse id_info in visitor
    
    Added a hook for id_info. To maintain the same default behavior, v_id_info enters the refs, but the default hook kid_info does not. Bloom_annotation overrides that by repalacing the hook with one that traverses
    
    * Used constant propagation information and added ident special cases
    
    Add hook to constness to match literals. For idents, it was necessary to exclude strings starting with "$..." and regexps. This is currently done poorly.
    
    * Removed print statements
    
    * Resolvd conflict
    
    * Also filter pattern in generic_vs_generic
    
    Constant propagation test case in java has more cases so that I could measure speedup
    
    * Reverted Generic_vs_generic changes
    
    Use of bloom filter check in generic_vs_generic did not make a difference
    
    * Returned must_analyze_statement_bloom... func to semgrep_generic
    
    * Removed print statements
    
    * Addressed comments
    
    * Use Str.first_chars str 4 instead of String.sub
    
    TODO: use a regex
    
    * Profile bloom annotating
    
    * Only check for metavariables/ellipses in the pattern
    
    * Added -bloom_filter to turn on the bloom filter, default off
    
    * Added a comment, removed a print statement

--- semgrep-core/Analyzing/Bloom_annotation.ml
+++ semgrep-core/Analyzing/Bloom_annotation.ml
@@ -13,6 +13,9 @@
  * license.txt for more details.
 *)
 module B = Bloom_filter
+module V = Visitor_AST
+open AST_generic
+open Common
 
 (*****************************************************************************)
 (* Prelude *)
@@ -41,8 +44,38 @@ module B = Bloom_filter
 *)
 
 (*****************************************************************************)
-(* Helpers *)
+(* List helpers *)
 (*****************************************************************************)
+
+(* Note: If the bottom most node has n strings and k parents, it will take
+ * O(kn) to add it to all the lists. Since it will also take O(kn) to add it
+ * to all the bloom filters, this is acceptable, but it would not be
+ * necessary if we used a linked list
+*)
+
+let push v (l : 'a list ref) = Common.push v l
+
+let push_list vs (l : 'a list ref) =
+  l := vs @ (!l)
+
+let add_all_to_bloom ids bf =
+  List.iter (fun id -> B.add id bf) ids
+
+(*****************************************************************************)
+(* Traversal methods *)
+(*****************************************************************************)
+
+(* TODO probably would rather not replicate code *)
+let regexp_regexp_string = "^=~/\\(.*\\)/\\([mi]?\\)$"
+let is_regexp_string s =
+  s =~ regexp_regexp_string
+
+let special_literal str =
+  str = "..." || (* Matching_generic. *)is_regexp_string str
+
+(* TODO: the second bit is a hack because my regexp skills are not great *)
+let special_ident str =
+  AST_generic_.is_metavar_name str || (String.length str > 4 && (Str.first_chars str 4) = "$...") || is_regexp_string str
 
 (* Use a visitor_AST to extract the strings from all identifiers,
  * and from all literals for now, except all semgrep stuff:
@@ -53,23 +86,95 @@ module B = Bloom_filter
  * See also Rules_filter.reserved_id and reserved_str and
  * Rules_filter.extract_specific_strings.
 *)
-let extract_strings _any =
-  []
+
+let rec statement_strings stmt =
+  let res = ref [] in
+  let top_level = ref true in
+  let visitor = V.mk_visitor {
+    V.default_visitor with
+    V.kident = (fun (_k, _) (str, _tok) ->
+      push str res
+    );
+    V.kexpr = (fun (k, _) x ->
+      (match x with
+       (* less: we could extract strings for the other literals too?
+        * atoms, chars, even int?
+       *)
+       | L (String (str, _tok)) ->
+           push str res
+       | TypedMetavar _ -> ()
+       | _ -> k x
+      )
+    );
+    V.kconstness = (fun (k, _) x ->
+      (match x with
+       | Lit (String (str, _tok)) ->
+           if not (special_literal str) then push str res
+       | _ -> k x
+      )
+    );
+    (* The default behavior of kid_info is to not call the continutation *)
+    (* We want to recurse so that we can index processed information like constants *)
+    V.kid_info = (fun (k, _) x -> k x);
+    V.kstmt = (fun (k, _) x ->
+      (* First statement visited is the current statement *)
+      if !top_level then begin top_level := false; k x end
+      else
+        (* For any other statement, recurse to add the filter *)
+        begin
+          let strs = statement_strings x in
+          let bf = B.create () in
+          add_all_to_bloom strs bf;
+          push_list strs res;
+          x.s_bf <- Some (bf)
+        end
+    );
+  } in
+  visitor (S stmt);
+  !res
 
 (*****************************************************************************)
 (* Analyze the pattern *)
 (*****************************************************************************)
 
-let bloom_of_expr _e =
-  B.create ()
-
-let bloom_of_stmt _st =
-  B.create ()
+let list_of_pattern_strings any =
+  let res = ref [] in
+  let visitor = V.mk_visitor {
+    V.default_visitor with
+    V.kident = (fun (_k, _) (str, _tok) ->
+      if not (special_ident str) then
+        push str res
+    );
+    V.kexpr = (fun (k, _) x ->
+      (match x with
+       (* less: we could extract strings for the other literals too?
+        * atoms, chars, even int?
+       *)
+       | L (String (str, _tok)) ->
+           if not (special_literal str) then
+             push str res
+       | TypedMetavar _ -> ()
+       | _ -> k x
+      )
+    );
+  } in
+  visitor any;
+  !res
 
 (*****************************************************************************)
 (* Analyze the code *)
 (*****************************************************************************)
 
 (* TODO: visit AST and set the s_bf field in statements *)
-let annotate_program _ast =
-  ()
+let annotate_program ast =
+  let visitor = V.mk_visitor {
+    V.default_visitor with
+    V.kstmt = (fun (_k, _) x ->
+      let bf = B.create () in
+      let ids = statement_strings x in
+      add_all_to_bloom ids bf;
+      x.s_bf <- Some (bf);
+    );
+  } in
+  visitor (Ss ast);
+[@@profiling]
--- semgrep-core/tests/java/equivalence_constant_propagation.java
+++ semgrep-core/tests/java/equivalence_constant_propagation.java
@@ -4,4 +4,20 @@ class Foo {
         //ERROR: match
 		foo(pwd);
 	}
+
+    void diff_word() {
+        foo("hello");
+    }
+    void diff_func() {
+        bar("password");
+    }
+
+    void same_const() {
+        //ERROR: match
+        foo("password");
+    }
+
+    void all_diff() {
+        bar("hello");
+    }
 }
