b8e2c9872c7edae26a503b544df5bd1dd7390dea
Yoann Padioleau <pad@returntocorp.com>
2021-02-11 11:09:04 +0100

    Cleanup for martin (#2546)
    * Cleanup for martin
    
    test plan:
    make
    make test
    
    * * .gitignore: more
    
    * * semgrep-core/tests/java/import_metavar_fullpath.sgrep: copy paste of metavar_name.sgrep, but to be consistent with then name used in tests/python/

--- spacegrep/src/lib/Parse_pattern.mli
+++ spacegrep/src/lib/Parse_pattern.mli
@@ -1,3 +1,8 @@
+(*
+   Turn tokenized lines into a tree, based on:
+   - indentation,
+   - matching braces within the same line.
+*)
 
 val of_lexbuf: ?is_doc:bool -> Lexing.lexbuf -> Pattern_AST.t
 
