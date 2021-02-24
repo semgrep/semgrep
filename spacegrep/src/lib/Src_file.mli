b6d9098f8137f54945cd8656fa200d1248059a92
Martin Jambon <martin@mjambon.com>
2021-02-24 03:16:23 -0800

    Add unit tests for spacegrep's output, change 'lines' field (#2607)
    * Fix dangling symlink issue in spacegrep/
    
    * Eliminate spurious trailing '""' in json 'lines' returned by spacegrep
    for semgrep-core.
    
    * Adding nosem annotation because I know what I'm doing.
    
    Co-authored-by: pad <yoann.padioleau@gmail.com>

--- spacegrep/src/lib/Src_file.mli
+++ spacegrep/src/lib/Src_file.mli
@@ -47,8 +47,13 @@ val region_of_loc_range : t -> Loc.t -> Loc.t -> string
 
    This includes the beginning of the first line and the end of the last line
    even if they're outside the requested range.
+
+   A trailing newline character (LF) is added unless 'force_trailing_newline'
+   is false. Regardless, a trailing newline is always included if there is
+   one in the source file.
 *)
 val lines_of_pos_range :
+  ?force_trailing_newline:bool ->
   ?highlight:(string -> string) ->
   ?line_prefix:string ->
   t -> Lexing.position -> Lexing.position -> string
@@ -57,11 +62,26 @@ val lines_of_pos_range :
    Extract the lines containing a pair of locations.
    A location itself is a range of positions, typically associated with
    an input token.
+
+   See 'lines_of_pos_range' for details.
 *)
 val lines_of_loc_range :
+  ?force_trailing_newline:bool ->
   ?highlight:(string -> string) ->
   ?line_prefix:string ->
   t -> Loc.t -> Loc.t -> string
+
+(* Same as 'lines_of_pos_range' but split the text into a list of lines. *)
+val list_lines_of_pos_range :
+  ?highlight:(string -> string) ->
+  ?line_prefix:string ->
+  t -> Lexing.position -> Lexing.position -> string list
+
+(* Same as 'lines_of_loc_range' but split the text into a list of lines. *)
+val list_lines_of_loc_range :
+  ?highlight:(string -> string) ->
+  ?line_prefix:string ->
+  t -> Loc.t -> Loc.t -> string list
 
 (* not for public use, only exposed to allow unit tests *)
 val insert_highlight : (string -> string) -> string -> int -> int -> string
