35552081178afc5272a5d8a0c6cec6addc03c253
brendon <brendon.go@gmail.com>
2021-02-24 13:03:51 -0800

    Release 0.41.1
    Force static linking of pcre in semgrep-core in MacOS builds

--- scripts/osx-release.sh
+++ scripts/osx-release.sh
@@ -8,10 +8,15 @@ git submodule update --init --recursive --depth 1
 
 eval "$(opam env)"
 
+# Remove pcre dynamically linked to force MacOS to use static
+# This needs to be done before make setup since it is used there
+rm /usr/local/opt/pcre/lib/libpcre.1.dylib
+
 make setup
 make config
 
 # Remove dynamically linked libraries to force MacOS to use static ones
+# This needs to be done after make setup but before make build-*
 rm /usr/local/lib/libtree-sitter.0.0.dylib
 rm /usr/local/lib/libtree-sitter.dylib
 
