c7b00c3442ac7d8bed7d1c606b701e0b1c1966e0
brendon <brendon.go@gmail.com>
2021-02-24 11:21:21 -0800

    Test deleting dylib before setup

--- scripts/osx-release.sh
+++ scripts/osx-release.sh
@@ -8,13 +8,14 @@ git submodule update --init --recursive --depth 1
 
 eval "$(opam env)"
 
+rm /usr/local/opt/pcre/lib/libpcre.1.dylib
+
 make setup
 make config
 
 # Remove dynamically linked libraries to force MacOS to use static ones
 rm /usr/local/lib/libtree-sitter.0.0.dylib
 rm /usr/local/lib/libtree-sitter.dylib
-rm /usr/local/opt/pcre/lib/libpcre.1.dylib
 
 make build-spacegrep
 make build-core
