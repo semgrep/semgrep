8f0aedd916e306d685b90242cc1c9a810029daef
Yoann Padioleau <pad@returntocorp.com>
2021-02-25 12:36:10 +0100

    [Go] Fix difference between pfff and tree-sitter on Function literal (lambda) (#2639)
    * [Go] Use latest pfff which fixes FuncLit to take a Block
    
    This closes https://github.com/returntocorp/semgrep/issues/2636
    
    test plan:
    make test with new tests file included
    
    Also the Go AST for FuncLit now has the Block (see last line below)
    ```
     /home/pad/semgrep/_build/default/pfff/cli/Main.exe -dump_go tests/go/misc_diff_pfff_treesitter.go
    [(Package ((), ("main", ())));
      (DFunc ((), ("test", ()),
         ({ fparams = []; fresults = [] },
          (Block
             ((),
              [(SimpleStmt
                  (DShortVars ([(Id ("makeApiHandlerContext", ()))], (),
                     [(FuncLit
                         ({ fparams = [];
                            fresults =
                            [(ParamClassic
                                { pname = None;
                                  ptype = (TFunc { fparams = []; fresults = [] });
                                  pdots = None })
                              ]
                            },
                          (Block
                             ((),
    ```
    
    and the diff with tree-sitter is now empty (well there are differences
    in sid, but I need to fix -diff_pfff_tree_sitter for that)
    ```
    + /home/pad/semgrep/_build/default/CLI/Main.exe -diff_pfff_tree_sitter tests/go/misc_diff_pfff_treesitter.go
    --- /tmp/tmp-75229-9ab682.x	2021-02-25 11:51:44.613545900 +0100
    +++ /tmp/tmp-75229-0efa07.x	2021-02-25 11:51:44.613545900 +0100
    @@ -1,4 +1,4 @@
    -[{ s = (DirectiveStmt (Package ((), [("main", ())]))); s_id = 2;
    +[{ s = (DirectiveStmt (Package ((), [("main", ())]))); s_id = 13;
        s_use_cache = false; s_backrefs = None; s_bf = None };
       { s =
         (DefStmt
    @@ -70,7 +70,7 @@
    ```
    
    * * semgrep-core/Parsing/Test_parsing.ml: misc

---
+++ semgrep-core/tests/go/misc_diff_pfff_treesitter.go
@@ -0,0 +1,14 @@
+package main
+
+func test() {
+    //ERROR: match
+    makeApiHandlerContext := func() func() {
+        return func() {
+            var1 = x
+            var2 = y
+            var3 = z
+
+            r = session.UpdateReporterRequest(r)
+        }
+    }
+}
