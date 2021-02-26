4b9eacb1af6fcfe0d930a00d6aeeff218b58d5f0
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2021-02-26 22:15:25 +0100

    Add tests for C# semgrep (#2655)
    Add several alpha-level tests:
    * concrete_syntax
    * dots_args
    * metavar_arg

--- semgrep-core/tests/csharp/concrete_syntax.cs
+++ semgrep-core/tests/csharp/concrete_syntax.cs
@@ -1,16 +1,24 @@
-class Foo {
-    void foo() {
-        //ERROR:
-        foo(1,2);
+public class ConcreteSyntax
+{
+    public static void Main()
+    {
+        // ERROR:
+        Foo(1, 2);
 
-        //ERROR:
-        foo(1,
+        // ERROR:
+        Foo(1,
             2);
 
-        //ERROR:
-        foo (1, // comment
-             2);
+        // ERROR:
+        Foo(1, // comment
+            2);
 
-        foo(2,1);
+        Foo(2, 1);
+
+        Foo(1, 2, 3);
+    }
+
+    private static void Foo(int a, int b, int c = 3)
+    {
     }
 }
