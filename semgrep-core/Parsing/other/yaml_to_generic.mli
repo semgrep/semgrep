e244e87bfb5eba918250a89dbb9c056a6497e26f
Emma Jin <emjin@users.noreply.github.com>
2021-03-01 11:33:45 -0500

    Ej 2601 yaml ellipses (#2660)
    * Complete basic support for semgrep ellipses and metavariables
    
    Adjust the line number of tokens, convert ellipses, add tests
    
    Test plan: makes test
    
    * Support yaml in semgrep
    
    * Addressed comments from #2651
    
    * Update e2e snapshots to include yaml as language

--- semgrep-core/Parsing/other/yaml_to_generic.mli
+++ semgrep-core/Parsing/other/yaml_to_generic.mli
@@ -1,7 +1,2 @@
-type env = {
-  str: string;
-  file: Common.filename
-}
-
-val program: env -> AST_generic.program
+val program: Common.filename -> AST_generic.program
 val any: string -> AST_generic.any
