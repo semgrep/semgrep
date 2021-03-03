3639f7797d1f1bb1f5a42d7e17cec31524bd83ec
Yoann Padioleau <pad@returntocorp.com>
2021-03-03 15:54:16 +0100

    Cleaning up Regexp_engine, providing more engines (Re and Pcre) (#2681)

--- semgrep-core/Core/Regexp_engine.mli
+++ semgrep-core/Core/Regexp_engine.mli
@@ -1,6 +1,31 @@
 
 module Str_engine : sig
-  type t = Str.regexp
-  val matching_string: string -> t
+  type t = string * Str.regexp
+  val show: t -> string
+
+  val regexp: string -> t
+  val matching_exact_string: string -> t
+
+  val run: t -> string -> bool
+end
+
+module Pcre_engine : sig
+  type t = string * Pcre.regexp
+  val show: t -> string
+  val equal: t -> t -> bool
+  val pp: Format.formatter -> t -> unit
+
+  val matching_exact_string: string -> t
+
+  val run: t -> string -> bool
+
+end
+
+module Re_engine: sig
+  type t = string * Re.t
+  val show: t -> string
+
+  val matching_exact_string: string -> t
+
   val run: t -> string -> bool
 end
