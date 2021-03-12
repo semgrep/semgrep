f1bc99be7087fb06a9c59cb5cba23f06a94b47df
Yoann Padioleau <pad@returntocorp.com>
2021-03-12 09:13:49 +0100

    Add dogfood benchmark (#2742)
    test plan:
    uncomment the Corpus line in run-bencmarks, and
    ./run-benchmarks

--- semgrep/tests/e2e/test_generate_config.py
+++ semgrep/tests/e2e/test_generate_config.py
@@ -9,5 +9,7 @@ def test_generate_config(run_semgrep_in_tmp):
 
 def test_generate_config_with_filename(run_semgrep_in_tmp):
     filename = "test.yml"
-    subprocess.check_output([sys.executable, "-m", "semgrep", "--generate-config", filename])
+    subprocess.check_output(
+        [sys.executable, "-m", "semgrep", "--generate-config", filename]
+    )
     run_semgrep_in_tmp(filename)
