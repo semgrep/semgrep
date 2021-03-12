0d8e9917f029483d799f93214cc9b0298ff7c012
Emma Jin <emjin@users.noreply.github.com>
2021-03-12 11:33:03 -0500

    Fix token location info and ellipsis bugs
    Correctly propagate end position through lists. Change end tokens in brackets to be inclusive rather than exclusive.
    
    Allow for ellipses conversion when the structure is like
    
    - id: $X
      ...
    ...
    
    Test plan: make test

--- semgrep-core/Parsing/other/yaml_to_generic.ml
+++ semgrep-core/Parsing/other/yaml_to_generic.ml
@@ -41,6 +41,8 @@ exception ImpossibleDots
 type env = {
   (* when we parse a pattern, the filename is fake ("<pattern_file>") *)
   file: Common.filename;
+  (* function *)
+  charpos_to_pos: (int -> int * int) option
 }
 
 (*****************************************************************************)
@@ -67,6 +69,11 @@ let get_res = function
   | Result.Error (`Msg s) -> raise (ParseError s)
   | Result.Ok v -> v
 
+let _pos_str {E.start_mark = {M.index = s_index; M.line = s_line; M.column = s_column};
+              E.end_mark = {M.index = e_index; M.line = e_line; M.column = e_column} } =
+  Printf.sprintf "index: %d line: %d col %d -> index: %d line: %d col %d"
+    s_index (s_line + 1) s_column e_index (e_line + 1) e_column
+
 let tok (index, line, column) str env =
   {Parse_info.token = Parse_info.OriginTok {str; charpos = index; line = line + 1; column; file = env.file};
    Parse_info.transfo = NoTransfo}
@@ -77,6 +84,13 @@ let mk_tok {E.start_mark = {M.index; M.line; M.column}; _} str env =
 
 let mk_bracket ({E.start_mark = {M.index = s_index; M.line = s_line; M.column = s_column}; _},
                 {E.end_mark = {M.index = e_index; M.line = e_line; M.column = e_column}; _}) v env =
+  let e_line, e_column =
+    match env.charpos_to_pos with
+    | None -> e_line, e_column
+    | Some f ->
+        let e_line, e_column = f (e_index - 1) in
+        e_line - 1, e_column
+  in
   tok (s_index, s_line, s_column) "(" env, v, tok (e_index, e_line, e_column) ")" env
 
 let mk_err err v ({E.start_mark = {M.line; M.column; _}; _} as pos) env =
@@ -115,18 +129,18 @@ let make_scalar _anchor _tag pos value env : A.expr =
 
 (* Sequences are arrays in the generic AST *)
 let make_sequence _anchor _tag start_pos (es, end_pos) env =
-  A.Container(A.Array, mk_bracket (start_pos, end_pos) es env)
+  A.Container(A.Array, mk_bracket (start_pos, end_pos) es env), end_pos
 
 (* Mappings are dictionaries in the generic AST *)
 let make_mappings _anchor _tag start_pos (es, end_pos) env =
   match es with
-  | [A.Ellipsis e] -> A.Ellipsis e
-  | _ -> A.Container(A.Dict, mk_bracket (start_pos, end_pos) es env)
+  | [A.Ellipsis e] -> A.Ellipsis e, end_pos
+  | _ -> A.Container(A.Dict, mk_bracket (start_pos, end_pos) es env), end_pos
 
 let make_mapping (pos1, pos2) ((key, value) : A.expr * A.expr) env =
   match key, value with
-  | A.Ellipsis _, A.Ellipsis _ -> A.Ellipsis (Parse_info.fake_info "...")
-  | _ -> A.Tuple (mk_bracket (pos1, pos2) [key; value] env)
+  | A.Ellipsis _, A.Ellipsis _ -> A.Ellipsis (Parse_info.fake_info "..."), pos2
+  | _ -> A.Tuple (mk_bracket (pos1, pos2) [key; value] env), pos2
 
 let make_doc start_pos (doc, end_pos) env =
   match doc with
@@ -141,7 +155,9 @@ let parse env parser : A.expr =
     | v, pos -> mk_err "Expected start of string, got" v pos env
   and read_documents acc : A.expr list * E.pos =
     match get_res (S.do_parse parser) with
-    | E.Document_start _, pos -> let docs = read_document () in (fst docs) :: (fst (read_documents acc)), pos
+    | E.Document_start _, _pos -> let docs, _ = read_document () in
+        let rest, end_pos = read_documents acc in
+        docs :: rest, end_pos
     | E.Stream_end, pos -> acc, pos
     | v, pos -> mk_err "Expected start of document or end of stream, got" v pos env
   and read_document () : A.expr * E.pos =
@@ -154,17 +170,21 @@ let parse env parser : A.expr =
     match res with
     | E.Alias { anchor }, pos -> A.N (make_alias anchor pos env), pos
     | E.Scalar { anchor; tag; value; _ }, pos -> make_scalar anchor tag pos value env, pos
-    | E.Sequence_start { anchor; tag; _ }, pos -> make_sequence anchor tag pos (read_sequence []) env, pos
-    | E.Mapping_start { anchor; tag; _ }, pos -> make_mappings anchor tag pos (read_mappings []) env, pos
+    | E.Sequence_start { anchor; tag; _ }, pos -> make_sequence anchor tag pos (read_sequence []) env
+    | E.Mapping_start { anchor; tag; _ }, pos -> make_mappings anchor tag pos (read_mappings []) env
     | v, pos -> mk_err "Expected a valid YAML element or end of sequence, got" v pos env
   and read_sequence acc : A.expr list * E.pos =
     match get_res (S.do_parse parser) with
     | E.Sequence_end, pos -> acc, pos
-    | v, pos -> let seq = (fst (read_node ~node_val:(Some(v, pos)) ())) in seq :: (fst (read_sequence acc)), pos
+    | v, pos -> let seq = (fst (read_node ~node_val:(Some(v, pos)) ())) in
+        let rest, end_pos = read_sequence acc in
+        seq :: rest, end_pos
   and read_mappings acc : A.expr list * E.pos =
     match get_res (S.do_parse parser) with
     | E.Mapping_end, pos -> acc, pos
-    | v, pos -> let map = read_mapping (v, pos) in map :: (fst (read_mappings acc)), pos
+    | v, pos -> let map = read_mapping (v, pos) in
+        let rest, end_pos = read_mappings acc in
+        map :: rest, end_pos
   and read_mapping first_node : A.expr =
     let key, pos1 =
       match first_node with
@@ -172,12 +192,15 @@ let parse env parser : A.expr =
       | v, pos -> mk_err "Expected a valid scalar, got" v pos env
     in
     let value, pos2 = read_node () in
-    make_mapping (pos1, pos2) (key, value) env
+    let value, _ = make_mapping (pos1, pos2) (key, value) env in
+    value
   in
   read_stream ()
 
 let make_pattern_expr e =
   match e with
+  (* If the user creates a pattern with a single field, assume they just want *
+   * to match the field, not the whole enclosing container                    *)
   | A.Container(A.Dict, (_lp, [x], _rp)) -> A.E x
   | _ -> A.E e
 
@@ -299,12 +322,13 @@ let preprocess_yaml str =
 (*****************************************************************************)
 
 let program file =
-  let str = preprocess_yaml (Common.read_file file) in
-  let env = { file } in
+  let str = Common.read_file file in
+  let charpos_to_pos = Some (Parse_info.full_charpos_to_pos_large file) in
+  let env = { file; charpos_to_pos } in
   [A.exprstmt (get_res (S.parser str) |> parse env)]
 
 let any str =
-  let env = { file = "<pattern_file>" } in
+  let env = { file = "<pattern_file>"; charpos_to_pos = None } in
   let str = preprocess_yaml str in
   (* Common.pr2 str; *)
   get_res (S.parser str) |> parse env |> make_pattern_expr
