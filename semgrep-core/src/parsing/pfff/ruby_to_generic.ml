c4bc8104e48e9762a413b247b057e1a27a294df6
Emma Jin <emjin@users.noreply.github.com>
2021-04-19 14:03:05 -0400

    Read ruby interpolated strings as ConcatString
    Closes https://github.com/returntocorp/semgrep/issues/2826 and https://github.com/returntocorp/semgrep/issues/2949
    
    Use ConcatString(InterpolatedConcat, ...) for string_contents_list to make use of the interpolated strings matching
    
    Test plan: make test

--- semgrep-core/src/parsing/pfff/ruby_to_generic.ml
+++ semgrep-core/src/parsing/pfff/ruby_to_generic.ml
@@ -237,8 +237,9 @@ and method_name mn =
 
 and string_contents_list (t1, xs, t2) =
   let xs = list string_contents xs in
-  G.OtherExpr (G.OE_Todo,
-               [G.Tk t1] @ (xs |> List.map (fun e -> G.E e)) @ [G.Tk t2])
+
+  G.Call (G.IdSpecial(G.ConcatString(G.InterpolatedConcat), PI.fake_info ""),
+              (t1, (xs |> List.map (fun e -> G.Arg e)), t2))
 
 and string_contents = function
   | StrChars s -> G.L (G.String s)
