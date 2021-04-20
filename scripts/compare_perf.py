54696d3f10fa01991721ce45827438d5f519c1fd
Emma Jin <emjin@users.noreply.github.com>
2021-04-19 20:12:10 -0400

    Improve stability of CI perf (#2953)
    * Improve stability of CI perf
    
    Retire perf-test job (the juice-shop test will never alarm because we are significantly faster, the dvna test can be subsumed into run-benchmarks) for simplicity and to avoid potential conflicting.
    
    Run tests multiple times, take the min of ours and the max of theirs.
    
    Ignore minor differences in small tests.
    
    * Compare max of baseline times against min of latest times
    
    * Add a fixed threshold of lenience
    
    * Move more medium benchmarks to small
    
    * Remove unnecessary lines

--- scripts/compare_perf.py
+++ scripts/compare_perf.py
@@ -10,17 +10,28 @@ def read_timing(filename: Path) -> List[float]:
 
 
 def main() -> None:
-    baseline_timing_file = Path(sys.argv[1])
-    baseline_times = read_timing(baseline_timing_file)
+    # There are two versions of each timing to improve stability
 
-    latest_timing_file = Path(sys.argv[2])
-    latest_times = read_timing(latest_timing_file)
+    baseline_timing_file_1 = Path(sys.argv[1])
+    baseline_timing_file_2 = Path(sys.argv[2])
+    baseline_times = zip(
+        read_timing(baseline_timing_file_1), read_timing(baseline_timing_file_2)
+    )
+    baseline_times = [max(t1, t2) for t1, t2 in baseline_times]
+
+    latest_timing_file_1 = Path(sys.argv[3])
+    latest_timing_file_2 = Path(sys.argv[4])
+    latest_times = zip(
+        read_timing(latest_timing_file_1), read_timing(latest_timing_file_2)
+    )
+    latest_times = [min(t1, t2) for t1, t2 in latest_times]
 
     for baseline_time, latest_time in zip(baseline_times, latest_times):
         print(f"Baseline: {baseline_time}, Latest: {latest_time}")
 
         # Assert latest time is not more than 6% slower than baseline
-        assert latest_time < baseline_time * 1.06
+        # or is within a fixed "probably environmental" range
+        assert latest_time < baseline_time * 1.06 or latest_time - baseline_time < 0.5
 
 
 if __name__ == "__main__":
