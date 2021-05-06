2c1cb25d24a7ad702ab75230bec87980f3e26d49
Matt Schwager <matt@returntocorp.com>
2021-05-06 10:17:14 -0600

    Remove '--debugging-json' and '--json-time' in favor of their equivalents

--- semgrep/tests/e2e/test_debugging_json.py
+++ semgrep/tests/e2e/test_debugging_json.py
@@ -3,7 +3,8 @@ def test_debugging_json(run_semgrep_in_tmp, snapshot):
         run_semgrep_in_tmp(
             "rules/eqeq.yaml",
             target_name="basic/stupid.py",
-            output_format="debugging-json",
+            options=["--debug"],
+            output_format="json",
         ),
         "results.json",
     )
