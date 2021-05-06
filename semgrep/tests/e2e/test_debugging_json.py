5c6b03f932331dba29c81beed455e6009f1a7c75
mschwager <mschwager@users.noreply.github.com>
2021-05-06 12:41:30 -0600

    Use OutputFormat in tests instead of strings (#3051)

--- semgrep/tests/e2e/test_debugging_json.py
+++ semgrep/tests/e2e/test_debugging_json.py
@@ -1,10 +1,13 @@
+from semgrep.constants import OutputFormat
+
+
 def test_debugging_json(run_semgrep_in_tmp, snapshot):
     snapshot.assert_match(
         run_semgrep_in_tmp(
             "rules/eqeq.yaml",
             target_name="basic/stupid.py",
             options=["--debug"],
-            output_format="json",
+            output_format=OutputFormat.JSON,
         ),
         "results.json",
     )
