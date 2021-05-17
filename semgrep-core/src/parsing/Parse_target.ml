8d4e884c9dbc55b6e4839fdea2c47c5d7d94fb26
Yoann Padioleau <pad@returntocorp.com>
2021-05-17 13:16:10 +0200

    use latest pfff (#3117)
    * use latest pfff
    
    * Use latest pfff with better ocaml parsing

--- semgrep-core/src/parsing/Parse_target.ml
+++ semgrep-core/src/parsing/Parse_target.ml
@@ -272,7 +272,7 @@ let just_parse_with_lang lang file =
       run file
         [
           Pfff (throw_tokens Parse_ml.parse);
-          TreeSitter Parse_ocaml_tree_sitter.parse;
+          (* TODO TreeSitter Parse_ocaml_tree_sitter.parse; *)
         ]
         Ml_to_generic.program
   | Lang.PHP ->
