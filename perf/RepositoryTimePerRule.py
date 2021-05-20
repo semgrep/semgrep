38e980b536a235696401e4651db766f2bc147185
Colleen Dai <colleen@returntocorp.com>
2021-05-19 17:27:37 -0700

    stop using dependencies

--- perf/RepositoryTimePerRule.py
+++ perf/RepositoryTimePerRule.py
@@ -1,14 +1,14 @@
 from collections import defaultdict
 from typing import Dict, List
+from operator import add
 
 import copy
 import json
-import numpy as np
 import sys
 
 class RepositoryTimePerRule:
     def __init__(
-        self, 
+        self,
         output_file: str,
         repo_to_times_per_rule: Dict[str, Dict[str, int]] = defaultdict(dict)
     ) -> None:
@@ -21,7 +21,7 @@ class RepositoryTimePerRule:
         except (UnicodeDecodeError, ValueError):
             print ("Unable to decode the Semgrep result as JSON.")
             sys.exit(1)
-        
+
         if not 'time' in times_per_file:
             print ("Semgrep-core ran without the --time flag, please try again with --time set to true.")
             sys.exit(1)
@@ -30,12 +30,12 @@ class RepositoryTimePerRule:
         for rule_id in times_per_file['time']['rules']:
             rule_ids.append(rule_id['id'])
 
-        total_time_per_rule: List[str] = np.zeros(len(rule_ids))
+        total_time_per_rule: List[str] = [0] * len(rule_ids)
         for target in times_per_file['time']['targets']:
-            total_time_per_rule = np.add(target['parse_times'], total_time_per_rule)
+            total_time_per_rule = list(map(add, target['parse_times'], total_time_per_rule))
 
         current_repo_times = dict(zip(rule_ids, total_time_per_rule))
-        self.repo_to_times_per_rule[repo_name] = current_repo_times 
+        self.repo_to_times_per_rule[repo_name] = current_repo_times
 
     def print_repo_to_times_per_rule(self):
         with open(self.output_file, 'w') as f:
