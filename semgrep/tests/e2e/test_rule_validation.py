48a51c4b552dc9c09f2f934c937e7754259b007b
Grayson H <grayson@returntocorp.com>
2021-05-21 10:53:18 -0500

    Report error when mapping value has duplicate keys (#3163)
    * Add unit tests for invalid metavariable-regex and invalid child patterns for testing rule validation
    
    * Remove fluff from invalid rule unit tests
    
    * Add check for duplicate key names in MappingNodes
    
    * Update unit tests for yaml rule  validation, including tests for duplicate keys
    
    * Add e2e tests for rule validation
    
    * Sort duplicate keys in error message to preserve determinism in test output
    
    * Regenerate snapshots with deterministic dupe key message
    
    * CHANGELOG

---
+++ semgrep/tests/e2e/test_rule_validation.py
@@ -0,0 +1,22 @@
+import json
+from subprocess import CalledProcessError
+import pytest
+
+
+@pytest.mark.parametrize(
+    "rule,target",
+    [
+        ("rules/invalid-rules/invalid-metavariable-regex.yaml", "basic/stupid.py"),
+        ("rules/invalid-rules/invalid-pattern-child.yaml", "basic/stupid.py"),
+        ("rules/invalid-rules/invalid-missing-top-item.yaml", "basic/stupid.py"),
+    ],
+)
+def test_validation_of_invalid_rules(run_semgrep_in_tmp, snapshot, rule, target):
+    with pytest.raises(CalledProcessError) as exc_info:
+        run_semgrep_in_tmp(rule, target_name=target)
+
+    semgrep_json_output = json.loads(exc_info.value.stdout)
+
+    snapshot.assert_match(
+        json.dumps(semgrep_json_output, indent=2, sort_keys=True), "results.json"
+    )
