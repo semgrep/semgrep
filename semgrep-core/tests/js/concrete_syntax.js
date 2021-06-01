f0ddccb42264784d98947d85b23ce599833047c4
Yoann Padioleau <pad@returntocorp.com>
2021-06-01 21:56:35 +0200

    Experimental support for Comby (#3248)
    * Experimental support for Comby
    
    This is just the first PR for comby.
    Next steps are to handle metavariables, and also
    to figure out why the line/col and the number of matches
    returned by Comby seems wrong
    
    test plan:
    test file included
    
    * fix basic_comby.js
    
    Comby is very space sensitive :(
    
    * more fix
    
    * use ruleid, not ERROR:

--- semgrep-core/tests/js/concrete_syntax.js
+++ semgrep-core/tests/js/concrete_syntax.js
@@ -1,14 +1,16 @@
 function foo() {
  //ERROR:
+    foo(1, 2);
+ //ERROR:
     foo(1,2);
-
+ //ERROR:
+    foo (1, 2);
  //ERROR:
  foo(1,
      2);
-
  //ERROR:
- foo (1, // comment
-      2);
+ foo(1, // comment
+     2);
 
  foo(2,1)
 }
