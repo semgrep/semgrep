6bd833a84f71b44a910e94a915efbcd7f77a2f16
pad <yoann.padioleau@gmail.com>
2021-06-01 17:49:46 +0200

    fix basic_comby.js
    Comby is very space sensitive :(

--- semgrep-core/tests/OTHER/rules/comby_basic.js
+++ semgrep-core/tests/OTHER/rules/comby_basic.js
@@ -1,14 +1,19 @@
 function foo() {
- //ERROR:
-    foo(1,2);
+    //ERROR:
+    foo(1, 2)
 
- //TODO should detect that:
- foo(1,
-     2);
+    //unfortunately it does not match
+    foo(1,2)
+    //neither this one :(
+    foo (1, 2)
 
- //TODO that one too:
- foo (1, // comment
-      2);
+    //ERROR: this is detected
+    foo(1,
+	2)
 
- foo(2,1)
+    //ERROR: this is detectedtoo
+    foo(1, // comment
+	 2)
+
+    foo(2,1)
 }
