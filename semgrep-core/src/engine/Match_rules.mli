b8c13070a76495be6e4fe63201edf78ecd552686
Brendon Go <brendon.go@gmail.com>
2021-06-03 10:53:40 -0700

    add: support equivalences in whole rule core running (#3259)

--- semgrep-core/src/engine/Match_rules.mli
+++ semgrep-core/src/engine/Match_rules.mli
@@ -7,6 +7,7 @@ val check :
   (string -> Metavariable.bindings -> Parse_info.t list Lazy.t -> unit) ->
   Config_semgrep.t ->
   Rule.rules ->
+  Equivalence.equivalences ->
   Common.filename * Rule.xlang * (Target.t * Error_code.error list) Lazy.t ->
   Report.times Report.match_result
 
