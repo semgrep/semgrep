3ca0f28b5fc5a74c27b6dfb418efebe400e68a30
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2021-06-06 16:56:13 +0200

    Support unsafe block in C#
    E.g.
    ```
    unsafe { *i = *i + 1 }
    ```

---
+++ semgrep-core/tests/csharp/parsing/unsafe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Unsafe
+{
+    private static unsafe void addOne(int* i)
+    {
+        *i = *i + 1;
+    }
+    
+    public static void Main()
+    {
+        unsafe
+        {
+            int i = 3;
+            addOne(&i);
+            Console.WriteLine(i);
+        }
+    }
+}
