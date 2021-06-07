996e2b5cc8dbfe6fc1a4774d65decfeee75856f9
Emma Jin <emjin@users.noreply.github.com>
2021-06-07 12:26:25 -0400

    Improve implementation of taking innermost container (#3291)
    Better solution than https://github.com/returntocorp/semgrep/pull/3267

--- semgrep/semgrep/core_runner.py
+++ semgrep/semgrep/core_runner.py
@@ -787,13 +787,17 @@ class CoreRunner:
         )
 
 
-# Note that this may remove matches that have the same range but different
-# metavariable bindings, and arbitrarily choose the first one in the list.
-# TODO: changes that? Do like in semgrep-core and return all the matches?
-# Also now Semgrep.ml Semgrep_generic.ml internally do some dedup, so we
-# may not need anymore to do it here.
+# This will remove matches that have the same range but different
+# metavariable bindings, choosing the last one in the list. We want the
+# last because if there multiple possible bindings, they will be returned
+# by semgrep-core from largest range to smallest. For an example, see
+# tests/e2e/test_message_interpolation.py::test_message_interpolation;
+# specifically, the multi-pattern-inside test
+#
+# Another option is to not dedup, since Semgrep.ml now does its own deduping
+# otherwise, and surface both matches
 def dedup_output(outputs: List[RuleMatch]) -> List[RuleMatch]:
-    return list({uniq_id(r): r for r in outputs}.values())
+    return list({uniq_id(r): r for r in reversed(outputs)}.values())[::-1]
 
 
 def dedup_errors(errors: List[SemgrepError]) -> List[SemgrepError]:
