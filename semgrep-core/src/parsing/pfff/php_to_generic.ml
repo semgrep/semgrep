semgrep/semgrep

commit 9ec7768caf16e7ca1aa50296e8442af83cad4cf7

[php] parsing: Convert stmt-expr patterns into pure expr patterns (#3288)
Closes #3191

test plan:
make test # test included

semgrep-core/src/parsing/pfff/php_to_generic.ml
@@ -636,6 +636,13 @@ let any = function
   | Program v1 ->
       let v1 = program v1 in
       G.Ss v1
+  (* We prefer an expr-pattern over a stmt-pattern, because expr-patterns can
+   * match anywhere. Also, in the current taint-mode, sources/sanitizers/sinks
+   * must be expr-patterns; so, without this, you could not e.g. use `echo` as
+   * a sink. *)
+  | Stmt (Expr (v1, _t)) ->
+      let v1 = expr v1 in
+      G.E v1
   | Stmt v1 ->
       let v1 = stmt v1 in
       G.S v1

semgrep-core/tests/Test.ml
@@ -371,6 +371,12 @@ let full_rule_regression_tests =
 let lang_tainting_tests =
   let taint_tests_path = Filename.concat tests_path "tainting_rules" in
   "lang tainting rules testing" >::: [
+    "tainting PHP" >::: (
+      let dir = Filename.concat taint_tests_path "php" in
+      let files = Common2.glob (spf "%s/*.php" dir) in
+      let lang = Lang.PHP in
+      tainting_tests_for_lang files lang
+    );
     "tainting Python" >::: (
       let dir = Filename.concat taint_tests_path "python" in
       let files = Common2.glob (spf "%s/*.py" dir) in

semgrep-core/tests/tainting_rules/php/echo.php
@@ -1,0 +1,16 @@
+<?php
+// https://github.com/returntocorp/semgrep/issues/3191
+
+function foo() {
+    //ERROR:
+    echo $_GET['a'];
+
+    //ERROR:
+    echo custom($_GET['b']);
+
+    //OK:
+    echo htmlspecialchars($_GET['c']);
+
+    //OK:
+    echo 'hello';
+}

semgrep-core/tests/tainting_rules/php/echo.yaml
@@ -1,0 +1,12 @@
+rules:
+  - id: test-taint-echo
+    languages:
+      - php
+    message: Working!
+    pattern-sources:
+      - $_GET[...]
+    pattern-sanitizers:
+      - htmlspecialchars(...)
+    pattern-sinks:
+      - echo ...;
+    severity: ERROR

commit 129409c92bc0b7fe6b0b677bbec1ae442fa91754

[php] Only coerce stmt-expr patterns into expr patterns in taint mode
PR #3288 messed up with the column numbers reported for matches in PHP
(where e.g. the ';' was no longer included in the match). We thus revert
PR #3288 (partially) and only do this transformation where we need it,
which is in taint-mode.

Fixes: 9ec7768caf1 ("[php] parsing: Convert stmt-expr patterns into pure expr patterns (#3288)")

semgrep-core/src/parsing/pfff/php_to_generic.ml
@@ -636,13 +636,6 @@ let any = function
   | Program v1 ->
       let v1 = program v1 in
       G.Ss v1
-  (* We prefer an expr-pattern over a stmt-pattern, because expr-patterns can
-   * match anywhere. Also, in the current taint-mode, sources/sanitizers/sinks
-   * must be expr-patterns; so, without this, you could not e.g. use `echo` as
-   * a sink. *)
-  | Stmt (Expr (v1, _t)) ->
-      let v1 = expr v1 in
-      G.E v1
   | Stmt v1 ->
       let v1 = stmt v1 in
       G.S v1

semgrep-core/src/tainting/Tainting_generic.ml
@@ -56,6 +56,12 @@ let match_pat_eorig pat =
         xs
         |> List.map (function
              | AST.E e -> e
+             | AST.S { AST.s = AST.ExprStmt (e, _); _ } ->
+                 (* Some statements in the input language are translated into
+                  * expressions in the Generic AST. This is e.g. the case of
+                  * `echo' in PHP. This small hack allows us to annotate those
+                  * statements as souces/sanitizers/sinks. *)
+                 e
              | _ ->
                  failwith "Only Expr patterns are supported in tainting rules")
       in