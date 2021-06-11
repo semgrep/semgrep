semgrep/semgrep

commit 7d36fc50af4021552c1d9ee999440bcd792afad7

matching: Add support for associative-commutative (AC) matching (#3332)
* matching: Add support for associative-commutative (AC) matching

Closes #3198

test plan:
make test # tests included

* Yoann's review

semgrep-core/src/matching/Generic_vs_generic.ml
@@ -627,6 +627,9 @@ and m_expr a b =
       m_expr a1 b1 >!> fun () ->
       (* try again deeper on b1 *)
       m_expr a b1
+  | ( A.Call (A.IdSpecial (A.Op aop, _toka), aargs),
+      B.Call (B.IdSpecial (B.Op bop, tokb), bargs) ) ->
+      m_call_op aop aargs bop tokb bargs
   (* boilerplate *)
   (* TODO: via m_name! and miss IdQualfied vs IdQualified otherwise *)
   | A.N (A.Id (a1, a2)), B.N (B.Id (b1, b2)) ->
@@ -1240,6 +1243,80 @@ and m_argument a b =
 and m_other_argument_operator = m_other_xxx
 
 (*e: function [[Generic_vs_generic.m_other_argument_operator]] *)
+
+(*---------------------------------------------------------------------------*)
+(* Associative-commutative iso *)
+(*---------------------------------------------------------------------------*)
+and m_call_op aop aargs bop tokb bargs =
+  let m_op_default =
+    m_arithmetic_operator aop bop >>= fun () -> m_arguments aargs bargs
+  in
+  (* We first try to perform AC-matching if possible, otherwise we fallback
+   * to the default matching strategy for non-AC operators. *)
+  match
+    ( H.ac_matching_nf aop (A.unbracket aargs),
+      H.ac_matching_nf bop (B.unbracket bargs) )
+  with
+  | Some aargs_ac, Some bargs_ac when aop =*= bop ->
+      if_config
+        (fun x -> x.ac_matching)
+        ~then_:(m_ac_op tokb aop aargs_ac bargs_ac)
+        ~else_:m_op_default
+  | ___else___ -> m_op_default
+
+(* Associative-Commutative (AC) matching of operators.
+ *
+ * This for example, will successfully match `a && b && c` against `b && a && c`!
+ * It will also match `if (<... b && c ...>) ...` against `if (a && b && c) ...`
+ * which was not matching before (`a && b && c` is parsed as `(a && b) && c`).
+ *
+ * POTENTIAL ISSUES:
+ *
+ * AC-matching of metavariables could lead to some strange behaviors given that
+ * we work with ranges. E.g. we can match `a && $X` against `b && a && c` by
+ * binding $X to `b && c`, but the range of $X will be that of the whole
+ * `b && a && c` expression. Eventually we should operate with the sub-ASTs
+ * matched by patterns rather than with their ranges.
+ *
+ * Autofix will probably not work well with AC-matching either.
+ *
+ * PERFORMANCE:
+ *
+ * Note that AC-matching is an NP-complete problem. This is a naive
+ * implementation with lots of dumb combinatorial search. If it
+ * becomes a perf issue, see paper by Steven M. Eker:
+ *
+ *     "Associative-Commutative Matching Via Bipartite Graph Matching"
+ *)
+and m_ac_op tok op aargs_ac bargs_ac =
+  (* partitition aargs_ac to separate metavariables and ellipsis (avars) from
+   * other kinds of expressions (aapps) *)
+  let avars, aapps =
+    aargs_ac
+    |> List.partition (function
+         | A.Ellipsis _ -> true
+         | A.N (A.Id ((str, _tok), _id_info)) -> MV.is_metavar_name str
+         | ___else___ -> false)
+  in
+  (* try to match each aapp with a different barg, this is a 1-to-1 matching *)
+  let bs_left =
+    match aapps with
+    (* if there are no aapps we don't want to fail *)
+    | [] -> m_combs_unit bargs_ac
+    | _ -> m_combs_1to1 m_expr aapps bargs_ac
+  in
+  (* try to match each variable with a unique disjoint subset of the remaining
+   * bs_left, ellipsis (`...`) can match the empty set. *)
+  match avars with
+  | [] -> m_combs_flatten bs_left
+  | _ ->
+      let m_var x bs' =
+        match (x, H.undo_ac_matching_nf tok op bs') with
+        | A.Ellipsis _, None -> return ()
+        | ___mvar___, None -> fail ()
+        | ___mvar___, Some op_bs' -> m_expr x op_bs'
+      in
+      m_combs_1toN m_var avars bs_left |> m_combs_flatten
 
 (*****************************************************************************)
 (* Type *)

semgrep-core/src/matching/Matching_generic.ml
@@ -395,10 +395,23 @@ let _ =
 (*s: function [[Matching_generic.all_elem_and_rest_of_list]] *)
 (* todo? optimize, probably not the optimal version ... *)
 let all_elem_and_rest_of_list xs =
-  let xs = Common.index_list xs |> List.map (fun (x, i) -> (i, x)) in
-  xs
-  |> List.map (fun (i, x) -> (x, lazy (List.remove_assq i xs |> List.map snd)))
+  let rec loop acc prev_xs = function
+    | [] -> acc
+    | x :: next_xs ->
+        let other_xs = lazy (List.rev_append prev_xs next_xs) in
+        let acc' = (x, other_xs) :: acc in
+        let prev_xs' = x :: prev_xs in
+        loop acc' prev_xs' next_xs
+  in
+  loop [] [] xs
   [@@profiling]
+
+let rec all_splits = function
+  | [] -> [ ([], []) ]
+  | x :: xs ->
+      all_splits xs
+      |> List.map (function ls, rs -> [ (x :: ls, rs); (ls, x :: rs) ])
+      |> List.flatten
 
 (*e: function [[Matching_generic.all_elem_and_rest_of_list]] *)
 
@@ -574,6 +587,59 @@ let rec m_list_in_any_order ~less_is_ok f xsa xsb =
             >||> aux xs
       in
       aux candidates
+
+(* ---------------------------------------------------------------------- *)
+(* stdlib: combinatorial search *)
+(* ---------------------------------------------------------------------- *)
+
+(* These functions are not regulat `tin -> tout` matchers, but "combinatorial"
+ * matchers `tin -> ('a list, tout) list. These `m_combs_xyz tin` return a list
+ * of `(rest, tout)` pairs, where `rest` is list of elements remaining to be
+ * matched.
+ *
+ * Used for Associative-Commutative (AC) matching! *)
+
+(* unit operation for the ('a list * tout) list monad *)
+let m_combs_unit xs (tin : tin) : (_ list * tout) list = [ (xs, [ tin ]) ]
+
+(* bind operation for the ('a list * tout) list monad *)
+let rec m_combs_bind comb_matches f : (_ list * tout) list =
+  match comb_matches with
+  | [] -> []
+  | (bs, tout) :: comb_matches' ->
+      let bs_matches = tout |> List.map (fun tin -> f bs tin) |> List.flatten in
+      bs_matches @ m_combs_bind comb_matches' f
+
+let m_combs_flatten comb_result (tin : tin) : tout =
+  comb_result tin |> List.map snd |> List.flatten
+
+let m_combs_fold m_combs xs comb_matches : (_ list * tout) list =
+  List.fold_left
+    (fun combs x -> m_combs_bind combs (fun ys' tin -> m_combs x ys' tin))
+    comb_matches xs
+
+(* Tries matching `a` against each `b_i` in `bs`, and for each succesful match
+ * returns `(bs \ b_i, m a b_i)`. This is essentially the combinatorial version
+ * of `or_list`. *)
+let m_combs_or (m : _ matcher) a bs tin : (_ list * tout) list =
+  bs |> all_elem_and_rest_of_list
+  |> List.filter_map (fun (b, other_bs) ->
+         match m a b tin with
+         | [] -> None
+         | tout -> Some (Lazy.force other_bs, tout))
+
+let m_combs_1to1 (m : _ matcher) xs ys (tin : tin) : (_ list * tout) list =
+  m_combs_fold (m_combs_or m) xs (m_combs_unit ys tin)
+
+(* Tries matching `a` against each possible sub-list [bs'] in [bs], and for
+ * each succesful match returns `(bs \ bs', m_1toN a bs')`. *)
+let m_combs_splits m_1toN a bs (tin : tin) : (_ list * tout) list =
+  bs |> all_splits
+  |> List.filter_map (fun (l, r) ->
+         match m_1toN a l tin with [] -> None | tout -> Some (r, tout))
+
+let m_combs_1toN m_1toN xs comb_result tin : (_ list * tout) list =
+  m_combs_fold (m_combs_splits m_1toN) xs (comb_result tin)
 
 (* ---------------------------------------------------------------------- *)
 (* stdlib: bool/int/string/... *)

semgrep-core/src/matching/Matching_generic.mli
@@ -103,6 +103,14 @@ val inits_and_rest_of_list_empty_ok : 'a list -> ('a list * 'a list) list
 val all_elem_and_rest_of_list : 'a list -> ('a * 'a list Lazy.t) list
 
 (*e: signature [[Matching_generic.all_elem_and_rest_of_list]] *)
+
+(* [all_splits xs] returns all possible pairs [(ls, rs)] such that [ls@rs]
+  * contains the same elements as [xs].
+  *
+  * e.g.
+  *     all_splits [1; 2] = [ ([1;2], []); ([2], [1]); ([1], [2]); ([], [1;2]) ] *)
+val all_splits : 'a list -> ('a list * 'a list) list
+
 val lazy_rest_of_list : 'a Lazy.t -> 'a
 
 (*s: signature [[Matching_generic.is_regexp_string]] *)
@@ -150,6 +158,28 @@ val m_list_with_dots : 'a matcher -> ('a -> bool) -> bool -> 'a list matcher
 
 (*e: signature [[Matching_generic.m_list_with_dots]] *)
 val m_list_in_any_order : less_is_ok:bool -> 'a matcher -> 'a list matcher
+
+(* Unit operation for the [('a list * tout) list] monad *)
+val m_combs_unit : 'a list -> tin -> ('a list * tout) list
+
+val m_combs_flatten : (tin -> ('a list * tout) list) -> tin -> tout
+
+val m_combs_1to1 :
+  'a matcher -> 'a list -> 'a list -> tin -> ('a list * tout) list
+(** [m_combs_1to1 m xs ys tin] computes all [k]-combinations of [ys] such that
+ * each [x] matches a different [y], where [k = List.length xs]. Each combination
+ * results in a pair [(ys', tout)] where [ys'] is the subset of [ys] that was not
+ * match to any [x]. If [k < List.length ys] there will be no matches.  *)
+
+val m_combs_1toN :
+  ('a -> 'b list -> tin -> tout) ->
+  'a list ->
+  (tin -> ('b list * tout) list) ->
+  tin ->
+  ('b list * tout) list
+(** [m_combs_1toN m_1toN xs comb_matches] computes, for each [(ys, tout)] pair
+ * in [comb_matches], all partitions of [ys] such that each [x] matches either
+ * a different sub-list of [ys] or the empty list. *)
 
 (* use = *)
 val m_eq : 'a matcher

semgrep-core/src/core/Config_semgrep.ml
@@ -37,6 +37,8 @@
 (*****************************************************************************)
 type t = {
   constant_propagation : bool;
+  (* associative-commutative matching *)
+  ac_matching : bool;
   (* !experimental: a bit hacky, and may introduce big perf regressions! *)
   (* should be used with DeepEllipsis; do it implicitely has issues *)
   go_deeper_expr : bool;
@@ -53,6 +55,11 @@ type t = {
 (*****************************************************************************)
 
 let default_config =
-  { constant_propagation = true; go_deeper_expr = true; go_deeper_stmt = true }
+  {
+    constant_propagation = true;
+    ac_matching = true;
+    go_deeper_expr = true;
+    go_deeper_stmt = true;
+  }
 
 (*e: semgrep/core/Config_semgrep.ml *)

semgrep-core/src/core/ast/AST_generic_helpers.ml
@@ -19,6 +19,8 @@ open Common
 open AST_generic
 module M = Map_AST
 
+let logger = Logging.get_logger [ __MODULE__ ]
+
 (*****************************************************************************)
 (* Prelude *)
 (*****************************************************************************)
@@ -215,6 +217,45 @@ let abstract_for_comparison_visitor recursor =
 
 let abstract_for_comparison_any x =
   abstract_for_comparison_visitor (fun visitor -> visitor.M.vany x)
+
+(*****************************************************************************)
+(* Associative-Commutative (AC) matching *)
+(*****************************************************************************)
+
+let is_AC_operator = function
+  | Or | And -> true
+  (* TODO: Plus, Mult, BitOr, BitAnd, ... ? *)
+  | __else__ -> false
+
+let ac_matching_nf op args =
+  (* yes... here we use exceptions like a "goto" to avoid the option monad *)
+  let rec nf args1 =
+    args1
+    |> List.map (function
+         | Arg e -> e
+         | ArgKwd _ | ArgType _ | ArgOther _ -> raise_notrace Exit)
+    |> List.concat_map nf_one
+  and nf_one = function
+    | Call (IdSpecial (Op op1, _tok1), (_, args1, _)) when op = op1 -> nf args1
+    | x -> [ x ]
+  in
+  if is_AC_operator op then (
+    try Some (nf args)
+    with Exit ->
+      logger#error
+        "ac_matching_nf: %s(%s): unexpected ArgKwd | ArgType | ArgOther"
+        (show_operator op) (show_arguments args);
+      None )
+  else None
+
+let undo_ac_matching_nf tok op : expr list -> expr option = function
+  | [] -> None
+  | [ arg ] -> Some arg
+  | a1 :: a2 :: args ->
+      let mk_op x y =
+        Call (IdSpecial (Op op, tok), fake_bracket [ Arg x; Arg y ])
+      in
+      Some (List.fold_left mk_op (mk_op a1 a2) args)
 
 (*****************************************************************************)
 (* Conversion *)

semgrep-core/src/core/ast/AST_generic_helpers.mli
@@ -41,6 +41,37 @@ val abstract_for_comparison_any : AST_generic.any -> AST_generic.any
  * with polymorphic operators.
 *)
 
+val is_AC_operator : AST_generic.operator -> bool
+(** Determines whether an operator will be subject to
+ * associative-communitative (AC) matching. Note that
+ * AC matching is expensive! *)
+
+val ac_matching_nf :
+  AST_generic.operator -> AST_generic.arguments -> AST_generic.expr list option
+(** [ac_matching_nf op args] converts the operands [args] of an
+ * AC-operator [op] to a normal form (NF) better suited for AC-matching.
+ * Essentially, we flatten out all [op]-operations. Note that this is not
+ * a canonical form (i.e., it is not a unique representation).
+ * E.g.
+ *    ac_matching_nf And [a; Call(Op And, [b; c])] = Some [a; b; c]
+ * The function returns [None] if [op] is not an AC-operator. It also
+ * returns [None] if the operands have an unexpected shape (see code),
+ * in which case we just log an error and skip AC-matching (rather
+ * than crashing). *)
+
+val undo_ac_matching_nf :
+  Parse_info.t ->
+  AST_generic.operator ->
+  AST_generic.expr list ->
+  AST_generic.expr option
+(** [undo_ac_matching_nf tok op args_nf] folds [args_nf] using [op]
+ * (in a sense, it "undoes" [ac_matching_nf]). Here [tok] is the token of
+ * the operand [op] in the source file. Note that this does not remember
+ * the original grouping of the operands!
+ * E.g.
+ *    undo_ac_matching_nf tok And [a; b; c] = Call(Op And, [Call(Op And, [a; b]); c])
+ *)
+
 val conv_op : AST_generic_.operator -> AST_generic.operator
 
 val conv_incr : AST_generic_.incr_decr -> AST_generic.incr_decr

semgrep-core/tests/python/ac_matching_dots.py
@@ -1,0 +1,12 @@
+
+#ERROR:
+if A and B:
+  foo()
+
+#ERROR:
+if C and B and A:
+  foo()
+
+#OK:
+if A and A:
+  foo()

semgrep-core/tests/python/ac_matching_dots.sgrep
@@ -1,0 +1,1 @@
+A and ... and B

commit 08c9906a04d537aa147854bf793161d66271db88

matching: Restrict AC matching to avoid a combinatorial explosion
Plus, rethink/refactor `m_comb_*` operators.

Fixes: 7d36fc50af4 ("matching: Add support for associative-commutative (AC) matching (#3332)")

test plan:
make test # tests included

semgrep-core/src/matching/Generic_vs_generic.ml
@@ -1251,8 +1251,8 @@ and m_call_op aop aargs bop tokb bargs =
   let m_op_default =
     m_arithmetic_operator aop bop >>= fun () -> m_arguments aargs bargs
   in
-  (* We first try to perform AC-matching if possible, otherwise we fallback
-   * to the default matching strategy for non-AC operators. *)
+  (* If this an AC operation we try to perform AC matching, otherwise we perform
+   * regular non-AC matching. *)
   match
     ( H.ac_matching_nf aop (A.unbracket aargs),
       H.ac_matching_nf bop (B.unbracket bargs) )
@@ -1267,30 +1267,36 @@ and m_call_op aop aargs bop tokb bargs =
 (* Associative-Commutative (AC) matching of operators.
  *
  * This for example, will successfully match `a && b && c` against `b && a && c`!
- * It will also match `if (<... b && c ...>) ...` against `if (a && b && c) ...`
+ * It will also match `if (<... b && c ...>) ...` against `if (a && b && c) S`
  * which was not matching before (`a && b && c` is parsed as `(a && b) && c`).
+ * In fact, we do not even need a deep-expr pattern, `if (b && c) ...` will also
+ * match `if (a && b && c) S`!
  *
- * POTENTIAL ISSUES:
+ * BEHAVIOR ISSUES:
  *
  * AC-matching of metavariables could lead to some strange behaviors given that
  * we work with ranges. E.g. we can match `a && $X` against `b && a && c` by
  * binding $X to `b && c`, but the range of $X will be that of the whole
- * `b && a && c` expression. Eventually we should operate with the sub-ASTs
- * matched by patterns rather than with their ranges.
+ * `b && a && c` expression. Eventually, if we switch to work with the sub-ASTs
+ * matched by patterns rather than with their ranges, this will stop being a
+ * problem. In the meantime, one can disable AC-matching per rule.
  *
  * Autofix will probably not work well with AC-matching either.
  *
- * PERFORMANCE:
+ * PERFORMANCE ISSUES:
  *
  * Note that AC-matching is an NP-complete problem. This is a naive
- * implementation with lots of dumb combinatorial search. If it
- * becomes a perf issue, see paper by Steven M. Eker:
+ * implementation with lots of dumb combinatorial search. We only use
+ * AC-matching when we expect it to be fast, otherwise we fall-back to
+ * regular non-AC matching.
+ *
+ * For a more efficient AC matching algorithm, see paper by Steven M. Eker:
  *
  *     "Associative-Commutative Matching Via Bipartite Graph Matching"
  *)
 and m_ac_op tok op aargs_ac bargs_ac =
   (* partitition aargs_ac to separate metavariables and ellipsis (avars) from
-   * other kinds of expressions (aapps) *)
+   * non-mvar-ish expressions (aapps). *)
   let avars, aapps =
     aargs_ac
     |> List.partition (function
@@ -1298,25 +1304,67 @@ and m_ac_op tok op aargs_ac bargs_ac =
          | A.N (A.Id ((str, _tok), _id_info)) -> MV.is_metavar_name str
          | ___else___ -> false)
   in
-  (* try to match each aapp with a different barg, this is a 1-to-1 matching *)
+  (* Try to match each aapp with a different barg, this is a 1-to-1 matching.
+   * Here we don't expect perf issues on real code, because each aapp will
+   * typically only match one bargs_ac, so there should be no combinatorial
+   * explosion. Of course one can construct a synthetic example that explodes!
+   * E.g. matching a long pattern `A && B && ...` against a very long decision
+   * `A && ... && A && B && ... && B && ...` could explode.
+   *)
   let bs_left =
     match aapps with
     (* if there are no aapps we don't want to fail *)
-    | [] -> m_combs_unit bargs_ac
-    | _ -> m_combs_1to1 m_expr aapps bargs_ac
+    | [] -> m_comb_unit bargs_ac
+    | _ -> m_comb_unit bargs_ac |> m_comb_fold (m_comb_1to1 m_expr) aapps
   in
-  (* try to match each variable with a unique disjoint subset of the remaining
-   * bs_left, ellipsis (`...`) can match the empty set. *)
-  match avars with
-  | [] -> m_combs_flatten bs_left
-  | _ ->
+  (* Try to match each variable with a unique disjoint subset of the remaining
+   * bs_left, ellipsis (`...`) can match the empty set. This can easily
+   * explode so we only perform full AC matching for variables when the
+   * number of bs left to match (num_bs_left) is small. Note that an mvar will
+   * typically match all subsets of the bs left!!! *)
+  let num_bs_left = List.length bargs_ac - List.length aapps in
+  let avars_no_end_dots =
+    (* An ending ellipsis (...) can be removed without affecting the result
+     * but reducing duplicates and improving perf. *)
+    match List.rev avars with
+    | A.Ellipsis _ :: rev_avars -> List.rev rev_avars
+    | ____else____ -> avars
+  in
+  match avars_no_end_dots with
+  (* Nothing to do here, just return. *)
+  | [] -> m_comb_flatten bs_left
+  | ___many___ when num_bs_left <= 3 ->
+      (* AC matching, it explodes easily so use with care! *)
       let m_var x bs' =
         match (x, H.undo_ac_matching_nf tok op bs') with
         | A.Ellipsis _, None -> return ()
         | ___mvar___, None -> fail ()
         | ___mvar___, Some op_bs' -> m_expr x op_bs'
       in
-      m_combs_1toN m_var avars bs_left |> m_combs_flatten
+      bs_left
+      |> m_comb_fold (m_comb_1toN m_var) avars_no_end_dots
+      |> m_comb_flatten
+  | ___many___ ->
+      (* FALLBACK: We add an ellipsis (`...`) at the end to make sure we match even if
+       * there are more bs left than avars, and then we do regular non-AC matching.
+       *
+       * TODO: Could we first fallback to associative-only matching? It could still
+       *       explode but not as easily
+       *)
+      (* TODO: Issue a proper warning to the user. *)
+      logger#debug
+        "Restricted AC-matching due to potential blow-up: op=%s avars#=%d \
+         bs_left#=%d\n"
+        (A.show_operator op) (List.length avars) num_bs_left;
+      m_comb_bind bs_left (fun bs' tin ->
+          let avars_dots = avars_no_end_dots @ [ A.Ellipsis (A.fake "...") ] in
+          let tout =
+            m_list__m_argument
+              (List.map A.arg avars_dots)
+              (List.map B.arg bs') tin
+          in
+          [ ([], tout) ])
+      |> m_comb_flatten
 
 (*****************************************************************************)
 (* Type *)

semgrep-core/src/matching/Matching_generic.ml
@@ -105,6 +105,10 @@ and tout = tin list
  *)
 type 'a matcher = 'a -> 'a -> tin -> tout
 
+type 'a comb_result = tin -> ('a * tout) list
+
+type 'a comb_matcher = 'a -> 'a list -> 'a list comb_result
+
 (*e: type [[Matching_generic.matcher]] *)
 
 (*****************************************************************************)
@@ -592,54 +596,44 @@ let rec m_list_in_any_order ~less_is_ok f xsa xsb =
 (* stdlib: combinatorial search *)
 (* ---------------------------------------------------------------------- *)
 
-(* These functions are not regulat `tin -> tout` matchers, but "combinatorial"
- * matchers `tin -> ('a list, tout) list. These `m_combs_xyz tin` return a list
- * of `(rest, tout)` pairs, where `rest` is list of elements remaining to be
- * matched.
- *
- * Used for Associative-Commutative (AC) matching! *)
+(* Used for Associative-Commutative (AC) matching! *)
 
-(* unit operation for the ('a list * tout) list monad *)
-let m_combs_unit xs (tin : tin) : (_ list * tout) list = [ (xs, [ tin ]) ]
+let m_comb_unit xs : _ comb_result = fun tin -> [ (xs, [ tin ]) ]
 
-(* bind operation for the ('a list * tout) list monad *)
-let rec m_combs_bind comb_matches f : (_ list * tout) list =
-  match comb_matches with
-  | [] -> []
-  | (bs, tout) :: comb_matches' ->
-      let bs_matches = tout |> List.map (fun tin -> f bs tin) |> List.flatten in
-      bs_matches @ m_combs_bind comb_matches' f
+let m_comb_bind (comb_result : _ comb_result) f : _ comb_result =
+ fun tin ->
+  let rec loop = function
+    | [] -> []
+    | (bs, tout) :: comb_matches' ->
+        let bs_matches =
+          tout |> List.map (fun tin -> f bs tin) |> List.flatten
+        in
+        bs_matches @ loop comb_matches'
+  in
+  loop (comb_result tin)
 
-let m_combs_flatten comb_result (tin : tin) : tout =
+let m_comb_flatten (comb_result : _ comb_result) (tin : tin) : tout =
   comb_result tin |> List.map snd |> List.flatten
 
-let m_combs_fold m_combs xs comb_matches : (_ list * tout) list =
+let m_comb_fold (m_comb : _ comb_matcher) (xs : _ list)
+    (comb_result : _ comb_result) : _ comb_result =
   List.fold_left
-    (fun combs x -> m_combs_bind combs (fun ys' tin -> m_combs x ys' tin))
-    comb_matches xs
+    (fun comb_result' x -> m_comb_bind comb_result' (m_comb x))
+    comb_result xs
 
-(* Tries matching `a` against each `b_i` in `bs`, and for each succesful match
- * returns `(bs \ b_i, m a b_i)`. This is essentially the combinatorial version
- * of `or_list`. *)
-let m_combs_or (m : _ matcher) a bs tin : (_ list * tout) list =
+let m_comb_1to1 (m : _ matcher) a bs : _ comb_result =
+ fun tin ->
   bs |> all_elem_and_rest_of_list
   |> List.filter_map (fun (b, other_bs) ->
          match m a b tin with
          | [] -> None
          | tout -> Some (Lazy.force other_bs, tout))
 
-let m_combs_1to1 (m : _ matcher) xs ys (tin : tin) : (_ list * tout) list =
-  m_combs_fold (m_combs_or m) xs (m_combs_unit ys tin)
-
-(* Tries matching `a` against each possible sub-list [bs'] in [bs], and for
- * each succesful match returns `(bs \ bs', m_1toN a bs')`. *)
-let m_combs_splits m_1toN a bs (tin : tin) : (_ list * tout) list =
+let m_comb_1toN m_1toN a bs : _ comb_result =
+ fun tin ->
   bs |> all_splits
   |> List.filter_map (fun (l, r) ->
          match m_1toN a l tin with [] -> None | tout -> Some (r, tout))
-
-let m_combs_1toN m_1toN xs comb_result tin : (_ list * tout) list =
-  m_combs_fold (m_combs_splits m_1toN) xs (comb_result tin)
 
 (* ---------------------------------------------------------------------- *)
 (* stdlib: bool/int/string/... *)

semgrep-core/src/matching/Matching_generic.mli
@@ -32,6 +32,17 @@ type 'a matcher = 'a -> 'a -> tin -> tout
 
 (*e: type [[Matching_generic.matcher]] *)
 
+type 'a comb_result = tin -> ('a * tout) list
+(** See [comb_matcher] *)
+
+type 'a comb_matcher = 'a -> 'a list -> 'a list comb_result
+(** A "combinatorial" matcher takes an element A, a list of elements Bs,
+ * and an input environment, and returns a list of pairs (Bs', tout),
+ * where tout represents a match between A and a sub-lit of Bs, and
+ * Bs' is a sub-list of Bs with the elements that were not matched.
+ *
+ * Used for Associative-Commutative (AC) matching! *)
+
 (* monadic combinators *)
 (*s: signature [[Matching_generic.monadic_bind]] *)
 val ( >>= ) : (tin -> tout) -> (unit -> tin -> tout) -> tin -> tout
@@ -159,27 +170,37 @@ val m_list_with_dots : 'a matcher -> ('a -> bool) -> bool -> 'a list matcher
 (*e: signature [[Matching_generic.m_list_with_dots]] *)
 val m_list_in_any_order : less_is_ok:bool -> 'a matcher -> 'a list matcher
 
-(* Unit operation for the [('a list * tout) list] monad *)
-val m_combs_unit : 'a list -> tin -> ('a list * tout) list
+val m_comb_unit : 'a -> 'a comb_result
+(** Unit operation for the comb_result monad. *)
 
-val m_combs_flatten : (tin -> ('a list * tout) list) -> tin -> tout
+val m_comb_bind : 'a comb_result -> ('a -> 'b comb_result) -> 'b comb_result
+(** Bind operation for the comb_result monad. *)
 
-val m_combs_1to1 :
-  'a matcher -> 'a list -> 'a list -> tin -> ('a list * tout) list
-(** [m_combs_1to1 m xs ys tin] computes all [k]-combinations of [ys] such that
- * each [x] matches a different [y], where [k = List.length xs]. Each combination
- * results in a pair [(ys', tout)] where [ys'] is the subset of [ys] that was not
- * match to any [x]. If [k < List.length ys] there will be no matches.  *)
+val m_comb_fold :
+  'a comb_matcher -> 'a list -> 'a list comb_result -> 'a list comb_result
+(** [m_comb_fold m_comb xs comb_result] folds [xs] by sequentially matching
+ * each [x] against each partial result in [comb_result].
+ *
+ * That is, m_comb_fold m_comb [x1; ...; xn] comb_result is:
+ *
+ *    m_comb_bind ((m_comb_bind comb_result (m_comb x1)) ...)) (m_comb xn)
+ *)
 
-val m_combs_1toN :
-  ('a -> 'b list -> tin -> tout) ->
-  'a list ->
-  (tin -> ('b list * tout) list) ->
-  tin ->
-  ('b list * tout) list
-(** [m_combs_1toN m_1toN xs comb_matches] computes, for each [(ys, tout)] pair
- * in [comb_matches], all partitions of [ys] such that each [x] matches either
- * a different sub-list of [ys] or the empty list. *)
+val m_comb_flatten : 'a comb_result -> tin -> tout
+(** [m_comb_flatten comb_result] takes each (xs, tout) pair in [comb_result],
+ * drops the [xs] and concatenates all the [tout]. *)
+
+val m_comb_1to1 : 'a matcher -> 'a comb_matcher
+(** [m_comb_1to1 m] returns a combinatorial matcher that, given an element [x]
+ * an a list of elements [ys], will try matching `x` against each `y` in `ys`.
+ * Each succesful match returns `(ys \ y, m x y)`. This is essentially the
+ * combinatorial version of `or_list`. *)
+
+val m_comb_1toN : ('a -> 'a list -> tin -> tout) -> 'a comb_matcher
+(** [m_comb_1toN m_1toN] returns a combinatorial matcher that, given an element
+ * [x] and a list of elements [ys], will try matching `x` against each possible
+ * sub-list [ys'] in [ys]. Each succesful match returns
+ * [(ys \ ys', m_1toN x ys')]. *)
 
 (* use = *)
 val m_eq : 'a matcher

semgrep-core/tests/php/ac_matching.php
@@ -1,0 +1,18 @@
+<?php
+// https://github.com/returntocorp/semgrep/issues/3198
+
+//ERROR:
+if (!check1($stuff) && !check2($stuff)) {
+    // Do stuff
+}
+
+//ERROR:
+if (!check1($stuff) && !check2($stuff) && !check3($stuff)) {
+    // Do stuff
+}
+
+//ERROR:
+if (!check3($stuff) && !check1($stuff) && !check2($stuff)) {
+    // DO other stuff
+}
+

semgrep-core/tests/php/ac_matching.sgrep
@@ -1,0 +1,1 @@
+!check1($VAR) && !check2($VAR)

semgrep-core/tests/python/ac_matching_explosion.py
@@ -1,0 +1,3 @@
+#ERROR:
+if A and B and C:
+    foo()

semgrep-core/tests/python/ac_matching_explosion.sgrep
@@ -1,0 +1,1 @@
+$A and $B and $C

semgrep-core/tests/python/ac_matching_explosion1.py
@@ -1,0 +1,3 @@
+#ERROR:
+if A and B and C and D and E and F and G and H and I and J and K and L and M and N and O and P and Q and R and S and T and U and V and W and X and Y and Z:
+    foo()

semgrep-core/tests/python/ac_matching_explosion1.sgrep
@@ -1,0 +1,1 @@
+A and $B

semgrep-core/tests/python/ac_matching_explosion2.py
@@ -1,0 +1,3 @@
+#ERROR:
+if A and B and C and D and E and F and G and H and I and J and K and L and M and N and O and P and Q and R and S and T and U and V and W and X and Y and Z and A and C:
+    foo()

semgrep-core/tests/python/ac_matching_explosion2.sgrep
@@ -1,0 +1,1 @@
+A and $B and C and $D and $E and $F and $G

semgrep-core/tests/python/ac_matching_explosion3.py
@@ -1,0 +1,3 @@
+#ERROR:
+if A and B and C and D and E and F and G and H and I and J and K and L and M and N and O and P and Q and R and S and T and U and V and W and X and Y and Z:
+    foo()

semgrep-core/tests/python/ac_matching_explosion3.sgrep
@@ -1,0 +1,1 @@
+$A and $B and $C and $D and $E and $F and $G

semgrep-core/tests/python/ac_matching_if.py
@@ -1,0 +1,3 @@
+#ERROR:
+if (A and B and C):
+    foo()

semgrep-core/tests/python/ac_matching_if.sgrep
@@ -1,0 +1,2 @@
+if <... B and C ...>:
+	foo()

semgrep-core/tests/python/ac_matching_if1.py
@@ -1,0 +1,3 @@
+#ERROR:
+if (A and B and C):
+    foo()

semgrep-core/tests/python/ac_matching_if1.sgrep
@@ -1,0 +1,2 @@
+if B and C:
+	foo()