7eb1874cf4959870bb324423d66c2bc347d70077
pad <yoann.padioleau@gmail.com>
2021-06-30 13:42:59 +0200

    more comments

--- semgrep-core/src/optimizing/Analyze_rule.ml
+++ semgrep-core/src/optimizing/Analyze_rule.ml
@@ -39,12 +39,13 @@ let logger = Logging.get_logger [ __MODULE__ ]
  *        require("foo.js")
  *        ...
  *
- * the current Mini_rules_filter used in Semgrep_generic will just see a flat
+ * the current Mini_rules_filter used in Match_patterns will just see a flat
  * list of patterns, and will look separately for 'eval' and 'foo.js'
  * and filter certain patterns; But really, even if 'foo.js' is mentioned
  * in a file, we should completly skip the file if 'eval' is not in the file
  * because after all 'foo.js' is mentioned in a pattern-not context!
- *
+ * The goal of this file is to process a rule holistically to
+ * overcome the limitations of Analyze_pattern.ml
  *
  * There are many optimization opportunities when semgrep-core can see
  * the whole rule:
