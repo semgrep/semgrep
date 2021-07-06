c1b02359192eb334bd9656d06edeba3a40682d63
Yoann Padioleau <pad@returntocorp.com>
2021-07-06 17:17:46 +0200

    adding boilerplate for parsing Vue files (#3477)
    * adding boilerplate for parsing Vue files
    
    test plan:
    make test
    
    * misc
    
    * fix semgrep-vue, do not ovverride html scanner

--- semgrep-core/src/parsing/Parse_target.ml
+++ semgrep-core/src/parsing/Parse_target.ml
@@ -299,7 +299,8 @@ let just_parse_with_lang lang file =
   | Lang.HTML ->
       (* less: there is an html parser in pfff too we could use as backup *)
       run file [ TreeSitter Parse_html_tree_sitter.parse ] (fun x -> x)
-  | Lang.Vue -> failwith "No Vue parser yet"
+  | Lang.Vue ->
+      run file [ TreeSitter Parse_vue_tree_sitter.parse ] Js_to_generic.program
 
 (*****************************************************************************)
 (* Entry point *)
