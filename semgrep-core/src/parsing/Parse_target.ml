8bb1974f7812c622ff0a12090d8d944d9477252b
pad <yoann.padioleau@gmail.com>
2021-07-06 11:26:16 +0200

    Adding boilerplate to convert tree-sitter CST for HTML to AST generic
    test plan:
    make test

--- semgrep-core/src/parsing/Parse_target.ml
+++ semgrep-core/src/parsing/Parse_target.ml
@@ -296,7 +296,9 @@ let just_parse_with_lang lang file =
         errors = [];
         stat = Parse_info.default_stat file;
       }
-  | Lang.HTML -> failwith "No HTML generic parser yet"
+  | Lang.HTML ->
+      (* less: there is an html parser in pfff too we could use as backup *)
+      run file [ TreeSitter Parse_html_tree_sitter.parse ] (fun x -> x)
   | Lang.Vue -> failwith "No Vue parser yet"
 
 (*****************************************************************************)
