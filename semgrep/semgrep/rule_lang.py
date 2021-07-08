3079a6756e823199cb688c80de92d322aac9d57e
Emma Jin <emjin@users.noreply.github.com>
2021-07-08 18:43:07 -0400

    Adjust position as necessary

--- semgrep/semgrep/rule_lang.py
+++ semgrep/semgrep/rule_lang.py
@@ -138,7 +138,7 @@ class Span:
         src_hash = SourceTracker.add_source(s)
         start = Position(line, col)
         lines = s.splitlines()
-        end = Position(line=len(lines), col=len(lines[-1]))
+        end = Position(line=(len(lines) + line - 1), col=(len(lines[-1]) + col) - 1)
         return Span(start=start, end=end, file=filename, source_hash=src_hash)
 
     def fix(self) -> "Span":
