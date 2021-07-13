a53d6d7a79a5dd375947f65f659d51e3c8229ec3
Iago Abal <mail@iagoabal.eu>
2021-07-13 10:31:51 +0200

    matching: Make record matching more consistent (#3506)
    1. Allow disabling the implicit ... for record patterns via `options:`.
    2. Match "spread fields" (JS `...x`), or any other kind of field, at any
       position; same as we already do for definitions.
    
    Closes #3120
    
    test plan:
    make test # tests included

---
+++ semgrep-core/tests/OTHER/rules/record_empty.js
@@ -0,0 +1,6 @@
+//ruleid:test
+a = {};
+//ok:test
+a = {x:1};
+//ok:test
+a = {x:1, y:2};
---
+++ semgrep-core/tests/OTHER/rules/record_one_spread.js
@@ -0,0 +1,8 @@
+//ok:test
+a = {};
+//ok:test
+a = {...x, ...y};
+//ruleid:test
+a = {...x};
+//ok:test
+a = {...x, y:1}
