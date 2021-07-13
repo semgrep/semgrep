5ee5c02d0b6059553f526fd2e8cba9f70f72234c
Daniel DeFreez <defreez@users.noreply.github.com>
2021-07-13 02:46:37 -0700

    synthesizing: Pattern_from_Targets ExprStmt lists (#3488)
    * synthesizing: Pattern_from_Targets ExprStmt lists
    
    Add support for targets that are ExprStmt lists in Pattern_from_Targets.
    
    For example, two separate targets might be
    
    ```
    x = req.query.foo;
    exec(x);
    ```
    
    and
    
    ```
    y = req.query.foo;
    exec(y);
    ```
    
    yielding the pattern
    
    ```
    $X = req.query.foo;
    exec($X);
    ```
    
    Limitations:
    - Targets must be of equal length. Cannot yet generate ellipsis for
    statements to equalize length of targets.
    - No additional support for statements other than ExprStmt.
    
    * Fix trailing whitespace.
    
    * Remove use of ($)

--- semgrep-core/src/synthesizing/Range_to_AST.mli
+++ semgrep-core/src/synthesizing/Range_to_AST.mli
@@ -5,3 +5,17 @@
 val expr_at_range : Range.t -> AST_generic.program -> AST_generic.expr option
 
 val any_at_range : Range.t -> AST_generic.program -> AST_generic.any option
+
+(* Repeatedly calls any_at_range to consume as much of the target input as possible.
+ * If a call at any_at_range does not consume the entire token range, then
+ * any_at_range is called again with a new target range starting where the last call
+ * left off.
+
+ * For example, range provided by the user is (0, 29). First call to any_at_range
+ * returns found (0, 17). The found match is added to the list and then
+ * any_at_range is called again with (18, 29). If another match is found
+ * then it is appended to the list.
+
+ * Empty list is returned if no matches are found.
+ *)
+val many_at_range : Range.t -> AST_generic.program -> AST_generic.any list
