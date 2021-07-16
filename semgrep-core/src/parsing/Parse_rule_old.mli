3597d0b2afbd2cc9178e42ecdfb989a6e434f8ef
Emma Jin <emjin@users.noreply.github.com>
2021-07-16 14:20:19 -0400

    Remove old parse rule files

--- semgrep-core/src/parsing/Parse_rule_old.mli
+++
@@ -1,8 +0,0 @@
-(*s: semgrep/parsing/Parse_rule.mli *)
-
-val parse : Common.filename -> Rule.rules
-
-(* used also by Convert_rule.ml *)
-val parse_metavar_cond : string -> AST_generic.expr
-
-(*e: semgrep/parsing/Parse_rule.mli *)
