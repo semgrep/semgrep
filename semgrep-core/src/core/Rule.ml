22eb1d32a60e59d44418dbcdca3a98f196fd9129
pad <yoann.padioleau@gmail.com>
2021-07-21 11:09:58 +0200

    misc

--- semgrep-core/src/core/Rule.ml
+++ semgrep-core/src/core/Rule.ml
@@ -8,7 +8,7 @@
  * modify it under the terms of the GNU Lesser General Public License
  * version 2.1 as published by the Free Software Foundation, with the
  * special exception on linking described in file license.txt.
-*
+ *
  * This library is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
