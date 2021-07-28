35f8fc51523ab39f8eb7847e9cca663e1fd66424
Emma Jin <emjin@users.noreply.github.com>
2021-07-28 16:22:14 -0700

    Update test error snapshots

--- semgrep/semgrep/core_runner.py
+++ semgrep/semgrep/core_runner.py
@@ -98,8 +98,8 @@ class CoreRunner:
                     path=range.get("path", []),
                     filename="semgrep temp file",
                 )
-                if error_json["message"] == "Parsing.ParseError":
-                    long_msg = f"Pattern could not be parsed as a {error_json['language']} semgrep pattern"
+                if error_json["message"] == "Parsing.Parse_error":
+                    long_msg = f"Pattern `{s.strip()}` could not be parsed as a {error_json['language']} semgrep pattern"
                 else:
                     long_msg = f"Error parsing {error_json['language']} pattern: {error_json['message']}"
 
