02ea29d1a1f3de16e75b5a1d1c4ad92608dd6a4f
Martin Jambon <martin@r2c.dev>
2021-07-29 02:09:59 -0700

    Print a warning when the stack size exceeds 7 MB. Raise a 'Stack_overflow' exception when the stack size exceeds half of the user-specified memory limit (requires 'ulimit -s unlimited' or equivalent).

--- semgrep-core/src/cli/Main.ml
+++ semgrep-core/src/cli/Main.ml
@@ -433,6 +433,19 @@ let timeout_function file f =
    It raises Out_of_memory if we're over the memory limit at the end of a
    major GC cycle.
 
+   This also handles stack overflows, which otherwise often produce
+   segfaults. For now, we don't have access to the process's maximum stack
+   size, so we do the following:
+   - show a warning when passing 7 MiB, since the default limit on Linux
+     is 8 MiB.
+   - if there's no maximum stack size (e.g. 'ulimit -s unlimited' in bash),
+     or if it's large enough, we detect when the stack size passes
+     50% of the memory limit specified by the user. This is a somewhat
+     arbitrary percentage, assumed to be plenty.
+
+   We can eliminate the warning if/when we have access to the process's
+   maximum stack size, or we could emit a warning at 80% of the limit.
+
    See https://discuss.ocaml.org/t/todays-trick-memory-limits-with-gc-alarms/4431
    for detailed explanations.
 *)
@@ -441,12 +454,31 @@ let run_with_memory_limit limit_mb f =
   else if limit_mb < 0 then
     invalid_arg (spf "run_with_memory_limit: negative argument %i" limit_mb)
   else
-    let limit = limit_mb * 1024 * 1024 in
+    let mem_limit = limit_mb * 1024 * 1024 in
+    let stack_limit = mem_limit / 2 in
+    let stack_warning = 7 * 1024 * 1024 in
+    let stack_already_warned = ref false in
     let limit_memory () =
-      let mem = (Gc.quick_stat ()).Gc.heap_words in
-      if mem > limit / (Sys.word_size / 8) then (
-        logger#info "maxout allocated memory: %d" (mem * (Sys.word_size / 8));
+      let stat = Gc.quick_stat () in
+      let heap_bytes = stat.heap_words * (Sys.word_size / 8) in
+      let stack_bytes = stat.stack_size * (Sys.word_size / 8) in
+      let mem_bytes = heap_bytes + stack_bytes in
+      if mem_bytes > mem_limit then (
+        logger#info
+          "exceeded heap+stack memory limit: %d bytes (stack=%d, heap=%d)"
+          mem_bytes stack_bytes heap_bytes;
         raise Out_of_memory)
+      else if stack_bytes > stack_limit then (
+        logger#info "exceeded stack size limit: %d bytes" stack_bytes;
+        raise Stack_overflow)
+      else if stack_bytes > stack_warning && not !stack_already_warned then (
+        logger#warning
+          "large stack size: %d bytes. If a crash follows, you should suspect \
+           a stack overflow. Make sure the maximum stack size is set to \
+           'unlimited' or to a value greater than %d bytes so as to obtain an \
+           exception rather than a segfault."
+          stack_bytes mem_limit;
+        stack_already_warned := true)
     in
     let alarm = Gc.create_alarm limit_memory in
     Fun.protect f ~finally:(fun () ->
