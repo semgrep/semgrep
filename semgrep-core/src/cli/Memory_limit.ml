94dc443c180777462ffb2d1479aee09e6a2e50e1
Martin Jambon <martin@r2c.dev>
2021-07-30 15:47:32 -0700

    Add missing files

---
+++ semgrep-core/src/cli/Memory_limit.ml
@@ -0,0 +1,85 @@
+(*
+   Avoid segfaults when the process runs out of memory.
+*)
+
+open Common
+
+let logger = Logging.get_logger [ __MODULE__ ]
+
+let default_stack_warning_mb = 5
+
+let default_stack_limit_mb = 7
+
+(*
+   Fail gracefully if memory becomes insufficient.
+
+   It raises Out_of_memory if we're over the memory limit at the end of a
+   major GC cycle.
+
+   This also handles stack overflows, which otherwise often produce
+   segfaults. For now, we don't have access to the process's maximum stack
+   size, so we do the following:
+   - show a warning when passing 7 MiB, since the default limit on Linux
+     is 8 MiB.
+   - if there's no maximum stack size (e.g. 'ulimit -s unlimited' in bash),
+     or if it's large enough, we detect when the stack size passes
+     50% of the memory limit specified by the user. This is a somewhat
+     arbitrary percentage, assumed to be plenty.
+
+   We can eliminate the warning if/when we have access to the process's
+   maximum stack size, or we could emit a warning at 80% of the limit.
+
+   See https://discuss.ocaml.org/t/todays-trick-memory-limits-with-gc-alarms/4431
+   for detailed explanations.
+*)
+let run_with_memory_limit ?(stack_warning_mb = default_stack_warning_mb)
+    ?(stack_limit_mb = default_stack_limit_mb) ~mem_limit_mb f =
+  if stack_warning_mb < 0 then
+    invalid_arg
+      (spf "run_with_memory_limit: negative argument stack_warning_mb %i"
+         stack_warning_mb);
+  if stack_limit_mb < 0 then
+    invalid_arg
+      (spf "run_with_memory_limit: negative argument stack_limit_mb %i"
+         stack_limit_mb);
+  if mem_limit_mb < 0 then
+    invalid_arg
+      (spf "run_with_memory_limit: negative argument mem_limit_mb %i"
+         mem_limit_mb);
+
+  if stack_warning_mb = 0 && stack_limit_mb = 0 && mem_limit_mb = 0 then f ()
+  else
+    let mem_limit = mem_limit_mb * 1024 * 1024 in
+    let stack_limit = stack_limit_mb * 1024 * 1024 in
+    let stack_warning = stack_warning_mb * 1024 * 1024 in
+    let stack_already_warned = ref false in
+    let limit_memory () =
+      let stat = Gc.quick_stat () in
+      let heap_bytes = stat.heap_words * (Sys.word_size / 8) in
+      let stack_bytes = stat.stack_size * (Sys.word_size / 8) in
+      let mem_bytes = heap_bytes + stack_bytes in
+      if mem_limit > 0 && mem_bytes > mem_limit then (
+        logger#info
+          "exceeded heap+stack memory limit: %d bytes (stack=%d, heap=%d)"
+          mem_bytes stack_bytes heap_bytes;
+        raise Out_of_memory)
+      else if stack_limit > 0 && stack_bytes > stack_limit then (
+        logger#info "exceeded stack size limit: %d bytes" stack_bytes;
+        raise Stack_overflow)
+      else if
+        stack_warning > 0
+        && stack_bytes > stack_warning
+        && not !stack_already_warned
+      then (
+        logger#warning
+          "large stack size: %d bytes. If a crash follows, you should suspect \
+           a stack overflow. Make sure the maximum stack size is set to \
+           'unlimited' or to a value greater than %d bytes so as to obtain an \
+           exception rather than a segfault."
+          stack_bytes mem_limit;
+        stack_already_warned := true)
+    in
+    let alarm = Gc.create_alarm limit_memory in
+    Fun.protect f ~finally:(fun () ->
+        Gc.delete_alarm alarm;
+        Gc.compact ())
---
+++ semgrep-core/src/cli/Memory_limit.mli
@@ -0,0 +1,71 @@
+(*
+   Avoid segfaults when the process runs out of memory.
+*)
+
+(*
+   Set approximate limits on stack size and combined heap+stack size,
+   resulting in 'Stack_overflow' and 'Out_of_memory' exceptions,
+   respectively.
+   This is done by checking the GC's stats at the end of each major GC
+   cycle. It is approximate but OS-independent. The goal is to reliably
+   raise an exception when running out of memory rather than getting
+   a segfault, which is unrecoverable in OCaml.
+
+   As of ocaml 4.12, segfaults often occur when running out of physical
+   memory. Segfaults used to occur also on stack overflows, although this
+   has been fixed in ocaml 4.10, at least partially.
+   See:
+   - https://discuss.ocaml.org/t/is-there-any-value-in-having-a-maximum-stack-size/8214/10
+   - https://github.com/returntocorp/semgrep/issues/3640
+
+   The limits set by this function call should be lower than the system
+   limits so as to get exceptions instead of segfaults.
+   The system limits are:
+
+   - for stack overflows, there's usually a limit. Check 'ulimit -s'
+     from bash. On Linux, 'ulimit -Ss unlimited' will remove the soft limit
+     in the current process and its future children. This is allowed if
+     the hard limit ('ulimit -Hs') is 'unlimited', which is the default.
+     The default (soft) limit on Linux and Darwin is 8 MiB. The corresponding
+     POSIX system call 'setrlimit' for setting the limit is said to be broken
+     on Darwin, where there's a hard limit of about 64 MiB.
+     On Windows, the limit is harcoded in the executable and defaults to 1 MiB.
+   - for OOM errors, the limit is dictated by how much physical memory is
+     available to all the processes. A Docker container typically has its
+     own memory limit which ensures the host won't be taken down if
+     processes in the container use too much memory.
+
+   Function parameters:
+
+   - stack_size_warning_mb: a warning will be printed if the stack size
+     is found to exceed this value (in MiB).
+   - stack_size_limit_mb: a 'Stack_overflow' exception will be raised if
+     the stack size is found to exceed this value (in MiB).
+   - mem_limit_mb: an 'Out_of_memory' exception will be raised if the
+     combined heap+stack size is found to exceed this value (in MiB).
+
+   All the parameters are expressed in MiB (2^20 bytes). A value of 0
+   indicates no limit.
+
+   Default values are the minimum values we're willing to support for
+   semgrep-core:
+   - default stack_size_warning_mb: 5 MiB
+   - default stack_size_limit_mb: 7 MiB (based on Linux default of 8 MiB)
+
+   Recommendations:
+   - for testing, use the lowest stack size limit that we're willing
+     to support.
+   - for production, use something like 85% of the system's maximum stack size
+     if there is one and it's known. If it's known to be unlimited,
+     something like 50% of 'mem_limit_mb' should be fine.
+*)
+val run_with_memory_limit :
+  ?stack_warning_mb:int ->
+  ?stack_limit_mb:int ->
+  mem_limit_mb:int ->
+  (unit -> 'a) ->
+  'a
+
+val default_stack_warning_mb : int
+
+val default_stack_limit_mb : int
