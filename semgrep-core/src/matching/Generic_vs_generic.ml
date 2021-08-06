1c38227923de80d297f25797debd49afc093f0d7
pad <yoann.padioleau@gmail.com>
2021-08-06 14:15:50 +0200

    factorize m_attr

--- semgrep-core/src/matching/Generic_vs_generic.ml
+++ semgrep-core/src/matching/Generic_vs_generic.ml
@@ -327,7 +327,7 @@ let _m_resolved_name (a1, a2) (b1, b2) =
 
 (* start of recursive need *)
 (*s: function [[Generic_vs_generic.m_name]] *)
-(* TODO: factorize metavariable and aliasing logic in m_expr, m_attr
+(* TODO: factorize with metavariable and aliasing logic in m_expr
  * TODO: remove MV.Id and use always MV.N?
  *)
 let rec m_name a b =
@@ -1616,34 +1616,12 @@ and m_keyword_attribute a b =
 and m_attribute a b =
   match (a, b) with
   (*s: [[Generic_vs_generic.m_attribute]] resolving alias case *)
-  (* equivalence: name resolving! *)
-  (* TODO: factorize with m_name again *)
-  | ( a,
-      B.NamedAttr
-        ( t1,
-          B.Id
-            ( b1,
-              {
-                B.id_resolved =
-                  {
-                    contents =
-                      Some
-                        ( ( B.ImportedEntity dotted
-                          | B.ImportedModule (B.DottedName dotted) ),
-                          _sid );
-                  };
-                _;
-              } ),
-          b2 ) ) ->
-      (* We also allow an unqualified pattern like @Attr to match resolved
-       * one like import org.foo.Attr; @Attr *)
-      m_attribute a (B.NamedAttr (t1, B.Id (b1, B.empty_id_info ()), b2))
-      >||> m_attribute a (B.NamedAttr (t1, H.name_of_ids dotted, b2))
   (*e: [[Generic_vs_generic.m_attribute]] resolving alias case *)
   (* boilerplate *)
   | G.KeywordAttr a1, B.KeywordAttr b1 -> m_wrap m_keyword_attribute a1 b1
   | G.NamedAttr (a0, a1, a2), B.NamedAttr (b0, b1, b2) ->
       m_tok a0 b0 >>= fun () ->
+      (* 'm_name' handles aliasing for attributes! *)
       m_name a1 b1 >>= fun () -> m_bracket m_list__m_argument a2 b2
   | G.OtherAttribute (a1, a2), B.OtherAttribute (b1, b2) ->
       m_other_attribute_operator a1 b1 >>= fun () -> (m_list m_any) a2 b2
