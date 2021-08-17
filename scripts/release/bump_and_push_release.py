e81c9aee1ad940b3e0732304a2d1689489e5269d
brendon <brendon.go@gmail.com>
2021-08-17 12:26:04 -0700

    Simplify release process

--- scripts/release/bump_and_push_release.py
+++ scripts/release/bump_and_push_release.py
@@ -1,23 +1,26 @@
-import subprocess
-
 from util import abort
 from util import diffs
-from util import git
 from util import release_version
+
+# from util import git
 
 
 BUMP_FILES = {"semgrep/semgrep/__init__.py", "setup.py", "CHANGELOG.md"}
 
 
-def bump_and_push(version: str):
-    subprocess.run(["make", "bump"], env={"SEMGREP_VERSION": version})
+def add_commit_push(version: str):
+    """
+    Add all changes to files in BUMP_FILES, creates a release commit, and pushes said commit
+
+    Assumes all files in BUMP_FILES were modified
+    """
     changed_files = [d[1] for d in diffs()]
     if set(changed_files) != BUMP_FILES:
         abort(2, f"Expected diffs only in {BUMP_FILES}")
-    git("add", *BUMP_FILES)
-    git("commit", "-m", f"Release {version}")
-    git("push", "origin", "HEAD")
+    # git("add", *BUMP_FILES)
+    # git("commit", "-m", f"Release {version}")
+    # git("push", "origin", "HEAD")
 
 
 if __name__ == "__main__":
-    bump_and_push(release_version())
+    add_commit_push(release_version())
