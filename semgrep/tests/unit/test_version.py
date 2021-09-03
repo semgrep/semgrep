f8ec7069bdd981242779f1907a992cecefbe52b7
Nathan Brahms <nathan@returntocorp.com>
2021-09-02 17:58:34 -0700

    fixup! Update version check to show messages from backend

--- semgrep/tests/unit/test_version.py
+++ semgrep/tests/unit/test_version.py
@@ -8,7 +8,7 @@ def test_version_check_caching(tmp_path):
 
     with mock.patch("semgrep.version._fetch_latest_version") as fetched_mock:
         fetched_mock.return_value = "1.2.3"
-        semgrep.version.is_running_latest(tmp_cache_path)
-        semgrep.version.is_running_latest(tmp_cache_path)
+        semgrep.version.version_check(tmp_cache_path)
+        semgrep.version.version_check(tmp_cache_path)
 
     assert fetched_mock.call_count == 1
