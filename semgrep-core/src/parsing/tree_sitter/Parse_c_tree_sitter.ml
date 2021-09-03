190d5542bc147079833ff93f0f253da0c2ddf29c
Emma Jin <emjin@users.noreply.github.com>
2021-09-03 10:24:17 -0700

    Free tree-sitter tree when done (#3792)
    * Free tree-sitter tree when done
    
    * Remove parallel.invoke
    
    * Remove helper semgrep rule
    
    * Remove the parallel.set, now unnecessary
    
    * Include comments
    
    * Update ocaml-tree-sitter-core

--- semgrep-core/src/parsing/tree_sitter/Parse_c_tree_sitter.ml
+++ semgrep-core/src/parsing/tree_sitter/Parse_c_tree_sitter.ml
@@ -1694,9 +1694,7 @@ and translation_unit (env : env) (xs : CST.translation_unit) : program =
 
 let parse file =
   H.wrap_parser
-    (fun () ->
-      Parallel.backtrace_when_exn := false;
-      Parallel.invoke Tree_sitter_c.Parse.file file ())
+    (fun () -> Tree_sitter_c.Parse.file file)
     (fun cst ->
       let env =
         { H.file; conv = H.line_col_to_pos file; extra = default_extra_env }
