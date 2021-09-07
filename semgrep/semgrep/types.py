dcf8f1b02fd6690d0614ce9a03b956dbe45bcd8f
Nathaniel Brahms <nbrahms@users.noreply.github.com>
2021-09-07 08:30:14 -0700

    Update version check to show messages from backend (#3812)
    This allows us to:
    - Control the version upgrade message from the backend
    - Explain to users _why_ they should upgrade
    - Show additional deprecation notices

---
+++ semgrep/semgrep/types.py
@@ -0,0 +1,4 @@
+from typing import Any
+from typing import Mapping
+
+JsonObject = Mapping[str, Any]
--- semgrep/tests/unit/test_version.py
+++ semgrep/tests/unit/test_version.py
@@ -7,8 +7,8 @@ def test_version_check_caching(tmp_path):
     tmp_cache_path = tmp_path / "semgrep_version"
 
     with mock.patch("semgrep.version._fetch_latest_version") as fetched_mock:
-        fetched_mock.return_value = "1.2.3"
-        semgrep.version.is_running_latest(tmp_cache_path)
-        semgrep.version.is_running_latest(tmp_cache_path)
+        fetched_mock.return_value = {"version": "1.2.3"}
+        semgrep.version.version_check(tmp_cache_path)
+        semgrep.version.version_check(tmp_cache_path)
 
     assert fetched_mock.call_count == 1
