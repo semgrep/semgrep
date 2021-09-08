a8e61cc6150c12950736c6099a5c77cd71db09a6
Emma Jin <emjin@users.noreply.github.com>
2021-09-07 17:03:18 -0700

    Add additional tests

--- semgrep/tests/situations_test/no_token_location.py
+++ semgrep/tests/situations_test/no_token_location.py
@@ -1,5 +1,7 @@
-from pydantic import BaseModel, validator
-from app.config import VALID_COMPANIES, ENVIRONMENT
+from app.config import ENVIRONMENT
+from app.config import VALID_COMPANIES
+from pydantic import BaseModel
+from pydantic import validator
 
 
 class Company(BaseModel):
@@ -7,7 +9,8 @@ class Company(BaseModel):
 
     @validator("company")
     def check_valid_company(cls, v):
-        if v not in VALID_COMPANIES and ENVIRONMENT != "test":  # dont validate companies if in test env
+        if (
+            v not in VALID_COMPANIES and ENVIRONMENT != "test"
+        ):  # dont validate companies if in test env
             raise Exception(f"Invalid company: {v}")
         return v
-
