86335cf4f1f885bf5a4f99d0715a62e5258ad8f6
brendon <brendon.go@gmail.com>
2021-09-09 21:43:33 -0700

    Demoable

--- semgrep/semgrep/error.py
+++ semgrep/semgrep/error.py
@@ -89,7 +89,7 @@ class SemgrepCoreError(SemgrepError):
     def to_dict_base(self) -> Dict[str, Any]:
         return {
             "type": self.error_type,
-            "message": self.message,
+            "message": f"{self.error_type}: {self.message}",
         }
 
     def __str__(self) -> str:
