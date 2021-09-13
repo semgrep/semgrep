d0645080b5a3eeed3e2dd8226399c2b7f23c36bc
Matthew McQuaid <matthew@returntocorp.com>
2021-09-13 16:10:04 -0700

    break test in php

---
+++ semgrep-core/tests/tainting_rules/php/break.php
@@ -0,0 +1,14 @@
+<?php
+$arr = array(1,2,3,4,5);
+foreach ($arr as $i) {
+    $bad = "ok";
+    foreach ($arr as $j) {
+        foreach ($arr as $k) {
+            $bad = $source;
+            if ($val == 2) {
+                break 2; 
+            }
+        }
+    }
+    sink($bad);
+}
