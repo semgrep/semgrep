d725224a9cd07ea3d4e4df2d4b298d970e21645f
pad <yoann.padioleau@gmail.com>
2021-09-20 13:10:41 +0200

    [HCL] alpha tests
    Not all alpha tests apply, so I've ported the one that make sense.
    
    This closes PA-280
    
    test plan:
    make test

---
+++ semgrep-core/tests/terraform/metavar_call.tf
@@ -0,0 +1,6 @@
+resource "aws_ebs_volume" "web_host_storage" {
+    #ERROR:
+    a = foo(1,2)
+
+    
+}
---
+++ semgrep-core/tests/terraform/metavar_equality_var.tf
@@ -0,0 +1,6 @@
+#ERROR:
+resource "aws_ebs_volume" "web_host_storage" {
+  myfile = open()
+  
+  x = close(myfile)
+}
