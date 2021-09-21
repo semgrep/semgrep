6f0f545d1f960589fa85d627323b2c3a9d062312
Nathan Brahms <nathan@returntocorp.com>
2021-09-21 14:53:19 -0700

    Link to PRIVACY.md in first-run notification

--- semgrep/semgrep/notifications.py
+++ semgrep/semgrep/notifications.py
@@ -22,6 +22,8 @@ def possibly_notify_user() -> None:
                 "NOTICE: Semgrep may send pseudonymous usage metrics to its server."
                 "\nBy default, Semgrep will only send usage metrics when rules are pulled from Semgrep's servers."
                 "\nTo disable metrics collection, run Semgrep with --metrics off."
+                "\n"
+                "\nFor more information, please see github.com/returntocorp/PRIVACY.md."
                 "\n",
             )
         )
