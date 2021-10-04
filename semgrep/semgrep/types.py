32872da26a8ae0ff4ee61c523b0a6fbae60da132
Emma Jin <emjin@users.noreply.github.com>
2021-10-04 17:30:15 -0400

    Remove unused type

--- semgrep/semgrep/types.py
+++ semgrep/semgrep/types.py
@@ -3,18 +3,10 @@ from enum import Enum
 from typing import Any
 from typing import Mapping
 from typing import NewType
-from typing import Sequence
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from semgrep.rule import Rule
-    from semgrep.rule_match import RuleMatch
 
 JsonObject = Mapping[str, Any]
 
 RuleId = NewType("RuleId", str)
-
-RuleMatchMap = Mapping["Rule", Sequence["RuleMatch"]]
 
 
 class MetricsState(Enum):
