81cc627474fdf0ca8f61ca1e854a54bb89483019
Iago Abal <mail@iagoabal.eu>
2021-10-04 22:01:56 +0200

    tests: Check that we still find matches with a $F(...) sanitizer (#3977)
    test plan:
    make test

--- semgrep-core/tests/tainting_rules/php/opaque_safe.php
+++ semgrep-core/tests/tainting_rules/php/opaque_safe.php
@@ -4,6 +4,17 @@ function not_tainted($data) {
     // do stuff
     return '2';
 }
-x = not_tainted(tainted('a'));
-// ok:tainted
-sink(x);
+
+//OK:
+sink(not_tainted(tainted('a')));
+
+$ok = not_tainted(tainted('a'));
+//OK:
+sink($ok);
+
+//ERROR:
+sink(tainted('b'));
+
+$bad = tainted('b');
+//ERROR:
+sink($bad);
