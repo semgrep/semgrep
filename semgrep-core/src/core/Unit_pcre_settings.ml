d343af2ea8a3c27e8e809531e88f79756a520fb6
Martin Jambon <martin@r2c.dev>
2021-10-05 15:34:48 -0700

    Add tests specifically for pcre match limit

--- semgrep-core/src/core/Unit_pcre_settings.ml
+++ semgrep-core/src/core/Unit_pcre_settings.ml
@@ -2,17 +2,35 @@
    Unit tests for Pcre_settings
 *)
 
+let test_match_limit_ok () =
+  let rex = Pcre_settings.regexp "(a+)+$" in
+  try ignore (Pcre.pmatch ~rex "aaaaaaaaaaaaaaaaaaa!")
+  with Pcre.Error Pcre.MatchLimit ->
+    Alcotest.fail "should not have failed with error MatchLimit"
+
+let test_match_limit_fail () =
+  let rex = Pcre_settings.regexp "(a+)+$" in
+  try
+    ignore (Pcre.pmatch ~rex "aaaaaaaaaaaaaaaaaaaaaaaaaa!");
+    Alcotest.fail "should have failed with error MatchLimit"
+  with Pcre.Error Pcre.MatchLimit -> ()
+
 let test_register_exception_printer () =
   (* This is a little dirty since we can't undo it. *)
   Pcre_settings.register_exception_printer ();
 
   let msg =
-    let rex = Pcre_settings.regexp "(a+)+$" in
-    try Pcre.pmatch ~rex "aaaaaaaaaaaaaaaaaaaaaaaaaa!" |> assert false
+    try
+      ignore (Pcre_settings.regexp "???");
+      Alcotest.fail "should have failed to compile the regexp"
     with e -> Printexc.to_string e
   in
-  Alcotest.(check string) "equal" "Pcre.Error(MatchLimit)" msg
+  Alcotest.(check string) "equal" "TODO" msg
 
 let tests =
   Testutil.pack_tests "pcre settings"
-    [ ("exception printer", test_register_exception_printer) ]
+    [
+      ("match limit ok", test_match_limit_ok);
+      ("match limit fail", test_match_limit_fail);
+      ("exception printer", test_register_exception_printer);
+    ]
