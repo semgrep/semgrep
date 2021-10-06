e78bc038ebebd34073bf387c300abe4c21d4b1f7
Nathaniel Brahms <nbrahms@users.noreply.github.com>
2021-10-05 17:38:27 -0700

    Add script to hash project urls (#3997)
    * Add script to hash project urls
    
    For ease in calculating hashes of known (viz., public or r2c-internal)
    projects.
    
    * fixup! Add script to hash project urls

---
+++ scripts/hash-project.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+E.g.
+
+  pipenv run scripts/hash-project.py $(git ls-remote --get-url)
+"""
+from sys import argv
+
+from semgrep.metric_manager import metric_manager
+
+metric_manager.set_project_hash(argv[1])
+print(metric_manager._project_hash)
