bc8c8022c8076c16ec930c9500bbb9591b32073b
Matthew McQuaid <matthew@returntocorp.com>
2021-10-13 16:01:21 -0700

    more stuff in the metavar test

--- semgrep-core/tests/tainting_rules/js/metavar_eq.js
+++ semgrep-core/tests/tainting_rules/js/metavar_eq.js
@@ -1,6 +1,7 @@
 
 var source1 = get(A)
 var source2 = get(B)
+var source3 = get(C)
 
 //ERROR:
 sink(A,source1)
@@ -10,3 +11,9 @@ sink(B,source2)
 
 //OK:
 sink(A,source2)
+
+//OK:
+sink(A,source3)
+
+//OK:
+sink(B,source3)
