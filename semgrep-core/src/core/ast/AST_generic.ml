c310862afc0653dd2af1b9ca559f578b1c78e5b3
Martin Jambon <martin@r2c.dev>
2021-10-18 15:00:51 -0700

    Explain more about when to use 'id_hidden'

--- semgrep-core/src/core/ast/AST_generic.ml
+++ semgrep-core/src/core/ast/AST_generic.ml
@@ -346,8 +346,21 @@ and id_info = {
    *)
   id_constness : constness option ref; [@equal fun _a _b -> true]
   (* THINK: Drop option? *)
-  (* id_hidden=true must be set for any ordinary identifier that doesn't
-     appear in the source code but is introduced in the AST after parsing.
+  (* id_hidden=true must be set for any artificial identifier that never
+     appears in source code but is introduced in the AST after parsing.
+
+     Don't use this for syntax desugaring or transpilation because the
+     resulting function name might exist in some source code. Consider the
+     following normalization:
+
+       !foo -> foo.contents
+                   ^^^^^^^^
+                 should not be marked as hidden because it could appear
+                 in target source code.
+
+     However, an artificial identifier like "!sh_quoted_expand!" should
+     be marked as hidden in bash.
+
      This allows not breaking the -fast/-filter_irrelevant_rules optimization
      that skips a target file if some identifier in the pattern AST doesn't
      exist in the source of the target.
