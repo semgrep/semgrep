c1360119ebdba2c35dab37a4a46aa0f8890e681e
Martin Jambon <martin@r2c.dev>
2021-10-18 11:41:42 -0700

    Print rule ID when printing message about a skipped file Also change the json field name from 'skipped_rule' to 'rule_id'.

--- semgrep/semgrep/core_output.py
+++ semgrep/semgrep/core_output.py
@@ -182,7 +182,11 @@ class CoreSkipped:
         path = Path(raw_json["path"])
         reason = SkipReason(raw_json["reason"])
         details = SkipDetails(raw_json["details"])
-        logger.verbose(f"skipped '{path}': {reason}: {details}")
+        if raw_rule_id:
+            rule_info = f"rule {raw_rule_id}"
+        else:
+            rule_info = "all rules"
+        logger.verbose(f"skipped '{path}' [{rule_info}]: {reason}: {details}")
         return cls(rule_id, path, reason, details)
 
 
