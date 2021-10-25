641dc956b4e61e3ad78384ac0ebc124b068fd55c
Emma Jin <emjin@users.noreply.github.com>
2021-10-25 11:01:54 -0700

    Move parse methods without caching to Run_semgrep

--- semgrep-core/src/runner/Parse_with_caching.mli
+++ semgrep-core/src/runner/Parse_with_caching.mli
@@ -4,7 +4,3 @@ val parse_generic :
   Lang.t ->
   string (* file *) ->
   AST_generic.program * Semgrep_error_code.error list
-
-val parse_equivalences : string -> Equivalence.equivalences
-
-val parse_pattern : Lang.t -> string -> AST_generic.any
--- semgrep-core/src/runner/Run_semgrep.mli
+++ semgrep-core/src/runner/Run_semgrep.mli
@@ -1,5 +1,7 @@
 open Runner_common
 
+val parse_pattern : Lang.t -> string -> AST_generic.any
+
 val semgrep_with_patterns_file : config -> string list -> unit
 
 val semgrep_with_rules_file : config -> string list -> unit
