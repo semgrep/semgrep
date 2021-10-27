63ea7c32b6980fcd7f96439180c25181b77fff63
Matthew McQuaid <matthew@returntocorp.com>
2021-10-27 15:33:44 -0700

    replace metavar_eq_conditional with a conditional test that doesn't require checking one sink is matched twice

--- semgrep-core/tests/tainting_rules/js/metavar_eq_conditional.js
+++ semgrep-core/tests/tainting_rules/js/metavar_eq_conditional.js
@@ -1,23 +1,12 @@
 
 
-var source1 = cond ? get(A) : get(B)
+var source = cond ? get(A) : get(B)
 
 
-//Since source1 could be tainted with either $X = A or $X = B, both of these sinks should be tainted
-
-//ERROR:
-sink(A,source1)
+//Since source could be tainted with either $X = A or $X = B, both of these sinks should be tainted
 
 //ERROR:
-sink(B,source1)
+sink(A,source)
 
-
-var source2 = cond ? get(A) : get(f(A))
- 
-// Since a sink can either assign $X = A or $X = f(A), and source can be tainted from either of these
-// there are two possible ways that this expression could be considered a tainted sink.
-//ERROR: 
-sink(f(A),source2)
-
-//OK:
-sink(B,source2)
+//ERROR:
+sink(B,source)
