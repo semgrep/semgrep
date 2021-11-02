7ead4a0f7bf26f7f85e1e1940ee38f4c13e0d4b4
Yoann Padioleau <pad@returntocorp.com>
2021-11-02 15:23:10 +0100

    [C++] test files for partial parsing checking (#4200)
    This ensures some C++ files were actually partially
    parsed by tree-sitter.
    
    test plan:
    make test

--- semgrep-core/src/parsing/Parse_target.mli
+++ semgrep-core/src/parsing/Parse_target.mli
@@ -1,11 +1,17 @@
 type parsing_result = {
   ast : AST_generic.program;
+  (* Partial errors while parsing the file (tree-sitter only) *)
   errors : Semgrep_error_code.error list;
   stat : Parse_info.parsing_stat;
 }
 
-(* This uses either the pfff or tree-sitter parsers.
+(* This uses a pfff or tree-sitter parser, or both.
  * This also resolve names and propagate constants.
+ *
+ * Note that the errors in parsing_result are partial errors
+ * that tree-sitter was able to recover from. For other parse errors,
+ * the function below will actually raise an exception
+ * (e.g. Parse_info.Pasing_error).
  *)
 val parse_and_resolve_name_use_pfff_or_treesitter :
   Lang.t -> Common.filename -> parsing_result
