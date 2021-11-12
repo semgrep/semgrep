c9e4a1694f91b4d0042645b5b28c566096aac739
Iago Abal <mail@iagoabal.eu>
2021-11-12 15:41:09 +0100

    Release 0.73.0

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ from setuptools import setup
 setup(
     name="semgrep_pre_commit_package",
     version="0.0.0",
-    install_requires=["semgrep==0.72.0"],
+    install_requires=["semgrep==0.73.0"],
 )
