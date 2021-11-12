d57b0b35f3594ae5f1666931073ab8424ce3975d
Iago Abal <mail@iagoabal.eu>
2021-11-12 17:12:35 +0100

    analyzing: cfg: Fix `try` blocks ending in `return` (#4243)
    * analyzing: cfg: Fix `try` blocks ending in `return`
    
    When building the CFG of a `try-catch` we connect the end of the
    try-block to the beginning of the catch-block. But, if the CFG of the
    try-block ended in `return`, this did not happen, and the catch-block
    became dead code, despite something in the try-block could throw an
    exception.
    
    Closes #4091
    
    test plan:
    make test # tests included
    
    * Yoann's review
    
    * Fix changelog after rebase

---
+++ semgrep-core/tests/tainting_rules/java/try_return.java
@@ -0,0 +1,30 @@
+// https://github.com/returntocorp/semgrep/issues/4091
+// We want to test that the try-block is properly connected to the
+// catch-block despite the try-block execution ends in a `return`.
+public class Main {
+    public void test1() {
+        String str = "safe";
+        try {
+            return could_throw();
+        }
+        catch (Throwable t) {
+            str = source();
+        }
+        //ERROR:
+        sink(str);
+    }
+
+    public void test2() {
+        int cannot_throw = 42;
+        String str = "safe";
+        try {
+            return cannot_throw;
+        }
+        // vvv dead code
+        catch (Throwable t) {
+            str = source();
+        }
+        //OK:
+        sink(str);
+    }
+}
