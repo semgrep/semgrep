semgrep/semgrep

commit 74aeae0aa46270336a27b9415b1632c8ebdd11df

Revert "Revert "feat(timing): profile config resolution and nosem handling timing"" (#4313)
* Revert "Revert "feat(timing): profile config resolution and nosem handling timing (#4292)" (#4312)"

This reverts commit 7f5ac6f20568be1896bff9067911e240ef860709.

* fixup! Revert "Revert "feat(timing): profile config resolution and nosem handling timing (#4292)" (#4312)"

semgrep/semgrep/output.py
@@ -109,7 +109,7 @@ def _build_time_json(
     time_info["rule_parse_info"] = [
         profiling_data.get_rule_parse_time(rule) for rule in rules
     ]
-    time_info["total_time"] = profiler.calls["total_time"][0] if profiler else -1.0
+    time_info["profiling_times"] = profiler.dump_stats() if profiler else {}
     target_bytes = [Path(str(target)).resolve().stat().st_size for target in targets]
     time_info["targets"] = [
         _build_time_target_json(rules, target, num_bytes, profiling_data)

semgrep/semgrep/metric_manager.py
@@ -44,7 +44,7 @@ class _MetricManager:
         self._num_targets: Optional[int] = None
         self._num_findings: Optional[int] = None
         self._num_ignored: Optional[int] = None
-        self._run_time: Optional[float] = None
+        self._profiling_times: Dict[str, float] = {}
         self._total_bytes_scanned: Optional[int] = None
         self._errors: List[str] = []
         self._file_stats: List[Dict[str, Any]] = []
@@ -137,8 +137,8 @@ class _MetricManager:
     def set_num_ignored(self, num_ignored: int) -> None:
         self._num_ignored = num_ignored
 
-    def set_run_time(self, run_time: float) -> None:
-        self._run_time = run_time
+    def set_profiling_times(self, profiling_times: Dict[str, float]) -> None:
+        self._profiling_times = profiling_times
 
     def set_total_bytes_scanned(self, total_bytes_scanned: int) -> None:
         self._total_bytes_scanned = total_bytes_scanned
@@ -197,7 +197,7 @@ class _MetricManager:
             "performance": {
                 "fileStats": self._file_stats,
                 "ruleStats": self._rule_stats,
-                "runTime": self._run_time,
+                "profilingTimes": self._profiling_times,
                 "numRules": self._num_rules,
                 "numTargets": self._num_targets,
                 "totalBytesScanned": self._total_bytes_scanned,

semgrep/semgrep/profile_manager.py
@@ -1,8 +1,6 @@
-import collections
 import time
 from typing import Any
 from typing import Dict
-from typing import List
 
 
 class ProfileManager:
@@ -10,18 +8,12 @@ class ProfileManager:
     An incredibly rudimentary tool for tracking profiled calls with an associated named key
     """
 
-    calls: Dict[str, List[float]] = collections.defaultdict(list)
-
-    def track(self, key: str, callable: Any, *args: Any, **kwargs: Any) -> Any:
-        start_time = time.time()
-        result = callable(*args, **kwargs)
-        self.calls[key].append(time.time() - start_time)
-        return result
+    calls: Dict[str, float] = {}
 
     # This method is an even more rudimentary tool for profiling
     # function calls
     def save(self, key: str, start_time: float) -> Any:
-        self.calls[key].append(time.time() - start_time)
+        self.calls[key] = time.time() - start_time
 
-    def dump_stats(self) -> Dict[str, List[float]]:
-        return dict(sorted(self.calls.items(), key=lambda x: x[1]))
+    def dump_stats(self) -> Dict[str, float]:
+        return self.calls

semgrep/semgrep/semgrep_main.py
@@ -125,10 +125,15 @@ def main(
         exclude = []
 
     project_url = get_project_url()
+
+    profiler = ProfileManager()
+
+    rule_start_time = time.time()
     configs_obj, errors = get_config(
         pattern, lang, configs, replacement=replacement, project_url=project_url
     )
     all_rules = configs_obj.get_rules(no_rewrite_rule_ids)
+    profiler.save("config_time", rule_start_time)
 
     if not severity:
         shown_severities = DEFAULT_SHOWN_SEVERITIES
@@ -181,15 +186,13 @@ def main(
         skip_unknown_extensions=skip_unknown_extensions,
     )
 
-    profiler = ProfileManager()
-
     join_rules, rest_of_the_rules = partition(
         lambda rule: rule.mode == JOIN_MODE,
         filtered_rules,
     )
     filtered_rules = rest_of_the_rules
 
-    start_time = time.time()
+    core_start_time = time.time()
     # actually invoke semgrep
     (
         rule_matches_by_rule,
@@ -218,21 +221,23 @@ def main(
             rule_matches_by_rule.update(join_rule_matches_by_rule)
             output_handler.handle_semgrep_errors(join_rule_errors)
 
-    profiler.save("total_time", start_time)
+    profiler.save("core_time", core_start_time)
 
+    ignores_start_time = time.time()
     filtered_matches_by_rule, nosem_errors, num_ignored_by_nosem = process_ignores(
         rule_matches_by_rule, output_handler, strict=strict, disable_nosem=disable_nosem
     )
+    profiler.save("ignores_time", ignores_start_time)
 
     output_handler.handle_semgrep_errors(semgrep_errors)
     output_handler.handle_semgrep_errors(nosem_errors)
 
     num_findings = sum(len(v) for v in filtered_matches_by_rule.values())
     stats_line = f"ran {len(filtered_rules)} rules on {len(all_targets)} files: {num_findings} findings"
-
-    error_types = list(e.semgrep_error_type() for e in semgrep_errors)
-
+    profiler.save("total_time", rule_start_time)
     if metric_manager.is_enabled():
+        error_types = list(e.semgrep_error_type() for e in semgrep_errors)
+
         metric_manager.set_project_hash(project_url)
         metric_manager.set_configs_hash(configs)
         metric_manager.set_rules_hash(filtered_rules)
@@ -240,7 +245,7 @@ def main(
         metric_manager.set_num_targets(len(all_targets))
         metric_manager.set_num_findings(num_findings)
         metric_manager.set_num_ignored(num_ignored_by_nosem)
-        metric_manager.set_run_time(profiler.calls["total_time"][0])
+        metric_manager.set_profiling_times(profiler.dump_stats())
         total_bytes_scanned = sum(t.stat().st_size for t in all_targets)
         metric_manager.set_total_bytes_scanned(total_bytes_scanned)
         metric_manager.set_errors(error_types)

commit 2093ea32dc6dfb6fc2cc4a4baf8d276c812b4ac3

[TestInfra] add test infrastructure to check for maturity level (#4315)
* [TestInfra] add test infrastructure to check for maturity level

This is more formal support to actually check whether a certain
language meet a certain maturity level for the tests.

This closes https://github.com/returntocorp/semgrep/issues/4300

test plan:
test file included
make test

* Fix CI failures on dummy-benchmarks

This fix a regression introduced by
https://github.com/returntocorp/semgrep/pull/4313
It was not caught at that time because the circle CI job
use the develop image to get the stats and this develop was still
the old one at the moment of the offending PR, so we got the
error at the following PR

test plan:
$ ./run-benchmarks --dummy --upload
now works

* comment

perf/run-benchmarks
@@ -125,10 +125,12 @@ def upload_result(
     if variant_name == STD and timings is not None:
         # Compute bps from semgrep timing data
         assert "rules" in timings
-        assert "total_time" in timings
+        assert "profiling_times" in timings
         assert "total_bytes" in timings
+        prof_times = timings["profiling_times"]
+        assert "total_time" in prof_times
         num_rules = len(timings["rules"])
-        bps = timings["total_bytes"] / (timings["total_time"] * num_rules)
+        bps = timings["total_bytes"] / (prof_times["total_time"] * num_rules)
 
         bps_url = f"{DASHBOARD_URL}/api/metric/{BPS_ENDPOINT}"
         upload(bps_url, str(bps))
@@ -143,7 +145,7 @@ def upload_result(
                     num_lines += sum(1 for _ in f)
                 except UnicodeDecodeError:
                     pass
-        lps = num_lines / (timings["total_time"] * num_rules)
+        lps = num_lines / (prof_times["total_time"] * num_rules)
 
         lps_url = f"{DASHBOARD_URL}/api/metric/{LPS_ENDPOINT}"
         upload(lps_url, str(lps))

semgrep-core/src/core/ast/Lang.ml
@@ -65,7 +65,7 @@ type t =
   | HTML
 
 [@@ocamlformat "disable"]
-[@@deriving show, eq]
+[@@deriving show { with_path = false }, eq]
 
 let is_js = function
   | Javascript

semgrep/semgrep/output.py
@@ -88,6 +88,10 @@ def _build_time_target_json(
     return target_json
 
 
+# coupling: if you change the JSON schema below, you probably need to
+# also modify perf/run-benchmarks. Run locally
+#    $ ./run-benchmarks --dummy --upload
+# to double check everything still works
 def _build_time_json(
     rules: List[Rule],
     targets: Set[Path],

semgrep-core/tests/Test.ml
@@ -10,7 +10,14 @@ let logger = Logging.get_logger [ __MODULE__ ]
 (*****************************************************************************)
 (* Purpose *)
 (*****************************************************************************)
-(* Unit tests entry point *)
+(* Unit tests entry point.
+ *
+ * From semgrep-core you can do
+ *
+ *   $./test test foo
+ *
+ * to run all the tests containing foo in their description.
+ *)
 
 (*****************************************************************************)
 (* Constants *)
@@ -19,6 +26,107 @@ let logger = Logging.get_logger [ __MODULE__ ]
 (* ran from _build/default/tests/ hence the '..'s below *)
 let tests_path = "../../../tests"
 let data_path = "../../../data"
+
+(* coupling: https://semgrep.dev/docs/language-support/
+ * See also https://r2c.quip.com/FOAuA4ThzULc/How-to-promote-a-language-
+*)
+type maturity_level = GA | Beta | Experimental
+[@@deriving show { with_path = false }]
+
+(* coupling: 
+ * https://semgrep.dev/docs/language-support/#maturity-definitions 
+ * ../../scripts/generate-cheatsheet.py
+*)
+let experimental_features = [
+    "concrete_syntax";
+    "deep_exprstmt";
+    "dots_args";
+    "dots_nested_stmts";
+    "dots_stmts";
+    "dots_string";
+    "metavar_arg";
+    "metavar_call";
+    "metavar_equality_var"
+    (* TODO: add dots_params? *)
+]
+
+let beta_features = experimental_features @ [
+    "metavar_class_def";
+    "metavar_func_def";
+    "metavar_cond";
+    "metavar_equality_expr";
+    "metavar_equality_stmt";
+    "metavar_import";
+    "metavar_stmt";
+]
+
+let ga_features = beta_features @ [
+    "deep_expr_operator";
+    "dots_method_chaining";
+    "equivalence_constant_propagation";
+    "equivalence_eq";
+    "equivalence_naming_import";
+    "metavar_anno";
+    "metavar_key_value";
+    "metavar_typed";
+    "regexp_string";
+]
+
+let assoc_maturity_level = [
+    GA, ga_features;
+    Beta, beta_features;
+    Experimental, experimental_features
+]
+
+(* coupling: ../../scripts/generate_cheatsheet.py LANGUAGE_EXCEPTIONS
+ * Note that for some languages, e.g., JSON, certain tests do not
+ * apply (NA), hence the exceptions listed above.
+ * For others, we should really add the test and/or corresponding feature.
+*)
+let language_exceptions = [
+    (* GA languages *)
+
+    (* TODO: NA for Java? *)
+    Lang.Java, 
+    ["equivalence_naming_import"; "metavar_key_value"];
+    (* TODO: why not metavar_typed? regexp_string? NA for naming_import? *)
+    Lang.Csharp, 
+    ["equivalence_naming_import"; "metavar_typed"; "regexp_string"];
+    (* TODO: metavar_anno sounds like an NA, but the other?? *)
+    Lang.Go, 
+    ["metavar_class_def"; "metavar_import"; "metavar_anno"];
+    (* metavar_typed is NA (dynamic language) *)
+    Lang.Javascript, 
+    ["equivalence_naming_import"; "metavar_typed";];
+    Lang.Typescript, 
+    ["equivalence_naming_import"; "metavar_typed";"metavar_anno";"metavar_class_def"];
+    (* good boy, metavar_typed is working just for constants though *)
+    Lang.Python, [];
+    (* metavar_typed is NA (dynamic language), metavar_anno also NA? *)
+    Lang.Ruby, 
+    ["equivalence_naming_import"; "metavar_typed";"metavar_anno"];
+
+    (* Beta languages *)
+
+    (* TODO: to fix *)
+    Lang.Kotlin, 
+    ["dots_stmts"; "metavar_equality_var"];
+
+    (* Experimental languages *)
+
+    (* TODO: dots_nested_stmts to fix for C and C++ *)
+    Lang.C,
+    ["dots_nested_stmts"];
+    Lang.Cplusplus,
+    ["dots_nested_stmts"];
+    (* good boy *)
+    Lang.Lua, [];
+    (* dots_stmts is maybe NA, same with deep_exprstmt *)
+    Lang.OCaml, 
+    ["deep_exprstmt";"dots_stmts"];
+    (* good boy *)
+    Lang.PHP, [];
+]
 
 (*****************************************************************************)
 (* Helpers *)
@@ -430,7 +538,72 @@ let filter_irrelevant_rules_tests =
     )
   )
 
-(*                 *)
+let maturity_tests =
+  (* TODO: infer dir and ext from lang using Lang helper functions *)
+  let check_maturity lang dir ext maturity =
+    pack_tests (spf "Maturity %s for %s" 
+        (show_maturity_level maturity)
+        (Lang.show lang)) (
+      let dir = Filename.concat tests_path dir in
+      let features = 
+        assoc_maturity_level |> List.assoc maturity in
+      let exns = 
+        try
+          List.assoc lang language_exceptions 
+        with Not_found -> []
+      in
+      let features = Common2.minus_set features exns in
+      features |> List.map (fun base ->
+         base, (fun () ->
+           let path = Filename.concat dir (base ^ ext) in
+           (* if it's a does-not-apply (NA) case, consider adding it
+            * to language_exceptions above
+            *)
+           if not (Sys.file_exists path)
+           then failwith (spf "missing test file %s for maturity %s" 
+                  path (show_maturity_level maturity))
+         )
+      )
+     )
+  in
+  (* coupling: https://semgrep.dev/docs/language-support/ *)
+  pack_suites "Maturity level testing" [
+    (* GA *)
+    check_maturity Lang.Csharp "csharp" ".cs" GA;
+    check_maturity Lang.Go "go" ".go" GA;
+    check_maturity Lang.Java "java" ".java" GA;
+    check_maturity Lang.Javascript "js" ".js" GA;
+    (* JSON has too many NA, not worth it *)
+    check_maturity Lang.Python "python" ".py" GA;
+    check_maturity Lang.Ruby "ruby" ".rb" GA;
+    check_maturity Lang.Typescript "ts" ".ts" GA;
+
+    (* Beta *)
+    check_maturity Lang.Hack "hack" ".hack" Beta;
+    check_maturity Lang.Kotlin "kotlin" ".kt" Beta;
+    (* Terraform/HCL has too many NA, not worth it *)
+
+    (* Experimental *)
+    check_maturity Lang.Bash "bash" ".bash" Experimental;
+    check_maturity Lang.C "c" ".c" Experimental;
+    check_maturity Lang.Cplusplus "cpp" ".cpp" Experimental;
+    check_maturity Lang.Lua "lua" ".lua" Experimental;
+    check_maturity Lang.OCaml "ocaml" ".ml" Experimental;
+    check_maturity Lang.PHP "php" ".php" Experimental;
+    (* TODO we say we support R, but not really actually *)
+    (* TODO: too many exns, we need to write tests!
+     check_maturity Lang.Rust "rust" ".rust" Experimental;
+    *)
+    check_maturity Lang.Scala "scala" ".scala" Experimental;
+    (* YAML has too many NA, not worth it *)
+
+    (* Not even experimental *)
+    (* R, HTML, Vue *)
+  ]
+
+(*****************************************************************************)
+(* All tests *)
+(*****************************************************************************)
 
 let tests = List.flatten [
   (* just expression vs expression testing for one language (Python) *)
@@ -458,6 +631,7 @@ let tests = List.flatten [
   metachecker_checks_tests;
   metachecker_regression_tests;
   filter_irrelevant_rules_tests;
+  maturity_tests;
 ]
 
 (*****************************************************************************)

semgrep-core/tests/dune
@@ -33,4 +33,8 @@
     semgrep_engine
     semgrep_system
   )
+ (preprocess
+   (pps
+      ppx_deriving.show
+   ))
 )

semgrep-core/tests/ts/dots_method_chaining.ts
@@ -1,0 +1,10 @@
+//ERROR: match
+f = o.foo().m().h().bar().z();
+
+//ERROR: match
+f = o.foo().bar();
+
+f = o.foo().m().h().z();
+
+//ERROR: match $O can match o.before()
+f = o.before().foo().m().h().bar().z();