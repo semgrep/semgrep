semgrep/semgrep

commit 20cd8a784b857d8aa818104b3ce874a204e1694b

Add support for Break and Continue in IL (#3925)
* initial support for break/continue in IL

* break test in php

* break/continue tests

* tests, and an explanatory comment

* changelog

* changelog

* add back two unreleased sections to changelog

* fix some typos

* rewording, replace todo with impossible

* add dummy nodes to unassigned labels

* explanatory comment

* delete old dummy stmt comment

semgrep-core/src/analyzing/CFG_build.ml
@@ -216,7 +216,7 @@ and cfg_todo state previ stmt =
   CfgFirstLast (newi, Some newi)
 
 and cfg_stmt_list state previ xs =
-  let lasti_opt =
+  let lasti_opt, labels =
     xs
     |> List.fold_left
          (fun (previ, labels) stmt ->
@@ -229,9 +229,17 @@ and cfg_stmt_list state previ xs =
                (lasti, [])
            | CfgLabel label -> (previ, label :: labels))
          (previ, [])
-    |> fst
   in
-  lasti_opt
+  match (lasti_opt, labels) with
+  | Some lasti, l :: ls ->
+      (* If we had labels at the end of our stmt list, we create a dummy node to assign them to,
+         and connect it to the last node we looked at
+      *)
+      let dummyi = state.g#add_node { n = NOther Noop } in
+      label_node state (l :: ls) dummyi;
+      add_arc (lasti, dummyi) state.g;
+      lasti_opt
+  | _ -> lasti_opt
 
 (*****************************************************************************)
 (* Main entry point *)

semgrep-core/src/analyzing/AST_to_IL.ml
@@ -39,9 +39,16 @@ type env = {
    * usually simple Instr, but can be also If when handling Conditional expr.
    *)
   stmts : stmt list ref;
+  (* When entering a loop, we create two labels, one to jump to if a Continue stmt is found
+     and another to jump to if a Break stmt is found. Since PHP supports breaking an arbitrary
+     number of loops up, we keep a stack of break labels instead of just one
+  *)
+  break_labels : label list;
+  cont_label : label option;
 }
 
-let empty_env lang = { lang; stmts = ref [] }
+let empty_env lang =
+  { stmts = ref []; break_labels = []; cont_label = None; lang }
 
 (*****************************************************************************)
 (* Error management *)
@@ -99,7 +106,7 @@ let fresh_var _env tok =
   let i = H.gensym () in
   (("_tmp", tok), i)
 
-let _fresh_label _env tok =
+let fresh_label _env tok =
   let i = H.gensym () in
   (("_label", tok), i)
 
@@ -711,6 +718,21 @@ let parameters _env params =
 (*****************************************************************************)
 (* Statement *)
 (*****************************************************************************)
+
+let mk_break_continue_labels env tok =
+  let cont_label = fresh_label env tok in
+  let break_label = fresh_label env tok in
+  let st_env =
+    {
+      env with
+      break_labels = break_label :: env.break_labels;
+      cont_label = Some cont_label;
+    }
+  in
+  let cont_label_s = [ mk_s (Label cont_label) ] in
+  let break_label_s = [ mk_s (Label break_label) ] in
+  (cont_label_s, break_label_s, st_env)
+
 let rec stmt_aux env st =
   match st.G.s with
   | G.ExprStmt (e, _) ->
@@ -733,16 +755,27 @@ let rec stmt_aux env st =
       ss @ [ mk_s (If (tok, e', st1, st2)) ]
   | G.Switch (_, _, _) -> todo (G.S st)
   | G.While (tok, e, st) ->
+      let cont_label_s, break_label_s, st_env =
+        mk_break_continue_labels env tok
+      in
       let ss, e' = expr_with_pre_stmts env e in
-      let st = stmt env st in
-      ss @ [ mk_s (Loop (tok, e', st @ ss)) ]
+      let st = stmt st_env st in
+      ss @ [ mk_s (Loop (tok, e', st @ cont_label_s @ ss)) ] @ break_label_s
   | G.DoWhile (tok, st, e) ->
-      let st = stmt env st in
+      let cont_label_s, break_label_s, st_env =
+        mk_break_continue_labels env tok
+      in
+      let st = stmt st_env st in
       let ss, e' = expr_with_pre_stmts env e in
-      st @ ss @ [ mk_s (Loop (tok, e', st @ ss)) ]
+      st @ ss
+      @ [ mk_s (Loop (tok, e', st @ cont_label_s @ ss)) ]
+      @ break_label_s
   | G.For (tok, G.ForEach (pat, tok2, e), st) ->
+      let cont_label_s, break_label_s, st_env =
+        mk_break_continue_labels env tok
+      in
       let ss, e' = expr_with_pre_stmts env e in
-      let st = stmt env st in
+      let st = stmt st_env st in
 
       let next_lval = fresh_lval env tok2 in
       let hasnext_lval = fresh_lval env tok2 in
@@ -774,11 +807,16 @@ let rec stmt_aux env st =
             (Loop
                ( tok,
                  cond,
-                 [ next_call ] @ assign @ st @ [ (* ss @ ?*) hasnext_call ] ));
+                 [ next_call ] @ assign @ st @ cont_label_s
+                 @ [ (* ss @ ?*) hasnext_call ] ));
         ]
+      @ break_label_s
   | G.For (tok, G.ForClassic (xs, eopt1, eopt2), st) ->
+      let cont_label_s, break_label_s, st_env =
+        mk_break_continue_labels env tok
+      in
       let ss1 = for_var_or_expr_list env xs in
-      let st = stmt env st in
+      let st = stmt st_env st in
       let ss2, cond =
         match eopt1 with
         | None ->
@@ -793,17 +831,43 @@ let rec stmt_aux env st =
             let ss, _eIGNORE = expr_with_pre_stmts env e in
             ss
       in
-      ss1 @ ss2 @ [ mk_s (Loop (tok, cond, st @ next @ ss2)) ]
+      ss1 @ ss2
+      @ [ mk_s (Loop (tok, cond, st @ cont_label_s @ next @ ss2)) ]
+      @ break_label_s
   | G.For (_, G.ForEllipsis _, _) -> sgrep_construct (G.S st)
   | G.For (tok, G.ForIn (xs, e), st) ->
+      let cont_label_s, break_label_s, st_env =
+        mk_break_continue_labels env tok
+      in
       let ss1 = for_var_or_expr_list env xs in
-      let st = stmt env st in
+      let st = stmt st_env st in
       let ss2, cond = expr_with_pre_stmts env (List.nth e 0) (* TODO list *) in
-      ss1 @ ss2 @ [ mk_s (Loop (tok, cond, st @ ss2)) ]
+      ss1 @ ss2
+      @ [ mk_s (Loop (tok, cond, st @ cont_label_s @ ss2)) ]
+      @ break_label_s
   (* TODO: repeat env work of controlflow_build.ml *)
-  | G.Continue _
-  | G.Break _ ->
-      todo (G.S st)
+  | G.Continue (tok, lbl_ident, _) -> (
+      match lbl_ident with
+      | G.LNone -> (
+          match env.cont_label with
+          | None -> impossible (G.Tk tok)
+          | Some lbl -> [ mk_s (Goto (tok, lbl)) ])
+      | G.LId lbl -> [ mk_s (Goto (tok, label_of_label env lbl)) ]
+      | G.LInt _
+      | G.LDynamic _ ->
+          todo (G.S st))
+  | G.Break (tok, lbl_ident, _) -> (
+      match lbl_ident with
+      | G.LNone -> (
+          match env.break_labels with
+          | [] -> impossible (G.Tk tok)
+          | lbl :: _ -> [ mk_s (Goto (tok, lbl)) ])
+      | G.LId lbl -> [ mk_s (Goto (tok, label_of_label env lbl)) ]
+      | G.LInt (i, _) -> (
+          match List.nth_opt env.break_labels i with
+          | None -> impossible (G.Tk tok)
+          | Some lbl -> [ mk_s (Goto (tok, lbl)) ])
+      | G.LDynamic _ -> impossible (G.Tk tok))
   | G.Label (lbl, st) ->
       let lbl = label_of_label env lbl in
       let st = stmt env st in

semgrep-core/src/core/il/IL.ml
@@ -280,6 +280,7 @@ and other_stmt =
   (* everything except VarDef (which is transformed in a Set instr) *)
   | DefStmt of G.definition
   | DirectiveStmt of G.directive
+  | Noop
 
 and label = ident * G.sid [@@deriving show { with_path = false }]
 

semgrep-core/tests/tainting_rules/go/continue.go
@@ -1,0 +1,11 @@
+func f() {
+	i := 0
+	for i < 10 {
+		i := i + 1
+		continue
+		//OK:
+		sink(source)
+	}
+	//ERROR:
+	sink(source)
+}

semgrep-core/tests/tainting_rules/go/continue.yaml
@@ -1,0 +1,12 @@
+rules:
+  - id: test-continue
+    languages:
+      - go
+    message: Match Found!
+    mode: taint
+    pattern-sinks:
+      - pattern: sink(...)
+    pattern-sources:
+      - pattern: source
+    severity: WARNING
+

semgrep-core/tests/tainting_rules/php/break.php
@@ -1,0 +1,20 @@
+<?php
+
+function f() {
+    $arr = array(1,2,3,4,5);
+    foreach ($arr as $i) {
+        $bad = "ok";
+        foreach ($arr as $j) {
+            foreach ($arr as $k) {
+                $bad = $source;
+                if ($val == 2) {
+                    break 2; 
+                    //OK:
+                    sink($source);
+                }
+            }
+        }
+        //ERROR:
+        sink($bad);
+    }
+}

semgrep-core/tests/tainting_rules/php/break.yaml
@@ -1,0 +1,12 @@
+rules:
+  - id: test-multi-break
+    languages:
+      - php
+    message: Match Found!
+    mode: taint
+    pattern-sinks:
+      - pattern: sink(...)
+    pattern-sources:
+      - pattern: $source
+    severity: WARNING
+

semgrep-core/tests/tainting_rules/python/break.py
@@ -1,0 +1,10 @@
+def f():
+    i = 0
+    while (i < 0):
+        if i == 4:
+            #ERROR:
+            sink(source)
+            break
+            #OK:
+            sink(source)
+        i += 1

commit b624783c80b5fcb9f955e54afcc1cdfb3be1e7d6

analyzing: CFG_build: Make sure labels are resolved
We did not create a dummy node to apply pending labels to when the
preceeding statement exited the function. We must create the dummy node
and label it, otherwise some `goto` may be left unresolved.

This also fixes the "Could not resolve label: _label" warnings that we
had been observing for a while.

Fixes: 20cd8a784b8 ("Add support for Break and Continue in IL (#3925)")

Closes #4265

test plan:
make test # test included

semgrep-core/src/analyzing/CFG_build.ml
@@ -261,14 +261,28 @@ and cfg_stmt_list state previ xs =
            | CfgLabel label -> (previ, label :: labels))
          (previ, [])
   in
-  match (lasti_opt, labels) with
-  | Some lasti, l :: ls ->
-      (* If we had labels at the end of our stmt list, we create a dummy node to assign them to,
-         and connect it to the last node we looked at
-      *)
+  match labels with
+  | l :: ls ->
+      (* If we have labels at the end of our list of stmt, we create a dummy
+       * node to assign them to.
+       *
+       *     lasti -> label: dummy
+       *
+       * This happens when there are labels at the end of a function's body:
+       *
+       *     void foo(x)
+       *     {
+       *       if (x > 0) goto label;
+       *       bar();
+       *       label:
+       *     }
+       *
+       * Such labels may be in the original sources, or they may be introduced
+       * by the AST-to-IL translation.
+       *)
       let dummyi = state.g#add_node { n = NOther (Noop "return") } in
       label_node state (l :: ls) dummyi;
-      add_arc (lasti, dummyi) state.g;
+      state.g |> add_arc_from_opt (lasti_opt, dummyi);
       Some dummyi
   | _ -> lasti_opt
 

semgrep-core/tests/java/cp_switch_throw.java
@@ -1,0 +1,28 @@
+// https://github.com/returntocorp/semgrep/issues/4265
+
+public class Test
+{
+    public bool test()
+    {
+        Foo foo = null;
+
+        //ERROR:
+        assert (foo == null);
+
+        try (Bar b = bar())
+        {
+            switch (scrut)
+            {
+                case A:
+                    foo = something;
+                    break;
+
+                default:
+                    throw new Exn();
+            }
+        }
+
+        //OK:
+        return foo == null;
+    }
+}

semgrep-core/tests/java/cp_switch_throw.sgrep
@@ -1,0 +1,1 @@
+$X == $X