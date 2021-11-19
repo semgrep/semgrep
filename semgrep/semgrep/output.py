semgrep/semgrep

commit 74aeae0aa46270336a27b9415b1632c8ebdd11df

Revert "Revert "feat(timing): profile config resolution and nosem handling timing"" (#4313)
* Revert "Revert "feat(timing): profile config resolution and nosem handling timing (#4292)" (#4312)"

This reverts commit 7f5ac6f20568be1896bff9067911e240ef860709.

* fixup! Revert "Revert "feat(timing): profile config resolution and nosem handling timing (#4292)" (#4312)"

semgrep/semgrep/output.py
@@ -109,7 +109,7 @@ def _build_time_json(
     time_info["rule_parse_info"] = [
         profiling_data.get_rule_parse_time(rule) for rule in rules
     ]
-    time_info["total_time"] = profiler.calls["total_time"][0] if profiler else -1.0
+    time_info["profiling_times"] = profiler.dump_stats() if profiler else {}
     target_bytes = [Path(str(target)).resolve().stat().st_size for target in targets]
     time_info["targets"] = [
         _build_time_target_json(rules, target, num_bytes, profiling_data)

semgrep/semgrep/metric_manager.py
@@ -44,7 +44,7 @@ class _MetricManager:
         self._num_targets: Optional[int] = None
         self._num_findings: Optional[int] = None
         self._num_ignored: Optional[int] = None
-        self._run_time: Optional[float] = None
+        self._profiling_times: Dict[str, float] = {}
         self._total_bytes_scanned: Optional[int] = None
         self._errors: List[str] = []
         self._file_stats: List[Dict[str, Any]] = []
@@ -137,8 +137,8 @@ class _MetricManager:
     def set_num_ignored(self, num_ignored: int) -> None:
         self._num_ignored = num_ignored
 
-    def set_run_time(self, run_time: float) -> None:
-        self._run_time = run_time
+    def set_profiling_times(self, profiling_times: Dict[str, float]) -> None:
+        self._profiling_times = profiling_times
 
     def set_total_bytes_scanned(self, total_bytes_scanned: int) -> None:
         self._total_bytes_scanned = total_bytes_scanned
@@ -197,7 +197,7 @@ class _MetricManager:
             "performance": {
                 "fileStats": self._file_stats,
                 "ruleStats": self._rule_stats,
-                "runTime": self._run_time,
+                "profilingTimes": self._profiling_times,
                 "numRules": self._num_rules,
                 "numTargets": self._num_targets,
                 "totalBytesScanned": self._total_bytes_scanned,

semgrep/semgrep/profile_manager.py
@@ -1,8 +1,6 @@
-import collections
 import time
 from typing import Any
 from typing import Dict
-from typing import List
 
 
 class ProfileManager:
@@ -10,18 +8,12 @@ class ProfileManager:
     An incredibly rudimentary tool for tracking profiled calls with an associated named key
     """
 
-    calls: Dict[str, List[float]] = collections.defaultdict(list)
-
-    def track(self, key: str, callable: Any, *args: Any, **kwargs: Any) -> Any:
-        start_time = time.time()
-        result = callable(*args, **kwargs)
-        self.calls[key].append(time.time() - start_time)
-        return result
+    calls: Dict[str, float] = {}
 
     # This method is an even more rudimentary tool for profiling
     # function calls
     def save(self, key: str, start_time: float) -> Any:
-        self.calls[key].append(time.time() - start_time)
+        self.calls[key] = time.time() - start_time
 
-    def dump_stats(self) -> Dict[str, List[float]]:
-        return dict(sorted(self.calls.items(), key=lambda x: x[1]))
+    def dump_stats(self) -> Dict[str, float]:
+        return self.calls

semgrep/semgrep/semgrep_main.py
@@ -125,10 +125,15 @@ def main(
         exclude = []
 
     project_url = get_project_url()
+
+    profiler = ProfileManager()
+
+    rule_start_time = time.time()
     configs_obj, errors = get_config(
         pattern, lang, configs, replacement=replacement, project_url=project_url
     )
     all_rules = configs_obj.get_rules(no_rewrite_rule_ids)
+    profiler.save("config_time", rule_start_time)
 
     if not severity:
         shown_severities = DEFAULT_SHOWN_SEVERITIES
@@ -181,15 +186,13 @@ def main(
         skip_unknown_extensions=skip_unknown_extensions,
     )
 
-    profiler = ProfileManager()
-
     join_rules, rest_of_the_rules = partition(
         lambda rule: rule.mode == JOIN_MODE,
         filtered_rules,
     )
     filtered_rules = rest_of_the_rules
 
-    start_time = time.time()
+    core_start_time = time.time()
     # actually invoke semgrep
     (
         rule_matches_by_rule,
@@ -218,21 +221,23 @@ def main(
             rule_matches_by_rule.update(join_rule_matches_by_rule)
             output_handler.handle_semgrep_errors(join_rule_errors)
 
-    profiler.save("total_time", start_time)
+    profiler.save("core_time", core_start_time)
 
+    ignores_start_time = time.time()
     filtered_matches_by_rule, nosem_errors, num_ignored_by_nosem = process_ignores(
         rule_matches_by_rule, output_handler, strict=strict, disable_nosem=disable_nosem
     )
+    profiler.save("ignores_time", ignores_start_time)
 
     output_handler.handle_semgrep_errors(semgrep_errors)
     output_handler.handle_semgrep_errors(nosem_errors)
 
     num_findings = sum(len(v) for v in filtered_matches_by_rule.values())
     stats_line = f"ran {len(filtered_rules)} rules on {len(all_targets)} files: {num_findings} findings"
-
-    error_types = list(e.semgrep_error_type() for e in semgrep_errors)
-
+    profiler.save("total_time", rule_start_time)
     if metric_manager.is_enabled():
+        error_types = list(e.semgrep_error_type() for e in semgrep_errors)
+
         metric_manager.set_project_hash(project_url)
         metric_manager.set_configs_hash(configs)
         metric_manager.set_rules_hash(filtered_rules)
@@ -240,7 +245,7 @@ def main(
         metric_manager.set_num_targets(len(all_targets))
         metric_manager.set_num_findings(num_findings)
         metric_manager.set_num_ignored(num_ignored_by_nosem)
-        metric_manager.set_run_time(profiler.calls["total_time"][0])
+        metric_manager.set_profiling_times(profiler.dump_stats())
         total_bytes_scanned = sum(t.stat().st_size for t in all_targets)
         metric_manager.set_total_bytes_scanned(total_bytes_scanned)
         metric_manager.set_errors(error_types)

commit 3768f094a811cceb019abd408df418d8792f608a

Fix CI failures on dummy-benchmarks
This fix a regression introduced by
https://github.com/returntocorp/semgrep/pull/4313
It was not caught at that time because the circle CI job
use the develop image to get the stats and this develop was still
the old one at the moment of the offending PR, so we got the
error at the following PR

test plan:
$ ./run-benchmarks --dummy --upload
now works

perf/run-benchmarks
@@ -125,10 +125,12 @@ def upload_result(
     if variant_name == STD and timings is not None:
         # Compute bps from semgrep timing data
         assert "rules" in timings
-        assert "total_time" in timings
+        assert "profiling_times" in timings
         assert "total_bytes" in timings
+        prof_times = timings["profiling_times"]
+        assert "total_time" in prof_times
         num_rules = len(timings["rules"])
-        bps = timings["total_bytes"] / (timings["total_time"] * num_rules)
+        bps = timings["total_bytes"] / (prof_times["total_time"] * num_rules)
 
         bps_url = f"{DASHBOARD_URL}/api/metric/{BPS_ENDPOINT}"
         upload(bps_url, str(bps))
@@ -143,7 +145,7 @@ def upload_result(
                     num_lines += sum(1 for _ in f)
                 except UnicodeDecodeError:
                     pass
-        lps = num_lines / (timings["total_time"] * num_rules)
+        lps = num_lines / (prof_times["total_time"] * num_rules)
 
         lps_url = f"{DASHBOARD_URL}/api/metric/{LPS_ENDPOINT}"
         upload(lps_url, str(lps))

semgrep/semgrep/output.py
@@ -88,6 +88,10 @@ def _build_time_target_json(
     return target_json
 
 
+# coupling: if you change the JSON schema below, you probably need to
+# also modify perf/run-benchmarks. Run locally
+#    $ ./run-benchmarks --dummy --upload
+# to double check everything still works
 def _build_time_json(
     rules: List[Rule],
     targets: Set[Path],