0c876e6a259e94c77829c275dbe2ac401d665906
Nathan Brahms <nathan@returntocorp.com>
2021-11-24 19:12:05 -0800

    fixup! Use semgrep-langs submodule for language data

--- semgrep/semgrep/semgrep_types.py
+++ semgrep/semgrep/semgrep_types.py
@@ -50,7 +50,7 @@ class LanguageDefinition:
 
 class _LanguageData:
     def __init__(self) -> None:
-        with (Path(__file__).parent.parent / "lang" / "lang.json").open() as fd:
+        with (Path(__file__).parent / "lang" / "lang.json").open() as fd:
             data = json.load(fd)
 
         self.definition_by_id: Mapping[Language, LanguageDefinition] = {
