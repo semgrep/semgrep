b6c5e761df56b56d559ba35656732fbe99b92230
Emma Jin <emjin@users.noreply.github.com>
2021-12-06 15:25:54 -0800

    Oops introduced the string in comment

--- semgrep-core/tests/OTHER/irrelevant_rules/pattern-and-one-general.js
+++ semgrep-core/tests/OTHER/irrelevant_rules/pattern-and-one-general.js
@@ -1,9 +1,6 @@
 // https://raw.githubusercontent.com/lodash/lodash/master/random.js
 
-// This file does not need to be parsed because "child_process" is
-// required for the rule to match but not in this file. Tests also
-// that "child" is insufficient
-
+// Note: "child" is insufficient to have this file be parsed
 require("child")
 
 /** Built-in method references without a dependency on `root`. */
