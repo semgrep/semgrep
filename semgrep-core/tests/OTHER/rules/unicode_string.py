8e7c6c72c0e1a662d094729d81c599a08d83469a
Martin Jambon <martin@r2c.dev>
2021-12-10 00:32:52 -0800

    Explain expectations for unicode matching

--- semgrep-core/tests/OTHER/rules/unicode_string.py
+++ semgrep-core/tests/OTHER/rules/unicode_string.py
@@ -1,4 +1,10 @@
-#ruleid:
-"snowman ☃"
+# Both 🎅 and 🐻 are UTF-8-encoded using 3 bytes.
 
-"snowman 🚀"
+#ruleid:
+"🎅"
+
+# The following shouldn't match the "🎅" pattern but it does, due to
+# our Unicode hack that sees all non-ascii bytes as Zs. Remove the 'ruleid'
+# line once the Unicode hack is gone.
+#ruleid:
+"🐻"
