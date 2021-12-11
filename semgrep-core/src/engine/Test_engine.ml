5d9b3a1c8a2895166fd869d9a508867287898745
Martin Jambon <martin@r2c.dev>
2021-12-11 01:42:41 -0800

    Add hack to allow matches on Unicode data with -fast (#4415)
    * Work around non-ascii byte substitution which was breaking the -fast
    (filter irrelevant rules) optimization.
    Fixes https://github.com/returntocorp/semgrep/issues/4336
    
    * Split big test "full rule" into one test case per file pair
    
    * Explain expectations for unicode matching
    
    * Update changelog
    
    * typo
    
    Co-authored-by: Emma Jin <emjin@users.noreply.github.com>
    
    Co-authored-by: Emma Jin <emjin@users.noreply.github.com>

--- semgrep-core/src/engine/Test_engine.ml
+++ semgrep-core/src/engine/Test_engine.ml
@@ -50,7 +50,7 @@ let first_xlang_of_rules rs =
 (* Entry point *)
 (*****************************************************************************)
 
-let test_rules ?(unit_testing = false) xs =
+let make_tests ?(unit_testing = false) xs =
   let fullxs, _skipped_paths =
     xs
     |> File_type.files_of_dirs_or_files (function
@@ -70,125 +70,145 @@ let test_rules ?(unit_testing = false) xs =
   let ext = "rule" in
   let total_mismatch = ref 0 in
 
-  fullxs
-  |> List.iter (fun file ->
-         logger#info "processing rule file %s" file;
-         let rules = Parse_rule.parse file in
+  let tests =
+    fullxs
+    |> Common.map (fun file ->
+           let test () =
+             logger#info "processing rule file %s" file;
+             let rules = Parse_rule.parse file in
 
-         (* just a sanity check *)
-         (* rules |> List.iter Check_rule.check; *)
-         let xlangs = xlangs_of_rules rules in
-         let xlang =
-           match xlangs with
-           | [] -> failwith (spf "no language found in %s" file)
-           | [ x ] -> x
-           | _ :: _ :: _ ->
-               let fst = first_xlang_of_rules rules in
-               pr2
-                 (spf
-                    "too many languages found in %s, picking the first one: %s"
-                    file (Xlang.show fst));
-               fst
-         in
-         let target =
-           try
-             let d, b, ext = Common2.dbe_of_filename file in
-             Common2.readdir_to_file_list d @ Common2.readdir_to_link_list d
-             |> Common.find_some (fun file2 ->
-                    let path2 = Filename.concat d file2 in
-                    (* Config files have a single .yaml extension (assumption),
-                     * but test files may have multiple extensions, e.g.
-                     * ".test.yaml" (YAML test files), ".sites-available.conf",
-                     * ... *)
-                    match Common2.dbe_of_filename_many_ext_opt file2 with
-                    | None -> None
-                    | Some (_, b2, ext2) ->
-                        if
-                          b = b2 && ext <> ext2
-                          (* .yaml.j2 files are Jinja2 templates to generate Semgrep config files *)
-                          && ext2 <> "yaml.j2"
-                          (* ugly: jsonnet exclusion below because of some .jsonnet and .yaml
-                           * ambiguities in tests/OTHER/rules *)
-                          && ext2 <> "jsonnet"
-                        then Some path2
-                        else None)
-           with Not_found ->
-             failwith (spf "could not find a target for %s" file)
-         in
-         logger#info "processing target %s" target;
-         (* ugly: this is just for tests/OTHER/rules/inception2.yaml, to use JSON
-          * to parse the pattern but YAML to parse the target *)
-         let xlang =
-           match (xlang, Lang.langs_of_filename target) with
-           | L (l, [ l2 ]), xs when not (List.mem l xs) ->
-               pr2 (spf "switching to another language: %s" (Lang.show l2));
-               Xlang.L (l2, [])
-           | _ -> xlang
-         in
+             (* just a sanity check *)
+             (* rules |> List.iter Check_rule.check; *)
+             let xlangs = xlangs_of_rules rules in
+             let xlang =
+               match xlangs with
+               | [] -> failwith (spf "no language found in %s" file)
+               | [ x ] -> x
+               | _ :: _ :: _ ->
+                   let fst = first_xlang_of_rules rules in
+                   pr2
+                     (spf
+                        "too many languages found in %s, picking the first \
+                         one: %s"
+                        file (Xlang.show fst));
+                   fst
+             in
+             let target =
+               try
+                 let d, b, ext = Common2.dbe_of_filename file in
+                 Common2.readdir_to_file_list d @ Common2.readdir_to_link_list d
+                 |> Common.find_some (fun file2 ->
+                        let path2 = Filename.concat d file2 in
+                        (* Config files have a single .yaml extension (assumption),
+                         * but test files may have multiple extensions, e.g.
+                         * ".test.yaml" (YAML test files), ".sites-available.conf",
+                         * ... *)
+                        match Common2.dbe_of_filename_many_ext_opt file2 with
+                        | None -> None
+                        | Some (_, b2, ext2) ->
+                            if
+                              b = b2 && ext <> ext2
+                              (* .yaml.j2 files are Jinja2 templates to generate Semgrep config files *)
+                              && ext2 <> "yaml.j2"
+                              (* ugly: jsonnet exclusion below because of some .jsonnet and .yaml
+                               * ambiguities in tests/OTHER/rules *)
+                              && ext2 <> "jsonnet"
+                            then Some path2
+                            else None)
+               with Not_found ->
+                 failwith (spf "could not find a target for %s" file)
+             in
+             logger#info "processing target %s" target;
+             (* ugly: this is just for tests/OTHER/rules/inception2.yaml, to use JSON
+              * to parse the pattern but YAML to parse the target *)
+             let xlang =
+               match (xlang, Lang.langs_of_filename target) with
+               | L (l, [ l2 ]), xs when not (List.mem l xs) ->
+                   pr2 (spf "switching to another language: %s" (Lang.show l2));
+                   Xlang.L (l2, [])
+               | _ -> xlang
+             in
 
-         (* expected *)
-         (* not tororuleid! not ok:! *)
-         let regexp = ".*\\b\\(ruleid\\|todook\\):.*" in
-         let expected_error_lines =
-           E.expected_error_lines_of_files ~regexp [ target ]
-         in
+             (* expected *)
+             (* not tororuleid! not ok:! *)
+             let regexp = ".*\\b\\(ruleid\\|todook\\):.*" in
+             let expected_error_lines =
+               E.expected_error_lines_of_files ~regexp [ target ]
+             in
 
-         (* actual *)
-         let lazy_ast_and_errors =
-           lazy
-             (match xlang with
-             | L (lang, _) ->
-                 let { Parse_target.ast; errors; _ } =
-                   Parse_target.parse_and_resolve_name_use_pfff_or_treesitter
-                     lang target
-                 in
-                 (ast, errors)
-             | LRegex
-             | LGeneric ->
-                 raise Impossible)
-         in
-         let file_and_more =
-           {
-             FM.file = target;
-             xlang;
-             lazy_content = lazy (Common.read_file target);
-             lazy_ast_and_errors;
-           }
-         in
-         E.g_errors := [];
-         Flag_semgrep.with_opt_cache := false;
-         let config = Config_semgrep.default_config in
-         let res =
-           try Run_rules.check (fun _ _ _ -> ()) config rules [] file_and_more
-           with exn ->
-             failwith (spf "exn on %s (exn = %s)" file (Common.exn_to_s exn))
-         in
-         if not (res.profiling.match_time >= 0.) then
-           (* match_time could be 0.0 if the rule contains no pattern or if the
-              rules are skipped. Otherwise it's positive. *)
-           failwith
-             (spf "invalid value for match time: %g" res.profiling.match_time);
-         if not (res.profiling.parse_time >= 0.) then
-           (* same for parse time *)
-           failwith
-             (spf "invalid value for parse time: %g" res.profiling.parse_time);
+             (* actual *)
+             let lazy_ast_and_errors =
+               lazy
+                 (match xlang with
+                 | L (lang, _) ->
+                     let { Parse_target.ast; errors; _ } =
+                       Parse_target
+                       .parse_and_resolve_name_use_pfff_or_treesitter lang
+                         target
+                     in
+                     (ast, errors)
+                 | LRegex
+                 | LGeneric ->
+                     raise Impossible)
+             in
+             let file_and_more =
+               {
+                 FM.file = target;
+                 xlang;
+                 lazy_content = lazy (Common.read_file target);
+                 lazy_ast_and_errors;
+               }
+             in
+             E.g_errors := [];
+             Flag_semgrep.with_opt_cache := false;
+             let config = Config_semgrep.default_config in
+             let res =
+               try
+                 Run_rules.check (fun _ _ _ -> ()) config rules [] file_and_more
+               with exn ->
+                 failwith
+                   (spf "exn on %s (exn = %s)" file (Common.exn_to_s exn))
+             in
+             if not (res.profiling.match_time >= 0.) then
+               (* match_time could be 0.0 if the rule contains no pattern or if the
+                  rules are skipped. Otherwise it's positive. *)
+               failwith
+                 (spf "invalid value for match time: %g"
+                    res.profiling.match_time);
+             if not (res.profiling.parse_time >= 0.) then
+               (* same for parse time *)
+               failwith
+                 (spf "invalid value for parse time: %g"
+                    res.profiling.parse_time);
 
-         res.matches |> List.iter JSON_report.match_to_error;
-         if not (res.errors = []) then failwith (spf "parsing error on %s" file);
+             res.matches |> List.iter JSON_report.match_to_error;
+             if not (res.errors = []) then
+               failwith (spf "parsing error on %s" file);
 
-         let actual_errors = !E.g_errors in
-         actual_errors
-         |> List.iter (fun e ->
-                logger#info "found error: %s" (E.string_of_error e));
-         match
-           E.compare_actual_to_expected actual_errors expected_error_lines
-         with
-         | Ok () -> Hashtbl.add newscore file Common2.Ok
-         | Error (num_errors, msg) ->
-             pr2 msg;
-             Hashtbl.add newscore file (Common2.Pb msg);
-             total_mismatch := !total_mismatch + num_errors;
-             if unit_testing then Alcotest.fail msg);
-  if not unit_testing then
-    Parse_info.print_regression_information ~ext xs newscore;
-  pr2 (spf "total mismatch: %d" !total_mismatch)
+             let actual_errors = !E.g_errors in
+             actual_errors
+             |> List.iter (fun e ->
+                    logger#info "found error: %s" (E.string_of_error e));
+             match
+               E.compare_actual_to_expected actual_errors expected_error_lines
+             with
+             | Ok () -> Hashtbl.add newscore file Common2.Ok
+             | Error (num_errors, msg) ->
+                 pr2 msg;
+                 Hashtbl.add newscore file (Common2.Pb msg);
+                 total_mismatch := !total_mismatch + num_errors;
+                 if unit_testing then Alcotest.fail msg
+           in
+           (file, test))
+  in
+  let print_summary () =
+    if not unit_testing then
+      Parse_info.print_regression_information ~ext xs newscore;
+    pr2 (spf "total mismatch: %d" !total_mismatch)
+  in
+  (tests, print_summary)
+
+let test_rules ?unit_testing xs =
+  let tests, print_summary = make_tests ?unit_testing xs in
+  tests |> List.iter (fun (_name, test) -> test ());
+  print_summary ()
