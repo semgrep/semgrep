ff78584df46a200e18207d64e4a10bc2cb1814e0
Yoann Padioleau <pad@returntocorp.com>
2021-12-15 12:19:18 +0100

    Rename Run_rules.ml back to Match_rules.ml and split it (#4436)
    Those Run_rules vs Match_rules were confusing. Better to be
    clear and have Match_search_rules.ml and Match_tainting_rule.ml
    
    test plan:
    make

--- semgrep-core/src/engine/Match_tainting_rules.mli
+++ semgrep-core/src/engine/Match_tainting_rules.mli
@@ -1,10 +1,11 @@
 (*
+   Check tainting-mode rules.
    Return matches, errors, match time.
 *)
 val check :
   (string -> Metavariable.bindings -> Parse_info.t list Lazy.t -> unit) ->
   Config_semgrep.t ->
-  Rule.rules ->
+  (Rule.rule * Rule.taint_spec) list ->
   Equivalence.equivalences ->
   File_and_more.t ->
   Report.times Report.match_result
