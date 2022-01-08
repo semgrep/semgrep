12edf96e036ec1195cda99dfb3053b11c3a46961
grayson <grayson@r2c.dev>
2022-01-07 18:09:13 -0600

    Propagate core errors up when using --test. This enables Semgrep to fail tests when parse errors occur when also running --test with --strict.

--- semgrep/semgrep/test.py
+++ semgrep/semgrep/test.py
@@ -267,6 +267,8 @@ def invoke_semgrep_multi(
 ) -> Tuple[Path, Optional[str], Any]:
     try:
         output = invoke_semgrep(config, targets, **kwargs)
+        if len(output['errors']) > 0:
+            raise Exception(output['errors'][0].get('message', "Semgrep core reported an error but no error message."))
     except Exception as error:
         # We must get the string of the error because the multiprocessing library
         # will fail the marshal the error and hang
