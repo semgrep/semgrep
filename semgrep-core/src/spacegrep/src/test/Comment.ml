a753bfda0b9e5ce4d2a1c7e42fb107168fb6669b
Martin Jambon <martin@r2c.dev>
2022-01-11 00:31:02 -0800

    Configurable comment support in spacegrep (#4485)
    * Add comment-skipping options to spacecat (needs testing + adding to spacegrep)
    
    * Add options for skipping comments to spacegrep CLI
    
    * Add unit tests for spacegrep comment removal
    
    * Restore string literals damaged by ocamlformat
    
    * Fix mess with blanks and newlines in string literals, at the expense of
    readability.
    
    * Add ocamlformat-ignore
    
    * Simplify Comment module implementation by using variants

---
+++ semgrep-core/src/spacegrep/src/test/Comment.ml
@@ -0,0 +1,90 @@
+(*
+   Unit tests for spacegrep comment support
+
+   Note that pre-commit hooks don't like trailing whitespace:
+   - a tool removes trailing whitespace, that's why we have to resort
+     to using '\n\' instead of just a newline in some places.
+   - ocamlformat may also rearrange string literals in a less readable way,
+     so it's disabled via a .ocamlformat-ignore file.
+   Additionally, OCaml ignores leading blanks in string literals
+   if the previous line ends with a '\' (line continuation).
+   If this gets too hard, just avoid line breaks in string literals.
+*)
+
+module C = Spacegrep.Comment
+
+(* name, comment style, input, expected output *)
+let tests = [
+  "shell-style comments",
+  C.shell_style,
+  "\
+a#xxx
+b
+c # yyy
+",
+  "\
+a    \n\
+b\n\
+c      \n\
+";
+
+  "end of file comment",
+  C.shell_style,
+  "a #end",
+  "a     ";
+
+  "c-style comments",
+  C.c_style,
+  "hello */ /* ignore /* this */ world",
+  "hello */                      world";
+
+  "cpp-style comments 1",
+  C.cpp_style,
+  "hello */ /* ignore /* this */ world",
+  "hello */                      world";
+
+  "cpp-style comments 2",
+  C.cpp_style,
+  "\
+hello // ignore this
+world // ignore this too",
+  "\
+hello               \n\
+world                   ";
+
+  "end-of-line comments first",
+  C.cpp_style,
+  "\
+hello /* //
+// */ world",
+  "hello /*   \n           ";
+
+  "identical start and end",
+  [C.Multiline ("|", "|")],
+  "a | ignore | b | c",
+  "a            b | c";
+
+  "newline in delimiter",
+  [C.Multiline ("comment", "end\n")],
+  "\
+hello
+comment
+  ign
+  ign end
+hey
+comment end not
+end
+bye
+",
+  "hello\n       \n     \n         \nhey\n               \n   \nbye\n"
+]
+
+let test =
+  "Comment",
+  tests |> List.map (fun (name, style, input, expected_output) ->
+    let run () =
+      let output = C.remove_comments_from_string style input in
+      Alcotest.(check string "equal" expected_output output)
+    in
+    (name, `Quick, run)
+  )
