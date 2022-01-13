cc0b5113dffc8d3b331110a90e4c4d5efa275d4a
Isaac Evans <409041+ievans@users.noreply.github.com>
2022-01-13 09:12:01 -0800

    Apply suggestions from code review

--- semgrep-core/src/parsing/Parse_rule.ml
+++ semgrep-core/src/parsing/Parse_rule.ml
@@ -524,8 +524,8 @@ and parse_formula_old env ((key, value) : key * G.expr) : R.formula_old =
         (spf "unexpected key %s, did you mean metavariable-regex" (fst key))
   (* These keys are handled in Python *)
   (* TODO create an empty construct instead of abusing Patterns *)
-  | "r2c-internal-only-project-depends-on" -> R.Patterns (t, [])
-  | "r2c-internal-only-patterns-from" -> R.Patterns (t, [])
+  | "r2c-internal-project-depends-on" -> R.Patterns (t, [])
+  | "r2c-internal-patterns-from" -> R.Patterns (t, [])
   | _ -> error_at_key env key (spf "unexpected key %s" (fst key))
 
 (* let extra = parse_extra env x in
