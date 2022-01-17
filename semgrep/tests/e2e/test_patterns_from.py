b019abf0a5db97c0bb956ee1df056537537fde0c
Isaac Evans <ine@mit.edu>
2022-01-17 12:34:57 -0800

    update tests

--- semgrep/tests/e2e/test_patterns_from.py
+++ semgrep/tests/e2e/test_patterns_from.py
@@ -18,7 +18,7 @@ import pytest
         ),
     ],
 )
-def test_join_rules(run_semgrep_in_tmp, snapshot, rule, target):
+def test_patterns_from(run_semgrep_in_tmp, snapshot, rule, target):
     snapshot.assert_match(
         run_semgrep_in_tmp(rule, target_name=target)[0],
         "results.json",
