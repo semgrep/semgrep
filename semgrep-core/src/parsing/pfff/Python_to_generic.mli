8331c393be714af718fd1ebe0c5bbdf0c11ba86f
pad <pad@r2c.dev>
2022-01-23 13:02:41 +0100

    [Python] convert some Assign to Vardef when possible
    This is currently not useful for semgrep but for codegraph.
    
    test plan:
    make test

--- semgrep-core/src/parsing/pfff/Python_to_generic.mli
+++ semgrep-core/src/parsing/pfff/Python_to_generic.mli
@@ -1,4 +1,5 @@
-val program : AST_python.program -> AST_generic.program
+val program :
+  ?assign_to_vardef:bool -> AST_python.program -> AST_generic.program
 
 val any : AST_python.any -> AST_generic.any
 
