b7f0219a58a4eb1bef9126bdd282cfe031d575db
Yoann Padioleau <pad@returntocorp.com>
2022-01-28 11:32:58 +0100

    Define more hooks for semgrep variants (#4581)
    test plan:
    make test

--- semgrep-core/src/matching/Normalize_generic.mli
+++ semgrep-core/src/matching/Normalize_generic.mli
@@ -3,5 +3,11 @@ val normalize_import_opt :
   AST_generic.directive_kind ->
   (AST_generic.tok * AST_generic.module_name) option
 
+(* you can extend the behavior of this function by setting
+ * hook_constant_propagation_and_evaluate_literal below.
+ *)
 val constant_propagation_and_evaluate_literal :
   AST_generic.expr -> AST_generic.svalue option
+
+val hook_constant_propagation_and_evaluate_literal :
+  (AST_generic.expr -> AST_generic.svalue option) option ref
