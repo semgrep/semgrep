9d7351839dd89a49e669212285d1542ad6f47f71
Iago Abal <mail@iagoabal.eu>
2022-02-24 10:36:48 +0100

    const-prop: Fix duplication of partial evaluation code for Generic
    Helps #PA-870
    
    test plan:
    make test

--- semgrep-core/src/analyzing/Constant_propagation.mli
+++ semgrep-core/src/analyzing/Constant_propagation.mli
@@ -1,3 +1,11 @@
+val hook_constant_propagation_and_evaluate_literal :
+  (AST_generic.expr -> AST_generic.svalue option) option ref
+(** Provide the analysis with extra information about constants. *)
+
+val constant_propagation_and_evaluate_literal :
+  AST_generic.expr -> AST_generic.svalue option
+(** Partially evaluate a Generic expression. *)
+
 (* Works by side effect on the generic AST by modifying its refs.
  * We pass the lang because some constant propagation algorithm may be
  * specific to a language.
