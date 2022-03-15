f377d6884a4a919599dcf528c0ed3e1b67d2deb4
Iago Abal <mail@iagoabal.eu>
2022-03-15 12:50:50 +0100

    taint-mode: Enable deep taint analysis (#4802)
    * taint-mode: Enable deep taint analysis
    
    This makes possible to implement deep (i.e., interfile interprocedural)
    taint analysis. But it does *NOT* implement it! Deep taint-mode is a
    feature reserved for Deep Semgrep.
    
    Helps PA-899
    
    test plan:
    make test # test included
    
    * Yoann's review

--- semgrep-core/src/analyzing/AST_to_IL.mli
+++ semgrep-core/src/analyzing/AST_to_IL.mli
@@ -4,3 +4,5 @@ val function_definition :
 val stmt : Lang.t -> AST_generic.stmt -> IL.stmt list
 
 val name_of_entity : AST_generic.entity -> IL.name option
+
+val var_of_id_info : AST_generic.ident -> AST_generic.id_info -> IL.name
--- semgrep-core/src/engine/Test_dataflow_tainting.ml
+++ semgrep-core/src/engine/Test_dataflow_tainting.ml
@@ -13,29 +13,28 @@ module DataflowX = Dataflow_core.Make (struct
 end)
 
 let test_dfg_tainting file =
+  (* TODO: Run an actual taint rule! *)
   let ast = Parse_target.parse_program file in
   let lang = List.hd (Lang.langs_of_filename file) in
   Naming_AST.resolve lang ast;
-  let fun_env = Hashtbl.create 8 in
   ast
   |> List.iter (fun item ->
          match item.s with
-         | DefStmt (ent, FuncDef def) ->
+         | DefStmt (_ent, FuncDef def) ->
              let xs = AST_to_IL.stmt lang (H.funcbody_to_stmt def.fbody) in
              let flow = CFG_build.cfg_of_stmts xs in
              pr2 "Tainting";
              let config =
                {
-                 Dataflow_tainting.is_source = (fun _ -> []);
+                 Dataflow_tainting.filepath = file;
+                 rule_id = "test_dfg_tainting";
+                 is_source = (fun _ -> []);
                  is_sink = (fun _ -> []);
                  is_sanitizer = (fun _ -> []);
-                 found_tainted_sink = (fun _ _ -> ());
+                 handle_findings = (fun _ _ _ -> ());
                }
              in
-             let opt_name = AST_to_IL.name_of_entity ent in
-             let mapping =
-               Dataflow_tainting.fixpoint config fun_env opt_name flow
-             in
+             let mapping = Dataflow_tainting.fixpoint config flow in
              DataflowX.display_mapping flow mapping (fun _ ->
                  "<pattern matches>")
          | _ -> ())
