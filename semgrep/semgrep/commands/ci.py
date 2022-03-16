be2e5e33b15b92d7297b2b3db886ac6b810f8c04
brendon <brendon.go@gmail.com>
2022-03-16 16:43:09 -0700

    semgrep ci fails if run not using policy

--- semgrep/semgrep/commands/ci.py
+++ semgrep/semgrep/commands/ci.py
@@ -15,6 +15,11 @@ from semgrep.error import FATAL_EXIT_CODE
 from semgrep.error import SemgrepError
 from semgrep.ignores import IGNORE_FILE_NAME
 from semgrep.semgrep_app import ScanHandler
+from semgrep.util import set_flags
+from semgrep.verbose_logging import getLogger
+
+
+logger = getLogger(__name__)
 
 # These patterns are excluded via --exclude regardless of other ignore configuration
 ALWAYS_EXCLUDE_PATTERNS = [".semgrep/", ".semgrep_logs/"]
@@ -47,15 +52,24 @@ def yield_exclude_args(requested_patterns: Sequence[str]) -> Iterable[str]:
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.pass_context
 @scan_options
-def ci(ctx: click.Context, *args: Any, **kwargs: Any) -> None:
+def ci(
+    ctx: click.Context,
+    verbose: bool,
+    debug: bool,
+    quiet: bool,
+    force_color: bool,
+    *args: Any,
+    **kwargs: Any,
+) -> None:
+    set_flags(verbose=verbose, debug=debug, quiet=quiet, force_color=force_color)
+
     token = Authentication.get_token()
     if not token:
-        click.echo("run `semgrep login` before using `semgrep ci`", err=True)
+        logger.info("run `semgrep login` before using `semgrep ci`")
         sys.exit(1)
     if not Authentication.is_valid_token(token):
-        click.echo(
+        logger.info(
             "API token not valid. Try to run `semgrep logout` and `semgrep login` again.",
-            err=True,
         )
         sys.exit(1)
 
@@ -65,58 +79,68 @@ def ci(ctx: click.Context, *args: Any, **kwargs: Any) -> None:
     try:
         scan_handler.start_scan(metadata)
     except Exception as e:
-        click.echo(f"Failed to start scan so exiting...", err=True)
+        logger.info(f"Failed to start scan so exiting...")
         exit_code = scan_handler.fail_open_exit_code(metadata["repository"], 1)
         if exit_code == 0:
-            click.echo(
+            logger.info(
                 "Fail Open is configured for this repository on semgrep.dev so exiting with code 0",
-                err=True,
             )
         sys.exit(exit_code)
 
     # Set config to rules if default of (,) or is --config policy
     if not kwargs.get("config") or kwargs.get("config") == ("policy",):
         kwargs["config"] = (scan_handler.scan_rules_url,)
+    else:
+        logger.info(
+            "`semgrep ci` can only run with policies configured in semgrep.dev you may be looking for `semgrep scan`"
+        )
+        sys.exit(2)
 
     # Append ignores configured on semgrep.dev
     kwargs["exclude"] = (*kwargs["exclude"], *scan_handler.ignore_patterns)
 
     try:
         start = time.time()
-        ret = ctx.invoke(scan, **kwargs)
+        ret = ctx.invoke(
+            scan,
+            debug=debug,
+            verbose=verbose,
+            quiet=quiet,
+            force_color=force_color,
+            **kwargs,
+        )
         total_time = time.time() - start
     except Exception as e:
-        click.echo(f"Encountered error when running rules: {e}", err=True)
+        logger.info(f"Encountered error when running rules: {e}")
         if isinstance(e, SemgrepError):
             exit_code = e.code
         else:
             exit_code = FATAL_EXIT_CODE
         exit_code = scan_handler.report_failure(exit_code)
         if exit_code == 0:
-            click.echo(
+            logger.info(
                 "Fail Open is configured for this repository on semgrep.dev so exiting with code 0",
-                err=True,
             )
         sys.exit(exit_code)
 
     if ret is not None:
-        click.echo("Reporting findings to semgrep.dev ...", err=True)
+        logger.info("Reporting findings to semgrep.dev ...")
         matches_by_rule, errors, rules, targets = ret
         # if report, finish scan, check fail_open
         scan_handler.report_findings(
             matches_by_rule, errors, rules, targets, total_time
         )
-        click.echo(f"Success.")
+        logger.info(f"Success.")
 
         # if metadata.event_name in audit_on:
-        #     click.echo(
+        #     logger.info(
         #         f"Audit mode is on for {metadata.event_name}, so exiting with code 0 even if matches found",
         #         err=True,
         #     )
         #     sys.exit(0)
         # else:
-        #     click.echo(f"Blocking matches found so exiting with code 1")
+        #     logger.info(f"Blocking matches found so exiting with code 1")
         #     sys.exit(1)
     else:
-        click.echo(f"Semgrep encountered unexpected issue while running ci scan")
+        logger.info(f"Semgrep encountered unexpected issue while running ci scan")
         sys.exit(1)
