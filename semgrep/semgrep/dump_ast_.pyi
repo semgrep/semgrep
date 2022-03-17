d915c9b9b4488d57f66dd80070c95d149d1122ba
Bence Nagy <nagy@bence.dev>
2022-03-17 14:27:00 -0700

    Remove .pyi files

--- semgrep/semgrep/dump_ast_.pyi
+++
@@ -1,6 +0,0 @@
-from typing import Any, Dict, List, Sequence, Set, Tuple, Optional, Collection
-
-# dump the AST of the targets or pattern using semgrep-core internally
-def dump_parsed_ast(
-    to_json: bool, language: str, pattern: Optional[str], targets_str: Sequence[str]
-) -> None: ...
