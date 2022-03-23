7af5cfc596c8cba01361cd6961c2e9bb84b6fe49
Iago Abal <mail@iagoabal.eu>
2022-03-23 12:14:00 +0100

    taint-mode: Attach trace to findings (#4857)
    * taint-mode: Attach trace to findings
    
    Helps PA-314
    Helps PA-315
    
    test plan:
    make test
    
    * Emma's review

--- semgrep-core/src/engine/Match_tainting_rules.mli
+++ semgrep-core/src/engine/Match_tainting_rules.mli
@@ -1,12 +1,13 @@
-val check_rule :
-  Rule.t ->
-  (string -> Metavariable.bindings -> Parse_info.t list Lazy.t -> unit) ->
-  Config_semgrep.t * Equivalence.equivalences ->
-  Rule.taint_spec ->
-  Xtarget.t ->
-  Report.rule_profiling Report.match_result
+type debug_taint = {
+  sources : Range_with_metavars.ranges;
+      (** Ranges matched by `pattern-sources:` *)
+  sanitizers : Range_with_metavars.ranges;
+      (** Ranges matched by `pattern-sanitizers:` *)
+  sinks : Range_with_metavars.ranges;  (** Ranges matched by `pattern-sinks:` *)
+}
+(** To facilitate debugging of taint rules. *)
 
-(* It could be a private function, but it is also used by Deep Semgrep *)
+(* It could be a private function, but it is also used by Deep Semgrep. *)
 val taint_config_of_rule :
   Config_semgrep_t.t ->
   Equivalence.equivalences ->
@@ -18,4 +19,12 @@ val taint_config_of_rule :
   Dataflow_tainting.finding list ->
   Dataflow_tainting.Taint.t Dataflow_core.env ->
   unit) ->
-  Dataflow_tainting.config
+  Dataflow_tainting.config * debug_taint
+
+val check_rule :
+  Rule.t ->
+  (string -> Metavariable.bindings -> Parse_info.t list Lazy.t -> unit) ->
+  Config_semgrep.t * Equivalence.equivalences ->
+  Rule.taint_spec ->
+  Xtarget.t ->
+  Report.rule_profiling Report.match_result * debug_taint
