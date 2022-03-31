273c76e073a1829d0f043114897224c62026ad26
Bence Nagy <nagy@bence.dev>
2022-03-30 17:32:08 -0700

    Make it possible to run any command in docker image

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ "$*" =~ "--config" ]] || [[ "$1" == "publish" ]]; then
+  # 1) --config is a required semgrep scan argument,
+  #    so if it's present, we assume the user meant to call semgrep.
+  #
+  #    -f is a valid --config alias,
+  #    but that could also be part of many shell commands used by e.g. GitLab CI,
+  #    and our docs always type out the full --config flag anyway,
+  #    so we don't assume user meant to call semgrep in those cases.
+  #
+  # 2) if the publish subcommand is used, we also assume the user meant to call semgrep.
+  exec semgrep "$@"
+fi
+
+exec "$@"
