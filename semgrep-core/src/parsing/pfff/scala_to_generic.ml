e2727989057f398da8d280a6399b5e54bbfd70ad
Matthew McQuaid <matthew@returntocorp.com>
2022-04-07 00:02:58 -0700

    reuse TypedExpr for typed metavars

--- semgrep-core/src/parsing/pfff/scala_to_generic.ml
+++ semgrep-core/src/parsing/pfff/scala_to_generic.ml
@@ -358,9 +358,9 @@ and v_expr e : G.expr =
   | DotAccessEllipsis (v1, v2) ->
       let v1 = v_expr v1 in
       G.DotAccessEllipsis (v1, v2) |> G.e
-  | TypedMetavar (v1, v2, v3) ->
+  | TypedExpr (Name (Id (s, tok), []), v2, v3) when String.get s 0 = '$' ->
       let v3 = v_type_ v3 in
-      G.TypedMetavar (v1, v2, v3) |> G.e
+      G.TypedMetavar ((s, tok), v2, v3) |> G.e
   | L v1 -> (
       let v1 = v_literal v1 in
       match v1 with
