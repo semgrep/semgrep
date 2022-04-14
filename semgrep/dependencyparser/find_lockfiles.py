98a5da3e723e1dc1e9fdf886f893326a84a6021c
Matthew McQuaid <matthew@returntocorp.com>
2022-04-13 17:25:08 -0700

    actually, use semgrepignore

--- semgrep/dependencyparser/find_lockfiles.py
+++ semgrep/dependencyparser/find_lockfiles.py
@@ -2,6 +2,7 @@
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Dict
+from typing import Iterable
 from typing import List
 from typing import Optional
 
@@ -12,6 +13,7 @@ from dependencyparser.models import PackageManagers
 from dependencyparser.parse_lockfile import LOCKFILE_PARSERS
 from dependencyparser.parse_lockfile import parse_lockfile_str
 from semgrep.semgrep_types import Language
+from semgrep.target_manager import TargetManager
 
 TARGET_LOCKFILE_FILENAMES = LOCKFILE_PARSERS.keys()
 
@@ -90,15 +92,20 @@ class DependencyTrie:
         return None
 
 
-def make_dependency_trie(target: Path, langs: List[Language]) -> DependencyTrie:
+def make_dependency_trie(
+    target: Path, langs: List[Language], target_manager: TargetManager
+) -> DependencyTrie:
     dep_trie = DependencyTrie()
     # Triple for loop, but the outer two are (basically) constant time and guaranteed to be almost instant
     for namespace in languages_to_namespaces(langs):
         for lockfile_type in NAMESPACE_TO_LOCKFILES[namespace]:
-            for lockfile in target.glob("**/" + lockfile_type):
-                # Temporary hack until semgrepignore functionality works on lockfiles
-                if "node_modules" in lockfile.parts:
-                    continue
+            lockfiles: Iterable[Path] = target.glob("**/" + lockfile_type)
+            lockfiles = (
+                target_manager.file_ignore.filter_paths(candidates=lockfiles).kept
+                if target_manager.file_ignore
+                else lockfiles
+            )
+            for lockfile in lockfiles:
                 deps = list(parse_lockfile_str(lockfile.read_text(), lockfile))
                 dep_trie.insert(lockfile, deps, namespace)
 
