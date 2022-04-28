3311e48f2d5c914b836f986f9a957c7cd006dfc0
pad <pad@r2c.dev>
2022-04-28 14:15:17 +0200

    [refactor] remove CoreMatch from core_output.py
    test plan:
    make test

--- semgrep/semgrep/core_output.py
+++ semgrep/semgrep/core_output.py
@@ -2,17 +2,11 @@
 This file encapsulates classes necessary in parsing semgrep-core
 json output into a typed object
 
-The objects in this class should expose functionality that returns
-objects that the rest of the codebase interacts with (e.g. the rest of
-the codebase should be interacting with RuleMatch objects instead of
-CoreMatch and SemgrepCoreError instead of CoreError objects).
-
 The precise type of the response from semgrep-core is specified in
 Semgrep_core_response.atd, currently at:
 https://github.com/returntocorp/semgrep/blob/develop/semgrep-core/src/core-response/Semgrep_core_response.atd
 """
 from pathlib import Path
-from typing import Any
 from typing import Dict
 from typing import List
 from typing import Optional
@@ -26,7 +20,6 @@ import semgrep.output_from_core as core
 from semgrep.error import LegacySpan
 from semgrep.error import Level
 from semgrep.error import SemgrepCoreError
-from semgrep.output_from_core import MetavarValue
 from semgrep.rule import Rule
 from semgrep.rule_match import RuleMatch
 from semgrep.rule_match import RuleMatchSet
@@ -34,30 +27,6 @@ from semgrep.types import JsonObject
 from semgrep.verbose_logging import getLogger
 
 logger = getLogger(__name__)
-
-
-@frozen
-class CoreMatch:
-    """
-    Encapsulates finding returned by semgrep-core
-    """
-
-    rule: core.RuleId
-    path: Path
-    start: core.Position
-    end: core.Position
-    extra: Dict[str, Any]
-    metavars: Dict[str, MetavarValue]
-
-    @classmethod
-    def make(cls, match: core.Match) -> "CoreMatch":
-        rule = match.rule_id
-        path = Path(match.location.path)
-        start = match.location.start
-        end = match.location.end
-        extra = match.extra.to_json()
-        metavars = match.extra.metavars
-        return cls(rule, path, start, end, extra, metavars)
 
 
 @frozen
@@ -140,7 +109,7 @@ class CoreOutput:
     Parses output of semgrep-core
     """
 
-    matches: List[CoreMatch]
+    matches: List[core.Match]
     errors: List[CoreError]
     skipped: List[core.SkippedTarget]
     timing: Optional[core.Time]
@@ -151,7 +120,7 @@ class CoreOutput:
         match_results = core.MatchResults.from_json(raw_json)
 
         parsed_errors = [CoreError.make(error) for error in match_results.errors]
-        parsed_matches = [CoreMatch.make(match) for match in match_results.matches]
+        matches = match_results.matches
         for skip in match_results.skipped_targets:
             if skip.rule_id:
                 rule_info = f"rule {skip.rule_id}"
@@ -164,7 +133,7 @@ class CoreOutput:
         skipped = match_results.skipped_targets
         timings = match_results.time
 
-        return cls(parsed_matches, parsed_errors, skipped, timings)
+        return cls(matches, parsed_errors, skipped, timings)
 
     def rule_matches(self, rules: List[Rule]) -> Dict[Rule, List[RuleMatch]]:
         """
@@ -184,12 +153,12 @@ class CoreOutput:
 
             return text
 
-        def read_metavariables(match: CoreMatch) -> Dict[str, str]:
+        def read_metavariables(match: core.Match) -> Dict[str, str]:
             result = {}
 
             # open path and ignore non-utf8 bytes. https://stackoverflow.com/a/56441652
-            with open(match.path, errors="replace") as fd:
-                for metavariable, metavariable_data in match.metavars.items():
+            with open(match.location.path, errors="replace") as fd:
+                for metavariable, metavariable_data in match.extra.metavars.items():
                     # Offsets are start inclusive and end exclusive
                     start_offset = metavariable_data.start.offset
                     end_offset = metavariable_data.end.offset
@@ -200,30 +169,30 @@ class CoreOutput:
 
             return result
 
-        def convert_to_rule_match(match: CoreMatch) -> RuleMatch:
-            rule = rule_table[match.rule.value]
+        def convert_to_rule_match(match: core.Match) -> RuleMatch:
+            rule = rule_table[match.rule_id.value]
             metavariables = read_metavariables(match)
             message = interpolate(rule.message, metavariables)
             fix = interpolate(rule.fix, metavariables) if rule.fix else None
 
             return RuleMatch(
-                match.rule.value,
+                match.rule_id.value,
                 message=message,
                 metadata=rule.metadata,
                 severity=rule.severity,
                 fix=fix,
                 fix_regex=rule.fix_regex,
-                path=match.path,
-                start=match.start,
-                end=match.end,
-                extra=match.extra,
+                path=Path(match.location.path),
+                start=match.location.start,
+                end=match.location.end,
+                extra=match.extra.to_json(),
             )
 
         # TODO: Dict[core.RuleId, RuleMatchSet]
         findings: Dict[Rule, RuleMatchSet] = {rule: RuleMatchSet() for rule in rules}
         seen_cli_unique_keys: Set[Tuple] = set()
         for match in self.matches:
-            rule = rule_table[match.rule.value]
+            rule = rule_table[match.rule_id.value]
             rule_match = convert_to_rule_match(match)
             if rule_match.cli_unique_key in seen_cli_unique_keys:
                 continue
