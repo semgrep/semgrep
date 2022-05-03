2f3f16b6479347d2078a772f59cb9db578a950e9
pad <pad@r2c.dev>
2022-05-03 10:00:14 +0200

    fix CI

--- semgrep/tests/conftest.py
+++ semgrep/tests/conftest.py
@@ -63,7 +63,7 @@ def _clean_output_json(output_json: str) -> str:
         for r in results:
             p = r.get("path")
             if p and "/tmp" in p:
-                del r["path"]
+                r["path"] = "/tmp/masked/path"
                 # the fingerprint contains the path too
                 r["extra"]["fingerprint"] = "0x42"
 
