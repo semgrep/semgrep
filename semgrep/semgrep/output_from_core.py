edb6c5663cb5929598827cb48a7e318acf4b5401
Yoann Padioleau <pad@r2c.dev>
2022-05-04 15:51:51 +0200

    [refactor] Collateral evolutions on the Semgrep_output_v1.atd rename (#5144)
    test plan:
    make test

--- semgrep/semgrep/output_from_core.py
+++ semgrep/semgrep/output_from_core.py
@@ -1,2 +1,2 @@
 # TODO: when ATD gets its module system, we would not need to this anymore
-from .semgrep_interfaces.semgrep_scan_output_v1 import *  # noqa
+from .semgrep_interfaces.semgrep_output_v1 import *  # noqa
