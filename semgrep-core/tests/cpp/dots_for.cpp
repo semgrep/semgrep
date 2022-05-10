c6d88af9d68c53d804fb65d8fda1ba6387114486
Yoann Padioleau <pad@r2c.dev>
2022-05-10 11:29:16 +0200

    C++: support ellipsis in loop as in  'for(...) {}' (#5178)
    test plan:
    test file included
    make test

---
+++ semgrep-core/tests/cpp/dots_for.cpp
@@ -0,0 +1,11 @@
+int main() {
+    if(5 + 5 == 10){
+        return 0;
+    }
+        //ERROR: match
+        for (int i = 1; i <= 5; ++i) {
+        cout << i << " ";
+        return 0;
+    }
+    return 0;
+}
