a9b7b94d1d328449c2c9ca852b275d5ad427b6e3
Yoann Padioleau <pad@r2c.dev>
2022-05-17 14:44:05 +0200

    [JS/TS] allow ellipsis in import (#5237)
    This closes #5012
    
    test plan:
    test file included

---
+++ semgrep-core/tests/ts/dots_import.ts
@@ -0,0 +1,15 @@
+//ERROR: match
+import {Request, Response} from 'express'
+
+module.exports = function servePublicFiles () {
+  return ({ params, query }: Request, res: Response, next: NextFunction) => {
+    const file = params.file
+
+    if (!file.includes('/')) {
+      verify(file, res, next)
+    } else {
+      res.status(403)
+      next(new Error('File names cannot contain forward slashes!'))
+    }
+  }
+}
