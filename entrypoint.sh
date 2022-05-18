1d67b24da4e7c0a5d40591cb8d1db2878264495f
Bence Nagy <nagy@bence.dev>
2022-05-18 14:30:30 -0700

    fix(entrypoint): Avoid warning if semgrep is already 1st arg (#5259)
    * fix(entrypoint): Avoid warning if segmrep is already 1st arg
    
    * fix(entrypoint): Add volume mount flag to deprecation warning
    
    Fixes CLI-142
    Fixes CLI-140

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ "$*" =~ "--config" || "$*" == "--help" || "$1" == "publish" || "$1" == "ci" ]]; then
+if [[ "$1" != "semgrep" && ("$*" =~ "--config" || "$*" == "--help" || "$1" == "publish" || "$1" == "ci") ]]; then
   # 1) --config is a required semgrep scan argument,
   #    so if it's present, we assume the user meant to call semgrep.
   #
@@ -14,8 +14,8 @@ if [[ "$*" =~ "--config" || "$*" == "--help" || "$1" == "publish" || "$1" == "ci
   >&2 echo "======= DEPRECATION WARNING ======="
   >&2 echo "The returntocorp/semgrep Docker image's custom entrypoint will be removed by June 2022."
   >&2 echo "Please update your command to explicitly call semgrep."
-  >&2 echo "Change from:  docker run returntocorp/semgrep $*"
-  >&2 echo "Change to:    docker run returntocorp/semgrep semgrep $*"
+  >&2 echo "Change from:  docker run -v \$(pwd):/src returntocorp/semgrep $*"
+  >&2 echo "Change to:    docker run -v \$(pwd):/src returntocorp/semgrep semgrep $*"
   exec semgrep "$@"
 fi
 
