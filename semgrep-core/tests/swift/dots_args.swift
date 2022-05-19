85eabd3b3725ada099ab7566e22535eb1791c958
Nat Mote <nat@r2c.dev>
2022-05-19 00:16:29 -0700

    test(swift): Add passing maturity tests (#5260)
    This documents progress towards experimental status for Swift.

---
+++ semgrep-core/tests/swift/dots_args.swift
@@ -0,0 +1,9 @@
+func foo() {
+    //ERROR:
+    foo(1,2,3,4,5);
+    //ERROR:
+    foo(5);
+    //ERROR:
+    foo(1, 5);
+}
+
---
+++ semgrep-core/tests/swift/metavar_arg.swift
@@ -0,0 +1,18 @@
+func foo() {
+    //ERROR:
+    foo(1,2);
+
+    //ERROR:
+    foo(a_very_long_constant_name,
+        2);
+
+    //ERROR:
+    foo (unsafe(), // indeed
+         2);
+
+    //ERROR:
+    foo(bar(1,3), 2);
+
+    foo(2,1);
+}
+
