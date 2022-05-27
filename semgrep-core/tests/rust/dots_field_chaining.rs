685ba1209083d90cc86a77c2a7cc3070752c5c20
Cooper Pierce <cooper@r2c.dev>
2022-05-26 17:42:50 -0700

    Add tests for field chains
    Currently we have a redundant match for the let expression line because
    we translate a Rust let expression as `OtherExpr`, and when matching
    against an `OtherExpr` we try all possible combinations. This seems like
    the incorrect translation, but also probably out of scope for initially
    adding this feature.

---
+++ semgrep-core/tests/rust/dots_field_chaining.rs
@@ -0,0 +1,19 @@
+struct A {
+    x: B,
+}
+
+struct B {
+    y: C,
+}
+
+struct C {
+    z: (),
+}
+
+fn main() {
+    let s = A { x: B { y: C { z: () } } };
+    s.x;
+    s.x.y;
+    // ERROR: match
+    s.x.y.z;
+}
