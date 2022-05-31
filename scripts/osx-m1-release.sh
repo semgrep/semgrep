e4bd0dde61a70e88f8f35c038190801dbd56a320
ajbt200128 <austin@r2c.dev>
2022-05-31 16:27:32 -0700

    Add workflows and m1 support

---
+++ scripts/osx-m1-release.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Because we're running this on a remote machine, we don't want to reinstall
+# everything every time
+set -e
+brew update # Needed to sidestep bintray brownout
+#coupling: this should be the same version than in our Dockerfile
+opam switch 4.12.0;
+git submodule update --init --recursive --depth 1
+
+eval "$(opam env)"
+
+make setup
+make config
+
+make build-core
+
+mkdir -p artifacts
+cp ./semgrep-core/_build/install/default/bin/semgrep-core artifacts
+zip -r artifacts.zip artifacts
--- scripts/setup-m1-builder.sh
+++ scripts/setup-m1-builder.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
+
 # THIS SHOULD ONLY BE RUN ONCE! Since this is a remote runner, its environment doesn't get reset
 # Assuming that brew is already installed
 # Install all ocaml things
 echo "Warning: This setup is interactive! You must add this machine as a github action runner manually"
+
+echo "Adding current user to wheel group!"
+echo "This is needed so the build process can be non-interactive"
+sudo dseditgroup -o edit -a build -t user "$(id -un)"
+echo "Modifying /usr/local/include/ group permission to be read write execute"
+sudo chmod g+rwx /usr/local/include/
+echo "Modifying /usr/local/bin/ group permission to be read write execute"
+sudo chmod g+rwx /usr/local/bin/
+echo "Modifying /usr/local/lib/ group permission to be read write execute"
+sudo chmod g+rwx /usr/local/lib/
+
 brew update # Needed to sidestep bintray brownout
 # Usually we install python 3.7 for building wheels, but 3.7 isn't supported on m1 macs
-brew install opam pkg-config coreutils python
+brew install opam pkg-config coreutils python pcre
 opam init --no-setup --bare;
 #coupling: this should be the same version than in our Dockerfile
 opam switch create 4.12.0;
@@ -13,8 +25,9 @@ opam switch 4.12.0;
 git submodule update --init --recursive --depth 1
 
 eval "$(opam env)"
-
-# Setup python symbolic links
+# Remove pcre dynamically linked to force MacOS to use static
+# This needs to be done before make setup since it is used there
+rm /usr/local/opt/pcre/lib/libpcre.1.dylib# Setup python symbolic links
 # Brew install won't alias python3 to python :/
 ln -s /opt/homebrew/bin/python3 /opt/homebrew/bin/python
 ln -s /opt/homebrew/bin/pip3 /opt/homebrew/bin/pip
@@ -27,6 +40,7 @@ mkdir actions-runner && cd actions-runner || exit
 curl -O -L https://github.com/actions/runner/releases/download/v2.292.0/actions-runner-osx-arm64-2.292.0.tar.gz
 # Extract the installer
 tar xzf ./actions-runner-osx-arm64-2.292.0.tar.gz
+
 # Must be sudo to run jobs
-sudo ./config.sh
-sudo ./run.sh
+./config.sh
+./run.sh
