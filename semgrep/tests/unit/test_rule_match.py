692078af85be967a073c178bfa862ead99215748
Matthew McQuaid <47466291+mmcqd@users.noreply.github.com>
2022-06-08 11:22:49 -0700

    Add untyped SCA output to app findings + test RuleMatch to app finding conversion (#5459)
    * test
    
    * dont mock open, add sca back into interface
    
    * update semgrep_interfaces
    
    * snapshot
    
    * format json output in python test
    
    * just use json.dumps
    
    * newline after the json because of precommit
    
    * standardize commit date
    
    * update interface commit

--- semgrep/tests/unit/test_rule_match.py
+++ semgrep/tests/unit/test_rule_match.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 from textwrap import dedent
 
@@ -268,3 +269,51 @@ def test_rule_match_set_indexes(mocker):
     assert sorted_matches[1].index == 1, "2nd duplicate match must be assigned index 1"
     assert sorted_matches[3].index == 2, "3rd duplicate match must be assigned index 2"
     assert sorted_matches[2].index == 0, "unique match must be assigned index 0"
+
+
+@pytest.mark.quick
+def test_rule_match_to_app_finding(snapshot, mocker):
+    mocker.patch.object(RuleMatch, "get_lines", lambda self: "foo()")
+    dependency_match = {
+        "dependency_pattern": {
+            "namespace": "pypi",
+            "package_name": "awscli",
+            "semver_range": "== 1.11.82",
+        },
+        "found_dependency": {
+            "allowed_hashes": {
+                "sha256": [
+                    "149e90d6d8ac20db7a955ad60cf0e6881a3f20d37096140088356da6c716b0b1",
+                    "ef6aaac3ca6cd92904cdd0d83f629a15f18053ec84e6432106f7a4d04ae4f5fb",
+                ]
+            },
+            "name": "awscli",
+            "namespace": "pypi",
+            "resolved_url": None,
+            "version": "1.11.82",
+        },
+        "lockfile": "targets/dependency_aware/Pipfile.lock",
+    }
+    match = RuleMatch(
+        message="message",
+        severity=RuleSeverity.ERROR,
+        match=core.CoreMatch(
+            rule_id=core.RuleId("rule.id"),
+            location=core.Location(
+                path="foo.py",
+                start=core.Position(0, 0, 0),
+                end=core.Position(0, 0, 0),
+            ),
+            extra=core.CoreMatchExtra(metavars=core.Metavars({})),
+        ),
+        extra={
+            "dependency_match_only": False,
+            "dependency_matches": [dependency_match],
+        },
+    )
+    app_finding = match.to_app_finding_format("0")
+    app_finding.commit_date = "1970-01-01T00:00:00"
+    app_finding_str = (
+        json.dumps(app_finding.to_json(), indent=2, sort_keys=True) + "\n"
+    )  # Needed because pre-commit always adds a newline, seems weird
+    snapshot.assert_match(app_finding_str, "results.json")
