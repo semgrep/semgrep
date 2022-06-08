2626c355055b3a3ad16f88fefa9a7a103d5b7708
Matthew McQuaid <matthew@returntocorp.com>
2022-06-08 00:38:38 -0700

    newline after the json because of precommit

--- semgrep/tests/unit/test_rule_match.py
+++ semgrep/tests/unit/test_rule_match.py
@@ -311,7 +311,8 @@ def test_rule_match_to_app_finding(snapshot, mocker):
             "dependency_matches": [dependency_match],
         },
     )
-    app_finding = json.dumps(
-        match.to_app_finding_format("0").to_json(), indent=2, sort_keys=True
-    )
+    app_finding = (
+        json.dumps(match.to_app_finding_format("0").to_json(), indent=2, sort_keys=True)
+        + "\n"
+    )  # Needed because pre-commit always adds a newline, seems weird
     snapshot.assert_match(app_finding, "results.json")
