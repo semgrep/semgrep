4f8c6b4e10b173e57d6cd51eeb797fc70a26b741
pad <pad@r2c.dev>
2022-06-14 11:46:58 +0200

    semgrep

--- semgrep-core/src/parsing/tree_sitter/Parse_r_tree_sitter.ml
+++ semgrep-core/src/parsing/tree_sitter/Parse_r_tree_sitter.ml
@@ -75,7 +75,7 @@ let map_pat_3e57880 (env : env) (tok : CST.pat_3e57880) =
 let map_special (env : env) ((v1, v2, v3) : CST.special) =
   let v1 = (* "%" *) token env v1 in
   let v2 =
-    List.map
+    Common.map
       (fun x ->
         match x with
         | `Pat_5e7ac5f x -> map_pat_5e7ac5f env x
@@ -91,7 +91,7 @@ let map_identifier (env : env) (x : CST.identifier) =
   | `BQUOT_rep_choice_pat_4ad362e_BQUOT (v1, v2, v3) ->
       let v1 = (* "`" *) token env v1 in
       let v2 =
-        List.map
+        Common.map
           (fun x ->
             match x with
             | `Pat_4ad362e x -> map_pat_4ad362e env x
@@ -107,7 +107,7 @@ let map_string_ (env : env) (x : CST.string_) =
   | `DQUOT_rep_choice_pat_de5d470_DQUOT (v1, v2, v3) ->
       let v1 = (* "\"" *) token env v1 in
       let v2 =
-        List.map
+        Common.map
           (fun x ->
             match x with
             | `Pat_de5d470 x -> map_pat_de5d470 env x
@@ -119,7 +119,7 @@ let map_string_ (env : env) (x : CST.string_) =
   | `SQUOT_rep_choice_pat_3e57880_SQUOT (v1, v2, v3) ->
       let v1 = (* "'" *) token env v1 in
       let v2 =
-        List.map
+        Common.map
           (fun x ->
             match x with
             | `Pat_3e57880 x -> map_pat_3e57880 env x
@@ -135,7 +135,7 @@ let rec map_argument (env : env) (x : CST.argument) =
   | `Defa_arg x -> map_default_argument env x
 
 and map_arguments (env : env) (xs : CST.arguments) =
-  List.map
+  Common.map
     (fun x ->
       match x with
       | `Arg x -> map_argument env x
@@ -292,7 +292,7 @@ and map_expression (env : env) (x : CST.expression) =
       todo env (v1, v2, v3)
   | `Paren_list (v1, v2, v3) ->
       let v1 = (* "(" *) token env v1 in
-      let v2 = List.map (map_expression env) v2 in
+      let v2 = Common.map (map_expression env) v2 in
       let v3 = (* ")" *) token env v3 in
       todo env (v1, v2, v3)
   | `Bin x -> map_binary env x
@@ -416,7 +416,7 @@ and map_formal_parameters (env : env) ((v1, v2, v3) : CST.formal_parameters) =
     | Some (v1, v2, v3) ->
         let v1 = map_formal_parameter env v1 in
         let v2 =
-          List.map
+          Common.map
             (fun (v1, v2) ->
               let v1 = (* "," *) token env v1 in
               let v2 = map_formal_parameter env v2 in
@@ -469,7 +469,7 @@ and map_pipe_rhs_argument (env : env) (x : CST.pipe_rhs_argument) =
       todo env (v1, v2, v3)
 
 and map_pipe_rhs_arguments (env : env) (xs : CST.pipe_rhs_arguments) =
-  List.map
+  Common.map
     (fun x ->
       match x with
       | `Pipe_rhs_arg x -> map_pipe_rhs_argument env x
@@ -477,7 +477,7 @@ and map_pipe_rhs_arguments (env : env) (xs : CST.pipe_rhs_arguments) =
     xs
 
 and map_program (env : env) (xs : CST.program) =
-  List.map
+  Common.map
     (fun (v1, v2) ->
       let v1 = map_expression env v1 in
       let v2 =
