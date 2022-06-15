726ea1a325b7a1f8471aac0f46299c9deb9415a4
Yoann Padioleau <pad@r2c.dev>
2022-06-15 17:43:29 +0200

    opti: new -use_parsing_cache option to semgrep-core (#5539)
    * opti: new -use_parsing_cache option to semgrep-core
    
    This PR caches the parsed generic ASTs on the disk (e.g., in
    ~/.semgrep/cache) to speedup semgrep between 2 runs.
    
    This fixes PA-1509
    
    test plan:
    ```
    $ rm -rf ~/.semgrep/cache
    $ time yy -profile -use_parsing_cache ~/.semgrep/cache/ -l java -rules ~/eqeq.yaml ~/work/lang-java/elasticsearch/ -log_config_file /tmp/xxx
    1m32s
    $ time yy -profile -use_parsing_cache ~/.semgrep/cache/ -l java -rules ~/eqeq.yaml ~/work/lang-java/elasticsearch/ -log_config_file /tmp/xxx
    11s
    ```
    
    * misc
    
    * fix

--- semgrep-core/src/parsing/Parse_target.ml
+++ semgrep-core/src/parsing/Parse_target.ml
@@ -375,6 +375,7 @@ let rec just_parse_with_lang lang file =
         [ TreeSitter (Parse_vue_tree_sitter.parse parse_embedded_js) ]
         (fun x -> x)
   | Lang.Hcl -> run file [ TreeSitter Parse_hcl_tree_sitter.parse ] (fun x -> x)
+  [@@profiling]
 
 (*****************************************************************************)
 (* Entry point *)
@@ -399,6 +400,7 @@ let parse_and_resolve_name lang file =
 
   logger#info "Parse_target.parse_and_resolve_name_use_pfff_or_treesitter done";
   res
+  [@@profiling]
 
 (* used in test files *)
 let parse_and_resolve_name_warn_if_partial lang file =
