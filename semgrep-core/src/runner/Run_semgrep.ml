777e903d73e190f40cf0607b3dbf75b25b3b54f6
pad <pad@r2c.dev>
2022-06-16 22:29:35 +0200

    new -generate_ast_binary and -generate_ast_json
    This will help PA-1509
    
    test plan:
    ```
    $ semgrep-core -l php -generate_ast_binary ~/hello.php
    ...
    saved marshalled generic AST in /home/pad/hello.php.ast.binary
    ```

--- semgrep-core/src/runner/Run_semgrep.ml
+++ semgrep-core/src/runner/Run_semgrep.ml
@@ -1,3 +1,17 @@
+(* Yoann Padioleau
+ *
+ * Copyright (C) 2020-2022 r2c
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation, with the
+ * special exception on linking described in file license.txt.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
+ * license.txt for more details.
+ *)
 open Common
 open Runner_config
 module PI = Parse_info
