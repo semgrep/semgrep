b67bfb7554274da6a39fbeaeed0171b1e5a8998d
Matthew McQuaid <matthew@returntocorp.com>
2022-06-23 10:24:00 -0400

    simple test

---
+++ semgrep-core/tests/js/deep_expr_xml.js
@@ -0,0 +1,32 @@
+export default function TestComponent4({assets, children, title}) {
+    return (
+      //ERROR: match
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="shortcut icon" href="favicon.ico" />
+          <link rel="stylesheet" href={assets['main.css']} />
+          <title>{title}</title>
+        </head>
+        //ERROR: match
+        <body>
+          //ERROR: match
+          <noscript
+            //ERROR: match
+            dangerouslySetInnerHTML={{
+              __html: `<b>Enable JavaScript to run this app.</b>`,
+            }}
+          />
+          {children}
+          //ERROR: match
+          <script
+            //ERROR: match
+            dangerouslySetInnerHTML={{
+              __html: `assetManifest = ${JSON.stringify(assets)};`,
+            }}
+          />
+        </body>
+      </html>
+    );
+  }
