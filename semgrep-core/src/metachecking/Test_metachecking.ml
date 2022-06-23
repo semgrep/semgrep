5e86e8594e66135126a8e5d0cd1241a7f4e919cf
Cooper Pierce <cooper@r2c.dev>
2022-06-23 10:51:36 -0700

    Add `-pp` flag to core to pass extract rule file
    It's unclear to me if this should really be a separate flag or not,
    given that I'm implementing it using a mode. This introduces a bit of an
    artificial barrier and will be a little bit less ergonomic when it comes
    to making cli changes later, but is fine for testing and we can perhaps
    change to sticking with just using multiple invocations of `-rules` later.

--- semgrep-core/src/metachecking/Test_metachecking.ml
+++ semgrep-core/src/metachecking/Test_metachecking.ml
@@ -40,6 +40,7 @@ let config =
     pattern_string = "";
     pattern_file = "";
     rules_file = "";
+    extractors_file = "";
     equivalences_file = "";
     metatypes_file = "";
     lang = None;
--- semgrep-core/src/runner/Runner_config.ml
+++ semgrep-core/src/runner/Runner_config.ml
@@ -20,6 +20,7 @@ type t = {
   pattern_string : string;
   pattern_file : filename;
   rules_file : filename;
+  extractors_file : filename;
   equivalences_file : string;
   metatypes_file : string;
   lang : Xlang.t option;
