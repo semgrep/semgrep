d05a6fcb3ee98cd47f5d3496d49db708e7d5da4f
Emma Jin <emjin@users.noreply.github.com>
2022-06-29 18:16:32 -0400

    Fix location error with unicode parsing
    This is a hacky solution (see comment) because the YAML parser counts charpos
    differently than Parse_info.
    
    Test plan: make test

--- semgrep-core/src/parsing/other/yaml_to_generic.ml
+++ semgrep-core/src/parsing/other/yaml_to_generic.ml
@@ -496,6 +496,22 @@ let preprocess_yaml str =
   in
   String.concat "\n" (process_lines lines [] [])
 
+let mask_unicode str =
+  (* The YAML parser returns the charpos as the number of unicode (not 8-bit)
+     characters. However, Parse_info.full_charpos_to_pos expects the charpos
+     to be the number of 8-bit characters. This difference causes incorrect
+     line/col to be assigned to the end tokens of mappings after unicode
+     characters. *)
+  (* Note that the YAML parser does return a correct line and col, which we
+     use everywhere else. However, it gives an exclusive end when returning
+     the token that ends a mapping/sequence/other bracket, whereas Semgrep 
+     expects an inclusive end. To adjust this, we currently need the charpos *)
+  let char_range = 128 in
+  let control_char_start_range = 32 in
+  let control_char_end_range = 1 in
+  let available_range = char_range - (control_char_end_range + control_char_start_range) in
+  String.of_seq (Seq.map (fun c -> let code = Char.code c in if code < char_range then c else Char.chr ((code mod available_range) + control_char_start_range)) (String.to_seq str)) 
+
 (*****************************************************************************)
 (* Entry points *)
 (*****************************************************************************)
@@ -503,7 +519,7 @@ let preprocess_yaml str =
 let program file =
   (* we do not preprocess the yaml here; ellipsis should be transformed
    * only in the pattern *)
-  let str = Common.read_file file in
+  let str = mask_unicode (Common.read_file file) in
   let charpos_to_pos = Some (Parse_info.full_charpos_to_pos_large file) in
   let parser = get_res file (S.parser str) in
   let env =
@@ -520,7 +536,7 @@ let program file =
 
 let any str =
   let file = "<pattern_file>" in
-  let str = preprocess_yaml str in
+  let str = mask_unicode (preprocess_yaml str) in
   let parser = get_res file (S.parser str) in
   let env =
     {
