d30da7627196fe2c2e24127450f6a45ab1b256fb
Martin Jambon <martin@r2c.dev>
2022-07-01 08:00:47 -0700

    Make statement patterns anchored (#5630)
    * Prevent a statement pattern 'a;' from matching the target 'f(a);'
    
    * Add test cases for anchored statement matching for Bash
    (for https://github.com/returntocorp/semgrep/issues/5472)
    
    * Add a test for anchored shell command matching
    (for https://github.com/returntocorp/semgrep/issues/5472)
    
    * Improve test cases
    
    * Add new options: implicit_deep_exprstmt, add more tests
    
    Also cleanup how we handl implicit deep exprstmt
    
    test plan:
    test file included
    make test
    
    * changelog
    
    Co-authored-by: pad <pad@r2c.dev>

--- semgrep-core/tests/kotlin/deep_exprstmt.kt
+++ semgrep-core/tests/kotlin/deep_exprstmt.kt
@@ -1,11 +1,15 @@
 fun foo() {
     //ERROR: match
+    foo()
     bar()
     //ERROR: match
+    foo()
     val x = bar()
     //ERROR: match
+    foo()
     foo2(bar())
     //ERROR: match
+    foo()
     return bar()
 }
   
--- semgrep-core/tests/ts/deep_exprstmt.ts
+++ semgrep-core/tests/ts/deep_exprstmt.ts
@@ -1,10 +1,20 @@
 function foo() {
     //ERROR: match
+    foo();
     bar();
     //ERROR: match
+    foo();
     x = bar();
     //ERROR: match
+    foo();
     print(bar());
     //ERROR: match
+    foo();
+    await bar();
+    //ERROR: match
+    foo();
+    bar().then(other => stuff());
+    //ERROR: match
+    foo();
     return bar();
 }
