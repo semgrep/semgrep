a337fdb6ae8d9a0f35532ef00264cd0ab4919f3a
Nathan Brahms <nathan@returntocorp.com>
2022-07-12 16:45:18 -0700

    fixup! chore(test): Remove additional test target pollution

--- cli/tests/e2e/test_match_based_id.py
+++ cli/tests/e2e/test_match_based_id.py
@@ -19,7 +19,7 @@ def test_duplicate_matches_indexing(run_semgrep_in_tmp, snapshot):
 
 @pytest.mark.kinda_slow
 @pytest.mark.parametrize(
-    "rule,target,expect_change",
+    "rule,target_name,expect_change",
     [
         # ("rules/match_based_id/","",True)
         ("rules/match_based_id/formatting.yaml", "formatting.c", False),
@@ -30,15 +30,33 @@ def test_duplicate_matches_indexing(run_semgrep_in_tmp, snapshot):
         ("rules/match_based_id/join.yaml", "join.py", True),
     ],
 )
-def test_id_change(run_semgrep_on_copied_files, tmp_path, rule, target, expect_change):
+def test_id_change(
+    run_semgrep_on_copied_files, tmp_path, rule, target_name, expect_change
+):
+    """
+    Ensures that match-based IDs are resistant to various types of changes in code.
 
-    suffix = Path(target).suffix
+    These changes are enumerated in
+       targets / match_based_id / (before|after) / <target_name>
 
-    # Static file path (necessary to obtain static id)
-    static_target = tmp_path / "targets" / ("_match_based_id" + suffix)
+    To edit these cases, edit these files directly. To add new cases, add a corresponding pair
+    of files, and update the parameterization above.
+
+    :param rule: The Semgrep rule that should trigger a finding
+    :param target: The filename of the target pair
+    :param expect_change: Whether or not to expect an ID change
+    """
+
+    # Since the match_based_id includes the target path, we must create a static target path.
+    # Note that b/c we use run_semgrep_on_copied_files, the current working directory
+    # for Semgrep is tmp_path; I specify the target as an absolute path so that things
+    # will bomb out if, for whatever reason, the working directory changes.
+    static_target = (
+        tmp_path / "targets" / ("_match_based_id" + Path(target_name).suffix)
+    )
 
     def run_on_target(subpath):
-        source_target = tmp_path / "targets" / "match_based_id" / subpath / target
+        source_target = tmp_path / "targets" / "match_based_id" / subpath / target_name
         shutil.copy(source_target, static_target)
         results, _ = run_semgrep_on_copied_files(
             rule,
