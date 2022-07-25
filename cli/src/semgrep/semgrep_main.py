b419edc6d64ddf5db271f454fdaa534dbdda9fc5
Ben Kettle <ben@r2c.dev>
2022-07-25 15:06:13 -0700

    put add ignores back in the right place

--- cli/src/semgrep/semgrep_main.py
+++ cli/src/semgrep/semgrep_main.py
@@ -441,7 +441,6 @@ def main(
     profiler.save("total_time", rule_start_time)
 
     metrics = get_state().metrics
-    metrics.add_ignores(target_manager.ignore_log.unsupported_lang_paths)
     if metrics.is_enabled:
         metrics.add_project_url(project_url)
         metrics.add_configs(configs)
@@ -450,6 +449,7 @@ def main(
         metrics.add_findings(filtered_matches_by_rule)
         metrics.add_errors(semgrep_errors)
         metrics.add_profiling(profiler)
+        metrics.add_ignores(target_manager.ignore_log.unsupported_lang_paths)
 
     if autofix:
         apply_fixes(filtered_matches_by_rule.kept, dryrun)
