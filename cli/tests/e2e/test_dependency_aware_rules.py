0b04ecc64bf28a723ca24d19984ae4dcb76e9c02
Matthew McQuaid <matthew@returntocorp.com>
2022-07-25 21:20:00 -0700

    refactor tests

--- cli/tests/e2e/test_dependency_aware_rules.py
+++ cli/tests/e2e/test_dependency_aware_rules.py
@@ -11,30 +11,30 @@ from ..conftest import TESTS_PATH
     [
         (
             "rules/dependency_aware/awscli_vuln.yaml",
-            "dependency_aware",
+            "dependency_aware/awscli",
         ),
         (
             "rules/dependency_aware/lodash-4.17.19.yaml",
-            "dependency_aware",
+            "dependency_aware/lodash",
         ),
         (
             "rules/dependency_aware/no-pattern.yaml",
             "dependency_aware",
         ),
-        ("rules/dependency_aware/go-sca.yaml", "dependency_aware"),
-        ("rules/dependency_aware/ruby-sca.yaml", "dependency_aware"),
-        ("rules/dependency_aware/log4shell.yaml", "dependency_aware"),
-        ("rules/dependency_aware/rust-sca.yaml", "dependency_aware"),
-        ("rules/dependency_aware/ansi-html.yaml", "dependency_aware"),
-        ("rules/dependency_aware/js-sca.yaml", "dependency_aware"),
-        ("rules/dependency_aware/generic-sca.yaml", "dependency_aware"),
+        ("rules/dependency_aware/go-sca.yaml", "dependency_aware/go"),
+        ("rules/dependency_aware/ruby-sca.yaml", "dependency_aware/ruby"),
+        ("rules/dependency_aware/log4shell.yaml", "dependency_aware/log4j"),
+        ("rules/dependency_aware/rust-sca.yaml", "dependency_aware/rust"),
+        ("rules/dependency_aware/ansi-html.yaml", "dependency_aware/ansi"),
+        ("rules/dependency_aware/js-sca.yaml", "dependency_aware/js"),
+        ("rules/dependency_aware/generic-sca.yaml", "dependency_aware/generic"),
         (
             "rules/dependency_aware/java-gradle-sca.yaml",
-            "dependency_aware",
+            "dependency_aware/gradle",
         ),
         (
             "rules/dependency_aware/python-poetry-sca.yaml",
-            "dependency_aware",
+            "dependency_aware/poetry",
         ),
         (
             "rules/dependency_aware/monorepo.yaml",
