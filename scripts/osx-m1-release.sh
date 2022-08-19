f9d93847f2954823086d208ccaceb274b02fe2fc
Martin Jambon <martin@r2c.dev>
2022-08-19 16:42:56 -0700

    This makes the osx and osx-m1 scripts identical. Next: remove one after tests are successful on GitHub Actions

--- scripts/osx-m1-release.sh
+++ scripts/osx-m1-release.sh
@@ -2,7 +2,7 @@
 # Build semgrep-core for MacOS
 #
 # This script is a quasi-duplicate of osx-release.sh. It looks like maybe
-# these files used to be different and but now are identical.
+# these files used to be different but now are identical.
 # TODO: share code with osx-release.sh. Having a command-line option for m1
 # would be preferable over maintaining duplicate code.
 #
@@ -12,14 +12,14 @@ set -eux
 # everything every time
 
 brew update # Needed to sidestep bintray brownout
+brew install opam pkg-config coreutils pcre
+opam init --no-setup --bare;
 #coupling: this should be the same version than in our Dockerfile
-opam switch 4.14.0;
+opam switch create 4.14.0
+opam switch 4.14.0
 git submodule update --init --recursive --depth 1
 
 eval "$(opam env)"
-
-# Needed so we don't make config w/ sudo
-export HOMEBREW_SYSTEM=1
 
 make setup
 
@@ -32,7 +32,7 @@ rm -f /usr/local/opt/pcre/lib/libpcre.1.dylib
 # This needs to be done after make setup but before make build-*
 TREESITTER_LIBDIR=semgrep-core/src/ocaml-tree-sitter-core/tree-sitter/lib
 echo "TREESITTER_LIBDIR is $TREESITTER_LIBDIR and contains:"
-ls -l "$TREESITTER_LIBDIR"
+ls -l "$TREESITTER_LIBDIR" || true
 
 echo "Deleting all the tree-sitter dynamic libraries to force static linking."
 rm -f "$TREESITTER_LIBDIR"/libtree-sitter.0.0.dylib
--- scripts/osx-release.sh
+++ scripts/osx-release.sh
@@ -3,27 +3,27 @@
 set -eux
 
 brew update # Needed to sidestep bintray brownout
-brew install opam pkg-config coreutils
+brew install opam pkg-config coreutils pcre
 opam init --no-setup --bare;
 #coupling: this should be the same version than in our Dockerfile
-opam switch create 4.14.0;
-opam switch 4.14.0;
+opam switch create 4.14.0
+opam switch 4.14.0
 git submodule update --init --recursive --depth 1
 
 eval "$(opam env)"
 
+make setup
+
 # Remove pcre dynamically linked to force MacOS to use static
 # This needs to be done before make setup since it is used there
-ls -l /usr/local/opt/pcre/lib || true  # doesn't exist - where is it?
+ls -l /usr/local/opt/pcre/lib || true
 rm -f /usr/local/opt/pcre/lib/libpcre.1.dylib
-
-make setup
 
 # Remove dynamically linked libraries to force MacOS to use static ones
 # This needs to be done after make setup but before make build-*
 TREESITTER_LIBDIR=semgrep-core/src/ocaml-tree-sitter-core/tree-sitter/lib
 echo "TREESITTER_LIBDIR is $TREESITTER_LIBDIR and contains:"
-ls -l "$TREESITTER_LIBDIR"
+ls -l "$TREESITTER_LIBDIR" || true
 
 echo "Deleting all the tree-sitter dynamic libraries to force static linking."
 rm -f "$TREESITTER_LIBDIR"/libtree-sitter.0.0.dylib
