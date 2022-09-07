e60bc13654d61c03082c102d4695ced1e91e5085
Emma Jin <emjin@users.noreply.github.com>
2022-09-06 21:34:25 -0700

    Annotate test

--- semgrep-core/tests/java/misc_instance_call.java
+++ semgrep-core/tests/java/misc_instance_call.java
@@ -3,7 +3,7 @@ import world.Hello;
 public class test {
     public static void main(String[] argv) throws Exception {
         Object m = new Hello.internalB.f(); // internalB is an internal class of class Hello, f is a staic function in the class B
-        //ruleid: internal_class_instance_call
+        // MATCH: internal_class_instance_call
         Object m = new Hello.internalB().t(); // internalB is an internal class of class Hello, t is an instance function in the class B
     }
 }
