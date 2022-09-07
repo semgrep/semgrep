6eec60929aa61887d33e663c447ba9f5e4e62340
spencerdrak <spencerdrak@users.noreply.github.com>
2022-09-07 15:37:15 +0000

    chore: Bump version to 0.112.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="semgrep_pre_commit_package",
-    version="0.111.1",
-    install_requires=["semgrep==0.111.1"],
+    version="0.112.0",
+    install_requires=["semgrep==0.112.0"],
     packages=[],
 )
