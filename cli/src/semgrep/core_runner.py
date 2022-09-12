a779dcc8b1cbdf8b8e78bd71a2e8f42b254266aa
Emma Jin <emjin@users.noreply.github.com>
2022-09-12 15:06:46 -0700

    Guard removal of rules and targets files (#6084)
    * Guard removal of rules and targets files
    
    Semgrep creates temporary rules and targets files to send to semgrep-core.
    These files may not be removed by the temporary file system. To avoid
    undefined behavior, we try to manually remove the file but do not crash
    if we fail.
    
    Test plan: make test
    
    * Add comments
    
    * Split up try-except for the different removes

--- cli/src/semgrep/core_runner.py
+++ cli/src/semgrep/core_runner.py
@@ -727,6 +727,10 @@ class CoreRunner:
         profiling_data: ProfilingData = ProfilingData()
         parsing_data: ParsingData = ParsingData()
 
+        # Note: temporary file behavior varies based on OS according to
+        # https://docs.python.org/3/library/tempfile.html. In the future
+        # if we support Windows semgrep-core may not be able to open
+        # these files
         rule_file_name = (
             str(state.env.user_data_folder / "semgrep_rules.json")
             if dump_command_for_core
@@ -894,8 +898,19 @@ class CoreRunner:
                     parsing_data.add_error(err)
             errors.extend(parsed_errors)
 
-        os.remove(rule_file_name)
-        os.remove(target_file_name)
+        # We use NamedTemporaryFile for the rules and targets files to
+        # create files with random names, but because of how we open
+        # the files we need to remove them ourselves. However, we are
+        # not guaranteed that a different implementation would not
+        # remove the file. Therefore, we wrap these in try-except.
+        try:
+            os.remove(rule_file_name)
+        except FileNotFoundError:
+            logger.warning(f"Rules file {rule_file_name} already removed")
+        try:
+            os.remove(target_file_name)
+        except FileNotFoundError:
+            logger.warning(f"Targets file {target_file_name} already removed")
 
         return (
             outputs,
