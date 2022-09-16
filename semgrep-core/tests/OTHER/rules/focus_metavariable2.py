da6ed644489ee80d9c7d462dd0e0985b1bf3c841
enncoded <14846866+enncoded@users.noreply.github.com>
2022-09-15 17:51:29 -0700

    Added new `focus-metavariable` test

---
+++ semgrep-core/tests/OTHER/rules/focus_metavariable2.py
@@ -0,0 +1,19 @@
+foo (
+	# ruleid: focus_metavariable2
+	1
+	,
+	# ok: focus_metavariable2
+	2
+	,
+	# ok: focus_metavariable2
+	3
+	,
+	# ruleid: focus_metavariable2
+	4
+)
+
+foo (
+	# ruleid: focus_metavariable2
+	a
+)
+
