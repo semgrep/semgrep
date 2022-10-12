ef7a5dc3dd7237234c23e9b71710d66a4bfe1819
Yoann Padioleau <pad@r2c.dev>
2022-10-12 13:57:04 +0200

    osemgrep: continue adjusting TOPORT (#6290)
    * osemgrep: continue adjusting TOPORT
    
    semgrep_core.py: not needed anymore, osemgrep is linked with
    semgrep-core
    
    test plan:
    make test
    
    * reduce scan.py

--- semgrep-core/src/osemgrep/core/CLI_common.ml
+++ semgrep-core/src/osemgrep/core/CLI_common.ml
@@ -1,31 +1,7 @@
 (*
    Shared parameters, options, and help messages for the semgrep CLI.
 *)
-
-open Printf
 open Cmdliner
-
-(* Turn "a" into "-a" and "abc" into "--abc" *)
-let add_option_dashes option_names =
-  Common.map
-    (fun s ->
-      assert (s <> "");
-      if String.length s = 1 then "-" ^ s else "--" ^ s)
-    option_names
-
-(* Define a flag that can be negated e.g. --foo and --no-foo.
-   It's not supported out-of-the-box by cmdliner but we want it for
-   backward compatibility with the Python CLI.
-   See https://github.com/dbuenzli/cmdliner/issues/164
-*)
-let negatable_flag ?(default = false) ~neg_options ~doc options =
-  let neg_doc =
-    let options_str = add_option_dashes options |> String.concat "/" in
-    sprintf "negates %s" options_str
-  in
-  let enable = (true, Arg.info options ~doc) in
-  let disable = (false, Arg.info neg_options ~doc:neg_doc) in
-  Arg.value (Arg.vflag default [ enable; disable ])
 
 let help_page_bottom =
   [
