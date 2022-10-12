16fcd36202bed4b7c04a9ad9a4e0aa4a200a7b57
Brandon Wu <brandon@r2c.dev>
2022-10-12 12:22:19 -0700

    pfff update, add python generic type function

--- semgrep-core/src/parsing/pfff/Python_to_generic.mli
+++ semgrep-core/src/parsing/pfff/Python_to_generic.mli
@@ -2,6 +2,7 @@ val program :
   ?assign_to_vardef:bool -> AST_python.program -> AST_generic.program
 
 val any : AST_python.any -> AST_generic.any
+val type_for_lsif : AST_python.type_ -> AST_generic.type_
 
 (* exception Error of string * Parse_info.info *)
 (* may raise Error *)
