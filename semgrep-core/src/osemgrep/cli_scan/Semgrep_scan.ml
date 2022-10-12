43ba42501393e1e7e267e93bb3eb6e9f6f42aba3
Yoann Padioleau <pad@r2c.dev>
2022-10-12 15:42:00 +0200

    osemgrep: simplify Scan_CLI.ml and Semgrep_scan.ml (#6292)
    * osemgrep: simplify Scan_CLI.ml and Semgrep_scan.ml
    
    Avoid run callbacks, just return the config in Scan_CLI.ml
    and then run everything in Semgrep_scan.ml
    
    All those run conf wrappers callbacks were confusing me. I think
    it's simpler like that.
    
    test plan:
    make test
    
    * comment
    
    * more type annotations
    
    * misc

--- semgrep-core/src/osemgrep/cli_scan/Semgrep_scan.ml
+++ semgrep-core/src/osemgrep/cli_scan/Semgrep_scan.ml
@@ -4,8 +4,15 @@
 
 (* All the business logic after command-line parsing. Return the desired
    exit code. *)
-let run (conf : Scan_CLI.conf) =
+let run (conf : Scan_CLI.conf) : Exit_code.t =
   let _res = Core_runner.invoke_semgrep conf in
   Exit_code.ok
 
-let main argv = Scan_CLI.parse_and_run argv run
+let main (argv : string array) : Exit_code.t =
+  let res = Scan_CLI.parse_argv argv in
+  (* LATER: this error handling could be factorized probably
+   * between the different subcommands at some point
+   *)
+  match res with
+  | Ok conf -> CLI_common.safe_run run conf
+  | Error exit_code -> exit_code
