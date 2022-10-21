7cf2baf5c19a14d8bced33b20f4a519109e9fc47
Yoann Padioleau <pad@r2c.dev>
2022-10-21 18:31:10 +0200

    Cleanup semgrep-core/src/core, reduce #files (#6369)
    * Cleanup semgrep-core/src/core, reduce #files
    
    This was done mostly using Codegraph, to detect
    what looks like wrong dependencies.
    I've mostly reduced the #files in core/, and move
    many in the new configuring/, some in targeting/,
    and many in reporting/.
    It's arguably cleaner.
    
    test plan:
    make test
    make index
    codegraph .
    and check that the dependencies make sense
    
    * misc
    
    * fix ocamlformat

--- semgrep-core/src/reporting/JSON_report.mli
+++ semgrep-core/src/reporting/JSON_report.mli
@@ -1,11 +1,20 @@
+type render_fix =
+  Lang.t ->
+  Metavariable.bindings ->
+  fix_pattern:string ->
+  target_contents:string lazy_t ->
+  string option
+
 (* Can return an Error because when have a NoTokenLocation exn when
  * trying to get the range of a match or metavar.
  *)
 val match_to_match :
+  render_fix option ->
   Pattern_match.t ->
   (Output_from_core_t.core_match, Semgrep_error_code.error) Common.either
 
 val match_results_of_matches_and_errors :
+  render_fix option ->
   int (* number of files processed, for the stats.okfiles *) ->
   Report.final_result ->
   Output_from_core_t.core_match_results
--- semgrep-core/src/runner/Run_semgrep.ml
+++ semgrep-core/src/runner/Run_semgrep.ml
@@ -800,7 +800,8 @@ let semgrep_with_rules_and_formatted_output config =
   match config.output_format with
   | Json _ -> (
       let res =
-        JSON_report.match_results_of_matches_and_errors (List.length files) res
+        JSON_report.match_results_of_matches_and_errors
+          (Some Autofix.render_fix) (List.length files) res
       in
       (*
         Not pretty-printing the json output (Yojson.Safe.prettify)
@@ -900,7 +901,8 @@ let semgrep_with_one_pattern config =
         semgrep_with_rules config (([ rule ], []), rules_parse_time)
       in
       let json =
-        JSON_report.match_results_of_matches_and_errors (List.length files) res
+        JSON_report.match_results_of_matches_and_errors
+          (Some Autofix.render_fix) (List.length files) res
       in
       let s = Out.string_of_core_match_results json in
       pr s
