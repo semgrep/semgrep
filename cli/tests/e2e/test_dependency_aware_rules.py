b97aca5bf24c8ec5af2f1098d0ab2a9da39e2011
Matthew McQuaid <47466291+mmcqd@users.noreply.github.com>
2022-11-01 16:33:53 -0700

    feat(SSC): Support requirements.txt and yarn 2/3 (#6425)
    * parse yarn 2/3 and requirements.txt
    
    * yarn2 test
    
    * requirements.txt test
    
    * update test, actually search for requirements
    
    * Update pytest snapshots
    
    * changelog
    
    * better parsing for requirements.txt, update test
    
    * update test rule ids
    
    * add some comments
    
    * Update pytest snapshots
    
    * add comments to requirements.txt test
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- cli/tests/e2e/test_dependency_aware_rules.py
+++ cli/tests/e2e/test_dependency_aware_rules.py
@@ -44,6 +44,11 @@ from ..conftest import TESTS_PATH
             "rules/dependency_aware/nested_package_lock.yaml",
             "dependency_aware/nested_package_lock/",
         ),
+        ("rules/dependency_aware/js-yarn2-sca.yaml", "dependency_aware/yarn2"),
+        (
+            "rules/dependency_aware/python-requirements-sca.yaml",
+            "dependency_aware/requirements",
+        ),
     ],
 )
 def test_dependency_aware_rules(run_semgrep_on_copied_files, snapshot, rule, target):
