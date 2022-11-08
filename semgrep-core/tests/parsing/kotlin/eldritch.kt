6d934d252534c7e3d18b249d2fb90ccdf735a6da
Brandon Wu <49291449+brandonspark@users.noreply.github.com>
2022-11-08 11:21:29 -0800

    chore(kotlin): parse constructor calls with trailing commas (#6458)
    * kotlin parsing changes
    
    * added changeloga nd test
    
    * changelog fix
    
    * kill the list.map
    
    * apply suggestions

---
+++ semgrep-core/tests/parsing/kotlin/eldritch.kt
@@ -0,0 +1,20 @@
+// Go ahead. Try to minimize this example.
+// If this ever stops working, you're on your own. This is some real eldritch black magic.
+// Start with the trailing comma after "f".
+
+class a() : b(
+    c = "d",
+    e = "f",
+) {
+
+    private val g = h.i<J>()
+
+    override fun k() {
+        l.m()
+        n()
+    }
+
+    fun o() {
+        p.q(r).s()
+    }
+}
