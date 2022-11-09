a36d6a46c004a3e5295e108e3416df4c682c6a39
pad <pad@r2c.dev>
2022-11-09 14:44:12 +0100

    osemgrep: handle --config auto, r2c, etc.
    test plan:
    ```
    ~/yy/bin/osemgrep --debug --config auto .
    ...
    ```

--- semgrep-core/src/osemgrep/configuring/Semgrep_dashdash_config.ml
+++ semgrep-core/src/osemgrep/configuring/Semgrep_dashdash_config.ml
@@ -18,7 +18,6 @@ open Common
 (* Types *)
 (*****************************************************************************)
 
-(* TODO? Auto *)
 type config_kind =
   (* ex: 'foo.yaml' *)
   | File of Common.filename
@@ -37,6 +36,14 @@ and registry_kind =
   | Snippet of string
   (* ex: 'pad:basic' *)
   | SavedSnippet of string (* username *) * string (* snippetname *)
+  (* shortcuts *)
+  (* "p/default" *)
+  | Auto
+  (* "p/r2c" *)
+  | R2c
+  (* Semgrep App shortcuts *)
+  | Policy
+  | SupplyChain
 [@@deriving show]
 
 (*****************************************************************************)
@@ -44,25 +51,37 @@ and registry_kind =
 (*****************************************************************************)
 let config_kind_of_config_str config_str =
   match config_str with
+  | "auto" -> R Auto
+  | "r2c" -> R R2c
+  | "policy" -> R Policy
+  | "supply-chain" -> R SupplyChain
   | s when s =~ "^r/\\(.*\\)" -> R (Registry (Common.matched1 s))
   | s when s =~ "^p/\\(.*\\)" -> R (Pack (Common.matched1 s))
   | s when s =~ "^s/\\(.*\\)" -> R (Snippet (Common.matched1 s))
+  (* TODO? could not find a Uri.is_url helper function *)
   | s when s =~ "^http[s]?://" -> URL (Uri.of_string s)
+  (* must be after the URL pattern above *)
   | s when s =~ "^\\(.*\\):\\(.*\\)" ->
       let user, snippet = Common.matched2 s in
       R (SavedSnippet (user, snippet))
   | dir when Sys.is_directory dir -> Dir dir
   | file when Sys.file_exists file -> File file
+  (* TOPORT? raise SemgrepError(f"config location `{loc}` is not a file or folder!") *)
   | _else_ -> failwith (spf "not a valid --config string: %s" config_str)
 
 let url_of_registry_kind rkind =
-  (* TODO: go through curl interface for now (c/) *)
+  (* we go through the CURL interface for now (c/) *)
+  (* python: was using env.semgrep_url *)
   let prefix = "https://semgrep.dev/c" in
   let url =
     match rkind with
     | Registry s -> spf "%s/r/%s" prefix s
-    | Pack s -> spf "%s/r/%s" prefix s
+    | Pack s -> spf "%s/p/%s" prefix s
     | Snippet s -> spf "%s/s/%s" prefix s
     | SavedSnippet (user, snippet) -> spf "%s/%s:%s" prefix user snippet
+    | Auto -> spf "%s/p/default" prefix
+    | R2c -> spf "%s/p/r2c" prefix
+    | Policy -> spf "TODO: handle --config policy"
+    | SupplyChain -> spf "TODO: handle --config supply-chain"
   in
   Uri.of_string url
