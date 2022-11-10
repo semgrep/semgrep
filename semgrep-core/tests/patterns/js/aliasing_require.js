4652da692d858f28b1cf7fd3cc3b3cead769f6a6
Nat Mote <nat@r2c.dev>
2022-11-09 17:11:20 -0800

    test(js): Add more tests for matching CJS require
    Semgrep should match on all of these. Handling CommonJS properly in
    Semgrep is also related to handling CommonJS in DeepSemgrep.
    
    Test plan: Automated tests

--- semgrep-core/tests/patterns/js/aliasing_require.js
+++ semgrep-core/tests/patterns/js/aliasing_require.js
@@ -1,3 +1,14 @@
 const { execSync } = require('child_process');
 //ERROR: match
 let ls = execSync("ls");
+
+const { execSync: es } = require('child_process');
+// MATCH:
+es("ls");
+
+const cp = require('child_process');
+// TODO:
+cp.execSync("ls");
+
+// TODO:
+require('child_process').execSync("ls");
