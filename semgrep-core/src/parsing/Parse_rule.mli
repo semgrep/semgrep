0d58119bd304b4a125398f05ad74a3298ded8d08
pad <pad@r2c.dev>
2022-11-16 15:20:48 +0100

    more comments

--- semgrep-core/src/parsing/Parse_rule.mli
+++ semgrep-core/src/parsing/Parse_rule.mli
@@ -1,4 +1,5 @@
-(* Parse a rule file.
+(* Parse a rule file, either in JSON or YAML (or even JSONNET) format
+ * depending on the filename extension.
  *
  * The parser accepts invalid rules, skips them, and returns them in
  * the list of errors.
@@ -11,7 +12,8 @@ val parse_and_filter_invalid_rules :
 
 (* This should be used mostly in testing code. Otherwise you should
  * use parse_and_filter_invalid_rules.
- * This function may raise (Rule.Err ....)
+ * This function may raise (Rule.Err ....) or Assert_failure (when
+ * there are invalid rules).
  *)
 val parse : Common.filename -> Rule.rules
 
@@ -20,5 +22,14 @@ val parse : Common.filename -> Rule.rules
  *)
 val parse_xpattern : Xlang.t -> string Rule.wrap -> Xpattern.t
 
-(* ex: foo.yaml, foo.yml, but not foo.test.yaml *)
+(* ex: foo.yaml, foo.yml, but not foo.test.yaml.
+ *
+ * Note that even if parse() above accepts JSON (and Jsonnet) files,
+ * foo.json (and foo.jsonnet) are currently not considered
+ * valid_rule_filename.
+ *
+ * This function is currently used for osemgrep, to get all
+ * the valid rule files when using --config <DIR>,
+ * and also in Test_engine.ml.
+ *)
 val is_valid_rule_filename : Common.filename -> bool
