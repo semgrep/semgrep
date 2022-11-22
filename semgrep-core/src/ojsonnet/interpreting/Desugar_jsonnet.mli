9395f9c9d26132c8b7650a46a3c5bee57260254e
Yoann Padioleau <pad@r2c.dev>
2022-11-22 11:44:46 +0100

    ojsonnet: skeleton for -dump_jsonnet_core (#6576)
    test plan:
    ```
    $ yy -dump_jsonnet_core string.jsonnet
    ...
    Failure Todo
    ```

--- semgrep-core/src/ojsonnet/interpreting/Desugar_jsonnet.mli
+++ semgrep-core/src/ojsonnet/interpreting/Desugar_jsonnet.mli
@@ -1,4 +1,4 @@
 (* TODO: probably need pass pwd, current file, import_callback so can customize
  * things as the desugar function will "desugar" imports.
  *)
-val desugar : AST_jsonnet.expr -> Core_jsonnet.expr
+val desugar_program : AST_jsonnet.program -> Core_jsonnet.program
