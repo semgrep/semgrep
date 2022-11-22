fe43df2eaa1d2996d4a6fe4d82690e06f86e5166
Yoann Padioleau <pad@r2c.dev>
2022-11-22 11:04:41 +0100

    ojsonnet: add std.jsonnet and fix parser to parse it (#6575)
    * ojsonnet: add std.jsonnet and fix parser to parse it
    
    test plan:
    ```
    $ yy -dump_jsonnet_ast string.jsonnet
    (Local ((),
       [(B (("x", ()), (), (L (Str (None, SingleQuote, ((), [("foo", ())], ()))))
           ))
         ],
       (), (L (Number ("1", ())))))
    $ yy -dump_jsonnet_ast ~/std.jsonnet
    ...
    ```
    
    * add Std_jsonnet.ml and fix Parse_jsonnet.ml

--- semgrep-core/src/ojsonnet/interpreting/Parse_jsonnet.mli
+++ semgrep-core/src/ojsonnet/interpreting/Parse_jsonnet.mli
@@ -1,1 +1,4 @@
+(* This should really be in ../parsing/ but that would lead to circular dependencies
+ * because we would use Parse_jsonnet_tree_sitter.ml which itself use AST_jsonnet.ml.
+ *)
 val parse_program : Common.filename -> AST_jsonnet.program
