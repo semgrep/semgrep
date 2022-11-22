cb7f3e371d824380ed116e4243255c35713a8f8e
pad <pad@r2c.dev>
2022-11-22 10:44:59 +0100

    add Std_jsonnet.ml and fix Parse_jsonnet.ml

--- semgrep-core/src/ojsonnet/interpreting/Parse_jsonnet.ml
+++ semgrep-core/src/ojsonnet/interpreting/Parse_jsonnet.ml
@@ -4,9 +4,18 @@
  *
  * This should really be in ../parsing/ but that would lead to circular dependencies
  * because we would use Parse_jsonnet_tree_sitter.ml which itself use AST_jsonnet.ml.
- *
- *
  *)
 
-let parse_program (_file : Common.filename) : AST_jsonnet.program =
-  failwith "TODO"
+let parse_program (file : Common.filename) : AST_jsonnet.program =
+  let res = Parse_jsonnet_tree_sitter.parse file in
+  (* similar to Parse_target.run_parser and the TreeSitter case *)
+  match (res.program, res.errors) with
+  | Some ast, [] -> ast
+  | None, [] ->
+      failwith "internal error: failed to recover typed tree from treesitter"
+  (* we don't care about partial parsing error; we need all the code for
+   * ojsonnet; we can't be satisfied by partial code.
+   *)
+  | (Some _ | None), x :: _xs ->
+      let e = Parse_target.error_of_tree_sitter_error x in
+      Exception.reraise e
--- semgrep-core/src/ojsonnet/tests/Test_ojsonnet.ml
+++ semgrep-core/src/ojsonnet/tests/Test_ojsonnet.ml
@@ -2,7 +2,6 @@ open Common
 
 let dump_jsonnet_ast file =
   (* let cst = Tree_sitter_jsonnet.Parse.file file in *)
-  let res = Parse_jsonnet_tree_sitter.parse file in
-  match res.program with
-  | None -> failwith "unknown error from tree-sitter parser"
-  | Some ast -> pr2 (AST_jsonnet.show_program ast)
+  (* let res = Parse_jsonnet_tree_sitter.parse file in *)
+  let ast = Parse_jsonnet.parse_program file in
+  pr2 (AST_jsonnet.show_program ast)
