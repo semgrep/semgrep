6f38298153642124ef4c61eeffc85a22773943f6
brandonspark <wu.brandonj@gmail.com>
2022-11-30 18:30:34 -0800

    add autofix tests for overlapping matches, along with deduplication code

--- cli/src/semgrep/autofix.py
+++ cli/src/semgrep/autofix.py
@@ -1,11 +1,15 @@
 import re
+from functools import cmp_to_key
 from pathlib import Path
 from typing import Dict
+from typing import Iterable
 from typing import List
 from typing import Set
 from typing import Tuple
 
 from semgrep.error import SemgrepError
+from semgrep.rule import Rule
+from semgrep.rule_match import OrderedRuleMatchList
 from semgrep.rule_match import RuleMatch
 from semgrep.rule_match import RuleMatchMap
 from semgrep.util import unit_str
@@ -130,6 +134,71 @@ def _write_contents(path: Path, contents: str) -> None:
     path.write_text(contents)
 
 
+def matches_compare(x: RuleMatch, y: RuleMatch) -> int:
+    # If paths are not the same, just order based on that.
+    # I just need a total ordering on matches.
+    if x.path < y.path:
+        return -1
+    elif x.path > y.path:
+        return 1
+    else:
+        if x.start.offset < y.start.offset:
+            return -1
+        elif y.start.offset < x.start.offset:
+            return 1
+        elif x.start.offset == y.start.offset:
+            if x.end.offset < y.end.offset:
+                return -1
+            elif x.end.offset > y.end.offset:
+                return 1
+            elif x.end.offset == y.end.offset:
+                return 0
+
+
+def matches_overlap(x: RuleMatch, y: RuleMatch) -> bool:
+    if x.path == y.path:
+        if x.start.offset < y.start.offset:
+            return x.end.offset > y.start.offset
+        elif y.start.offset < x.start.offset:
+            return y.end.offset > x.start.offset
+        elif x.start.offset == y.start.offset:
+            return True
+
+    # If they are not from the same file, they cannot overlap.
+    return False
+
+
+def deduplicate_overlapping_matches(
+    rules_and_matches: Iterable[Tuple[Rule, OrderedRuleMatchList]]
+) -> OrderedRuleMatchList:
+
+    final_matches = []
+
+    ordered_matches = sorted(
+        (match for _, matches in rules_and_matches for match in matches),
+        key=cmp_to_key(matches_compare),
+    )
+    acc = None
+
+    for match in ordered_matches:
+        if acc is None:
+            acc = match
+            continue
+
+        if matches_overlap(acc, match):
+            logger.info("Two autofix matches overlap, arbitrarily picking first one")
+            # Don't do anything, keep `acc` the same, and throw `match` out.`
+
+        else:
+            final_matches.append(acc)
+            acc = match
+
+    if acc is not None:
+        final_matches.append(acc)
+
+    return final_matches
+
+
 def apply_fixes(rule_matches_by_rule: RuleMatchMap, dryrun: bool = False) -> None:
     """
     Modify files in place for all files with findings from rules with an
@@ -137,46 +206,50 @@ def apply_fixes(rule_matches_by_rule: RuleMatchMap, dryrun: bool = False) -> Non
     """
     modified_files: Set[Path] = set()
     modified_files_offsets: Dict[Path, FileOffsets] = {}
-    for _, rule_matches in rule_matches_by_rule.items():
-        for rule_match in rule_matches:
-            fix = rule_match.fix
-            fix_regex = rule_match.fix_regex
-            filepath = rule_match.path
-            # initialize or retrieve/update offsets for the file
-            file_offsets = modified_files_offsets.get(
-                filepath, FileOffsets(0, 0, rule_match.start.line)
-            )
-            if file_offsets.active_line != rule_match.start.line:
-                file_offsets.active_line = rule_match.start.line
-                file_offsets.col_offset = 0
-            if fix:
-                try:
-                    fixobj, new_file_offset = _basic_fix(rule_match, file_offsets, fix)
-                except Exception as e:
-                    raise SemgrepError(f"unable to modify file {filepath}: {e}")
-            elif fix_regex:
-                regex = fix_regex.regex
-                replacement = fix_regex.replacement
-                count = fix_regex.count or 0
-                try:
-                    fixobj, new_file_offset = _regex_replace(
-                        rule_match, file_offsets, regex, replacement, count
-                    )
-                except Exception as e:
-                    raise SemgrepError(
-                        f"unable to use regex to modify file {filepath} with fix '{fix}': {e}"
-                    )
-            else:
-                continue
-            # endif
-            if not dryrun:
-                _write_contents(rule_match.path, fixobj.fixed_contents)
-                modified_files.add(filepath)
-                modified_files_offsets[filepath] = new_file_offset
-            else:
-                rule_match.extra[
-                    "fixed_lines"
-                ] = fixobj.fixed_lines  # Monkey patch in fixed lines
+
+    nonoverlapping_matches = deduplicate_overlapping_matches(
+        rule_matches_by_rule.items()
+    )
+
+    for rule_match in nonoverlapping_matches:
+        fix = rule_match.fix
+        fix_regex = rule_match.fix_regex
+        filepath = rule_match.path
+        # initialize or retrieve/update offsets for the file
+        file_offsets = modified_files_offsets.get(
+            filepath, FileOffsets(0, 0, rule_match.start.line)
+        )
+        if file_offsets.active_line != rule_match.start.line:
+            file_offsets.active_line = rule_match.start.line
+            file_offsets.col_offset = 0
+        if fix:
+            try:
+                fixobj, new_file_offset = _basic_fix(rule_match, file_offsets, fix)
+            except Exception as e:
+                raise SemgrepError(f"unable to modify file {filepath}: {e}")
+        elif fix_regex:
+            regex = fix_regex.regex
+            replacement = fix_regex.replacement
+            count = fix_regex.count or 0
+            try:
+                fixobj, new_file_offset = _regex_replace(
+                    rule_match, file_offsets, regex, replacement, count
+                )
+            except Exception as e:
+                raise SemgrepError(
+                    f"unable to use regex to modify file {filepath} with fix '{fix}': {e}"
+                )
+        else:
+            continue
+        # endif
+        if not dryrun:
+            _write_contents(rule_match.path, fixobj.fixed_contents)
+            modified_files.add(filepath)
+            modified_files_offsets[filepath] = new_file_offset
+        else:
+            rule_match.extra[
+                "fixed_lines"
+            ] = fixobj.fixed_lines  # Monkey patch in fixed lines
 
     if not dryrun:
         if len(modified_files):
