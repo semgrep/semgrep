semgrep/semgrep

commit 44b4506575042965b096890e6e5a18e980e7b18e

semgrep ci runs DeepSemgrep when it is enabled in App (#6624)
Closes PA-2226

cli/src/semgrep/commands/ci.py
@@ -320,6 +320,10 @@ def ci(
                 logger.info(f"Could not start scan {e}")
                 sys.exit(FATAL_EXIT_CODE)
 
+            # Run DeepSemgrep when available but only for full scans
+            is_full_scan = metadata.merge_base_ref is None
+            deep = scan_handler.deepsemgrep if scan_handler and is_full_scan else False
+
             # Append ignores configured on semgrep.dev
             requested_excludes = scan_handler.ignore_patterns if scan_handler else []
             if requested_excludes:
@@ -346,6 +350,7 @@ def ci(
                 lockfile_scan_info,
             ) = semgrep.semgrep_main.main(
                 core_opts_str=core_opts,
+                deep=deep,
                 output_handler=output_handler,
                 target=[os.curdir],  # semgrep ci only scans cwd
                 pattern=None,

cli/src/semgrep/app/scans.py
@@ -37,6 +37,7 @@ class ScanHandler:
         self.ignore_patterns: List[str] = []
         self._policy_names: List[str] = []
         self._autofix = False
+        self._deepsemgrep = False
         self.dry_run = dry_run
         self._dry_run_rules_url: str = ""
         self._skipped_syntactic_ids: List[str] = []
@@ -47,56 +48,63 @@ class ScanHandler:
     @property
     def deployment_id(self) -> Optional[int]:
         """
-        Seperate property for easy of mocking in test
+        Separate property for easy of mocking in test
         """
         return self._deployment_id
 
     @property
     def deployment_name(self) -> str:
         """
-        Seperate property for easy of mocking in test
+        Separate property for easy of mocking in test
         """
         return self._deployment_name
 
     @property
     def policy_names(self) -> List[str]:
         """
-        Seperate property for easy of mocking in test
+        Separate property for easy of mocking in test
         """
         return self._policy_names
 
     @property
     def autofix(self) -> bool:
         """
-        Seperate property for easy of mocking in test
+        Separate property for easy of mocking in test
         """
         return self._autofix
 
     @property
+    def deepsemgrep(self) -> bool:
+        """
+        Separate property for easy of mocking in test
+        """
+        return self._deepsemgrep
+
+    @property
     def skipped_syntactic_ids(self) -> List[str]:
         """
-        Seperate property for easy of mocking in test
+        Separate property for easy of mocking in test
         """
         return self._skipped_syntactic_ids
 
     @property
     def skipped_match_based_ids(self) -> List[str]:
         """
-        Seperate property for easy of mocking in test
+        Separate property for easy of mocking in test
         """
         return self._skipped_match_based_ids
 
     @property
     def scan_params(self) -> str:
         """
-        Seperate property for easy of mocking in test
+        Separate property for easy of mocking in test
         """
         return self._scan_params
 
     @property
     def rules(self) -> str:
         """
-        Seperate property for easy of mocking in test
+        Separate property for easy of mocking in test
         """
         return self._rules
 
@@ -182,6 +190,7 @@ class ScanHandler:
         self._policy_names = body["policy_names"]
         self._rules = body["rule_config"]
         self._autofix = body.get("autofix") or False
+        self._deepsemgrep = body.get("deepsemgrep") or False
         self._skipped_syntactic_ids = body.get("triage_ignored_syntactic_ids") or []
         self._skipped_match_based_ids = body.get("triage_ignored_match_based_ids") or []
         self.ignore_patterns = body.get("ignored_files") or []

commit 47217fa8d10dd228d8e28704de2e0549534dc8fd

semgrep ci installs DeepSemgrep automatically when needed
Fixes: 44b45065750 ("semgrep ci runs DeepSemgrep when it is enabled in App (#6624)")

test plan:
Remove deep-semgrep, enable DeepSemgrep in App/Settings, and
run `semgrep ci` on a Java repo

cli/src/semgrep/commands/ci.py
@@ -16,6 +16,7 @@ import click
 import semgrep.semgrep_main
 from semgrep.app import auth
 from semgrep.app.scans import ScanHandler
+from semgrep.commands.install import run_install_deep_semgrep
 from semgrep.commands.scan import CONTEXT_SETTINGS
 from semgrep.commands.scan import scan_options
 from semgrep.commands.wrapper import handle_command_errors
@@ -33,6 +34,7 @@ from semgrep.output import OutputSettings
 from semgrep.project import ProjectConfig
 from semgrep.rule import Rule
 from semgrep.rule_match import RuleMatchMap
+from semgrep.semgrep_core import SemgrepCore
 from semgrep.state import get_state
 from semgrep.util import git_check_output
 from semgrep.util import unit_str
@@ -323,6 +325,10 @@ def ci(
             # Run DeepSemgrep when available but only for full scans
             is_full_scan = metadata.merge_base_ref is None
             deep = scan_handler.deepsemgrep if scan_handler and is_full_scan else False
+            SemgrepCore.path()  # initializes a SemgrepCore class-field needed for .deep_path() to work
+            deep_path = SemgrepCore.deep_path()
+            if deep and deep_path is None:
+                run_install_deep_semgrep()
 
             # Append ignores configured on semgrep.dev
             requested_excludes = scan_handler.ignore_patterns if scan_handler else []

cli/src/semgrep/commands/install.py
@@ -20,18 +20,7 @@ from semgrep.verbose_logging import getLogger
 logger = getLogger(__name__)
 
 
-@click.command(hidden=True)
-@handle_command_errors
-def install_deep_semgrep() -> None:
-    """
-    Install the DeepSemgrep binary (Experimental)
-
-    The binary is installed in the same directory that semgrep-core
-    is installed in. Does not reinstall if existing binary already exists.
-
-    Must be logged in and have access to DeepSemgrep beta
-    Visit https://semgrep.dev/deep-semgrep-beta for more information
-    """
+def run_install_deep_semgrep() -> None:
     state = get_state()
     state.terminal.configure(verbose=False, debug=False, quiet=False, force_color=False)
 
@@ -106,3 +95,18 @@ def install_deep_semgrep() -> None:
         stderr=subprocess.DEVNULL,
     ).rstrip()
     logger.info(f"DeepSemgrep Version Info: ({version})")
+
+
+@click.command(hidden=True)
+@handle_command_errors
+def install_deep_semgrep() -> None:
+    """
+    Install the DeepSemgrep binary (Experimental)
+
+    The binary is installed in the same directory that semgrep-core
+    is installed in.
+
+    Must be logged in and have access to DeepSemgrep beta
+    Visit https://semgrep.dev/deep-semgrep-beta for more information
+    """
+    run_install_deep_semgrep()