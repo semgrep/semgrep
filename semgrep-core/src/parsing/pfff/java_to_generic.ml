e119e66168670c6c74aea7cfd47c13c71450075c
Brandon Wu <49291449+brandonspark@users.noreply.github.com>
2022-12-06 02:06:06 -0800

    chore: parse java interface stubs to have FBNothing (#6661)
    * parse java interface stubs to have FBNothing
    
    * precommit ahh
    
    * changelog

--- semgrep-core/src/parsing/pfff/java_to_generic.ml
+++ semgrep-core/src/parsing/pfff/java_to_generic.ml
@@ -600,7 +600,7 @@ and parameter_binding = function
       G.ParamRest (tk, p)
   | ParamEllipsis t -> G.ParamEllipsis t
 
-and method_decl { m_var; m_formals; m_throws; m_body } =
+and method_decl ?(cl_kind = None) { m_var; m_formals; m_throws; m_body } =
   let ent, rett = var m_var in
   let v2 = parameters m_formals in
   let v3 = list typ m_throws in
@@ -610,13 +610,13 @@ and method_decl { m_var; m_formals; m_throws; m_body } =
     v3
     |> Common.map (fun t -> G.OtherAttribute (("Throw", G.fake ""), [ G.T t ]))
   in
+  let fbody =
+    match (cl_kind, v4) with
+    | Some (Interface, _), { s = G.Block (_, [], _); _ } -> G.FBNothing
+    | _ -> FBStmt v4
+  in
   ( { ent with G.attrs = ent.G.attrs @ throws },
-    {
-      G.fparams = v2;
-      frettype = rett;
-      fbody = G.FBStmt v4;
-      fkind = (G.Method, G.fake "");
-    } )
+    { G.fparams = v2; frettype = rett; fbody; fkind = (G.Method, G.fake "") } )
 
 and field v = var_with_init v
 
@@ -649,8 +649,8 @@ and enum_constant (v1, v2, v3) =
   let def = { G.ee_args = v2; ee_body = v3 } in
   (ent, G.EnumEntryDef def)
 
-and class_body (l, xs, r) : G.field list G.bracket =
-  let xs = decls xs |> Common.map (fun x -> G.F x) in
+and class_body ?(cl_kind = None) (l, xs, r) : G.field list G.bracket =
+  let xs = decls ~cl_kind xs |> Common.map (fun x -> G.F x) in
   (l, xs, r)
 
 and class_decl
@@ -671,7 +671,7 @@ and class_decl
   let v5 = option class_parent cl_extends in
   let v6 = list ref_type cl_impls in
   let cparams = parameters cl_formals in
-  let fields = class_body cl_body in
+  let fields = class_body ~cl_kind:(Some cl_kind) cl_body in
   let ent = G.basic_entity v1 ~attrs:(more_attrs @ v4) ~tparams:v3 in
   let cdef =
     {
@@ -692,13 +692,13 @@ and class_kind_and_more (x, t) =
   | AtInterface -> ((G.Interface, t), [ G.attr AnnotationClass t ])
   | Record -> ((G.Class, t), [ G.attr RecordClass t ])
 
-and decl decl : G.stmt =
+and decl ?(cl_kind = None) decl : G.stmt =
   match decl with
   | Class v1 ->
       let ent, def = class_decl v1 in
       G.DefStmt (ent, G.ClassDef def) |> G.s
   | Method v1 ->
-      let ent, def = method_decl v1 in
+      let ent, def = method_decl ~cl_kind v1 in
       G.DefStmt (ent, G.FuncDef def) |> G.s
   | Field v1 ->
       let ent, def = field v1 in
@@ -713,7 +713,7 @@ and decl decl : G.stmt =
   | EmptyDecl t -> G.Block (t, [], t) |> G.s
   | AnnotationTypeElementTodo t -> G.OtherStmt (G.OS_Todo, [ G.Tk t ]) |> G.s
 
-and decls v : G.stmt list = list decl v
+and decls ?(cl_kind = None) v : G.stmt list = list (decl ~cl_kind) v
 
 and import = function
   | ImportAll (t, xs, tok) -> G.ImportAll (t, G.DottedName xs, tok)
