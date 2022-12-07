semgrep/semgrep

commit 1eb330d6887d33e9ca0c8c0730a36727736aeca0

tainting: Make identification of sinks more precise (#6640)
* tainting: Make identification of sinks more precise

This way we will not incorrectly report `sink(ok1 if tainted else ok2)`
as a tainted sink. Taint-mode will no longer consider that `tainted`
itself is being passed into a sink, but the whole if-expression.

Closes PA-2142

test plan:
make test # tests added

* Emma's review

semgrep-core/src/tainting/Dataflow_tainting.ml
@@ -65,7 +65,13 @@ module SMap = Map.Make (String)
 
 type var = Var_env.var
 type overlap = float
-type 'spec tmatch = { spec : 'spec; pm : PM.t; overlap : overlap }
+
+type 'spec tmatch = {
+  spec : 'spec;
+  spec_pm : PM.t;
+  range : Range.t;
+  overlap : overlap;
+}
 
 type a_propagator = {
   kind : [ `From | `To ];
@@ -76,7 +82,7 @@ type a_propagator = {
 type config = {
   filepath : Common.filename;
   rule_id : string;
-  is_source : G.any -> Rule.taint_source tmatch list;
+  is_source : G.any -> R.taint_source tmatch list;
   is_propagator : AST_generic.any -> a_propagator tmatch list;
   is_sink : G.any -> R.taint_sink tmatch list;
   is_sanitizer : G.any -> R.taint_sanitizer tmatch list;
@@ -94,12 +100,90 @@ type mapping = Lval_env.t D.mapping
 (* HACK: Tracks tainted functions intrafile. *)
 type fun_env = (var, Taints.t) Hashtbl.t
 
+(* NOTE "Top sinks":
+ * (See note "Taint-tracking via ranges" in 'Match_tainting_mode.ml' for context.)
+ *
+ * We use sub-range checks to determine whether a piece of code is a sink, and this
+ * can lead to unintuitive results. For example, `sink(if tainted then ok1 else ok2)`
+ * is reported as a tainted sink despite `if tainted then ok1 else ok2` is actually
+ * not tainted. The problem is that `tainted` is inside what `sink(...)` matches,
+ * and using a sub-range check we end up considering `tainted` itself as a sink.
+ *
+ * Unfortunately simply checking for exact matches is fragile, because sometimes we
+ * are not able to match sinks exactly. For example, if you want `echo ...;` to be a
+ * sink in PHP, you cannot omit the ';' as `echo` is not an expression. But in the
+ * IL, `echo` is represented as a `Call` instruction and the ';' is not part of the
+ * `iorig`.
+ *
+ * A simple alternative would have been to add an `exact: true` option to request
+ * exact matches. This is trivial to implement but it is more a "patch" than a
+ * proper fix. It breaks some taint rules, and users would often not understand when
+ * and why they need to set (or unset) this option to make their rules work.
+ *
+ * So, instead, we went for a solution that requires no extra option, it just works,
+ * but it is a lot more complex to implement. Given a sink specification, we
+ * check whether the sink matches any of the top-level nodes in the CFG (see
+ * 'IL.node_kind'). Given two sink matches, if one is contained inside the other,
+ * then we consider them the same match and we take the larger one as the canonical.
+ * We store the canonical sink matches in a 'Top_sinks' data structure, and we use
+ * these "top sinks" matches as a practical definition of what an "exact match" is.
+ * For example, if the sink specification is `echo ...;`, and the code we have is
+ * `echo $_GET['foo'];`, then this method will determine that `echo $_GET['foo']`
+ * is the best match we can get, and that becomes the definition of exact. Then,
+ * when we check whether an expression or instruction is a sink, if its range is a
+ * strict sub-range of one of these top sinks, we simply disregard it (because it
+ * is not an exact match). In our example above the top sink match will be
+ * `sink(if tainted then ok1 else ok2)`, so we will disregard `tainted` as a sink
+ * because we know there is a better match.
+ *)
+module Top_sinks = struct
+  module S = Set.Make (struct
+    type t = R.taint_sink tmatch
+
+    let compare m1 m2 =
+      let sink_id_cmp = String.compare m1.spec.R.sink_id m2.spec.R.sink_id in
+      if sink_id_cmp <> 0 then sink_id_cmp
+      else
+        (* If m1 is contained in m2 or vice-versa, then they are the *same* sink match.
+         * We only want to keep one match per sink, the best match! *)
+        let r1 = m1.range in
+        let r2 = m2.range in
+        if Range.(r1 $<=$ r2 || r2 $<=$ r1) then 0 else Stdlib.compare r1 r2
+  end)
+
+  type t = S.t
+
+  let empty = S.empty
+
+  let add m' sinks =
+    (* We check if we have another match for the *same* sink specification
+     * (i.e., same 'sink_id'), and if so we must keep the best match and drop
+     * the other one. *)
+    match S.find_opt m' sinks with
+    | None -> S.add m' sinks
+    | Some m ->
+        let r = m.range in
+        let r' = m'.range in
+        (* Note that by `S`s definition, either `r` is contained in `r'` or vice-versa. *)
+        if r'.start > r.start || r'.end_ < r.end_ then
+          (* The new match is a worse fit so we keep the current one. *)
+          sinks
+        else (* We found a better (larger) match! *)
+          S.add m' (S.remove m sinks)
+
+  let is_best_match sinks m' =
+    match S.find_opt m' sinks with
+    | None -> true
+    | Some m -> m.range = m'.range
+end
+
 (* THINK: Separate read-only enviroment into a new a "cfg" type? *)
 type env = {
   options : Config_semgrep.t; (* rule options *)
   config : config;
   fun_name : var option;
   lval_env : Lval_env.t;
+  top_sinks : Top_sinks.t;
 }
 
 (*****************************************************************************)
@@ -152,6 +236,10 @@ let orig_is_source config orig = config.is_source (any_of_orig orig)
 let orig_is_sanitized config orig = config.is_sanitizer (any_of_orig orig)
 let orig_is_sink config orig = config.is_sink (any_of_orig orig)
 
+let orig_is_best_sink env orig =
+  orig_is_sink env.config orig
+  |> List.filter (Top_sinks.is_best_match env.top_sinks)
+
 let any_of_lval lval =
   match lval with
   | { rev_offset = { oorig; _ } :: _; _ } -> any_of_orig oorig
@@ -164,14 +252,36 @@ let any_of_lval lval =
 let lval_is_source config lval = config.is_source (any_of_lval lval)
 let lval_is_sanitized config lval = config.is_sanitizer (any_of_lval lval)
 let lval_is_sink config lval = config.is_sink (any_of_lval lval)
-let trace_of_match x = T.trace_of_pm (x.pm, x.spec)
+let trace_of_match x = T.trace_of_pm (x.spec_pm, x.spec)
 
 let taints_of_matches xs =
-  xs |> Common.map (fun x -> (x.pm, x.spec)) |> T.taints_of_pms
+  xs |> Common.map (fun x -> (x.spec_pm, x.spec)) |> T.taints_of_pms
 
 let report_findings env findings =
   if findings <> [] then
     env.config.handle_findings env.fun_name findings env.lval_env
+
+let top_level_sinks_in_nodes config flow =
+  flow.CFG.reachable |> CFG.NodeiSet.to_seq
+  |> Seq.concat_map (fun ni ->
+         let node = flow.CFG.graph#nodes#assoc ni in
+         match node.n with
+         | NInstr instr -> orig_is_sink config instr.iorig |> List.to_seq
+         | NCond (_, exp)
+         | NReturn (_, exp)
+         | NThrow (_, exp) ->
+             orig_is_sink config exp.eorig |> List.to_seq
+         | Enter
+         | Exit
+         | TrueNode
+         | FalseNode
+         | Join
+         | NGoto _
+         | NLambda _
+         | NOther _
+         | NTodo _ ->
+             Seq.empty)
+  |> Seq.fold_left (fun s x -> Top_sinks.add x s) Top_sinks.empty
 
 let unify_mvars_sets mvars1 mvars2 =
   let xs =
@@ -488,7 +598,11 @@ let rec check_tainted_lval env (lval : IL.lval) : Taints.t * Lval_env.t =
   let new_taints, lval_in_env, lval_env = check_tainted_lval_aux env lval in
   let taints_from_env = status_to_taints lval_in_env in
   let taints = Taints.union new_taints taints_from_env in
-  let sinks = lval_is_sink env.config lval |> Common.map trace_of_match in
+  let sinks =
+    lval_is_sink env.config lval
+    |> List.filter (Top_sinks.is_best_match env.top_sinks)
+    |> Common.map trace_of_match
+  in
   let findings = findings_of_tainted_sinks { env with lval_env } taints sinks in
   report_findings { env with lval_env } findings;
   (taints, lval_env)
@@ -648,7 +762,7 @@ and check_tainted_expr env exp : Taints.t * Lval_env.t =
       (Taints.empty, lval_env)
   | None ->
       let sinks =
-        orig_is_sink env.config exp.eorig |> Common.map trace_of_match
+        orig_is_best_sink env exp.eorig |> Common.map trace_of_match
       in
       let taints_sources =
         orig_is_source env.config exp.eorig |> taints_of_matches
@@ -717,6 +831,11 @@ let check_function_signature env fun_exp args_taints =
   | Some _, _ ->
       None
 
+let check_orig_if_sink env orig taints =
+  let sinks = orig_is_best_sink env orig |> Common.map trace_of_match in
+  let findings = findings_of_tainted_sinks env taints sinks in
+  report_findings env findings
+
 (* Test whether an instruction is tainted, and if it is also a sink,
  * report the finding too (by side effect). *)
 (* TODO: This should return a new var_env rather than just taint, it
@@ -743,18 +862,24 @@ let check_tainted_instr env instr : Taints.t * Lval_env.t =
           |> Common2.swap
         in
         let e_taints, lval_env = check_expr { env with lval_env } e in
+        let all_args_taints =
+          List.fold_left Taints.union Taints.empty all_taints
+        in
         let call_taints =
           match check_function_signature env e args_taints with
           | Some call_taints -> call_taints
           | None ->
-              if propagate_through_functions env then
-                (* Assume that the function will propagate
-                   * the taint of its arguments. *)
-                List.fold_left Taints.union e_taints all_taints
-              else
+              if not (propagate_through_functions env) then (
                 (* If we asume that function calls are "safe", then taints from
-                 * the arguments are not present in the function's output. *)
-                e_taints
+                 * the arguments are not present in the function's output.
+                 * If the function is a sink, then we consider that all its
+                 * parameters are sinks. *)
+                check_orig_if_sink env instr.iorig all_args_taints;
+                e_taints)
+              else
+                (* Otherwise assume that the function will propagate
+                   * the taint of its arguments. *)
+                Taints.union e_taints all_args_taints
         in
         (call_taints, lval_env)
     | CallSpecial (_, _, args) ->
@@ -771,7 +896,7 @@ let check_tainted_instr env instr : Taints.t * Lval_env.t =
       (Taints.empty, env.lval_env)
   | [] ->
       let sinks =
-        orig_is_sink env.config instr.iorig |> Common.map trace_of_match
+        orig_is_best_sink env instr.iorig |> Common.map trace_of_match
       in
       let taint_sources =
         orig_is_source env.config instr.iorig |> taints_of_matches
@@ -795,6 +920,7 @@ let check_tainted_instr env instr : Taints.t * Lval_env.t =
 let check_tainted_return env tok e : Taints.t * Lval_env.t =
   let sinks =
     env.config.is_sink (G.Tk tok) @ orig_is_sink env.config e.eorig
+    |> List.filter (Top_sinks.is_best_match env.top_sinks)
     |> Common.map trace_of_match
   in
   let taints, var_env' = check_tainted_expr env e in
@@ -820,21 +946,24 @@ let input_env ~enter_env ~(flow : F.cfg) mapping ni =
       | [ penv ] -> penv
       | penv1 :: penvs -> List.fold_left Lval_env.union penv1 penvs)
 
-let (transfer :
-      Config_semgrep.t ->
-      config ->
-      Lval_env.t ->
-      string option ->
-      flow:F.cfg ->
-      Lval_env.t D.transfn) =
- fun options config enter_env opt_name ~flow
+let transfer :
+    Config_semgrep.t ->
+    config ->
+    Lval_env.t ->
+    string option ->
+    flow:F.cfg ->
+    top_sinks:Top_sinks.t ->
+    Lval_env.t D.transfn =
+ fun options config enter_env opt_name ~flow ~top_sinks
      (* the transfer function to update the mapping at node index ni *)
        mapping ni ->
   (* DataflowX.display_mapping flow mapping show_tainted; *)
   let in' : Lval_env.t = input_env ~enter_env ~flow mapping ni in
   let node = flow.graph#nodes#assoc ni in
   let out' : Lval_env.t =
-    let env = { options; config; fun_name = opt_name; lval_env = in' } in
+    let env =
+      { options; config; fun_name = opt_name; lval_env = in'; top_sinks }
+    in
     match node.F.n with
     | NInstr x ->
         let taints, lval_env' = check_tainted_instr env x in
@@ -842,9 +971,15 @@ let (transfer :
         let lval_env' =
           match opt_lval with
           | Some lval ->
-              (* We call `check_tainted_var` here because the assigned `var`
+              (* We call `check_tainted_lval` here because the assigned `lval`
                * itself could be annotated as a source of taint. *)
-              check_tainted_lval { env with lval_env = lval_env' } lval |> snd
+              let taints, lval_env' =
+                check_tainted_lval { env with lval_env = lval_env' } lval
+              in
+              (* We check if the instruction is a sink, and if so the taints
+               * from the `lval` could make a finding. *)
+              check_orig_if_sink env x.iorig taints;
+              lval_env'
           | None -> lval_env'
         in
         let lval_env' =
@@ -911,9 +1046,15 @@ let (fixpoint :
     | None -> Lval_env.empty
     | Some in_env -> in_env
   in
+  let top_sinks =
+    (* Here we compute the "canonical" or "top" sink matches, for each sink we check
+     * whether there is a "best match" among the top nodes in the CFG.
+     * See NOTE "Top sinks" *)
+    top_level_sinks_in_nodes config flow
+  in
   (* THINK: Why I cannot just update mapping here ? if I do, the mapping gets overwritten later on! *)
   (* DataflowX.display_mapping flow init_mapping show_tainted; *)
   DataflowX.fixpoint ~eq_env:Lval_env.equal ~init:init_mapping
-    ~trans:(transfer options config enter_env opt_name ~flow)
+    ~trans:(transfer options config enter_env opt_name ~flow ~top_sinks)
       (* tainting is a forward analysis! *)
     ~forward:true ~flow

semgrep-core/src/core/Rule.ml
@@ -172,6 +172,7 @@ and taint_sanitizer = {
 }
 
 and taint_sink = {
+  sink_id : string;  (** See 'Parse_rule.parse_taint_sink'. *)
   sink_formula : formula;
   sink_requires : AST_generic.expr;
       (* A Boolean expression over taint labels. See also 'taint_source'.

semgrep-core/src/engine/Match_tainting_mode.ml
@@ -64,7 +64,7 @@ let logger = Logging.get_logger [ __MODULE__ ]
  * limited to expressions or single statements.
  *
  * However, using sub-range checks leads to duplicates. For example, the PHP
- * expression `sink("$source" . 'here')` will be transalted to IL as two
+ * expression `sink("$source" . 'here')` will be translated to IL as two
  * instructions `tmp = "$source" . 'here'` and `sink(tmp)`. If `sink(...)`
  * is a `pattern-sinks`, then both instructions' ranges are inside
  * the `pattrn-sinks` ranges. If `$source` is a `pattern-sources`, then both
@@ -284,9 +284,9 @@ let any_is_in_sources_matches rule any matches =
   |> List.filter_map (fun (rwm, ts) ->
          if Range.( $<=$ ) r rwm.RM.r then
            Some
-             (let pm = RM.range_to_pattern_match_adjusted rule rwm in
+             (let spec_pm = RM.range_to_pattern_match_adjusted rule rwm in
               let overlap = overlap_with ~match_range:rwm.RM.r r in
-              { D.spec = ts; pm; overlap })
+              { D.spec = ts; spec_pm; range = r; overlap })
          else None)
 
 (* Check whether `any` matches either the `from` or the `to` of any of the
@@ -300,12 +300,12 @@ let any_is_in_propagators_matches rule any matches :
       matches
       |> List.concat_map (fun prop ->
              let var = prop.id in
-             let pm = RM.range_to_pattern_match_adjusted rule prop.rwm in
+             let spec_pm = RM.range_to_pattern_match_adjusted rule prop.rwm in
              let is_from = is_exact_match ~match_range:prop.from r in
              let is_to = is_exact_match ~match_range:prop.to_ r in
              let mk_match kind =
                let spec : D.a_propagator = { kind; prop = prop.spec; var } in
-               { D.spec; pm; overlap = 1.0 }
+               { D.spec; spec_pm; range = r; overlap = 1.0 }
              in
              (if is_from then [ mk_match `From ] else [])
              @ (if is_to then [ mk_match `To ] else [])
@@ -318,9 +318,9 @@ let any_is_in_sanitizers_matches rule any matches =
   |> List.filter_map (fun (rwm, spec) ->
          if Range.( $<=$ ) r rwm.RM.r then
            Some
-             (let pm = RM.range_to_pattern_match_adjusted rule rwm in
+             (let spec_pm = RM.range_to_pattern_match_adjusted rule rwm in
               let overlap = overlap_with ~match_range:rwm.RM.r r in
-              { D.spec; pm; overlap })
+              { D.spec; spec_pm; range = r; overlap })
          else None)
 
 let any_is_in_sinks_matches rule any matches =
@@ -330,9 +330,9 @@ let any_is_in_sinks_matches rule any matches =
   |> List.filter_map (fun (rwm, spec) ->
          if Range.( $<=$ ) r rwm.RM.r then
            Some
-             (let pm = RM.range_to_pattern_match_adjusted rule rwm in
+             (let spec_pm = RM.range_to_pattern_match_adjusted rule rwm in
               let overlap = overlap_with ~match_range:rwm.RM.r r in
-              { D.spec; pm; overlap })
+              { D.spec; spec_pm; range = r; overlap })
          else None)
 
 let lazy_force x = Lazy.force x [@@profiling]
@@ -501,7 +501,7 @@ let check_fundef lang options taint_config opt_ent fdef =
     in
     let taints =
       source_pms
-      |> Common.map (fun (x : _ D.tmatch) -> (x.pm, x.spec))
+      |> Common.map (fun (x : _ D.tmatch) -> (x.spec_pm, x.spec))
       |> T.taints_of_pms
     in
     Lval_env.add env (IL_helpers.lval_of_var var) taints

semgrep-core/src/parsing/Parse_rule.ml
@@ -1124,6 +1124,7 @@ let parse_taint_sanitizer ~(is_old : bool) env (key : key) (value : G.expr) =
 
 let parse_taint_sink ~(is_old : bool) env (key : key) (value : G.expr) :
     Rule.taint_sink =
+  let sink_id = String.concat ":" env.path in
   let f =
     if is_old then parse_formula_old_from_dict else parse_formula_from_dict
   in
@@ -1134,7 +1135,7 @@ let parse_taint_sink ~(is_old : bool) env (key : key) (value : G.expr) :
     |> Option.value ~default:(R.default_sink_requires tok)
   in
   let sink_formula = f env sink_dict in
-  { sink_formula; sink_requires }
+  { sink_id; sink_formula; sink_requires }
 
 (*****************************************************************************)
 (* Parsers for extract mode *)
@@ -1179,7 +1180,7 @@ let parse_mode env mode_opt (rule_dict : dict) : R.mode =
   | Some ("taint", _) ->
       let parse_specs parse_spec env key x =
         ( snd key,
-          parse_list env key
+          parse_listi env key
             (fun env -> parse_spec env (fst key ^ "list item", snd key))
             x )
       in

semgrep-core/src/tainting/Dataflow_tainting.mli
@@ -7,7 +7,18 @@ type overlap = float
  * 1.0 means that the AST node matches the annotation perfectly. For
  * practical purposes we can interpret >0.99 as being the same as 1.0. *)
 
-type 'spec tmatch = { spec : 'spec; pm : Pattern_match.t; overlap : overlap }
+type 'spec tmatch = {
+  spec : 'spec;
+      (** The specification on which the match is based, e.g. a taint source.
+      * This spec should have a pattern formula. *)
+  spec_pm : Pattern_match.t;
+      (** A pattern match obtained directly from the spec's pattern formula. *)
+  range : Range.t;
+      (** The range of this particular match, which will be a subrange of 'spec_pm',
+      * see note on 'Taint-tracking via ranges' in 'Match_tainting_mode.ml'. *)
+  overlap : overlap;
+      (** The overlap of this match ('range') with the spec match ('spec_pm'). *)
+}
 
 type a_propagator = {
   kind : [ `From | `To ];

semgrep-core/tests/rules/taint_assume_safe_funcs.php
@@ -5,10 +5,7 @@ function not_tainted($data) {
     return '2';
 }
 
-//Problem here is that `tainted('a')` is in itself considered a sink because
-//currently taint-mode considers anything that falls within a sink-range to be
-//a sink... and the sink range here is whatever it matches `sink(...)`!
-//todook:tainted
+//ok:tainted
 sink(not_tainted(tainted('a')));
 
 $ok = not_tainted(tainted('a'));

semgrep-core/tests/rules/taint_best_fit_sink.py
@@ -1,0 +1,11 @@
+#ruleid: test
+sink(tainted())
+
+#ok:test
+sink(ok1 if tainted() else ok2)
+
+#ok:test
+sink(not_a_propagator(tainted()))
+
+#ok:test
+sink(some_array[tainted()])

semgrep-core/tests/rules/taint_best_fit_sink.yaml
@@ -1,0 +1,15 @@
+rules:
+  - id: test
+    languages:
+      - python
+    message: Match
+    mode: taint
+    options:
+      taint_assume_safe_functions: true
+      taint_assume_safe_indexes: true
+    pattern-sinks:
+      - pattern: sink(...)
+    pattern-sources:
+      - pattern: tainted(...)
+    severity: WARNING
+

commit 0dec70e78d64fc9e108215b51fa0cd38b3eaf90c

tainting: Fix regression in DeepSemgrep after PR #6640
Better check unconditionally whether the function is a sink and, if so,
check whether its arguments are tainted. When the function acts like a
taint propagator, that will produce some duplicate findings, but they
should be filtered out during deduplication anyways.

In addition, the taint of the function expression itself should always
be propagated, also in DeepSemgrep. The function/method itself could be
a taint source. Plus, for now it helps us identify `x.foo()` as tainted
when `x` is tainted, although in the future we want to do this properly.

Fixes: 1eb330d6887 ("tainting: Make identification of sinks more precise (#6640)")

test plan:
make test

semgrep-core/src/tainting/Dataflow_tainting.ml
@@ -865,23 +865,25 @@ let check_tainted_instr env instr : Taints.t * Lval_env.t =
         let all_args_taints =
           List.fold_left Taints.union Taints.empty all_taints
         in
+        let opt_taint_sig = check_function_signature env e args_taints in
+        (* If the function is a sink, then we consider that all its parameters
+           * are sinks. *)
+        check_orig_if_sink env instr.iorig all_args_taints;
         let call_taints =
-          match check_function_signature env e args_taints with
+          match opt_taint_sig with
           | Some call_taints -> call_taints
           | None ->
-              if not (propagate_through_functions env) then (
-                (* If we asume that function calls are "safe", then taints from
-                 * the arguments are not present in the function's output.
-                 * If the function is a sink, then we consider that all its
-                 * parameters are sinks. *)
-                check_orig_if_sink env instr.iorig all_args_taints;
-                e_taints)
+              if not (propagate_through_functions env) then Taints.empty
               else
                 (* Otherwise assume that the function will propagate
                    * the taint of its arguments. *)
-                Taints.union e_taints all_args_taints
+                all_args_taints
         in
-        (call_taints, lval_env)
+        (* We add the taint of the function itselt (i.e., 'e_taints') too.
+         * DEEP: In DeepSemgrep this also helps identifying `x.foo()` as tainted
+         * when `x` is tainted, because the call is represented in IL as `(x.foo)()`.
+         * TODO: Properly track taint through objects. *)
+        (Taints.union e_taints call_taints, lval_env)
     | CallSpecial (_, _, args) ->
         args
         |> Common.map IL_helpers.exp_of_arg