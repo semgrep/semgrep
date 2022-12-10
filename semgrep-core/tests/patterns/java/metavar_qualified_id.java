643f3d0f44640bfd5fa3cedff9a6a6ff6b618c34
Emma Jin <emjin@users.noreply.github.com>
2022-12-09 16:44:40 -0800

    Add another test and rename

--- semgrep-core/tests/patterns/java/metavar_qualified_id.java
+++ semgrep-core/tests/patterns/java/metavar_qualified_id.java
@@ -3,10 +3,13 @@ class Test {
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        // MATCH: 
         FileInputStream fis = new java.FileInputStream(fileName);
 
+        // MATCH:
         FileInputStream fis = new java.io.FileInputStream(fileName);
 
+        // MATCH: 
         FileOutputStream fos = new java.io.FileOutputStream(new java.io.FileInputStream(fileName).getFD());
     }
 }
