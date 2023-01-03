3b9ae4e3fb65123a584a0b72bb20df367beb13fa
Redoubts <Redoubts@users.noreply.github.com>
2023-01-03 18:44:34 -0500

    Loosen pin for attrs

--- cli/setup.py
+++ cli/setup.py
@@ -115,7 +115,7 @@ install_requires = [
     # Flexibility is achieved by, in order of preference:
     # 1. x.0~= operator pinning to x major version
     # 2. >=x,<y operator pinning to multiple major versions
-    "attrs~=21.3",
+    "attrs>=21.3",
     "boltons~=21.0",
     "colorama~=0.4.0",
     "click~=8.1",
