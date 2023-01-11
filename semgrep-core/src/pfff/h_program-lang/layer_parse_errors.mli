3209e89c3b35818b223bb7c711dde3fd92417ce4
Yoann Padioleau <pad@r2c.dev>
2023-01-11 12:36:16 +0100

    trim pfff/h_program-lang part 2 (#6809)
    This was possible because of the trimming of all the lang-xxx-analyze
    in the previous PR
    
    test plan:
    make test

--- semgrep-core/src/pfff/h_program-lang/layer_parse_errors.mli
+++
@@ -1,5 +0,0 @@
-val gen_red_green_layer :
-  root:Common.dirname -> Parse_info.parsing_stat list -> Layer_code.layer
-
-val gen_heatmap_layer :
-  root:Common.dirname -> Parse_info.parsing_stat list -> Layer_code.layer
