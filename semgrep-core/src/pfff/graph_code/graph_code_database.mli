77d5ba7f0d88a8787a9b64fb018966e2bf10d6e3
pad <pad@r2c.dev>
2023-01-11 14:49:30 +0100

    trim pfff/h_program-lang part3
    test plan:
    make test

--- semgrep-core/src/pfff/graph_code/graph_code_database.mli
+++
@@ -1,1 +0,0 @@
-val db_of_graph_code : Common.dirname -> Graph_code.t -> Database_code.database
--- semgrep-core/src/pfff/lang_php/analyze/highlight_php.mli
+++
@@ -1,6 +0,0 @@
-val visit_program :
-  tag:(Parse_info.t -> Highlight_code.category -> unit) ->
-  Highlight_code.highlighter_preferences ->
-  (string, Database_code.entity) Hashtbl.t ->
-  Cst_php.program * Parser_php.token list ->
-  unit
