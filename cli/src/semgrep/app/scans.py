581edcba23ee83dc1f241ca43f1f00e13cd7462d
dataholly <holly@r2c.dev>
2023-01-19 17:13:33 -0800

    cli: intuitive order ci findings

--- cli/src/semgrep/app/scans.py
+++ cli/src/semgrep/app/scans.py
@@ -278,6 +278,17 @@ class ScanHandler:
             for matches_of_rule in matches_by_rule.values()
             for match in matches_of_rule
         ]
+        all_matches.reverse()  # rising filename and line numbers in app
+        sort_order = {  # used only to order rules by severity
+            "EXPERIMENT": 0,
+            "INVENTORY": 1,
+            "INFO": 2,
+            "WARNING": 3,
+            "ERROR": 4,
+        }
+        all_matches = sorted(
+            all_matches, key=lambda match: sort_order[match.severity.value]
+        )
         new_ignored, new_matches = partition(
             all_matches, lambda match: bool(match.is_ignored)
         )
