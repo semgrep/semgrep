c4ca355ab15a183b800f467b68fd1e869f922012
brandonspark <wu.brandonj@gmail.com>
2023-01-27 13:36:02 -0800

    add comment

--- cli/src/semgrep/output_extra.py
+++ cli/src/semgrep/output_extra.py
@@ -10,6 +10,10 @@ from semgrep.parsing_data import ParsingData
 from semgrep.profiling import ProfilingData
 
 
+# This class exists to wrap some of the output returned by `semgrep-core`, on its way up
+# through the call stack.
+# This class is easily extendable if we want to add more information to the CLI output
+# in the future.
 @frozen
 class OutputExtra:
     all_targets: Set[Path]
