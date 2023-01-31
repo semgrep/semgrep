semgrep/semgrep

commit b05dda9c20d8a34a50820fd323024de8ded6d9c8

Revert "Revert "ResolvedName of dotted_ident -> GlobalName of string list (#6900)" (#6965)" (#6966)
* Revert "Revert "ResolvedName of dotted_ident -> GlobalName of string list (#6900)" (#6965)"

This reverts commit 49faa10b1bc875abf894b0ac367363512a67db80.

* commented out the failing test.

Note that join-mode is experimental and currently
rely on abstract_content which we should deprecate.

test plan:
make test all green

libs/ast_generic/AST_generic.ml
@@ -235,7 +235,7 @@ module IdInfoId = Gensym.MkId ()
 (* A single unique id: sid (uid would be a better name, but it usually
  * means "user id" for people).
  *
- * This single id simplifies further analysis which need less to care about
+ * This single id simplifies further analysis that need to care less about
  * maintaining scoping information, for example to deal with variable
  * shadowing, or functions using the same parameter names
  * (even though you still need to handle specially recursive functions), etc.
@@ -276,8 +276,9 @@ and resolved_name_kind =
   (* sgrep: those cases allow to match entities/modules even if they were
    * aliased when imported.
    * both dotted_ident must at least contain one element *)
-  | ImportedEntity of dotted_ident
-  | ImportedModule of module_name
+  | ImportedEntity of canonical_name
+  | ImportedModule of
+      canonical_name (* just the DottedName part of module_name*)
   (* used in Go, where you can pass types as arguments and where we
    * need to resolve those cases
    *)
@@ -285,25 +286,29 @@ and resolved_name_kind =
   (* used for C *)
   | Macro
   | EnumConstant
-  (* for deep semgrep
+  (* This is for deep semgrep.
    *
-   * ResolvedName (resolved_name, alternate_names)
+   * GlobalName (canonical_name, alternate_names)
    *
-   * resolved_name: The canonical, global name that the symbol resolves to.
+   * canonical_name: The canonical, global name that the symbol resolves to.
    *
    * alternate_names: Other names that users may write when referring to this
-   * symbol. For example, in JS, the resolved_name may include the file path of
-   * the file where the symbol is defined, but users may want to write patterns
-   * to match based on the module specifier, e.g.:
+   * symbol. For example, in JS, the canonical_name may include the file path
+   * of the file where the symbol is defined, but users may want to write
+   * patterns to match based on the module specifier, e.g.:
    *
    * import {bar} from 'foo';
    * bar;
    *
    * We might store ['/path/to/node_modules/foo/src/x.js', 'bar'] as the
-   * resolved_name, but we also want to match the pattern `foo.bar` so we will
+   * canonical_name, but we also want to match the pattern `foo.bar` so we will
    * store ['foo', 'bar'] as an alternate name.
    * *)
-  | ResolvedName of dotted_ident * dotted_ident list
+  | GlobalName of canonical_name * alternate_name list
+
+and canonical_name = string list
+
+and alternate_name = string list
 [@@deriving show { with_path = false }, eq, hash]
 
 (* Start of big mutually recursive types because of the use of 'any'
@@ -1967,6 +1972,9 @@ let basic_id_info ?(hidden = false) resolved =
   }
 
 (* TODO: move AST_generic_helpers.name_of_id and ids here *)
+
+let dotted_to_canonical xs = Common.map fst xs
+let canonical_to_dotted tid xs = xs |> Common.map (fun s -> (s, tid))
 
 (* ------------------------------------------------------------------------- *)
 (* Entities *)

libs/ast_generic/Visitor_AST.ml
@@ -172,23 +172,24 @@ let (mk_visitor :
   and v_resolved_name (v1, v2) =
     v_resolved_name_kind v1;
     v_sid v2
+  and v_canonical_name v1 = v_list v_string v1
   and v_resolved_name_kind = function
     | LocalVar -> ()
     | Parameter -> ()
     | EnclosedVar -> ()
     | Global -> ()
     | ImportedEntity v1 ->
-        let v1 = v_dotted_ident v1 in
+        let v1 = v_canonical_name v1 in
         ()
     | ImportedModule v1 ->
-        let v1 = v_module_name v1 in
+        let v1 = v_canonical_name v1 in
         ()
     | Macro -> ()
     | EnumConstant -> ()
     | TypeName -> ()
-    | ResolvedName (v1, v2) ->
-        let v1 = v_dotted_ident v1 in
-        let v2 = v_list v_dotted_ident v2 in
+    | GlobalName (v1, v2) ->
+        let v1 = v_canonical_name v1 in
+        let v2 = v_list v_canonical_name v2 in
         ()
   and v_name_info
       { name_middle = v4; name_top = v3; name_last = v1; name_info = v2 } =

cli/tests/e2e/test_join_rules.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-@pytest.mark.slow
+@pytest.mark.kinda_slow
 @pytest.mark.parametrize(
     "rule,target",
     [
@@ -34,7 +34,7 @@ def test_join_rules(run_semgrep_in_tmp, snapshot, rule, target):
     )
 
 
-@pytest.mark.slow
+@pytest.mark.kinda_slow
 @pytest.mark.parametrize(
     "rule,target",
     [
@@ -46,10 +46,13 @@ def test_join_rules(run_semgrep_in_tmp, snapshot, rule, target):
             "rules/join_rules/recursive/java-callgraph-example/vulnado-sqli.yaml",
             "join_rules/recursive/java-callgraph-example/vulnado-chain-broken",
         ),
-        (
-            "rules/join_rules/recursive/flask-deep-stored-xss-example/flask-stored-xss.yaml",
-            "join_rules/recursive/flask-deep-stored-xss-example",
-        ),
+        # TODO: regression because of
+        # https://github.com/returntocorp/semgrep/pull/6900
+        # commented for now
+        # (
+        #    "rules/join_rules/recursive/flask-deep-stored-xss-example/flask-stored-xss.yaml",
+        #    "join_rules/recursive/flask-deep-stored-xss-example",
+        # ),
     ],
 )
 def test_recursive_join_rules(run_semgrep_in_tmp, snapshot, rule, target):

languages/python/generic/Python_to_generic.ml
@@ -142,8 +142,11 @@ let resolved_name = function
   | GlobalVar -> Some (G.Global, G.SId.unsafe_default)
   | ClassField -> None
   | ImportedModule xs ->
-      Some (G.ImportedModule (G.DottedName xs), G.SId.unsafe_default)
-  | ImportedEntity xs -> Some (G.ImportedEntity xs, G.SId.unsafe_default)
+      let xs = G.dotted_to_canonical xs in
+      Some (G.ImportedModule xs, G.SId.unsafe_default)
+  | ImportedEntity xs ->
+      let xs = G.dotted_to_canonical xs in
+      Some (G.ImportedEntity xs, G.SId.unsafe_default)
   | NotResolved -> None
 
 let expr_context = function

libs/ast_generic/AST_generic_to_v1.ml
@@ -96,22 +96,25 @@ let rec map_resolved_name (v1, v2) =
   let v2 = AST_generic.SId.to_int v2 in
   (v1, v2)
 
+and map_canonical_name v1 =
+  map_of_list (fun x -> (map_of_string x, map_tok fk)) v1
+
 and map_resolved_name_kind = function
   | LocalVar -> `Local
   | Parameter -> `Param
   | EnclosedVar -> `OtherResolvedNameKind "EnclosedVar"
   | Global -> `Global
   | ImportedEntity v1 ->
-      let v1 = map_dotted_ident v1 in
+      let v1 = map_canonical_name v1 in
       `ImportedEntity v1
   | ImportedModule v1 ->
-      let v1 = map_module_name v1 in
-      `ImportedModule v1
+      let v1 = map_canonical_name v1 in
+      `ImportedModule (`DottedName v1)
   | Macro -> `OtherResolvedNameKind "Macro"
   | EnumConstant -> `OtherResolvedNameKind "EnumConstant"
   | TypeName -> `OtherResolvedNameKind "TypeName"
-  | ResolvedName (v1, _v2less) ->
-      let v1 = map_dotted_ident v1 in
+  | GlobalName (v1, _v2less) ->
+      let v1 = map_canonical_name v1 in
       `ImportedEntity v1
 
 and map_id_info x =

libs/ast_generic/Map_AST.ml
@@ -109,24 +109,25 @@ let (mk_visitor : visitor_in -> visitor_out) =
     let v1 = map_resolved_name_kind v1 in
     let v2 = map_sid v2 in
     (v1, v2)
+  and map_canonical_name v1 = map_of_list map_of_string v1
   and map_resolved_name_kind = function
     | LocalVar -> LocalVar
     | Parameter -> Parameter
     | EnclosedVar -> EnclosedVar
     | Global -> Global
     | ImportedEntity v1 ->
-        let v1 = map_dotted_ident v1 in
+        let v1 = map_canonical_name v1 in
         ImportedEntity v1
     | ImportedModule v1 ->
-        let v1 = map_module_name v1 in
+        let v1 = map_canonical_name v1 in
         ImportedModule v1
     | Macro -> Macro
     | EnumConstant -> EnumConstant
     | TypeName -> TypeName
-    | ResolvedName (v1, v2) ->
-        let v1 = map_dotted_ident v1 in
-        let v2 = map_of_list map_dotted_ident v2 in
-        ResolvedName (v1, v2)
+    | GlobalName (v1, v2) ->
+        let v1 = map_canonical_name v1 in
+        let v2 = map_of_list map_canonical_name v2 in
+        GlobalName (v1, v2)
   and map_name_info
       {
         name_last = v1;

libs/ast_generic/Meta_AST.ml
@@ -44,24 +44,26 @@ let rec vof_resolved_name (v1, v2) =
   let v2 = OCaml.vof_int (SId.to_int v2) in
   OCaml.VTuple [ v1; v2 ]
 
+and vof_canonical_name v1 = OCaml.vof_list OCaml.vof_string v1
+
 and vof_resolved_name_kind = function
   | LocalVar -> OCaml.VSum ("LocalVar", [])
   | Parameter -> OCaml.VSum ("Parameter", [])
   | EnclosedVar -> OCaml.VSum ("EnclosedVar", [])
   | Global -> OCaml.VSum ("Global", [])
   | ImportedEntity v1 ->
-      let v1 = vof_dotted_ident v1 in
+      let v1 = vof_canonical_name v1 in
       OCaml.VSum ("ImportedEntity", [ v1 ])
   | ImportedModule v1 ->
-      let v1 = vof_module_name v1 in
+      let v1 = vof_canonical_name v1 in
       OCaml.VSum ("ImportedModule", [ v1 ])
   | Macro -> OCaml.VSum ("Macro", [])
   | EnumConstant -> OCaml.VSum ("EnumConstant", [])
   | TypeName -> OCaml.VSum ("TypeName", [])
-  | ResolvedName (v1, v2) ->
-      let v1 = vof_dotted_ident v1 in
-      let v2 = OCaml.vof_list vof_dotted_ident v2 in
-      OCaml.VSum ("ResolvedName", [ v1; v2 ])
+  | GlobalName (v1, v2) ->
+      let v1 = vof_canonical_name v1 in
+      let v2 = OCaml.vof_list vof_canonical_name v2 in
+      OCaml.VSum ("GlobalName", [ v1; v2 ])
 
 let rec vof_qualifier = function
   | QDots v1 ->

src/analyzing/Constant_propagation.ml
@@ -119,7 +119,7 @@ let is_global = function
   | _ -> false
 
 let is_resolved_name = function
-  | ResolvedName _ -> true
+  | GlobalName _ -> true
   | _ -> false
 
 let is_lang env l2 =

commit 7c665b8c70749f6e503cea9d6441f77ef676aade

Fix small regression introduced by #6966
test plan:
make test # fixes one regression

libs/ast_generic/AST_generic.ml
@@ -1974,7 +1974,9 @@ let basic_id_info ?(hidden = false) resolved =
 (* TODO: move AST_generic_helpers.name_of_id and ids here *)
 
 let dotted_to_canonical xs = Common.map fst xs
-let canonical_to_dotted tid xs = xs |> Common.map (fun s -> (s, tid))
+
+let canonical_to_dotted tid xs =
+  xs |> Common.map (fun s -> (s, Parse_info.fake_info tid s))
 
 (* ------------------------------------------------------------------------- *)
 (* Entities *)

libs/ast_generic/Visitor_AST.ml
@@ -1473,7 +1473,7 @@ let extract_ranges :
         ranges := Some (smaller orig_left left, larger orig_right right)
   in
   let incorporate_token tok =
-    if PI.is_origintok tok then
+    if PI.has_origin_loc tok then
       let tok_loc = PI.unsafe_token_location_of_info tok in
       incorporate_tokens (tok_loc, tok_loc)
   in
@@ -1515,7 +1515,7 @@ let extract_ranges :
     res
 
 let range_of_tokens tokens =
-  List.filter PI.is_origintok tokens |> PI.min_max_ii_by_pos
+  List.filter PI.has_origin_loc tokens |> PI.min_max_ii_by_pos
   [@@profiling]
 
 let range_of_any_opt any =

libs/lib_parsing/Parse_info.ml
@@ -327,6 +327,13 @@ let is_origintok ii =
   | OriginTok _ -> true
   | _ -> false
 
+let has_origin_loc ii =
+  match ii.token with
+  | OriginTok _
+  | FakeTokStr (_, Some _) ->
+      true
+  | _ -> false
+
 (* info about the current location *)
 
 (* original info *)
@@ -347,7 +354,9 @@ type posrv =
 
 let compare_pos ii1 ii2 =
   let get_pos = function
-    | OriginTok pi -> Real pi
+    | OriginTok pi
+    | FakeTokStr (_, Some (pi, _)) ->
+        Real pi
     (* todo? I have this for lang_php/
         | FakeTokStr (s, Some (pi_orig, offset)) ->
             Virt (pi_orig, offset)

libs/lib_parsing/Parse_info.mli
@@ -142,6 +142,7 @@ exception NoTokenLocation of string
 val fake_token_location : token_location
 val is_fake : t -> bool
 val is_origintok : t -> bool
+val has_origin_loc : t -> bool
 
 (* NOTE: These functions introduce unsafe fake tokens, prefer safe functions
  * below, use these only as a last resort! *)

src/core/Range.ml
@@ -110,7 +110,7 @@ let range_of_token_locations (start_loc : PI.token_location)
 
 let range_of_tokens xs =
   try
-    let xs = List.filter PI.is_origintok xs in
+    let xs = List.filter PI.has_origin_loc xs in
     let mini, maxi = PI.min_max_ii_by_pos xs in
     let start = PI.pos_of_info mini in
     let end_ =

src/reporting/JSON_report.ml
@@ -116,7 +116,7 @@ let metavar_string_of_any any =
      we have y = 2 but there is no source location for 2.
      Handle such cases *)
   any |> V.ii_of_any
-  |> List.filter PI.is_origintok
+  |> List.filter PI.has_origin_loc
   |> List.sort Parse_info.compare_pos
   |> Common.map PI.str_of_info |> Matching_report.join_with_space_if_needed
 
@@ -184,7 +184,7 @@ let tokens_to_single_loc toks =
    * taint rule finding but it shouldn't happen in practice. *)
   let locations =
     tokens_to_locations
-      (List.filter PI.is_origintok toks |> List.sort PI.compare_pos)
+      (List.filter PI.has_origin_loc toks |> List.sort PI.compare_pos)
   in
   let* first_loc, last_loc = first_and_last locations in
   Some

cli/tests/e2e/test_join_rules.py
@@ -49,10 +49,10 @@ def test_join_rules(run_semgrep_in_tmp, snapshot, rule, target):
         # TODO: regression because of
         # https://github.com/returntocorp/semgrep/pull/6900
         # commented for now
-        # (
-        #    "rules/join_rules/recursive/flask-deep-stored-xss-example/flask-stored-xss.yaml",
-        #    "join_rules/recursive/flask-deep-stored-xss-example",
-        # ),
+        (
+            "rules/join_rules/recursive/flask-deep-stored-xss-example/flask-stored-xss.yaml",
+            "join_rules/recursive/flask-deep-stored-xss-example",
+        ),
     ],
 )
 def test_recursive_join_rules(run_semgrep_in_tmp, snapshot, rule, target):