22b1cea73b38289216df183d6b1e3bedebd26906
Iago Abal <mail@iagoabal.eu>
2023-02-01 19:47:28 +0100

    Make abstract_content more meaningful
    Instead of attaching "origin" tokens to "fake" ids, we attach to them
    "fake" tokens that are associated with real locations.
    
    Follows: 291df812a61 ("ResolvedName of dotted_ident -> GlobalName of string list (#6900)")
    
    test plan:
    % bin/semgrep-core -l py -e 'return $F(...)' tests/rules/taint_flask.py -json
      #^ abstract_content for $F is "flask.render_template" as you would expect!

--- libs/ast_generic/AST_generic.ml
+++ libs/ast_generic/AST_generic.ml
@@ -1979,7 +1979,9 @@ let basic_id_info ?(hidden = false) resolved =
 (* TODO: move AST_generic_helpers.name_of_id and ids here *)
 
 let dotted_to_canonical xs = Common.map fst xs
-let canonical_to_dotted tid xs = xs |> Common.map (fun s -> (s, tid))
+
+let canonical_to_dotted tid xs =
+  xs |> Common.map (fun s -> (s, Parse_info.fake_info tid s))
 
 (* ------------------------------------------------------------------------- *)
 (* Entities *)
