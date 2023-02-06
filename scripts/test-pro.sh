fbdd960b95a61611fb9340016997a7d3ef2ef1fb
brandonspark <wu.brandonj@gmail.com>
2023-02-06 10:42:01 -0800

    let's try something else, cd into root and also ls -a

--- scripts/test-pro.sh
+++ scripts/test-pro.sh
@@ -4,4 +4,6 @@ cp /root/semgrep-core-proprietary /usr/local/bin/semgrep-core-proprietary
 
 chmod +x /usr/local/bin/semgrep-core-proprietary
 
-semgrep --config "p/default-v2" /root --interfile
+cd /root
+
+semgrep --config "p/default-v2" . --interfile
