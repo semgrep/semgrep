db989a07b0fa71b0e2159e85220138625bb1e049
brandonspark <wu.brandonj@gmail.com>
2023-02-06 11:20:29 -0800

    comments

--- scripts/test-pro.sh
+++ scripts/test-pro.sh
@@ -2,8 +2,10 @@
 
 cp /root/semgrep-core-proprietary /usr/local/bin/semgrep-core-proprietary
 
+# We need to give it executable permissions or it won't run
 chmod +x /usr/local/bin/semgrep-core-proprietary
 
+# Relocate to the directory, because otherwise some weirdness happens and
+# we run with all the rules, taking ~30 minutes.
 cd /root
-
 semgrep --config "p/default-v2" . --interfile
