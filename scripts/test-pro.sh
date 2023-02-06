834d996738bb0b026c805f3c8ecca75212a230c2
brandonspark <wu.brandonj@gmail.com>
2023-02-06 09:08:00 -0800

    use default-v2 instead

--- scripts/test-pro.sh
+++ scripts/test-pro.sh
@@ -4,4 +4,4 @@ cp /root/semgrep-core-proprietary /usr/local/bin/semgrep-core-proprietary
 
 chmod +x /usr/local/bin/semgrep-core-proprietary
 
-semgrep --config "p/deepsemgrep" /root --interfile
+semgrep --config "p/default-v2" /root --interfile
