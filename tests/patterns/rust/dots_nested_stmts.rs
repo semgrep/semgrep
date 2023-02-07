a5abf8ea9d965941db49bd588dccbe750ae811b8
Yoann Padioleau <pad@r2c.dev>
2023-02-07 21:33:11 +0100

    Rust: beta support (#7092)
    The current parsing rate is at 99.3% according to
    https://metabase.corp.r2c.dev/question/554-parsing-rate-semgrep
    we have 5+ rust rules in semgrep-rules, and all the beta-level
    features are now implemented
    
    This closes #6545
    
    test plan:
    test file included
    make test

---
+++ tests/patterns/rust/dots_nested_stmts.rs
@@ -0,0 +1,10 @@
+fn test() {
+        //ERROR:
+        if (x == some_cond) {
+            Console.WriteLine("matched");
+	}
+        else {
+            Console.WriteLine("not matched");
+	}
+}
+
---
+++ tests/patterns/rust/metavar_equality_expr.rs
@@ -0,0 +1,9 @@
+fn test_equal() {
+    a = 1;
+    b = 2;
+    //ERROR: match
+    if (a+b == a+b) {
+        return 1;
+    }
+    return 0;
+}
