0291302c530ea49cedb3d7382b22f6934f143293
Yoann Padioleau <pad@r2c.dev>
2023-02-07 16:38:22 +0100

    Rust: add alpha tests and enable back check_maturity for Rust (#7090)
    This will help #6545
    
    test plan:
    test file included
    make core-test

---
+++ tests/patterns/rust/metavar_arg.rs
@@ -0,0 +1,17 @@
+fn foo() {
+    //ERROR:
+    foo(1,2);
+
+    //ERROR:
+    foo(a_very_long_constant_name,
+        2);
+
+    //ERROR:
+    foo (unsafe_func(), // indeed
+         2);
+
+    //ERROR:
+    foo(bar(1,3), 2);
+
+    foo(2,1);
+}
