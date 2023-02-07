ccf4253b942f9c386a110a13e1fd484fb6f410b7
pad <pad@r2c.dev>
2023-02-07 20:25:53 +0100

    Rust: beta support
    The current parsing rate is at 99.3% according to
    https://metabase.corp.r2c.dev/question/554-parsing-rate-semgrep
    we have 5+ rust rules in semgrep-rules, and all the beta-level
    features are now implemented
    
    This closes #6545
    
    test plan:
    test file included
    make test

---
+++ tests/patterns/rust/metavar_import.rs
@@ -0,0 +1,7 @@
+//ERROR: match
+use java::util::ArrayList;
+//ERROR: match
+use java::util::List;
+
+fn Foo() {
+}
---
+++ tests/patterns/rust/metavar_stmt.rs
@@ -0,0 +1,8 @@
+fn foo() {
+    v = 1;
+    //ERROR:
+    if (v > 2) {
+        return 1;
+    }
+}
+
