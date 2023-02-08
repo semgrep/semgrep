56791f99015a9393baabcdfbc68603a16e762e60
Emma Jin <emjin@users.noreply.github.com>
2023-02-08 10:02:21 -0800

    Commands should override the toggle

--- cli/src/semgrep/commands/ci.py
+++ cli/src/semgrep/commands/ci.py
@@ -163,6 +163,15 @@ def fix_head_if_github_action(metadata: GitMeta) -> Iterator[None]:
     """,
     envvar="SEMGREP_SUPPRESS_ERRORS",
 )
+@click.option(
+    "--oss-only",
+    "oss_only",
+    default=False,
+    help="""
+        Run `semgrep ci` on this repo using only OSS features. Equivalent to a
+        ci run without the Semgrep Pro toggle on.
+    """,
+)
 @handle_command_errors
 def ci(
     ctx: click.Context,
@@ -199,6 +208,7 @@ def ci(
     pro_languages: bool,
     pro_intrafile: bool,
     pro: bool,
+    oss_only: bool,
     sarif: bool,
     quiet: bool,
     rewrite_rule_ids: bool,
@@ -326,18 +336,24 @@ def ci(
             is_full_scan = metadata.merge_base_ref is None
             engine = EngineType.OSS
             if scan_handler and scan_handler.deepsemgrep:
-                if is_full_scan:
-                    if pro_intrafile:
-                        # Intra-file (inter-proc) + Pro languages
-                        engine = EngineType.PRO_INTRAFILE
-                    elif pro_languages:
-                        # Just Pro languages
-                        engine = EngineType.PRO_LANG
-                    else:
-                        # Inter-file + Pro languages
-                        engine = EngineType.PRO_INTERFILE
-                else:
-                    EngineType.PRO_LANG
+                engine = (
+                    EngineType.PRO_INTERFILE if is_full_scan else EngineType.PRO_LANG
+                )
+
+            # Flags override the toggle
+            if pro and is_full_scan:
+                # Inter-file + Pro languages
+                # This behaves slightly differently from semgrep scan, because
+                # in CI we currently do not allow interfile analysis
+                engine = EngineType.PRO_INTERFILE
+            elif pro_intrafile:
+                # Intra-file (inter-proc) + Pro languages
+                engine = EngineType.PRO_INTRAFILE
+            elif pro_languages:
+                # Just Pro languages
+                engine = EngineType.PRO_LANG
+            elif oss_only:
+                engine = EngineType.OSS
 
             (semgrep_pro_path, _deep_semgrep_path) = determine_semgrep_pro_path()
 
