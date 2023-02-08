3a328a52fd2e202defb4aee33637740ee56f55e0
emjin <emjin@users.noreply.github.com>
2023-02-08 18:42:28 +0000

    chore: Bump version to 1.10.0

--- cli/setup.py
+++ cli/setup.py
@@ -148,7 +148,7 @@ extras_require = {"experiments": ["jsonnet~=0.18"]}
 
 setuptools.setup(
     name="semgrep",
-    version="1.9.0",
+    version="1.10.0",
     author="Return To Corporation",
     author_email="support@r2c.dev",
     description="Lightweight static analysis for many languages. Find bug variants with patterns that look like source code.",
