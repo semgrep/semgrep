330627a824c2badbc46c45792b02f49cd818fbc7
Brandon Wu <49291449+brandonspark@users.noreply.github.com>
2023-02-27 05:58:06 -0800

    chore: remove fdef from taint signature hook (#7207)
    * take away fdef from taint signature hook
    
    * add comments
    
    * changelog

--- src/tainting/Dataflow_tainting.mli
+++ src/tainting/Dataflow_tainting.mli
@@ -95,7 +95,7 @@ val str_of_name : IL.name -> var
 val hook_function_taint_signature :
   (config ->
   AST_generic.expr ->
-  (AST_generic.function_definition * Taint.finding list) option)
+  (AST_generic.parameters (* params of function *) * Taint.finding list) option)
   option
   ref
 (** Deep Semgrep *)
