9b5bcbf7336a47a35aae5fb1b42f3e7081d01d7a
Emma Jin <emjin@users.noreply.github.com>
2023-03-15 12:33:20 -0700

    Tests: add benchmarks for pro
    Refactor benchmarks for OSS and add benchmarks for pro.
    
    Test plan:
    automated tests

---
+++ scripts/run-benchmarks.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env bash
+
+set e
+
+cd cli || return
+
+# Run timing benchmark
+pipenv install semgrep==0.122.0
+pipenv run python -m semgrep --version
+export PATH=/github/home/.local/bin:$PATH
+
+pipenv run python3 ../perf/run-benchmarks --small-only --std-only --save-to baseline_timing1.json --no-time
+jq . baseline_timing1.json
+pipenv run python3 ../perf/run-benchmarks --small-only --std-only --save-to baseline_timing2.json --no-time
+jq . baseline_timing2.json
+pipenv uninstall -y semgrep
+
+# Install latest
+pipenv install -e .
+
+# Run latest timing benchmark
+pipenv run python -m semgrep --version
+pipenv run semgrep-core -version
+pipenv run python3 ../perf/run-benchmarks --small-only --std-only --save-to timing1.json
+jq . timing1.json
+pipenv run python3 ../perf/run-benchmarks --small-only --std-only --save-to timing2.json --save-findings-to findings.json
+jq . timing2.json
+jq . findings.json
+
+# Compare timing infos
+../perf/compare-perf baseline_timing1.json baseline_timing2.json timing1.json timing2.json "$1" "$2"
+../perf/compare-bench-findings findings.json
