bb89c4974b443e2d7c0365e58b56d8f3034fd97e
Emma Jin <emjin@users.noreply.github.com>
2023-03-15 12:40:48 -0700

    Update to 1.14.0 to match develop

--- scripts/run-benchmarks.sh
+++ scripts/run-benchmarks.sh
@@ -5,7 +5,7 @@ set e
 cd cli || return
 
 # Run timing benchmark
-pipenv install semgrep==0.122.0
+pipenv install semgrep==1.14.0
 pipenv run python -m semgrep --version
 export PATH=/github/home/.local/bin:$PATH
 
