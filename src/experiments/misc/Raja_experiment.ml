a4dca9b35af731223e39a64742085f732ec6109a
Yoann Padioleau <pad@r2c.dev>
2023-03-16 14:50:31 +0100

    Small experiment for Raja (#7343)
    test plan:
    ```
    semgrep --core-opts="-raja" -l ocaml --config ~/test.yaml ~/test.ml --json | jq
    {
      "errors": [],
      "paths": {
        "_comment": "<add --verbose for a list of skipped paths>",
        "scanned": [
          "/home/pad/test.ml"
        ]
      },
      "results": [
        {
          "check_id": "home.pad.stupid-eq",
          "end": {
            "col": 11,
            "line": 5,
            "offset": 39
          },
          "extra": {
            "engine_kind": "OSS",
            "extra_extra": {
              "function_name": "bar",
              "function_range": {
                "end": 56,
                "start": 17
              }
            },
            "fingerprint": "873b52c00b5b7d8a14d66330ffc767cfacb2b64ff075c4294a67264235c166324bc3f1e0f540853bef4d997a22d43e661d452ffa0a0d8ba93c9fbef2543808a1_0",
            "is_ignored": false,
            "lines": "  if n = n",
            "message": "Found it",
            "metadata": {
              "reference": "wikipedia",
              "type": "XSS"
    
    ...
    ```

--- src/experiments/misc/Raja_experiment.ml
+++ src/experiments/misc/Raja_experiment.ml
@@ -1,5 +1,7 @@
 module Out = Output_from_core_j
 
+let logger = Logging.get_logger [ __MODULE__ ]
+
 (*****************************************************************************)
 (* Prelude *)
 (*****************************************************************************)
@@ -15,6 +17,33 @@ module Out = Output_from_core_j
 (* Helpers *)
 (*****************************************************************************)
 
+let find_function_info (range_offsets : int * int)
+    (ranges : Function_range.ranges) : Function_range.function_info option =
+  let start, end_ = range_offsets in
+  ranges
+  |> List.find_opt (fun { Function_range.range = t1, t2; _ } ->
+         let offset1 = Parse_info.pos_of_info t1 in
+         let offset2 = Parse_info.pos_of_info t2 in
+         start >= offset1 && end_ <= offset2)
+
+(*****************************************************************************)
+(* Cache *)
+(*****************************************************************************)
+let (cache : (Fpath.t, Function_range.ranges) Hashtbl.t) = Hashtbl.create 101
+
+let ranges_of_path (path : Fpath.t) : Function_range.ranges =
+  match Hashtbl.find_opt cache path with
+  | Some x -> x
+  | None ->
+      (* alt: could use just_parse_with_lang and get the lang
+       * from the languages: field in the rule corresponding to
+       * the rule_id of the match
+       *)
+      let ast = Parse_target.parse_program (Fpath.to_string path) in
+      let ranges = Function_range.ranges ast in
+      Hashtbl.add cache path ranges;
+      ranges
+
 (*****************************************************************************)
 (* Entry point *)
 (*****************************************************************************)
@@ -24,18 +53,35 @@ let adjust_core_match_results (x : Out.core_match_results) :
   let matches =
     x.matches
     |> Common.map (fun (m : Out.core_match) ->
-           let extra =
-             {
-               m.extra with
-               extra_extra =
-                 Some
-                   (`Assoc
-                     [
-                       ("function_name", `String "TODO: funcname");
-                       ("function_body", `String "TODO: body text or range?");
-                     ]);
-             }
+           let _rule_id = m.rule_id in
+           let path = Fpath.v m.location.path in
+           let ranges = ranges_of_path path in
+           let start, end_ =
+             (m.location.start.offset, m.location.end_.offset)
            in
-           { m with extra })
+           match find_function_info (start, end_) ranges with
+           | None ->
+               logger#info "no function info found for range: %d, %d" start end_;
+               m
+           | Some { Function_range.name; range = t1, t2 } ->
+               let offset1 = Parse_info.pos_of_info t1 in
+               let offset2 = Parse_info.pos_of_info t2 in
+               let extra =
+                 {
+                   m.extra with
+                   extra_extra =
+                     Some
+                       (`Assoc
+                         [
+                           ("function_name", `String name);
+                           ( "function_range",
+                             `Assoc
+                               [
+                                 ("start", `Int offset1); ("end", `Int offset2);
+                               ] );
+                         ]);
+                 }
+               in
+               { m with extra })
   in
   { x with matches }
