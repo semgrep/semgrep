215e3dd5b282cb91ea7e1f6d20f48c9ec6d99e11
pad <pad@r2c.dev>
2023-03-21 18:57:25 +0100

    Remove support for Comby
    Writing Comby patterns were never really advertised, and
    none of our semgrep-rules use the feature.
    It also complicates our build (there is some conflict between
    comby-kernel and atdgen), and add lots of dependencies
    to OCaml libraries.
    
    Without comby, engine.js goes from 4.7MB to 4.2MB
    and the compressed engine.js.br is now at 760_000 bytes!
    
    test plan:
    dune --profile=release
    ls _build/.../engine.js

--- src/metachecking/Translate_rule.ml
+++ src/metachecking/Translate_rule.ml
@@ -73,8 +73,7 @@ and translate_formula f : [> `O of (string * Yaml.value) list ] =
       | Sem (_, _)
       | Spacegrep _ ->
           `O [ ("pattern", `String (fst pstr)) ]
-      | Regexp _ -> `O [ ("regex", `String (fst pstr)) ]
-      | Comby _ -> failwith "comby not supported in new")
+      | Regexp _ -> `O [ ("regex", `String (fst pstr)) ])
   | Inside (_, f) -> `O [ ("inside", (translate_formula f :> Yaml.value)) ]
   | And (_, { conjuncts; focus; conditions; _ }) ->
       `O
@@ -130,7 +129,6 @@ let replace_pattern rule_fields translated_formula =
             "patterns";
             "pattern-either";
             "pattern-regex";
-            "pattern-comby";
             "pattern-sources";
           ]
       then [ ("match", translated_formula) ]
