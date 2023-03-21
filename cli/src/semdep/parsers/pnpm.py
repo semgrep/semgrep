4c3be47413879da8367afcd15f0c53a382da8a7c
Adam Berman <adam@returntocorp.com>
2023-03-20 17:27:11 -0700

    fix whitespace

--- cli/src/semdep/parsers/pnpm.py
+++ cli/src/semdep/parsers/pnpm.py
@@ -73,8 +73,8 @@ packages_identifier = string("packages:")
 # "/foo/1.2.3:"
 # "/@foo/bar/1.2.3:"
 raw_dependency = whitespace.optional() >> (
-    regex("/(@.+/.+)/([^:]+)",flags=0,group=(1,2))
-    | regex("/(.+)/([^:]+)",flags=0,group=(1,2))
+    regex("/(@.+/.+)/([^:]+)", flags=0, group=(1,2))
+    | regex("/(.+)/([^:]+)", flags=0, group=(1,2))
 )
 
 # resolution: {integrity: sha512-...}
