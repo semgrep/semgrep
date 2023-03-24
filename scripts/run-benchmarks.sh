e5139bdafcdbb49f06b9b62b25759eb90a280ef5
Emma Jin <emjin@users.noreply.github.com>
2023-03-23 22:13:13 -0700

    Update semgrep side benchmarks

--- scripts/run-benchmarks.sh
+++ scripts/run-benchmarks.sh
@@ -9,9 +9,12 @@ pipenv install semgrep==1.14.0
 pipenv run python -m semgrep --version
 export PATH=/github/home/.local/bin:$PATH
 
-pipenv run python3 ../perf/run-benchmarks --small-only --std-only --save-to baseline_timing1.json --no-time
+config_path=../perf/configs/ci_small_repos.yaml
+echo $config_path
+
+pipenv run python3 ../perf/run-benchmarks --config $config_path --std-only --save-to baseline_timing1.json --no-time
 jq . baseline_timing1.json
-pipenv run python3 ../perf/run-benchmarks --small-only --std-only --save-to baseline_timing2.json --no-time
+pipenv run python3 ../perf/run-benchmarks --config $config_path --std-only --save-to baseline_timing2.json --no-time
 jq . baseline_timing2.json
 pipenv uninstall -y semgrep
 
@@ -21,12 +24,12 @@ pipenv install -e .
 # Run latest timing benchmark
 pipenv run python -m semgrep --version
 pipenv run semgrep-core -version
-pipenv run python3 ../perf/run-benchmarks --small-only --std-only --save-to timing1.json
+pipenv run python3 ../perf/run-benchmarks --config $config_path --std-only --save-to timing1.json
 jq . timing1.json
-pipenv run python3 ../perf/run-benchmarks --small-only --std-only --save-to timing2.json --save-findings-to findings.json
+pipenv run python3 ../perf/run-benchmarks --config $config_path --std-only --save-to timing2.json --save-findings-to ci_small_repos_findings.json
 jq . timing2.json
-jq . findings.json
+jq . ci_small_repos_findings.json
 
 # Compare timing infos
 ../perf/compare-perf baseline_timing1.json baseline_timing2.json timing1.json timing2.json "$1" "$2"
-../perf/compare-bench-findings findings.json
+../perf/compare-bench-findings ci_small_repos_findings.json
