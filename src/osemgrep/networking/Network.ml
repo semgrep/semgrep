40327e11abfbcc0309572c6f3a002f23a7059406
Hannes Mehnert <hannes@mehnert.org>
2023-03-27 17:03:49 +0200

    osemgrep: use http-lwt-client instead of cohttp-lwt-unix
    Previously, neither IPv6 nor HTTP/2 were supported, and CoHTTP (with lots of
    functors inside) was used for HTTP requests. Now, the functor-free opam package
    http-lwt-client is used, relying on happy-eyeballs to use IPv4 or IPv6 and
    httpaf for HTTP/1 and h2 for HTTP/2 support.

--- src/osemgrep/networking/Network.ml
+++ src/osemgrep/networking/Network.ml
@@ -1,12 +1,16 @@
-open Lwt.Syntax
-open Cohttp_lwt_unix
+open Http_lwt_client
 
 (* TODO: extend to allow to curl with JSON as answer *)
 let get url =
-  let promise =
-    let* _resp, body = Client.get url in
-    Cohttp_lwt.Body.to_string body
-  in
-  let content = Lwt_main.run promise in
-  content
+  let bodyf _ acc data = Lwt.return (acc ^ data) in
+  let promise = request (Uri.to_string url) bodyf "" in
+  let r = Lwt_main.run promise in
+  match r with
+  | Ok (response, content)
+    when Status.is_successful response.status ->
+    Ok content
+  | Ok (response, _) ->
+    Error ("HTTP request failed, server response " ^
+           Status.to_string response.status)
+  | Error `Msg msg -> Error ("HTTP request failed: " ^ msg)
   [@@profiling]
