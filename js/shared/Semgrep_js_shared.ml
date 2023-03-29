abc02c56405e863d3b590be472a2ecd8f91704c8
Tom Petr <tom@r2c.dev>
2023-03-29 11:02:15 -0400

    more work

--- js/shared/Semgrep_js_shared.ml
+++ js/shared/Semgrep_js_shared.ml
@@ -1,18 +1,21 @@
 open Js_of_ocaml
 
-let make_js_module parse parse_pattern =
-  (* if running as WebWorker, handle parse() and parsePattern() messages *)
-  (if Js.Unsafe.global##.onmessage <> Js.undefined then
-   let handle_message msg =
-     match msg with
-     | "parse", filename -> Worker.post_message (parse filename)
-     | "parsePattern", str -> Worker.post_message (parse_pattern str)
-     | _ -> ()
-   in
-   Worker.set_onmessage handle_message);
+let write_file filename contents =
+  let oc = open_out_bin filename in
+  Printf.fprintf oc "%s" contents;
+  close_out oc
 
+let read_file filename =
+  let ic = open_in_bin filename in
+  let s = really_input_string ic (in_channel_length ic) in
+  close_in ic;
+  s
+
+let make_js_module parse parse_pattern =
   Js.export_all
     (object%js
-       method parse = parse
+       method parseFile = parse
        method parsePattern = parse_pattern
+       method writeFile = write_file
+       method readFile = read_file
     end)
--- js/shared/entrypoint.js
+++ js/shared/entrypoint.js
@@ -1,8 +1,33 @@
 import ParserModuleFactory from "./semgrep-parser";
 
-const ParserFactory = async () => {
+export const ParserFactory = async () => {
   globalThis.ParserModule = await ParserModuleFactory();
   return require("./Parser.bc");
 };
 
-export default ParserFactory;
+export const ParserWorkerFactory = (workerURL) => {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker(workerURL);
+
+    const makeMethod = (methodName) => {
+      return (args) =>
+        new Promise((resolve, reject) => {
+          worker.onmessage = (message) => resolve(message.data);
+          worker.onerror = reject;
+          worker.postMessage([methodName, ...args]);
+        });
+    };
+
+    worker.onmessage = (message) => {
+      if (message.data === "initialized") {
+        resolve({
+          parseFile: makeMethod("parseFile"),
+          parsePattern: makeMethod("parsePattern"),
+          writeFile: makeMethod("writeFile"),
+          readFile: makeMethod("readFile"),
+        });
+      }
+    };
+    worker.onerror = reject;
+  });
+};
