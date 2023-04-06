58859efa2b3f71ae32491f440314136fb31a28ba
brandonspark <wu.brandonj@gmail.com>
2023-04-06 13:40:35 -0700

    feat: exponentiation in metavariable comparison

---
+++ tests/rules/pow_metavar_comparison.py
@@ -0,0 +1,12 @@
+
+# ruleid: pow-metavar-comparison 
+x = foo(2, 3)
+
+# ruleid: pow-metavar-comparison 
+y = foo(2.0, 3)
+
+# ruleid: pow-metavar-comparison 
+w = foo(2, 3.0)
+
+# ruleid: pow-metavar-comparison 
+z = foo(2.0, 3.0)
