72e995735fa82153937ab9314383b23dd0306a74
Hannes Mehnert <hannes@mehnert.org>
2023-04-12 13:12:22 +0200

    Osemgrep: sync output with pysemgrep (#7499)
    Before this commit, the output of osemgrep was:
    [ scan status ] [ scan summary ] [ findings ]
    
    After this commit, the output is (similar to pysemgrep)
    [ scan status ] [ findings ] [ scan summary ]

--- src/osemgrep/cli_scan/Core_runner.mli
+++ src/osemgrep/cli_scan/Core_runner.mli
@@ -24,7 +24,6 @@ type result = {
 *)
 val invoke_semgrep_core :
   ?respect_git_ignore:bool ->
-  ?ignored_targets:Semgrep_output_v1_t.skipped_target list ->
   conf ->
   (* LATER? use Config_resolve.rules_and_origin instead? *)
   Rule.rules ->
