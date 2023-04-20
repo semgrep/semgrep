4f9c83ccbc8db6a36fb10bb196aca9c50fa014d7
pad <pad@r2c.dev>
2023-04-20 13:27:11 +0200

    cleanup: remove lib_parsing/meta_parse_info.ml
    Just move its content to ast_generic/Meta_AST.ml,
    which we should remove at some point.
    
    test plan:
    make core

--- libs/ast_generic/Meta_AST.mli
+++ libs/ast_generic/Meta_AST.mli
@@ -1,8 +1,10 @@
-(* Note that you probably should use AST_generic.show_any, which is
- * auto-generated rather than this file to display debug information.
+(* Deprecated: you should use AST_generic.show_any, which is
+ * auto-generated, rather than this file to debug ASTs.
+ *
  * We keep Meta_AST.ml just because we use it to generate the JSON
- * from the OCaml.v type of the generic AST in semgrep (and use that
- * JSON in the semgrep Web UI).
+ * from the OCaml.v type of the generic AST in Semgrep
+ * TODO: we don't display ASTs anymore in the Semgrep playground,
+ * and semgrep --dump-ast is deprecated, so we should remove this file.
  *)
 
 val vof_any : AST_generic.any -> OCaml.v
@@ -25,3 +27,15 @@ val vof_raw_tree : AST_generic.raw_tree -> OCaml.v
 val vof_incr_decr : AST_generic.incr_decr -> OCaml.v
 val vof_inc_dec : AST_generic.incr_decr * AST_generic.prefix_postfix -> OCaml.v
 val vof_prepost : AST_generic.prefix_postfix -> OCaml.v
+val vof_info : Tok.t -> OCaml.v
+
+type dumper_precision = {
+  full_info : bool;
+  token_info : bool;
+  type_info : bool;
+}
+
+val default_dumper_precision : dumper_precision
+val _current_precision : dumper_precision ref
+val vof_info_adjustable_precision : Tok.t -> OCaml.v
+val cmdline_flags_precision : unit -> Arg_helpers.flag_spec list
--- libs/lib_parsing/Parsing_result.mli
+++
@@ -1,9 +0,0 @@
-(* a parser can also "return" an exception like Lexical_error,
- * or Parsing_error (unless Flag_parsing.error_recovery is true).
- *)
-type ('ast, 'toks) t = {
-  ast : 'ast;
-  (* Note that the token list contains usually also the comment-tokens *)
-  tokens : 'toks list;
-  stat : Parsing_stat.t;
-}
