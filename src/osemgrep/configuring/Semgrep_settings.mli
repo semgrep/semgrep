26a8eaa6efaf69f53999dde6833ce3f0818069cd
Hannes Mehnert <hannes@mehnert.org>
2023-04-27 13:16:08 +0200

    osemgrep: login and logout subcommands (#7637)
    test plan:
      make core
      osemgrep login
      osemgrep logout
    
    Only the login and logout commands are now ported, the api_token is not
    (yet) used in http requests (this will be done in a followup PR).
    
    PR checklist:
    
    - [x] Purpose of the code is [evident to future
    readers](https://semgrep.dev/docs/contributing/contributing-code/#explaining-code)
    - [x] Tests included or PR comment includes a reproducible test plan
    - [x] Documentation is up-to-date
    - [x] A changelog entry was [added to
    changelog.d](https://semgrep.dev/docs/contributing/contributing-code/#adding-a-changelog-entry)
    for any user-facing change
    - [x] Change has no security implications (otherwise, ping security
    team)
    
    If you're unsure about any of this, please see:
    
    - [Contribution
    guidelines](https://semgrep.dev/docs/contributing/contributing-code)!
    - [One of the more specific guides located
    here](https://semgrep.dev/docs/contributing/contributing/)

---
+++ src/osemgrep/configuring/Semgrep_settings.mli
@@ -0,0 +1,9 @@
+(* content of the the ~/.semgrep/settings.yml file. See also Semgrep_envvars.user_settings_file *)
+type t = {
+  has_shown_metrics_notification : bool option;
+  api_token : string option;
+  anonymous_user_id : Uuidm.t;
+}
+
+val save : t -> unit
+val get : unit -> t
