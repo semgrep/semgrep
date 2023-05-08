5b75214c9197111561cc6163ba4a00f887431251
Vivek Khimani <vivek@r2c.dev>
2023-05-08 15:23:59 -0400

    changes to snapshot tests

--- cli/tests/e2e/test_ci.py
+++ cli/tests/e2e/test_ci.py
@@ -734,6 +734,9 @@ def test_full_run(
     for f in findings_and_ignores_json["ignores"]:
         assert f["commit_date"] is not None
         f["commit_date"] = "sanitized"
+    for f in findings_and_ignores_json["prev_scan_findings"]:
+        assert f["commit_date"] is not None
+        f["commit_date"] = "sanitized"
     snapshot.assert_match(
         json.dumps(findings_and_ignores_json, indent=2), "findings_and_ignores.json"
     )
