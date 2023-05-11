b9acf3febb468b16cfa99488a7ce5a9d054e1d36
Hannes Mehnert <hannes@mehnert.org>
2023-05-11 13:26:59 +0200

    pre-commit hook: add two empty lines between tests

--- cli/tests/e2e/test_rule_parser.py
+++ cli/tests/e2e/test_rule_parser.py
@@ -36,6 +36,7 @@ def test_rule_parser__failure(run_semgrep_in_tmp: RunSemgrep, snapshot, filename
 def test_regex_with_bad_language(run_semgrep_in_tmp: RunSemgrep, snapshot):
     run_semgrep_in_tmp("rules/syntax/badlanguage.yaml", assert_exit_code=7)
 
+
 @pytest.mark.osempass
 @pytest.mark.kinda_slow
 def test_nonexisting_faile(run_semgrep_in_tmp: RunSemgrep, snapshot):
