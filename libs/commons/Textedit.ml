64bedb210aa8d6318a06130693d6a29cc0091319
Heejong Lee <heejong@gmail.com>
2023-05-10 18:18:22 -0700

    update edits in apply_edits

--- libs/commons/Textedit.ml
+++ libs/commons/Textedit.ml
@@ -52,27 +52,10 @@ let remove_overlapping_edits edits =
   in
   f [] [] edits
 
-(* To align the autofix logic with the Python CLI, remove the newline
-     character when replacing an entire line with an empty string.*)
-let remove_newline_for_empty_replacement before after replacement =
-  let b_len = String.length before in
-  let a_len = String.length after in
-  let check_left = b_len = 0 || String.get before (b_len - 1) = '\n' in
-  let check_right = a_len = 0 || String.get after 0 = '\n' in
-  let is_empty_replacement = String.length replacement = 0 in
-  match check_left && check_right && is_empty_replacement with
-  | true when b_len > 0 ->
-      Some (String.sub before 0 (b_len - 1) ^ replacement ^ after)
-  | true when a_len > 0 ->
-      Some (before ^ replacement ^ String.sub after 1 (a_len - 1))
-  | _ -> None
-
 let apply_edit_to_text text { start; end_; replacement_text; _ } =
   let before = Str.string_before text start in
   let after = Str.string_after text end_ in
-  Option.value
-    (remove_newline_for_empty_replacement before after replacement_text)
-    ~default:(before ^ replacement_text ^ after)
+  before ^ replacement_text ^ after
 
 let apply_edits_to_text text edits =
   let edits = List.sort (fun e1 e2 -> e1.start - e2.start) edits in
@@ -112,12 +95,28 @@ let partition_edits_by_file edits =
     edits_by_file;
   edits_by_file
 
+(* To align the autofix logic with the Python CLI, remove the newline
+     character when replacing an entire line with an empty string.*)
+let remove_newline_for_empty_replacement text edit =
+  let text_len = String.length text in
+  let line_start = edit.start = 0 || String.get text (edit.start - 1) = '\n' in
+  let is_empty_replacement = String.length edit.replacement_text = 0 in
+  let skip_char c i =
+    if i + 1 <= text_len && String.get text i = c then i + 1 else i
+  in
+  if line_start && is_empty_replacement then
+    { edit with end_ = edit.end_ |> skip_char '\r' |> skip_char '\n' }
+  else edit
+
 let apply_edits ~dryrun edits =
   let edits_by_file = partition_edits_by_file edits in
   let all_conflicting_edits = ref [] in
   Hashtbl.iter
     (fun file file_edits ->
       let file_text = Common.read_file file in
+      let file_edits =
+        List.map (remove_newline_for_empty_replacement file_text) file_edits
+      in
       let new_text =
         match apply_edits_to_text file_text file_edits with
         | Success x -> x
