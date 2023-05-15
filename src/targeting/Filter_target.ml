8acb37a7447099503779918c3b6428322bf677c7
Yoann Padioleau <pad@r2c.dev>
2023-05-15 13:37:15 +0200

    osemgrep: remove Filter_target.target_cache (#7795)
    There was no proof it was optimizing anything and in any case
    it was used at the wrong place and generating wrong Lang_job.t
    The paths: include/exclude should be done in Run_semgrep.ml probably.
    
    test plan:
    make core-test
    make osempass
    
    
    PR checklist:
    
    - [x] Purpose of the code is [evident to future
    readers](https://semgrep.dev/docs/contributing/contributing-code/#explaining-code)
    - [x] Tests included or PR comment includes a reproducible test plan
    - [x] Documentation is up-to-date
    - [x] A changelog entry was [added to
    changelog.d](https://semgrep.dev/docs/contributing/contributing-code/#adding-a-changelog-entry)
    for any user-facing change
    - [x] Change has no security implications (otherwise, ping security
    team)
    
    If you're unsure about any of this, please see:
    
    - [Contribution
    guidelines](https://semgrep.dev/docs/contributing/contributing-code)!
    - [One of the more specific guides located
    here](https://semgrep.dev/docs/contributing/contributing/)

--- src/targeting/Filter_target.ml
+++ src/targeting/Filter_target.ml
@@ -3,14 +3,16 @@
 (*************************************************************************)
 (* Filter target candidates.
 
-   TODO: Handles all file include/exclude logic for semgrep
+   TODO: Handles the paths: include/exclude
 
-   filtering each (rule, target) pair can
-   become problematic since the number of such pairs is O(number of targets
-   * number of rules).
-   WORKS? This is why we cache the results of this step.
+   Filtering each (rule, target) pair can become problematic since the number
+   of such pairs is O(number of targets * number of rules).
+   TODO? This is why we should cache the results of this step.
    This allows reducing the number of rules to the number of different
    languages and patterns used by the rules.
+   update: there used to be such an opti, but I removed it, because
+   it was not clear that was the actual bottleneck. Gitignore
+   seems currently to be the thing to optimize.
 
    Partially translated from target_manager.py
 *)
@@ -19,84 +21,21 @@
 (* Types *)
 (*************************************************************************)
 
-(*
-   Some rules will use 'include' (required_path_patterns) and 'exclude'
-   (excluded_path_patterns) to select targets that don't have an extension
-   such as 'Dockerfile'. We expect most rules written for a language
-   to use the same combination of include/exclude. This allows caching
-   across the many rules that target the same language.
-*)
-type target_cache_key = {
-  path : Fpath.t;
-  lang : Xlang.t;
-  required_path_patterns : string list;
-  excluded_path_patterns : string list;
-}
-
-type target_cache = (target_cache_key, bool) Hashtbl.t
-
 (*************************************************************************)
 (* Helpers *)
 (*************************************************************************)
-
-let create_cache () = Hashtbl.create 1000
-
-let match_glob_pattern ~pat path =
-  let regex = Glob.Parse.parse_string pat in
-  Glob.Match.run
-    Glob.Match.(compile ~source:(string_loc ~source_kind:None pat) regex)
-    (Fpath.to_string path)
-
-let match_a_path_pattern path_patterns path =
-  match path_patterns with
-  | [] -> (* <grimacing face emoji> *) true
-  | pats -> List.exists (fun pat -> match_glob_pattern ~pat path) pats
-
-let match_language (xlang : Xlang.t) path =
-  match xlang with
-  | L (lang, langs) ->
-      (* ok if the file appears to be in one of rule's languages *)
-      List.exists
-        (fun lang -> Guess_lang.inspect_file_p lang path)
-        (lang :: langs)
-  | LRegex
-  | LGeneric ->
-      true
 
 (*************************************************************************)
 (* Entry point *)
 (*************************************************************************)
 
 (* Used by Core_runner.split_jobs_by_language *)
-let filter_target_for_lang ~cache ~lang ~required_path_patterns
-    ~excluded_path_patterns path =
-  let cond () =
-    match_a_path_pattern required_path_patterns path
-    && (Common.null excluded_path_patterns
-       || not (match_a_path_pattern excluded_path_patterns path))
-    && match_language lang path
-  in
-  (* TODO: use Common.memoize *)
-  let key : target_cache_key =
-    { path; lang; required_path_patterns; excluded_path_patterns }
-  in
-  match Hashtbl.find_opt cache key with
-  | Some res -> res
-  | None ->
-      let res = cond () in
-      Hashtbl.replace cache key res;
-      res
-
-(* TODO? useful?
-   let filter_target_for_rule cache (rule : Rule.t) path =
-     let required_path_patterns, excluded_path_patterns =
-       match rule.paths with
-       | Some { include_; exclude } -> (include_, exclude)
-       | None -> ([], [])
-     in
-     filter_target_for_lang ~cache ~lang:rule.languages ~required_path_patterns
-       ~excluded_path_patterns path
-
-   let filter_targets_for_rule cache rule files =
-     List.filter (filter_target_for_rule cache rule) files
-*)
+let filter_target_for_xlang (xlang : Xlang.t) (path : Fpath.t) : bool =
+  match xlang with
+  | L (lang, langs) ->
+      (* ok if the file appears to be in one of rule's languages *)
+      lang :: langs
+      |> List.exists (fun lang -> Guess_lang.inspect_file_p lang path)
+  | LRegex
+  | LGeneric ->
+      true
