6576e6b4d3e046bea6f8c4d0350acabdd86e225e
brandonspark <wu.brandonj@gmail.com>
2023-05-15 11:56:55 -0400

    tests: more tests and changelog

---
+++ tests/patterns/kotlin/not_string_metavar.kt
@@ -0,0 +1,10 @@
+
+val x = "anything"
+
+val x = "hi there"
+
+// ERROR: match
+val x = "hi $THERE"
+
+// ERROR: match
+val x = "hi ${THERE}"
---
+++ tests/patterns/kotlin/single_interpolated_ident.kt
@@ -0,0 +1,8 @@
+
+val x = "whatever"
+
+// ERROR: match
+val x = "$FOO"
+
+// ERROR: match
+val x = "${FOO}"
