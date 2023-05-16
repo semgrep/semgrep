cee9a5e5526420e285f4ac48473978f5895fd165
adamkvitek <adamkvitek@gmail.com>
2023-05-16 17:30:17 +0200

    Specify README content type as Markdown

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 # type: ignore
 # Used for pre-commit since it expects a setup.py in repo root
 # for actual setup.py see cli/setup.py
-from setuptools import setup
+from setuptools import setup(
+    long_description_content_type="text/markdown",
+    )
 
 setup(
     name="semgrep_pre_commit_package",
