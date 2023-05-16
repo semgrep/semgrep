semgrep/semgrep

commit eeaecf7a9b40fd833a0d1e0141069e3dfd880a3d

const-prop: Private static variables set once in a static block (#7527)
* const-prop: Private static variables set once in a static block

They will be considered like `final` for constant-propagation purposes,
for now just in Java.

Closes PA-2228

test plan:
make test # tests added

* Update pytest snapshots

---------

Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

src/analyzing/Constant_propagation.ml
@@ -70,11 +70,19 @@ type env = {
   lang : Lang.t option;
   (* basic constant propagation of literals for semgrep *)
   constants : (var, svalue) Hashtbl.t;
+  attributes : (var, G.attribute list) Hashtbl.t;
   in_lvalue : bool ref;
+  in_static_block : bool ref;
 }
 
 let default_env lang =
-  { lang; constants = Hashtbl.create 64; in_lvalue = ref false }
+  {
+    lang;
+    constants = Hashtbl.create 100;
+    attributes = Hashtbl.create 100;
+    in_lvalue = ref false;
+    in_static_block = ref false;
+  }
 
 type lr_stats = {
   (* note that a VarDef defining the value will count as 1 *)
@@ -115,14 +123,6 @@ let rec lvars_in_lhs expr =
 (* Environment Helpers *)
 (*****************************************************************************)
 
-let is_global = function
-  | Global -> true
-  | _ -> false
-
-let is_resolved_name = function
-  | GlobalName _ -> true
-  | _ -> false
-
 let is_lang env l2 =
   match env.lang with
   | None -> false
@@ -132,6 +132,16 @@ let is_js env =
   match env.lang with
   | None -> false
   | Some lang -> Lang.is_js lang
+
+let is_global = function
+  | Global -> true
+  | _ -> false
+
+let is_class_field env = function
+  | EnclosedVar -> is_lang env Lang.Java
+  | __else__ -> false
+
+let is_resolved_name _kind sid = not (SId.is_unsafe_default sid)
 
 let add_constant_env ident (sid, svalue) env =
   match svalue with
@@ -153,6 +163,14 @@ let find_name env name =
   match name with
   | Id (id, id_info) -> find_id env id id_info
   | IdQualified _ -> None
+
+let is_assigned_just_once stats var =
+  let id_str, sid = var in
+  match Hashtbl.find_opt stats var with
+  | Some stats -> !(stats.lvalue) = 1
+  | None ->
+      logger#debug "No stats for (%s,%s)" id_str (G.SId.show sid);
+      false
 
 let deep_constant_propagation_and_evaluate_literal x =
   match !hook_constant_propagation_and_evaluate_literal with
@@ -537,6 +555,22 @@ let propagate_basic lang prog =
               name =
                 EN
                   (Id
+                    (id, { id_resolved = { contents = Some (_kind, sid) }; _ }));
+              attrs;
+              _;
+            },
+            VarDef { vinit = None; _ } ) ->
+            if is_assigned_just_once stats (H.str_of_ident id, sid) then
+              (* This is a potential constant but it's defined later on, so we store
+               * it's attributes so check them later, e.g. to know whether it has
+               * `private` visibility. An example of this is a class field that
+               * is initialized in the constructor or in a `static` block. *)
+              Hashtbl.replace env.attributes (fst id, sid) attrs;
+            super#visit_definition venv x
+        | ( {
+              name =
+                EN
+                  (Id
                     ( id,
                       {
                         id_resolved = { contents = Some (_kind, sid) };
@@ -548,37 +582,41 @@ let propagate_basic lang prog =
             },
             VarDef { vinit = Some e; _ } )
         (* note that some languages such as Python do not have VarDef.
-         * todo? should add those somewhere instead of in_lvalue detection?*)
-          -> (
-            match Hashtbl.find_opt stats (H.str_of_ident id, sid) with
-            | Some stats ->
-                (if
-                 H.has_keyword_attr Const attrs
-                 || H.has_keyword_attr Final attrs
-                 || (!(stats.lvalue) = 1 && is_js env)
-                 || !(stats.lvalue) = 1 && env.lang = Some Lang.Java
-                    && List.exists is_private attrs
-                then
-                 match (!id_svalue, e.e) with
-                 (* When the name already has an svalue computed, just use
-                  * that. DeepSemgrep assigns svalues sometimes in its naming
-                  * phase. *)
-                 | Some svalue, _ -> add_constant_env id (sid, svalue) env
-                 | None, L literal -> add_constant_env id (sid, Lit literal) env
-                 (* For any other symbolic expression, it is OK to propagate it symbolically so long as
-                    the lvalue is only assigned to once.
-                    Although we may propagate expressions with identifiers in them, those identifiers
-                    will simply not have an `svalue` if they are non-propagated as well.
-                 *)
-                 | None, _ when Dataflow_svalue.is_symbolic_expr e ->
-                     add_constant_env id (sid, Sym e) env
-                 | None, _ -> ());
-                super#visit_definition venv x
-            | None ->
-                logger#debug "No stats for (%s,%s)" (H.str_of_ident id)
-                  (G.SId.show sid);
-                super#visit_definition venv x)
+         * todo? should add those somewhere instead of in_lvalue detection?*) ->
+            let assigned_just_once =
+              is_assigned_just_once stats (H.str_of_ident id, sid)
+            in
+            (if
+             H.has_keyword_attr Const attrs
+             || H.has_keyword_attr Final attrs
+             || (assigned_just_once && is_js env)
+             || assigned_just_once && is_lang env Lang.Java
+                && List.exists is_private attrs
+            then
+             match (!id_svalue, e.e) with
+             (* When the name already has an svalue computed, just use
+              * that. DeepSemgrep assigns svalues sometimes in its naming
+              * phase. *)
+             | Some svalue, _ -> add_constant_env id (sid, svalue) env
+             | None, L literal -> add_constant_env id (sid, Lit literal) env
+             (* For any other symbolic expression, it is OK to propagate it symbolically so long as
+                the lvalue is only assigned to once.
+                Although we may propagate expressions with identifiers in them, those identifiers
+                will simply not have an `svalue` if they are non-propagated as well.
+             *)
+             | None, _ when Dataflow_svalue.is_symbolic_expr e ->
+                 add_constant_env id (sid, Sym e) env
+             | None, _ -> ());
+            super#visit_definition venv x
         | _ -> super#visit_definition venv x
+
+      method! visit_stmt_kind venv x =
+        (match x with
+        | OtherStmtWithStmt (OSWS_Block ("Static", _), [], block) ->
+            Common.save_excursion env.in_static_block true (fun () ->
+                self#visit_stmt venv block)
+        | __else__ -> ());
+        super#visit_stmt_kind venv x
 
       (* the uses (and also defs for Python Assign) *)
       method! visit_expr venv x =
@@ -628,21 +666,28 @@ let propagate_basic lang prog =
               },
               _,
               rexp ) ->
-            eval env rexp
-            |> Option.iter (fun svalue ->
-                   match Hashtbl.find_opt stats (H.str_of_ident id, sid) with
-                   | Some stats ->
-                       if
-                         !(stats.lvalue) = 1
-                         (* restrict to Python/Ruby/PHP/JS/TS Globals for now *)
-                         && (is_lang env Lang.Python || is_lang env Lang.Ruby
-                           || is_lang env Lang.Php || is_js env)
-                         && (is_global kind || is_resolved_name kind)
-                       then add_constant_env id (sid, svalue) env
-                   | None ->
-                       logger#debug "No stats for (%s,%s)" (H.str_of_ident id)
-                         (G.SId.show sid);
-                       ());
+            let opt_svalue = eval env rexp in
+            (let is_private_class_field =
+               match Hashtbl.find_opt env.attributes (fst id, sid) with
+               | None -> false
+               | Some attrs ->
+                   List.exists is_private attrs && is_class_field env kind
+             in
+             if
+               is_assigned_just_once stats (H.str_of_ident id, sid)
+               (* restricted to prevent unexpected const-prop FPs *)
+               && ((is_lang env Lang.Python || is_lang env Lang.Ruby
+                  || is_lang env Lang.Php || is_js env)
+                   && is_global kind
+                  || is_lang env Lang.Java && is_private_class_field
+                     && !(env.in_static_block))
+               && is_resolved_name kind sid
+             then
+               match opt_svalue with
+               | Some svalue -> add_constant_env id (sid, svalue) env
+               | None ->
+                   if Dataflow_svalue.is_symbolic_expr rexp then
+                     add_constant_env id (sid, Sym rexp) env);
             self#visit_expr venv rexp
         | Assign (e1, _, e2)
         | AssignOp (e1, _, e2) ->

libs/commons/Gensym.ml
@@ -35,6 +35,7 @@ module MkId () : sig
   val mk : unit -> t
   val to_int : t -> int
   val unsafe_default : t
+  val is_unsafe_default : t -> bool
   val unsafe_reset_counter : unit -> unit
 end = struct
   open Ppx_hash_lib.Std.Hash.Builtin
@@ -49,5 +50,6 @@ end = struct
 
   let to_int = Fun.id
   let unsafe_default = -1
+  let is_unsafe_default id = id = unsafe_default
   let unsafe_reset_counter () = counter := 0
 end

libs/commons/Gensym.mli
@@ -12,6 +12,7 @@ module MkId () : sig
      `unsafe_default`s.
   *)
   val unsafe_default : t
+  val is_unsafe_default : t -> bool
 
   (* This will reset the internal counter used by the identifiers, making `mk`
      generate already-generated identifiers.

tests/rules/cp_private_class_attr2.java
@@ -1,0 +1,25 @@
+class Test {
+
+    private static int s;
+
+    private int x;
+
+    static {
+        s = 42;
+    }
+
+    public void doSomething1() {
+        x = 42;
+    }
+
+    public void doSomething2(){
+        //ok:test
+        foo(x);
+    }
+
+    public void doSomething3(){
+        //ruleid:test
+        foo(s);
+    }
+
+}

tests/rules/cp_private_class_attr2.yaml
@@ -1,0 +1,7 @@
+rules:
+  - id: test
+    severity: ERROR
+    message: Test
+    languages:
+      - java
+    pattern: foo(42)

tests/rules/sym_prop_class_attr.java
@@ -1,0 +1,31 @@
+package example;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+
+class BadDocumentBuilderFactoryStatic {
+
+    private static DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+    public void doSomething(){
+        //ruleid:documentbuilderfactory-disallow-doctype-decl-missing
+        dbf.newDocumentBuilder();
+    }
+
+}
+
+class BadDocumentBuilderFactoryStatic {
+
+    private static DocumentBuilderFactory dbf;
+
+    static {
+        dbf = DocumentBuilderFactory.newInstance();
+    }
+
+    public void doSomething(){
+        //ruleid:documentbuilderfactory-disallow-doctype-decl-missing
+        dbf.newDocumentBuilder();
+    }
+
+}

tests/rules/sym_prop_class_attr.yaml
@@ -1,0 +1,16 @@
+rules:
+  - id: documentbuilderfactory-disallow-doctype-decl-missing
+    options:
+      symbolic_propagation: true
+    severity: ERROR
+    message: Test.
+    mode: taint
+    pattern-sources:
+      - pattern: |
+          DocumentBuilderFactory.newInstance();
+    pattern-sinks:
+      - patterns:
+          - pattern: $FACTORY.newDocumentBuilder();
+    languages:
+      - java
+

commit 3807d5070246f7cc64399b8d06da1ccb936ea05c

fix: sym-prop: Make sure we perform cycle checks
Fixes: eeaecf7a9b4 ("const-prop: Private static variables set once in a static block (#7527)")

Closes PA-2740

test plan:
make test # test added

src/analyzing/Constant_propagation.ml
@@ -117,6 +117,34 @@ let rec lvars_in_lhs expr =
   | Container ((Tuple | Array), (_, es, _)) -> List.concat_map lvars_in_lhs es
   | __else__ -> []
 
+let no_cycles_in_sym_prop sid exp =
+  let for_all_sid : (G.sid -> bool) -> G.any -> bool =
+    (* Check that all sid's satisfy a given condition. We use refs so that
+     * we can have a single visitor for all calls, given that the old
+     * `mk_visitor` was kind of expensive, and constructing a visitor object
+     * may be as well. *)
+    let ff = ref (fun _ -> true) in
+    let ok = ref true in
+    let vout =
+      object
+        inherit [_] G.iter
+
+        method! visit_resolved_name _env (_, sid) =
+          ok := !ok && !ff sid;
+          if not !ok then raise Exit
+      end
+    in
+    fun f ast ->
+      ff := f;
+      ok := true;
+      try
+        vout#visit_any () ast;
+        !ok
+      with
+      | Exit -> false
+  in
+  for_all_sid (fun sid' -> sid' <> sid) (E exp)
+
 (*****************************************************************************)
 (* Environment Helpers *)
 (*****************************************************************************)
@@ -401,9 +429,23 @@ let var_stats prog : var_stats =
             super#visit_definition env x
         | _ -> super#visit_definition env x
 
-      (* TODO An `ExprStmt` method call (probably returning 'void') should count as
-       * a potential assignment too... We need a visit_stmt here. E.g. in Ruby
-       * `a.concat(b)` is going to update `a`. *)
+      (* TODO: An `ExprStmt` method call (probably returning 'void') should count as a
+       * potential assignment too... E.g. in Ruby `a.concat(b)` is going to update `a`. *)
+      method! visit_stmt env x =
+        (match x.s with
+        | For
+            ( _,
+              ForEach
+                ( PatId
+                    (id, { id_resolved = { contents = Some (_kind, sid) }; _ }),
+                  _,
+                  _ ),
+              _ ) ->
+            let var = (H.str_of_ident id, sid) in
+            let stat = get_stat_or_create var h in
+            incr stat.lvalue
+        | _ -> ());
+        super#visit_stmt env x
 
       method! visit_expr env x =
         match x.e with
@@ -626,7 +668,9 @@ let propagate_basic lang prog =
                 Although we may propagate expressions with identifiers in them, those identifiers
                 will simply not have an `svalue` if they are non-propagated as well.
              *)
-             | None, _ when Dataflow_svalue.is_symbolic_expr e ->
+             | None, _
+               when Dataflow_svalue.is_symbolic_expr e
+                    && no_cycles_in_sym_prop sid e ->
                  add_constant_env id (sid, Sym e) env
              | None, _ -> ());
             super#visit_definition venv x
@@ -708,8 +752,10 @@ let propagate_basic lang prog =
                match opt_svalue with
                | Some svalue -> add_constant_env id (sid, svalue) env
                | None ->
-                   if Dataflow_svalue.is_symbolic_expr rexp then
-                     add_constant_env id (sid, Sym rexp) env;
+                   if
+                     Dataflow_svalue.is_symbolic_expr rexp
+                     && no_cycles_in_sym_prop sid rexp
+                   then add_constant_env id (sid, Sym rexp) env;
                    ());
             self#visit_expr venv rexp
         | Assign (e1, _, e2)

tests/rules/sym_prop_no_cycle.py
@@ -1,0 +1,3 @@
+for p in FOO:
+    #ruleid: test
+    p = p

tests/rules/sym_prop_no_cycle.yaml
@@ -1,0 +1,11 @@
+rules:
+  - id: test
+    languages:
+      - python
+    options:
+      symbolic_propagation: true
+    pattern: |
+      p = ...
+      ... 
+    severity: ERROR
+    message: Test