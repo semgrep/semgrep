c090e7178d7c8064e67d8c33fab6b66d31012e95
Emma Jin <--get>
2023-05-18 14:31:35 -0700

    typo

--- src/runner/Run_semgrep.ml
+++ src/runner/Run_semgrep.ml
@@ -903,7 +903,7 @@ let semgrep_with_rules_and_formatted_output config =
         |> List.iter (fun explain -> Matching_explanation.print explain);
       (* the match has already been printed above. We just print errors here *)
       if not (null res.errors) then (
-        pr "WARNING: some files were skipped on only partially analyzed:";
+        pr "WARNING: some files were skipped or only partially analyzed:";
         res.errors |> List.iter (fun err -> pr (E.string_of_error err)))
 
 (*****************************************************************************)
