87b1718faf2b71db79e5324166274f799da3beac
Iago Abal <iagoabal+github@gmail.com>
2023-05-23 11:26:16 +0200

    sym-prop: Allow some Sym-svalues to survive after JOINs (#7852)
    Previously any Sym-value would be "killed" at a JOIN point, as a
    precautionary measure to avoid `id_svalue` cycles. Here we relax this
    condition to allow trivial cases to work. Basically, if a Sym-value for
    a variable 'x' was recorded before a fork in the CFG, and then we reach
    the JOIN without 'x' having been modified, so that the Sym-value
    assigned to 'x' in all branches is exactly (physically) the same, then
    we allow that Sym-value to survive.
    
    Helps PA-2739
    
    test plan:
    make test # new tests
    
    PR checklist:
    
    - [x] Purpose of the code is [evident to future
    readers](https://semgrep.dev/docs/contributing/contributing-code/#explaining-code)
    - [x] Tests included or PR comment includes a reproducible test plan
    - [ ] Documentation is up-to-date
    - [x] A changelog entry was [added to
    changelog.d](https://semgrep.dev/docs/contributing/contributing-code/#adding-a-changelog-entry)
    for any user-facing change
    - [x] Change has no security implications (otherwise, ping security
    team)
    
    If you're unsure about any of this, please see:
    
    - [Contribution
    guidelines](https://semgrep.dev/docs/contributing/contributing-code)!
    - [One of the more specific guides located
    here](https://semgrep.dev/docs/contributing/contributing/)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/rules/sym_prop_merge1.java
+++ tests/rules/sym_prop_merge1.java
@@ -10,10 +10,7 @@ public class LoginServlet extends HttpServlet {
             //ruleid: test
             baz(userName);
         }
-        // TODO: Right now we kill all sym-prop values at JOIN points (i.e. after if-
-        //       or loop statements). In principle we would not have to do this when
-        //       all the branches arriving at the JOIN carry the same sym-prop value.
-        //todoruleid: test
+        //ruleid: test
         baz(userName);
     }
 }
