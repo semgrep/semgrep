fc8cc11d5d1ef9ea914017e59d33c69dc00800f8
brandonspark <wu.brandonj@gmail.com>
2023-05-23 15:24:17 -0400

    test: add test for semgrep scan --help text

--- cli/tests/e2e/test_help.py
+++ cli/tests/e2e/test_help.py
@@ -16,3 +16,17 @@ def test_help_text(tmp_path, snapshot, help_flag):
     )
     result = runner.invoke(cli, [help_flag], env={})
     snapshot.assert_match(result.output, "help.txt")
+
+
+@pytest.mark.quick
+@pytest.mark.parametrize("help_flag", ["--help", "-h"])
+@pytest.mark.osempass
+def test_scan_help_text(tmp_path, snapshot, help_flag):
+    """
+    Test to make sure scan help text doesn't change unintentionally
+    """
+    runner = SemgrepRunner(
+        env={"SEMGREP_SETTINGS_FILE": str(tmp_path / ".settings.yaml")}
+    )
+    result = runner.invoke(cli, ["scan", help_flag], env={})
+    snapshot.assert_match(result.output, "help.txt")
