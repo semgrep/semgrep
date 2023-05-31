d2b55eaa7d63ae9d4667a2beb7477a38074c35cc
brandonspark <wu.brandonj@gmail.com>
2023-05-31 19:10:38 -0400

    refactor: change printf to echo for shellcheck

--- scripts/run-core-test.sh
+++ scripts/run-core-test.sh
@@ -13,6 +13,6 @@ NC='\033[0m' # No Color
 
 dune runtest -f --no-buffer src/tests
 if ! dune runtest -f --no-buffer src/osemgrep; then
-  printf "\n${RED}If expectation tests have failed, run \`dune promote\` to apply the corresponding diff.${NC}\n\n"
+  echo -e "\n${RED}If expectation tests have failed, run \`dune promote\` to apply the corresponding diff.${NC}\n\n"
   exit 1
 fi
