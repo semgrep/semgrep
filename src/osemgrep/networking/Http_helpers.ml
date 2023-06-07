27eb7f5d8a87a44c84c3e9aded4ecc2635f0a9f5
Hannes Mehnert <hannes@mehnert.org>
2023-06-07 16:14:34 +0200

    explain the happy-eyeballs here

--- src/osemgrep/networking/Http_helpers.ml
+++ src/osemgrep/networking/Http_helpers.ml
@@ -1,5 +1,20 @@
 open Http_lwt_client
 
+(* happy eyeballs is an Internet standard
+   (https://datatracker.ietf.org/doc/html/rfc8305) and an OCaml package which
+   purpose is to establish a TCP connection, independent of the internet
+   protocol version:
+   - the input is a hostname (and a port), the output is either a file
+     descriptor or an error
+   - it does DNS resolution (for both A (IPv4) and AAAA (IPv6)) - 3 attempts
+     with a timeout of "resolve_timeout" (here: 2 seconds)
+   - it then attempts to establish to the IPv6 address(es) and IPv4 address(es)
+     (with a preference to IPv6), using a 10 seconds timeout ("connect_timeout",
+     defaults to 10 seconds)
+
+   The HTTP lwt client library uses happy_eyeballs as the underlying
+   layer for establishing connections.
+*)
 let happy_eyeballs =
   let happy_eyeballs =
     Happy_eyeballs.create ~resolve_timeout:(Duration.of_sec 2)
