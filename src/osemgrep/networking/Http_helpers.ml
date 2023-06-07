167d1956e52c25ec10f8508ec424a04478926751
Hannes Mehnert <hannes@mehnert.org>
2023-06-07 19:08:13 +0200

    osemgrep: increase the HTTP timeout: DNS resolution is 2 seconds now, (#7984)
    TCP connection establishment 10 seconds
    
    PR checklist:
    
    - [x] Purpose of the code is [evident to future
    readers](https://semgrep.dev/docs/contributing/contributing-code/#explaining-code)
    - [x] Tests included or PR comment includes a reproducible test plan
    - [x] Documentation is up-to-date
    - [x] A changelog entry was [added to
    changelog.d](https://semgrep.dev/docs/contributing/contributing-code/#adding-a-changelog-entry)
    for any user-facing change
    - [x] Change has no security implications (otherwise, ping security
    team)
    
    If you're unsure about any of this, please see:
    
    - [Contribution
    guidelines](https://semgrep.dev/docs/contributing/contributing-code)!
    - [One of the more specific guides located
    here](https://semgrep.dev/docs/contributing/contributing/)

--- src/osemgrep/networking/Http_helpers.ml
+++ src/osemgrep/networking/Http_helpers.ml
@@ -1,9 +1,31 @@
 open Http_lwt_client
 
+(* happy eyeballs is an Internet standard
+   (https://datatracker.ietf.org/doc/html/rfc8305) and an OCaml package which
+   purpose is to establish a TCP connection, independent of the internet
+   protocol version:
+   - the input is a hostname (and a port), the output is either a file
+     descriptor or an error
+   - it does DNS resolution (for both A (IPv4) and AAAA (IPv6)) - 3 attempts
+     with a timeout of "resolve_timeout" (here: 2 seconds)
+   - it then attempts to establish to the IPv6 address(es) and IPv4 address(es)
+     (with a preference to IPv6), using a 10 seconds timeout ("connect_timeout",
+     defaults to 10 seconds)
+
+   The HTTP lwt client library uses happy_eyeballs as the underlying
+   layer for establishing connections.
+*)
+let happy_eyeballs =
+  let happy_eyeballs =
+    Happy_eyeballs.create ~resolve_timeout:(Duration.of_sec 2)
+      (Mtime_clock.elapsed_ns ())
+  in
+  Happy_eyeballs_lwt.create ~happy_eyeballs ()
+
 (* TODO: extend to allow to curl with JSON as answer *)
 let get ?headers url =
   let bodyf _ acc data = Lwt.return (acc ^ data) in
-  let promise = request ?headers (Uri.to_string url) bodyf "" in
+  let promise = request ~happy_eyeballs ?headers (Uri.to_string url) bodyf "" in
   let r = Lwt_main.run promise in
   match r with
   | Ok (response, content) when Status.is_successful response.status ->
@@ -18,7 +40,8 @@ let get ?headers url =
 let post ~body ?(headers = [ ("content-type", "application/json") ]) url =
   let bodyf _ acc data = Lwt.return (acc ^ data) in
   let promise =
-    request ~meth:`POST ~headers ~body (Uri.to_string url) bodyf ""
+    request ~happy_eyeballs ~meth:`POST ~headers ~body (Uri.to_string url) bodyf
+      ""
   in
   let r = Lwt_main.run promise in
   match r with
