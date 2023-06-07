semgrep/semgrep

commit 7e7641fb1b3623661c87d5e29b654ab989ba0d26

feat(taint): Relate Java getters and setters to properties (take 2) (#7983)
Fixes: 22b421dc597 ("feat(taint): Relate Java getters and setters to
properties (#7977)")
Reverts: 62f60ffb068 ("Revert "feat(taint): Relate Java getters and
setters to properties (#7977)" (#7980)")

Previously in Java, Semgrep would consider `o.getX()` tainted if a
tainted argument was passed to `o.setX(...)`. However, it would not
relate that taint back to the underlying property itself. This means
that taint would only be tracked if getters and setters were used in
concert. If a regular assignment to the property was made, followed by a
getter, or if a setter was used, followed by a direct access to the
property, Semgrep would fail to track taint.

Here, I've changed the handling of `o.setX(...)` to make Semgrep
consider the property `o.x` tainted, and added functionality to consider
`o.getX()` tainted if `o.x` is tainted. The modified tests illustrate
the change in behavior.

Test plan: Automated tests
(Also, see returntocorp/semgrep-proprietary#767)

Co-authored-by: Nat Mote <nat@semgrep.com>
Co-authored-by: Iago Abal <iago@semgrep.com>

---------

PR checklist:

- [x] Purpose of the code is [evident to future
readers](https://semgrep.dev/docs/contributing/contributing-code/#explaining-code)
- [x] Tests included or PR comment includes a reproducible test plan
- [x] Documentation is up-to-date
- [x] A changelog entry was [added to
changelog.d](https://semgrep.dev/docs/contributing/contributing-code/#adding-a-changelog-entry)
for any user-facing change
- [x] Change has no security implications (otherwise, ping security
team)

If you're unsure about any of this, please see:

- [Contribution
guidelines](https://semgrep.dev/docs/contributing/contributing-code)!
- [One of the more specific guides located
here](https://semgrep.dev/docs/contributing/contributing/)

Co-authored-by: Nat Mote <nat@semgrep.com>

src/tainting/Dataflow_tainting.ml
@@ -704,28 +704,6 @@ let find_pos_in_actual_args args_taints fparams =
         "cannot match taint variable with function arguments (%i: %s)" i s;
     taint_opt
 
-let propagate_taint_via_java_setter env e args all_args_taints =
-  match (e, args) with
-  | ( {
-        e =
-          Fetch
-            ({ base = Var _obj; rev_offset = [ ({ o = Dot m; _ } as offset) ] }
-            as lval);
-        _;
-      },
-      [ _ ] )
-    when env.lang =*= Lang.Java
-         && Stdcompat.String.starts_with ~prefix:"set" (fst m.IL.ident) ->
-      let prop_name = "get" ^ Str.string_after (fst m.IL.ident) 3 in
-      let prop_lval =
-        let o = Dot { m with ident = (prop_name, snd m.IL.ident) } in
-        { lval with rev_offset = [ { offset with o } ] }
-      in
-      if not (Taints.is_empty all_args_taints) then
-        Lval_env.add env.lval_env prop_lval all_args_taints
-      else env.lval_env
-  | __else__ -> env.lval_env
-
 let fix_poly_taint_with_field env lval st =
   if env.lang =*= Lang.Java || Lang.is_js env.lang then
     match lval.rev_offset with
@@ -737,16 +715,9 @@ let fix_poly_taint_with_field env lval st =
             st
         | `Tainted taints -> (
             match !(n.id_info.id_type) with
-            | Some { t = TyFun _; _ }
-              when env.lang <> Lang.Java
-                   || Option.is_some !(n.id_info.id_resolved)
-                   || not
-                        (Stdcompat.String.starts_with ~prefix:"get"
-                           (fst n.ident)) ->
+            | Some { t = TyFun _; _ } ->
                 (* We have an l-value like `o.f` where `f` has a function type,
-                 * so it's a method call. We will only fix poly-taint in such case
-                 * when it's an unresolved Java `getX` method. In any other case,
-                 * we do nothing here. *)
+                 * so it's a method call, we do nothing here. *)
                 st
             | __else__ ->
                 (* Not a method call (to the best of our knowledge) or
@@ -962,6 +933,47 @@ let rec check_tainted_lval env (lval : IL.lval) : Taints.t * Lval_env.t =
   report_findings { env with lval_env } findings;
   (taints, lval_env)
 
+and propagate_taint_via_unresolved_java_getters_and_setters env e args
+    all_args_taints =
+  match e with
+  | {
+   e =
+     Fetch
+       ({
+          base = Var _obj;
+          rev_offset =
+            [ { o = Dot { IL.ident = method_str, method_tok; id_info; _ }; _ } ];
+        } as lval);
+   _;
+  }
+    when env.lang =*= Lang.Java
+         && Option.is_none !(id_info.id_resolved)
+         && String.length method_str > 3 -> (
+      (* e.g. getFooBar/setFooBar -> fooBar *)
+      let prop_str =
+        String.uncapitalize_ascii (Str.string_after method_str 3)
+      in
+      (* TODO: Fix this with naming info coming from Pro via some hook. *)
+      let prop_name =
+        {
+          ident = (prop_str, method_tok);
+          sid = G.SId.unsafe_default;
+          id_info = G.empty_id_info ();
+        }
+      in
+      let prop_lval =
+        { lval with rev_offset = [ { o = Dot prop_name; oorig = NoOrig } ] }
+      in
+      match args with
+      | [] when Stdcompat.String.(starts_with ~prefix:"get" method_str) ->
+          check_tainted_lval env prop_lval
+      | [ _ ] when Stdcompat.String.starts_with ~prefix:"set" method_str ->
+          if not (Taints.is_empty all_args_taints) then
+            (Taints.empty, Lval_env.add env.lval_env prop_lval all_args_taints)
+          else (Taints.empty, env.lval_env)
+      | __else__ -> (Taints.empty, env.lval_env))
+  | __else__ -> (Taints.empty, env.lval_env)
+
 and check_tainted_lval_aux env (lval : IL.lval) :
     Taints.t
     (* `Sanitized means that the lval matched a sanitizer "right now", whereas
@@ -1026,7 +1038,7 @@ and check_tainted_lval_aux env (lval : IL.lval) :
                 (* HACK(field-sensitivity): If we encounter `obj.x` and `obj` has
                    * polymorphic taint, and we know nothing specific about `obj.x`, then
                    * we add the same offset `.x` to the polymorphic taint coming from `obj`.
-                   * (See also 'propagate_taint_via_java_setter'.)
+                   * (See also 'propagate_taint_via_unresolved_java_getters_and_setters'.)
                    *
                    * For example, given `function foo(o) { sink(o.x); }`, and being '0 the
                    * polymorphic taint of `o`, this allows us to record that what goes into
@@ -1531,11 +1543,14 @@ let check_tainted_instr env instr : Taints.t * Lval_env.t =
                      * the taint of its arguments. *)
                   all_args_taints
               in
-              let lval_env =
-                (* HACK: Java: If we encounter `obj.setX(arg)` we interpret this as `obj.getX = arg`. *)
-                propagate_taint_via_java_setter { env with lval_env } e args
-                  all_args_taints
+              let getter_taints, lval_env =
+                (* HACK: Java: If we encounter `obj.setX(arg)` we interpret it as
+                 * `obj.x = arg`, if we encounter `obj.getX()` we interpret it as
+                 * `obj.x`. *)
+                propagate_taint_via_unresolved_java_getters_and_setters
+                  { env with lval_env } e args all_args_taints
               in
+              let call_taints = Taints.union call_taints getter_taints in
               (call_taints, lval_env)
         in
         (* We add the taint of the function itselt (i.e., 'e_taints') too.

tests/rules/taint_get_set_sensitivity1.java
@@ -1,0 +1,22 @@
+public class H {
+    public void h1() {
+        RE e = null;
+        e = new RE();
+        e.setX(tainted);
+        // OK: test
+        sink(e.y);
+        // ruleid: test
+        sink(e.x);
+
+    }
+    public void h2() {
+        d = new RE();
+        d.x = tainted;
+        // OK:
+        sink(d.getY());
+        // ruleid: test
+        sink(d.getX());
+        // ruleid: test
+        sink(d.x);
+    }
+}

tests/rules/taint_get_set_sensitivity1.yaml
@@ -1,0 +1,11 @@
+rules:
+  - id: test
+    languages:
+      - java
+    severity: WARNING
+    mode: taint
+    message: Test
+    pattern-sources:
+      - pattern: tainted
+    pattern-sinks:
+      - pattern: sink(...)

commit 43e6770495789e26529b036b56db1beefc3e1c1e

tainting: Fix perf issue due to allocating tons of `id_info's
Fixes: 7e7641fb1b3 ("feat(taint): Relate Java getters and setters to properties (take 2) (#7983)")

test plan:
See returntocorp/semgrep-proprietary#767 (passes benchmarks)

src/tainting/Dataflow_tainting.ml
@@ -927,6 +927,10 @@ let find_lval_taint_sources env incoming_taints lval =
   let lval_env = Lval_env.add env.lval_env lval taints_sources_mut in
   (Taints.union taints_sources_reg taints_sources_mut, lval_env)
 
+let fixme_fake_id_info_for_propagate_taint_via_unresolved_java_getters_and_setters
+    =
+  G.empty_id_info ()
+
 let rec check_tainted_lval env (lval : IL.lval) : Taints.t * Lval_env.t =
   let new_taints, lval_in_env, lval_env = check_tainted_lval_aux env lval in
   let taints_from_env = status_to_taints lval_in_env in
@@ -968,7 +972,10 @@ and propagate_taint_via_unresolved_java_getters_and_setters env e args
         {
           ident = (prop_str, method_tok);
           sid = G.SId.unsafe_default;
-          id_info = G.empty_id_info ();
+          id_info =
+            (* Allocating one of this every time turns out to be very expensive.
+             * Perhaps we should cache them using (obj_ty, prop_str) as a cache key ? *)
+            fixme_fake_id_info_for_propagate_taint_via_unresolved_java_getters_and_setters;
         }
       in
       let prop_lval =