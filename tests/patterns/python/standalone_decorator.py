5d8a1721133a32e79c1847b83095912427bd5008
Yosef Alsuhaibani <yosefalsuhaibani@Yosefs-MBP.local.meter>
2023-06-14 10:43:54 -0700

    Extended testing

--- tests/patterns/python/standalone_decorator.py
+++ tests/patterns/python/standalone_decorator.py
@@ -10,7 +10,22 @@ def grumble():
 
 #ERROR:
 @decorator("oh")
+#ERROR:
 @decorator("my")
 def grr():
   return 0
+
+#ERROR:
+@decorator(10)
+class ifed():
+  #OK:
+  def __init__():
+    return None
+
+#OK:
+class room(1,2,3):
+  #ERROR:
+  @look("up")
+  def __init__():
+    return None
 
