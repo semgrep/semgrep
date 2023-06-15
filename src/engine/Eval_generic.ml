47dc6438568d99d535948dc4a80775a41763b518
Sophia Roshal <sophia.roshal@gmail.com>
2023-06-15 13:44:28 -0700

    refactoring to factor out helper function

--- src/engine/Eval_generic.ml
+++ src/engine/Eval_generic.ml
@@ -132,6 +132,32 @@ let print_result xopt =
 (* Eval algorithm *)
 (*****************************************************************************)
 
+(* Helper function to convert string date to Epoch time *)
+let string_to_date s code =
+  let yyyy_mm_dd = String.split_on_char '-' s in
+  match yyyy_mm_dd with
+  | [ y; m; d ] -> (
+      match (int_of_string_opt y, int_of_string_opt m, int_of_string_opt d) with
+      | Some yv, Some mv, Some dv when CalendarLib.Date.is_valid_date yv mv dv
+        ->
+          (*ok to put in arbitrary values for last 3 arguments since they are ignored *)
+          let time : Unix.tm =
+            {
+              tm_sec = 0;
+              tm_min = 0;
+              tm_hour = 0;
+              tm_mday = dv;
+              tm_mon = mv;
+              tm_year = yv - 1900;
+              tm_wday = 0;
+              tm_yday = 0;
+              tm_isdst = false;
+            }
+          in
+          Float (fst (Unix.mktime time))
+      | _ -> raise (NotHandled code))
+  | _ -> raise (NotHandled code)
+
 let value_of_lit ~code x =
   match x with
   | G.Bool (b, _t) -> Bool b
@@ -208,32 +234,7 @@ let rec eval env code =
   | G.Call ({ e = G.N (G.Id (("strptime", _), _)); _ }, (_, [ Arg e ], _)) -> (
       let v = eval env e in
       match v with
-      | String s -> (
-          let yyyy_mm_dd = String.split_on_char '-' s in
-          match yyyy_mm_dd with
-          | [ y; m; d ] -> (
-              match
-                (int_of_string_opt y, int_of_string_opt m, int_of_string_opt d)
-              with
-              | Some yv, Some mv, Some dv
-                when CalendarLib.Date.is_valid_date yv mv dv ->
-                  (*ok to put in arbitrary values for last 3 arguments since they are ignored? *)
-                  let time : Unix.tm =
-                    {
-                      tm_sec = 0;
-                      tm_min = 0;
-                      tm_hour = 0;
-                      tm_mday = dv;
-                      tm_mon = mv;
-                      tm_year = yv - 1900;
-                      tm_wday = 0;
-                      tm_yday = 0;
-                      tm_isdst = false;
-                    }
-                  in
-                  Float (fst (Unix.mktime time))
-              | _ -> raise (NotHandled code))
-          | _ -> raise (NotHandled code))
+      | String s -> string_to_date s code
       | __else__ -> raise (NotHandled code))
   | G.Call ({ e = G.N (G.Id (("today", _), _)); _ }, (_, _, _)) ->
       Float (Unix.time ())
