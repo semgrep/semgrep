6cd5e00f555ac02dba878422afe6f88739c32515
pad <pad@r2c.dev>
2023-06-23 14:17:40 +0200

    Revert "feat(c#): restore GA parse rate (#8023)"
    This reverts commit fd7f3468d150a88f7980f0b7f364c7f4ee39c977.

--- tests/parsing/csharp/makeref.cs
+++ tests/parsing/csharp/makeref.cs
@@ -1,14 +1,7 @@
-class HelloWorldLinq
-{
-    public static void Main()
-    {
-      int x = 5;
-      var reference = __makeref(x);
-      Console.WriteLine(__refvalue(reference, int));
-      Console.WriteLine(__reftype(reference));
+int x = 5;
+var reference = __makeref(x);
+Console.WriteLine(__refvalue(reference, int));
+Console.WriteLine(__reftype(reference));
 
-      // TODO: C# tree-sitter grammar doesn't like this
-      // __refvalue(reference, int) = 10;
-      Console.WriteLine(__refvalue(reference, int));
-    }
-}
+__refvalue(reference, int) = 10;
+Console.WriteLine(__refvalue(reference, int));
