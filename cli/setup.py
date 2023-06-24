4d7aef03f305f1491db24f50a21b4878f1040efb
Yoann Padioleau <pad@r2c.dev>
2023-06-24 13:55:18 +0200

    Change semgrep entry point, dispatch to osemgrep if --experimental (#8121)
    test plan:
    ```
    cd cli
    pipenv install --dev
    pipenv shell
    time semgrep --help
    0.335s
    time semgrep --experimental --help
    0.077s
    ```
    
    
    PR checklist:
    
    - [x] Purpose of the code is [evident to future
    readers](https://semgrep.dev/docs/contributing/contributing-code/#explaining-code)
    - [x] Tests included or PR comment includes a reproducible test plan
    - [ ] Documentation is up-to-date
    - [x] A changelog entry was [added to
    changelog.d](https://semgrep.dev/docs/contributing/contributing-code/#adding-a-changelog-entry)
    for any user-facing change
    - [x] Change has no security implications (otherwise, ping security
    team)
    
    If you're unsure about any of this, please see:
    
    - [Contribution
    guidelines](https://semgrep.dev/docs/contributing/contributing-code)!
    - [One of the more specific guides located
    here](https://semgrep.dev/docs/contributing/contributing/)
    
    ---------
    
    Co-authored-by: Tom Petr <tom@r2c.dev>

--- cli/setup.py
+++ cli/setup.py
@@ -157,7 +157,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/returntocorp/semgrep",
-    scripts=["bin/semgrep"],
+    scripts=["bin/semgrep", "bin/pysemgrep"],
     packages=setuptools.find_packages(where="src"),
     package_dir={"": "src"},
     package_data={"semgrep": [os.path.join(BIN_DIR, "*")]},
