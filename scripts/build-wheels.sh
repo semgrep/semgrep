db5ff9c696dff1fcbe3ed478d12277a739f327dc
pad <pad@r2c.dev>
2023-06-27 12:09:43 +0200

    Simplify cli/setup.py
    test plan:
    pip install --dev
    wait for green CI checks

--- scripts/build-wheels.sh
+++ scripts/build-wheels.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-# This script build "wheels" which is a format used by the Pypi package manager.
-# See https://realpython.com/python-wheels/
+# This script build "wheels", which is a format used by the Pypi package manager
+# to distribute binaries (for us semgrep-core) with regular Python code.
+# See https://packaging.python.org/en/latest/glossary/#term-Wheel
+# and https://realpython.com/python-wheels/ for more information.
 # This script is called from our GHA build-xxx workflows.
-# Note that cli/setup.py called below assumes the presence of a SEMGREP_CORE_BIN
-# environment variable to find the semgrep-core binary.
+# It assumes the semgrep-core binary has been copied under cli/src/semgrep/bin
+# for pip to package semgrep correctly.
 
 set -e
 pip3 install setuptools wheel
