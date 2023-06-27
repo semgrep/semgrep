93bafdfd8882502b01454b97d93e2009c352ef8d
Hannes Mehnert <hannes@mehnert.org>
2023-06-27 10:00:37 +0200

    extract json decoding functions to toplevel

--- src/osemgrep/networking/Scan_helper.ml
+++ src/osemgrep/networking/Scan_helper.ml
@@ -1,3 +1,26 @@
+(* TODO: specify as ATD the reply of api/agent/deployments/scans *)
+let extract_scan_id data =
+  try
+    let json = JSON.json_of_string data in
+    match json with
+    | Object xs -> (
+        match List.assoc_opt "scan" xs with
+        | Some (Object dd) -> (
+            match List.assoc_opt "id" dd with
+            | Some (Int i) -> Ok (string_of_int i)
+            | Some (String s) -> Ok s
+            | _else ->
+                Error
+                  ("Bad json in body when looking for scan id: no id: " ^ data))
+        | _else ->
+            Error
+              ("Bad json in body when trying to find scan id: no scan: " ^ data)
+        )
+    | _else -> Error ("Bad json in body when asking for scan id: " ^ data)
+  with
+  | e ->
+      Error ("Couldn't parse json, error: " ^ Printexc.to_string e ^ ": " ^ data)
+
 let start_scan ~dry_run ~token url meta =
   if dry_run then (
     Logs.app (fun m -> m "Would have sent POST request to create scan");
@@ -13,30 +36,7 @@ let start_scan ~dry_run ~token url meta =
     let scan_endpoint = Uri.with_path url "api/agent/deployments/scans" in
     let body = JSON.(string_of_json (Object [ ("meta", meta) ])) in
     match Http_helpers.post ~body ~headers scan_endpoint with
-    | Ok body -> (
-        try
-          let json = JSON.json_of_string body in
-          match json with
-          | Object xs -> (
-              match List.assoc_opt "scan" xs with
-              | Some (Object dd) -> (
-                  match List.assoc_opt "id" dd with
-                  | Some (Int i) -> Ok (string_of_int i)
-                  | Some (String s) -> Ok s
-                  | _else ->
-                      Error
-                        ("Bad json in body when looking for scan id: no id: "
-                       ^ body))
-              | _else ->
-                  Error
-                    ("Bad json in body when trying to find scan id: no scan: "
-                   ^ body))
-          | _else -> Error ("Bad json in body when asking for scan id: " ^ body)
-        with
-        | e ->
-            Error
-              ("Couldn't parse json, error: " ^ Printexc.to_string e ^ ": "
-             ^ body))
+    | Ok body -> extract_scan_id body
     | Error (status, msg) ->
         let pre_msg =
           if status = 404 then
@@ -50,6 +50,19 @@ Please make sure they have been set correctly.
             url msg
         in
         Error msg)
+
+(* TODO: specify as ATD the reply to api/agent/deployments/scans/config *)
+let extract_rule_config data =
+  try
+    match Yojson.Basic.from_string data with
+    | `Assoc e -> (
+        match List.assoc "rule_config" e with
+        | `String e -> Ok e
+        | _else -> Error ("Couldn't retrieve config: no rule_config in " ^ data)
+        )
+    | _else -> Error ("Couldn't retrieve config: not an json object: " ^ data)
+  with
+  | e -> Error ("Failed to decode config: " ^ Printexc.to_string e ^ ": " ^ data)
 
 let fetch_scan_config ~token ~sca ~dry_run ~full_scan repository =
   (* TODO (see below): once we have the CLI logic in place to ignore findings that are from old rule versions
@@ -74,22 +87,7 @@ let fetch_scan_config ~token ~sca ~dry_run ~full_scan repository =
   Logs.debug (fun m -> m "finished downloading from %s" (Uri.to_string url));
   match content with
   | Error _ as e -> e
-  | Ok content -> (
-      try
-        match Yojson.Basic.from_string content with
-        | `Assoc e -> (
-            match List.assoc "rule_config" e with
-            | `String e -> Ok e
-            | _else ->
-                Error ("Couldn't retrieve config: no rule_config in " ^ content)
-            )
-        | _else ->
-            Error ("Couldn't retrieve config: not an json object: " ^ content)
-      with
-      | e ->
-          Error
-            ("Failed to decode config: " ^ Printexc.to_string e ^ ": " ^ content)
-      )
+  | Ok content -> extract_rule_config content
 
 let report_failure ~dry_run ~token ~scan_id exit_code =
   if dry_run then (
