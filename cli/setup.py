7d4dc47067525980d649549aa2088db37ca85861
aryx <aryx@users.noreply.github.com>
2023-06-28 15:50:11 +0000

    chore: Bump version to 1.30.0

--- cli/setup.py
+++ cli/setup.py
@@ -113,7 +113,7 @@ extras_require = {"experiments": ["jsonnet~=0.18"]}
 
 setuptools.setup(
     name="semgrep",
-    version="1.29.0",
+    version="1.30.0",
     author="Return To Corporation",
     author_email="support@r2c.dev",
     description="Lightweight static analysis for many languages. Find bug variants with patterns that look like source code.",
