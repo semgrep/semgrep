2580b4eee36168662573c2072e66e62c32bbb3a6
Tom Petr <tom@r2c.dev>
2023-07-04 11:03:56 -0400

    remove arm64 aborts

--- cli/src/semgrep/cli.py
+++ cli/src/semgrep/cli.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-import platform
-import sys
 from typing import Dict
 
 import click
@@ -14,7 +12,6 @@ from semgrep.commands.publish import publish
 from semgrep.commands.scan import scan
 from semgrep.commands.shouldafound import shouldafound
 from semgrep.default_group import DefaultGroup
-from semgrep.error import FATAL_EXIT_CODE
 from semgrep.state import get_state
 from semgrep.util import git_check_output
 from semgrep.verbose_logging import getLogger
@@ -47,16 +44,6 @@ def maybe_set_git_safe_directories() -> None:
         )
 
 
-def abort_if_linux_arm64() -> None:
-    """
-    Exit with FATAL_EXIT_CODE if the user is running on Linux ARM64.
-    Print helpful error message.
-    """
-    if platform.machine() in {"arm64", "aarch64"} and platform.system() == "Linux":
-        logger.error("Semgrep does not support Linux ARM64")
-        sys.exit(FATAL_EXIT_CODE)
-
-
 @click.group(cls=DefaultGroup, default_command="scan", name="semgrep")
 @click.help_option("--help", "-h")
 @click.pass_context
@@ -70,8 +57,6 @@ def cli(ctx: click.Context) -> None:
     """
     state = get_state()
     state.terminal.init_for_cli()
-
-    # abort_if_linux_arm64()
 
     commands: Dict[str, click.Command] = ctx.command.commands  # type: ignore
 
