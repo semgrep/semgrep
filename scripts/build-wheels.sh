379c2e2cac04f1a2ea2dd3e2148c5cceab1d45a3
Chris Dolan <chris@semgrep.com>
2023-07-06 19:48:57 -0600

    dump more info

--- scripts/build-wheels.sh
+++ scripts/build-wheels.sh
@@ -15,6 +15,8 @@ echo "SETUPTOOLS WHEEL INFO"
 cat /opt/homebrew/lib/python3.11/site-packages/setuptools-67.6.1.dist-info/WHEEL
 echo "WHEEL WHEEL INFO"
 cat /opt/homebrew/lib/python3.11/site-packages/wheel-0.40.0.dist-info/WHEEL
+echo "SYS AND DISTUTILS INFO"
+python3 -c "import sys, setuptools._distutils; print(sys.version_info); print(setuptools._distutils.util.get_platform())"
 cd cli && python3 setup.py sdist bdist_wheel
 # Zipping for a stable name to upload as an artifact
 zip -r dist.zip dist
