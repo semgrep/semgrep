4ee1546f110f3fda6df2ebec7b8898e7f651ecac
spencerdrak <spencerdrak@users.noreply.github.com>
2023-07-07 17:32:20 +0000

    chore: Bump version to 1.31.1

--- cli/setup.py
+++ cli/setup.py
@@ -113,7 +113,7 @@ extras_require = {"experiments": ["jsonnet~=0.18"]}
 
 setuptools.setup(
     name="semgrep",
-    version="1.31.0",
+    version="1.31.1",
     author="Return To Corporation",
     author_email="support@r2c.dev",
     description="Lightweight static analysis for many languages. Find bug variants with patterns that look like source code.",
