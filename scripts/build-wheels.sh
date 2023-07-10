f1b90aaf2fbd2be0f9db4565233055c095ee8f69
Spencer Drakontaidis <spencer@semgrep.com>
2023-07-10 14:01:21 -0600

    test: fix path

--- scripts/build-wheels.sh
+++ scripts/build-wheels.sh
@@ -21,7 +21,7 @@ pip3 list
 echo "SYSTEM VERSION PLIST INFO"
 cat /System/Library/CoreServices/SystemVersion.plist
 echo "SYS AND DISTUTILS INFO"
-python3 debug.py
+python3 scripts/debug.py
 cd cli && python3 setup.py sdist bdist_wheel
 # Zipping for a stable name to upload as an artifact
 zip -r dist.zip dist
