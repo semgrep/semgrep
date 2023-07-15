8118c0cc33b63a1dfbae5c1ead506fd0adb91731
Chris Dolan <chris@semgrep.com>
2023-07-14 22:11:55 -0600

    could this be it???

--- scripts/build-wheels.sh
+++ scripts/build-wheels.sh
@@ -9,6 +9,6 @@
 
 set -e
 pip3 install setuptools==67.6.1 wheel==0.40.0
-cd cli && python3 setup.py sdist bdist_wheel
+cd cli && python3 setup.py sdist bdist_wheel "$@"
 # Zipping for a stable name to upload as an artifact
 zip -r dist.zip dist
