450235a163c1295acf1195f70f1a0aa1ebb7c23a
Sophia Roshal <sophia.roshal@gmail.com>
2023-07-17 15:55:35 -0700

    adding one simple test to demonstrate test infrastructure works

--- libs/ojsonnet/Unit_jsonnet.ml
+++ libs/ojsonnet/Unit_jsonnet.ml
@@ -1,7 +1,5 @@
 open Common
 open File.Operators
-
-(*open Testutil*)
 module Y = Yojson.Basic
 
 let dir = Fpath.v "tests/jsonnet/eval"
@@ -16,9 +14,6 @@ let related_file_of_target ~ext ~file =
     in
     Error msg
 
-(* CURRENTLY VERY BAD TESTING, need to be able to print out output
-   *  rather than expected true/false, leaving this as is for now to just set up
-   * tests to run, then will change*)
 let test_maker () =
   Common2.glob (spf "%s/*%s" !!dir "jsonnet")
   |> File.Path.of_strings
@@ -36,12 +31,14 @@ let test_maker () =
 
              let ast = Parse_jsonnet.parse_program file in
              let core = Desugar_jsonnet.desugar_program file ast in
-             let value_ =
-               Eval_jsonnet.eval_program core Core_jsonnet.empty_env
+             let value_ = Eval_jsonnet.eval_program core in
+             let json =
+               JSON.to_yojson (Manifest_jsonnet.manifest_value value_)
              in
-             let json = JSON.to_yojson (Eval_jsonnet.manifest_value value_) in
-             let fmt = format_of_string "expected %s but got %s" in
-             let result = Printf.sprintf fmt (Y.show correct) (Y.show json) in
-             Alcotest.(check bool) result (Y.equal json correct) true ))
+             let fmt = format_of_string "expected %s \n but got %s" in
+             let result =
+               Printf.sprintf fmt (Y.to_string correct) (Y.to_string json)
+             in
+             Alcotest.(check bool) result true (Y.equal json correct) ))
 
 let tests () = test_maker ()
