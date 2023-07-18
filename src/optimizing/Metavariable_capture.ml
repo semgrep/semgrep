d1c8a6862f6b366761876696f72f7c1abe268ecb
Yoann Padioleau <pad@r2c.dev>
2023-07-18 13:13:53 +0200

    cleanup: remove AST_generic.s_backrefs (#8302)
    Followup to #8300 to remove cruft related to
    the stmts caching
    
    test plan:
    make core
    
    
    PR checklist:
    
    - [x] Purpose of the code is [evident to future
    readers](https://semgrep.dev/docs/contributing/contributing-code/#explaining-code)
    - [x] Tests included or PR comment includes a reproducible test plan
    - [x] Documentation is up-to-date
    - [x] A changelog entry was [added to
    changelog.d](https://semgrep.dev/docs/contributing/contributing-code/#adding-a-changelog-entry)
    for any user-facing change
    - [x] Change has no security implications (otherwise, ping security
    team)
    
    If you're unsure about any of this, please see:
    
    - [Contribution
    guidelines](https://semgrep.dev/docs/contributing/contributing-code)!
    - [One of the more specific guides located
    here](https://semgrep.dev/docs/contributing/contributing/)

--- src/optimizing/Metavariable_capture.ml
+++ src/optimizing/Metavariable_capture.ml
@@ -43,33 +43,3 @@ let add_capture k v env =
   let full_env = kv :: env.full_env in
   let min_env = kv :: env.min_env in
   { env with full_env; min_env }
-
-(*
-     To be called as early as possible after passing a backreference
-     which may no longer be needed when descending down the pattern.
-     For now, we call this only when reaching a new stmt pattern node.
-
-     For simplicity, we assume any member of 'min_env' may no longer be
-     needed. It may be more efficient to accumulate the backreferences
-     that were encountered since the last stmt and only consider removing
-     their bindings, rather than considering all the bindings in min_env.
-
-     If we don't call this, the cache keys will be overspecified, reducing
-     or preventing reuse.
-  *)
-let update_min_env env (stmt_pat : AST_generic.stmt) =
-  if debug then printf "update_min_env\n";
-  let backrefs =
-    match stmt_pat.s_backrefs with
-    | None -> assert false (* missing initialization *)
-    | Some x -> x
-  in
-  let min_env =
-    List.filter
-      (fun (k, _v) ->
-        let keep = Set_.mem k backrefs in
-        if debug then printf "keep %s in min env: %B\n" k keep;
-        keep)
-      env.min_env
-  in
-  { env with min_env; last_stmt_backrefs = backrefs }
