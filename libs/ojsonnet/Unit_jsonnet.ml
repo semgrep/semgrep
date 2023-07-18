ada175e3faab48a89e562f4298bc18994dcad740
Sophia Roshal <SophiaSR@users.noreply.github.com>
2023-07-18 00:23:58 -0700

    Adding testing infrastructure for jsonnet interpreter (#8296)
    This pull request sets up testing infrastructure for the interpreter for
    jsonnet. This pull request just contains 1 simple test that demonstrates
    that the infrastructure works.
    PR checklist:
    
    - [ ] Purpose of the code is [evident to future
    readers](https://semgrep.dev/docs/contributing/contributing-code/#explaining-code)
    - [ ] Tests included or PR comment includes a reproducible test plan
    - [ ] Documentation is up-to-date
    - [ ] A changelog entry was [added to
    changelog.d](https://semgrep.dev/docs/contributing/contributing-code/#adding-a-changelog-entry)
    for any user-facing change
    - [ ] Change has no security implications (otherwise, ping security
    team)
    
    If you're unsure about any of this, please see:
    
    - [Contribution
    guidelines](https://semgrep.dev/docs/contributing/contributing-code)!
    - [One of the more specific guides located
    here](https://semgrep.dev/docs/contributing/contributing/)

---
+++ libs/ojsonnet/Unit_jsonnet.ml
@@ -0,0 +1,44 @@
+open Common
+open File.Operators
+module Y = Yojson.Basic
+
+let dir = Fpath.v "tests/jsonnet/eval"
+
+let related_file_of_target ~ext ~file =
+  let dirname, basename, _e = Common2.dbe_of_filename !!file in
+  let path = Common2.filename_of_dbe (dirname, basename, ext) in
+  if Sys.file_exists path then Ok (Fpath.v path)
+  else
+    let msg =
+      spf "could not find %s file for test '%s' in %s" ext basename dirname
+    in
+    Error msg
+
+let test_maker () =
+  Common2.glob (spf "%s/*%s" !!dir "jsonnet")
+  |> File.Path.of_strings
+  |> Common.map (fun file ->
+         ( Fpath.basename file,
+           fun () ->
+             let comparison_file_path =
+               match related_file_of_target ~ext:"json" ~file with
+               | Ok json_file -> json_file
+               | Error msg -> failwith msg
+             in
+             let correct =
+               Y.from_string (File.read_file comparison_file_path)
+             in
+
+             let ast = Parse_jsonnet.parse_program file in
+             let core = Desugar_jsonnet.desugar_program file ast in
+             let value_ = Eval_jsonnet.eval_program core in
+             let json =
+               JSON.to_yojson (Manifest_jsonnet.manifest_value value_)
+             in
+             let fmt = format_of_string "expected %s \n but got %s" in
+             let result =
+               Printf.sprintf fmt (Y.to_string correct) (Y.to_string json)
+             in
+             Alcotest.(check bool) result true (Y.equal json correct) ))
+
+let tests () = test_maker ()
--- src/tests/Test.ml
+++ src/tests/Test.ml
@@ -69,6 +69,7 @@ let tests () =
       (* TODO Unit_matcher.spatch_unittest ~xxx *)
       (* TODO Unit_matcher_php.unittest; (* sgrep, spatch, refactoring, unparsing *) *)
       Unit_engine.tests ();
+      Unit_jsonnet.tests ();
       Unit_metachecking.tests ();
       Unit_LS.tests;
       Aliengrep.Unit_tests.tests;
