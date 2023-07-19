07431a765901fcaeb14d91aa3601001c094f3663
Sophia Roshal <SophiaSR@users.noreply.github.com>
2023-07-19 10:55:05 -0700

    restructuring jsonnet to carry around an environment rather than using explicit lazy (#8297)
    This is a restructuring of the types in jsonnet. 
    The key points are that now instead of an explicit "lazy" wrapping of
    objects and arrays, we now carry around the environment as well. this is
    needed since we need to be able to make modifications to the environment
    in which object fields are evaluated based on which object they are part
    of. Another part of this restructuring includes delaying the setting of
    self/super until the latest possible point, thus being able to access
    the correct fields when objects are merged.

--- libs/ojsonnet/Manifest_jsonnet.mli
+++ libs/ojsonnet/Manifest_jsonnet.mli
@@ -1,7 +1,1 @@
-exception Error of string * Tok.t
-
-(* TODO: at some point we probably would prefer an AST_generic.program
- * which could track the origin of tokens through import, eval, and
- * manifestation and that we could pass to osemgrep to evaluate the rules.
- *)
 val manifest_value : Value_jsonnet.value_ -> JSON.t
--- src/parsing/Manifest_jsonnet_to_AST_generic.ml
+++ src/parsing/Manifest_jsonnet_to_AST_generic.ml
@@ -12,9 +12,11 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
  * LICENSE for more details.
  *)
-module V = Value_jsonnet
+
 module G = AST_generic
 module A = AST_jsonnet
+module E = Eval_jsonnet
+module V = Value_jsonnet
 
 (*****************************************************************************)
 (* Prelude *)
@@ -26,7 +28,6 @@ module A = AST_jsonnet
 (*****************************************************************************)
 (* Helpers *)
 (*****************************************************************************)
-
 let error tk s = raise (Parsing_error.Other_error (s, tk))
 let fb = Tok.unsafe_fake_bracket
 
@@ -39,22 +40,24 @@ let rec value_to_expr (v : V.value_) : G.expr =
   | V.Primitive x ->
       let literal =
         match x with
-        | V.Null tk -> G.Null tk
-        | V.Bool (b, tk) -> G.Bool (b, tk)
-        | V.Double (f, tk) -> G.Float (Some f, tk)
-        | V.Str (s, tk) -> G.String (fb (s, tk))
+        | Null tk -> G.Null tk
+        | Bool (b, tk) -> G.Bool (b, tk)
+        | Double (f, tk) -> G.Float (Some f, tk)
+        | Str (s, tk) -> G.String (fb (s, tk))
       in
       G.L literal |> G.e
-  | V.Function { f_tok = tk; _ } -> error tk "Function value"
-  | V.Array (l, arr, r) ->
+  | Lambda { f_tok = tk; _ } -> error tk "Lambda value"
+  | Array (l, arr, r) ->
       let xs =
         arr |> Array.to_list
-        |> Common.map (fun lzv ->
-               let v = Lazy.force lzv.V.v in
-               value_to_expr v)
+        |> Common.map (fun (entry : V.lazy_value) ->
+               value_to_expr
+                 (match entry.value with
+                 | Val v -> v
+                 | Unevaluated e -> E.eval_program_with_env entry.env e))
       in
       G.Container (G.Array, (l, xs, r)) |> G.e
-  | V.Object (l, (_assertsTODO, fields), r) ->
+  | Object (l, (_assertsTODO, fields), r) ->
       (* TODO: evaluate asserts *)
       let xs =
         fields
@@ -63,7 +66,12 @@ let rec value_to_expr (v : V.value_) : G.expr =
                | A.Hidden -> None
                | A.Visible
                | A.ForcedVisible ->
-                   let v = Lazy.force fld_value.v in
+                   (*let v = Lazy.force fld_value.v *)
+                   let v =
+                     match fld_value.value with
+                     | Val v -> v
+                     | Unevaluated e -> E.eval_program_with_env fld_value.env e
+                   in
                    let e = value_to_expr v in
                    let k = G.L (G.String (fb fld_name)) |> G.e in
                    Some (G.keyval k (snd fld_name) e))
