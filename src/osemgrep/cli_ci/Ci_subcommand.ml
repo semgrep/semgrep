semgrep/semgrep

commit f595062048e301597190959a950a120ce5be90a9

osemgrep ci: add data to be sent to completed endpoint (#8213)
the data is not yet the same as the python implementation (some fields
are empty)

test plan: make core

PR checklist:

- [x] Purpose of the code is [evident to future
readers](https://semgrep.dev/docs/contributing/contributing-code/#explaining-code)
- [x] Tests included or PR comment includes a reproducible test plan
- [x] Documentation is up-to-date
- [x] A changelog entry was [added to
changelog.d](https://semgrep.dev/docs/contributing/contributing-code/#adding-a-changelog-entry)
for any user-facing change
- [x] Change has no security implications (otherwise, ping security
team)

If you're unsure about any of this, please see:

- [Contribution
guidelines](https://semgrep.dev/docs/contributing/contributing-code)!
- [One of the more specific guides located
here](https://semgrep.dev/docs/contributing/contributing/)

src/osemgrep/cli_ci/Ci_subcommand.ml
@@ -209,8 +209,9 @@ let finding_of_cli_match _commit_date index (m : Out.cli_match) : Out.finding =
   r
 
 (* from scans.py *)
-let prepare_for_report ~blocking_findings:_ findings _errors rules ~targets
-    ~ignored_targets:_ ~commit_date ~engine_requested:_ =
+let prepare_for_report ~blocking_findings findings errors rules ~targets
+    ~(ignored_targets : Out.cli_skipped_target list option) ~commit_date
+    ~engine_requested =
   let rule_ids =
     Common.map (fun r -> Rule.ID.to_string (fst r.Rule.id)) rules
   in
@@ -276,37 +277,56 @@ let prepare_for_report ~blocking_findings:_ findings _errors rules ~targets
   then
     Logs.app (fun m -> m "Some experimental rules were run during execution.");
 
-  (* (*ignored_ext_freqs = Counter(
-         [os.path.splitext(path)[1] for path in ignored_targets]
-       )
-       ignored_ext_freqs.pop("", None)  # don't count files with no extension
-       *)
-       (* dependency_counts = {k: len(v) for k, v in lockfile_dependencies.items()} *)
+  let ignored_ext_freqs =
+    Option.value ~default:[] ignored_targets
+    |> Common.group_by (fun (skipped_target : Out.cli_skipped_target) ->
+           Fpath.get_ext (Fpath.v skipped_target.Out.path))
+    |> List.filter (fun (ext, _) -> not (String.equal ext ""))
+    (* don't count files with no extension *)
+    |> Common.map (fun (ext, xs) -> (ext, JSON.Int (List.length xs)))
+  in
 
-       let complete = {
-         "exit_code" = if blocking_finding then 1 else 0 ;
-         "dependency_parser_errors": List [] ; (* [e.to_json() for e in dependency_parser_errors], *)
-         "stats": {
-           "findings": List.length new_matches ;
-           "errors": [error.to_dict() for error in errors];
-           "total_time": total_time;
-           "unsupported_exts": dict(ignored_ext_freqs);
-           "lockfile_scan_info": { } (* dependency_counts *) ;
-           "parse_rate": {
-         lang: {
-           "targets_parsed": data.num_targets - data.targets_with_errors,
-                             "num_targets": data.num_targets,
-                             "bytes_parsed": data.num_bytes - data.error_bytes,
-                             "num_bytes": data.num_bytes,
-                         }
-                         for (lang, data) in parse_rate.get_errors_by_lang().items()
-                     },
-                     "engine_requested": engine_requested.name,
-                 },
-     }
-       in
+  (* dependency_counts = {k: len(v) for k, v in lockfile_dependencies.items()} *)
 
-     (*
+  (* TODO: add this data structure to the semgrep_output_v1.atd spec
+     POST to /api/agent/scans/<scan_id>/complete *)
+  let complete =
+    let errors =
+      List.map
+        (fun e ->
+          JSON.json_of_string (Semgrep_output_v1_j.string_of_cli_error e))
+        errors
+    in
+    JSON.Object
+      [
+        ("exit_code", if blocking_findings then JSON.Int 1 else JSON.Int 0);
+        ("dependency_parser_errors", JSON.Array []);
+        (* [e.to_json() for e in dependency_parser_errors], *)
+        ( "stats",
+          JSON.Object
+            [
+              ("findings", JSON.Int (List.length new_matches));
+              ("errors", JSON.Array errors);
+              ("total_time", JSON.Float 0.0);
+              (* total_time *)
+              ("unsupported_exts", JSON.Object ignored_ext_freqs);
+              ("lockfile_scan_info", JSON.Object []);
+              (* dependency_counts *)
+              ("parse_rate", JSON.Object [])
+              (*
+  lang: {
+    "targets_parsed": data.num_targets - data.targets_with_errors,
+                      "num_targets": data.num_targets,
+                      "bytes_parsed": data.num_bytes - data.error_bytes,
+                      "num_bytes": data.num_bytes,
+  }
+      for (lang, data) in parse_rate.get_errors_by_lang().items() *);
+            ] );
+        ( "engine_requested",
+          JSON.String
+            (Semgrep_output_v1_j.string_of_engine_kind engine_requested) );
+        ("dependencies", JSON.Object [])
+        (*
              if self._dependency_query:
                  lockfile_dependencies_json = {}
                  for path, dependencies in lockfile_dependencies.items():
@@ -314,9 +334,10 @@ let prepare_for_report ~blocking_findings:_ findings _errors rules ~targets
                          dependency.to_json() for dependency in dependencies
                      ]
                  complete["dependencies"] = lockfile_dependencies_json
-     *)
-  *)
-  (findings_and_ignores, JSON.Null)
+     *);
+      ]
+  in
+  (findings_and_ignores, complete)
 
 (*****************************************************************************)
 (* Main logic *)

commit fccc3327dc06e8467a213c9a930ff1464dea5da2

fix CI, semgrep error
This fixes the error introduced in
https://github.com/returntocorp/semgrep/pull/8213
but that was hard to diagnose because we weirdly can't see
precommit errors generated by osemgrep

test plan:
make check_for_emacs

src/osemgrep/cli_ci/Ci_subcommand.ml
@@ -292,7 +292,7 @@ let prepare_for_report ~blocking_findings findings errors rules ~targets
      POST to /api/agent/scans/<scan_id>/complete *)
   let complete =
     let errors =
-      List.map
+      Common.map
         (fun e ->
           JSON.json_of_string (Semgrep_output_v1_j.string_of_cli_error e))
         errors