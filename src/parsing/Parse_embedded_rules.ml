1078bb51cd5a6c1bf696f58ac7d62795a91ad355
pad <pad@r2c.dev>
2023-08-02 15:37:04 +0200

    small cleanup and add TODO comments

--- src/parsing/Parse_embedded_rules.ml
+++ src/parsing/Parse_embedded_rules.ml
@@ -1,22 +1,57 @@
-let start_string = "embedded-semgrep:"
-let end_string = "embedded-semgrep-end"
+(* Sophia Roshal
+ *
+ * Copyright (C) 2023 Semgrep Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation, with the
+ * special exception on linking described in file LICENSE.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
+ * LICENSE for more details.
+ *)
+
+(*****************************************************************************)
+(* Prelude *)
+(*****************************************************************************)
+(* Instead of reading rules from a semgrep.yml file, the idea of this module
+ * is to let users "embed" semgrep rules inside their code, with special
+ * markers (see below), which can be automatically extracted
+ * from semgrep itself.
+ *)
+
+(*****************************************************************************)
+(* Constants *)
+(*****************************************************************************)
+
+let start_string = "<semgrep>"
+let end_string = "</semgrep>"
+
+(*****************************************************************************)
+(* Helpers *)
+(*****************************************************************************)
 
 let find s1 s2 loc =
   let re = Str.regexp_string s2 in
   try Str.search_forward re s1 loc with
   | Not_found -> -1
 
+(*****************************************************************************)
+(* Entry points *)
+(*****************************************************************************)
+
 let has_embedded_rules fpath =
-  let f = Fpath.to_string fpath in
-  let file_contents = Common.read_file f in
+  let file_contents = File.read_file fpath in
   find file_contents start_string 0 > -1
 
 let parse_embedded_rules fpath =
-  let f = Fpath.to_string fpath in
-  let file_contents = Common.read_file f in
+  let file_contents = File.read_file fpath in
+
   let rec parse_embedded_rules_helper (acc_rule, acc_error) prev_loc str =
     let next_loc = find str start_string prev_loc in
-    if next_loc == -1 then (acc_rule, acc_error)
+    if next_loc = -1 then (acc_rule, acc_error)
     else
       let end_loc = find str end_string (next_loc + 1) in
       let rule_string =
@@ -24,17 +59,22 @@ let parse_embedded_rules fpath =
           (next_loc + String.length start_string)
           (end_loc - (next_loc + String.length start_string))
       in
+      (* TODO: support also .json and especially .jsonnet rules! *)
       let temp_file = Filename.open_temp_file "temp_rule" ".yaml" in
       Out_channel.output_string (snd temp_file) rule_string;
       Out_channel.close (snd temp_file);
       let path = Fpath.of_string (fst temp_file) in
       match path with
       | Ok p ->
+          (* TODO? maybe we want to pass the parse_rule function as a parameter
+           * so we can pass Parse_rule.parse variants such as
+           * Rule_fetching.load_rules_from_source which for example handle
+           * rules in jsonnet
+           *)
           let rules, errors = Parse_rule.parse_and_filter_invalid_rules p in
           parse_embedded_rules_helper
             (rules @ acc_rule, errors @ acc_error)
             end_loc str
       | Error _ -> failwith "not a correct file path"
   in
-
   parse_embedded_rules_helper ([], []) 0 file_contents
