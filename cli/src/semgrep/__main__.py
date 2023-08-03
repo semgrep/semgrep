db27aa7a39cbe71460e68f6386ce7e627a65c642
Zach Zeleznick <zz@semgrep.com>
2023-08-02 20:27:56 -0700

    address comment

--- cli/src/semgrep/__main__.py
+++ cli/src/semgrep/__main__.py
@@ -3,7 +3,10 @@ import sys
 
 from semgrep.cli import cli
 
-
+# NOTE: This is the entrypoint for the `pysemgrep` command.
+# To match the program usage help between pysemgrep (legacy)
+# and osemgrep (new) – and to hide complexity for our users -
+# here we specify `semgrep` as the program name for pysemgrep.
 def main() -> int:
     cli(prog_name="semgrep")
     return 0
