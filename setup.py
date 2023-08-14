52543b77abc9cb2ca5122aa2e71593318a368e19
emjin <emjin@users.noreply.github.com>
2023-08-14 22:40:09 +0000

    chore: Bump version to 1.36.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="semgrep_pre_commit_package",
-    version="1.35.0",
-    install_requires=["semgrep==1.35.0"],
+    version="1.36.0",
+    install_requires=["semgrep==1.36.0"],
     packages=[],
 )
