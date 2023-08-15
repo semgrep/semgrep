f44bb9b4e82fc7638b3a55398d420931dd5065e4
Jonas Bergler <jonas@semgrep.com>
2023-08-15 15:10:01 +1200

    add a test

--- cli/tests/e2e/test_config_resolver.py
+++ cli/tests/e2e/test_config_resolver.py
@@ -6,6 +6,7 @@ from tests.semgrep_runner import SemgrepRunner
 from semgrep.cli import cli
 from semgrep.config_resolver import ConfigFile
 from semgrep.config_resolver import ConfigLoader
+from semgrep.error import SemgrepError
 
 
 @pytest.mark.quick
@@ -36,3 +37,37 @@ def test_new_feature_registry_config(monkeypatch, snapshot, mocker, tmp_path):
     )
     result = runner.invoke(cli, ["scan", "--config", "p/ci"])
     snapshot.assert_match(result.output, "output.txt")
+
+
+@pytest.mark.quick
+def test_fallback_config_works(monkeypatch, snapshot, mocker, tmp_path):
+    config_file = ConfigFile(
+        None,
+        "rules: []",
+        "https://semgrep.dev/p/ci",
+    )
+    patched_download = mocker.patch.object(
+        ConfigLoader,
+        "_download_config_from_url",
+        side_effect=[
+            SemgrepError(
+                f"Failed to download configuration. HTTP 500 when fetching URL"
+            ),
+            config_file,
+        ],
+    )
+
+    runner = SemgrepRunner(
+        env={
+            "SEMGREP_SETTINGS_FILE": str(tmp_path / ".settings.yaml"),
+            "SEMGREP_APP_TOKEN": "",
+        }
+    )
+    result = runner.invoke(cli, ["scan", "--debug", "--config", "supply-chain"])
+
+    assert "https://semgrep.dev/api" in patched_download.call_args_list[0].args[0]
+    assert (
+        "https://fail-open.prod.semgrep.dev/"
+        in patched_download.call_args_list[1].args[0]
+    )
+    assert "loaded 1 configs" in result.stdout
