semgrep/semgrep

commit c1ca429c1836d59e721665261957de7d4f3d43ad

Optimize deep statement matching (#852)
* Optimize deep statement matching

This should close #827 and #664

The code to handle foo(); ...; bar(); was very naive
and was doing lots of useless work. This fixes that.

Test plan:
time pipenv run semgrep -f ~/semgrep/tests/PERF/ajin.yaml ~/semgrep/tests/PERF/three.js
3.2s

(was 3min before)

+ /home/pad/github/semgrep/semgrep-core/_build/default/bin/Main.exe -profile -lang py -f tests/PERF/ellipsis-python.sgrep tests/PERF/my_first_calculator.py
---------------------
profiling result
---------------------
Main total                               :      1.670 sec          1 count
Parse_python.parse                       :      0.918 sec          1 count
Parse_python.tokens                      :      0.525 sec          2 count
Semgrep.check                            :      0.458 sec          1 count
Parser_python.main                       :      0.277 sec          1 count
Semgrep.match_sts_sts                    :      0.186 sec      41627 count

(was 85sec before)

+ /home/pad/github/semgrep/semgrep-core/_build/default/bin/Main.exe -profile -lang js -f tests/PERF/ellipsis-js.sgrep tests/PERF/three.js
---------------------
profiling result
---------------------
Main total                               :      2.151 sec          1 count
Parse_js.parse                           :      1.236 sec          1 count
Parse_js.tokens                          :      0.398 sec          2 count
Semgrep.check                            :      0.389 sec          1 count
Semgrep.match_sts_sts                    :      0.239 sec      16824 count
Parser_js.module_item                    :      0.192 sec        609 count

(was a lot more before)

* Adding -debug -profile to semgrep-core and SEMGREP_CORE_DEBUG/PROFILE env var

Those options are useful to debug or profile semgrep-core.
Using the environment variable allows us to pass options to semgrep-core

semgrep-core/matching/Generic_vs_generic.ml
@@ -1259,13 +1259,74 @@ and m_other_attribute_operator = m_other_xxx
 
 (*s: function [[Generic_vs_generic.m_stmts_deep]] *)
 and m_stmts_deep (xsa: A.stmt list) (xsb: A.stmt list) =
-  if !Flag.go_deeper_stmt && (has_ellipsis_stmts xsa)
-  then
-    m_list__m_stmt xsa xsb >!> (fun () ->
-      let xsb' = SubAST_generic.flatten_substmts_of_stmts xsb in
-      m_list__m_stmt xsa xsb'
-    )
-  else m_list__m_stmt xsa xsb
+  (* opti: this was the old code:
+   *   if !Flag.go_deeper_stmt && (has_ellipsis_stmts xsa)
+   *   then
+   *   m_list__m_stmt xsa xsb >!> (fun () ->
+   *     let xsb' = SubAST_generic.flatten_substmts_of_stmts xsb in
+   *     m_list__m_stmt xsa xsb'
+   *   )
+   *   else m_list__m_stmt xsa xsb
+   *
+   * but this was really slow on huge files because with a pattern like
+   * 'foo(); ...; bar();' we would call flatten_substmts_of_stmts
+   * on each sequences in the program, even though foo(); was not
+   * matched first.
+   * Better to first match the first element, and if it matches and
+   * we have a '...' that was not matched on the current sequence,
+   * then we try with flatten_substmts_of_stmts.
+   *
+   * The code below is mostly a copy paste of m_list__m_stmt. We could
+   * factorize, but I prefer to control and limit the number of places
+   * where we call m_stmts_deep. Once we call m_list__m_stmt, we
+   * are in a simpler world where the list of stmts will not grow.
+   *)
+  match xsa, xsb with
+  | [], [] ->
+      return ()
+  (*s: [[Generic_vs_generic.m_list__m_stmt()]] empty list vs list case *)
+  (* less-is-ok:
+   * it's ok to have statements after in the concrete code as long as we
+   * matched all the statements in the pattern (there is an implicit
+   * '...' at the end, in addition to implicit '...' at the beginning
+   * handled by kstmts calling the pattern for each subsequences).
+   * TODO: sgrep_generic though then display the whole sequence as a match
+   * instead of just the relevant part.
+   *)
+  | [], _::_ ->
+      return ()
+  (*e: [[Generic_vs_generic.m_list__m_stmt()]] empty list vs list case *)
+
+  (* dots: '...', can also match no statement *)
+  | [A.ExprStmt (A.Ellipsis _i)], [] ->
+      return ()
+
+  | (A.ExprStmt (A.Ellipsis i))::xsa, xb::xsb ->
+    (* let's first try the without going deep *)
+     (
+      (* can match nothing *)
+      (m_list__m_stmt xsa (xb::xsb)) >||>
+      (* can match more *)
+      (env_add_matched_stmt xb >>= (fun () ->
+       (m_list__m_stmt ((A.ExprStmt (A.Ellipsis i))::xsa) xsb)
+      ))
+     ) >!> (fun () ->
+        if !Flag.go_deeper_stmt
+        then
+          let xsb' = SubAST_generic.flatten_substmts_of_stmts (xb::xsb) in
+          m_list__m_stmt ((A.ExprStmt (A.Ellipsis i))::xsa) xsb'
+        else fail ()
+     )
+
+  (* the general case *)
+  | xa::aas, xb::bbs ->
+      m_stmt xa xb >>= (fun () ->
+        env_add_matched_stmt xb >>= (fun () ->
+        m_stmts_deep aas bbs
+      ))
+  | _::_, _ ->
+      fail ()
+
 (*e: function [[Generic_vs_generic.m_stmts_deep]] *)
 
 and _m_stmts (xsa: A.stmt list) (xsb: A.stmt list) =

semgrep-core/bin/Main.ml
@@ -48,12 +48,20 @@ module J = Json_type
 (* Flags *)
 (*****************************************************************************)
 
+(* You can set those environment variables to enable debugging/profiling
+ * instead of using -debug or -profile. This is useful when you don't call
+ * directly semgrep-core but instead use the semgrep Python wrapper.
+ *)
+let env_debug = "SEMGREP_CORE_DEBUG"
+let env_profile = "SEMGREP_CORE_PROFILE"
+
 (*s: constant [[Main_semgrep_core.verbose]] *)
 let verbose = ref false
 (*e: constant [[Main_semgrep_core.verbose]] *)
 (*s: constant [[Main_semgrep_core.debug]] *)
 let debug = ref false
 (*e: constant [[Main_semgrep_core.debug]] *)
+let profile = ref false
 (*s: constant [[Main_semgrep_core.error_recovery]] *)
 (* try to continue processing files, even if one has a parse error with -e/f.
  * note that -rules_file does its own error recovery.
@@ -420,6 +428,7 @@ let iter_generic_ast_of_files_and_get_matches_and_exn_to_errors f files =
            | [] ->
               failwith (spf "can not extract generic AST from %s" file)
          in
+         if !debug then pr2 (spf "PARSING: %s" file);
          let ast = parse_generic lang file in
 
          (* calling the hook *)
@@ -539,6 +548,16 @@ let sgrep_with_rules rules_file xs =
   in
   print_matches_and_errors files matches errs
 (*e: function [[Main_semgrep_core.sgrep_with_rules]] *)
+
+(* when called from semgrep-python, error messages in semgrep-core or
+ * certain profiling statistics may refer to rule id that are generated
+ * by semgrep-python, making it hard to know what the problem is.
+ * At least we can save this generated rule file to help debugging.
+ *)
+let save_rules_file_in_tmp () =
+  let tmp = Filename.temp_file "semgrep_core_rule-" ".yaml" in
+  pr2 (spf "saving rules file for debugging in: %s" tmp);
+  Common.write_file ~file:tmp (Common.read_file !rules_file)
 
 (*****************************************************************************)
 (* Semgrep -tainting_rules_file *)
@@ -786,7 +805,15 @@ let options () =
   (*s: [[Main_semgrep_core.options]] concatenated flags *)
   Flag_parsing_cpp.cmdline_flags_macrofile () @
   (*x: [[Main_semgrep_core.options]] concatenated flags *)
-  Common2.cmdline_flags_devel () @
+  (* inlining of: Common2.cmdline_flags_devel () @ *)
+  [ "-debugger",         Arg.Set Common.debugger,
+    " option to set if launched inside ocamldebug";
+    "-profile",          Arg.Unit (fun () ->
+        Common.profile := Common.ProfAll;
+        profile := true;
+    ),
+    " output profiling information";
+  ] @
   (*x: [[Main_semgrep_core.options]] concatenated flags *)
   Meta_parse_info.cmdline_flags_precision () @
   (*x: [[Main_semgrep_core.options]] concatenated flags *)
@@ -843,8 +870,25 @@ let main () =
     spf "Usage: %s [options] <pattern> <files_or_dirs> \nOptions:"
       (Filename.basename Sys.argv.(0))
   in
+
+  let argv =
+   (Array.to_list Sys.argv) @
+   (if Sys.getenv_opt "SEMGREP_CORE_DEBUG" <> None then ["-debug"] else[])@
+   (if Sys.getenv_opt "SEMGREP_CORE_PROFILE" <> None then ["-profile"] else[])
+  in
+
   (* does side effect on many global flags *)
-  let args = Common.parse_options (options()) usage_msg Sys.argv in
+  let args = Common.parse_options (options()) usage_msg (Array.of_list argv) in
+
+  if !debug then begin
+    pr2 "Debug mode On";
+    pr2 (spf "Executed as: %s" (Sys.argv|>Array.to_list|> String.concat " "));
+  end;
+  if !profile then begin
+    pr2 "Profile mode On";
+    pr2 "disabling -j when in profiling mode";
+    ncores := 1;
+  end;
 
   (* must be done after Arg.parse, because Common.profile is set by it *)
   Common.profile_code "Main total" (fun () ->
@@ -868,13 +912,11 @@ let main () =
         (match () with
         (*s: [[Main_semgrep_core.main()]] main entry match cases *)
         | _ when !rules_file <> "" ->
-           (try  sgrep_with_rules !rules_file (x::xs)
+           (try
+               sgrep_with_rules !rules_file (x::xs);
+               if !profile then save_rules_file_in_tmp ();
             with exn -> begin
-             if Sys.getenv_opt "SEMGREP_CORE_DEBUG" <> None then begin
-               let tmp = Filename.temp_file "semgrep_core_rule-" ".yaml" in
-               pr2 (spf "saving rule file leading to the error in: %s" tmp);
-               Common.write_file ~file:tmp (Common.read_file !rules_file);
-             end;
+             if !debug then save_rules_file_in_tmp ();
              pr (format_output_exception exn);
              exit 2
              end

semgrep-core/matching/Semgrep_generic.ml
@@ -58,19 +58,27 @@ type ('a, 'b) matcher = 'a -> 'b ->
 (*****************************************************************************)
 
 (*s: function [[Semgrep_generic.match_e_e]] *)
-let match_e_e pattern e =
+let match_e_e2 pattern e =
   let env = Matching_generic.empty_environment () in
   GG.m_expr pattern e env
 (*e: function [[Semgrep_generic.match_e_e]] *)
+let match_e_e ruleid a b =
+ Common.profile_code "Semgrep.match_e_e" (fun () ->
+    Common.profile_code ("rule:" ^ ruleid) (fun () ->
+      match_e_e2 a b))
 
 (*s: function [[Semgrep_generic.match_st_st]] *)
-let match_st_st pattern e =
+let match_st_st2 pattern e =
   let env = Matching_generic.empty_environment () in
   GG.m_stmt pattern e env
 (*e: function [[Semgrep_generic.match_st_st]] *)
+let match_st_st ruleid a b =
+  Common.profile_code "Semgrep.match_st_st" (fun () ->
+    Common.profile_code ("rule:" ^ ruleid) (fun () ->
+      match_st_st2 a b))
 
 (*s: function [[Semgrep_generic.match_sts_sts]] *)
-let match_sts_sts pattern e =
+let match_sts_sts2 pattern e =
   let env = Matching_generic.empty_environment () in
   (* When matching statements, we need not only to report whether
    * there is match, but also the actual statements that were matched.
@@ -106,6 +114,10 @@ let match_sts_sts pattern e =
     | _ -> raise Impossible
   )
 (*e: function [[Semgrep_generic.match_sts_sts]] *)
+let match_sts_sts ruleid a b =
+  Common.profile_code "Semgrep.match_sts_sts" (fun () ->
+    Common.profile_code ("rule:" ^ ruleid) (fun () ->
+      match_sts_sts2 a b))
 
 (*s: function [[Semgrep_generic.match_any_any]] *)
 (* for unit testing *)
@@ -119,11 +131,11 @@ let match_any_any pattern e =
 (*****************************************************************************)
 
 (*s: function [[Semgrep_generic.match_e_e_for_equivalences]] *)
-let match_e_e_for_equivalences a b =
+let match_e_e_for_equivalences ruleid a b =
   Common.save_excursion Flag.equivalence_mode true (fun () ->
   Common.save_excursion Flag.go_deeper_expr false (fun () ->
   Common.save_excursion Flag.go_deeper_stmt false (fun () ->
-    match_e_e a b
+    match_e_e ruleid a b
   )))
 (*e: function [[Semgrep_generic.match_e_e_for_equivalences]] *)
 
@@ -159,7 +171,7 @@ let subst_e (bindings: MV.metavars_binding) e =
 (*****************************************************************************)
 
 (*s: function [[Semgrep_generic.apply_equivalences]] *)
-let apply_equivalences equivs any =
+let apply_equivalences2 equivs any =
   let expr_rules = ref [] in
   let stmt_rules = ref [] in
 
@@ -191,7 +203,8 @@ let apply_equivalences equivs any =
          | [] -> x'
          | (l, r)::xs ->
            (* look for a match on original x, not x' *)
-           let matches_with_env = match_e_e_for_equivalences l x in
+           let matches_with_env = match_e_e_for_equivalences "<equivalence>"
+                    l x in
            (match matches_with_env with
            (* todo: should generate a Disj for each possibilities? *)
            | env::_xs ->
@@ -215,7 +228,9 @@ let apply_equivalences equivs any =
    } in
   visitor.M.vany any
 (*e: function [[Semgrep_generic.apply_equivalences]] *)
-
+let apply_equivalences a b =
+  Common.profile_code "Semgrep.apply_equivalences" (fun () ->
+      apply_equivalences2 a b)
 
 (*****************************************************************************)
 (* Main entry point *)
@@ -264,7 +279,7 @@ let check2 ~hook rules equivs file _lang ast =
        * against an expression recursively
        *)
       !expr_rules |> List.iter (fun (pattern, rule) ->
-         let matches_with_env = match_e_e pattern x in
+         let matches_with_env = match_e_e rule.R.id pattern x in
          if matches_with_env <> []
          then (* Found a match *)
            matches_with_env |> List.iter (fun env ->
@@ -282,7 +297,7 @@ let check2 ~hook rules equivs file _lang ast =
     (* mostly copy paste of expr code but with the _st functions *)
     V.kstmt = (fun (k, _) x ->
       !stmt_rules |> List.iter (fun (pattern, rule) ->
-         let matches_with_env = match_st_st pattern x in
+         let matches_with_env = match_st_st rule.R.id pattern x in
          if matches_with_env <> []
          then (* Found a match *)
            matches_with_env |> List.iter (fun env ->
@@ -302,7 +317,7 @@ let check2 ~hook rules equivs file _lang ast =
        * the heavy stuff (e.g., handling '...' between statements) rarely.
        *)
       !stmts_rules |> List.iter (fun (pattern, rule) ->
-         let matches_with_env = match_sts_sts pattern x in
+         let matches_with_env = match_sts_sts rule.R.id pattern x in
          if matches_with_env <> []
          then (* Found a match *)
            matches_with_env |> List.iter (fun (env, matched_statements) ->
@@ -329,9 +344,7 @@ let check2 ~hook rules equivs file _lang ast =
 (*e: function [[Semgrep_generic.check2]] *)
 
 (*s: function [[Semgrep_generic.check]] *)
-let check ~hook rules equivs file lang =
-  Common.profile_code "Sgrep_generic.check" (
-    fun () -> check2 ~hook rules equivs file lang
-  )
+let check ~hook a b c d e =
+  Common.profile_code "Semgrep.check" (fun () -> check2 ~hook a b c d e)
 (*e: function [[Semgrep_generic.check]] *)
 (*e: semgrep/matching/Semgrep_generic.ml *)

semgrep-core/matching/Semgrep_generic.mli
@@ -20,7 +20,7 @@ type ('a, 'b) matcher = 'a -> 'b ->
 (* used by tainting *)
 
 (*s: signature [[Semgrep_generic.match_e_e]] *)
-val match_e_e: (AST_generic.expr, AST_generic.expr) matcher
+val match_e_e: string -> (AST_generic.expr, AST_generic.expr) matcher
 (*e: signature [[Semgrep_generic.match_e_e]] *)
 
 (*s: signature [[Semgrep_generic.match_any_any]] *)

semgrep-core/matching/SubAST_generic.ml
@@ -32,7 +32,7 @@ module V = Visitor_AST
 
 (*s: function [[SubAST_generic.subexprs_of_expr]] *)
 (* used for deep expression matching *)
-let subexprs_of_expr e =
+let subexprs_of_expr2 e =
   match e with
   | L _
   | Id _ | IdQualified _  | IdSpecial _
@@ -81,6 +81,8 @@ let subexprs_of_expr e =
     -> []
   | DisjExpr _ -> raise Common.Impossible
 (*e: function [[SubAST_generic.subexprs_of_expr]] *)
+let subexprs_of_expr a = Common.profile_code "Sub.subexprs_of_expr" (fun () ->
+      subexprs_of_expr2 a)
 
 (*****************************************************************************)
 (* Statements *)
@@ -208,7 +210,7 @@ let do_visit_with_ref mk_hooks = fun any ->
 (*e: function [[SubAST_generic.do_visit_with_ref]] *)
 
 (*s: function [[SubAST_generic.lambdas_in_expr]] *)
-let lambdas_in_expr e =
+let lambdas_in_expr2 e =
   do_visit_with_ref (fun aref -> { V.default_visitor with
     V.kexpr = (fun (k, _) e ->
       match e with
@@ -217,31 +219,59 @@ let lambdas_in_expr e =
     );
   }) (E e)
 (*e: function [[SubAST_generic.lambdas_in_expr]] *)
+let lambdas_in_expr a = Common.profile_code "Sub.lambdas_in_expr" (fun () ->
+      lambdas_in_expr2 a)
+
+(* opti: using memoization speed things up a bit too
+ * (but again, this is still slow when called many many times).
+ * todo? note that this is not the optimal memoization we can do because
+ * using Hashtbl where the key is a full expression can be slow (hashing
+ * huge expressions still takes some time). It would be better to
+ * return a unique identifier to each expression to remove the hashing cost.
+ *)
+let hmemo = Hashtbl.create 101
+let lambdas_in_expr_memo2 a =
+  Common.memoized hmemo a (fun () -> lambdas_in_expr a)
+let lambdas_in_expr_memo a = Common.profile_code "Sub.lambdas_in_expr_memo"
+ (fun () -> lambdas_in_expr_memo2 a)
 
 (*****************************************************************************)
 (* Really substmts_of_stmts *)
 (*****************************************************************************)
 
 (*s: function [[SubAST_generic.flatten_substmts_of_stmts]] *)
-let flatten_substmts_of_stmts xs =
+let flatten_substmts_of_stmts2 xs =
+  (* opti: using a ref, List.iter, and Common.push instead of a mix of
+   * List.map, List.flatten and @ below speed things up
+   * (but it is still slow when called many many times)
+   *)
+  let res = ref [] in
+
   let rec aux x =
-    let xs = substmts_of_stmt x in
-    (* getting deeply nested lambdas stmts *)
-    let extras =
-       if not !Flag_semgrep.go_really_deeper_stmt
-       then []
-       else
-         let es = subexprs_of_stmt x in
-         let lambdas = es |> List.map lambdas_in_expr |> List.flatten in
-         lambdas |> List.map (fun def -> def.fbody)
-    in
-
-
     (* return the current statement first, and add substmts *)
-    [x] @
-    (extras |> List.map aux |> List.flatten) @
-    (xs |> List.map aux |> List.flatten)
+    Common.push x res;
+
+    (* this can be really slow because lambdas_in_expr() below can be called
+     * a zillion times on big files (see tests/PERF/) if we do the
+     * matching naively in m_stmts_deep.
+     *)
+    (if not !Flag_semgrep.go_really_deeper_stmt
+    then ()
+    else begin
+       let es = subexprs_of_stmt x in
+       (* getting deeply nested lambdas stmts *)
+       let lambdas = es |> List.map lambdas_in_expr_memo |> List.flatten in
+       lambdas |> List.map (fun def -> def.fbody) |> List.iter aux
+    end);
+
+    let xs = substmts_of_stmt x in
+    xs |> List.iter aux;
   in
-  xs |> List.map aux |> List.flatten
+  xs |> List.iter aux;
+  List.rev !res
 (*e: function [[SubAST_generic.flatten_substmts_of_stmts]] *)
+let flatten_substmts_of_stmts a =
+  Common.profile_code "Sub.flatten_substmts_of_stmts" (fun () ->
+      flatten_substmts_of_stmts2 a)
+
 (*e: semgrep/matching/SubAST_generic.ml *)

semgrep-core/tainting/Tainting_generic.ml
@@ -54,7 +54,8 @@ let match_pat_instr pat =
     let pat = Common2.foldl1 (fun x acc -> AST.DisjExpr (x, acc)) xs in
     (fun instr ->
        let eorig = instr.IL.iorig in
-       let matches_with_env = Semgrep_generic.match_e_e pat eorig in
+       let matches_with_env = Semgrep_generic.match_e_e "<tainting>" pat eorig
+          in
        matches_with_env <> []
     )
 (*e: function [[Tainting_generic.match_pat_instr]] *)

semgrep-core/tests/PERF/ajin.yaml
@@ -1,0 +1,52 @@
+rules:
+  - id: cpu_kill
+    patterns:
+      - pattern-either:
+          - pattern: |
+              var $PARSER = new libxmljs.SaxParser();
+              ...
+              $PARSER.parseString(..., <... $REQ.$QUERY ...>, ...);
+          - pattern: |
+              var $PARSER = new libxmljs.SaxParser();
+              ...
+              $PARSER.foo(...);
+          - pattern: |
+              var $PARSER = new libxmljs.SaxParser();
+              ...
+              $PARSER.bar(...);
+          - pattern: |
+              var $PARSER = new libxmljs.SaxParser();
+              ...
+              $PARSER.again(...);
+          - pattern: |
+              var $PARSER = new libxmljs.SaxParser();
+              ...
+              $PARSER.again(...);
+          - pattern: |
+              var $PARSER = new libxmljs.SaxParser();
+              ...
+              $PARSER.blaa(...);
+          - pattern: |
+              var $PARSER = new libxmljs.SaxParser();
+              ...
+              $PARSER.fo(...);
+          - pattern: |
+              var $PARSER = new libxmljs.SaxParser();
+              ...
+              $PARSER.ell(...);
+          - pattern: |
+              var $PARSER = new libxmljs.SaxParser();
+              ...
+              $PARSER.nothing(...);
+          - pattern: |
+              var $PARSER = new libxmljs.SaxParser();
+              ...
+              $PARSER.again(...);
+          - pattern: |
+              var $PARSER = new libxmljs.SaxParser();
+              ...
+              $PARSER.fance(...);
+    message: CPU gone!
+    languages:
+      - javascript
+    severity: ERROR

semgrep-core/tests/PERF/ellipsis-js.sgrep
@@ -1,0 +1,4 @@
+$F = open(...);
+...
+$F.close();
+

commit 8e718bee8af699413da6d04df5b0d7d08034d4ee

matching: ellipsis: Always try going deep (#8440)
Pattern `... bar()` would behave strangely because it would only try to
match deeply if it did not match anything non-deeply. So depending on
how the target code looked, `... bar()` would match or not match inside
e.g. an `if` statement. This was confusing and not a great property for
Semgrep to have.

The regression was introduced in PR #852 as part of a set of
optimizations done back in 0.9.0 (!), but at present reverting this one
thing does not seem to have any negative perf impact.

Follows: c1ca429c183 ("Optimize deep statement matching (#852)")

Closes returntocorp/semgrep-rules#660
Closes PA-2992

test plan:
make test # new tests
Also, compared against develop running p/default on 32 repos from
stress-test-monorepo, and no meaningful slowdown or increase in memory
usage was observed.

src/matching/Generic_vs_generic.ml
@@ -2249,15 +2249,14 @@ and m_stmts_deep ~inside ~less_is_ok (xsa : G.stmt list) (xsb : G.stmt list) =
       m_stmts_deep ~inside ~less_is_ok xsa bbs
   | ( ({ s = G.ExprStmt ({ e = G.Ellipsis _i; _ }, _); _ } :: _ as xsa),
       (_ :: _ as xsb) ) ->
-      (* let's first try without going deep *)
-      m_list__m_stmt xsa xsb >!> fun () ->
-      if_config
-        (fun x -> x.go_deeper_stmt)
-        ~then_:
-          (match SubAST_generic.flatten_substmts_of_stmts xsb with
-          | None -> fail () (* was already flat *)
-          | Some (xsb, _UNUSED_last_stmt) -> m_list__m_stmt xsa xsb)
-        ~else_:(fail ())
+      m_list__m_stmt xsa xsb
+      >||> if_config
+             (fun x -> x.go_deeper_stmt)
+             ~then_:
+               (match SubAST_generic.flatten_substmts_of_stmts xsb with
+               | None -> fail () (* was already flat *)
+               | Some (xsb, _UNUSED_last_stmt) -> m_list__m_stmt xsa xsb)
+             ~else_:(fail ())
   (* dots: metavars: $...BODY *)
   | ( ({ s = G.ExprStmt ({ e = G.N (G.Id ((s, _), _idinfo)); _ }, _); _ } :: _
       as xsa),

tests/rules/ellipsis_stmts_deep.py
@@ -1,0 +1,13 @@
+# https://github.com/returntocorp/semgrep-rules/issues/660
+
+def decorator_factory( foo ):
+    def decorator( function ):
+        # ok:reproducer-660
+        def function_wrapper( *args, **kwargs ):
+            # Do something with 'foo'.
+            return function( *args, **kwargs )
+        return function_wrapper
+    return decorator
+
+@decorator_factory( 'bar' )
+def test( ): ''' Simple reproducer. '''

tests/rules/ellipsis_stmts_deep.yaml
@@ -1,0 +1,29 @@
+rules:
+  - id: reproducer-660
+    patterns:
+      - pattern-inside: |
+          def $F(...):
+              ...
+              def $FF(...):
+                 ...
+              ...
+      - pattern-not-inside: |
+          def $F(...):
+              ...
+              def $FF(...):
+                 ...
+              ...
+              <... $FF ...>
+      - pattern: |
+          def $FF(...):
+            ...
+      - focus-metavariable: $FF
+    message: function `$FF` is defined inside a function but never used
+    languages:
+      - python
+    severity: ERROR
+    metadata:
+      category: maintainability
+      technology:
+        - python
+      license: Commons Clause License Condition v1.0[LGPL-2.1-only]

tests/rules/ellipsis_stmts_deep1.ts
@@ -1,0 +1,13 @@
+
+foo();
+
+obj.met(async () => {
+  something();
+  // ruleid: test
+  x = baz();
+});
+
+bar();
+
+// ok: test
+y = baz();

tests/rules/ellipsis_stmts_deep1.yaml
@@ -1,0 +1,19 @@
+rules:
+  - id: test
+    message: >
+      Test
+    languages:
+      - typescript
+    severity: WARNING
+    patterns:
+      - pattern: baz()
+      - pattern-inside: |
+          foo();
+          ...
+          $X = baz();
+      - pattern-not-inside: |
+          foo();
+          ...
+          bar();
+          ...
+          $X = baz();