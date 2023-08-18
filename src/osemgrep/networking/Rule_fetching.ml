16b6c3b62bd863a4e82387446cbcc7aca521a95a
Sophia Roshal <sophia.roshal@gmail.com>
2023-08-18 14:04:03 -0700

    running through cli works

--- src/osemgrep/networking/Rule_fetching.ml
+++ src/osemgrep/networking/Rule_fetching.ml
@@ -271,7 +271,7 @@ let parse_rule ~registry_caching (file : Fpath.t) :
           ~import_callback:(import_callback ~registry_caching)
           file ast
       in
-      let value_ = Eval_jsonnet.eval_program core in
+      let value_ = Eval_jsonnet_subst.eval_expr core in
       let gen = Manifest_jsonnet_to_AST_generic.manifest_value value_ in
       (* TODO: put to true at some point *)
       Parse_rule.parse_generic_ast ~error_recovery:false file gen
