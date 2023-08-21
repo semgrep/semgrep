ee9fc47eb4fde7eda350bc9b7c9ca12b47afb619
pad <pad@r2c.dev>
2023-08-21 14:01:04 +0200

    misc

--- cli/src/semgrep/main.py
+++ cli/src/semgrep/main.py
@@ -1,9 +1,9 @@
 from semgrep.cli import cli
 
-# NOTE: This is the entrypoint for the `pysemgrep` command.
-# To match the program usage help between pysemgrep (legacy)
-# and osemgrep (new) – and to hide complexity for our users -
-# here we specify `semgrep` as the program name for pysemgrep.
+# This is the entrypoint for the `pysemgrep` command.
 def main() -> int:
+    # To match the program usage help between pysemgrep (legacy)
+    # and osemgrep (new) – and to hide complexity for our users -
+    # here we specify `semgrep` as the program name for pysemgrep.
     cli(prog_name="semgrep")
     return 0
