0f2deaf4815c48bf46cfde739fe2d9bbaafae889
pad <pad@r2c.dev>
2023-08-21 13:52:12 +0200

    comments

--- cli/setup.py
+++ cli/setup.py
@@ -112,7 +112,7 @@ extras_require = {"experiments": ["jsonnet~=0.18"]}
 setuptools.setup(
     name="semgrep",
     version="1.36.0",
-    author="Return To Corporation",
+    author="Semgrep Inc.",
     author_email="support@semgrep.com",
     description="Lightweight static analysis for many languages. Find bug variants with patterns that look like source code.",
     cmdclass=cmdclass,
