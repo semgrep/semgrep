b5fbfbf25cc02a0e3164103f08c8ecd4bb3f7b08
Iago Abal <mail@iagoabal.eu>
2023-08-21 17:10:09 +0200

    naming: java: Fix shadowing of class attributes
    Closes PA-3010
    
    test plan:
    make test # new test

--- src/naming/Naming_AST.ml
+++ src/naming/Naming_AST.ml
@@ -195,12 +195,28 @@ let _add_ident_function_scope _id _resolved _scopes = raise Todo
 let untyped_ent name = { entname = name; enttype = None }
 
 (* see also lookup_scope_opt below taking as a parameter the environment *)
-let rec lookup s xxs =
+let rec lookup ?(class_attr = false) s xxs =
   match xxs with
   | [] -> None
   | xs :: xxs -> (
       match List.assoc_opt s xs with
       | None -> lookup s xxs
+      | Some res when class_attr -> (
+          match res.entname with
+          | EnclosedVar, _ -> Some res
+          (* If we are looking for a class attribute, and we encounter something
+           * else, e.g. a 'Parameter', then we should keep looking. This happens
+           * e.g. in this situation:
+           *
+           *     class Test {
+           *         private int x;
+           *         public void test(int x) {
+           *             foo(this.x);           // <--- this.x is not shadowed by
+           *                                    //      the `x` parameter.
+           *         }
+           *     }
+           *)
+          | __else__ -> lookup s xxs)
       | Some res -> Some res)
 
 (* for Python, PHP *)
@@ -274,7 +290,7 @@ let set_resolved env id_info x =
   if not !(env.in_type) then id_info.id_type := x.enttype
 
 (* accessors *)
-let lookup_scope_opt (s, _) env =
+let lookup_scope_opt ?(class_attr = false) (s, _) env =
   let scopes = env.names in
 
   let actual_scopes =
@@ -294,7 +310,13 @@ let lookup_scope_opt (s, _) env =
         *)
         | _ -> [ xs ] @ xxs @ [ !(scopes.global); !(scopes.imported) ])
   in
-  lookup s actual_scopes
+  (* actual_scopes |> List.iter (fun scope ->
+       logger#flash "SCOPE";
+       scope |> List.iter (fun (s, si) ->
+         logger#flash "- %s : %s" s (show_resolved_name si.entname);
+       )
+     ); *)
+  lookup ~class_attr s actual_scopes
 
 (*****************************************************************************)
 (* Error management *)
@@ -801,11 +823,12 @@ let resolve lang prog =
       method! visit_name venv x =
         match x with
         | Id (id, id_info) -> (
-            match lookup_scope_opt id env with
-            | Some resolved ->
-                (* name resolution *)
-                set_resolved env id_info resolved
-            | _ -> ())
+            if Option.is_none !(id_info.id_resolved) then
+              match lookup_scope_opt id env with
+              | Some resolved ->
+                  (* name resolution *)
+                  set_resolved env id_info resolved
+              | _ -> ())
         | IdQualified
             {
               name_last = id, None;
@@ -908,7 +931,7 @@ let resolve lang prog =
         | DotAccess
             ({ e = IdSpecial ((This | Self), _); _ }, _, FN (Id (id, id_info)))
           -> (
-            match lookup_scope_opt id env with
+            match lookup_scope_opt ~class_attr:true id env with
             (* TODO: this is a v0 for doing naming and typing of fields.
              * we should really use a different lookup_scope_class, that
              * would handle shadowing of fields from locals, etc. but it's
