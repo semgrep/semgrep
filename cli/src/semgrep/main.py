f5b7ba33454cc9ed449bc1755caef627023a6c5f
pad <pad@r2c.dev>
2023-08-21 14:01:55 +0200

    no utf8

--- cli/src/semgrep/main.py
+++ cli/src/semgrep/main.py
@@ -3,7 +3,7 @@ from semgrep.cli import cli
 # This is the entrypoint for the `pysemgrep` command.
 def main() -> int:
     # To match the program usage help between pysemgrep (legacy)
-    # and osemgrep (new) – and to hide complexity for our users -
+    # and osemgrep (new) - and to hide complexity for our users -
     # here we specify `semgrep` as the program name for pysemgrep.
     cli(prog_name="semgrep")
     return 0
