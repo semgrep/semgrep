51c6646a68f04b1851e2f3d78f07e9668fa2035d
Martin Jambon <martin@semgrep.com>
2023-08-29 19:30:36 -0700

    Comment out 'osempass' for the new test with a note

--- cli/tests/e2e/test_version_constraints.py
+++ cli/tests/e2e/test_version_constraints.py
@@ -1,8 +1,9 @@
 import pytest
 from tests.fixtures import RunSemgrep
 
-
-@pytest.mark.osempass
+# TODO: to pass with osemgrep, we need to mask the variable bits of output
+# with "<MASKED>" like it's done by SemgrepResult.as_snapshot in conftest.py.
+# @pytest.mark.osempass
 @pytest.mark.kinda_slow
 def test_version_constraints(run_semgrep_in_tmp: RunSemgrep, snapshot):
     snapshot.assert_match(
