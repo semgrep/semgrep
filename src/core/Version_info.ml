dd1f3873ccb6ce1ca9143f9b83846e87811c2e75
Yoann Padioleau <pad@r2c.dev>
2023-09-15 18:02:03 +0200

    cleanup Version_info.mli (#8723)
    As I was preparing to do a some important file reorg of src/core/,
    I though the interface of Version_info.mli was a bit complicated
    and redundant, so I've cleaned it a bit
    
    test plan:
    make core

--- src/core/Version_info.ml
+++ src/core/Version_info.ml
@@ -6,14 +6,18 @@
 
 type t = Semver.t
 
+(* we don't have access to Semver code so can't add the deriving there
+ * so we need to roll our own boilerplate imitating what
+ * a simple [@@deriving show, ord] would do above.
+ *)
 let pp fmt x = Format.pp_print_string fmt (Semver.to_string x)
 let compare = Semver.compare
-let of_string = Semver.of_string
+let show = Semver.to_string
 let to_string = Semver.to_string
-let string = Version.version
+let of_string = Semver.of_string
 
 let version =
-  match Semver.of_string string with
+  match Semver.of_string Version.version with
   | Some x -> x
   | None ->
       failwith
