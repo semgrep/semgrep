a6e726ec48fbb30bb65b4268f2f6d99898f0d689
Kurt Boberg <98792107+kurt-r2c@users.noreply.github.com>
2023-09-21 13:32:41 -0700

    Entropy V2 hooks for OSS Semgrep (#8794)
    OSS wiring for Pro entropy binding.
    
    If hook is not present, Semgrep will silently pass over the analyzer
    with a no-op and debug log that there was no hook installed.
    
    ---------
    
    Co-authored-by: Andre Kuhlenschmidt <andre@semgrep.com>

--- src/engine/Match_search_mode.ml
+++ src/engine/Match_search_mode.ml
@@ -531,6 +531,8 @@ let children_explanations_of_xpat (env : env) (xpat : Xpattern.t) : ME.t list =
 (* Metavariable condition evaluation *)
 (*****************************************************************************)
 
+let hook_pro_entropy_analysis : (string -> bool) option ref = ref None
+
 let rec filter_ranges (env : env) (xs : (RM.t * MV.bindings list) list)
     (cond : R.metavar_cond) : (RM.t * MV.bindings list) list =
   let file = env.xtarget.file in
@@ -617,6 +619,19 @@ let rec filter_ranges (env : env) (xs : (RM.t * MV.bindings list) list)
               *)
              | Some capture_bindings -> Some (r, capture_bindings @ new_bindings)
              )
+         | R.CondAnalysis (mvar, CondEntropyV2) -> (
+             match !hook_pro_entropy_analysis with
+             (* TODO - nice UX handling of this - tell the user that they ran a rule in OSS w/o Pro hook and so their rule didn't do anything *)
+             | None ->
+                 logger#error
+                   "EntropyV2 rule encountered without loading proprietary \
+                    plugin";
+                 None
+             | Some f ->
+                 let bindings = r.mvars in
+                 Metavariable_analysis.analyze_string_metavar env bindings mvar
+                   f
+                 |> map_bool r)
          | R.CondAnalysis (mvar, CondEntropy) ->
              let bindings = r.mvars in
              Metavariable_analysis.analyze_string_metavar env bindings mvar
