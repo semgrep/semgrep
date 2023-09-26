dd3ced59dc2c8ec7629efe931b685d3ba7f4b610
Nat Mote <nat@semgrep.com>
2023-09-26 09:41:41 -0700

    test: Java boxing equivalences
    I started working on this because I thought that sorting this out was a
    prerequisite to fixing something that I needed fixed for the Pro Engine.
    However, it turned out not to be. I still think that these equivalences
    would be nice to have, but I probably won't work on them in the near
    future. However, since I wrote the tests I figured I might as well
    commit them.
    
    Test plan: Automated tests

---
+++ tests/patterns/java/boxing_equivalences_string.java
@@ -0,0 +1,16 @@
+// These aren't really boxing equivalences since Java has no primitive string
+// type, but they essentially follow the same pattern as the other
+// boxing_equivalences tests, and String deserves special handling because it is
+// used pervasively and is a builtin in many other languages.
+public class boxing_equivalences_string {
+  public static void main(String[] args) {
+    String s1 = "asdf";
+    java.lang.String s2 = "asdf";
+    // MATCH:
+    System.out.println(s1);
+    // TODO:
+    System.out.println(s2);
+    // MATCH:
+    System.out.println("asdf");
+  }
+}
