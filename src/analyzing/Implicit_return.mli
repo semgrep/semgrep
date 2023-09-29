016eb0e254019d6e47a1869d4076159f6dd3889c
Amarin Phaosawasdi <amchiclet@users.noreply.github.com>
2023-09-28 18:17:42 -0700

    add comments to mli file

--- src/analyzing/Implicit_return.mli
+++ src/analyzing/Implicit_return.mli
@@ -1,1 +1,5 @@
+(* Mark all expression nodes that may be executed before exiting a function
+ * as "returning nodes", so later on these expressions can be matched with
+ * return statements.
+ *)
 val mark_implicit_return_nodes : Lang.t -> AST_generic.program -> unit
