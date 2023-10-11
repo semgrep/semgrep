5f11846aaa5b4b4ddae44f8107cbc90bb18668b0
Iago Abal <iagoabal+github@gmail.com>
2023-10-11 10:51:54 +0200

    Revert "feat: add support for implicit return (#8826)" (#8955)
    This reverts commit 51c4586d6e59d6eac464b4aada99ce1828d9c864.
    
    This PR breaks inter-procedural taint analysis, and it seems that the
    fix is more complicated than expected, so we will have to work on a new
    PR that addresses these issues.

--- tests/patterns/julia/implicit_return_inside_funcbody_expr.jl
+++
@@ -1,5 +0,0 @@
-# ERROR:
-f() = "implicit return for function definition without function keyword"
-
-# ERROR:
-f() = return "explicit return for function definition without function keyword should still match"
