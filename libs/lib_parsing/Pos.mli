0e07b2aee167fe4b61a2b3050ce34fb4f8affa4c
Martin Jambon <martin@semgrep.com>
2023-10-16 18:19:15 -0700

    Make the Pos module bigarray accesses safe by returning the first or the last position of the valid range when the input position is out of range.

--- libs/lib_parsing/Pos.mli
+++ libs/lib_parsing/Pos.mli
@@ -34,6 +34,13 @@ val string_of_pos : t -> string
 (* Adjust line x col in a position *)
 (*****************************************************************************)
 
+(*
+   Return (line, column) from a byte position.
+
+   If the byte position is out of range, the functions of this type return
+   the first position or the last position in the file.
+   Empty files admit at least one valid byte position.
+*)
 type bytepos_to_linecol_fun = int -> int * int
 
 (* Can we deprecate those full_charpos_xxx? use
