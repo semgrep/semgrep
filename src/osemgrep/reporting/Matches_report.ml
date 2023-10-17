1f9c252dac82aa8de7cd82320e0bd8f32027e92e
Cooper Pierce <cooper@semgrep.com>
2023-10-17 13:58:55 -0700

    Add TODOs for masking in osemgrep

--- src/osemgrep/reporting/Matches_report.ml
+++ src/osemgrep/reporting/Matches_report.ml
@@ -12,6 +12,8 @@ module Out = Semgrep_output_v1_t
 (*****************************************************************************)
 
 let ellipsis_string = " ... "
+let mask_char = '*'
+let mask_show_pct = 0.2
 let base_indent = String.make 10 ' '
 let findings_indent_depth = String.make 12 ' '
 
@@ -178,6 +180,7 @@ let pp_finding ~max_chars_per_line ~max_lines_per_finding ~color_output
                 else String.length line)
            in
            let a, b, c = cut line start_color end_color in
+           (* TODO(secrets): Apply masking to b *)
            (* The 24m is "no underline", and for python compatibility *)
            let esc =
              if Fmt.style_renderer ppf = `Ansi_tty then Fmt.any "\027[24m"
