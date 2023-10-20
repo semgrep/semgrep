ffd3202f0180b019e12d86edc93a1e3df1cfde6f
pad <pad@r2c.dev>
2023-10-20 15:42:13 +0200

    osemgrep: switch to objects for Git_meta.ml
    The original implementation in meta.py was using objects
    so let's use objects too. Romain's original approach
    was to use regular modules, but we need dynamic dispatch
    here so simpler to just use oo.
    
    test plan:
    make core-test (but there is no tests using Git_meta.ml I think)

--- src/osemgrep/cli_ci/Git_metadata.mli
+++ src/osemgrep/cli_ci/Git_metadata.mli
@@ -13,6 +13,27 @@ type env = {
 
 include Project_metadata.S with type env := env
 
+class git_meta :
+  ?scan_environment:string ->
+  baseline_ref:Digestif.SHA1.t option ->
+  env ->
+object
+  method project_metadata : Semgrep_output_v1_t.project_metadata
+
+  (* to be overriden in the children *)
+  method branch : string option
+  method ci_job_url : Uri.t option
+  method commit_sha : Digestif.SHA1.t option
+  method commit_timestamp : string option
+  method event_name : string
+  method is_full_scan : bool
+  method pr_id : string option
+  method pr_title : string option
+  method repo_name : string
+  method repo_url : Uri.t option
+  method merge_base_ref : Digestif.SHA1.t option
+end
+
 (* used in Github_metadata.ml *)
 val get_event_name : env -> string option
 val get_branch : env -> string option
