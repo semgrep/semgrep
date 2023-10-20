5116982684173723624526d3d7cdfd2ce90c29b4
Martin Jambon <martin@semgrep.com>
2023-10-19 19:37:18 -0700

    osemgrep test runs faster -> 0.35 s is no longer suspicious

--- cli/tests/e2e/test_performance.py
+++ cli/tests/e2e/test_performance.py
@@ -16,10 +16,6 @@ def test_debug_performance(run_semgrep_in_tmp: RunSemgrep):
     run_semgrep_in_tmp("rules/long_message.yaml", target_name="simple.yaml")
     time_without_debug = time.time() - start_without_debug
 
-    assert (
-        time_without_debug > 0.8
-    ), f"runtime was suspiciously low ({time_without_debug:.6f} s), was the rule optimized out?"
-
     start_with_debug = time.time()
     run_semgrep_in_tmp(
         "rules/long_message.yaml",
@@ -27,6 +23,19 @@ def test_debug_performance(run_semgrep_in_tmp: RunSemgrep):
         options=["--debug", "--time"],
     )
     time_with_debug = time.time() - start_with_debug
+
+    # The error message is short on purpose because pytest truncates it.
+    # We want to see the times, and without re-running the thing.
+    assert (
+        # threshold used to be 0.8 but osemgrep is faster than pysemgrep
+        time_without_debug
+        > 0.3
+    ), (
+        f"Runtime was suspiciously low"
+        f" ({time_without_debug:.3f}s,"
+        f" {time_with_debug:.3f}s)."
+        f" Was the rule optimized out?"
+    )
 
     # pad: this used to be 0.1, but got some FPs on unrelated changes like
     # https://github.com/returntocorp/semgrep/runs/6468878138?check_suite_focus=true
