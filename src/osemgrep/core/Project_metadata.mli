fb896edd5ec8370543c1083960d829087e1547c3
Yoann Padioleau <pad@r2c.dev>
2023-10-20 19:29:25 +0200

    osemgrep: fully switch to the objects for meta, remove old code (#9057)
    test plan:
    make core

--- src/osemgrep/core/Project_metadata.mli
+++ src/osemgrep/core/Project_metadata.mli
@@ -5,23 +5,6 @@
  *)
 type t = Semgrep_output_v1_t.project_metadata
 
-(* Each Xxx_metadata.ml can implement this signature *)
-module type S = sig
-  (* usually a record representing a set of environment variables *)
-  type env
-
-  (* Extract the environment variable via Cmdliner.
-   * Why using cmdliner? Why not simply access them with Sys.getenv_opt?
-   * The advantage of cmdliner is that we can document those variables
-   * and the env constant below can be combined in Ci_CLI.ml so those
-   * variables can be part of the 'semgrep ci' man page!!
-   *)
-  val env : env Cmdliner.Term.t
-
-  (* entry point *)
-  val make : env -> t
-end
-
 (* a few helpers *)
 val get_url_from_sstp_url : string option -> Uri.t option
 val get_repo_name_from_repo_url : string option -> string option
