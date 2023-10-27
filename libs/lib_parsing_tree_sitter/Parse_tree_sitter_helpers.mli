9d4b923cfa9297ff6b496ea80b8261fcd48173cd
Yoann Padioleau <pad@r2c.dev>
2023-10-27 09:39:59 +0200

    Revert "feat(rust): proper parsing for macro args (#8928)" (#9101)
    This reverts commit 22f57a485f6b6b0e6b9156860b56e1cdbbd5e3c2.

--- libs/lib_parsing_tree_sitter/Parse_tree_sitter_helpers.mli
+++ libs/lib_parsing_tree_sitter/Parse_tree_sitter_helpers.mli
@@ -8,9 +8,6 @@ type 'a env = {
 (* to fill in conv *)
 val line_col_to_pos : Common.filename -> (int * int, int) Hashtbl.t
 
-(* to fill in conv for a pattern string *)
-val line_col_to_pos_str : string -> (int * int, int) Hashtbl.t
-
 (* Tree_sitter_run tokens to Tok.t converters *)
 val token : 'a env -> Tree_sitter_run.Token.t -> Tok.t
 val str : 'a env -> Tree_sitter_run.Token.t -> string * Tok.t
