semgrep/semgrep

commit 1ff14f72cfc24120026fd4e771bfd85e2f0a4bce

pysemgrep: cleanup error.py part 2 (#8988)
test plan:
make e2e

cli/src/semgrep/error.py
@@ -3,9 +3,7 @@ import inspect
 import sys
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Any
 from typing import cast
-from typing import Dict
 from typing import List
 from typing import Optional
 from typing import Sequence
@@ -68,6 +66,7 @@ class SemgrepError(Exception):
         self.level = level
         super().__init__(*args)
 
+    # to be overriden in children
     def type_(self) -> out.ErrorType:
         return out.ErrorType(out.SemgrepError())
 
@@ -83,9 +82,6 @@ class SemgrepError(Exception):
         """
         return dataclasses.replace(base, message=str(self))
 
-    def to_dict(self) -> Dict[str, Any]:
-        return cast(Dict[str, Any], self.to_CliError().to_json())
-
     def format_for_terminal(self) -> str:
         level_tag = (
             with_color(Colors.red, "[", bgcolor=Colors.red)
@@ -100,7 +96,7 @@ class SemgrepError(Exception):
 
         return f"{level_tag} {self}"
 
-    # TODO: @classmethod?
+    # stored in our metrics payload.errors.errors
     def semgrep_error_type(self) -> str:
         return str(self.type_().to_json())
 
@@ -129,6 +125,9 @@ class SemgrepCoreError(SemgrepError):
         # and have some <json name="..."> annotations to generate the right string
         else:
             return str(type_.to_json())
+
+    def semgrep_error_type(self) -> str:
+        return self._error_type_string()
 
     def type_(self) -> out.ErrorType:
         return self.core.error_type
@@ -170,10 +169,6 @@ class SemgrepCoreError(SemgrepError):
         Return if this error is a match timeout
         """
         return isinstance(self.core.error_type.value, out.Timeout)
-
-    # TODO: remove this SemgrepCoreError prefix
-    def semgrep_error_type(self) -> str:
-        return f"SemgrepCoreError: {self._error_type_string()}"
 
     @property
     def _error_message(self) -> str:
@@ -443,6 +438,7 @@ class UnknownLanguageError(ErrorWithSpan):
 
 
 # cf. https://stackoverflow.com/questions/1796180/how-can-i-get-a-list-of-all-classes-within-current-module-in-python/1796247#1796247
+# This is used only in join_rules.py
 ERROR_MAP = {
     classname: classdef
     for classname, classdef in inspect.getmembers(

cli/src/semgrep/formatter/sarif.py
@@ -366,9 +366,8 @@ class SarifFormatter(BaseFormatter):
 
     @staticmethod
     def _semgrep_error_to_sarif_notification(error: SemgrepError) -> Mapping[str, Any]:
-        # TODO: replace error_to_dict with typed error.to_CliError
-        error_dict = error.to_dict()
-        descriptor = error_dict["type"]
+        cli_error = error.to_CliError()
+        descriptor = cli_error.type_
 
         error_to_sarif_level = {
             out.Error_(): "error",
@@ -376,11 +375,11 @@ class SarifFormatter(BaseFormatter):
         }
         level = error_to_sarif_level[error.level.value]
 
-        message = error_dict.get("message")
+        message = cli_error.message
         if message is None:
-            message = error_dict.get("long_msg")
+            message = cli_error.long_msg
         if message is None:
-            message = error_dict.get("short_msg", "")
+            message = cli_error.short_msg or ""
 
         return {
             "descriptor": {"id": descriptor},

cli/src/semgrep/formatter/text.py
@@ -4,7 +4,6 @@ from itertools import groupby
 from pathlib import Path
 from shutil import get_terminal_size
 from typing import Any
-from typing import cast
 from typing import Dict
 from typing import Iterable
 from typing import Iterator
@@ -415,9 +414,7 @@ def print_time_summary(
     # Count errors
 
     semgrep_core_errors = [
-        cast(SemgrepCoreError, err)
-        for err in error_output
-        if SemgrepError.semgrep_error_type(err) == "SemgrepCoreError"
+        err for err in error_output if isinstance(err, SemgrepCoreError)
     ]
     errors = {
         (err.core.location.path.value, err.core.error_type.kind)

commit 411a2085dd88a90e0456759450fa05e90874f49a

Fix --sarif in the presence of errors
This closes #9091

The regression was introduced in #8988 when
we switched the representation of an error type in
semgrep_output_v1.atd

Mypy didn't spot the error because we didn't use types in
sarif.py to constrain descriptor, and we are using
untyped dictionary all over the place in sarif.py instead
of using proper schema.

test plan:
test file included

make e2e

cli/src/semgrep/error.py
@@ -96,7 +96,8 @@ class SemgrepError(Exception):
         return f"{level_tag} {self}"
 
 
-# used in text output and currently stored in our metrics payload.errors.errors
+# used in text and sarif output, and currently also stored in our metrics
+# payload.errors.errors
 def error_type_string(type_: out.ErrorType) -> str:
     # convert to the same string of out.ParseError for now
     if isinstance(type_.value, out.PartialParsing):

cli/src/semgrep/formatter/sarif.py
@@ -8,6 +8,7 @@ from typing import Sequence
 
 import semgrep.semgrep_interfaces.semgrep_output_v1 as out
 from semgrep import __VERSION__
+from semgrep.error import error_type_string
 from semgrep.error import SemgrepError
 from semgrep.formatter.base import BaseFormatter
 from semgrep.rule import Rule
@@ -367,7 +368,7 @@ class SarifFormatter(BaseFormatter):
     @staticmethod
     def _semgrep_error_to_sarif_notification(error: SemgrepError) -> Mapping[str, Any]:
         cli_error = error.to_CliError()
-        descriptor = cli_error.type_
+        descriptor: str = error_type_string(cli_error.type_)
 
         error_to_sarif_level = {
             out.Error_(): "error",

cli/tests/e2e/snapshots/test_output/test_sarif_output_when_errors/results.sarif
@@ -1,0 +1,32 @@
+{
+  "$schema": "https://docs.oasis-open.org/sarif/sarif/v2.1.0/os/schemas/sarif-schema-2.1.0.json",
+  "runs": [
+    {
+      "invocations": [
+        {
+          "executionSuccessful": true,
+          "toolExecutionNotifications": [
+            {
+              "descriptor": {
+                "id": "SemgrepError"
+              },
+              "level": "error",
+              "message": {
+                "text": "File not found: targets/basic/inexistent.py"
+              }
+            }
+          ]
+        }
+      ],
+      "results": [],
+      "tool": {
+        "driver": {
+          "name": "semgrep",
+          "rules": [],
+          "semanticVersion": "placeholder"
+        }
+      }
+    }
+  ],
+  "version": "2.1.0"
+}

cli/tests/e2e/test_output.py
@@ -382,6 +382,20 @@ def test_sarif_output_with_dataflow_traces(run_semgrep_in_tmp: RunSemgrep, snaps
 
 @pytest.mark.kinda_slow
 @pytest.mark.osemfail
+def test_sarif_output_when_errors(run_semgrep_in_tmp: RunSemgrep, snapshot):
+    snapshot.assert_match(
+        run_semgrep_in_tmp(
+            "rules/eqeq.yaml",
+            target_name="basic/inexistent.py",
+            output_format=OutputFormat.SARIF,
+            assert_exit_code=2,
+        ).stdout,
+        "results.sarif",
+    )
+
+
+@pytest.mark.kinda_slow
+@pytest.mark.osemfail
 def test_json_output_with_dataflow_traces(run_semgrep_in_tmp: RunSemgrep, snapshot):
     snapshot.assert_match(
         run_semgrep_in_tmp(