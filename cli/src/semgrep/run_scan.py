semgrep/semgrep

commit f9b734863f79372d70a088324ce8e69afd62839a

pysemgrep: remove constants.RuleSeverity (#8958)
can just use semgrep_output_v1.atd MatchSeverity type

test plan:
make e2e

cli/src/semgrep/run_scan.py
@@ -39,7 +39,6 @@ from semgrep.config_resolver import get_config
 from semgrep.constants import DEFAULT_DIFF_DEPTH
 from semgrep.constants import DEFAULT_TIMEOUT
 from semgrep.constants import OutputFormat
-from semgrep.constants import RuleSeverity
 from semgrep.core_runner import CoreRunner
 from semgrep.core_runner import get_contributions
 from semgrep.engine import EngineType
@@ -340,7 +339,7 @@ def run_scan(
     List[Rule],
     ProfileManager,
     OutputExtra,
-    Collection[RuleSeverity],
+    Collection[out.MatchSeverity],
     Dict[str, List[FoundDependency]],
     List[DependencyParserError],
     int,
@@ -397,7 +396,7 @@ def run_scan(
         shown_severities = DEFAULT_SHOWN_SEVERITIES
         filtered_rules = all_rules
     else:
-        shown_severities = {RuleSeverity(s) for s in severity}
+        shown_severities = {out.MatchSeverity.from_json(s) for s in severity}
         filtered_rules = [rule for rule in all_rules if rule.severity.value in severity]
     filtered_rules = filter_exclude_rule(filtered_rules, exclude_rule)
 
@@ -492,7 +491,7 @@ def run_scan(
         contributions = out.Contributions([])
 
     experimental_rules, unexperimental_rules = partition(
-        filtered_rules, lambda rule: rule.severity == RuleSeverity.EXPERIMENT
+        filtered_rules, lambda rule: (isinstance(rule.severity.value, out.Experiment))
     )
 
     logger.verbose("Rules:")

cli/src/semgrep/app/scans.py
@@ -25,7 +25,6 @@ import semgrep.semgrep_interfaces.semgrep_output_v1 as out
 from semdep.parsers.util import DependencyParserError
 from semgrep import __VERSION__
 from semgrep.constants import DEFAULT_SEMGREP_APP_CONFIG_URL
-from semgrep.constants import RuleSeverity
 from semgrep.error import SemgrepError
 from semgrep.parsing_data import ParsingData
 from semgrep.project import ProjectConfig
@@ -311,11 +310,11 @@ class ScanHandler:
         # appear in an intuitive order.  this requires reversed ordering here.
         all_matches.reverse()
         sort_order = {  # used only to order rules by severity
-            "EXPERIMENT": 0,
-            "INVENTORY": 1,
-            "INFO": 2,
-            "WARNING": 3,
-            "ERROR": 4,
+            out.Experiment(): 0,
+            out.Inventory(): 1,
+            out.Info(): 2,
+            out.Warning(): 3,
+            out.Error(): 4,
         }
         # NB: sorted guarantees stable sort, so within a given severity level
         # issues remain sorted as before
@@ -351,7 +350,9 @@ class ScanHandler:
 
         findings_and_ignores = ci_scan_results.to_json()
 
-        if any(match.severity == RuleSeverity.EXPERIMENT for match in new_ignored):
+        if any(
+            isinstance(match.severity.value, out.Experiment) for match in new_ignored
+        ):
             logger.info("Some experimental rules were run during execution.")
 
         ignored_ext_freqs = Counter(

cli/src/semgrep/config_resolver.py
@@ -34,7 +34,6 @@ from semgrep.constants import DEFAULT_SEMGREP_CONFIG_NAME
 from semgrep.constants import ID_KEY
 from semgrep.constants import PLEASE_FILE_ISSUE_TEXT
 from semgrep.constants import RULES_KEY
-from semgrep.constants import RuleSeverity
 from semgrep.error import InvalidRuleSchemaError
 from semgrep.error import SemgrepError
 from semgrep.error import UNPARSEABLE_YAML_EXIT_CODE
@@ -67,7 +66,7 @@ DEFAULT_CONFIG = {
             "pattern": "$X == $X",
             "message": "$X == $X is a useless equality check",
             "languages": ["python"],
-            "severity": RuleSeverity.ERROR.value,
+            "severity": out.Error().to_json(),
         },
     ],
 }
@@ -581,7 +580,7 @@ def manual_config(
         "pattern": pattern_tree,
         "message": pattern,
         "languages": [lang],
-        "severity": RuleSeverity.ERROR.value,
+        "severity": out.Error().to_json(),
     }
 
     if replacement:

cli/src/semgrep/constants.py
@@ -1,7 +1,6 @@
 import re
 from enum import auto
 from enum import Enum
-from typing import Type
 
 RULES_KEY = "rules"
 ID_KEY = "id"
@@ -47,25 +46,6 @@ class OutputFormat(Enum):
 
     def is_json(self) -> bool:
         return self in [OutputFormat.JSON, OutputFormat.SARIF]
-
-
-# Ensure consistency with 'severity' in 'rule_schema_v1.yaml'
-# TODO: get rid of, reuse semgrep_output_v1.severity type
-class RuleSeverity(Enum):
-    INFO = "INFO"
-    WARNING = "WARNING"
-    ERROR = "ERROR"
-    INVENTORY = "INVENTORY"
-    EXPERIMENT = "EXPERIMENT"
-
-    @classmethod
-    def _missing_(cls: Type[Enum], value: object) -> Enum:
-        if not isinstance(value, str):
-            raise TypeError(f"invalid rule severity type: {type(value)}")
-        for member in cls:
-            if member.value.lower() == value:
-                return member
-        raise ValueError(f"invalid rule severity value: {value}")
 
 
 class RuleScanSource(Enum):

cli/src/semgrep/core_output.py
@@ -19,7 +19,6 @@ from typing import Tuple
 
 import semgrep.semgrep_interfaces.semgrep_output_v1 as out
 import semgrep.util as util
-from semgrep.constants import RuleSeverity
 from semgrep.error import FATAL_EXIT_CODE
 from semgrep.error import OK_EXIT_CODE
 from semgrep.error import SemgrepCoreError
@@ -189,9 +188,7 @@ def core_matches_to_rule_matches(
             extra=match.extra.to_json(),
             message=message,
             metadata=metadata,
-            severity=RuleSeverity(match.extra.severity)
-            if match.extra.severity
-            else rule.severity,
+            severity=match.extra.severity if match.extra.severity else rule.severity,
             fix=fix,
             fix_regex=fix_regex,
         )

cli/src/semgrep/core_runner.py
@@ -1010,7 +1010,7 @@ Exception raised: `{e}`
         )
         by_severity = collections.defaultdict(list)
         for rule, findings in findings_by_rule.items():
-            by_severity[rule.severity.value.lower()].extend(findings)
+            by_severity[rule.severity.to_json().lower()].extend(findings)
 
         by_sev_strings = [
             f"{len(findings)} {sev}" for sev, findings in by_severity.items()

cli/src/semgrep/formatter/base.py
@@ -7,7 +7,6 @@ from typing import Mapping
 from typing import Sequence
 
 import semgrep.semgrep_interfaces.semgrep_output_v1 as out
-from semgrep.constants import RuleSeverity
 from semgrep.error import SemgrepError
 from semgrep.rule import Rule
 from semgrep.rule_match import RuleMatch
@@ -21,7 +20,7 @@ class BaseFormatter(abc.ABC):
         semgrep_structured_errors: Sequence[SemgrepError],
         cli_output_extra: out.CliOutputExtra,
         extra: Mapping[str, Any],
-        shown_severities: Collection[RuleSeverity],
+        shown_severities: Collection[out.MatchSeverity],
         is_ci_invocation: bool,
     ) -> str:
         filtered_rules = (r for r in rules if r.severity in shown_severities)

cli/src/semgrep/formatter/emacs.py
@@ -19,7 +19,7 @@ class EmacsFormatter(BaseFormatter):
             if rule_match.rule_id != CLI_RULE_ID
             else None
         )
-        match_severity = rule_match.severity.value.lower()
+        match_severity = rule_match.severity.value.to_json().lower()
         severity = match_severity + f"({check_id})" if check_id else match_severity
         return [
             str(rule_match.path),

commit 5310428fe988b2684acc6c1cf4be9089a67e1125

Fix --severity regression
This closes #9062

The regression was introduced in #8958 when switching
from Enum to ATD-defined MatchSeverity in semgrep_output_v1.atd
Not sure why mypy didn't complain about the
  `rule.severity.value in severity`
where we compare a MatchSeverity and a string.

Note that osemgrep --severity was working!

Anyway, this is fixed now.

test plan:
added test in test_severity.py
'make e2e'.

cli/src/semgrep/run_scan.py
@@ -404,7 +404,9 @@ def run_scan(
         filtered_rules = all_rules
     else:
         shown_severities = {out.MatchSeverity.from_json(s) for s in severity}
-        filtered_rules = [rule for rule in all_rules if rule.severity.value in severity]
+        filtered_rules = [
+            rule for rule in all_rules if rule.severity in shown_severities
+        ]
     filtered_rules = filter_exclude_rule(filtered_rules, exclude_rule)
 
     output_handler.handle_semgrep_errors(config_errors)

cli/tests/e2e/test_severity.py
@@ -10,6 +10,7 @@ def test_severity_error(run_semgrep_in_tmp: RunSemgrep, snapshot):
     assert json_str != ""
     assert '"severity": "INFO"' not in json_str
     assert '"severity": "WARNING"' not in json_str
+    snapshot.assert_match(json_str, "results.json")
 
 
 @pytest.mark.kinda_slow