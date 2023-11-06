semgrep/semgrep

commit ca7b648d5475d3d8997facbe5ceda13bfab63b58

feat(js)!: unix.js (#9090)
## What:
This PR implements the Unix syscall dependencies of Semgrep in
Javascript, so that it may be used by the JSCaml code.

## Why:
When transpiling the language server code in OCaml to Javascript, we
must also be able to have the Javascript code invoke the OCaml
dependencies. Namely, this takes the form of the basis library's Unix
dependencies, such as syscalls like `caml_unix_spawn` and friends, which
are normally external C calls.

Since we're aiming to run everything in Javasceript, we can't use those
C calls, however. So we re-implement them to run in JS.

## How:
Re-implemented the calls in JS. Also did some organization.

## Test plan:
`make test` from `js/tests`.

js/shared/core.js
@@ -19,9 +19,80 @@ function ocaml_terminal_size_get() {
 }
 
 //Provides: unix_open
-//Requires: caml_sys_open
+//Requires: caml_sys_open, caml_list_of_js_array, caml_list_to_js_array
 function unix_open(path, flags, perm) {
-  return caml_sys_open(path, flags, perm);
+  // Why is this needed?
+  // There is a Unix.open_flag type, which is different than
+  // that of the Stdlib.open_flag type.
+  // As such, some flags (such as O_CREAT) appear in a different
+  // index of the variant type than the corresponding Open_creat,
+  // for instance.
+  // So before we can call the system open command, we need to
+  // translate the Unix open flags to stdlib open flags.
+  function todo(flag) {
+    console.debug(`TODO: map unix flag ${flag} to stdlib flag`);
+    return [];
+  }
+  function map_unix_flag_to_stdlib_flag(flag) {
+    switch (flag) {
+      // O_RDONLY
+      case 0:
+        return [0];
+      // O_WRONLY
+      case 1:
+        return [1];
+      // O_RDWR
+      case 2:
+        return [0, 1];
+      //O_NONBLOCK
+      case 3:
+        return [8];
+      //O_APPEND
+      case 4:
+        return [2];
+      //O_CREAT
+      case 5:
+        return [3];
+      //O_TRUNC
+      case 6:
+        return [4];
+      //O_EXCL
+      case 7:
+        return [5];
+      //O_NOCTTY
+      case 8:
+        return todo(flag);
+      //O_DSYNC
+      case 9:
+        return todo(flag);
+      //O_SYNC
+      case 10:
+        return todo(flag);
+      //O_RSYNC
+      case 11:
+        return todo(flag);
+      //O_SHARE_DELETE
+      case 12:
+        return todo(flag);
+      //O_CLOEXEC
+      case 13:
+        return todo(flag);
+      //O_KEEPEXEC
+      case 14:
+        return todo(flag);
+    }
+  }
+
+  let parsed_flags = caml_list_to_js_array(flags);
+  let translated_flags_nested = parsed_flags.map((flag) =>
+    map_unix_flag_to_stdlib_flag(flag)
+  );
+  let translated_flags_flattened = translated_flags_nested.reduce((acc, l) =>
+    acc.concat(l)
+  );
+  let new_flags = caml_list_of_js_array(translated_flags_flattened);
+
+  return caml_sys_open(path, new_flags, perm);
 }
 
 //Provides: unix_environment const

js/language_server/Main.ml
@@ -25,80 +25,19 @@
 open Js_of_ocaml
 open Semgrep_js_shared
 
-(* JS specific IO for the RPC server *)
-module Io = RPC_server.MakeLSIO (struct
-  type input = in_channel
-  type output = out_channel
-
-  let read_line ic = input_line ic |> Lwt.return_some
-  let stdin = stdin
-  let stdout = stdout
-  let flush () = flush stdout |> Lwt.return
-  let atomic f oc = f oc
-
-  let write _ str =
-    (* nosem *)
-    print_string str;
-    Lwt.return ()
-
-  let read_exactly ic n =
-    let rec read_exactly acc = function
-      | 0 -> String.of_seq (acc |> List.rev |> List.to_seq)
-      | n -> read_exactly (input_char ic :: acc) (n - 1)
-    in
-    let exact = read_exactly [] n in
-    Lwt.return (Some exact)
-end)
-
 (*****************************************************************************)
 (* Code *)
 (*****************************************************************************)
-let _Promise = Js.Unsafe.global##._Promise
-
-let promise_of_lwt lwt =
-  new%js _Promise
-    (Js.wrap_callback (fun resolve reject ->
-         try%lwt
-           let%lwt res = lwt () in
-           Js.Unsafe.fun_call resolve [| Js.Unsafe.inject res |]
-         with
-         | e ->
-             let msg = Printexc.to_string e in
-             Firebug.console##error (Js.string msg);
-             Js.Unsafe.fun_call reject
-               [| Js.Unsafe.inject (new%js Js.error_constr (Js.string msg)) |]))
-
-(* Stolen from Logs' logs_browser.ml *)
-let ppf, flush =
-  let b = Buffer.create 255 in
-  let flush () =
-    let s = Buffer.contents b in
-    Buffer.clear b;
-    s
-  in
-  (Format.formatter_of_buffer b, flush)
-
-let console_report _src _level ~over k msgf =
-  let k _ =
-    Firebug.console##error (Js.string (flush ()));
-    over ();
-    k ()
-  in
-  msgf @@ fun ?header ?tags fmt ->
-  ignore tags;
-  match header with
-  | None -> Format.kfprintf k ppf ("@[" ^^ fmt ^^ "@]@.")
-  | Some h -> Format.kfprintf k ppf ("[%s] @[" ^^ fmt ^^ "@]@.") h
 
 let _ =
-  RPC_server.io_ref := (module Io);
+  RPC_server.io_ref := (module Semgrep_js_shared.Io);
   Logs.set_level (Some Logs.Debug);
-  Logs.set_reporter { Logs.report = console_report };
+  Logs.set_reporter { Logs.report = Semgrep_js_shared.console_report };
   Http_helpers.client_ref := Some (module Cohttp_lwt_jsoo.Client);
   Js.export_all
     (object%js
        method init = init_jsoo
-       method start = promise_of_lwt LS.start
+       method start = Semgrep_js_shared.promise_of_lwt LS.start
        method getMountpoints = get_jsoo_mountpoint ()
        method setParsePattern = setParsePattern
        method setJustParseWithLang = setJustParseWithLang

js/language_server/dune
@@ -9,6 +9,7 @@
   logs.browser
   semgrep.language_server
   semgrep.semgrep_js_shared
+  semgrep.semgrep_node_js_shared
   lwt_platform.js
 
    networking.http_helpers
@@ -17,7 +18,6 @@
  (modes js)
  (js_of_ocaml
   (javascript_files
-   unix.js
    XMLHttpRequest.js
  ))
  (preprocess

js/node_shared/dune
@@ -1,0 +1,18 @@
+; What is this folder for? We may have some common functionalities that we
+; would like to share among purely the Node-specific Javascript code. For
+; instance, our `unix.js` is something which should not be included into all
+; of the JSCaml code (like the parsers), but just the test code and actual
+; LSP code.
+; To make this distinction apparent, we have `shared`, which is for things
+; universal to all of the JSCaml code, and `node_shared`, for only things
+; which should be distributed via Node.js.
+(library
+ (public_name semgrep.semgrep_node_js_shared)
+ (name semgrep_node_js_shared)
+ (wrapped false)
+ (js_of_ocaml
+  (javascript_files
+    unix.js
+ ))
+ (preprocess
+  (pps js_of_ocaml-ppx lwt_ppx)))

js/shared/dune
@@ -10,6 +10,8 @@
    yaml_ctypes_overrides
    semgrep.parsing
    semgrep.data
+   semgrep.language_server
+   lwt_platform.js
  )
  (js_of_ocaml
   (javascript_files
@@ -17,4 +19,4 @@
     core.js ; misc libraries
  ))
  (preprocess
-  (pps js_of_ocaml-ppx)))
+  (pps js_of_ocaml-ppx lwt_ppx)))

js/tests/dune
@@ -8,12 +8,17 @@
  (libraries
    js_of_ocaml
    semgrep.semgrep_js_shared
+   semgrep.semgrep_node_js_shared
    semgrep.engine
    integers_stubs_js
    libpcre_stubs_js
    libyaml_stubs_js
    alcotest
+   semgrep.language_server
+  lwt_platform.js
+  alcotest-lwt
+  cohttp-lwt-jsoo
  )
  (modes js)
  (preprocess
-  (pps js_of_ocaml-ppx)))
+  (pps js_of_ocaml-ppx lwt_ppx)))

js/tests/index.test.js
@@ -88,7 +88,7 @@ const entrypoint = async () => {
     setJustParseWithLang(parseLang);
     setParsePattern(parsePattern);
 
-    run("");
+    await run("");
     process.exit(0);
   } catch (e) {
     console.log("Error running tests:\n");

libs/commons/dune
@@ -19,6 +19,9 @@
    parmap
    digestif.ocaml
    sexplib
+   ; async stuff
+   lwt
+   alcotest-lwt
  )
  ; can't use profiling.ppx because of circular dependencies :(
  (preprocess

commit 1c001dcc34f068d52d12d98fe33ee23df22a9f50

fix(js): emit `CliOutput` instead of `CoreOutput` (#9132)
## What:
This PR changes the `CoreOutput` emitted by the JS engine to `CliOutput`
instead, which is needed by the Editor.

## Why:
The Editor does not understand the (quite similar) `CoreOutput` type,
and this is making assumptions in the consumption step erroneous.

## How:
We call `Output.preprocess_result` in order to get the types right.

This actually surfaces an error introduced in #9090, which is that the
`caml_sys_open` external call does not actually permit both reading and
writing flags to be set at once. To fix this, some corresponding
adjustments were made in `core.js`.

## Test plan:
`make test` in `js/engine` passes.

Closes PDX-6

js/engine/Main.ml
@@ -53,6 +53,7 @@ let _ =
                    { path = f; analyzer = xlang; products = Product.all })
                source_files
            in
+           let default_config = Scan_CLI.default in
            let config : Core_scan_config.t =
              {
                Core_scan_config.default with
@@ -65,10 +66,14 @@ let _ =
            let timed_rules = (rules_and_errors, 0.) in
            let res = Core_scan.scan config timed_rules in
            let res =
-             Core_json_output.core_output_of_matches_and_errors
-               (Some Autofix.render_fix) res
+             Core_runner.create_core_result (fst rules_and_errors) (Ok res)
            in
-           Semgrep_output_v1_j.string_of_core_output res
+           (* This is just the default configuration, but this function
+              doesn't actually depend upon the parts of the config that we
+              set above.
+           *)
+           let cli_output = Output.preprocess_result default_config res in
+           Semgrep_output_v1_j.string_of_cli_output cli_output
          in
          Semgrep_js_shared.wrap_with_js_error execute
     end)

js/engine/dune
@@ -4,6 +4,7 @@
   js_of_ocaml
   semgrep.running
   semgrep.semgrep_js_shared
+  semgrep.semgrep_node_js_shared
   tree-sitter.run
   ; semgrep.parsing_languages ; skipped to go from 16MB to 7MB
   )

js/shared/core.js
@@ -19,7 +19,7 @@ function ocaml_terminal_size_get() {
 }
 
 //Provides: unix_open
-//Requires: caml_sys_open, caml_list_of_js_array, caml_list_to_js_array
+//Requires: caml_sys_fds, caml_list_of_js_array, caml_list_to_js_array, resolve_fs_device, MlFakeFd, MlFakeFile, caml_create_bytes, caml_raise_no_such_file, caml_raise_sys_error
 function unix_open(path, flags, perm) {
   // Why is this needed?
   // There is a Unix.open_flag type, which is different than
@@ -83,6 +83,7 @@ function unix_open(path, flags, perm) {
     }
   }
 
+  /* Produce the new flags, in the Stdlib.open_flag type */
   let parsed_flags = caml_list_to_js_array(flags);
   let translated_flags_nested = parsed_flags.map((flag) =>
     map_unix_flag_to_stdlib_flag(flag)
@@ -92,7 +93,96 @@ function unix_open(path, flags, perm) {
   );
   let new_flags = caml_list_of_js_array(translated_flags_flattened);
 
-  return caml_sys_open(path, new_flags, perm);
+  /* OK, here some weird stuff is happening.
+     Ideally, we could just hand everything off to caml_sys_open, and call
+     it a day.
+     Unfortunately, the APIs of Unix and Stdlib differ slightly. Not only
+     are their types of open_flag in a different order, but Unix.open_flag
+     supports O_RDWR, and Stdlib.open_flag has no corresponding variant.
+     This ultimately comes out of the fact that Stdlib only supports two
+     explicit functions which open out channels and in channels respectively,
+     and do not need to open one which can do both.
+     This is fine for the OCaml API, but disastrous for us, because we're
+     trying to piggy-back off the Stdlib implementation, which cannot do
+     both read and write.
+     Looking at the jsoo code for these file devices, however, there is no
+     reason why it _can't_ do both reading and writing at the same time,
+     in principle, but it has checks which raise exceptions when it detects
+     both Open_rdonly and Open_wronly, to be in line with the OCaml Stdlib
+     behavior.
+     To avoid having to implement all this virtual simulated file system
+     nonsense, we can still piggy-back off the caml_sys_open impl, but we
+     need to duck underneath the checks for conflicting flags.
+     Below, this code inlines the majority of the body of caml_sys_open,
+     but without the bad checks. We expect that this code should not really
+     change in the future.
+   */
+
+  /* inlined from caml_sys_open */
+  var f = {};
+  while (new_flags) {
+    switch (new_flags[1]) {
+      case 0:
+        f.rdonly = 1;
+        break;
+      case 1:
+        f.wronly = 1;
+        break;
+      case 2:
+        f.append = 1;
+        break;
+      case 3:
+        f.create = 1;
+        break;
+      case 4:
+        f.truncate = 1;
+        break;
+      case 5:
+        f.excl = 1;
+        break;
+      case 6:
+        f.binary = 1;
+        break;
+      case 7:
+        f.text = 1;
+        break;
+      case 8:
+        f.nonblock = 1;
+        break;
+    }
+    new_flags = new_flags[2];
+  }
+  var root = resolve_fs_device(path);
+
+  /* NOTE(bad-open):
+    So far, by inlining, we have avoided one check for the conflicting flags.
+    Unfortunately, the next call to root.device.open has one more, and it's a
+    lot of effort to go to inlining it, because there's a lot of object
+    properties it relies on.
+    However, root.device.open doesn't actually really care about the wronly
+    and rdonly flags, except to check for the conflict, and it passes back an
+    object which has the final flags set inside of it (for future use).
+    So what we can do is remove one of the flags, so as to remove the conflict,
+    and then set it in the resulting MlFakeFd object, as if nothing happened.
+   */
+  let is_wronly = false;
+  if (f.wronly) {
+    f.wronly = 0;
+    is_wronly = true;
+  }
+
+  /* inlined from caml_sys_open */
+  var file = root.device.open(root.rest, f);
+
+  /* see NOTE(bad-open) */
+  if (is_wronly) {
+    file.flags.wronly = 1;
+  }
+
+  /* inlined from caml_sys_open_internal */
+  let idx = caml_sys_fds.length;
+  caml_sys_fds[idx] = file;
+  return idx | 0;
 }
 
 //Provides: unix_environment const

js/engine/tests/__snapshots__/index.test.js.snap
@@ -2,7 +2,6 @@
 
 exports[`yaml parser parses a pattern with pattern-regex 1`] = `
 {
-  "engine_requested": "OSS",
   "errors": [],
   "explanations": [
     {
@@ -49,7 +48,9 @@ exports[`yaml parser parses a pattern with pattern-regex 1`] = `
     },
   ],
   "paths": {
-    "scanned": [],
+    "scanned": [
+      "test.yaml",
+    ],
   },
   "results": [
     {
@@ -61,8 +62,13 @@ exports[`yaml parser parses a pattern with pattern-regex 1`] = `
       },
       "extra": {
         "engine_kind": "OSS",
+        "fingerprint": "<MASKED>",
+        "is_ignored": false,
+        "lines": "foo: bar",
         "message": "test",
+        "metadata": {},
         "metavars": {},
+        "severity": "ERROR",
         "validation_state": "NO_VALIDATOR",
       },
       "path": "test.yaml",
@@ -73,12 +79,6 @@ exports[`yaml parser parses a pattern with pattern-regex 1`] = `
       },
     },
   ],
-  "rules_by_engine": [
-    [
-      "test",
-      "OSS",
-    ],
-  ],
   "skipped_rules": [],
   "version": "<MASKED>",
 }
@@ -86,7 +86,6 @@ exports[`yaml parser parses a pattern with pattern-regex 1`] = `
 
 exports[`yaml parser parses a simple pattern 1`] = `
 {
-  "engine_requested": "OSS",
   "errors": [],
   "explanations": [
     {
@@ -147,7 +146,9 @@ exports[`yaml parser parses a simple pattern 1`] = `
     },
   ],
   "paths": {
-    "scanned": [],
+    "scanned": [
+      "test.yaml",
+    ],
   },
   "results": [
     {
@@ -159,7 +160,11 @@ exports[`yaml parser parses a simple pattern 1`] = `
       },
       "extra": {
         "engine_kind": "OSS",
+        "fingerprint": "<MASKED>",
+        "is_ignored": false,
+        "lines": "foo: bar",
         "message": "test",
+        "metadata": {},
         "metavars": {
           "$X": {
             "abstract_content": "bar",
@@ -175,6 +180,7 @@ exports[`yaml parser parses a simple pattern 1`] = `
             },
           },
         },
+        "severity": "ERROR",
         "validation_state": "NO_VALIDATOR",
       },
       "path": "test.yaml",
@@ -184,12 +190,6 @@ exports[`yaml parser parses a simple pattern 1`] = `
         "offset": 0,
       },
     },
-  ],
-  "rules_by_engine": [
-    [
-      "test",
-      "OSS",
-    ],
   ],
   "skipped_rules": [],
   "version": "<MASKED>",

js/engine/tests/index.test.js
@@ -45,6 +45,12 @@ describe("yaml parser", () => {
         .replaceAll("PRO", "OSS")
     );
     result["version"] = "<MASKED>";
+    // The fingerprint here is inconsistent across CI and locally.
+    // We don't know why. It appears to not be because of the OCaml translation,
+    // because the blakejs library was also inconsistent across platforms.
+    result["results"].map(
+      (match) => (match["extra"]["fingerprint"] = "<MASKED>")
+    );
     expect(result).toMatchSnapshot();
   });
   test("parses a pattern with pattern-regex", async () => {
@@ -60,6 +66,9 @@ describe("yaml parser", () => {
         .replaceAll("PRO", "OSS")
     );
     result["version"] = "<MASKED>";
+    result["results"].map(
+      (match) => (match["extra"]["fingerprint"] = "<MASKED>")
+    );
     expect(result).toMatchSnapshot();
   });
 });