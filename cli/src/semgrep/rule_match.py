9118571239ddb07a99bb63ca7810ec21a9f6e190
Cooper Pierce <cooper@semgrep.com>
2023-11-08 17:31:48 -0800

    Make validation status different for deduplication
    Previously we could reasonably have findings which were equal under cli
    match id but had different validation states. The common case for this
    is a secret comprising a part A and a part B. Such A+B secrets report a
    finding at one part (wlog A) and the deduplication logic does not
    consider A+B and A+B' to be distinct on account of there not being a
    clear way to tell B and B' apart when they are not used in the message.
    
    However, we don't want to consider most findings distinct only on the
    basis of non-consumed metavariables since this results in many findings
    which are duplicative. We should consider either this method, or
    considering all metavariable used in the validators (if any). In either
    case, we need to come up with a clear way to disambiguate findings,
    since in the case where we have A+B, a putative secret, and A+B', ibid.,
    and we report findings at A, it is not clear from the output what
    matters in cases where A+B is valid and A+B' is not (e.g.).
    
    It may be worthwhile considering making `anywhere` result in "additional
    context" similar to a taint trace, which is shown for all findings. That
    said, this may be less than ideal in the main view in the app.
    (Consider, for instance, A0, A1, A2, A3 and the same for B0-3, where
    A0+B0 is valid and so on---then we would flag 4 valid secrets and 12
    "invalid" ones where the invalid ones are irrelevant)

--- cli/src/semgrep/rule_match.py
+++ cli/src/semgrep/rule_match.py
@@ -209,23 +209,15 @@ class RuleMatch:
 
         Used for deduplication in the CLI before writing output.
         """
-        if self.from_transient_scan:
+        return (
+            # REVIEW: comment still relevant?
             # NOTE: We include the previous scan's rules in the config for consistent fixed status work.
             # For unique hashing/grouping, previous and current scan rules must have distinct check IDs.
             # Hence, previous scan rules are annotated with a unique check ID, while the original ID is kept in metadata.
             # As check_id is used for cli_unique_key, this patch fetches the check ID from metadata for previous scan findings.
             # TODO: Once the fixed status work is stable, all findings should fetch the check ID from metadata.
             # This fallback prevents breaking current scan results if an issue arises.
-            return (
-                self.annotated_rule_name,
-                str(self.path),
-                self.start.offset,
-                self.end.offset,
-                self.message,
-                None,
-            )
-        return (
-            self.rule_id,
+            self.annotated_rule_name if self.from_transient_scan else self.rule_id,
             str(self.path),
             self.start.offset,
             self.end.offset,
@@ -236,6 +228,14 @@ class RuleMatch:
             # if self.match.extra.dataflow_trace
             # else None,
             None,
+            # TODO: it may be better to instead consider the validator with
+            # metavariables interpolated in, as this gives a greater picture of
+            # the differences in validation (different validation attempts
+            # would then be considered different, rather than only outcomes).
+            # However, this would require us to carry much more information
+            # about the rule around with the match, so I've gone for a simpler
+            # option here.
+            self.match.extra.validation_state,
         )
 
     @ci_unique_key.default
