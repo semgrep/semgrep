1d2773648b5a2f2ed73da8c14143cfc975c27ba4
Heejong Lee <heejong@gmail.com>
2023-11-10 12:08:24 -0800

    add comment

--- tests/rules/assign_in_cond_expr_cpp.cpp
+++ tests/rules/assign_in_cond_expr_cpp.cpp
@@ -6,6 +6,8 @@ void assignment_expression_parens() {
   }
 
   void *data2 = NULL;
+  // 11/10/2023: tree-sitter-cpp can't parse assignments in conditional
+  // expression. only menhir cpp parser can parse this for now.
   while ((data2 = get_taint())) {
     // ruleid: assign-in-cond-expr
     sink(data2);
@@ -13,6 +15,10 @@ void assignment_expression_parens() {
 }
 
 class Foo {
+  // added to check if menhir parser can parse virtual specifiers.
+  // minimized version of https://semgrep.dev/playground/s/zzBG
+  //
+  // TODO: safe to remove once tree-sitter-cpp successfully parses this file.
   void bar() override {};
 };
 
