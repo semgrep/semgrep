64fb709234802e022d03d71d42840b6a20b95141
Zach Zeleznick <zz@semgrep.com>
2023-11-15 08:05:58 -0800

    add new cli ux for improved messaging

--- src/osemgrep/cli_scan/Scan_subcommand.ml
+++ src/osemgrep/cli_scan/Scan_subcommand.ml
@@ -142,6 +142,78 @@ let file_match_results_hook (conf : Scan_CLI.conf) (rules : Rule.rules)
           ~color_output:conf.force_color Format.std_formatter cli_matches)
       ~finally:(fun () -> Unix.lockf Unix.stdout Unix.F_ULOCK 0))
 
+(*****************************************************************************)
+(* Pretty Printing for CLI UX *)
+(*****************************************************************************)
+let print_logo () : unit =
+  let logo =
+    Ocolor_format.asprintf
+      {|
+┌──── @{<green>○○○@} ────┐
+│ Semgrep CLI │
+└─────────────┘
+|}
+  in
+  Logs.app (fun m -> m "%s" logo);
+  ()
+
+let feature_status_str ~(enabled : bool) : string =
+  let status =
+    if enabled then Ocolor_format.asprintf {|@{<green>✔@}|}
+    else Ocolor_format.asprintf {|@{<red>✘@}|}
+  in
+  status
+
+let print_feature_section ~(includes_token : bool) ~(engine : Engine_type.t) :
+    unit =
+  let secrets_enabled =
+    match engine with
+    | PRO
+        Engine_type.
+          { secrets_config = Some Engine_type.{ allow_all_origins = _; _ }; _ }
+      ->
+        true
+    | OSS
+    | PRO Engine_type.{ secrets_config = None; _ } ->
+        false
+  in
+  let features =
+    [
+      ( ( "Semgrep OSS",
+          "Basic security coverage for first-party code vulnerabilities." ),
+        true );
+      ( ( "Semgrep Code (SAST)",
+          "Find and fix vulnerabilities in the code you write with advanced \
+           scanning and expert security rules." ),
+        includes_token );
+      ( ( "Semgrep Secrets",
+          "Detect and validate potential secrets in your code." ),
+        secrets_enabled );
+    ]
+  in
+  (* Print our set of features and whether each is enabled *)
+  List.iter
+    (fun tup ->
+      Logs.app (fun m ->
+          m "%s %s"
+            (snd tup |> feature_status_str)
+            (Ocolor_format.asprintf {|@{<bold>%s@}|} (fst (fst tup))));
+      Logs.app (fun m ->
+          m "  %s %s\n" (snd tup |> feature_status_str) (snd (fst tup))))
+    features;
+  ()
+
+let display_rule_source ~(rule_source : Rules_source.t) : unit =
+  let msg =
+    match rule_source with
+    | Configs _ ->
+        Ocolor_format.asprintf {|@{<bold>  %s@}|}
+          "Loading rules from registry..."
+    | Pattern _ -> Ocolor_format.asprintf {|@{  %s@}|} "Using custom pattern."
+  in
+  Logs.app (fun m -> m "%s" msg);
+  ()
+
 (*************************************************************************)
 (* Helpers *)
 (*************************************************************************)
@@ -536,6 +608,10 @@ let run_scan_files (conf : Scan_CLI.conf) (profiler : Profiler.t)
 let run_scan_conf (conf : Scan_CLI.conf) : Exit_code.t =
   let profiler = Profiler.make () in
   Profiler.start profiler ~name:"total_time";
+
+  (* Print Semgrep CLI logo ASAP to minimize time to first meaningful content paint *)
+  print_logo ();
+
   (* Metrics initialization (and finalization) is done in CLI.ml,
    * but here we "configure" it (enable or disable it) based on CLI flags.
    *)
@@ -555,6 +631,23 @@ let run_scan_conf (conf : Scan_CLI.conf) : Exit_code.t =
       settings)
     |> Profiler.record profiler ~name:"config_time"
   in
+
+  (* Print feature section for enabled products if pattern mode is not being used.
+     Ideally, pattern mode should be a different subcommand, but for now we will
+     conditionally print the feature section.
+  *)
+  (fun () ->
+    match conf.rules_source with
+    | Pattern _ ->
+        Logs.app (fun m ->
+            m "%s"
+              (Ocolor_format.asprintf {|@{<bold>  %s@}|}
+                 "Code scanning at ludicrous speed.\n"))
+    | _ ->
+        print_feature_section
+          ~includes_token:(settings.api_token <> None)
+          ~engine:conf.engine_type)
+    ();
 
   (* step0: potentially notify user about metrics *)
   if not (settings.has_shown_metrics_notification =*= Some true) then (
@@ -583,13 +676,14 @@ let run_scan_conf (conf : Scan_CLI.conf) : Exit_code.t =
 
   (* step1: getting the rules *)
 
-  (* Rule_fetching.rules_and_origin record also contain errors *)
+  (* Display a message, ideally a progress bar to denote rule fetching *)
+  display_rule_source ~rule_source:conf.rules_source;
+
   let rules_and_origins =
     Rule_fetching.rules_from_rules_source ~token_opt:settings.api_token
       ~rewrite_rule_ids:conf.rewrite_rule_ids
       ~registry_caching:conf.registry_caching conf.rules_source
   in
-
   (* step2: getting the targets *)
   let targets_and_skipped =
     Find_targets.get_targets conf.targeting_conf conf.target_roots
