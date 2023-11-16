9456ec52e7e73c0fb491fb8bf09d3200130f9232
Zach Zeleznick <zz@semgrep.com>
2023-11-16 01:03:15 -0800

    add spinner to rule fetching

--- src/osemgrep/networking/Console_Spinner.ml
+++ src/osemgrep/networking/Console_Spinner.ml
@@ -17,6 +17,40 @@ let show_spinner delay_ms : unit =
     Unix.sleepf (Float.of_int delay_ms /. Float.of_int (1000 * 100))
   done
 
+let spinner_async stop : unit =
+  (* nosemgrep *)
+  ANSITerminal.(print_string [] "\027[?25l");
+  (* hide cursor to make progess indicator more visible *)
+  let jump_y = ref true in
+  let print_frame ~frame_index:i : unit =
+    let spinner = spinner.(i mod Array.length spinner) in
+    ANSITerminal.move_bol ();
+    (* ensure we update only the progress indicator *)
+    if !jump_y then (
+      (* jump to the line above to add the indicator *)
+      ANSITerminal.move_cursor 0 (-1);
+      jump_y := false)
+    else ();
+    (* extra guard against printing among race conditions *)
+    if not !stop then ANSITerminal.printf [ ANSITerminal.green ] "%s" spinner
+  in
+  let rec loop () =
+    if !stop then (
+      (* nosemgrep *)
+      ANSITerminal.(print_string [] "\027[?25h");
+      (* restore cursor *)
+      Lwt.return ())
+    else
+      let%lwt () = Lwt_unix.sleep 0.1 in
+      (* create a cancellable promise *)
+      for frame_index = 1 to 10 do
+        print_frame ~frame_index;
+        Unix.sleepf 0.05 (* ensure we have a fast refresh rate at 50ms *)
+      done;
+      loop ()
+  in
+  Lwt.async loop
+
 let erase_spinner () : unit =
   ANSITerminal.move_cursor 0 (-1);
   ANSITerminal.move_bol ();
