c52e70e7258f5100d79bef6e29770cc747acc58e
ajbt200128 <austin@cutedogs.org>
2023-11-21 10:43:58 -0800

    Fix custom methods sent w/ no params

--- src/osemgrep/language_server/custom_requests/Login.ml
+++ src/osemgrep/language_server/custom_requests/Login.ml
@@ -7,20 +7,16 @@ let meth = "semgrep/login"
 
 let on_request (server : RPC_server.t) params : Yojson.Safe.t option =
   ignore server;
-  match params with
-  | None ->
-      if Semgrep_login.is_logged_in () then (
-        RPC_server.notify_show_message ~kind:MessageType.Info
-          "Already logged in to Semgrep Code";
-        None)
-      else
-        let id, uri = Semgrep_login.make_login_url () in
-        Some
-          (`Assoc
-            [
-              ("url", `String (Uri.to_string uri));
-              ("sessionId", `String (Uuidm.to_string id));
-            ])
-  | Some _ ->
-      Logs.warn (fun m -> m "semgrep/login got params but expected none");
-      None
+  ignore params;
+  if Semgrep_login.is_logged_in () then (
+    RPC_server.notify_show_message ~kind:MessageType.Info
+      "Already logged in to Semgrep Code";
+    None)
+  else
+    let id, uri = Semgrep_login.make_login_url () in
+    Some
+      (`Assoc
+        [
+          ("url", `String (Uri.to_string uri));
+          ("sessionId", `String (Uuidm.to_string id));
+        ])
