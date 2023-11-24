semgrep/semgrep

commit e93afb54fb71868bf19cc33df0988ae9e1c60d61

fix: tainting: Do not clean LHS if there were side-effects (#9263)
When we encountered an assignment `lval := expr` where `expr` returned
no taints, we automatically cleaned `lval`. This was correct in the
early days of taint-mode, before we introduced taint by side-effect, but
it is wrong now. The LHS `lval` may be tainted by side-effect, in which
case we cannot clean it just because `expr` returns no taint. Now that
we introduced `by-side-effect: only` it is also possible for `expr` to
taint `lval` by side-effect and return no immediate taint.

Needs PR semgrep/semgrep-rules-jsonnet#398 to update a bunch of
'todoruleid's that are fixed by this PR.

Closes PA-3164

test plan:
make test # new test

src/tainting/Dataflow_tainting.ml
@@ -1727,8 +1727,19 @@ let transfer :
                  * See [Taint_lval_env] for details. *)
                 Lval_env.add lval_env' lval taints
               else
-                (* Instruction returns safe data, remove taints from lval.
-                 * See [Taint_lval_env] for details. *)
+                (* The RHS returns no taint, but taint could propagate by
+                 * side-effect too. So, we check whether the taint assigned
+                 * to 'lval' has changed to determine whether we need to
+                 * clean 'lval' or not. *)
+                let lval_taints_changed =
+                  let lval_taints_before =
+                    Lval_env.dumb_find in' lval |> status_to_taints
+                  in
+                  let lval_taints_after =
+                    Lval_env.dumb_find lval_env' lval |> status_to_taints
+                  in
+                  not (Taints.equal lval_taints_before lval_taints_after)
+                in
                 match x.i with
                 | New _ ->
                     (* Pro/HACK: `x = new T(args)` is interpreted as `x.T(args)` where `T`
@@ -1738,7 +1749,16 @@ let transfer :
                      *
                      * TODO: `new T(args)` should return an "object taint signature". *)
                     lval_env'
-                | _ -> Lval_env.clean lval_env' lval)
+                | _ when lval_taints_changed ->
+                    (* The taint of 'lval' has changed, so there was a source or
+                     * sanitizer acting by side-effect on this instruction. Thus we do NOT
+                     * do anything more here. *)
+                    lval_env'
+                | _ ->
+                    (* No side-effects on 'lval', and the instruction returns safe data,
+                     * so we assume that the assigment acts as a sanitizer and therefore
+                     * remove taints from lval. See [Taint_lval_env] for details. *)
+                    Lval_env.clean lval_env' lval)
           | None ->
               (* Instruction returns 'void' or its return value is ignored. *)
               lval_env'

tests/rules/taint_by_side_effect_lhs.py
@@ -1,0 +1,4 @@
+def test():
+    x = source()
+    #ruleid: test
+    sink(x)

tests/rules/taint_by_side_effect_lhs.yaml
@@ -1,0 +1,17 @@
+rules:
+  - id: test
+    languages:
+      - python
+    message: Test
+    mode: taint
+    pattern-sinks:
+      - pattern: |
+          sink(...)
+    pattern-sources:
+      - by-side-effect: true
+        patterns:
+          - pattern: |
+              $X = source()
+          - focus-metavariable: $X
+    severity: ERROR
+

commit 0a3ad6984d41d0f0348c0ecebf967909dc1da1e7

fix: tainting: Remove Pro-hack for `lval = new ...`
The hack consisted in not cleaning the taint in `lval` when the RHS was
a `new`-expression. This is because in Pro we are tainting `lval` by
side-effect and we did not want Semgrep to clean that taint.

In principle, this hack could have been removed since PR #9263, once we
were explicitly checking for side-effectful updates to `lval` and only
cleaning `lval` if there were none. But PR #9263 was only checking
changes to `lval` itself, and we have to check changes to `lval` as well
to any of its extensions `lval.a1. ... .aN`.

Once PR #9263 is fixed, this hack for `new` is no longer needed and, in
fact, it was causing FPs in double-free rules, when an object is both
allocated and freed inside a loop.

Fixes: e93afb54fb7 (fix: tainting: Do not clean LHS if there were side-effects (#9263)")

test plan:
make test # new test

src/tainting/Dataflow_tainting.ml
@@ -1721,7 +1721,7 @@ let transfer :
         let lval_env' =
           let has_taints = not (Taints.is_empty taints) in
           match opt_lval with
-          | Some lval -> (
+          | Some lval ->
               if has_taints then
                 (* Instruction returns tainted data, add taints to lval.
                  * See [Taint_lval_env] for details. *)
@@ -1732,33 +1732,18 @@ let transfer :
                  * to 'lval' has changed to determine whether we need to
                  * clean 'lval' or not. *)
                 let lval_taints_changed =
-                  let lval_taints_before =
-                    Lval_env.dumb_find in' lval |> status_to_taints
-                  in
-                  let lval_taints_after =
-                    Lval_env.dumb_find lval_env' lval |> status_to_taints
-                  in
-                  not (Taints.equal lval_taints_before lval_taints_after)
+                  not (Lval_env.equal_by_lval in' lval_env' lval)
                 in
-                match x.i with
-                | New _ ->
-                    (* Pro/HACK: `x = new T(args)` is interpreted as `x.T(args)` where `T`
-                     * is the constructor. But `x.T` does not return any taint, taint is
-                     * propagated to `x` by side-effect, and in a field-sensitivity way.
-                     * If we clean `x` here, then we would be removing that taint.
-                     *
-                     * TODO: `new T(args)` should return an "object taint signature". *)
-                    lval_env'
-                | _ when lval_taints_changed ->
-                    (* The taint of 'lval' has changed, so there was a source or
-                     * sanitizer acting by side-effect on this instruction. Thus we do NOT
-                     * do anything more here. *)
-                    lval_env'
-                | _ ->
-                    (* No side-effects on 'lval', and the instruction returns safe data,
-                     * so we assume that the assigment acts as a sanitizer and therefore
-                     * remove taints from lval. See [Taint_lval_env] for details. *)
-                    Lval_env.clean lval_env' lval)
+                if lval_taints_changed then
+                  (* The taint of 'lval' has changed, so there was a source or
+                   * sanitizer acting by side-effect on this instruction. Thus we do NOT
+                   * do anything more here. *)
+                  lval_env'
+                else
+                  (* No side-effects on 'lval', and the instruction returns safe data,
+                   * so we assume that the assigment acts as a sanitizer and therefore
+                   * remove taints from lval. See [Taint_lval_env] for details. *)
+                  Lval_env.clean lval_env' lval
           | None ->
               (* Instruction returns 'void' or its return value is ignored. *)
               lval_env'

src/tainting/Taint_lval_env.ml
@@ -106,6 +106,7 @@ let normalize_lval lval =
   in
   Some { IL.base; rev_offset }
 
+(* Test whether 'lval1' is the same as, or a prefix of, 'lval2'. *)
 let lval_is_prefix lval1 lval2 =
   let open IL in
   let eq_name x y = LV.compare_name x y = 0 in
@@ -287,6 +288,30 @@ let equal
   && VarMap.equal Taints.equal propagated1 propagated2 (* needed ? *)
   && LvalSet.equal cleaned1 cleaned2
   && Taints.equal control1 control2
+
+let equal_by_lval { tainted = tainted1; _ } { tainted = tainted2; _ } lval =
+  match normalize_lval lval with
+  | None ->
+      (* Cannot track taint for this l-value; e.g. because the base is not a simple
+         variable. We just return the same environment untouched. *)
+      false
+  | Some lval ->
+      LvalMap.merge
+        (fun lv opt_taint1 opt_taint2 ->
+          if lval_is_prefix lval lv then
+            match (opt_taint1, opt_taint2) with
+            | None, None -> None
+            | Some _, None
+            | None, Some _ ->
+                (* not equals *)
+                (* TODO: Check if `t` is the empty set ? *)
+                Some ()
+            | Some t1, Some t2 ->
+                if Taints.equal t1 t2 then None else (* not equals *)
+                                                  Some ()
+          else None)
+        tainted1 tainted2
+      |> LvalMap.is_empty
 
 let to_string taint_to_str { tainted; propagated; cleaned; control } =
   (* FIXME: lval_to_str *)

src/tainting/Taint_lval_env.mli
@@ -70,5 +70,10 @@ val union : env -> env -> env
      best case scenario to reduce FPs. *)
 
 val equal : env -> env -> bool
+
+val equal_by_lval : env -> env -> IL.lval -> bool
+(** Check whether two environments assign the exact same taint to and l-value
+ * and each one of its extensions. *)
+
 val to_string : (Taint.taints -> string) -> env -> string
 val seq_of_tainted : env -> (IL.lval * Taint.taints) Seq.t

tests/rules/taint_typestate3.cpp
@@ -1,0 +1,11 @@
+void test() {
+  while (nondet) {
+    int *v1 = new int;
+    int *v2;
+    v2 = new int;
+    // ok: test
+    delete v1;
+    // ok: test
+    delete v2;
+  }
+}

tests/rules/taint_typestate3.yaml
@@ -1,0 +1,17 @@
+rules:
+  - id: test
+    languages:
+      - cpp
+    message: Test
+    mode: taint
+    pattern-sources:
+      - by-side-effect: only
+        patterns:
+          - pattern: delete $VAR;
+          - focus-metavariable: $VAR
+    pattern-sinks:
+      - patterns:
+          - pattern: delete $VAR;
+          - focus-metavariable: $VAR
+    severity: ERROR
+