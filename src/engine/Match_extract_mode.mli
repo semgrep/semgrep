ffff5fa560cfb5859b106d2781d3929fda0eb887
pad <pad@r2c.dev>
2023-11-24 10:44:47 +0100

    Small cleanups
    test plan:
    make core

--- src/engine/Match_extract_mode.mli
+++ src/engine/Match_extract_mode.mli
@@ -1,10 +1,12 @@
 (*
    Generates a list of targets corresponding to extract mode rule matches in
-   the provided target file. Each generated target will also have a
-   function which can transform match results in the generated target/file to
-   match results corresponding to the file from which that extraction occured.
+   the provided original target file. Each "extracted" generated target will
+   also have a function which can transform match results in the extracted
+   target to match results corresponding to the file from which that
+   extraction occured (the original target).
 
-   Note that this internally calls Match_rules.check().
+   Note that this internally calls Match_rules.check(), hence the
+   few label arguments.
 *)
 val extract :
   match_hook:(string -> Pattern_match.t -> unit) ->
