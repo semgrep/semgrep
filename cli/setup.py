0dde27bb3122d59fb2e8ee92eb59a09b7056578b
IagoAbal <IagoAbal@users.noreply.github.com>
2023-11-29 12:02:11 +0000

    chore: Bump version to 1.51.0

--- cli/setup.py
+++ cli/setup.py
@@ -131,7 +131,7 @@ extras_require = {"experiments": ["jsonnet~=0.18"]}
 
 setuptools.setup(
     name="semgrep",
-    version="1.50.0",
+    version="1.51.0",
     author="Semgrep Inc.",
     author_email="support@semgrep.com",
     description="Lightweight static analysis for many languages. Find bug variants with patterns that look like source code.",
