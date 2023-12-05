8f894586a7164c9675cd6bbf72f39d70e410bff3
pad <pad@r2c.dev>
2023-12-05 09:32:11 +0100

    No need tmp_ split

--- TCB/Cap.mli
+++ TCB/Cap.mli
@@ -1,10 +1,12 @@
 (* Capabilities implemented as simple abstract types and explicit
  * parameters ("Lambda the ultimate security tool").
  *
- * Note that most of the types below are on purpose [abstract] and there is
+ * Note that most of the types below are on purpose abstract and there is
  * no way to build/forge them except by calling the restricted (statically
  * and dynamically) Cap.main() below which is passing all capabilities
- * to the entry point of your program.
+ * to the entry point of your program. This entry point can then restrict
+ * the set of capabilities to pass to other functions by using the :> cast
+ * operator.
  *)
 
 (**************************************************************************)
@@ -22,7 +24,7 @@ module Process : sig
   type argv
   type env
 
-  (* advanced stuff *)
+  (* advanced stuff (TODO? pid? kill? split signal?)  *)
   type signal
   type exit
 
@@ -34,12 +36,12 @@ end
 
 (* read/write on root/cwd/tmp *)
 module FS : sig
+  (* LATER: could restrict to root_but_no_proc_nor_sys_nor_etc *)
   type root_r
   type root_w
   type cwd_r
   type cwd_w
-  type tmp_r
-  type tmp_w
+  type tmp
 end
 
 module Exec : sig
@@ -66,31 +68,34 @@ end
 
 type root = < root_r : FS.root_r ; root_w : FS.root_w >
 type cwd = < cwd_r : FS.cwd_r ; cwd_w : FS.cwd_w >
-type tmp = < tmp_r : FS.tmp_r ; tmp_w : FS.tmp_w >
+type tmp = < tmp : FS.tmp >
 type fs = < root ; cwd ; tmp >
-type console = < stdin : Console.stdin ; stdout : Console.stdout >
-
-type process_multi =
-  < fork : Process.fork ; domain : Process.domain ; thread : Process.thread >
-
-type process_single = < signal : Process.signal ; exit : Process.exit >
-
-type process =
-  < console
-  ; process_single
-  ; process_multi
-  ; argv : Process.argv
-  ; env : Process.env >
+type stdin = < stdin : Console.stdin >
+type stdout = < stdout : Console.stdout >
+type console = < stdin ; stdout >
+type fork = < fork : Process.fork >
+type domain = < domain : Process.domain >
+type thread = < thread : Process.thread >
+type process_multi = < fork ; domain ; thread >
+type signal = < signal : Process.signal >
+type exit = < exit : Process.exit >
+type process_single = < signal ; exit >
+type argv = < argv : Process.argv >
+type env = < env : Process.env >
+type process = < console ; process_single ; process_multi ; argv ; env >
+type exec = < exec : Exec.t >
 
 (* TODO: extend *)
 type network = < network : Network.t >
-type misc = < time : Misc.time ; random : Misc.random >
+type time = < time : Misc.time >
+type random = < random : Misc.random >
+type misc = < time ; random >
 
 (* alt: called "Stdenv.Base.env" in EIO *)
 type all_caps =
   < process
   ; fs (* a mix of fs and process_multi as it requires both *)
-  ; exec : Exec.t
+  ; exec
   ; network
   ; misc >
 
