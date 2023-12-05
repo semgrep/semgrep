d25045282758915ac787aa25725e6cab9917bd9e
ihji <ihji@users.noreply.github.com>
2023-12-05 21:13:17 +0000

    chore: Bump version to 1.52.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="semgrep_pre_commit_package",
-    version="1.51.0",
-    install_requires=["semgrep==1.51.0"],
+    version="1.52.0",
+    install_requires=["semgrep==1.52.0"],
     packages=[],
 )
