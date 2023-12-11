0bff21d50ee89a3aea7b699be8133ffcf84e992a
pad <pad@r2c.dev>
2023-12-11 08:58:05 +0100

    more

--- TCB/CapSys.mli
+++ TCB/CapSys.mli
@@ -1,2 +1,4 @@
+(* Capability-aware wrappers of the dangerous functions in Sys.ml *)
+
 val argv : Cap.Process.argv -> string array
 val set_signal : Cap.Process.signal -> int -> Sys.signal_behavior -> unit
