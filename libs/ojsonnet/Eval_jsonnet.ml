e5d634b63fde61d0477cce9a744a289f424a1d84
pad <pad@r2c.dev>
2023-12-15 17:31:54 +0100

    ojsonnet: factorize code between subst and envir evaluator
    also removed the new strict evaluator for now, better
    to incorporate back one after the factorization
    
    test plan:
    make test

--- libs/ojsonnet/Eval_jsonnet.ml
+++ libs/ojsonnet/Eval_jsonnet.ml
@@ -2,10 +2,10 @@ let eval_program prog =
   match !Conf_ojsonnet.eval_strategy with
   | EvalSubst -> Eval_jsonnet_subst.eval_program prog
   | EvalEnvir -> Eval_jsonnet_envir.eval_program prog
-  | EvalStrict -> Eval_jsonnet_strict.eval_program prog
+(*  | EvalStrict -> Eval_jsonnet_strict.eval_program prog *)
 
 let manifest_value v =
   match !Conf_ojsonnet.eval_strategy with
   | EvalSubst -> Eval_jsonnet_subst.manifest_value v
   | EvalEnvir -> Eval_jsonnet_envir.manifest_value v
-  | EvalStrict -> Eval_jsonnet_strict.manifest_value v
+(*  | EvalStrict -> Eval_jsonnet_strict.manifest_value v *)
