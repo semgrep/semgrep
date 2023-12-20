9ccb5c1e22019ecb2362c91bea2bb8b1d01e2b27
amchiclet <amchiclet@users.noreply.github.com>
2023-12-20 18:40:01 +0000

    chore: Bump version to 1.54.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="semgrep_pre_commit_package",
-    version="1.54.0",
-    install_requires=["semgrep==1.54.0"],
+    version="1.54.1",
+    install_requires=["semgrep==1.54.1"],
     packages=[],
 )
