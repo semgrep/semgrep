1ab07551137ece30ad2f87eb742fe8df0c321169
Martin Jambon <martin@semgrep.com>
2024-01-12 18:23:59 -0800

    Simplify test

--- libs/alcotest_ext/tests/Failing_test.ml
+++ libs/alcotest_ext/tests/Failing_test.ml
@@ -24,8 +24,7 @@ consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
 non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 |});
-    t "missing snapshot" ~checked_output:Stdout (fun () ->
-        print_endline "hello");
+    t "missing snapshot" ~checked_output:Stdout (fun () -> ());
   ]
 
 let () =
