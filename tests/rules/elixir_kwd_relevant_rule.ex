9ee53bfed73a57ec866e5d1ddc3456fa90171ad6
Iago Abal <mail@iagoabal.eu>
2024-01-15 18:23:34 +0100

    tests: Move Elixir tests to Pro
    And make a test fail when we cannot successfully read the rules file,
    rather than silently consider it as a passing test.
    
    test plan:
    See TODO

--- tests/rules/elixir_kwd_relevant_rule.ex
+++
@@ -1,9 +0,0 @@
-defmodule MyAppWeb.UserController do
-  use MyAppWeb, :controller
-
-  #ruleid: match
-  def show(conn, %{"id" => id}) do
-    user = Repo.get(User, id)
-    render(conn, :show, user: user)
-  end
-end
