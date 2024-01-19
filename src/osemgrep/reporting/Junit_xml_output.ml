5d4f02a8b3ce9c4d3f844b6fa31b33b5f7e53141
Reynir Björnsson <reynir@reynir.dk>
2024-01-19 20:49:42 +0000

    osemgrep: implement junit_xml output (#9626)
    This implements the junit_xml output for osemgrep. The single junit_xml
    test does not pass because the order of the test results is different.
    Otherwise it closely mimics the pysemgrep junit_xml output.
    
    The reason for the difference in order is because the osemgrep code uses
    the cli_output (json) which is sorted. Maybe the solution is to sort the
    output in pysemgrep as well? Especially with the following comment in
    mind:
    
        # Sort results so as to guarantee the same results across different
        # runs. Results may arrive in a different order due to parallelism
        # (-j option).

---
+++ src/osemgrep/reporting/Junit_xml_output.ml
@@ -0,0 +1,67 @@
+module OutT = Semgrep_output_v1_t
+
+let string_of_severity = function
+  | `Info -> "INFO"
+  | `Warning -> "WARNING"
+  | `Error -> "ERROR"
+  | `Experiment -> "EXPERIMENT"
+  | `Inventory -> "INVENTORY"
+
+let junit_test_cases out (results : OutT.cli_match list) =
+  results
+  |> List.iter (fun (result : OutT.cli_match) ->
+         let open Xmlm in
+         output out
+           (`El_start
+             ( ("", "testcase"),
+               [
+                 (("", "name"), Rule_ID.to_string result.check_id);
+                 (("", "classname"), Fpath.to_string result.path);
+                 (("", "file"), Fpath.to_string result.path);
+                 (("", "line"), string_of_int result.start.line);
+               ] ));
+         output out
+           (`El_start
+             ( ("", "failure"),
+               [
+                 (("", "type"), string_of_severity result.extra.severity);
+                 (("", "message"), result.extra.message);
+               ] ));
+         output out (`Data result.extra.lines);
+         output out `El_end;
+         output out `El_end)
+
+let junit_xml_output (cli_output : OutT.cli_output) =
+  let b = Buffer.create 1024 in
+  let open Xmlm in
+  let out = Xmlm.make_output (`Buffer b) in
+  let num_results = List.length cli_output.results in
+  output out (`Dtd None);
+  output out
+    (`El_start
+      ( ("", "testsuites"),
+        [
+          (("", "disabled"), "0");
+          (("", "errors"), "0");
+          (("", "failures"), string_of_int num_results);
+          (("", "tests"), string_of_int num_results);
+          (* XXX(reynir): due to python quirk this is a flaot *)
+          (("", "time"), "0.0");
+        ] ));
+  output out
+    (`El_start
+      ( ("", "testsuite"),
+        [
+          (("", "disabled"), "0");
+          (("", "errors"), "0");
+          (("", "failures"), string_of_int num_results);
+          (("", "name"), "semgrep results");
+          (("", "skipped"), "0");
+          (("", "tests"), string_of_int num_results);
+          (* XXX(reynir): due to python quirk this is an integer *)
+          (("", "time"), "0");
+        ] ));
+  junit_test_cases out cli_output.results;
+  output out `El_end;
+  output out `El_end;
+  Buffer.contents b
