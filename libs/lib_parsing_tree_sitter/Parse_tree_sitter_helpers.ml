a373719e4a26da0e85dbc90a3b183644dce6d981
Emma Jin <emma@semgrep.com>
2024-01-23 17:57:54 -0800

    fix: report token errors properly
    Investigation of https://github.com/semgrep/semgrep/issues/9628
    
    When we can't figure out how to convert a row/column to a bytepos, we just put in
    -1 and move on. In the linked Github issue, this causes an opaque exception when
    we try to use the bytepos in autofix. Better to raise an exception.
    
    Fixing this causes a pattern parse error in one of our Dockerfile rules. TODO that
    parse error needs to be fixed as well in this PR.
    
    Test plan: run p/default against Dockerfile in linked ticket
    TODO automated tests

--- libs/lib_parsing_tree_sitter/Parse_tree_sitter_helpers.ml
+++ libs/lib_parsing_tree_sitter/Parse_tree_sitter_helpers.ml
@@ -75,12 +75,16 @@ let token env (tok : Tree_sitter_run.Token.t) =
   (* Parse_info is 1-line based and 0-column based, like Emacs *)
   let line = start.Tree_sitter_run.Loc.row + 1 in
   let column = start.Tree_sitter_run.Loc.column in
+  let file = env.file in
   let bytepos =
     try h (line, column) with
-    | Not_found -> -1
-    (* TODO? more strict? raise exn? *)
+    | Not_found ->
+        raise
+          (Tok.NoTokenLocation
+             (Printf.sprintf
+                "Could not convert from location %d:%d in %s to a position" line
+                column file))
   in
-  let file = env.file in
   let pos = Pos.make ~line ~column ~file bytepos in
   let tok_loc = { Tok.str; pos } in
   Tok.tok_of_loc tok_loc
