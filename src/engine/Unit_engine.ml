8a245dc7238cbf9d94365cff4521a473acfe3703
Brandon Wu <49291449+brandonspark@users.noreply.github.com>
2024-01-24 17:01:23 -0800

    feat(autofix): aligned fixes (#9488)
    ## What:
    This PR makes it so that autofixes with newlines involved now try to
    synchronize the indentation of the inserted lines.
    
    ## Why:
    Autofix right now is pretty blunt, and just pastes the literal fix text
    into the file, plus interpolation of metavariables and stuff.
    
    This is usually OK, but is weird in the multiline case. For instance, if
    I have the following fix:
    ```
    foo()
    bar()
    ```
    if I replace in this target:
    ```
    def f():
      to_replace()
    ```
    I will get
    ```
    def f():
      foo()
    bar()
    ```
    
    This is because inserting the literal string `"foo()\nbar()` doesn't
    account for the fact that the newline will send the `bar()` all the way
    to the left, which is out of sync with the start of the replacement,
    which was at a non-zero column.
    
    ## How:
    This PR just pads every line after the first to make the columns align.
    
    ## Test plan:
    Automated tests.
    
    Closes #3070

--- src/engine/Unit_engine.ml
+++ src/engine/Unit_engine.ml
@@ -507,8 +507,10 @@ let autofix_tests_for_lang ~polyglot_pattern_path files lang =
                | Ok fix_file -> Fix (UFile.read_file fix_file)
                | Error _ -> (
                    (* A poor man's configuration format.
-                      Either two or three lines.
-                      regex-replacement or regex-count-replacement.
+                      This can either be two lines, the regex to match
+                      and the replacement content (one line),
+                      or 3+ lines, the regex to match, the number of matches
+                      to replace, and the replacement text (possibly multiline)
                    *)
                    match
                      related_file_of_target ~polyglot_pattern_path
@@ -517,12 +519,16 @@ let autofix_tests_for_lang ~polyglot_pattern_path files lang =
                    | Ok fix_regex_file -> (
                        match UFile.cat fix_regex_file with
                        | [ l1; l2 ] -> FixRegex (l1, None, l2)
-                       | [ l1; l2; l3 ] ->
-                           FixRegex (l1, Some (int_of_string l2), l3)
+                       | l1 :: l2 :: l3 :: rest ->
+                           FixRegex
+                             ( l1,
+                               Some (int_of_string l2),
+                               String.concat "\n" (l3 :: rest) )
                        | _ ->
                            failwith
                              (Common.spf
-                                "found fix-regex file %s with <> 2 lines"
+                                "found fix-regex file %s with invalid number \
+                                 of lines"
                                 (Fpath.to_string fix_regex_file)))
                    | Error _ ->
                        failwith
---
+++ tests/autofix/python/aligned_fix.py
@@ -0,0 +1,2 @@
+def f():
+  to_replace()
