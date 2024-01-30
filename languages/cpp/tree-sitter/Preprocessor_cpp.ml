b168bbbf8cca87a4c3958834864915f15a94859d
brandonspark <wu.brandonj@gmail.com>
2024-01-30 09:08:19 -0800

    change to semgrep inc

--- languages/cpp/tree-sitter/Preprocessor_cpp.ml
+++ languages/cpp/tree-sitter/Preprocessor_cpp.ml
@@ -1,6 +1,6 @@
 (* Brandon Wu
  *
- * Copyright (C) 2024 r2c
+ * Copyright (C) 2024 Semgrep Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
