44d8d2b4aa7a883904948173868355c31931d322
Reynir Björnsson <reynir@reynir.dk>
2024-01-31 14:44:09 +0100

    Vendor and use python-str-repr
    Library lives at https://github.com/reynir/python-str-repr,
    corresponds to commit 3364270405576341e8704f021a255a95ad23b9d3.
    
    Mark new test as osempass

---
+++ libs/python-str-repr/test/test_python_str_repr.ml
@@ -0,0 +1,45 @@
+let repr = Python_str_repr.repr
+
+let make_test ?unicode_version (message, input, expected) =
+  message, `Quick, fun () -> Alcotest.(check string) message expected (repr ?unicode_version input)
+
+let test_repr =
+  List.map make_test [
+    "empty", "", "''";
+    "simple", "a", "'a'";
+    "one single quote", {|'|}, {|"'"|};
+    "one single double quote", {|"|}, {|'"'|};
+    "tab character", "tab\tthis", {|'tab\tthis'|};
+    "line feed", "new\nline", {|'new\nline'|};
+    "carraige return line feed", "Host: example.com\r\n", {|'Host: example.com\r\n'|};
+
+    "non-printable ascii", "\x00\x7f\x20\x10", {|'\x00\x7f \x10'|};
+
+    "mixed quotes", "\"I told him 'no' back then.\"", {|'"I told him \'no\' back then."'|};
+    "mixed quotes, mainly single quotes", "'''\"'''", {|'\'\'\'"\'\'\''|};
+    "mixed quotes, mainly double quotes", "\"\"\"'\"\"\"", {|'"""\'"""'|};
+]
+
+let test_repr_unicode =
+  List.map make_test [
+    "unicode 'æ'", "æ", "'æ'";
+    "unicode '\u{80}'", "\u{80}", "'\\x80'";
+  ]
+
+let test_repr_unicode_version = [
+  make_test ~unicode_version:(13, 0)
+    ("'\\u061d' unicode version 13.0.0", "\u{061d}", "'\\u061d'");
+  (* assuming uucp unicode version >= 14.0.0 *)
+  make_test ("'\\u061d' unicode version >= 14.0.0", "\u{061d}", "'\u{061d}'");
+]
+
+let test_suites = [
+  "repr", test_repr;
+  "repr unicode", test_repr_unicode;
+  "repr unicode versions", test_repr_unicode_version;
+]
+
+let () =
+  Alcotest.run
+    "python-str-repr"
+    test_suites
