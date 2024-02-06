3ef8b07ebd4f318c9495cc54eb93d88eb2438655
Cooper Pierce <cooper@semgrep.com>
2024-02-06 13:57:16 -0800

    Add interface for Source

---
+++ libs/source/Source.mli
@@ -0,0 +1,18 @@
+(* Cooper Pierce
+ *
+ * Copyright (c) Semgrep Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation, with the
+ * special exception on linking described in file LICENSE.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
+ * LICENSE for more details.
+ *)
+type t = File of Fpath.t [@@deriving show, eq, ord, sexp]
+
+val to_string : t -> string
+val to_string_opt : ?unspecified:string -> t option -> string
