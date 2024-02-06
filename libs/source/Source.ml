af778e761cbbc7218c2a55ec906a7ed1b3aebd93
Cooper Pierce <cooper@semgrep.com>
2024-02-06 12:00:37 -0800

    Improve package metadata for source

--- libs/source/Source.ml
+++ libs/source/Source.ml
@@ -1,3 +1,17 @@
+(* Cooper Pierce
+ *
+ * Copyright (c) Semgrep Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation, with the
+ * special exception on linking described in file LICENSE.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
+ * LICENSE for more details.
+ *)
 type t = File of Fpath.t [@@deriving show, eq, ord]
 
 (* TODO: should be private *)
