776fb807ae473b6dbaf795e803d639ab13b56960
Chris Dolan <chris@semgrep.com>
2024-02-14 03:50:34 -0700

    WIP: GitHub workflow presubmit (#9761)
    Co-authored-by: pad <pad@r2c.dev>

--- scripts/jsonnet_to_yaml.sh
+++ scripts/jsonnet_to_yaml.sh
@@ -2,8 +2,12 @@
 
 set -euo pipefail
 
-# This script requires to have jsonnet (any jsonnet should work),
-# and yq from https://github.com/mikefarah/yq
+# This script requires to have:
+#  - jsonnet (any jsonnet should work, e.g., go-jsonnet)
+#  - yq (a.k.a go-yq) from https://github.com/mikefarah/yq
+#    Note that many distributions come with a default yq that is not
+#    as powerful as https://github.com/mikefarah/yq so follow
+#    the instructions there to install yq on your machine
 #
 # alt: use ../../_build/install/default/bin/ojsonnet --envir --yaml $< >> $@ ...
 # but ojsonnet is still buggy and some people (infra) don't have the ocaml
