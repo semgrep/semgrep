4a2c849e40e22500c3de1d565b01ceacd8ffdf72
Emma Jin <emma@semgrep.com>
2024-02-15 14:18:36 -0800

    Fix build

--- src/core_scan/Core_scan.ml
+++ src/core_scan/Core_scan.ml
@@ -560,7 +560,7 @@ let parse_equivalences equivalences_file =
 (*****************************************************************************)
 
 let handle_target_with_trace handle_target t =
-  let target_name = Target.internal_path t in
+  let target_name = Target.internal_path t |> Fpath.to_string in
   let%trace span = "Core_scan.handle_target" in
   let res = handle_target t in
   Tracing.add_data_to_span span [ ("filename", `String target_name) ];
