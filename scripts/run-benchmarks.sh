10ec8d70fc59ffaae182c2cb0f78acdeec1c6066
Emma Jin <emma@semgrep.com>
2024-02-16 17:28:48 -0800

    Trace only the second run

--- scripts/run-benchmarks.sh
+++ scripts/run-benchmarks.sh
@@ -31,7 +31,7 @@ pipenv run semgrep --version
 pipenv run semgrep-core -version
 pipenv run python3 ../perf/run-benchmarks --config $config_path --std-only --save-to timing1.json
 jq . timing1.json
-pipenv run python3 ../perf/run-benchmarks --config $config_path --std-only --save-to timing2.json --save-findings-to ci_small_repos_findings.json
+pipenv run python3 ../perf/run-benchmarks --config $config_path --std-only --trace --save-to timing2.json --save-findings-to ci_small_repos_findings.json
 jq . timing2.json
 jq . ci_small_repos_findings.json
 
