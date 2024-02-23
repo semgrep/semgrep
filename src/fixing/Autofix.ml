9ae9aa9772649f2a8a2ede1088af8bbc667ed175
pad <pad@r2c.dev>
2024-02-23 16:18:00 +0100

    Fix the warnings generated by the autofix code in 'make check'
    test plan:
    'make check' does not display autofix warning anymore

--- src/fixing/Autofix.ml
+++ src/fixing/Autofix.ml
@@ -1,6 +1,6 @@
 (* Nat Mote
  *
- * Copyright (C) 2019-2022 r2c
+ * Copyright (C) 2019-2022 Semgrep Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
