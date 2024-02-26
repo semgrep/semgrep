fbd642ea72260f2bafb92c5dc11b5fb065b9a88f
Cooper Pierce <cooper@semgrep.com>
2024-02-26 14:16:05 -0800

    feat: Add an origin for git objects (#9840)
    This PR adds an additional variant on `Origin.t` for targets generated
    from git objects. There are some related updates to Git_wrapper
    implementing additional functionality needed to effectively use these,
    as well as a minor type update for some timestamp types.
    
    The currently implementation of `Git_wrapper.batch_cat_file_blob`
    is less than ideal; I plan on implementing it using Eio once that
    is an option. 
    
    Test plan: existing coverage here; additional functionality using this
    will be tested in pro.

--- libs/git_wrapper/Git_wrapper.mli
+++ libs/git_wrapper/Git_wrapper.mli
@@ -86,6 +86,7 @@ type 'extra obj = { kind : obj_type; sha : sha; extra : 'extra }
 [@@deriving show]
 
 type batch_check_extra = { size : int } [@@deriving show]
+type batch_extra = { contents : string } [@@deriving show]
 type ls_tree_extra = { path : Fpath.t } [@@deriving show]
 
 (* git status *)
@@ -130,19 +131,29 @@ val is_tracked_by_git : ?cwd:Fpath.t -> Fpath.t -> bool
 
 (* precondition: cwd must be a directory *)
 val dirty_files : ?cwd:Fpath.t -> unit -> Fpath.t list
-(** Returns a list of files that are dirty in a git repo *)
+(** [dirty_files ()] is the list of files which are dirty in a git repo, i.e.,
+    files which differ at all from the current index to the HEAD commit, plus
+    untracked files. Note that this means this list includes files which were
+    deleted. *)
 
 val init : ?cwd:Fpath.t -> ?branch:string -> unit -> unit
-(** Initialize a git repo in the given directory.
+(** [init ()] creates an empty git repository in the current directory. If
+    [cwd] is specified, its value is passed to git's [-C] flag. If
+    [branch] is specified, it is used as the name of the default branch.
+    Otherwise the default branch is named 'main' to avoid warnings that depend
+    on the git version.
+
+    Initialize a git repo in the given directory.
     The branch is set by default to 'main' to avoid warnings that depend
     on the git version.
 *)
 
 val add : ?cwd:Fpath.t -> ?force:bool -> Fpath.t list -> unit
-(** Add the given files to the git repo *)
+(** [add files] adds the [files] to the git index. *)
 
 val commit : ?cwd:Fpath.t -> string -> unit
-(** Commit the given files to the git repo with the given message *)
+(** [commit msg] creates a commit with containing the current contents of the
+    index with [msg] as the commit message. *)
 
 val get_project_url : ?cwd:Fpath.t -> unit -> string option
 (** [get_project_url ()] tries to get the URL of the project from
@@ -152,7 +163,7 @@ val get_project_url : ?cwd:Fpath.t -> unit -> string option
 *)
 
 val get_git_logs : ?cwd:Fpath.t -> ?since:float option -> unit -> string list
-(** [get_git_logs()] will run 'git log' in the current directory
+(** [get_git_logs ()] will run 'git log' in the current directory
     and returns for each log a JSON string that fits the schema
     defined in semgrep_output_v1.atd contribution type.
     It returns an empty list if it found nothing relevant.
@@ -162,39 +173,68 @@ val get_git_logs : ?cwd:Fpath.t -> ?since:float option -> unit -> string list
 
 val cat_file_batch_check_all_objects :
   ?cwd:Fpath.t -> unit -> batch_check_extra obj list option
-(** [cat_file_batch_all_objects ()] will run `git log
- * --batch-all-objects --batch-check` in the current working directory (or such
- * directory provided by `cwd`)
- *
- * A batch format sufficient for obtaining the information in
- * [batch_check_extra] will be used and that information will be attached to each
- * object.
+(** [cat_file_batch_all_objects ()] will run [git log
+   --batch-all-objects --batch-check] in the current working directory (or such
+   directory provided by [cwd])
+
+   A batch format sufficient for obtaining the information in
+   [batch_check_extra] will be used and that information will be attached to each
+   object.
  *)
 
 val cat_file_blob : ?cwd:Fpath.t -> sha -> (string, string) result
-(** [cat_file_blob sha] will run `git cat-file blob sha` and return either
- * - [Ok contents], where [contents] is the contents of the blob; or
- * - [Error message] where [message] is a brief message indicating why git
- *   could not perform the action, e.g., [sha] is not the sha of a blob or
- *   [sha] does not designate an object.
+(** [cat_file_blob sha] will run [git cat-file blob sha] and return either
+    {ul
+      {- [Ok contents], where [contents] is the contents of the blob; or}
+      {- [Error message] where [message] is a brief message indicating why git
+      could not perform the action, e.g., [sha] is not the sha of a blob or
+      [sha] does not designate an object.}
+    }
+ *)
+
+val batch_cat_file_blob :
+  ?cwd:Fpath.t ->
+  sha list ->
+  ((batch_extra obj, string) result Seq.t, string) result
+(** [batch_cat_file_blob blobs] will run [git cat-file --batch] for each blob
+    object whose sha is listed in [blobs] and return either:
+    {ul
+      {- [Ok output], where [output] is a sequence batch output, where the
+      elements are either [Ok info], where [info] is the information returned
+      about that blob; or [Error message] where [message] is a brief message
+      indicating why git could not perform the action in relation to an
+      individual blob, e.g., a sha in [blobs] is not the sha of a blob or does
+      not designate an object.
+
+      Note that the output Seq must be consumed to prevent a resource leak,
+      since this is implemented by saving the results, and then parsing
+      additional blobs on-demand. Ideally we would rely on Eio for this, but
+      that's currently blocked on 5.x.}
+      {- [Error message], where [message] is a brief message indicating why git
+      could not perform the entire batch operation, e.g., the directory in
+      which git was run is not a git repo.}
+    }
+
+    Note: we make no ordering guarantee of objects in the output list relative
+    to the order in the input list.
  *)
 
 val object_size : ?cwd:Fpath.t -> sha -> int option
 (** [object_size sha] evaluates to [Some s] where [s] is the size of the object
-  * designated by [sha] in bytes, or [None] if an error occured (e.g. the
-  * object didn't exist).
+    designated by [sha] in bytes, or [None] if an error occured (e.g. the
+    object didn't exist).
   *)
 
 val commit_timestamp : ?cwd:Fpath.t -> sha -> Timedesc.Timestamp.t option
 (** [commit_datetime sha] evaluates to [Some dt] where [dt] is the date and
- * time of the commit designated by [sha] or [None] if an error occured (e.g.,
- * the sha was for another object type).
+   time of the commit designated by [sha] or [None] if an error occured (e.g.,
+   the sha was for another object type).
  *)
 
 val ls_tree :
   ?cwd:Fpath.t -> ?recurse:bool -> sha -> ls_tree_extra obj list option
 (** [ls_tree ~recurse sha] will run `git ls-tree --full-tree` and report the
- * listed objects and their file paths relative to that tree. If [recurse] is
- * specified to be true (it is false by default) then the `-r` option is passed
- * and git will recurse into subtrees.
+   listed objects and their file paths relative to that tree. If [recurse] is
+   specified to be true (it is false by default) then the `-r` option is passed
+   and git will recurse into subtrees.
  *)
