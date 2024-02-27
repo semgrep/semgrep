e0ebc6e48ef45e9817327a4b6b2d4a0136c08f3d
Alexis Grant <alexis@r2c.dev>
2024-02-27 14:11:59 -0800

    try to clean url in OCaml wrapper too

--- libs/git_wrapper/Git_wrapper.ml
+++ libs/git_wrapper/Git_wrapper.ml
@@ -546,7 +546,10 @@ let commit ?cwd msg =
 let get_project_url ?cwd () : string option =
   let cmd = (git, cd cwd @ [ "ls-remote"; "--get-url" ]) in
   match UCmd.string_of_run ~trim:true cmd with
-  | Ok (url, _) -> Some url
+  | Ok (url, _) ->
+      Some
+        (let r = Str.regexp {|//.*:.*@\(.*\)|} in
+         Str.replace_first r {|//\1|} url)
   | Error _ ->
       UFile.find_first_match_with_whole_line (Fpath.v ".git/config") ".com"
 (* TODO(dinosaure): this line is pretty weak due to the [".com"] (what happens
