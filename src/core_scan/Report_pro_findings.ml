9ee23ea57d70b92f463b28689acf7615d20ddaf4
Emma Jin <emma@semgrep.com>
2024-02-29 10:39:36 -0800

    Slight refactor

--- src/core_scan/Report_pro_findings.ml
+++ src/core_scan/Report_pro_findings.ml
@@ -20,13 +20,6 @@
 
 (* Check if the taint trace of the pattern crosses functions or files *)
 
-let filename_of_token (tok : Tok.t) =
-  match tok with
-  | OriginTok loc -> Some loc.pos.file
-  | FakeTok _ -> None
-  | ExpandedTok (loc, _) -> Some loc.pos.file
-  | Ab -> None
-
 let tokens_of_trace_item (trace_item : Pattern_match.taint_trace_item) =
   let rec tokens_of_call_trace (call_trace : Pattern_match.taint_call_trace) =
     match call_trace with
@@ -40,13 +33,19 @@ let tokens_of_trace_item (trace_item : Pattern_match.taint_trace_item) =
 
 let is_interfile_trace (trace : Pattern_match.taint_trace) =
   let trace_tokens = trace |> List.concat_map tokens_of_trace_item in
-  let token_filenames = trace_tokens |> List_.map_filter filename_of_token in
+  let token_filenames =
+    trace_tokens
+    |> List_.map_filter (fun t ->
+           match Tok.loc_of_tok t with
+           | Ok { pos; _ } -> Some pos.file
+           | Error _ -> None)
+  in
   (* iff two tokens have different filenames, the trace crosses files *)
   match token_filenames with
   | []
   | [ _ ] ->
       false
-  | f :: fs -> not (List.for_all (fun x -> x = f) fs)
+  | f :: fs -> List.exists (fun x -> x <> f) fs
 
 let is_interprocedural_trace (trace : Pattern_match.taint_trace) =
   trace
