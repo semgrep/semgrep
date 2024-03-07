4e43dfc084c5120d1a2a6d5be573ea6e9ea4d958
Amarin Phaosawasdi <amchiclet@users.noreply.github.com>
2024-03-06 16:59:29 -0800

    added pro test

---
+++ cli/tests/default/e2e-pro/test_taint_intrafile.py
@@ -0,0 +1,22 @@
+import pytest
+from tests.fixtures import RunSemgrep
+
+from semgrep.constants import OutputFormat
+
+
+@pytest.mark.osemfail
+@pytest.mark.parametrize(
+    "rule,target",
+    [("rules/taint_intrafile.yaml", "taint/taint_intrafile.py")],
+)
+def test_taint_traces(run_semgrep_in_tmp: RunSemgrep, tmp_path, snapshot, rule, target):
+    semgrep_result = run_semgrep_in_tmp(
+        rule,
+        target_name=target,
+        output_format=OutputFormat.TEXT,
+        options=["--pro-intrafile"],
+    )
+    snapshot.assert_match(
+        semgrep_result.stdout,
+        "results.txt",
+    )
