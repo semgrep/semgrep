a0eff40d0788180439ad4a3e33cd8fe4519fd26a
Amarin Phaosawasdi <amchiclet@users.noreply.github.com>
2024-03-12 13:51:27 -0700

    remove the need for token

--- cli/tests/e2e-pro-need-login/test_taint_intrafile.py
+++ cli/tests/e2e-pro-need-login/test_taint_intrafile.py
@@ -1,7 +1,39 @@
 import pytest
 from tests.fixtures import RunSemgrep
+from tests.semgrep_runner import SemgrepRunner
 
+from semgrep.cli import cli
 from semgrep.constants import OutputFormat
+
+
+def fake_logout_login(tmp_path, mocker):
+    # Fake the login, so that we can run --pro_intrafile
+    runner = SemgrepRunner(
+        env={"SEMGREP_SETTINGS_FILE": str(tmp_path / ".settings.yaml")},
+        use_click_runner=True,
+    )
+    fake_key = "key123"
+
+    # Patch Token Validation:
+    mocker.patch(
+        "semgrep.app.auth.get_deployment_from_token", return_value="deployment_name"
+    )
+
+    # Logout
+    result = runner.invoke(cli, subcommand="logout", args=[])
+    assert result.exit_code == 0
+    assert result.output.startswith("Logged out")
+
+    # Login with env token
+    result = runner.invoke(
+        cli,
+        subcommand="login",
+        args=[],
+        env={"SEMGREP_APP_TOKEN": fake_key},
+    )
+    assert result.exit_code == 0
+    assert result.output.startswith("Saved login token")
+    assert "<redacted>" in result.output
 
 
 @pytest.mark.kinda_slow
@@ -11,15 +43,20 @@ from semgrep.constants import OutputFormat
     [("rules/taint_intrafile.yaml", "taint/taint_intrafile.py")],
 )
 def test_taint_intrafile(
-    run_semgrep_in_tmp: RunSemgrep, tmp_path, snapshot, rule, target
+    run_semgrep_in_tmp: RunSemgrep, tmp_path, mocker, snapshot, rule, target
 ):
+    fake_logout_login(tmp_path, mocker)
+
+    # Test
     semgrep_result = run_semgrep_in_tmp(
         rule,
         target_name=target,
-        output_format=OutputFormat.TEXT,
+        # The text format could have worked, but it may have trailing white
+        # spaces, and lint complains, so json output is a bit more robust.
+        output_format=OutputFormat.JSON,
         options=["--pro-intrafile"],
     )
     snapshot.assert_match(
         semgrep_result.stdout,
-        "results.txt",
+        "results.json",
     )
