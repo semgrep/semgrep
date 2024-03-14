362007fbb5fca10d4d625e7c7c67d038d8a721df
Emma Jin <emjin@users.noreply.github.com>
2024-03-14 13:20:30 -0700

    feat(tracing): add data to the top level spans of semgrep otel runs (#9907)
    This will make it easier for us to understand the times
    
    Unfortunately, a lot of information is hard to get by the point we enter
    semgrep-core. With osemgrep as the entry point, we would be able to add
    a lot more. Adding it during the migration is fairly difficult.

---
+++ src/core/Trace_data.mli
@@ -0,0 +1,30 @@
+(** Helpers to prepare data for Opentelemetry tracing *)
+
+(* Types *)
+
+type analysis_flags = {
+  secrets_validators : bool;  (** True when secrets validators are enabled *)
+  allow_all_origins : bool;
+      (** True when secrets validators from any origin may be used. This value
+          is discarded if secrets_validators is false *)
+  historical_scan : bool;  (** True when historical scans are enabled *)
+  deep_intra_file : bool;
+      (** True when deep intrafile scans (aka interproc taint) is enabled *)
+  deep_inter_file : bool;
+      (** True when interfile scans are enabled. Only one of `deep_inter_file`
+         and `deep_intra_file` should be true. *)
+}
+[@@derving show]
+
+(* Helpers *)
+
+val no_analysis_features : unit -> analysis_flags
+(** For analysis run with the oss engine, we know all the flags will be false *)
+
+val data_of_languages : Xlang.t list -> (string * Tracing.user_data) list
+(** Convenience function to turn a list of interfile languages into otel data *)
+
+val get_top_level_data :
+  int -> string -> analysis_flags -> (string * Tracing.user_data) list
+(** Create the tags for the top level span. These tags make it easy to see
+    the traces we care about *)
--- src/core_scan/Core_scan.ml
+++ src/core_scan/Core_scan.ml
@@ -1070,10 +1070,20 @@ let scan ?match_hook (caps : < Cap.tmp >) config
     else NoPrefiltering
   in
 
+  (* Add information to the trace *)
+  let num_targets = List.length all_targets in
+  let num_skipped_targets = List.length skipped in
+  Tracing.add_data_to_opt_span config.top_level_span
+    [
+      ("num_rules", `Int (List.length valid_rules));
+      ("num_targets", `Int num_targets);
+      ("num_skipped_targets", `Int num_skipped_targets);
+    ];
+
   (* Let's go! *)
   Logs.info (fun m ->
-      m ~tags "processing %d files, skipping %d files" (List.length all_targets)
-        (List.length skipped));
+      m ~tags "processing %d files, skipping %d files" num_targets
+        num_skipped_targets);
   let file_results, scanned_targets =
     all_targets
     |> iter_targets_and_get_matches_and_exn_to_errors config
@@ -1107,10 +1117,12 @@ let scan ?match_hook (caps : < Cap.tmp >) config
       (List_.map (fun r -> (r, `OSS)) valid_rules)
       invalid_rules scanned interfile_languages_used ~rules_parse_time
   in
+  let num_matches = List.length res.processed_matches in
+  let num_errors = List.length res.errors in
+  Tracing.add_data_to_opt_span config.top_level_span
+    [ ("num_matches", `Int num_matches); ("num_errors", `Int num_errors) ];
   Logs.info (fun m ->
-      m ~tags "found %d matches, %d errors"
-        (List.length res.processed_matches)
-        (List.length res.errors));
+      m ~tags "found %d matches, %d errors" num_matches num_errors);
 
   let processed_matches, new_errors, new_skipped =
     filter_files_with_too_many_matches_and_transform_as_timeout
