52a0e97c9b88db94929170bde175e14118ac356c
Emma Jin <emma@semgrep.com>
2024-03-21 18:01:06 -0700

    Update snapshots with masked values

--- cli/tests/conftest.py
+++ cli/tests/conftest.py
@@ -248,6 +248,8 @@ ALWAYS_MASK: Maskers = (
     re.compile(r"python (\d+[.]\d+[.]\d+[ ]+)"),
     re.compile(r'SEMGREP_SETTINGS_FILE="(.+?)"'),
     re.compile(r'SEMGREP_VERSION_CACHE_PATH="(.+?)"'),
+    re.compile(r"Using Semgrep Pro Version: (.*)"),
+    re.compile(r"Installed at (.*)"),
     # Dates
     re.compile(r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:(?:\.\d+)?Z)?"),
     # Hide any substring that resembles a temporary file path.
