b4c65d16e61b513e0b5d20d42a65e9d90bbac1a6
Brandon Wu <49291449+brandonspark@users.noreply.github.com>
2024-03-26 14:54:03 -0700

    refactor(lsp): explicit params to semgrep/search (#9916)
    ## What:
    This PR makes the parameters to the `semgrep/search` LSP command more
    explicit.
    
    Before, we were destructuring the JSON manually, which is error-prone.
    
    ## How:
    Introduced an explicit type for thep arams, and a `Request_params`
    module.
    
    Caveat, this will matter on the order of the `Assoc` that we receive,
    but this is what `ocaml-lsp` does too:
    
    https://github.com/ocaml/ocaml-lsp/blob/master/ocaml-lsp-server/src/custom_requests/req_wrapping_ast_node.ml#L8
    
    ## Test plan:
    Tested that the LSP search capability works manually.

--- src/osemgrep/language_server/custom_requests/Search.ml
+++ src/osemgrep/language_server/custom_requests/Search.ml
@@ -1,30 +1,55 @@
-open Yojson.Safe.Util
 open Lsp
 open Lsp.Types
-open Jsonrpc
 open Fpath_.Operators
 module Conv = Convert_utils
 module OutJ = Semgrep_output_v1_t
 
 let meth = "semgrep/search"
 
+(*****************************************************************************)
+(* Parameters *)
+(*****************************************************************************)
+
+module Request_params = struct
+  type t = { pattern : string; lang : Xlang.t option }
+
+  (* This schema means that it matters what order the arguments are in!
+     This is a little undesirable, but it's annoying to be truly
+     order-agnostic, and this is what `ocaml-lsp` also does.
+     https://github.com/ocaml/ocaml-lsp/blob/ad209576feb8127e921358f2e286e68fd60345e7/ocaml-lsp-server/src/custom_requests/req_wrapping_ast_node.ml#L8
+  *)
+  let of_jsonrpc_params params : t option =
+    match params with
+    | Some (`Assoc [ ("pattern", `String pattern); ("language", lang) ]) ->
+        let lang_opt =
+          match lang with
+          | `String lang -> Some (Xlang.of_string lang)
+          | _ -> None
+        in
+        Some { pattern; lang = lang_opt }
+    | __else__ -> None
+
+  let _of_jsonrpc_params_exn params : t =
+    match of_jsonrpc_params params with
+    | None -> failwith "expected jsonrpc schema matching search"
+    | Some res -> res
+end
+
+(*****************************************************************************)
+(* Entry point *)
+(*****************************************************************************)
+
 (** on a semgrep/search request, get the pattern and (optional) language params.
     We then try and parse the pattern in every language (or specified lang), and
     scan like normal, only returning the match ranges per file *)
 let on_request runner params =
-  match params with
+  match Request_params.of_jsonrpc_params params with
   | None -> None
   | Some params ->
-      let params = Structured.yojson_of_t params in
-      let pattern = params |> member "pattern" |> to_string in
-      let lang_opt = params |> member "language" |> to_string_option in
-      let xlang_opt =
-        Option.bind lang_opt (fun l -> Some (Xlang.of_string l))
-      in
       (* TODO: figure out why rules_from_rules_source_async hangs *)
       (* let src = Rules_source.(Pattern (pattern, xlang_opt, None)) in *)
       let rules_and_origins =
-        Rule_fetching.rules_from_pattern (pattern, xlang_opt, None)
+        Rule_fetching.rules_from_pattern (params.pattern, params.lang, None)
       in
       let rules, _ =
         Rule_fetching.partition_rules_and_errors rules_and_origins
