b27fdbf3ed7ff5d1c201da0af609d522c790012e
Heejong Lee <heejong@gmail.com>
2024-03-26 21:28:48 +0900

    Revert "Enable inter-file diff scan for all users (#9869)"
    This reverts commit ad2cf47892cc0dead53e9117da52972b9127ef3c.

--- cli/src/semgrep/commands/scan.py
+++ cli/src/semgrep/commands/scan.py
@@ -316,12 +316,6 @@ _scan_options: List[Callable] = [
         type=int,
         default=DEFAULT_DIFF_DEPTH,
     ),
-    optgroup.option(
-        "--no-interfile-diff-scan",
-        "disable_interfile_diff_scan_flag",
-        is_flag=True,
-        hidden=True,
-    ),
     optgroup.option("--dump-command-for-core", "-d", is_flag=True, hidden=True),
     optgroup.option(
         "--no-secrets-validation",
@@ -428,7 +422,6 @@ def scan(
     config: Optional[Tuple[str, ...]],
     debug: bool,
     diff_depth: int,
-    disable_interfile_diff_scan_flag: bool,
     dump_engine_path: bool,
     requested_engine: Optional[EngineType],
     run_secrets_flag: bool,
@@ -503,7 +496,7 @@ def scan(
     engine_type = EngineType.decide_engine_type(
         requested_engine=requested_engine,
         run_secrets=run_secrets_flag,
-        enable_pro_diff_scan=not disable_interfile_diff_scan_flag,
+        enable_pro_diff_scan=diff_depth >= 0,
     )
 
     # this is useful for our CI job to find where semgrep-core (or semgrep-core-proprietary)
