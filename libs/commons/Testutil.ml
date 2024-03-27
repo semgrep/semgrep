6806e993d4797b7abf68b931e68e2adeb15364bd
Martin Jambon <martin@semgrep.com>
2024-03-27 12:19:20 -0700

    Use Testutil.mask_temp_paths instead of Testo.mask_temp_paths so as to mask temporary paths obtained after applying Unix.realpath which is not available until ocaml 4.13. See https://github.com/semgrep/testo/pull/42

--- libs/commons/Testutil.ml
+++ libs/commons/Testutil.ml
@@ -6,3 +6,12 @@
 let run what f =
   UPrintf.printf "running %s...\n%!" what;
   Common.protect ~finally:(fun () -> UPrintf.printf "done with %s.\n%!" what) f
+
+let mask_temp_paths ?depth ?replace () =
+  let mask_original_path = Testo.mask_temp_paths ?depth ?replace () in
+  let mask_physical_path =
+    Testo.mask_temp_paths ?depth ?replace
+      ~tmpdir:(UFilename.get_temp_dir_name () |> UUnix.realpath)
+      ()
+  in
+  fun text -> text |> mask_original_path |> mask_physical_path
