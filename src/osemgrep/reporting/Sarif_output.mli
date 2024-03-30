420bf3719ea8aa23b20d6df72e9842a182e175c7
Amarin Phaosawasdi <amchiclet@users.noreply.github.com>
2024-03-30 10:50:19 -0700

    address code review

--- src/osemgrep/reporting/Sarif_output.mli
+++ src/osemgrep/reporting/Sarif_output.mli
@@ -1,6 +1,7 @@
+(* Formats the CLI output to the SARIF format. *)
 val sarif_output :
-  bool ->
-  string ->
+  hide_nudge:bool ->
+  engine_label:string ->
   Rule.hrules ->
   Semgrep_output_v1_t.cli_output ->
   Sarif.Sarif_v_2_1_0_t.sarif_json_schema
