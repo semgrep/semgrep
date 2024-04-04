f0d9340df8323409d65e44111536b6e7942bf5e2
brandonspark <wu.brandonj@gmail.com>
2024-04-04 13:49:02 -0700

    clean up convert utils

--- src/osemgrep/language_server/util/Convert_utils.ml
+++ src/osemgrep/language_server/util/Convert_utils.ml
@@ -8,12 +8,6 @@ let range_of_cli_match (m : OutJ.cli_match) =
       (Position.create ~line:(m.start.line - 1) ~character:(m.start.col - 1))
     ~end_:(Position.create ~line:(m.end_.line - 1) ~character:(m.end_.col - 1))
 
-let range_of_toks ((l1 : Tok.location), (l2 : Tok.location)) =
-  let line, col, _ = Tok.end_pos_of_loc l2 in
-  Range.create
-    ~start:(Position.create ~line:(l1.pos.line - 1) ~character:l1.pos.column)
-    ~end_:(Position.create ~line:(line - 1) ~character:col)
-
 let convert_severity (severity : OutJ.match_severity) : DiagnosticSeverity.t =
   match severity with
   | `Error -> Error
--- src/osemgrep/language_server/util/Convert_utils.mli
+++ src/osemgrep/language_server/util/Convert_utils.mli
@@ -1,11 +1,6 @@
 val range_of_cli_match : Semgrep_output_v1_t.cli_match -> Lsp.Types.Range.t
 (** [range_of_cli_match cli_match] returns the lsp range of the given cli_match. *)
 
-(* This is meant to be used with Pattern_match.range_locs, where these two tokens
-   may be the same.
-*)
-val range_of_toks : Tok.location * Tok.location -> Lsp.Types.Range.t
-
 val convert_severity :
   Semgrep_output_v1_t.match_severity -> Lsp.Types.DiagnosticSeverity.t
 (** [convert_severity s] returns the lsp severity corresponding to the semgrep severity [s]. *)
