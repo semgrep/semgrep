b614f36dfe2afa2519a6c399068dca53b93e73ed
Emma Jin <emma@semgrep.com>
2024-04-05 14:36:11 -0700

    Finish addressing review comments

--- libs/tracing/Tracing.mli
+++ libs/tracing/Tracing.mli
@@ -55,6 +55,10 @@ val configure_tracing : string -> unit
     backend with threads, HTTP connections, etc. when called *)
 
 val with_tracing :
-  string -> (string * Trace_core.user_data) list -> (span -> 'a) -> 'a
+  string ->
+  (string * Trace_core.user_data) list ->
+  string option ->
+  (span -> 'a) ->
+  'a
 (** Setup instrumentation and run the passed function.
    Stops instrumenting once that function is finished. *)
