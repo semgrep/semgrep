01b1f31fc8250d25cb2a96f6faead763fa8123a0
Amarin Phaosawasdi <amchiclet@users.noreply.github.com>
2024-04-16 09:43:14 -0700

    refactor: pull function out of formatter/json.py
    The function `rule_match_to_CliMatch` will be used in when preparing
    arguments to osemgrep's RPC call for SARIF formatting (a separate
    follow up PR), so this PR pulls it out to base.py

--- cli/src/semgrep/formatter/base.py
+++ cli/src/semgrep/formatter/base.py
@@ -12,6 +12,40 @@ from semgrep.rule import Rule
 from semgrep.rule_match import RuleMatch
 
 
+def rule_match_to_CliMatch(rule_match: RuleMatch) -> out.CliMatch:
+    extra = out.CliMatchExtra(
+        message=rule_match.message,
+        metadata=out.RawJson(rule_match.metadata),
+        severity=rule_match.severity,
+        fingerprint=rule_match.match_based_id,
+        # 'lines' already contains '\n' at the end of each line
+        lines="".join(rule_match.lines).rstrip(),
+        metavars=rule_match.match.extra.metavars,
+        dataflow_trace=rule_match.dataflow_trace,
+        engine_kind=rule_match.match.extra.engine_kind,
+        validation_state=rule_match.match.extra.validation_state,
+    )
+
+    if rule_match.extra.get("sca_info"):
+        extra.sca_info = rule_match.extra.get("sca_info")
+    if rule_match.extra.get("fixed_lines"):
+        extra.fixed_lines = rule_match.extra.get("fixed_lines")
+    if rule_match.fix is not None:
+        extra.fix = rule_match.fix
+    if rule_match.is_ignored is not None:
+        extra.is_ignored = rule_match.is_ignored
+    if rule_match.extra.get("extra_extra"):
+        extra.extra_extra = out.RawJson(rule_match.extra.get("extra_extra"))
+
+    return out.CliMatch(
+        check_id=out.RuleId(rule_match.rule_id),
+        path=out.Fpath(str(rule_match.path)),
+        start=rule_match.start,
+        end=rule_match.end,
+        extra=extra,
+    )
+
+
 class BaseFormatter(abc.ABC):
     def output(
         self,
