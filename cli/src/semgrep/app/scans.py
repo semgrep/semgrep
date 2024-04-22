3c045af0de127ceb992edeec3954351db0b35504
Alexis Grant <alexis@r2c.dev>
2024-04-22 15:28:01 -0700

    fix spacing in message addition

--- cli/src/semgrep/app/scans.py
+++ cli/src/semgrep/app/scans.py
@@ -214,8 +214,8 @@ class ScanHandler:
 
         if response.status_code == 401:
             logger.info(
-                "API token not valid. Try to run `semgrep logout` and `semgrep login` again. \
-                 Or in CI, ensure your SEMGREP_APP_TOKEN variable is set correctly.",
+                "API token not valid. Try to run `semgrep logout` and `semgrep login` again."
+                "Or in CI, ensure your SEMGREP_APP_TOKEN variable is set correctly.",
             )
             sys.exit(INVALID_API_KEY_EXIT_CODE)
 
