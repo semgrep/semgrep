e5ba29cd69e907dff9e50c1860f365d13601c130
Martin Jambon <martin@r2c.dev>
2024-04-23 13:20:15 -0700

    Add options to 'with_temp_file' (#10136)
    This is for https://github.com/semgrep/semgrep/pull/10131
    
    * `~str` is now `~contents` for clarity.
    * `~ext` was replaced by `~suffix` so that we don't have to worry about
    whether the period is included (in fact we had a bug like this where it
    was `~ext:".ml"` instead of `~ext:"ml"`).
    * temporary file names are now prefixed by the name of the current
    program.
    * I migrated `UTmp.Legacy` to `Fpath`.
    * `new_temp_file` now takes the same arguments as `with_temp_file`.
    
    semgrep-pro PR: https://github.com/semgrep/semgrep-proprietary/pull/1447

--- libs/commons/CapTmp.ml
+++ libs/commons/CapTmp.ml
@@ -1,7 +1,11 @@
-let with_temp_file _caps = UTmp.with_temp_file
+let with_temp_file ?contents ?persist ?prefix ?suffix ?temp_dir _caps f =
+  UTmp.with_temp_file ?contents ?persist ?prefix ?suffix ?temp_dir f
+
 let get_temp_dir_name _caps = UTmp.get_temp_dir_name ()
 let erase_temp_files _caps = UTmp.erase_temp_files ()
-let new_temp_file ?temp_dir _caps = UTmp.new_temp_file ?temp_dir
+
+let new_temp_file ?prefix ?suffix ?temp_dir _caps =
+  UTmp.new_temp_file ?prefix ?suffix ?temp_dir ()
 
 let replace_named_pipe_by_regular_file_if_needed _caps =
   UTmp.replace_named_pipe_by_regular_file_if_needed
