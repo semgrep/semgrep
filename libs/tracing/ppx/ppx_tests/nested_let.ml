62259432a9aec65339c0e28dc864d1ec88d5be92
Emma Jin <emma@semgrep.com>
2024-04-25 14:37:38 -0700

    chore: make tracing tests build with dune
    Test plan: `make unused-libs` shouldn't cause any errors related to Tracing

--- libs/tracing/ppx/ppx_tests/nested_let.ml
+++ libs/tracing/ppx/ppx_tests/nested_let.ml
@@ -2,7 +2,9 @@ let top a b =
   let add_one a = a + 1 [@@trace] in
   add_one a + b
 
+let add_data_to_span _sp _data = ()
+
 let top2 a b =
   let%trace sp = "example" in
   a + 1;
-  Tracing.add_data_to_span sp [ ("a", `Int 1) ]
+  add_data_to_span sp [ ("a", `Int 1) ]
