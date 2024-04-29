44a6043ce66d7955fe1185304d3564f1c13f4102
Andre Kuhlenschmidt <andre@semgrep.com>
2024-04-29 16:50:18 -0700

    fix test

--- tests/rules/struct_tags.go
+++ tests/rules/struct_tags.go
@@ -2,7 +2,9 @@ package main
 
 type Rectangle struct {
     Top     int `json:"top"`
+    //ruleid: match	
     Left     int `json:"left" bencode:"foo"`
+    //ruleid: match	
     Width  int `bencode:"bar" json:"width"`
     Height int `json:"height"`
 }
