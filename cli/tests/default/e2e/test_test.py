ebabff531686c2a132a64ecc367d29442985b703
pad <pad@r2c.dev>
2024-05-01 15:53:39 +0200

    cleanup cli/tests/default/e2e/test_test.py (was test_cli_test.py)
    Use better rule and target names, add comments,
    move the test to the appropriate file, etc.
    
    test plan:
    cd cli; make e2e

--- cli/tests/default/e2e/test_test.py
+++ cli/tests/default/e2e/test_test.py
@@ -1,6 +1,16 @@
-# Tests for 'semgrep test' (accessibly also as 'semgrep scan --test').
-# See https://semgrep.dev/docs/writing-rules/testing-rules/
-# See also test_fixtest.py for the autofix + 'semgrep test' tests.
+# Tests for 'semgrep --test' (accessible also now via 'osemgrep test').
+# See https://semgrep.dev/docs/writing-rules/testing-rules/ for more info.
+#
+# See also test_fixtest.py for the autofix "fixtest" tests.
+#
+# TODO:
+#  - test to detect wrong ruleid: in target file (missed an annotation)
+#    with passed=false in the JSON
+#  - test to detect invalid ruleid: (wrong ruleid syntax)
+#  - test to take a single directory and iterate over (what we do
+#    in semgrep-rules/), but harder given how run_semgrep_in_tmp() was
+#    designed with always a 'config' and a 'target_name' parameter
+#    (and the fact that the e2e rules and targets are in different dirs)
 import os
 import subprocess
 from pathlib import Path
@@ -12,12 +22,13 @@ from tests.semgrep_runner import SEMGREP_BASE_SCAN_COMMAND
 from semgrep.constants import OutputFormat
 
 
+# test the basic JSON output of --test reporting the passed checks
 @pytest.mark.kinda_slow
 def test_cli_test_basic(run_semgrep_in_tmp: RunSemgrep, snapshot):
     results, _ = run_semgrep_in_tmp(
-        "rules/cli_test/basic/",
+        "rules/cli_test/basic.yaml",
         options=["--test"],
-        target_name="cli_test/basic/",
+        target_name="cli_test/basic.py",
         output_format=OutputFormat.JSON,
     )
 
@@ -27,14 +38,34 @@ def test_cli_test_basic(run_semgrep_in_tmp: RunSemgrep, snapshot):
     )
 
 
+# The --test flag can accept a directory as a target
+# TODO: this actually does not really test the ability to iterate over
+# a directory (ability heavily used in semgrep-rules/scripts/run-test.sh)
 @pytest.mark.kinda_slow
+def test_cli_test_directory(run_semgrep_in_tmp: RunSemgrep, snapshot):
+    results, _ = run_semgrep_in_tmp(
+        "rules/cli_test/directory/",
+        options=["--test"],
+        target_name="cli_test/directory/",
+        output_format=OutputFormat.JSON,
+    )
+
+    snapshot.assert_match(
+        results,
+        "results.json",
+    )
+
+
+# TODO: adding "--timeout", "1", does not seem to speedup things
+@pytest.mark.slow
 @pytest.mark.osemfail
 def test_timeout(run_semgrep_in_tmp: RunSemgrep, snapshot):
     results, _ = run_semgrep_in_tmp(
-        "rules/cli_test/error/",
+        "rules/cli_test/rule_that_timeout.yaml",
         options=["--test"],
-        target_name="cli_test/error/",
+        target_name="cli_test/long.py",
         output_format=OutputFormat.JSON,
+        assert_exit_code=1,
     )
     snapshot.assert_match(
         results,
@@ -86,23 +117,6 @@ def test_cli_test_multiline_annotations(run_semgrep_in_tmp: RunSemgrep, snapshot
     )
 
 
-@pytest.mark.kinda_slow
-@pytest.mark.osemfail
-def test_parse_errors(run_semgrep_in_tmp: RunSemgrep, snapshot):
-    _results, errors = run_semgrep_in_tmp(
-        "rules/cli_test/parse_errors/",
-        options=["--verbose"],
-        target_name="cli_test/parse_errors/invalid_javascript.js",
-        output_format=OutputFormat.TEXT,
-        force_color=True,
-        strict=False,
-    )
-    snapshot.assert_match(
-        errors,
-        "errors.txt",
-    )
-
-
 @pytest.mark.slow
 def test_cli_test_from_entrypoint(snapshot):
     env = {}
@@ -131,7 +145,7 @@ def test_cli_test_from_entrypoint(snapshot):
 def test_cli_test_match_rules_same_message(run_semgrep_in_tmp: RunSemgrep, snapshot):
     results, _ = run_semgrep_in_tmp(
         "rules/cli_test/match_rules_same_message/rules.yml",
-        target_name="cli_test/basic/",
+        target_name="cli_test/basic.py",
         output_format=OutputFormat.TEXT,
         force_color=True,
     )
