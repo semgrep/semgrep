9ee744a8de5691e35c8d24bbd5825e8d92899548
brandonspark <wu.brandonj@gmail.com>
2024-05-02 13:55:45 -0700

    fix tests

--- tests/rules/as_metavariable.py
+++ tests/rules/as_metavariable.py
@@ -1,3 +1,8 @@
 
+# ruleid: as-metavariable
 def foo():
   qux()
+
+# ok: as-metavariable
+def foo():
+  qu()
--- tests/rules/as_metavariable_textual.py
+++ tests/rules/as_metavariable_textual.py
@@ -1,2 +1,6 @@
 
+# ruleid: as-metavariable-textual
 foo(bar)
+
+# ok: as-metavariable-textual
+foo(baz)
