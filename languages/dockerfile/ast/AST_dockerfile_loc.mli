ad1ee0b0ab3cc7f91c93574660f4a70a9bbdc835
Martin Jambon <martin@semgrep.com>
2024-05-10 18:28:29 -0700

    Update dockerfile parser step from tree-sitter CST to Dockerfile-specific AST, including support for heredocs.

--- languages/dockerfile/ast/AST_dockerfile_loc.mli
+++ languages/dockerfile/ast/AST_dockerfile_loc.mli
@@ -1,11 +1,14 @@
-(* Location extraction for different AST_dockerfile constructs *)
+(* Location extraction for different AST_dockerfile constructs
+
+   All functions end either in '_loc' or '_tok'.
+*)
 
 val docker_string_fragment_loc :
   AST_dockerfile.docker_string_fragment -> Tok_range.t
 
 val docker_string_loc : AST_dockerfile.docker_string -> Tok_range.t
 val instruction_loc : AST_dockerfile.instruction -> Tok_range.t
-val argv_or_shell_loc : AST_dockerfile.argv_or_shell -> Tok_range.t
+val command_loc : AST_dockerfile.command -> Tok_range.t
 val param_loc : AST_dockerfile.param -> Tok_range.t
 val image_spec_loc : AST_dockerfile.image_spec -> Tok_range.t
 val label_pair_loc : AST_dockerfile.label_pair -> Tok_range.t
@@ -13,4 +16,6 @@ val expose_port_loc : AST_dockerfile.expose_port -> Tok_range.t
 val str_or_ellipsis_loc : AST_dockerfile.str_or_ellipsis -> Tok_range.t
 val array_or_paths_loc : AST_dockerfile.array_or_paths -> Tok_range.t
 val healthcheck_loc : AST_dockerfile.healthcheck -> Tok_range.t
+val wrap_tok : 'a * Tok.t -> Tok.t
 val wrap_loc : 'a * Tok.t -> Tok_range.t
+val var_or_metavar_loc : AST_dockerfile.var_or_metavar -> Tok_range.t
