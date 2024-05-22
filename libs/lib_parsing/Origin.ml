73100928340685d329cf5b7b332cf86d6d83aaf7
Yoann Padioleau <pad@returntocorp.com>
2024-05-22 17:41:42 +0200

    server: fix make server (semgrep/semgrep-proprietary#1568)
    Lots of API change in semgrep in the last 6 months so
    we need to update the server code to those latest changes
    (this is why we want a monorepo ...)
    
    test plan:
    make server
    
    synced from Pro a6f2f97003f6826f8e16fe35f303f5aeced790d4

--- libs/lib_parsing/Origin.ml
+++ libs/lib_parsing/Origin.ml
@@ -1,6 +1,6 @@
 (* Cooper Pierce
  *
- * Copyright (c) Semgrep Inc.
+ * Copyright (c) 2023, Semgrep Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
--- src/osemgrep/reporting/Cli_json_output.mli
+++ src/osemgrep/reporting/Cli_json_output.mli
@@ -1,4 +1,6 @@
-(* entry point *)
+(* entry point
+ * TODO: what is dryrun for?? for autofix?
+ *)
 val cli_output_of_core_results :
   dryrun:bool ->
   logging_level:Logs.level option ->
@@ -11,7 +13,7 @@ val cli_output_of_core_results :
 (* internals used in Scan_subcommant.ml *)
 val exit_code_of_error_type : Semgrep_output_v1_t.error_type -> Exit_code.t
 
-(* internals used also for incremental display of matches The Fixed_lines.env is
+(* internals used also for incremental display of matches. The Fixed_lines.env is
  * used for deciding whether a fixed_lines elements is included in the
  * cli_match. This depends on whether an overlapping fix was already included in
  * an earlier cli_match in the same list of matches. *)
