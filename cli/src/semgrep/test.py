c799c909c7bd21927bc054e8c45a75443637d062
Brendon Go <brendon.go@gmail.com>
2024-05-29 15:25:30 -0400

    fixup! Add comments
    synced from Pro 3bfb8f7a1f066aba9a1d39a6b8076d86a3b4d343

--- cli/src/semgrep/test.py
+++ cli/src/semgrep/test.py
@@ -1,3 +1,16 @@
+"""
+For each directory containing YAML rules, run those rules on the file in the
+same directory with the same name but different extension.
+E.g. eqeq.yaml runs on eqeq.py.
+Validate that the output is annotated in the source file with by looking for a
+comment like:
+
+ ```
+ # ruleid:eqeq-is-bad
+ ```
+ On the preceeding line.
+
+ """
 import collections
 import difflib
 import functools
