a66dfe32b2004e36cae5e1d9b974d8a65b741112
Lewis <LewisArdern@live.co.uk>
2024-06-05 14:20:02 -0700

    Update test_clean_project_url.py
    Making it clear its not a real token, just incase people report it.

--- cli/tests/default/unit/test_clean_project_url.py
+++ cli/tests/default/unit/test_clean_project_url.py
@@ -7,6 +7,7 @@ from semgrep.git import clean_project_url
 def test_git_url_clean():
     assert (
         clean_project_url(
+            # This is not a real token
             "https://gitlab-ci-token:glcbt-64_wFuiRFQk9t841JHKQnAT@gitlab.company.world/app/test-case.git"
         )
         == "https://gitlab.company.world/app/test-case.git"
