1c58268e8ae4c7cc6bfb7acf0ff86e203cfc7588
Yoann Padioleau <pad@returntocorp.com>
2024-07-07 15:28:56 +0200

    cleanup Main.ml and Pro_main.ml (semgrep/semgrep-proprietary#1785)
    Be more consistent
    
    test plan:
    make
    make test
    
    synced from Pro 8c52b9baca442315aefded47e0004a611d1d9615

--- libs/commons/CapConsole.ml
+++ libs/commons/CapConsole.ml
@@ -1,2 +1,3 @@
 let print _cap = UConsole.print
+let eprint _cap = UConsole.eprint
 let ocolor_format_printf _cap = Ocolor_format.printf
--- libs/commons/CapConsole.mli
+++ libs/commons/CapConsole.mli
@@ -1,5 +1,8 @@
 (* Print a string, print a newline, and flush the stdout channel. *)
 val print : Cap.Console.stdout -> string -> unit
 
+(* Print a string, print a newline, and flush the stderr channel. *)
+val eprint : Cap.Console.stderr -> string -> unit
+
 val ocolor_format_printf :
   Cap.Console.stdout -> ('b, Format.formatter, unit) format -> 'b
