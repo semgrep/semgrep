b2dcaee52c74561d9acc177cd46b13163afd40e0
Katrina Liu <84615241+liukatkat@users.noreply.github.com>
2024-07-08 13:04:44 -0600

    chore: fix masking function to mask more than 2 digits (semgrep/semgrep-proprietary#1791)
    Currently, we only mask timestamps that match this regex
    `{|\[([0-9]{2}\.[0-9]{2})\]|}`, but if running `make test` takes a
    longer time, the timestamp might not be `<2 digits>.<2 digits>`. This PR
    fixes the regex so that we match timestamps with more digits, (e.g.
    `[732.12]`).
    
    Test plan: All existing tests should still pass.
    
    synced from Pro cb42e5b599e2cbe7fcc347c216e060a620c1f05f

--- libs/commons/Testutil_logs.ml
+++ libs/commons/Testutil_logs.ml
@@ -4,9 +4,9 @@
 let mask_time =
   Testo.mask_pcre_pattern
     ~replace:(fun _ -> "<MASKED TIMESTAMP>")
-    {|\[([0-9]{2}\.[0-9]{2})\]|}
+    {|\[([0-9]+\.[0-9]{2})\]|}
 
 let mask_log_lines =
   Testo.mask_pcre_pattern
     ~replace:(fun _ -> "<MASKED LOG LINE>")
-    {|\[[0-9]{2}\.[0-9]{2}\][^\n]*|}
+    {|\[[0-9]+\.[0-9]{2}\][^\n]*|}
