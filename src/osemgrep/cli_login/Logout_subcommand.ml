d7752604a288bec93db658005ece0bd2bb385e06
Yoann Padioleau <pad@returntocorp.com>
2024-07-11 17:18:23 +0200

    split test_login.py in separate testing functions (semgrep/semgrep-proprietary#1825)
    One test function per feature test
    
    test plan:
    make e2e
    
    synced from Pro 91f674447aad90340af93e65065f655f0ed12b40

--- src/osemgrep/cli_login/Logout_subcommand.ml
+++ src/osemgrep/cli_login/Logout_subcommand.ml
@@ -8,8 +8,9 @@
 (*****************************************************************************)
 (* Types *)
 (*****************************************************************************)
-(* TODO: actually even stdout is not used, we abuse Logs.app *)
 type caps = < Cap.stdout >
+
+let logout_message = "Logged out (log back in with `semgrep login`)"
 
 (*****************************************************************************)
 (* Main logic *)
@@ -17,25 +18,21 @@ type caps = < Cap.stdout >
 
 (* All the business logic after command-line parsing. Return the desired
    exit code. *)
-let run_conf (_caps : caps) (conf : Logout_CLI.conf) : Exit_code.t =
+let run_conf (caps : caps) (conf : Logout_CLI.conf) : Exit_code.t =
   CLI_common.setup_logging ~force_color:false ~level:conf.common.logging_level;
   Logs.debug (fun m -> m "conf = %s" (Logout_CLI.show_conf conf));
   let settings = Semgrep_settings.load ~include_env:false () in
   match settings.Semgrep_settings.api_token with
   | None ->
-      Logs.app (fun m ->
-          m "%s You are not logged in! This command had no effect."
-            (Console.warning_tag ()));
+      Logs.warn (fun m ->
+          m "You are not logged in! This command had no effect.");
+      CapConsole.print caps#stdout logout_message;
       Exit_code.ok ~__LOC__
   | Some _ ->
       let settings = Semgrep_settings.{ settings with api_token = None } in
       if Semgrep_settings.save settings then (
-        let message =
-          Ocolor_format.asprintf
-            {|%s Logged out! You can log back in with @{<cyan>`semgrep login`@}|}
-            (Console.success_tag ())
-        in
-        Logs.app (fun m -> m "%s" message);
+        (* old: {|%s Logged out! You can log back in with @{<cyan>`semgrep login`@}|} *)
+        CapConsole.print caps#stdout logout_message;
         Exit_code.ok ~__LOC__)
       else Exit_code.fatal ~__LOC__
 
