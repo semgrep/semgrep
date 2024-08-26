fcb940dc26dc30823226862dd1e5d7019205bea5
Yoann Padioleau <pad@returntocorp.com>
2024-08-26 17:26:46 +0200

    New Unit_interfile.ml extracted from Unit_steps_scan.ml (semgrep/semgrep-proprietary#2097)
    Most of the testing code in Unit_steps_scan.ml was not
    dependent of steps_scan/ and with the previous PR
    splitting the multisteps tests out of the interfile tests,
    we can now move most of the interfile test code closer
    to deep_scan/
    
    test plan:
    make test
    
    synced from Pro bcec0e287fa45ca1b479f4664e81b1fc61343e86

--- src/engine/tests/Test_compare_matches.mli
+++ src/engine/tests/Test_compare_matches.mli
@@ -5,10 +5,13 @@ val location_of_core_error : Core_error.t -> Fpath.t * int
 
 (* extract all the lines with ERROR: comment in test files *)
 val expected_error_lines_of_files :
-  ?regexp:string ->
+  ?regexp:string (* default to default_error_regexp below *) ->
   ?ok_regexp:string option ->
   Fpath.t list ->
   (Fpath.t * int) (* line with ERROR *) list
+
+(* ".*\\(ERROR\\|MATCH\\):" *)
+val default_error_regexp : string
 
 (* Return the number of errors and an error message, if there's any error. *)
 val compare_actual_to_expected :
