61a9c0860e37890aaaf065394388c17a6c89ea5c
maxmysten <max.spector@mystenlabs.com>
2024-09-02 04:28:45 -0400

    LSP.js moved in b04f3e74141c8841c57bdbbdf8c4e01ca121d8e7 so removing changes

--- js/languages/move_on_sui/tests/index.test.js
+++
@@ -1,9 +0,0 @@
-const { createParser, testParser } = require("../../shared/parser");
-
-const LANG = "move_on_sui";
-
-describe(`${LANG} parser`, () => {
-  const parserPromise = createParser(`${__dirname}/../dist/index.cjs`);
-
-  testParser(LANG, parserPromise);
-});
