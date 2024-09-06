85f981aa30b51cc228c18afbc78bef17f62e5c5b
SayyantR <124740176+SayyantR@users.noreply.github.com>
2024-09-06 16:00:56 -0700

    feat(sca): Added pre-release comparisons to golang version comparison logic (semgrep/semgrep-proprietary#2025)
    synced from Pro 79943b9974d688447f3bc828b17544820e090858

--- cli/tests/default/e2e-other/test_golang_version.py
+++ cli/tests/default/e2e-other/test_golang_version.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import List
 from typing import Optional
 from typing import Tuple
 
@@ -6,8 +7,10 @@ import pytest
 
 from semdep.golang_version import cmp_core
 from semdep.golang_version import cmp_golang_versions
+from semdep.golang_version import cmp_pre_release
 from semdep.golang_version import cmp_pseudo
 from semdep.golang_version import compare_golang_specifier
+from semdep.golang_version import GolangPreReleaseVersion
 from semdep.golang_version import GolangPseudoVersion
 from semdep.golang_version import GolangVersionCore
 from semdep.golang_version import parse_golang_core
@@ -34,6 +37,11 @@ from semdep.golang_version import ParsedGolangVersion
             ),
             False,
         ),
+        (("0.0.0-20190620085101-78d2af792bab", ">=0.20.0-alpha.0"), False),
+        (("0.0.0-20190620085101-78d2af792bab", "<0.20.0-alpha.2"), True),
+        (("0.20.0-alpha.0", ">=0.20.0-alpha.0"), True),
+        (("0.2.0", ">=0.20.0-alpha.0"), False),
+        (("1.0.0-1.a", ">=1.0.0-2"), False),
     ],
 )
 def test_golang_version_comparison(versions: Tuple[str, str], results: bool):
@@ -69,28 +77,74 @@ def test_parse_pseudo_datetime():
     [
         (
             "0.0.0-20220225172249-27dd8689420f",
-            (0, 0, 0, datetime(2022, 2, 25, 17, 22, 49), "27dd8689420f"),
+            (
+                0,
+                0,
+                0,
+                datetime(
+                    2022,
+                    2,
+                    25,
+                    17,
+                    22,
+                    49,
+                ),
+                "27dd8689420f",
+                None,
+            ),
         ),
-        ("0.17.0", (0, 17, 0, None, None)),
+        ("0.17.0", (0, 17, 0, None, None, None)),
         (
             "0.1.1-0.20221104162952-702349b0e862",
-            (0, 1, 1, datetime(2022, 11, 4, 16, 29, 52), "702349b0e862"),
+            (0, 1, 1, None, None, ["0", "20221104162952-702349b0e862"]),
+        ),
+        ("1.3.7-alpha.0", (1, 3, 7, None, None, ["alpha", "0"])),
+        ("2.1.4-beta.3", (2, 1, 4, None, None, ["beta", "3"])),
+        ("0-alpha", (0, 0, 0, None, None, ["alpha"])),
+        (
+            "0.0.0-alpha-pre.0.x",
+            (0, 0, 0, None, None, ["alpha-pre", "0", "x"]),
+        ),
+        (
+            "0.0.0-alpha-pre.beta-another-identifier.3.x",
+            (
+                0,
+                0,
+                0,
+                None,
+                None,
+                ["alpha-pre", "beta-another-identifier", "3", "x"],
+            ),
         ),
     ],
 )
 def test_parse_golang_version(
     raw_version: str,
-    components: Tuple[int, int, int, Optional[datetime], Optional[str]],
+    components: Tuple[
+        int,
+        int,
+        int,
+        Optional[datetime],
+        Optional[str],
+        Optional[List[str]],
+    ],
 ):
-    major, minor, patch, timestamp, commit_hash = components
+    (major, minor, patch, timestamp, commit_hash, pre_release_identifiers) = components
     parsed_golang_core = GolangVersionCore(major=major, minor=minor, patch=patch)
     parsed_golang_pseudo = None
+    parsed_golang_pre_release = None
     if timestamp and commit_hash:
         parsed_golang_pseudo = GolangPseudoVersion(
             timestamp=timestamp, commit_hash=commit_hash
         )
+    elif pre_release_identifiers is not None:
+        parsed_golang_pre_release = GolangPreReleaseVersion(
+            pre_release_identifiers=pre_release_identifiers
+        )
     parsed_golang_version = ParsedGolangVersion(
-        core=parsed_golang_core, pseudo=parsed_golang_pseudo
+        core=parsed_golang_core,
+        pseudo=parsed_golang_pseudo,
+        pre_release=parsed_golang_pre_release,
     )
     assert parse_golang_version(raw_version) == parsed_golang_version
 
@@ -145,6 +199,69 @@ def test_cmp_pseudo(
 ):
     parsed_version_pseudo, specifier_version_pseudo = pseudos
     assert cmp_pseudo(parsed_version_pseudo, specifier_version_pseudo) == diff
+
+
+@pytest.mark.quick
+@pytest.mark.parametrize(
+    "pre_releases,diff",
+    [
+        (
+            (
+                GolangPreReleaseVersion(["alpha", "0"]),
+                GolangPreReleaseVersion(["alpha", "0"]),
+            ),
+            0,
+        ),
+        (
+            (
+                GolangPreReleaseVersion(["beta", "3"]),
+                GolangPreReleaseVersion(["beta", "0"]),
+            ),
+            3,
+        ),
+        (
+            (
+                GolangPreReleaseVersion(["alpha", "0"]),
+                GolangPreReleaseVersion(["alpha", "3"]),
+            ),
+            -3,
+        ),
+        (
+            (
+                GolangPreReleaseVersion(["alpha", "0"]),
+                GolangPreReleaseVersion(["beta", "0"]),
+            ),
+            -1,
+        ),
+        (
+            (
+                GolangPreReleaseVersion(["beta"]),
+                GolangPreReleaseVersion(["alpha"]),
+            ),
+            1,
+        ),
+        (
+            (
+                GolangPreReleaseVersion(["alpha", "incompatible"]),
+                GolangPreReleaseVersion(["alpha", "0"]),
+            ),
+            1,
+        ),
+        (
+            (
+                GolangPreReleaseVersion(["alpha+incomplete"]),
+                GolangPreReleaseVersion(["alpha", "0"]),
+            ),
+            1,
+        ),
+    ],
+)
+def test_cmp_golang_pre_release(
+    pre_releases: Tuple[GolangPreReleaseVersion, GolangPreReleaseVersion],
+    diff: int,
+):
+    parsed_pre_release, specifier_pre_release = pre_releases
+    assert cmp_pre_release(parsed_pre_release, specifier_pre_release) == diff
 
 
 @pytest.mark.quick
