f2f63e7e32484456903bd648183294fee6e124d4
Sal Olivares <salolivares@users.noreply.github.com>
2024-09-13 12:39:50 -0700

    feat(sca): improve lockfile and manifest matching  (semgrep/semgrep-proprietary#2126)
    Refactor the existing lockfile/manifest matching code to allow for easy 
    support for pattern based matching in the future. This PR should have 
    no effect on functionality.
    
    synced from Pro 6b7f4cd595769643cf32fb91875e923158f33458

---
+++ cli/src/semdep/parsers/cargo.py
@@ -0,0 +1,32 @@
+from typing import Generator
+from typing import Optional
+
+from semgrep.semgrep_interfaces.semgrep_output_v1 import Cargo
+from semgrep.semgrep_interfaces.semgrep_output_v1 import Ecosystem
+from semgrep.semgrep_interfaces.semgrep_output_v1 import FoundDependency
+from semgrep.semgrep_interfaces.semgrep_output_v1 import Transitivity
+from semgrep.semgrep_interfaces.semgrep_output_v1 import Unknown
+
+
+def parse_cargo(
+    lockfile_text: str, manifest_text: Optional[str]
+) -> Generator[FoundDependency, None, None]:
+    def parse_dep(s: str) -> FoundDependency:
+        lines = s.split("\n")[1:]
+        dep = lines[0].split("=")[1].strip()[1:-1]
+        version = lines[1].split("=")[1].strip()[1:-1]
+        if len(lines) >= 4 and lines[3].startswith("checksum"):
+            hash = {"sha256": [lines[3].split("=")[1].strip()[1:-1]]}
+        else:
+            hash = {}
+        return FoundDependency(
+            package=dep,
+            version=version,
+            ecosystem=Ecosystem(Cargo()),
+            resolved_url=None,
+            allowed_hashes=hash,
+            transitivity=Transitivity(Unknown()),
+        )
+
+    deps = lockfile_text.split("[[package]]")[1:]
+    yield from (parse_dep(dep) for dep in deps)
