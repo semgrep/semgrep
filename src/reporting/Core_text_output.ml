388f90bc1d0ccdc51bca87a4a7a8d2b69b9766be
Nat Mote <nat@semgrep.com>
2024-09-18 09:24:55 -0700

    refactor: Use core_match to produce semgrep-core text output (semgrep/semgrep-proprietary#2270)
    This text output is for development purposes only, but we also use it in
    e2e tests. Currently, we print matches as they occur, which means that
    we can't sort or deduplicate them. Because we print them before sorting,
    otherwise inconsequential changes in how we compute findings can disturb
    the e2e test output.
    
    It would also be nice to have this output better match what actually
    gets sent to end users.
    
    For these reasons, and to improve code cleanliness, I would like to
    print the text output at the same place where we print the JSON output,
    if JSON output is requested. However, our current sorting and
    deduplication operates over `core_match`.
    
    So, in preparation for that change, I am migrating this code to print
    the output based on `core_match` rather than `Pattern_match.t`.
    
    Test plan:
    * Automated tests.
    * In `tests/interfile/targeting/single_file`, run
    `semgrep-core-proprietary -deep_inter_file -l java -rules deep.yaml
    User.java` and observe no differences in the output, including in the
    taint trace.
    
    synced from Pro b500ded8ba04f1302003379d413cbeafe91d8ac4

--- src/reporting/Core_text_output.ml
+++ src/reporting/Core_text_output.ml
@@ -14,6 +14,8 @@
  * LICENSE for more details.
  *)
 open Common
+open Fpath_.Operators
+module Out = Semgrep_output_v1_j
 
 (*****************************************************************************)
 (* Prelude *)
@@ -50,83 +52,68 @@ let rec join_with_space_if_needed xs =
 (* Print_aux *)
 (*****************************************************************************)
 
-let print_match_toks ?(str = "") ?(spaces = 0) (caps : < Cap.stdout >)
-    (ii : Tok.t list) : unit =
+let print_match_lines ?(str = "") ?(spaces = 0) (caps : < Cap.stdout >)
+    (file : Fpath.t) (start_line : int) (end_line : int) : unit =
   let print = CapConsole.print caps#stdout in
-  try
-    let mini, maxi = Tok_range.min_max_toks_by_pos ii in
-    let end_line, _, _ = Tok.end_pos_of_loc (Tok.unsafe_loc_of_tok maxi) in
-    let file, line = (Tok.file_of_tok mini, Tok.line_of_tok mini) in
-    let prefix = spf "%s:%d" file line in
-    let lines_str =
-      UFile.lines_of_file (Tok.line_of_tok mini, end_line) (Fpath.v file)
-    in
-    let prefix = if str = "" then prefix else prefix ^ " " ^ str in
-    let spaces_string = String.init spaces (fun _ -> ' ') in
-    print (spaces_string ^ prefix);
-    (* todo? some context too ? *)
-    lines_str |> List.iter (fun s -> print (spaces_string ^ " " ^ s))
-  with
-  | Failure "get_pos: Ab or FakeTok" ->
-      print "<could not locate match, FakeTok or AbstractTok>"
+  let prefix = spf "%s:%d" !!file start_line in
+  let lines_str = UFile.lines_of_file (start_line, end_line) file in
+  let prefix = if str = "" then prefix else prefix ^ " " ^ str in
+  let spaces_string = String.init spaces (fun _ -> ' ') in
+  print (spaces_string ^ prefix);
+  (* todo? some context too ? *)
+  lines_str |> List.iter (fun s -> print (spaces_string ^ " " ^ s))
 
-let print_intermediate_vars ~spaces (caps : < Cap.stdout >) (toks : Tok.t list)
-    : unit =
+let print_intermediate_vars ~spaces (caps : < Cap.stdout >)
+    (vars : Out.match_intermediate_var list) : unit =
   let spaces_string = String.init spaces (fun _ -> ' ') in
   let print str = CapConsole.print caps#stdout (spaces_string ^ str) in
   let rec loop_print curr_file = function
     | [] -> ()
-    | tok :: toks -> (
-        match Tok.loc_of_tok tok with
-        | Error _ ->
-            (* The toks are supposed to be real toks, so this should not happen, but
-             * it would be better to have a list of locs then. *)
-            ()
-        | Ok loc ->
-            let pos : Pos.t = loc.pos in
-            if pos.file <> curr_file then print pos.file;
-            print (spf "- %s @l.%d" loc.str pos.line);
-            loop_print pos.file toks)
+    | (var : Out.match_intermediate_var) :: vars ->
+        if Some var.location.path <> curr_file then print !!(var.location.path);
+        print (spf "- %s @l.%d" var.content var.location.start.line);
+        loop_print (Some var.location.path) vars
   in
-  loop_print "<NO FILE>" toks
+  loop_print None vars
 
 let rec print_taint_call_trace (caps : < Cap.stdout >) ~spaces = function
-  | Pattern_match.Toks toks -> print_match_toks caps ~spaces toks
-  | Call { call_toks; intermediate_vars; call_trace } ->
+  | Out.CliLoc (loc, _content) ->
+      print_match_lines caps ~spaces loc.path loc.start.line loc.end_.line
+  | CliCall ((loc, _content), intermediate_vars, call_trace) ->
       let print = CapConsole.print caps#stdout in
       let spaces_string = String.init spaces (fun _ -> ' ') in
       print (spaces_string ^ "call to");
-      print_match_toks caps ~spaces call_toks;
+      print_match_lines caps ~spaces loc.path loc.start.line loc.end_.line;
       if intermediate_vars <> [] then (
         print (spf "%sthese intermediate values are tainted:" spaces_string);
         print_intermediate_vars caps ~spaces:(spaces + 2) intermediate_vars);
       print (spaces_string ^ "then");
       print_taint_call_trace caps ~spaces:(spaces + 2) call_trace
 
-let print_taint_trace (caps : < Cap.stdout >) taint_trace =
-  taint_trace |> Lazy.force
-  |> List.iteri (fun idx { Pattern_match.source_trace; tokens; sink_trace } ->
-         let print = CapConsole.print caps#stdout in
-         if idx =*= 0 then print "  * Taint may come from this source:"
-         else print "  * Taint may also come from this source:";
-         print_taint_call_trace caps ~spaces:4 source_trace;
-         if tokens <> [] then (
-           print "  * These intermediate values are tainted:";
-           print_intermediate_vars caps ~spaces:4 tokens);
-         print "  * This is how taint reaches the sink:";
-         print_taint_call_trace caps ~spaces:4 sink_trace)
+let print_taint_trace (caps : < Cap.stdout >)
+    (taint_trace : Out.match_dataflow_trace) =
+  let print = CapConsole.print caps#stdout in
+  print "  * Taint may come from this source:";
+  taint_trace.taint_source
+  |> Option.iter (print_taint_call_trace caps ~spaces:4);
+  (match taint_trace.intermediate_vars with
+  | Some []
+  | None ->
+      ()
+  | Some intermediate_vars ->
+      print "  * These intermediate values are tainted:";
+      print_intermediate_vars caps ~spaces:4 intermediate_vars);
+  print "  * This is how taint reaches the sink:";
+  taint_trace.taint_sink |> Option.iter (print_taint_call_trace caps ~spaces:4)
 
 (*****************************************************************************)
 (* Entry point *)
 (*****************************************************************************)
 
-let print_match (caps : < Cap.stdout >) (match_ : Pattern_match.t) : unit =
-  let Pattern_match.{ tokens = (lazy tokens_matched_code); taint_trace; _ } =
-    match_
-  in
-  let str = spf "with rule %s" (Rule_ID.to_string match_.rule_id.id) in
-  (* there are a few fake tokens in the generic ASTs now (e.g.,
-   * for DotAccess generated outside the grammar) *)
-  let toks = tokens_matched_code |> List.filter Tok.is_origintok in
-  print_match_toks caps ~str toks;
-  taint_trace |> Option.iter (print_taint_trace caps)
+let print_match (caps : < Cap.stdout >) (match_ : Out.core_match) : unit =
+  let str = spf "with rule %s" (Rule_ID.to_string match_.check_id) in
+  let file = match_.path in
+  let start_line = match_.start.line in
+  let end_line = match_.end_.line in
+  print_match_lines caps ~str file start_line end_line;
+  match_.extra.dataflow_trace |> Option.iter (print_taint_trace caps)
--- src/reporting/Core_text_output.mli
+++ src/reporting/Core_text_output.mli
@@ -1,7 +1,7 @@
 (* This file is mostly deprecated. You should use osemgrep text output instead *)
 
 (* this can also display metavars and taint traces *)
-val print_match : < Cap.stdout > -> Pattern_match.t -> unit
+val print_match : < Cap.stdout > -> Semgrep_output_v1_j.core_match -> unit
 
 (* used also in Metavar_replacement.ml *)
 val join_with_space_if_needed : string list -> string
