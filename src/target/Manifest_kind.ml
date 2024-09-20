1e2377cf4de0188dd1fe2ec5fe823074ff9f92a4
Ben Kettle <ben@semgrep.com>
2024-09-20 11:36:25 -0700

    chore(dep-resolution): add (unused) dependency resolution framework (semgrep/semgrep-proprietary#2258)
    synced from Pro d1b4ff0fc8708eb22f9be19b0b8da47d3cf07b3e

--- src/target/Manifest_kind.ml
+++ src/target/Manifest_kind.ml
@@ -1,16 +1,31 @@
-type t = PackageJson [@@deriving show, eq, yojson]
+type t =
+  | PackageJson
+    (* An NPM package.json manifest file - https://docs.npmjs.com/cli/v10/configuring-npm/package-json *)
+  | PomXml
+    (* A Maven pom.xml manifest file - https://maven.apache.org/guides/introduction/introduction-to-the-pom.html *)
+  | BuildGradle
+    (* A Gradle build.gradle build file  - https://docs.gradle.org/current/userguide/build_file_basics.html *)
+[@@deriving show, eq, yojson]
 
 let to_ecosystem : t -> Semgrep_output_v1_t.ecosystem = function
   | PackageJson -> `Npm
+  | PomXml -> `Maven
+  | BuildGradle -> `Maven
 
-let supported_manifests = String.concat "," [ "package.json" ]
+let of_string s =
+  let unsupported_manifest_message (manifest_s : string) =
+    let supported_manifests =
+      String.concat "," [ "package.json"; "pom.xml"; "build.gradle" ]
+    in
 
-let unsupported_manifest_message (manifest_s : string) =
-  Common.spf "unsupported manifest: %s; supported manifest types are: %s"
-    manifest_s supported_manifests
+    Common.spf "unsupported manifest: %s; supported manifest types are: %s"
+      manifest_s supported_manifests
+  in
 
-let of_string = function
+  match s with
   | "PackageJson" -> PackageJson
+  | "PomXml" -> PomXml
+  | "BuildGradle" -> BuildGradle
   | s -> failwith (unsupported_manifest_message s)
 
 (* For use in Input_to_core.atd *)
