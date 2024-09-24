25be15cecab0ca0cca4f18a81a9206f0f5ec59bb
Sal Olivares <salolivares@users.noreply.github.com>
2024-09-24 16:34:03 -0700

    feat(sca): support different requirement lockfile patterns (semgrep/semgrep-proprietary#2182)
    Related PR: https://github.com/semgrep/semgrep-proprietary/pull/2126
    Depends on https://github.com/semgrep/semgrep-proprietary/pull/2252
    
    This PR introduces expanded support for requirement lockfiles. Semgrep
    will now find
    any `*requirement*.txt` file and lockfiles in a requirements folder
    (`**/requirements/*.txt`).
    This functionality will be gated behind the
    `--enable-experimental-requirements` CLI
    flag introduced in
    https://github.com/semgrep/semgrep-proprietary/pull/2252.
    
    synced from Pro a77fe3ea3c5af85c4a6d504dd58e57d838e2c932

--- cli/tests/default/unit/test_lockfile.py
+++ cli/tests/default/unit/test_lockfile.py
@@ -3,10 +3,15 @@ from pathlib import Path
 import pytest
 
 from semdep.lockfile import create_matcher
+from semdep.lockfile import EcosystemLockfiles
 from semdep.lockfile import ExactLockfileMatcher
 from semdep.lockfile import filter_lockfile_paths
 from semdep.lockfile import is_valid_lockfile
 from semdep.lockfile import Lockfile
+from semdep.lockfile import NEW_REQUIREMENTS_MATCHERS
+from semdep.lockfile import OLD_REQUIREMENTS_MATCHERS
+from semdep.lockfile import PatternLockfileMatcher
+from semdep.lockfile import RequirementsLockfileMatcher
 from semdep.parsers.pipfile import parse_pipfile
 from semgrep.semgrep_interfaces.semgrep_output_v1 import Cargo
 from semgrep.semgrep_interfaces.semgrep_output_v1 import Composer
@@ -54,6 +59,205 @@ class TestExactLockfileMatcher:
         manifest_path = matcher.get_manifest_path(Path("Pipfile.lock"))
         assert manifest_path == (Path("Pipfile") if create_manifest else None)
 
+    @pytest.mark.quick
+    def test_get_parent_path(self):
+        matcher = ExactLockfileMatcher(
+            lockfile="Pipfile.lock",
+            manifest="Pipfile",
+            parser=parse_pipfile,
+            ecosystem=Ecosystem(Pypi()),
+        )
+
+        assert matcher.get_subproject_root(Path("Pipfile.lock")) == Path(".")
+        assert matcher.get_subproject_root(Path("a/b/c/Pipfile.lock")) == Path("a/b/c")
+
+
+class TestPatternLockfileMatcher:
+    @pytest.mark.quick
+    def test_is_match(self):
+        matcher = PatternLockfileMatcher(
+            pattern="*requirements*.txt",
+            manifest="requirements.in",
+            parser=parse_pipfile,
+            ecosystem=Ecosystem(Pypi()),
+        )
+
+        # Basic cases
+        assert matcher.is_match(Path("requirements.txt")) is True
+        assert matcher.is_match(Path("requirements3.txt")) is True
+        assert matcher.is_match(Path("a/b/c/requirements.txt")) is True
+        assert matcher.is_match(Path("a/b/c/requirements3.txt")) is True
+
+        # Non-standard requirements cases
+        assert matcher.is_match(Path("requirements.dev.txt")) is True
+        assert matcher.is_match(Path("requirements.dev3.txt")) is True
+        assert matcher.is_match(Path("requirements-prod.txt")) is True
+        assert matcher.is_match(Path("requirements-dev.txt")) is True
+        assert matcher.is_match(Path("dev-requirements.txt")) is True
+        assert matcher.is_match(Path("dev-requirements3.txt")) is True
+        assert matcher.is_match(Path("prod-requirements.txt")) is True
+        assert matcher.is_match(Path("requirements_lock.txt")) is True
+
+        # Requirement folder cases
+        assert matcher.is_match(Path("requirements/prod.txt")) is True
+        assert matcher.is_match(Path("a/b/c/requirements/base.txt")) is True
+        assert matcher.is_match(Path("a/b/c/requirements/hello/dev.txt")) is True
+
+        # Non-requirements cases
+        assert matcher.is_match(Path("a/b/c/requirements.in")) is False
+        assert matcher.is_match(Path("unknown.lock")) is False
+
+    @pytest.mark.quick
+    @pytest.mark.parametrize("create_manifest", [True, False])
+    @pytest.mark.parametrize(
+        ("lockfile_path", "manifest_path"),
+        [
+            ("requirements.txt", "requirements.in"),
+            ("requirements3.txt", "requirements.in"),
+            ("a/b/c/requirements.txt", "a/b/c/requirements.in"),
+            ("a/b/c/requirements3.txt", "a/b/c/requirements.in"),
+        ],
+    )
+    def test_get_manifest_path(
+        self, tmp_path, monkeypatch, create_manifest, lockfile_path, manifest_path
+    ):
+        lockfile_full_path = tmp_path / Path(lockfile_path)
+        lockfile_full_path.parent.mkdir(parents=True, exist_ok=True)
+        lockfile_full_path.touch()
+        if create_manifest:
+            manifest_full_path = tmp_path / Path(manifest_path)
+            manifest_full_path.touch()
+
+        monkeypatch.chdir(tmp_path)
+
+        matcher = PatternLockfileMatcher(
+            pattern="*requirements*.txt",
+            manifest="requirements.in",
+            parser=parse_pipfile,
+            ecosystem=Ecosystem(Pypi()),
+        )
+        matcher_manifest_path = matcher.get_manifest_path(Path(lockfile_path))
+        assert matcher_manifest_path == (
+            Path(manifest_path) if create_manifest else None
+        )
+
+    @pytest.mark.quick
+    def test_get_parent_path(self):
+        matcher = PatternLockfileMatcher(
+            pattern="*requirements*.txt",
+            manifest="requirements.in",
+            parser=parse_pipfile,
+            ecosystem=Ecosystem(Pypi()),
+        )
+
+        assert matcher.get_subproject_root(Path("requirements.txt")) == Path(".")
+        assert matcher.get_subproject_root(Path("a/b/c/requirements.txt")) == Path(
+            "a/b/c"
+        )
+
+
+class TestRequirementsLockfileMatcher:
+    @pytest.mark.quick
+    def test_is_match(self) -> None:
+        matcher = RequirementsLockfileMatcher(
+            pattern="*requirements*.txt",
+            manifest="requirements.in",
+            parser=parse_pipfile,
+            ecosystem=Ecosystem(Pypi()),
+        )
+
+        # Basic cases
+        assert matcher.is_match(Path("requirements.txt")) is True
+        assert matcher.is_match(Path("requirements3.txt")) is True
+        assert matcher.is_match(Path("a/b/c/requirements.txt")) is True
+        assert matcher.is_match(Path("a/b/c/requirements3.txt")) is True
+
+        # Non-standard requirements cases
+        assert matcher.is_match(Path("requirements.dev.txt")) is True
+        assert matcher.is_match(Path("requirements.dev3.txt")) is True
+        assert matcher.is_match(Path("requirements-prod.txt")) is True
+        assert matcher.is_match(Path("requirements-dev.txt")) is True
+        assert matcher.is_match(Path("dev-requirements.txt")) is True
+        assert matcher.is_match(Path("dev-requirements3.txt")) is True
+        assert matcher.is_match(Path("prod-requirements.txt")) is True
+        assert matcher.is_match(Path("requirements_lock.txt")) is True
+
+        # Requirement folder cases
+        assert matcher.is_match(Path("requirements/prod.txt")) is True
+        assert matcher.is_match(Path("a/b/c/requirements/base.txt")) is True
+        assert matcher.is_match(Path("a/b/c/requirements/hello/dev.txt")) is True
+
+        # Non-requirements cases
+        assert matcher.is_match(Path("a/b/c/requirements.in")) is False
+        assert matcher.is_match(Path("unknown.lock")) is False
+
+    @pytest.mark.quick
+    @pytest.mark.parametrize("create_manifest", [True, False])
+    @pytest.mark.parametrize(
+        ("lockfile_path", "manifest_path"),
+        [
+            ("requirements.txt", "requirements.in"),
+            ("requirements3.txt", "requirements.in"),
+            ("a/b/c/requirements.txt", "a/b/c/requirements.in"),
+            ("a/b/c/requirements3.txt", "a/b/c/requirements.in"),
+            ("requirements-dev.txt", "requirements.in"),
+            ("requirements-prod.txt", "requirements.in"),
+            ("dev-requirements.txt", "requirements.in"),
+            ("prod-requirements.txt", "requirements.in"),
+            ("requirements_lock.txt", "requirements.in"),
+            ("requirements/prod.txt", "requirements.in"),
+            # TODO(sal): Is this even a case that appears?
+            ("requirements/prod.txt", "requirements/prod.in"),
+        ],
+    )
+    def test_get_manifest_path(
+        self, tmp_path, monkeypatch, create_manifest, lockfile_path, manifest_path
+    ):
+        lockfile_full_path = tmp_path / Path(lockfile_path)
+        lockfile_full_path.parent.mkdir(parents=True, exist_ok=True)
+        lockfile_full_path.touch()
+        if create_manifest:
+            manifest_full_path = tmp_path / Path(manifest_path)
+            manifest_full_path.touch()
+
+        monkeypatch.chdir(tmp_path)
+
+        matcher = RequirementsLockfileMatcher(
+            pattern="*requirements*.txt",
+            manifest="requirements.in",
+            parser=parse_pipfile,
+            ecosystem=Ecosystem(Pypi()),
+        )
+        matcher_manifest_path = matcher.get_manifest_path(Path(lockfile_path))
+        assert matcher_manifest_path == (
+            Path(manifest_path) if create_manifest else None
+        )
+
+    @pytest.mark.quick
+    def test_get_parent_path(self):
+        matcher = RequirementsLockfileMatcher(
+            pattern="*requirements*.txt",
+            manifest="requirements.in",
+            parser=parse_pipfile,
+            ecosystem=Ecosystem(Pypi()),
+        )
+
+        assert matcher.get_subproject_root(Path("requirements.txt")) == Path(".")
+        assert matcher.get_subproject_root(Path("a/b/c/requirements.txt")) == Path(
+            "a/b/c"
+        )
+        assert matcher.get_subproject_root(Path("requirements-dev.txt")) == Path(".")
+        assert matcher.get_subproject_root(Path("requirements/prod.txt")) == Path(".")
+        assert matcher.get_subproject_root(Path("a/b/c/requirements/base.txt")) == Path(
+            "a/b/c"
+        )
+
+        # TODO(sal): Is this even a case we want to support? Unsure if we want to support
+        # non standard requirements files in subdirectories under `requirements/`.
+        assert matcher.get_subproject_root(
+            Path("a/b/c/requirements/base/dev.txt")
+        ) == Path("a/b/c")
+
 
 class TestCreateMatcher:
     @pytest.mark.quick
@@ -62,7 +266,6 @@ class TestCreateMatcher:
         [
             "Pipfile.lock",
             "poetry.lock",
-            "requirements.txt",
             "package-lock.json",
             "yarn.lock",
             "pnpm-lock.yaml",
@@ -83,6 +286,24 @@ class TestCreateMatcher:
         matcher = create_matcher(path)
         assert isinstance(matcher, ExactLockfileMatcher)
         assert matcher.lockfile == lockfile
+
+    @pytest.mark.quick
+    @pytest.mark.parametrize(
+        "lockfile",
+        [
+            "requirements.txt",
+            "requirements.dev.txt",
+            "requirements/prod.txt",
+            "a/b/c/requirements/base.txt",
+        ],
+    )
+    def test_valid_pattern_match(self, lockfile):
+        EcosystemLockfiles.init(use_new_requirements_matchers=True)
+
+        matcher = create_matcher(Path(lockfile))
+
+        assert isinstance(matcher, PatternLockfileMatcher)
+        assert matcher.pattern == "*requirements*.txt"
 
     @pytest.mark.quick
     def test_unknown_match(self):
@@ -140,6 +361,40 @@ class TestLockfile:
             Path(lockfile_path).parent / Path(manifest) if manifest else None
         )
 
+    @pytest.mark.kinda_slow
+    @pytest.mark.parametrize(
+        "lockfile_path, ecosystem, manifest_path",
+        [
+            ("requirements.txt", Ecosystem(Pypi()), "requirements.in"),
+            ("a/b/c/requirements.txt", Ecosystem(Pypi()), "a/b/c/requirements.in"),
+            (
+                "a/b/c/requirements/base.txt",
+                Ecosystem(Pypi()),
+                "a/b/c/requirements/base.in",
+            ),
+        ],
+    )
+    def test_pattern_matchers(
+        self, lockfile_path, ecosystem, manifest_path, tmp_path, monkeypatch
+    ):
+        EcosystemLockfiles.init(use_new_requirements_matchers=True)
+
+        # Create the lockfile and manifest in the temporary directory
+        lockfile_full_path = tmp_path / Path(lockfile_path)
+        lockfile_full_path.parent.mkdir(parents=True, exist_ok=True)
+        lockfile_full_path.touch()
+        manifest_full_path = tmp_path / Path(manifest_path)
+        manifest_full_path.touch()
+        monkeypatch.chdir(tmp_path)
+
+        # Create the Lockfile instance
+        lockfile = Lockfile.from_path(Path(lockfile_path))
+
+        # Assert the Lockfile instance is correct
+        assert lockfile.path == Path(lockfile_path)
+        assert lockfile.ecosystem == ecosystem
+        assert lockfile.manifest_path == Path(manifest_path)
+
 
 @pytest.mark.quick
 def test_filter_lockfile_paths():
@@ -166,3 +421,29 @@ def test_filter_lockfile_paths():
 def test_is_valid_lockfile(lockfile, expected):
     ecosystem = Ecosystem(Pypi())
     assert is_valid_lockfile(ecosystem, lockfile) is expected
+
+
+class TestEcosystemLockfiles:
+    @pytest.mark.quick
+    def test_base_case(self):
+        EcosystemLockfiles.init(use_new_requirements_matchers=False)
+
+        # Use the old requirements lockfile matchers for Python
+        assert (
+            EcosystemLockfiles.ecosystem_to_lockfiles[Ecosystem(Pypi())]
+            == OLD_REQUIREMENTS_MATCHERS
+        )
+
+        # Use the new requirements lockfile matchers for Python
+        EcosystemLockfiles.init(use_new_requirements_matchers=True)
+        assert (
+            EcosystemLockfiles.ecosystem_to_lockfiles[Ecosystem(Pypi())]
+            == NEW_REQUIREMENTS_MATCHERS
+        )
+
+        # Use the old requirements lockfile matchers for Python
+        EcosystemLockfiles.init(use_new_requirements_matchers=False)
+        assert (
+            EcosystemLockfiles.ecosystem_to_lockfiles[Ecosystem(Pypi())]
+            == OLD_REQUIREMENTS_MATCHERS
+        )
--- cli/tests/default/unit/test_subproject.py
+++ cli/tests/default/unit/test_subproject.py
@@ -2,69 +2,289 @@ from pathlib import Path
 
 import pytest
 
+from semdep.lockfile import EcosystemLockfiles
+from semdep.lockfile import Lockfile
 from semgrep.semgrep_interfaces.semgrep_output_v1 import Ecosystem
+from semgrep.semgrep_interfaces.semgrep_output_v1 import FoundDependency
+from semgrep.semgrep_interfaces.semgrep_output_v1 import Fpath
 from semgrep.semgrep_interfaces.semgrep_output_v1 import Maven
 from semgrep.semgrep_interfaces.semgrep_output_v1 import Pypi
+from semgrep.semgrep_interfaces.semgrep_output_v1 import Transitivity
+from semgrep.semgrep_interfaces.semgrep_output_v1 import Unknown
+from semgrep.subproject import _create_dependency_source
 from semgrep.subproject import find_closest_subproject
 from semgrep.subproject import LockfileDependencySource
+from semgrep.subproject import MultiLockfileDependencySource
 from semgrep.subproject import Subproject
 
 
-@pytest.mark.quick
-def test_finds_subproject_in_same_directory():
-    expected = Subproject(
-        root_dir=Path("/a/b/c"),
-        dependency_source=LockfileDependencySource(
-            lockfile_path=Path("/a/b/c/requirements.txt"),
-            manifest_path=Path("/a/b/c/requirements.in"),
-        ),
-        ecosystem=Ecosystem(Pypi()),
-        found_dependencies=[],
-    )
-    extra = [
-        Subproject(
-            root_dir=Path("/a/b"),
+def create_tmp_file(path: Path):
+    path.parent.mkdir(parents=True, exist_ok=True)
+    path.touch()
+
+
+class TestFindClosestSubproject:
+    @pytest.mark.quick
+    def test_finds_subproject_in_same_directory(self, tmp_path, monkeypatch):
+        lockfile_path = Path("a/b/c/requirements.txt")
+        create_tmp_file(tmp_path / lockfile_path)
+        extra_lockfile_path = Path("a/b/requirements.txt")
+        create_tmp_file(tmp_path / Path(extra_lockfile_path))
+
+        monkeypatch.chdir(tmp_path)
+
+        expected = Subproject(
+            root_dir=Path("a/b/c"),
             dependency_source=LockfileDependencySource(
-                lockfile_path=Path("/a/b/requirements.txt"),
-                manifest_path=Path("/a/b/requirements.in"),
+                lockfile_path=lockfile_path,
+                manifest_path=Path("a/b/c/requirements.in"),
             ),
             ecosystem=Ecosystem(Pypi()),
             found_dependencies=[],
         )
-    ]
+        extra = [
+            Subproject(
+                root_dir=Path("a/b"),
+                dependency_source=LockfileDependencySource(
+                    lockfile_path=extra_lockfile_path,
+                    manifest_path=Path("a/b/requirements.in"),
+                ),
+                ecosystem=Ecosystem(Pypi()),
+                found_dependencies=[],
+            )
+        ]
 
-    assert (
-        find_closest_subproject(
-            Path("/a/b/c/test.py"), Ecosystem(Pypi()), [expected, *extra]
-        )
-        == expected
-    ), "Should return subproject with lockfile in same directory"
+        assert (
+            find_closest_subproject(
+                Path("a/b/c/test.py"), Ecosystem(Pypi()), [*extra, expected]
+            )
+            == expected
+        ), "Should return subproject with lockfile in same directory"
 
+    @pytest.mark.quick
+    def test_finds_subproject_for_requested_ecosystem(self, tmp_path, monkeypatch):
+        lockfile_path = Path("a/b/gradle.lockfile")
+        create_tmp_file(tmp_path / lockfile_path)
+        extra_lockfile_path = Path("a/b/c/requirement.txt")
+        create_tmp_file(tmp_path / Path(extra_lockfile_path))
 
-@pytest.mark.quick
-def test_finds_subproject_for_requested_ecosystem():
-    expected = Subproject(
-        root_dir=Path("/a/b"),
-        dependency_source=LockfileDependencySource(
-            lockfile_path=Path("/a/b/gradle.lockfile"),
-            manifest_path=Path("/a/b/build.gradle"),
-        ),
-        ecosystem=Ecosystem(Maven()),
-        found_dependencies=[],
-    )
-    extra = [
-        Subproject(
-            root_dir=Path("/a/b/c"),
+        monkeypatch.chdir(tmp_path)
+
+        expected = Subproject(
+            root_dir=Path("a/b"),
             dependency_source=LockfileDependencySource(
-                lockfile_path=Path("/a/b/c/requirement.txt"),
-                manifest_path=Path("/a/b/c/requirements.in"),
+                lockfile_path=lockfile_path,
+                manifest_path=Path("a/b/build.gradle"),
             ),
-            ecosystem=Ecosystem(Pypi()),
+            ecosystem=Ecosystem(Maven()),
             found_dependencies=[],
         )
-    ]
+        extra = [
+            Subproject(
+                root_dir=Path("a/b/c"),
+                dependency_source=LockfileDependencySource(
+                    lockfile_path=extra_lockfile_path,
+                    manifest_path=Path("a/b/c/requirements.in"),
+                ),
+                ecosystem=Ecosystem(Pypi()),
+                found_dependencies=[],
+            )
+        ]
 
-    result = find_closest_subproject(
-        Path("/a/b/c/app/test.java"), Ecosystem(Maven()), [expected, *extra]
+        result = find_closest_subproject(
+            Path("a/b/c/app/test.java"), Ecosystem(Maven()), [expected, *extra]
+        )
+        assert result == expected, "Should return subproject with requested ecosystem"
+
+
+class TestSubproject:
+    @pytest.mark.quick
+    @pytest.mark.parametrize(
+        "lockfile_path", [Path("a/b/c/requirements.txt"), Path("requirements.txt")]
     )
-    assert result == expected, "Should return subproject with requested ecosystem"
+    def test_base_case(self, lockfile_path):
+        found_dependencies = [
+            FoundDependency(
+                package="requests",
+                version="2.26.0",
+                ecosystem=Ecosystem(Pypi()),
+                allowed_hashes={},
+                transitivity=Transitivity(Unknown()),
+                lockfile_path=Fpath(str(lockfile_path)),
+            )
+        ]
+
+        subproject = Subproject(
+            root_dir=Path("a/b/c"),
+            dependency_source=LockfileDependencySource(
+                lockfile_path=lockfile_path,
+                manifest_path=Path("a/b/c/requirements.in"),
+            ),
+            ecosystem=Ecosystem(Pypi()),
+            found_dependencies=found_dependencies,
+        )
+
+        assert subproject.map_lockfile_to_dependencies() == {
+            str(lockfile_path): found_dependencies
+        }, "Should return mapping of lockfile path to dependencies"
+
+        assert subproject.get_lockfile_paths() == [
+            lockfile_path
+        ], "Should return lockfile path"
+
+    @pytest.mark.quick
+    def test_multiple_lockfiles(self):
+        lockfile_path = Path("a/b/c/requirements/base.txt")
+        extra_lockfile_path = Path("a/b/requirements/dev.txt")
+        found_dependencies = [
+            FoundDependency(
+                package="requests",
+                version="2.26.0",
+                ecosystem=Ecosystem(Pypi()),
+                allowed_hashes={},
+                transitivity=Transitivity(Unknown()),
+                lockfile_path=Fpath(str(lockfile_path)),
+            ),
+            FoundDependency(
+                package="flask",
+                version="2.0.0",
+                ecosystem=Ecosystem(Pypi()),
+                allowed_hashes={},
+                transitivity=Transitivity(Unknown()),
+                lockfile_path=Fpath(str(extra_lockfile_path)),
+            ),
+        ]
+
+        multi_lockfile_source = MultiLockfileDependencySource(
+            sources=[
+                LockfileDependencySource(
+                    lockfile_path=lockfile_path, manifest_path=None
+                ),
+                LockfileDependencySource(
+                    lockfile_path=extra_lockfile_path, manifest_path=None
+                ),
+            ]
+        )
+
+        subproject = Subproject(
+            root_dir=Path("a/b/c"),
+            dependency_source=multi_lockfile_source,
+            ecosystem=Ecosystem(Pypi()),
+            found_dependencies=found_dependencies,
+        )
+
+        lockfile_deps_map = subproject.map_lockfile_to_dependencies()
+        assert lockfile_deps_map[str(lockfile_path)][0] == found_dependencies[0]
+        assert lockfile_deps_map[str(extra_lockfile_path)][0] == found_dependencies[1]
+
+        assert subproject.get_lockfile_paths() == [
+            lockfile_path,
+            extra_lockfile_path,
+        ], "Should return lockfile paths"
+
+    @pytest.mark.quick
+    def test_dep_missing_lockfile_path(self):
+        lockfile_path = Path("requirements.txt")
+        found_dependencies = [
+            FoundDependency(
+                package="requests",
+                version="2.26.0",
+                ecosystem=Ecosystem(Pypi()),
+                allowed_hashes={},
+                transitivity=Transitivity(Unknown()),
+            )
+        ]
+
+        subproject = Subproject(
+            root_dir=Path("a/b/c"),
+            dependency_source=LockfileDependencySource(
+                lockfile_path=lockfile_path,
+                manifest_path=Path("a/b/c/requirements.in"),
+            ),
+            ecosystem=Ecosystem(Pypi()),
+            found_dependencies=found_dependencies,
+        )
+
+        assert subproject.map_lockfile_to_dependencies() == {
+            str(
+                subproject.root_dir.joinpath(Path("unknown_lockfile"))
+            ): found_dependencies
+        }, "Should return mapping of lockfile path to dependencies"
+
+        assert subproject.get_lockfile_paths() == [
+            lockfile_path
+        ], "Should return lockfile path"
+
+
+class TestLockfileDependencySource:
+    @pytest.mark.quick
+    def test_base_case(self):
+        lockfile_path = Path("a/b/c/requirements.txt")
+
+        source = LockfileDependencySource(
+            lockfile_path=lockfile_path, manifest_path=None
+        )
+
+        assert source.get_lockfile_paths() == [
+            lockfile_path
+        ], "Should return lockfile path"
+
+
+class TestMultiLockfileDependencySource:
+    @pytest.mark.quick
+    def test_base_case(self):
+        lockfile_path = Path("a/b/c/requirements.txt")
+        extra_lockfile_path = Path("a/b/requirements/dev.txt")
+
+        source = MultiLockfileDependencySource(
+            sources=[
+                LockfileDependencySource(
+                    lockfile_path=lockfile_path, manifest_path=None
+                ),
+                LockfileDependencySource(
+                    lockfile_path=extra_lockfile_path, manifest_path=None
+                ),
+            ]
+        )
+
+        assert source.get_lockfile_paths() == [
+            lockfile_path,
+            extra_lockfile_path,
+        ], "Should return lockfile paths"
+
+
+class TestCreateDependencySource:
+    @pytest.mark.quick
+    def test_single_lockfile(self):
+        lockfile_path = Path("requirements.txt")
+        lockfiles = [Lockfile.from_path(lockfile_path)]
+
+        source = _create_dependency_source(lockfiles)
+
+        assert source == LockfileDependencySource(
+            lockfile_path=lockfile_path, manifest_path=None
+        ), "Should return LockfileDependencySource"
+
+    @pytest.mark.quick
+    def test_multiple_lockfiles(self):
+        EcosystemLockfiles.init(use_new_requirements_matchers=True)
+
+        lockfile_path = Path("requirements.txt")
+        extra_lockfile_path = Path("requirements/dev.txt")
+        lockfiles = [
+            Lockfile.from_path(lockfile_path),
+            Lockfile.from_path(extra_lockfile_path),
+        ]
+
+        source = _create_dependency_source(lockfiles)
+
+        assert source == MultiLockfileDependencySource(
+            sources=[
+                LockfileDependencySource(
+                    lockfile_path=lockfile_path, manifest_path=None
+                ),
+                LockfileDependencySource(
+                    lockfile_path=extra_lockfile_path, manifest_path=None
+                ),
+            ]
+        ), "Should return MultiLockfileDependencySource"
