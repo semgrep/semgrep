3b3beea9c29440dcf7ca12ad04d3599c2c60bd41
Z Zeleznick <zz@semgrep.com>
2024-10-04 12:08:13 -0700

    fix(int-sec): Ensure we clean `project_url` for metrics case (semgrep/semgrep-proprietary#2357)
    Relates to #2356 
    
    synced from Pro e01696edf9047c6166be7556651ad946691094a3

--- cli/tests/default/unit/test_clean_project_url.py
+++ cli/tests/default/unit/test_clean_project_url.py
@@ -1,6 +1,9 @@
+from unittest.mock import patch
+
 import pytest
 
 from semgrep.git import clean_project_url
+from semgrep.git import get_project_url
 
 
 @pytest.mark.quick
@@ -11,3 +14,10 @@ def test_git_url_clean():
         )
         == "https://gitlab.company.world/app/test-case.git"
     )
+
+
+@pytest.mark.quick
+@patch("semgrep.git.clean_project_url")
+def test_get_project_url(patched_clean_project_url):
+    get_project_url()
+    patched_clean_project_url.assert_called_once()
