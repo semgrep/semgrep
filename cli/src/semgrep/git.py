c24a26f1bbc5c4806c851733e2df1d30ef41518e
Z Zeleznick <zz@semgrep.com>
2024-10-04 12:08:13 -0700

    fix(int-sec): Ensure we clean `project_url` for metrics case (semgrep/semgrep-proprietary#2357)
    Relates to #2356 
    
    synced from Pro e01696edf9047c6166be7556651ad946691094a3

--- cli/src/semgrep/git.py
+++ cli/src/semgrep/git.py
@@ -76,19 +76,23 @@ def git_check_output(command: Sequence[str], cwd: Optional[str] = None) -> str:
 
 def get_project_url() -> Optional[str]:
     """
-    Returns the current git project's default remote URL, or None if not a git project / no remote
+    Returns the current git project's default remote URL, or None if not a git project / no remote.
+    NOTE: We need to ensure that we clean the URL to remove any credentials as Gitlab includes
+    a token in the URL (e.g.`https://gitlab-ci-token):${CI_JOB_TOKEN}@gitlab.example.com/<namespace>/<project>`)
+    which is sensitive information that we should not expose.
     """
+    project_url = None
     try:
-        remote_url = git_check_output(["git", "ls-remote", "--get-url"])
-        return clean_project_url(remote_url)
+        project_url = git_check_output(["git", "ls-remote", "--get-url"])
     except Exception as e:
         logger.debug(f"Failed to get project url from 'git ls-remote': {e}")
         try:
             # add \n to match urls from git ls-remote (backwards compatibility)
-            return manually_search_file(".git/config", ".com", "\n")
+            project_url = manually_search_file(".git/config", ".com", "\n")
         except Exception as e:
             logger.debug(f"Failed to get project url from .git/config: {e}")
             return None
+    return clean_project_url(project_url) if project_url else None
 
 
 def clean_project_url(url: str) -> str:
