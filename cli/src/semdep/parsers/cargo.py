fda9d3ab44af354d7b50e26b68a6ebc866a0399a
SayyantR <124740176+SayyantR@users.noreply.github.com>
2024-10-08 15:36:44 -0700

    feat(sca): Added line numbers to `cargo.py` parser (semgrep/semgrep-proprietary#2369)
    synced from Pro be9fe4d1c1e34fb185508dc558e1a37827bbf3db

--- cli/src/semdep/parsers/cargo.py
+++ cli/src/semdep/parsers/cargo.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import Generator
+from typing import List
 from typing import Optional
 
 from semgrep.semgrep_interfaces.semgrep_output_v1 import Cargo
@@ -13,7 +14,7 @@ from semgrep.semgrep_interfaces.semgrep_output_v1 import Unknown
 def parse_cargo(
     lockfile_path: Path, lockfile_text: str, manifest_text: Optional[str]
 ) -> Generator[FoundDependency, None, None]:
-    def parse_dep(s: str) -> FoundDependency:
+    def parse_dep(s: str, raw_lines: List[str]) -> FoundDependency:
         lines = s.split("\n")[1:]
         dep = lines[0].split("=")[1].strip()[1:-1]
         version = lines[1].split("=")[1].strip()[1:-1]
@@ -29,7 +30,9 @@ def parse_cargo(
             allowed_hashes=hash,
             transitivity=Transitivity(Unknown()),
             lockfile_path=Fpath(str(lockfile_path)),
+            line_number=raw_lines.index(lines[0]) + 1,
         )
 
+    raw_lines = lockfile_text.split("\n")
     deps = lockfile_text.split("[[package]]")[1:]
-    yield from (parse_dep(dep) for dep in deps)
+    yield from (parse_dep(dep, raw_lines) for dep in deps)
