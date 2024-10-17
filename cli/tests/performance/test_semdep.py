182b2e701342f2e49bafd4362bf878d70e457831
Ben Kettle <ben@semgrep.com>
2024-10-17 11:23:33 -0800

    refactor(sca-targeting): rework sca lockfile identification to support lockfileless (semgrep/semgrep-proprietary#2379)
    This PR reworks most of the way that we identify lockfiles, building a
    framework that allows us to use other dependency sources as well. To do
    so, it:
    - Separates subproject resolution into two phases, identification and
    dependency resolution. While previously lockfile identification and
    parsing were intertwined, this PR breaks them apart. This allows us to
    mix-and-match dependency resolution methods (lockfile parsing, dynamic
    dependency resolution) for a single subproject, which is necessary to
    enable PTT for customers that use lockfiles.
    - Introduces the concept of a `ResolvedSubproject`. A
    `ResolvedSubproject` includes dependency information, while a
    `Subproject` now just signifies that we have identified a subproject at
    a certain location. This enables the above splitting of subproject
    resolution and will also allow us to identify subprojects for package
    managers that we do not yet support, allowing us to collect better
    metrics.
    - Abstracts `LockfileMatcher`s to become `SubprojectMatcher`s and
    changes them to be more "pure". Instead of accessing the filesystem
    directly to find manifests, these matchers create subprojects from a
    list of candidates. As before, the matchers are used by the target
    manager to identify files that might be a dependency source.
    - Introduces a new `PackageManagerType` enum to Python only and removes
    several usages of the `Ecosystem` enum in the lockfile identification
    code. This is somewhat similar to `Ecosystem`, but I felt that
    `Ecosystem` was overloaded: the ecosystem is really a property of the
    resolved dependencies, and not the subproject itself. This new
    `PackageManagerType` enum is used to identify which resolver/parser we
    should use for each subproject that the matchers create.
    - Removes the ability to find lockfiles for a single ecosystem. Instead,
    allows finding all lockfiles, manifests, and other dependency source
    files by passing a special string "dependency_source_files" to the
    target manager.
    
    synced from Pro 17797e6cbc99a5ad5bbeef8c7b124e50e067245c

--- cli/tests/performance/test_semdep.py
+++ cli/tests/performance/test_semdep.py
@@ -3,39 +3,52 @@ from time import time
 
 import pytest
 
-from semdep.lockfile import Lockfile
+from semdep.parsers.gem import parse_gemfile
+from semdep.parsers.go_mod import parse_go_mod
+from semdep.parsers.gradle import parse_gradle
+from semdep.parsers.package_lock import parse_package_lock
+from semdep.parsers.pipfile import parse_pipfile
+from semdep.parsers.poetry import parse_poetry
+from semdep.parsers.pom_tree import parse_pom_tree
+from semdep.parsers.requirements import parse_requirements
+from semdep.parsers.util import SemgrepParser
+from semdep.parsers.yarn import parse_yarn
 
 
 @pytest.mark.kinda_slow
 @pytest.mark.parametrize(
-    "file_size,target,max_time",
+    "file_size,target,max_time,parser",
     [
-        (file_size, target, max_time)
+        (file_size, target, max_time, parser)
         # These times are set relative to Github Actions, they should be lower when
         # running locally.
         # Local time expectation is more like 1, 5, 10
         # update: 10k is actually taking more time locally, 3.5 on pad's Linux machine
         for file_size, max_time in [("10k", 4), ("50k", 15), ("100k", 30)]
-        for target in [
-            "Gemfile.lock",
-            "go.mod",
-            "gradle.lockfile",
-            "maven_dep_tree.txt",
-            "package-lock.json",
-            "poetry.lock",
-            "requirements.txt",
-            "yarn.lock",
-            "Pipfile.lock",
+        for target, parser in [
+            ("Gemfile.lock", parse_gemfile),
+            ("go.mod", parse_go_mod),
+            ("gradle.lockfile", parse_gradle),
+            ("maven_dep_tree.txt", parse_pom_tree),
+            ("package-lock.json", parse_package_lock),
+            ("poetry.lock", parse_poetry),
+            ("requirements.txt", parse_requirements),
+            ("yarn.lock", parse_yarn),
+            ("Pipfile.lock", parse_pipfile),
         ]
     ],
 )
 def test_dependency_aware_timing(
-    lockfile_path_in_tmp_for_perf, file_size, target, max_time
+    lockfile_path_in_tmp_for_perf,
+    file_size: str,
+    target: str,
+    max_time: int,
+    parser: SemgrepParser,
 ):
     start = time()
 
     lockfile_path = Path(f"targets_perf_sca/{file_size}/{target}")
-    _, error = Lockfile.from_path(lockfile_path).parse()
+    _, error = parser(lockfile_path, None)
 
     end = time()
 
