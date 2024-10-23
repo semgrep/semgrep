67606f5eba691fe431f1de9195627c5106da440c
Yoann Padioleau <pad@returntocorp.com>
2024-10-23 14:39:23 +0200

    osemgrep test --pro: Plug also DeepScan (semgrep/semgrep-proprietary#2424)
    We now run CoreScan, ProScan, and DeepScan, each time
    looking for different annotations.
    
    test plan:
    ```
    $ time ./bin/osemgrep-pro test --pro OSS/tests/semgrep-rules
    ...
    5914/5922: 8 unit tests did not pass:
    ...
    real: 0m23.202s
    ```
    
    synced from Pro 2f0c41d8bf3373363f9996581b1f25ef14de88af

--- tests/rules/metavariable_name_resolution.java
+++ tests/rules/metavariable_name_resolution.java
@@ -3,7 +3,7 @@ import org.foo.Foo;
 public class Test {
     public static void main(String[] args) {
         Foo foo = new Foo();
-        // ruleid: metavariable-resolution-test
+        // ruleid: deeptodoruleid: metavariable-resolution-test
         foo.bar();
         foo.baz();
     }
