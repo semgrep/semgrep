004707577f6c30d2ce39273bb403e68235adae26
Yoann Padioleau <yoann.padioleau@gmail.com>
2025-01-17 10:39:09 +0100

    osemgrep-sca: fix the warning about malformed versions in SCA rules (semgrep/semgrep-proprietary#2892)
    test plan:
    ./bin/osemgrep ci --experimental
    does not show anymore warnings like:
    [13.25][WARNING]: unrecognized version format for
    1087.1089.v2f1b_9a_b_040e4
    
    synced from Pro 38d435c028732fecfc31a4b2d12ebeb0628e2b95

--- src/parsing/Parse_SCA_version.ml
+++ src/parsing/Parse_SCA_version.ml
@@ -1,6 +1,6 @@
 (* Matthew McQuaid, Yoann Padioleau
  *
- * Copyright (C) 2024 Semgrep Inc.
+ * Copyright (C) 2024-2025 Semgrep Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
@@ -30,22 +30,32 @@ open Common
 exception Error of string
 
 (*****************************************************************************)
-(* Helpers *)
-(*****************************************************************************)
-
-(*****************************************************************************)
 (* Entrypoints *)
 (*****************************************************************************)
 
-(* TODO: port part of specifiers.py
+(* ex: "4.0.0-beta.86" "15.0-rc-1" "14.0-rc1" "2.4.0rc0" "4.0.0-beta"
+ *  "2.3.4-p2" "2.0.0-M1" "2.0.0-sp1" ...
+ *)
+let other_version =
+  Pcre2_.regexp ~flags:[ `CASELESS ]
+    {|^[0-9]+(\.[0-9]+)*[-.]?(alpha|beta|pre|rc|dev|final|release|incubating|milestone|next|stable|lts|preview|fp|cr|sp|pl|[abempquv])[-.]?[0-9]*$|}
+
+(* ex: "117.veb" "0.5.0b3.dev42" "720.vbe985dd73d66" "1206.v14049fa"
+ * "1087.1089.v2f1b_9a_b_040e4" (FYI just 1 SCA rule is using '_' in its version
+ * constraint)
+ *)
+let almost_bailout_version =
+  Pcre2_.regexp {|^[0-9]+(\.[0-9]+)*[-.][a-zA-Z0-9._-]+$|}
+
+(* TODO: port part of specifiers.py?
  * TODO: pass more context (e.g., a Tok.t) for better Logs.warn or error
  *)
 let parse (str : string) : SCA_version.t =
   (* just enough to parse some toy package-lock.json and enough
-   * to parse the version part of the version constraints in our SCA rules
+   * to parse the version part of the version constraints in our SCA rules.
    *)
   match str with
-  (* "1.2.3" *)
+  (* ex: (most common version format) "1.2.3" *)
   | _ when str =~ "^\\([0-9]+\\)\\.\\([0-9]+\\)\\.\\([0-9]+\\)$" ->
       let s1, s2, s3 = Common.matched3 str in
       SCA_version.V
@@ -54,7 +64,7 @@ let parse (str : string) : SCA_version.t =
           minor = int_of_string s2;
           incrementals = [ int_of_string s3 ];
         }
-  (* "40.0" *)
+  (* ex: "40.0" *)
   | _ when str =~ "^\\([0-9]+\\)\\.\\([0-9]+\\)$" ->
       let s1, s2 = Common.matched2 str in
       SCA_version.V
@@ -63,14 +73,27 @@ let parse (str : string) : SCA_version.t =
           minor = int_of_string s2;
           incrementals = [];
         }
-  (* "40" *)
+  (* ex: "40" *)
   | _ when str =~ "^\\([0-9]+\\)$" ->
       let s1 = Common.matched1 str in
       SCA_version.V { major = int_of_string s1; minor = 0; incrementals = [] }
-  (* alt: raise (Error (spf "wrong version format for %s" str)) in *)
+  (* ex: "2.2.8.1" *)
+  | _ when str =~ "^\\([0-9]+\\)\\.\\([0-9]+\\)\\.\\([0-9]+\\)\\.\\([0-9]+\\)$"
+    ->
+      let s1, s2, s3, s4 = Common.matched4 str in
+      SCA_version.V
+        {
+          major = int_of_string s1;
+          minor = int_of_string s2;
+          incrementals = [ int_of_string s3; int_of_string s4 ];
+        }
   | _ ->
-      (* nosemgrep: no-logs-in-library *)
-      Logs.warn (fun m -> m "unrecognized version format for %s" str);
+      if
+        (not (Pcre2_.pmatch_noerr ~rex:other_version str))
+        && not (Pcre2_.pmatch_noerr ~rex:almost_bailout_version str)
+        (* nosemgrep: no-logs-in-library *)
+      then Logs.warn (fun m -> m "unrecognized version format for %s" str);
+      (* alt: raise (Error (spf "wrong version format for %s" str)) in *)
       SCA_version.Other str
 
 (* TODO: port part of specifiers.py *)
