3bf3b24c4b5435b2d5cfdc45b6704b366e5488fa
Yoann Padioleau <yoann.padioleau@gmail.com>
2025-01-24 18:14:23 +0100

    Remove Pro_hooks.ml (semgrep/semgrep-proprietary#2942)
    It losts his raison d'etre because we now use Hook.t everywhere
    so all those hooks are saved and restored thx to Hook.with_hook_set
    
    test plan:
    make
    
    synced from Pro 1251765fd4225c087440d1debe0a9c46b81a7057

--- src/engine/Pro_hooks.mli
+++
@@ -1,6 +0,0 @@
-(* Saves current Pro hooks, and temporarily resets them for running a function.
- * When the function has completed, it restores the saved Pro hooks. This
- * confines the reach of the "reset", making it easier to reason about, and
- * causing fewer surprises.
- *)
-val save_pro_hooks_and_reset : (unit -> 'a) -> 'a
