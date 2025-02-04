cca9b397892a14ff038c71742ff570fd7da511af
Nat Mote <nat@semgrep.com>
2025-02-04 08:33:00 -0800

    fix: Disable tracing in Parmap child processes (semgrep/semgrep-proprietary#3024)
    synced from Pro ed3d61fa9f0b2e6aef5f38c3eb44ea40b60683d6

--- src/core_scan/Parmap_targets.ml
+++ src/core_scan/Parmap_targets.ml
@@ -83,7 +83,15 @@ let core_error_of_path_exc (internal_path : Fpath.t) (e : Exception.t) :
    :)
 *)
 
-let init job =
+let init _job =
+  (* With the OCaml 5 upgrade we started sporadically seeing strange Parmap
+   * errors again when tracing is enabled. This remains somewhat mysterious.
+   * Disable tracing in child processes for now. Fortunately OCaml 5 gives us
+   * more options for a couple pieces of this unfortunate area so hopefully we
+   * will be able to replace some of the problematic components soon.
+   *
+   * TODO Re-enable tracing (and update initialize below!) *)
+  (*
   (* Set a global attribute to the job number so we know when we look at
      traces/logs/metrics which job it came from! *)
   Tracing.add_global_attribute Trace_data.Attributes.job (`Int job);
@@ -91,11 +99,17 @@ let init job =
      map_targets___* funcs *)
   (* NOTE: this only restarts tracing in the child *)
   Tracing.restart_tracing ()
+  *)
+  ()
 
 let finalize () =
   (* Stop tracing to ensure traces are flushed *)
   (* NOTE: this only stops tracing in the child *)
+  (* TODO See note in init *)
+  (*
   Tracing.stop_tracing ~exit_active_spans:false ()
+  *)
+  ()
 
 (* Run jobs in parallel, using number of cores specified with -j *)
 let map_targets__run_in_forked_process_do_not_modify_globals caps (ncores : int)
