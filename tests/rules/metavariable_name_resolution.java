b549fa7d19c9d96f0cc062593eb9dda4904decd2
Heejong Lee <heejong@gmail.com>
2025-02-21 19:02:42 -0800

    Fix `range not found` error in metavar pattern match (semgrep/semgrep-proprietary#3148)
    test plan: `make test`
    
    synced from Pro 1181762cb52642b2c8fbb3eaac1b7a1593c7511d

--- tests/rules/metavariable_name_resolution.java
+++ tests/rules/metavariable_name_resolution.java
@@ -3,7 +3,7 @@ import org.foo.Foo;
 public class Test {
     public static void main(String[] args) {
         Foo foo = new Foo();
-        // ruleid: deeptodoruleid: metavariable-resolution-test
+        // ruleid: metavariable-resolution-test
         foo.bar();
         foo.baz();
     }
