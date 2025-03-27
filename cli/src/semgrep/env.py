b711cfe1419747d1e7dc1d4cb34478bb233c3298
Tom Petr <trpetr@gmail.com>
2025-03-27 13:25:33 -0400

    Manual sync of OSS PR 10915 (semgrep/semgrep-proprietary#3516)
    synced from Pro 9cdc4c3f578fa95fbfc02c2d12997d9c4154ca7c

--- cli/src/semgrep/env.py
+++ cli/src/semgrep/env.py
@@ -105,7 +105,12 @@ class Env:
         value = os.getenv("SEMGREP_VERSION_CACHE_PATH")
         if value:
             return Path(value)
-        return Path.home() / ".cache" / "semgrep_version"
+        cache_home = os.getenv("XDG_CACHE_HOME")
+        if cache_home and Path(cache_home).is_dir():
+            parent_dir = Path(cache_home)
+        else:
+            parent_dir = Path.home() / ".cache"
+        return parent_dir / "semgrep_version"
 
     @git_command_timeout.default
     def git_command_timeout_default(self) -> int:
