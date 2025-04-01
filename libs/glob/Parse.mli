ce31235ffb83eae6bba55f57e06a776904da42d4
Martin Jambon <github@mjambon.com>
2025-03-31 18:22:10 -0700

    Add backward compatibility for Semgrepignore v1 patterns starting with `./` (semgrep/semgrep-proprietary#3532)
    Add backward compatibility for Semgrepignore v1 patterns by treating
    patterns that start with `./` as if they started with `/`. This is
    because many Semgrep users already have such patterns in their
    Semgrepignore files even though they don't have any effect in Gitignore
    files.
    
    In a later PR, we'll print a deprecation warning and eventually we'll
    remove support for this.
    
    synced from Pro ed406682e1cb63d297d1388e6babe7212784ae99

--- libs/glob/Parse.mli
+++ libs/glob/Parse.mli
@@ -1,6 +1,16 @@
-val parse_string : string -> Pattern.t
+(*
+   Parse a glob pattern.
+*)
+
+val parse_string : ?deprecated_absolute_dotslash:bool -> string -> Pattern.t
 (** Parse a glob pattern according to
     {{: https://pubs.opengroup.org/onlinepubs/9799919799/utilities/V3_chap02.html#tag_19_14 }
       [glob(7)] }, which is the POSIX standard for old-fashioned shell globbing
       patterns for matching file paths. Additionally, we support [**] as per
-      the {{: https://git-scm.com/docs/gitignore } gitignore specification}. *)
+      the {{: https://git-scm.com/docs/gitignore } gitignore specification}.
+
+    deprecated_absolute_dotslash: should be false according to the standard.
+    If true, it causes the pattern './a' to be parsed as '/a'.
+    It is a deprecated option provided for backward compatibility
+    with Semgrepignore v1 behavior.
+*)
