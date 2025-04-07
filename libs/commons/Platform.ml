27b31a5d4fd42a4d0dcb06b20b9290102f6f62d1
Shon Feder <shon.feder@gmail.com>
2025-04-07 16:13:33 -0400

    feat(windows): add windows as binary install target to osemgrep (semgrep/semgrep-proprietary#3233)
    The PR fixes running the `install-semgrep-pro` command with
    `--custom-binary` flag on Windows, which is handled by `osemgrep`. This
    will fix the `make dev` target on Windows.
    
    Test plan:
    
    1. Run `semgrep install-semgrep-pro
    --custom-binary=bin/semgrep-core-proprietary.exe`
    2. Verify that the `semgrep ci --pro` command works
    3. Re-run the install step in 1 to ensure a new binary can be installed
    (on new releases/new builds)
    
    synced from Pro ee6c0d9ec6aa1b6924dfe546fd0a506db4f3e368

--- libs/commons/Platform.ml
+++ libs/commons/Platform.ml
@@ -1,29 +1,37 @@
-(* TODO: other options for Windows! *)
 type arch = Arm | Arm64 | X86_64 | OtherArch of string
-type kernel = Darwin | Linux | OtherKernel of string
+type kernel = Darwin | Linux | Windows | OtherKernel of string
 
-(* TODO: should use CapExec.string_of_run instead of with_open_process_in *)
+module Log = Log_commons.Log
 
 let arch (caps : < Cap.exec >) =
-  CapExec.with_open_process_in caps#exec "uname -m" (fun chan ->
-      let s =
-        In_channel.input_all chan |> String.trim |> String.lowercase_ascii
-      in
-      match s with
+  (Cmd.Name "uname", [ "-m" ]) |> CapExec.string_of_run ~trim:true caps#exec
+  |> function
+  | Ok (output, (_, `Exited 0)) -> (
+      match String.lowercase_ascii output with
       | "arm" -> Arm
       | "arm64" -> Arm64
       | "x86_64" -> X86_64
-      | _ -> OtherArch s)
+      | s -> OtherArch s)
+  | Ok (error, (_, _))
+  | Error (`Msg error) ->
+      Log.warn (fun m -> m "Running `uname -m` failed with an error: %s" error);
+      OtherArch "unknown"
+
+(* We want to check for [Sys.win32] rather than [Sys.cygwin] because we
+   target native Windows executables without a cygwin dependency. *)
+let is_windows = Sys.win32
 
 let kernel (caps : < Cap.exec >) =
-  CapExec.with_open_process_in caps#exec "uname" (fun chan ->
-      let s =
-        In_channel.input_all chan |> String.trim |> String.lowercase_ascii
-      in
-      match s with
-      | "darwin" -> Darwin
-      | "linux" -> Linux
-      | _ -> OtherKernel s)
-
-(* TODO? || Sys.cygwin? *)
-let is_windows = Sys.win32
+  if is_windows then Windows
+  else
+    (Cmd.Name "uname", []) |> CapExec.string_of_run ~trim:true caps#exec
+    |> function
+    | Ok (output, (_, `Exited 0)) -> (
+        match String.lowercase_ascii output with
+        | "darwin" -> Darwin
+        | "linux" -> Linux
+        | s -> OtherKernel s)
+    | Ok (error, (_, _))
+    | Error (`Msg error) ->
+        Log.warn (fun m -> m "Running uname failed with an error: %s" error);
+        OtherKernel "unknown"
