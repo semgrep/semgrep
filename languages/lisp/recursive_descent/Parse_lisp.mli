d91ad7a4a76e0a301b24f32f9dfe9afa667b9386
Yosef <72322110+yosefAlsuhaibani@users.noreply.github.com>
2025-05-02 16:22:16 -0400

    chore: delete unused lisp parser (semgrep/semgrep-proprietary#3795)
    synced from Pro 28957274994744e97a0640307216b6069b32acad

--- languages/lisp/recursive_descent/Parse_lisp.mli
+++
@@ -1,9 +0,0 @@
-(* the token list contains also the comment-tokens *)
-type program_and_tokens = Ast_lisp.program option * Parser_lisp.token list
-
-(* This is the main function *)
-val parse : Fpath.t -> program_and_tokens * Parsing_stat.t
-val parse_program : Fpath.t -> Ast_lisp.program
-
-(* internal *)
-val tokens : Parsing_helpers.input_source -> Parser_lisp.token list
