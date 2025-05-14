d4284d650771ceabdc531ee5102cb0c0aa23eaae
Heejong Lee <heejong@gmail.com>
2025-05-14 15:04:30 -0700

    update typed metavar test: typed_metavar_metavar_regex.go (semgrep/semgrep-proprietary#3886)
    synced from Pro a4df9bf6174fa2c0ddda0855d39922de45a24dba

--- tests/rules/typed_metavar_metavar_regex.go
+++ tests/rules/typed_metavar_metavar_regex.go
@@ -1,4 +1,9 @@
 func f(c *http.Request) {
+  // The combination of a typed metavariable and a regex does not always work
+  // in pro interfile mode because the type is semantically resolved and often
+  // contains fake tokens. In pro interfile mode, `metavariable-type` should be
+  // used instead of relying on non-syntactic metavariable binding. See
+  // CODE-7993 for more details.
   // ruleid: deeptodoruleid: typed-metavar-metavar-regex
   c.Foo()
 }
