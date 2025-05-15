f8c49731d8b4980425174eb2174e01bb91b5763b
Nathan Taylor <nathan.taylor@semgrep.com>
2025-05-14 18:24:06 -0700

    feat(multicore): Support concurrent access to memoized functions (semgrep/semgrep-proprietary#3737)
    We have a few instances of using `Common.memoized` that assume
    single-threaded access to the backing hashtable. This of course won't do
    in the multicore world, so this patch puts out an equivalent one that
    synchronizes on a Mutex. Uses will come in a subsequent patch.
    
    synced from Pro deb342bc480ffed3ec70c35d073c43bff33b9308

---
+++ libs/parallelism/SharedMemo.ml
@@ -0,0 +1,57 @@
+(* Nathan Taylor
+ *
+ * Copyright (C) 2025 Semgrep Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation, with the
+ * special exception on linking described in file LICENSE.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
+ * LICENSE for more details.
+ *)
+
+(*****************************************************************************)
+(* Prelude *)
+(*****************************************************************************)
+
+(** Similar to Commons.memoized, but also consumes a mutex in order to ensure
+ * safe current access to the memoized state. *)
+
+(*****************************************************************************)
+(* API *)
+(*****************************************************************************)
+
+let call_and_remember mtx ht k f =
+  (* Assumption: [k] will more often than not be a cache hit.  As a result,
+   * in the unlikely event of a cache miss, we pay the cost of unlocking and
+   * relocking to insert the new kv pair. *)
+  match Mutex.protect mtx (fun () -> Hashtbl.find_opt ht k) with
+  | Some v -> v
+  | None ->
+      (* Note: f could well be an expensive computation, so do not starve
+       * other accesses to the hashtable while we call it by holding the mutex.
+       *
+       * This does leave open the possibility that two threads will race on
+       * computation on the same key: whoever gets there first will not be
+       * overridden by the straggler.  Since f has to be deterministic, this
+       * is fine (and while unfortunate, still preferable to holding the lock
+       * through the computation. *)
+      let v = f () in
+      Mutex.protect mtx (fun () ->
+          match Hashtbl.find_opt ht k with
+          (* Someone beat us to the insert! So it goes; discard our copy. *)
+          | Some v' -> v'
+          | None ->
+              Hashtbl.add ht k v;
+              v)
+
+let make_with_state mtx ht f =
+ fun k -> call_and_remember mtx ht k (fun () -> f k)
+
+let make f =
+  let mtx = Mutex.create () in
+  let ht = Hashtbl.create 101 in
+  make_with_state mtx ht f
