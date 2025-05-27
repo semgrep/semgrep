c8e26d1f5062b9614bf061eb9dc63ee710b8abb8
Nathan Taylor <nathan.taylor@semgrep.com>
2025-05-27 15:44:07 -0700

    fix(multicore): spacegrep: yield to Eio during matching (semgrep/semgrep-proprietary#3952)
    synced from Pro de51cad2544dcffca5e5573f9fbf0e967b22b9b6

--- src/spacegrep/src/lib/Match.ml
+++ src/spacegrep/src/lib/Match.ml
@@ -258,6 +258,7 @@ let rec match_ (conf : conf) ~(dots : dots option) (env : env)
       dots:dots option -> env -> Loc.t -> Pattern_AST.node list -> match_result)
     : match_result =
   if !debug then Dump_match.print pat doc;
+  Domains.maybe_yield ();
   match (pat, doc) with
   | [], doc -> (
       match doc_matches_dots ~dots last_loc doc with
