85bc0022e4c07b10434b08c3a5432d173cd302ba
Tean Lai <113626567+tean-lai@users.noreply.github.com>
2025-06-09 10:18:09 -0700

    fix(matching): js matching no longer includes surrounding parentheses (semgrep/semgrep-proprietary#4004)
    ## Why
    Addresses [this issue](https://github.com/semgrep/semgrep/issues/9522),
    where JavaScript autofix consumes parentheses around expressions we
    wanted to match, which can break conditionals.
    
    ## What
    Modified the conversion process from TS CST to TS AST. Before, if,
    while, switch, etc. statements contained a parenthesized expression.
    When an expression is matched to a parenthesized expression, the range
    includes the parenthesis, so autofix would break the syntax. Now, the
    conversion process discards the parentheses during the translation, so
    matches won't contain the parentheses.
    
    ## Testing plan 
    Wrote some unit tests to check that expressions inside the parenthesis
    would be matched, and not the parentheses itself.
    
    Some previously written tests disagreed with changes, but I think in
    this case they were slightly off because they expected parentheses to
    match with the expression, so some minor changes to those test files
    were also made.
    
    ## Alternatives considered
    Considered removed the `set_e_range` that would be run when converting
    JS CST parenthesized expressions to generic AST expressions. However,
    this would cause a regression that is not preferable, see
    [6233](https://github.com/semgrep/semgrep/issues/6233).
    
    Closes [#9522](https://github.com/semgrep/semgrep/issues/9522)
    
    synced from Pro 4d4dc1654a5052445ac5770ffe11a7fddb79546a

---
+++ tests/rules/js_bracket.js
@@ -0,0 +1,19 @@
+
+if (
+    // ruleid: js-bracket
+    foo()
+) {}
+
+
+// ok: js-bracket
+console.log(
+    // ruleid: js-bracket
+    foo()
+)
+
+
+// ok: js-bracket
+while (
+    // ruleid: js-bracket
+    foo()
+) {}
