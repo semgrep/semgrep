5b712ee7973eb1fab716d240abe1d2b72107ee81
Yosef <72322110+yosefAlsuhaibani@users.noreply.github.com>
2025-06-08 22:11:54 -0400

    chore: remove No_tcb (semgrep/semgrep-proprietary#4040)
    synced from Pro e6be8acddfdb067dfb66ede38360cd4cf993c16c

--- libs/commons/Testutil_mock.ml
+++ libs/commons/Testutil_mock.ml
@@ -1,10 +1,10 @@
 let with_setenv envvar str f =
-  let old = USys.getenv_opt envvar in
-  UUnix.putenv envvar str;
+  let old = Sys.getenv_opt envvar in
+  Unix.putenv envvar str;
   Common.finalize f (fun () ->
       match old with
-      | Some str -> UUnix.putenv envvar str
+      | Some str -> Unix.putenv envvar str
       (* ugly: Unix does not provide unsetenv,
        * see https://discuss.ocaml.org/t/unset-environment-variable/9025
        *)
-      | None -> UUnix.putenv envvar "")
+      | None -> Unix.putenv envvar "")
