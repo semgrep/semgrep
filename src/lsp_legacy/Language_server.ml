1d87ec3183ecd17fab38ce1044ef362afe811651
Brandon Wu <49291449+brandonspark@users.noreply.github.com>
2025-06-18 13:47:47 -0700

    chore: literally just copy oss lsp over to pro (semgrep/semgrep-proprietary#4089)
    synced from Pro e4c5d3dc2dab65cd3417d7e47bb94cba89c8c284

--- src/lsp_legacy/Language_server.ml
+++ src/lsp_legacy/Language_server.ml
@@ -1,4 +1,6 @@
 type caps =
   < Core_scan.caps ; Cap.random ; Cap.network ; Cap.tmp ; Cap.readdir >
 
-let start caps = Lwt_platform.run (LS.start caps)
+let start caps =
+  Logs.info (fun m -> m "Starting legacy language server");
+  Lwt_platform.run (LS.start caps)
