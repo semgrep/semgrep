8a69887fc26234cc88fbf90cf7e856ce5649e3b9
Nathan Taylor <nathan.taylor@semgrep.com>
2025-07-15 17:26:31 -0500

    feat(cli): add tracing annotations to RPC calls (semgrep/semgrep-proprietary#4235)
    synced from Pro 9e6fe821c82a4d9ae74bec5d1f0bb67657e09446

--- cli/src/semgrep/rpc_call.py
+++ cli/src/semgrep/rpc_call.py
@@ -3,6 +3,7 @@ from typing import Optional
 from typing import Tuple
 
 import semgrep.semgrep_interfaces.semgrep_output_v1 as out
+from semgrep import tracing
 from semgrep.rpc import rpc_call
 from semgrep.verbose_logging import getLogger
 
@@ -13,6 +14,7 @@ logger = getLogger(__name__)
 ##############################################################################
 
 
+@tracing.trace()
 def format(
     formatter: out.OutputFormat, ctx: out.FormatContext, output: out.CliOutput
 ) -> str:
@@ -23,6 +25,7 @@ def format(
     return ret.value
 
 
+@tracing.trace()
 def apply_fixes(args: out.ApplyFixesParams) -> Optional[out.ApplyFixesReturn]:
     call = out.FunctionCall(out.CallApplyFixes(args))
     ret: Optional[out.RetApplyFixes] = rpc_call(call, out.RetApplyFixes)
@@ -33,6 +36,7 @@ def apply_fixes(args: out.ApplyFixesParams) -> Optional[out.ApplyFixesReturn]:
     return ret.value
 
 
+@tracing.trace()
 def sarif_format(
     sarif_format: out.SarifFormat, ctx: out.FormatContext, cli_out: out.CliOutput
 ) -> Optional[out.RetSarifFormat]:
@@ -45,6 +49,7 @@ def sarif_format(
     return ret
 
 
+@tracing.trace()
 def contributions() -> out.Contributions:
     call = out.FunctionCall(out.CallContributions())
     ret: Optional[out.RetContributions] = rpc_call(call, out.RetContributions)
@@ -54,6 +59,7 @@ def contributions() -> out.Contributions:
     return ret.value
 
 
+@tracing.trace()
 def validate(fp: out.Fpath) -> bool:
     call = out.FunctionCall(out.CallValidate(fp))
     ret: Optional[out.RetValidate] = rpc_call(call, out.RetValidate)
@@ -63,6 +69,7 @@ def validate(fp: out.Fpath) -> bool:
     return ret.value
 
 
+@tracing.trace()
 def resolve_dependencies(
     dependency_sources: List[out.DependencySource],
     download_dependency_source_code: bool,
@@ -83,6 +90,7 @@ def resolve_dependencies(
     return ret.value
 
 
+@tracing.trace()
 def upload_symbol_analysis(
     token: str, scan_id: int, symbol_analysis: out.SymbolAnalysis
 ) -> None:
@@ -100,6 +108,7 @@ def upload_symbol_analysis(
         logger.debug(f"Uploading symbol analysis succeeded with {ret.value}")
 
 
+@tracing.trace()
 def transitive_reachability_filter(
     args: out.TransitiveReachabilityFilterParams,
 ) -> List[out.TransitiveFinding]:
@@ -114,6 +123,7 @@ def transitive_reachability_filter(
     return ret.value
 
 
+@tracing.trace()
 def dump_rule_partitions(args: out.DumpRulePartitionsParams) -> bool:
     call = out.FunctionCall(out.CallDumpRulePartitions(args))
     ret: Optional[out.RetDumpRulePartitions] = rpc_call(call, out.RetDumpRulePartitions)
@@ -123,6 +133,7 @@ def dump_rule_partitions(args: out.DumpRulePartitionsParams) -> bool:
     return ret.value
 
 
+@tracing.trace()
 def get_targets(scanning_roots: out.ScanningRoots) -> out.TargetDiscoveryResult:
     def summarize(desc: str, xs: list, threshold: int = 30) -> None:
         if len(xs) > 0:
@@ -146,6 +157,7 @@ def get_targets(scanning_roots: out.ScanningRoots) -> out.TargetDiscoveryResult:
     return ret.value
 
 
+@tracing.trace()
 def match_subprojects(dependency_source_files: List[out.Fpath]) -> List[out.Subproject]:
     call = out.FunctionCall(out.CallMatchSubprojects(dependency_source_files))
     ret: Optional[out.RetMatchSubprojects] = rpc_call(call, out.RetMatchSubprojects)
