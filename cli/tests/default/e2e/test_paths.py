0fb31074ab7233a181f6ee3f7573ca98a51081e3
Martin Jambon <github@mjambon.com>
2025-07-22 14:38:29 -0700

    Disable test_paths because it makes pytest hang on Windows (semgrep/semgrep-proprietary#4258)
    This pytest test hangs on Windows but the same semgrep command works fine when executed in a standalone fashion.
    
    Test plan: CI tests should all pass and not hang
    
    synced from Pro d6660d7bc04a2ebe38c3b4e4430891ec6c98a534

--- cli/tests/default/e2e/test_paths.py
+++ cli/tests/default/e2e/test_paths.py
@@ -1,9 +1,20 @@
 import pytest
+from tests.conftest import skip_on_windows
 from tests.fixtures import RunSemgrep
 
 
 # Test in-rule path filtering: paths.include, paths.exclude.
 # The patterns now obey the Semgrepignore/Gitignore spec.
+#
+# Skip on Windows: this test makes pytest hang indefinitely on Windows
+# since PR https://github.com/semgrep/semgrep-proprietary/pull/3859
+# Behaves fine when run as a standalone semgrep or osemgrep command.
+#
+# This happens because we log a warning about ambiguous rule exclude paths, and
+# for some reason that message is too long and causes pytest to hang on windows
+# only. Even if we split up the logger warning AND flush, it still hangs. So who
+# knows, we just won't run this on Windows.
+@skip_on_windows
 @pytest.mark.kinda_slow
 def test_paths(run_semgrep_in_tmp: RunSemgrep, posix_snapshot):
     posix_snapshot.assert_match(
