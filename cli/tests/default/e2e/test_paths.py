df02b204c516c3bf5881d34c7047d21ccc7fe5a4
Martin Jambon <github@mjambon.com>
2025-07-23 10:30:42 -0700

    Isolate the test_paths tests that fail on Windows with pytest (semgrep/semgrep-proprietary#4299)
    This is follow-up to #4258. Here, we minimize the number of tests that
    we skip on Windows (due hanging happening when logging too much stuff
    with `logger.warning` when running semgrep from pytest).
    
    Test plan: CI should pass and not hang.
    
    synced from Pro 7c4b8a9fee022f40987883f8aff558ae43a4031e

--- cli/tests/default/e2e/test_paths.py
+++ cli/tests/default/e2e/test_paths.py
@@ -6,6 +6,14 @@ from tests.fixtures import RunSemgrep
 # Test in-rule path filtering: paths.include, paths.exclude.
 # The patterns now obey the Semgrepignore/Gitignore spec.
 #
+@pytest.mark.kinda_slow
+def test_paths(run_semgrep_in_tmp: RunSemgrep, posix_snapshot):
+    posix_snapshot.assert_match(
+        run_semgrep_in_tmp("rules/paths.yaml", target_name="exclude_include").stdout,
+        "results.json",
+    )
+
+
 # Skip on Windows: this test makes pytest hang indefinitely on Windows
 # since PR https://github.com/semgrep/semgrep-proprietary/pull/3859
 # Behaves fine when run as a standalone semgrep or osemgrep command.
@@ -16,8 +24,10 @@ from tests.fixtures import RunSemgrep
 # knows, we just won't run this on Windows.
 @skip_on_windows
 @pytest.mark.kinda_slow
-def test_paths(run_semgrep_in_tmp: RunSemgrep, posix_snapshot):
+def test_paths_warnings(run_semgrep_in_tmp: RunSemgrep, posix_snapshot):
     posix_snapshot.assert_match(
-        run_semgrep_in_tmp("rules/paths.yaml", target_name="exclude_include").stdout,
+        run_semgrep_in_tmp(
+            "rules/paths_warnings.yaml", target_name="exclude_include"
+        ).stdout,
         "results.json",
     )
