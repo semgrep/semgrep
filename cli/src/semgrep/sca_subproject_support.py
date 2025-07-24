c398ffde2be02ceef537a5e34d0fb3f221c7ffab
Tikhon Jelvis <tikhon@jelv.is>
2025-07-24 14:48:59 -0700

    feat(tr): support for poetry (semgrep/semgrep-proprietary#4208)
    synced from Pro b08cabdcc23f050d36deb165b0fb8e880192f4dc

--- cli/src/semgrep/sca_subproject_support.py
+++ cli/src/semgrep/sca_subproject_support.py
@@ -48,6 +48,7 @@ PTT_DYNAMIC_RESOLUTION_SUBPROJECT_KINDS: List[SubprojectKind] = [
 TR_OCAML_RESOLVER_SUBPROJECT_KINDS: List[SubprojectKind] = [
     (out.ManifestKind(out.PackageJson()), out.LockfileKind(out.NpmPackageLockJson())),
     (out.ManifestKind(out.PyprojectToml()), out.LockfileKind(out.UvLock())),
+    (out.ManifestKind(out.PyprojectToml()), out.LockfileKind(out.PoetryLock())),
 ]
 
 # Subproject kinds that we use ocaml parsers for always.
@@ -63,4 +64,5 @@ ALWAYS_OCAML_PARSER_SUBPROJECT_KINDS: List[SubprojectKind] = [
 TRANSITIVE_REACHABILITY_SUBPROJECT_KINDS: List[SubprojectKind] = [
     (out.ManifestKind(out.PackageJson()), out.LockfileKind(out.NpmPackageLockJson())),
     (out.ManifestKind(out.PyprojectToml()), out.LockfileKind(out.UvLock())),
+    (out.ManifestKind(out.PyprojectToml()), out.LockfileKind(out.PoetryLock())),
 ]
