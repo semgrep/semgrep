78f5db1d683d08a720e83ba0018684169479d1f7
Brandon Wu <49291449+brandonspark@users.noreply.github.com>
2025-07-25 09:08:17 -0700

    feat(mcp): `semgrep mcp` subcommand (semgrep/semgrep-proprietary#4297)
    synced from Pro 31b14975a9531a0aeae5ff9c35aec6cc46a44acc

---
+++ src/osemgrep/cli_mcp/Mcp_CLI.mli
@@ -0,0 +1,19 @@
+(*
+   'semgrep mcp' command-line parsing.
+*)
+
+(*
+   The result of parsing a 'semgrep lsp' command.
+*)
+
+type conf = { common : CLI_common.conf } [@@deriving show]
+
+(*
+   Usage: parse_argv [| "semgrep-mcp"; <args> |]
+
+   Turn argv into a conf structure.
+
+   This function may raise an exn in case of an error parsing argv
+   but this should be caught by CLI.safe_run.
+*)
+val parse_argv : string array -> conf
---
+++ src/osemgrep/cli_mcp/Mcp_subcommand.mli
@@ -0,0 +1,16 @@
+type caps =
+  < Core_scan.caps ; Cap.random ; Cap.network ; Cap.tmp ; Cap.readdir >
+
+val hook_run_mcp : (Mcp_CLI.conf -> unit) option Hook.t
+
+(*
+   Parse a semgrep-mcp command, execute it and exit.
+
+   Usage: main [| "semgrep-mcp"; ... |]
+
+   This function returns an exit code to be passed to the 'exit' function.
+*)
+val main : < caps ; .. > -> string array -> Exit_code.t
+
+(* internal *)
+val run_conf : < caps ; .. > -> Mcp_CLI.conf -> Exit_code.t
