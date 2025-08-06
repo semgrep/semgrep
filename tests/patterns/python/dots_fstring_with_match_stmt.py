9d1ddaee70ecf5c746185fe8adbb294aac189452
ento <ento+github@i.pearlwaffles.xyz>
2025-08-06 15:31:35 -0800

    Fix dots_fstring_with_match_stmt.py test
    Replace with test cases from dots_fstring.py

--- tests/patterns/python/dots_fstring_with_match_stmt.py
+++ tests/patterns/python/dots_fstring_with_match_stmt.py
@@ -1,5 +1,13 @@
-#ERROR: match
-x = "foo"
+def foo():
+  #ERROR: match
+  print(f"this is good")
+
+  # ERROR: match
+  print(f"this should {match}")
+
+  print("hello")
+
+  print("hello" "world")
 
 match status:
     case 400:
