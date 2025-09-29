813a77b7ca5ebb51956b19a03d26290ae905019e
Iago Abal <iago@semgrep.com>
2025-09-29 10:52:05 +0200

    scala: Enable patterns `{ ... }` to match partial functions (semgrep/semgrep-proprietary#4735)
    E.g. `{ case 1 => "1" }`.
    
    Closes CODE-9106
    
    Test plan:
    make test # added two new tests
    
    synced from Pro 665a0b0d151732820bffd0249b63b2dfa0493e11

---
+++ tests/patterns/scala/block_ellipsis1.scala
@@ -0,0 +1,18 @@
+package controllers
+
+import cats.effect._
+import cats.implicits._
+import org.http4s._
+import org.http4s.dsl.io._
+import org.http4s.server._
+import org.http4s.circe._
+import org.http4s.multipart.Multipart
+import io.circe.generic.auto._
+
+object Http4sController {
+  // MATCH:
+  val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
+    case GET -> Root /  "users" / userId =>
+      Ok(s"User ID: $userId")
+  }
+}
