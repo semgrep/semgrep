4a709f3b3cda2dd73e9113f39473c3947c7ed82b
Yosef <72322110+yosefAlsuhaibani@users.noreply.github.com>
2026-01-26 19:54:22 -0500

    chore(ci): clean up python/uv setup in our builds. (semgrep/semgrep-proprietary#5452)
    Unifies `setup-python` with `setup-uv`; it also switches how we install `uv` in docker, by moving over to `astral-sh`'s alpine docker image w/`uv` pre-installed + copying `uv` from the distroless image to pin the version.
    
    Test plan: CI
    
    synced from Pro 25ea5efe60bfdec0ff9aaea541648266e2afaaa6

--- scripts/build-wheels.sh
+++ scripts/build-wheels.sh
@@ -5,21 +5,20 @@
 # and https://realpython.com/python-wheels/ for more information.
 # This script is called from our GHA build-xxx workflows.
 # It assumes the semgrep-core binary has been copied under cli/src/semgrep/bin
-# for pip to package semgrep correctly.
+# for pip to package semgrep correctly. And that `uv` is installed
 
 set -ex
-# Need latest pip otherwise twine fails to install
-python3 -m pip install --break-system-packages --upgrade pip
-# Need latest versions here otherwise we end up with a malformed package where
-# it marks the README as an RST file which fails to parse.
-python3 -m pip install --break-system-packages --upgrade setuptools wheel twine
-cd cli && python3 setup.py sdist bdist_wheel "$@"
+
+# Invoke `uv` to build the wheel, which invokes the build backend (`setup.py`)
+cd cli
+uv sync
+uv build
 
 # Do some sanity checks on the built packages. These checks are done as part of
 # uploading to pypi (in the gh-action-pypi-publish action), but we only run that
 # job on actual releases. Checking here will catch malformed packages on PR
 # rather than on release.
-twine check dist/*.whl
+uv tool run twine check dist/*.whl
 
 # Zipping for a stable name to upload as an artifact
 if command -v zip &>/dev/null; then
