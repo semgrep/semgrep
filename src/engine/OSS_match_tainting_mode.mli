cb445aa42ed1d0b2362dbf3d209bc6c49a0191f1
Iago Abal <iago@semgrep.com>
2026-02-04 22:23:09 +0100

    tainting: pro: Fork Taint_lval_env (semgrep/semgrep-proprietary#5543)
    synced from Pro c216822bb857ad89c001d1a1188c1b8080e2f0e4

--- src/engine/OSS_match_tainting_mode.mli
+++ src/engine/OSS_match_tainting_mode.mli
@@ -14,7 +14,7 @@ val check_fundef :
   Taint_rule_inst.t ->
   IL.name option (** entity being analyzed *) ->
   AST_to_IL.ctx ->
-  ?glob_env:Taint_lval_env.t ->
+  ?glob_env:OSS_taint_lval_env.t ->
   AST_generic.function_definition ->
   Fun_CFG.t * Shape_and_sig.Effects.t * OSS_dataflow_tainting.mapping
 (** Check a function definition using a [Dataflow_tainting.config] (which can
--- src/tainting/OSS_taint_input_env.mli
+++ src/tainting/OSS_taint_input_env.mli
@@ -14,9 +14,9 @@
 
 val mk_fun_input_env :
   Taint_rule_inst.t ->
-  ?glob_env:Taint_lval_env.t ->
+  ?glob_env:OSS_taint_lval_env.t ->
   IL.param list ->
-  Taint_lval_env.t * Shape_and_sig.Effects.t
+  OSS_taint_lval_env.t * Shape_and_sig.Effects.t
 (** Constructs the initial taint environment for a given function definition.
     Essentially, it records the parameters that are taint sources, or whose
     default value is a taint source.
@@ -25,6 +25,6 @@ val mk_fun_input_env :
 val mk_file_env :
   Taint_rule_inst.t ->
   AST_generic.program ->
-  Taint_lval_env.t * Shape_and_sig.Effects.t
+  OSS_taint_lval_env.t * Shape_and_sig.Effects.t
 (** Constructs the global taint environment for a given file: it finds global-
   and class- variable definitions that are final, and checks if they are tainted. *)
