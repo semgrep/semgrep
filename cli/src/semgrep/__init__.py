8ed5c8865496a653ea2ea967c501d4965feed149
tef-semgrep <247230491+tef-semgrep@users.noreply.github.com>
2026-02-13 12:55:40 -0800

    Revert "Release Version 1.152.0 (semgrep/semgrep-proprietary#5615)" (semgrep/semgrep-proprietary#5621)
    This reverts commit 1d94b572e1448144c25c10af04cef1d41c7970da.
    
    An issue with the docker builds is blocking the release, and so we will
    rollback for now.
    
    synced from Pro 8605bbb15c31a53a154d20f977b5113582e8fee1

--- cli/src/semgrep/__init__.py
+++ cli/src/semgrep/__init__.py
@@ -10,4 +10,4 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the file
 # LICENSE for more details.
 #
-__VERSION__ = "1.152.0"
+__VERSION__ = "1.151.0"
--- src/core/Version.ml
+++ src/core/Version.ml
@@ -15,4 +15,4 @@
 
   Automatically modified by scripts/release/bump.
 *)
-let version = "1.152.0"
+let version = "1.151.0"
