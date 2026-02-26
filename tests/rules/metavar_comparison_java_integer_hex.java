f644023404ce549d1760f08a754ae8857aa3265f
Dani Bennet <danijulianbennet@gmail.com>
2026-02-26 07:50:48 +0000

    Add failing test for Java hex integer literals in metavariable-comparison

---
+++ tests/rules/metavar_comparison_java_integer_hex.java
@@ -0,0 +1,12 @@
+class IntegerHexTest {
+    public static void test() {
+        // ruleid: java-integer-hex-comparison
+        int a = 1;
+
+        // ruleid: java-integer-hex-comparison
+        int b = 0x1;
+
+        // ruleid: java-integer-hex-comparison
+        int c = 0x01; // EXPECTED but currently FAILS
+    }
+}
