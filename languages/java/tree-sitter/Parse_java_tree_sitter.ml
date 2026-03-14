e0d950804cd995c690334cf69c45e6b1cd9e3637
Dani Bennet <danijulianbennet@gmail.com>
2026-03-14 05:04:49 +0000

    Fix parser syntax and apply pre-commit formatting

--- languages/java/tree-sitter/Parse_java_tree_sitter.ml
+++ languages/java/tree-sitter/Parse_java_tree_sitter.ml
@@ -135,7 +135,8 @@ let inferred_parameters (env : env) ((v1, v2, v3, v4) : CST.inferred_parameters)
     token env v4
     (* ")" *)
   in
-  (v1, v2 :: v3, v4) Int (Parsed_int.parse_c_octal (s, t))
+  (v1, v2 :: v3, v4);
+  Int (Parsed_int.parse_c_octal (s, t))
 
 let multiline_string_fragment (env : env) (x : CST.multiline_string_fragment) =
   match x with
