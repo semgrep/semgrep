17b52152bad3383b0c630c674759fd16881a4ea9
Marco Slaviero <marco@thinkst.com>
2026-03-18 09:52:46 -0700

    Fix Python parsing of non-f-strings when match statements are used (semgrep/semgrep-proprietary#5810)
    This PR fixes two bugs that manifest when a Python file has a `match`
    statement. If a file has a match statement and either: 1. an empty
    string, or
    2. double quotes present in a docstring.
    
    Then the file fails to be parsed with a syntax error.
    
    The fix extends the parser to also match empty strings, and docstrings
    with quotes.
    
    It fixes https://github.com/semgrep/semgrep/issues/11287
    
    Test plan:
    - Added empty strings to the f-strings tests
    - Added a parser test for quotes in a docstring
    - `$ make test` passes without error
    
    It also fixes a tiny error in the Makefile, so the `install` target
    works correctly, which is useful for development.
    
    
    
    Synced from OSS PR https://github.com/semgrep/semgrep/pull/11442
    Author: @thinkst-marco
    Imported by: @liukatkat
    
    Closes https://github.com/semgrep/semgrep/pull/11442
    OSS-sync-author: Marco Slaviero <marco@thinkst.com>
    
    Test plan:
    
    synced from Pro aee9845dc191f0f100bc974fb4a5bc6e37206c0a

--- tests/patterns/python/dots_fstring_with_match_stmt.py
+++ tests/patterns/python/dots_fstring_with_match_stmt.py
@@ -5,9 +5,14 @@ def foo():
   # ERROR: match
   print(f"this should {match}")
 
+  # ERROR: match
+  print(f"")
+
   print("hello")
 
   print("hello" "world")
+
+  print("")
 
 match status:
     case 400:
