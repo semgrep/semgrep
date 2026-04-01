06353ecc9d0e2347ec08e67856cc04c0c6ee6659
Nathan Taylor <nathan.taylor@semgrep.com>
2026-03-31 20:26:47 -0500

    feat: add I/O stats tracking to Disk_cache (semgrep/semgrep-proprietary#5910)
    While investigating some performance issues regarding taint config caching, I realised that we don't have any visibility into how much we're using the disk cache, be it the size of the cache or how often we're reading/writing.  This patch introduces statistics into the Disk_cache functor to be plumbed up into appropriate OTel traces.
    
    API change: `Disk_cache.S.rm` now returns a `result`, which it should have done before anyway, since both `stat`ting and `unlink`ing can fail.
    
    A later patch, once we've merged the taintconf patches for RTO, will actually make use of this - the intention was to nail down the API and ensure I'm wrangling OTel correctly here.
    
    Test plan: This doesn't do anything meaningful yet, so, just CI.
    
    synced from Pro ea09a4d53d5f1d648eadfefc6ffd134383e8803a

--- src/core/tests/Unit_disk_cache.ml
+++ src/core/tests/Unit_disk_cache.ml
@@ -54,7 +54,7 @@ let test_round_trip () =
       let handle = write_ok cache "key1" 42 in
       let v = read_ok handle in
       Alcotest.(check int) "round-trip value" 42 v;
-      Int_cache.rm handle)
+      Int_cache.rm handle |> ignore)
 
 let test_closure_round_trip () =
   with_cache (fun cache ->
@@ -66,14 +66,14 @@ let test_closure_round_trip () =
           | Error _ -> Alcotest.fail "read closure failed"
           | Ok f' ->
               Alcotest.(check int) "closure result" 42 (f' 41);
-              Fn_cache.rm handle))
+              Fn_cache.rm handle |> ignore))
 
 let test_rm_deletes_file () =
   with_cache (fun cache ->
       let handle = write_ok cache "key_rm" 99 in
       (* verify we can read it *)
       let _ = read_ok handle in
-      Int_cache.rm handle;
+      Int_cache.rm handle |> ignore;
       (* reading after rm should fail *)
       match Int_cache.read handle with
       | Error _ -> ()
@@ -87,8 +87,8 @@ let test_different_keys_different_files () =
       let v2 = read_ok h2 in
       Alcotest.(check int) "first value" 1 v1;
       Alcotest.(check int) "second value" 2 v2;
-      Int_cache.rm h1;
-      Int_cache.rm h2)
+      Int_cache.rm h1 |> ignore;
+      Int_cache.rm h2 |> ignore)
 
 let test_same_key_overwrites () =
   with_cache (fun cache ->
@@ -96,7 +96,7 @@ let test_same_key_overwrites () =
       let h2 = write_ok cache "same_key" 2 in
       let v = read_ok h2 in
       Alcotest.(check int) "overwritten value" 2 v;
-      Int_cache.rm h2)
+      Int_cache.rm h2 |> ignore)
 
 (*****************************************************************************)
 (* Entry point *)
