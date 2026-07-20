d3a28af2fbbd937a0a3eb502ed2d6c6cfa469a55
Iago Abal <iago@semgrep.com>
2026-07-20 17:25:01 +0200

    tainting: Split OSS/Pro shape & effect types (semgrep/semgrep-proprietary#6769)
    synced from Pro 27e5673f4e0967d907d39f9cdcb869f147b14e2e

--- src/tainting/OSS_taint_input_env.mli
+++ src/tainting/OSS_taint_input_env.mli
@@ -13,18 +13,18 @@
 (** Input environments for taint analysis. *)
 
 val mk_fun_input_env :
-  Taint_rule_inst.t ->
+  OSS_taint_rule_inst.t ->
   ?glob_env:OSS_taint_lval_env.t ->
   IL.param list ->
-  OSS_taint_lval_env.t * Shape_and_sig.Effects.t
+  OSS_taint_lval_env.t * OSS_taint_effects.Effects.t
 (** Constructs the initial taint environment for a given function definition.
     Essentially, it records the parameters that are taint sources, or whose
     default value is a taint source.
     It is exposed to be used by inter-file taint analysis in Pro.  *)
 
 val mk_file_env :
-  Taint_rule_inst.t ->
+  OSS_taint_rule_inst.t ->
   AST_generic.program ->
-  OSS_taint_lval_env.t * Shape_and_sig.Effects.t
+  OSS_taint_lval_env.t * OSS_taint_effects.Effects.t
 (** Constructs the global taint environment for a given file: it finds global-
   and class- variable definitions that are final, and checks if they are tainted. *)
